5c1450406ee9268db8662d5cad188af858723788
Lukasz Czyz <lukasz.czyzz@gmail.com>
2022-07-15 11:40:07 +0200

    SLIM - fix license headers.

--- intern/slim/slim_capi.h
+++ intern/slim/slim_capi.h
@@ -1,32 +1,6 @@
-/*
- * ***** BEGIN GPL LICENSE BLOCK *****
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * The Original Code is Copyright (C) 2001-2002 by NaN Holding BV.
- * All rights reserved.
- *
- * The Original Code is: all of this file.
- *
- * Contributor(s): Aurel Gruber
- *
- * ***** END GPL LICENSE BLOCK *****
- */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 
-#ifndef slim_capi_h
-#define slim_capi_h
+#pragma once
 
 #include "slim_matrix_transfer.h"
 
@@ -63,5 +37,3 @@ void SLIM_free_data(void *slim_data_ptr);
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* slim_capi_h */
