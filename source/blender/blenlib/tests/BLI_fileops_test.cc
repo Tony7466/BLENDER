04359b840cbb6305b430c262c163c46b23f5c71c
SonnyCampbell <sonny.campbell@unity3d.com>
2023-01-05 12:27:41 +0000

    T99807: Separate logic for changing working directory.
    Differential Revision: https://developer.blender.org/D16922

--- source/blender/blenlib/tests/BLI_fileops_test.cc
+++ source/blender/blenlib/tests/BLI_fileops_test.cc
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
 #include "BLI_fileops.hh"
+#include "BLI_path_util.h"
+#include "BLI_string.h"
 
 #include "testing/testing.h"
 
@@ -35,6 +37,37 @@ TEST(fileops, fstream_open_charptr_filename)
   in.close(); /* This should not crash. */
 
   /* Reading the file not tested here. That's deferred to `std::fstream` anyway. */
+}
+
+TEST(fileops, change_working_directory)
+{
+  char original_wd[FILE_MAX];
+  BLI_current_working_dir(original_wd, FILE_MAX);
+
+  char temp_wd[FILE_MAX];
+  BLI_path_join(temp_wd, FILE_MAX, original_wd, "test_temp");
+
+  if (BLI_exists(temp_wd)) {
+    BLI_delete(temp_wd, true, false);
+  }
+
+  bool result = BLI_change_working_dir(temp_wd);
+  ASSERT_FALSE(result);
+
+  BLI_dir_create_recursive(temp_wd);
+
+  result = BLI_change_working_dir(temp_wd);
+  ASSERT_TRUE(result);
+
+  char cwd[FILE_MAX];
+  BLI_current_working_dir(cwd, FILE_MAX);
+
+  ASSERT_TRUE(BLI_path_cmp(cwd, temp_wd) == 0);
+
+  result = BLI_change_working_dir(original_wd);
+  ASSERT_TRUE(result);
+
+  BLI_delete(temp_wd, true, false);
 }
 
 }  // namespace blender::tests
