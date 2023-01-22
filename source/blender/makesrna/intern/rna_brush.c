01c579db5487c1c431c8a2b3b18d78d89a0813ff
Robin Hohnsbeen <robin@hohnsbeen.de>
2023-01-22 11:15:35 +0100

    Sculpt: Added vector displacement for sculpting draw brush (area plane method only for now)
    Vector displacement maps (VDM) provide a way to create complex displacements that can have overhangs in one brush dab.
    This is unlike standard displacement with height maps that only displace in normal direction.
    Forms like ears, curled horns, etc can be created in one click if VMDs are used.
    
    This patch reads the RGB channels of a texture in a brush stroke and interprets them as individual vectors, that are used for the vertex offset.
    As of now, this is only working for the draw brush using the area plane method. Symmetry and radial symmetry is working.
    
    Sculpt: Store inverse of brush_local_mat beforehand so it won't be calculated for every displaced vertex
    
    Brush_local_mat_inv is needed to transform vectors of a vertex displacement map back to object space.
    
    Sculpt: Added hint in tooltip that vector displacement works only with area plane mapping
    
    Differential Revision: https://developer.blender.org/D17080

--- source/blender/makesrna/intern/rna_brush.c
+++ source/blender/makesrna/intern/rna_brush.c
@@ -2896,7 +2896,7 @@ static void rna_def_brush(BlenderRNA *brna)
   prop = RNA_def_property(srna, "use_rgb_as_vector_displacement", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, NULL, "flag2", BRUSH_RGB_AS_VECTOR_DISPLACEMENT);
   RNA_def_property_ui_text(prop, "RGB as vector displacement",
-"Handles each pixel as individual vector for displacement instead of an offset in normal direction");
+"Handles each pixel as individual vector for displacement. Works only with area plane mapping");
   RNA_def_property_update(prop, 0, "rna_Brush_update");
 
   prop = RNA_def_property(srna, "normal_weight", PROP_FLOAT, PROP_FACTOR);
