658eb946a08f92a88d462f67864301ee44b374e9
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-02-03 11:41:40 +0100

    first light

--- source/blender/editors/space_graph/graph_intern.h
+++ source/blender/editors/space_graph/graph_intern.h
@@ -111,6 +111,7 @@ void GRAPH_OT_paste(struct wmOperatorType *ot);
 void GRAPH_OT_duplicate(struct wmOperatorType *ot);
 void GRAPH_OT_delete(struct wmOperatorType *ot);
 void GRAPH_OT_clean(struct wmOperatorType *ot);
+void GRAPH_OT_slider_choice(struct wmOperatorType *ot);
 void GRAPH_OT_blend_to_neighbor(struct wmOperatorType *ot);
 void GRAPH_OT_breakdown(struct wmOperatorType *ot);
 void GRAPH_OT_ease(struct wmOperatorType *ot);
