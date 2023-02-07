b1033913f7a43648f60e056ba9faf22141130359
Hans Goudey <h.goudey@me.com>
2023-02-07 13:35:41 -0500

    Fix #100957: Dyntopo shows false positive data loss warnings
    Because of #95965, some attributes are stored as generic attributes
    in Mesh but have special handling for the conversion to BMesh.
    
    Expose a function to tell whether certain attribute names are handled
    specially in the conversion, and refactor the error checking process
    to use it. Also check for generic attributes on the face domain which
    wasn't done before.

--- source/blender/editors/sculpt_paint/sculpt_dyntopo.cc
+++ source/blender/editors/sculpt_paint/sculpt_dyntopo.cc
@@ -274,15 +274,14 @@ static int sculpt_dynamic_topology_toggle_exec(bContext *C, wmOperator * /*op*/)
 
   return OPERATOR_FINISHED;
 }
-
 static int dyntopo_warning_popup(bContext *C, wmOperatorType *ot, enum eDynTopoWarnFlag flag)
 {
   uiPopupMenu *pup = UI_popup_menu_begin(C, IFACE_("Warning!"), ICON_ERROR);
   uiLayout *layout = UI_popup_menu_layout(pup);
 
   if (flag & (DYNTOPO_WARN_VDATA | DYNTOPO_WARN_EDATA | DYNTOPO_WARN_LDATA)) {
-    const char *msg_error = TIP_("Vertex Data Detected!");
-    const char *msg = TIP_("Dyntopo will not preserve vertex colors, UVs, or other customdata");
+    const char *msg_error = TIP_("Attribute Data Detected");
+    const char *msg = TIP_("Dyntopo will not preserve colors, UVs, or other attributes");
     uiItemL(layout, msg_error, ICON_INFO);
     uiItemL(layout, msg, ICON_NONE);
     uiItemS(layout);
@@ -305,6 +304,24 @@ static int dyntopo_warning_popup(bContext *C, wmOperatorType *ot, enum eDynTopoW
   return OPERATOR_INTERFACE;
 }
 
+static bool dyntopo_supports_customdata_type(const CustomDataLayer &layer)
+{
+  if (CD_TYPE_AS_MASK(layer.type) & CD_MASK_PROP_ALL) {
+    if (layer.name[0] == '\0') {
+      return false;
+    }
+    /* Some data is stored as generic attributes on #Mesh but in flags or field on #BMesh. */
+    return BM_attribute_stored_in_bmesh_builtin(layer.name);
+  }
+  /* Some layers just encode #Mesh topology or are handled as special cases for dyntopo. */
+  return ELEM(layer.type, CD_MEDGE, CD_MFACE, CD_MLOOP, CD_MPOLY, CD_PAINT_MASK, CD_ORIGINDEX);
+}
+
+static bool dyntopo_supports_customdata_layers(const blender::Span<CustomDataLayer> layers)
+{
+  return std::all_of(layers.begin(), layers.end(), dyntopo_supports_customdata_type);
+}
+
 enum eDynTopoWarnFlag SCULPT_dynamic_topology_check(Scene *scene, Object *ob)
 {
   Mesh *me = static_cast<Mesh *>(ob->data);
@@ -315,18 +332,17 @@ enum eDynTopoWarnFlag SCULPT_dynamic_topology_check(Scene *scene, Object *ob)
   BLI_assert(ss->bm == nullptr);
   UNUSED_VARS_NDEBUG(ss);
 
-  for (int i = 0; i < CD_NUMTYPES; i++) {
-    if (!ELEM(i, CD_MEDGE, CD_MFACE, CD_MLOOP, CD_MPOLY, CD_PAINT_MASK, CD_ORIGINDEX)) {
-      if (CustomData_has_layer(&me->vdata, i)) {
-        flag |= DYNTOPO_WARN_VDATA;
-      }
-      if (CustomData_has_layer(&me->edata, i)) {
-        flag |= DYNTOPO_WARN_EDATA;
-      }
-      if (CustomData_has_layer(&me->ldata, i)) {
-        flag |= DYNTOPO_WARN_LDATA;
-      }
-    }
+  if (!dyntopo_supports_customdata_layers({me->vdata.layers, me->vdata.totlayer})) {
+    flag |= DYNTOPO_WARN_VDATA;
+  }
+  if (!dyntopo_supports_customdata_layers({me->edata.layers, me->edata.totlayer})) {
+    flag |= DYNTOPO_WARN_EDATA;
+  }
+  if (!dyntopo_supports_customdata_layers({me->pdata.layers, me->pdata.totlayer})) {
+    flag |= DYNTOPO_WARN_LDATA;
+  }
+  if (!dyntopo_supports_customdata_layers({me->ldata.layers, me->ldata.totlayer})) {
+    flag |= DYNTOPO_WARN_LDATA;
   }
 
   {
