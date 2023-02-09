b0c5527df2f64ab993a30a4115d65e8627b680d4
Jacques Lucke <jacques@blender.org>
2023-02-09 19:09:55 +0100

    progress

--- source/blender/blenlib/tests/BLI_index_mask2_test.cc
+++ source/blender/blenlib/tests/BLI_index_mask2_test.cc
@@ -2,18 +2,24 @@
 
 #include "BLI_index_mask2.hh"
 #include "BLI_strict_flags.h"
+#include "BLI_timeit.hh"
 
 #include "testing/testing.h"
 
 namespace blender::tests {
 
+IndexMask2 mask;
+
 TEST(index_mask2, Test)
 {
-  IndexMaskForRange mask_for_range(IndexRange((1 << 16) - 10, 30));
-  IndexMask2 mask = mask_for_range;
-  for (const int64_t i : mask) {
-    std::cout << i << "\n";
+  for ([[maybe_unused]] const int64_t i : IndexRange(5)) {
+    SCOPED_TIMER("test");
+    IndexMaskForRange mask_for_range(IndexRange(100'000'000));
+    mask = mask_for_range;
   }
+  // for (const int64_t i : mask) {
+  //   std::cout << i << "\n";
+  // }
 }
 
 }  // namespace blender::tests
