96aa7ab36743bb28804ff42eef867104b61812be
DESKTOP-ON14TH5\Sonny Campbell <sonny.campbell@unity3d.com>
2023-02-09 15:55:42 +0000

    T99807: Separate logic for changing working directory.
    Set dir parameter to const for BLI_change_working_dir.
    Added non-ASCII characters to the test directory.
    
    Mac and Linux fixes. Linux now sets PWD when using BLI_change_working_dir. Mac uses NSFileManager through some new Mac-only wrapper functions.
    
    Changed temporary directory to use a directory in the flags_test_asset_dir.
    Ensure that operation to change working directory is from the main thread.

--- source/blender/blenlib/intern/storage.c
+++ source/blender/blenlib/intern/storage.c
@@ -54,7 +54,32 @@
 #include "BLI_linklist.h"
 #include "BLI_path_util.h"
 #include "BLI_string.h"
+#include "BLI_threads.h"
 #include "BLI_utildefines.h"
+
+bool BLI_change_working_dir(const char *dir)
+{
+  BLI_assert(BLI_thread_is_main());
+
+  if (!BLI_is_dir(dir)) {
+    return false;
+  }
+#if defined(WIN32)
+  wchar_t wdir[FILE_MAX];
+  if (conv_utf_8_to_16(dir, wdir, ARRAY_SIZE(wdir)) != 0) {
+    return false;
+  }
+  return _wchdir(wdir) == 0;
+#elif defined(__APPLE__)
+  return BLI_apple_chdir(dir) == 0;
+#else
+  int result = chdir(dir);
+  if (result == 0) {
+    BLI_setenv("PWD", dir);
+  }
+  return result == 0;
+#endif
+}
 
 char *BLI_current_working_dir(char *dir, const size_t maxncpy)
 {
@@ -66,6 +91,9 @@ char *BLI_current_working_dir(char *dir, const size_t maxncpy)
     }
   }
   return NULL;
+#elif defined(__APPLE__)
+  BLI_strncpy(dir, BLI_apple_getcwd(maxncpy), maxncpy);
+  return dir;
 #else
   const char *pwd = BLI_getenv("PWD");
   if (pwd) {
--- source/blender/blenlib/tests/BLI_fileops_test.cc
+++ source/blender/blenlib/tests/BLI_fileops_test.cc
@@ -1,8 +1,11 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
-#include "BLI_fileops.hh"
-
 #include "testing/testing.h"
+
+#include "BLI_fileops.hh"
+#include "BLI_path_util.h"
+#include "BLI_string.h"
+#include "BLI_threads.h"
 
 namespace blender::tests {
 
@@ -35,6 +38,41 @@ TEST(fileops, fstream_open_charptr_filename)
   in.close(); /* This should not crash. */
 
   /* Reading the file not tested here. That's deferred to `std::fstream` anyway. */
+}
+
+TEST(fileops, change_working_directory)
+{
+  BLI_threadapi_init();
+
+  char original_wd[FILE_MAX];
+  BLI_current_working_dir(original_wd, FILE_MAX);
+
+  const std::string test_temp_dir = blender::tests::flags_test_asset_dir() + "/" +
+                                    "fileops_test_temp_новый";
+
+  if (BLI_exists(test_temp_dir.c_str())) {
+    BLI_delete(test_temp_dir.c_str(), true, false);
+  }
+
+  bool result = BLI_change_working_dir(test_temp_dir.c_str());
+  ASSERT_FALSE(result);
+
+  BLI_dir_create_recursive(test_temp_dir.c_str());
+
+  result = BLI_change_working_dir(test_temp_dir.c_str());
+  ASSERT_TRUE(result);
+
+  char cwd[FILE_MAX];
+  BLI_current_working_dir(cwd, FILE_MAX);
+
+  ASSERT_TRUE(BLI_path_cmp_normalized(cwd, test_temp_dir.c_str()) == 0);
+
+  result = BLI_change_working_dir(original_wd);
+  ASSERT_TRUE(result);
+
+  BLI_delete(test_temp_dir.c_str(), true, false);
+
+  BLI_threadapi_exit();
 }
 
 }  // namespace blender::tests
