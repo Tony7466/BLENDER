547a196c01b65bc352d4d15d6be803dd1fd578eb
Falk David <falkdavid@gmx.de>
2023-02-11 16:28:38 +0100

    Curves: Add support for proportional editing

--- source/blender/editors/transform/transform_convert_curves.cc
+++ source/blender/editors/transform/transform_convert_curves.cc
@@ -28,6 +28,7 @@ static void createTransCurvesVerts(bContext * /*C*/, TransInfo *t)
   MutableSpan<TransDataContainer> trans_data_contrainers(t->data_container, t->data_container_len);
   Array<Vector<int64_t>> selected_indices_per_object(t->data_container_len);
   Array<IndexMask> selection_per_object(t->data_container_len);
+  const bool is_prop_edit = (t->flag & T_PROP_EDIT) != 0;
 
   /* Count selected elements per object and create TransData structs. */
   for (const int i : trans_data_contrainers.index_range()) {
@@ -35,10 +36,15 @@ static void createTransCurvesVerts(bContext * /*C*/, TransInfo *t)
     Curves *curves_id = static_cast<Curves *>(tc.obedit->data);
     bke::CurvesGeometry &curves = curves_id->geometry.wrap();
 
-    selection_per_object[i] = ed::curves::retrieve_selected_points(curves,
-                                                                   selected_indices_per_object[i]);
+    if (is_prop_edit) {
+      tc.data_len = curves.point_num;
+    }
+    else {
+      selection_per_object[i] = ed::curves::retrieve_selected_points(
+          curves, selected_indices_per_object[i]);
+      tc.data_len = selection_per_object[i].size();
+    }
 
-    tc.data_len = selection_per_object[i].size();
     if (tc.data_len > 0) {
       tc.data = MEM_cnew_array<TransData>(tc.data_len, __func__);
     }
@@ -52,28 +58,64 @@ static void createTransCurvesVerts(bContext * /*C*/, TransInfo *t)
     }
     Curves *curves_id = static_cast<Curves *>(tc.obedit->data);
     bke::CurvesGeometry &curves = curves_id->geometry.wrap();
-    IndexMask selected_indices = selection_per_object[i];
 
     float mtx[3][3], smtx[3][3];
     copy_m3_m4(mtx, tc.obedit->object_to_world);
     pseudoinverse_m3_m3(smtx, mtx, PSEUDOINVERSE_EPSILON);
 
     MutableSpan<float3> positions = curves.positions_for_write();
-    threading::parallel_for(selected_indices.index_range(), 1024, [&](const IndexRange range) {
-      for (const int selection_i : range) {
-        TransData *td = &tc.data[selection_i];
-        float *elem = reinterpret_cast<float *>(&positions[selected_indices[selection_i]]);
-        copy_v3_v3(td->iloc, elem);
-        copy_v3_v3(td->center, td->iloc);
-        td->loc = elem;
+    if (is_prop_edit) {
+      OffsetIndices<int> points_by_curve = curves.points_by_curve();
+      VArray<bool> selection = curves.attributes().lookup_or_default<bool>(
+          ".selection", ATTR_DOMAIN_POINT, true);
+      threading::parallel_for(curves.curves_range(), 1024, [&](const IndexRange range) {
+        for (const int curve_i : range) {
+          bool has_any_selected = false;
+          for (const int point_i : points_by_curve[curve_i]) {
+            TransData *td = &tc.data[point_i];
+            float *elem = reinterpret_cast<float *>(&positions[point_i]);
+            copy_v3_v3(td->iloc, elem);
+            copy_v3_v3(td->center, td->iloc);
+            td->loc = elem;
 
-        td->flag = TD_SELECTED;
-        td->ext = nullptr;
+            if (selection[point_i]) {
+              has_any_selected = true;
+            }
 
-        copy_m3_m3(td->smtx, smtx);
-        copy_m3_m3(td->mtx, mtx);
-      }
-    });
+            td->flag = (selection[point_i]) ? TD_SELECTED : 0;
+            td->ext = nullptr;
+
+            copy_m3_m3(td->smtx, smtx);
+            copy_m3_m3(td->mtx, mtx);
+          }
+
+          if (!has_any_selected) {
+            for (const int point_i : points_by_curve[curve_i]) {
+              TransData *td = &tc.data[point_i];
+              td->flag |= TD_NOTCONNECTED;
+            }
+          }
+        }
+      });
+    }
+    else {
+      IndexMask selected_indices = selection_per_object[i];
+      threading::parallel_for(selected_indices.index_range(), 1024, [&](const IndexRange range) {
+        for (const int selection_i : range) {
+          TransData *td = &tc.data[selection_i];
+          float *elem = reinterpret_cast<float *>(&positions[selected_indices[selection_i]]);
+          copy_v3_v3(td->iloc, elem);
+          copy_v3_v3(td->center, td->iloc);
+          td->loc = elem;
+
+          td->flag = TD_SELECTED;
+          td->ext = nullptr;
+
+          copy_m3_m3(td->smtx, smtx);
+          copy_m3_m3(td->mtx, mtx);
+        }
+      });
+    }
   }
 }
 
