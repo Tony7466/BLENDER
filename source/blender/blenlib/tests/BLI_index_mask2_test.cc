169acd0fdb9631ca24fc0390b153cac5dfe9ea2e
Jacques Lucke <jacques@blender.org>
2023-02-11 10:55:51 +0100

    remove old code

--- source/blender/blenlib/tests/BLI_index_mask2_test.cc
+++ source/blender/blenlib/tests/BLI_index_mask2_test.cc
@@ -6,20 +6,10 @@
 
 #include "testing/testing.h"
 
-namespace blender::tests {
-
-IndexMask2 mask;
+namespace blender::index_mask::tests {
 
 TEST(index_mask2, Test)
 {
-  for ([[maybe_unused]] const int64_t i : IndexRange(5)) {
-    SCOPED_TIMER("test");
-    IndexMaskForRange mask_for_range(IndexRange(100'000'000));
-    mask = mask_for_range;
-  }
-  // for (const int64_t i : mask) {
-  //   std::cout << i << "\n";
-  // }
 }
 
-}  // namespace blender::tests
+}  // namespace blender::index_mask::tests
