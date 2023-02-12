0976bcb313f86825a3b6921d3bb7282364cc09cf
Joseph Eagar <joeedh@gmail.com>
2023-02-12 12:41:59 -0800

    mempool_asan: Remove extraneous # character

--- source/blender/blenlib/intern/BLI_mempool.c
+++ source/blender/blenlib/intern/BLI_mempool.c
@@ -19,7 +19,7 @@
 #include "atomic_ops.h"
 
 #include "BLI_utildefines.h"
-#
+
 #include "BLI_asan.h"
 #include "BLI_mempool.h"         /* own include */
 #include "BLI_mempool_private.h" /* own include */
