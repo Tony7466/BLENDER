cbd2b17b7d95bea7100672648671a31ad965ee7a
Sergey Sharybin <sergey@blender.org>
2023-02-21 16:15:24 +0100

    Remove duplicated assignment of kintegrator->use_light_tree
    Also added comment why this is fine.

--- intern/cycles/scene/integrator.cpp
+++ intern/cycles/scene/integrator.cpp
@@ -255,8 +255,9 @@ void Integrator::device_update(Device *device, DeviceScene *dscene, Scene *scene
   kintegrator->scrambling_distance = scrambling_distance;
   kintegrator->sobol_index_mask = reverse_integer_bits(next_power_of_two(aa_samples - 1) - 1);
 
-  kintegrator->use_light_tree = scene->integrator->get_use_light_tree() &&
-                                device->info.has_light_tree;
+  /* NOTE: The kintegrator->use_light_tree is assigned th the efficient value in the light manager,
+   * and the synchronization code is expected to tag the light manager for update when the
+   * `use_light_tree` is changed. */
   if (light_sampling_threshold > 0.0f && !kintegrator->use_light_tree) {
     kintegrator->light_inv_rr_threshold = scene->film->get_exposure() / light_sampling_threshold;
   }
