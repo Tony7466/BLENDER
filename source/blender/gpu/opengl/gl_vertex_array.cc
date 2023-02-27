cd8e6c214b3c8fa962d1b9d96aff4c1019f85c7d
Miguel Pozo <pragma37@gmail.com>
2023-02-27 18:33:27 +0100

    Support draw_resource_id_with_thin_fallback in the OGL backend

--- source/blender/gpu/opengl/gl_vertex_array.cc
+++ source/blender/gpu/opengl/gl_vertex_array.cc
@@ -121,12 +121,16 @@ void GLVertArray::update_bindings(const GLuint vao,
 
   if (batch->resource_id_buf) {
     const ShaderInput *input = interface->attr_get("drw_ResourceID");
+    int size = 1;
+    if (input == nullptr) {
+      input = interface->attr_get("vertex_in_drw_ResourceID_");
+      size = 2;
+    }
     if (input) {
       dynamic_cast<GLStorageBuf *>(unwrap(batch->resource_id_buf))->bind_as(GL_ARRAY_BUFFER);
       glEnableVertexAttribArray(input->location);
       glVertexAttribDivisor(input->location, 1);
-      glVertexAttribIPointer(
-          input->location, 1, to_gl(GPU_COMP_I32), sizeof(uint32_t), (GLvoid *)nullptr);
+      glVertexAttribIPointer(input->location, size, to_gl(GPU_COMP_I32), 0, (GLvoid *)nullptr);
       attr_mask &= ~(1 << input->location);
     }
   }
