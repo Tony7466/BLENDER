10f9e50b278b22915ce9bb639c233721625f4e4e
Prakhar-Singh-Chouhan <prakhar-singh-chouhan@noreply.localhost>
2023-03-07 06:49:03 +0100

    Update 'source/blender/draw/engines/overlay/shaders/overlay_edit_mesh_vert.glsl'

--- source/blender/draw/engines/overlay/shaders/overlay_edit_mesh_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_edit_mesh_vert.glsl
@@ -29,9 +29,8 @@ void main()
   GPU_INTEL_VERTEX_SHADER_WORKAROUND
 
   vec3 world_pos = point_object_to_world(pos);
-  gl_Position = point_world_to_homogenous(world_pos);
   vec3 view_pos = point_world_to_view(world_pos);
-  gl_Position = point_view_to_ndc(view_pos);
+  gl_Position = point_world_to_homogenous(world_pos);
 
   /* Offset Z position for retopology overlay. */
   gl_Position.z += get_homogenous_z_offset(view_pos.z, gl_Position.w, retopologyOffset);
