d0d2d156770dc8726df4cf7587042a6b772d7032
Miguel Pozo <pragma37@gmail.com>
2023-03-07 20:49:27 +0100

    Copy eeevee-rewrite sss implementation

--- source/blender/draw/engines/eevee_next/eevee_instance.hh
+++ source/blender/draw/engines/eevee_next/eevee_instance.hh
@@ -30,6 +30,7 @@
 #include "eevee_sampling.hh"
 #include "eevee_shader.hh"
 #include "eevee_shadow.hh"
+#include "eevee_subsurface.hh"
 #include "eevee_sync.hh"
 #include "eevee_view.hh"
 #include "eevee_world.hh"
@@ -48,6 +49,7 @@ class Instance {
   ShaderModule &shaders;
   SyncModule sync;
   MaterialModule materials;
+  SubsurfaceModule subsurface;
   PipelineModule pipelines;
   ShadowModule shadows;
   LightModule lights;
@@ -94,6 +96,7 @@ class Instance {
       : shaders(*ShaderModule::module_get()),
         sync(*this),
         materials(*this),
+        subsurface(*this),
         pipelines(*this),
         shadows(*this),
         lights(*this),
---
+++ source/blender/draw/engines/eevee_next/eevee_subsurface.hh
@@ -0,0 +1,69 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later
+ * Copyright 2021 Blender Foundation.
+ */
+
+/** \file
+ * \ingroup eevee
+ *
+ */
+
+#pragma once
+
+#include "eevee_shader.hh"
+#include "eevee_shader_shared.hh"
+
+namespace blender::eevee {
+
+/* -------------------------------------------------------------------- */
+/** \name Subsurface
+ *
+ * \{ */
+
+class Instance;
+
+struct SubsurfaceModule {
+ private:
+  Instance &inst_;
+  /** Contains samples locations. */
+  SubsurfaceDataBuf data_;
+  /** Contains translucence profile for a single color channel. */
+  GPUTexture *transmittance_tx = nullptr;
+
+ public:
+  SubsurfaceModule(Instance &inst) : inst_(inst)
+  {
+    /* Force first update. */
+    data_.sample_len = -1;
+  };
+
+  ~SubsurfaceModule()
+  {
+    GPU_TEXTURE_FREE_SAFE(transmittance_tx);
+  };
+
+  void end_sync();
+
+  const GPUUniformBuf *ubo_get(void) const
+  {
+    return data_;
+  }
+
+  GPUTexture **transmittance_ref_get(void)
+  {
+    return &transmittance_tx;
+  }
+
+ private:
+  void precompute_samples_location();
+  void precompute_transmittance_profile();
+
+  /** Christensen-Burley implementation. */
+  static float burley_setup(float radius, float albedo);
+  static float burley_sample(float d, float x_rand);
+  static float burley_eval(float d, float r);
+  static float burley_pdf(float d, float r);
+};
+
+/** \} */
+
+}  // namespace blender::eevee
