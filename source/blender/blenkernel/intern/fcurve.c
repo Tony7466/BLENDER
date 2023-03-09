39433707bee3992d1ce93606ef59f62b24c47ee1
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-03-09 12:20:26 +0100

    fix issues coming from structure change

--- source/blender/blenkernel/intern/fcurve.c
+++ source/blender/blenkernel/intern/fcurve.c
@@ -707,16 +707,16 @@ static bool calculate_fpt_bounds(const FCurve *fcu, const float frame_range[2], 
   if (frame_range != NULL) {
     /* Start index can be calculated because fpt has a key on every full frame. */
     const float start_index_f = frame_range[0] - fcu->fpt[0].vec[0];
-    const float end_index_f = start_index + frame_range[1] - frame_range[0];
+    const float end_index_f = start_index_f + frame_range[1] - frame_range[0];
 
-    if (start_index > fcu->totvert - 1 || end_index < 0) {
+    if (start_index_f > fcu->totvert - 1 || end_index_f < 0) {
       /* Range is outside of keyframe samples. */
       return false;
     }
 
     /* Range might be partially covering keyframe samples. */
-    start_index = clamp_i(start_index, 0, fcu->totvert - 1);
-    end_index = clamp_i(end_index, 0, fcu->totvert - 1);
+    start_index = clamp_i(start_index_f, 0, fcu->totvert - 1);
+    end_index = clamp_i(end_index_f, 0, fcu->totvert - 1);
   }
 
   /* X range can be directly calculated from end verts. */
