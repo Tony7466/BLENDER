7430a4f30285d15f78994192f7b7bfb63b465f03
AgAmemnno <kaz380@hotmail.co.jp>
2023-03-11 00:40:18 +0900

    RESTORE:vk_texture.cc

--- source/blender/gpu/vulkan/vk_texture.cc
+++ source/blender/gpu/vulkan/vk_texture.cc
@@ -150,9 +150,10 @@ bool VKTexture::allocate()
   image_info.mipLevels = 1;
   image_info.arrayLayers = 1;
   image_info.format = to_vk_format(format_);
-  image_info.tiling            = VK_IMAGE_TILING_LINEAR;
+  image_info.tiling = VK_IMAGE_TILING_LINEAR;
   image_info.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-  image_info.usage = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
+  image_info.usage = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
+                     VK_IMAGE_USAGE_STORAGE_BIT;
   image_info.samples = VK_SAMPLE_COUNT_1_BIT;
 
   VkResult result;
