d870d7092b67dd482f0bca32bb48b37bce58f75d
Michael Parkin-White <mparkinwhite@apple.com>
2023-03-13 11:24:26 +0000

    GPU: Add GPU frame capture support.
    Adds two modes of GPU frame capture support for
    enhanced debugging. GPU frame capture begin/end
    allow instantaneous frame capture of all GPU commands
    within the capture boundary.
    
    GPU frame capture scopes allow several user-defined capture
    regions which can wrap key parts of code. These scopes are
    exposed to connected GPU tools allowing the user to manually
    trigger a capture of a known scope at the desired time.
    
    This is currently integrated with the Metal backend for
    support with Xcode.
    
    Related to #105591

--- source/blender/gpu/GPU_debug.h
+++ source/blender/gpu/GPU_debug.h
@@ -29,6 +29,56 @@ void GPU_debug_get_groups_names(int name_buf_len, char *r_name_buf);
  */
 bool GPU_debug_group_match(const char *ref);
 
+/** GPU Frame capture support.
+ * Allows instananeous Frame capture of GPU calls between begin/end. */
+void GPU_debug_capture_begin(void);
+void GPU_debug_capture_end(void);
+
+/** GPU debug frae capture scopes.
+ * Allows creation of a GPU Frame Capture scope that define a region within which an external GPU
+ * Frame capture tool can perform a deferred capture of GPU API calls within the boundary upon user
+ * request. */
+
+/* Returns a pointer wrapping a n API-specific capture scope.
+ * A capture scope should be created a single time and only used within one begin/end pair.*/
+void *GPU_debug_capture_scope_create(const char *name);
+
+/* Used to declare the region within which GPU calls are captured when the scope is triggered.
+ * These functions will return true if the desired tool is actively capturing this scope when
+ * executed. Otherwise, false.*/
+bool GPU_debug_capture_scope_begin(void *scope);
+void GPU_debug_capture_scope_end(void *scope);
+
+/** GPU Debug Capture Usage Example
+ *
+ ** Instant frame capture. **
+ *
+ * #include "GPU_debug.h"
+ * static void do_render_engine(Render *re)
+ * {
+ *   GPU_debug_capture_begin();
+ *   RE_engine_render(re, false);
+ *   GPU_debug_capture_end();
+ * }
+ *
+ *
+ ** Capture Scopes. **
+ *
+ * void *capture_scope = nullptr;
+ * static void do_render_engine(Render *re)
+ * {
+ *   if (!capture_scope) {
+ *     // Create capture scope which will display in external tool.
+ *     capture_scope = GPU_debug_capture_scope_create("Render Frame");
+ *   }
+ *
+ *   // Commands within scope boundary captured when requested in tool.
+ *   GPU_debug_capture_scope_begin(capture_scope);
+ *   RE_engine_render(re, false);
+ *   GPU_debug_capture_scope_end(capture_scope);
+ * }
+ */
+
 #ifdef __cplusplus
 }
 #endif
