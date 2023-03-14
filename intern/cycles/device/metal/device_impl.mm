3d44c8651eba49221e2caf35f0e6a1bffa35b152
Michael Jones <michael_p_jones@apple.com>
2023-03-14 16:27:03 +0000

    Use VLOG_WARNING instead of printf

--- intern/cycles/device/metal/device_impl.mm
+++ intern/cycles/device/metal/device_impl.mm
@@ -568,7 +568,7 @@ void MetalDevice::compile_and_load(int device_id, MetalPipelineType pso_type)
     if (MetalDevice *instance = get_device_by_ID(device_id, lock)) {
       if (mtlLibrary) {
         if (error && [error localizedDescription]) {
-          printf("MSL compilation messages:\n%s\n", [[error localizedDescription] UTF8String]);
+          VLOG_WARNING << "MSL compilation messages: " << [[error localizedDescription] UTF8String];
         }
 
         instance->mtlLibrary[pso_type] = mtlLibrary;
