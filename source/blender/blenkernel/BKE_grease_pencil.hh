7c4c8868d15c8f06281b61a63b966cd9ce672fd8
Falk David <falk@blender.org>
2023-03-17 12:57:15 +0100

    WIP Fix copy and move issue

--- source/blender/blenkernel/BKE_grease_pencil.hh
+++ source/blender/blenkernel/BKE_grease_pencil.hh
@@ -39,11 +39,12 @@ class TreeNode : public ::GreasePencilLayerTreeNode {
     this->type = type;
     this->name = BLI_strdup(name.c_str());
   }
-  TreeNode(const TreeNode &other)
+  TreeNode(const TreeNode &other) : children_(other.children_)
   {
+    this->type = other.type;
     this->name = BLI_strdup(other.name);
   }
-  TreeNode(TreeNode &&other)
+  TreeNode(TreeNode &&other) : children_(std::move(other.children_))
   {
     this->name = other.name;
     other.name = nullptr;
