b22b3f74728c53d21fb93002c60566425a1b7757
Miguel Pozo <pragma37@gmail.com>
2023-03-21 19:14:46 +0100

    draw volume objects

--- source/blender/draw/engines/eevee_next/eevee_instance.cc
+++ source/blender/draw/engines/eevee_next/eevee_instance.cc
@@ -140,7 +140,8 @@ void Instance::scene_sync()
 
 void Instance::object_sync(Object *ob)
 {
-  const bool is_renderable_type = ELEM(ob->type, OB_CURVES, OB_GPENCIL_LEGACY, OB_MESH, OB_LAMP);
+  const bool is_renderable_type = ELEM(
+      ob->type, OB_CURVES, OB_GPENCIL_LEGACY, OB_MESH, OB_LAMP, OB_VOLUME);
   const int ob_visibility = DRW_object_visibility_in_active_context(ob);
   const bool partsys_is_visible = (ob_visibility & OB_VISIBLE_PARTICLES) != 0 &&
                                   (ob->type == OB_MESH);
