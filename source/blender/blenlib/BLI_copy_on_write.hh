bbd7a7f6d334992eec8baefce843b02a9f58c9e7
Jacques Lucke <jacques@blender.org>
2023-03-22 12:52:30 +0100

    initial commit

---
+++ source/blender/blenlib/BLI_copy_on_write.hh
@@ -0,0 +1,96 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/** \file
+ * \ingroup bli
+ */
+
+#include <atomic>
+
+#include "BLI_compiler_attrs.h"
+#include "BLI_utildefines.h"
+#include "BLI_utility_mixins.hh"
+
+/**
+ * #bCopyOnWrite allows implementing copy-on-write behavior, i.e. it allows sharing read-only data
+ * between multiple independend systems (e.g. meshes). The data is only copied when it is shared
+ * and is about to be modified. This is in contrast to making copies before it is actually known
+ * that it is necessary.
+ *
+ * Internally, this is mostly just a glorified reference count. If the reference count is 1, the
+ * data only has a single owner and is mutable. If it is larger than 1, it is shared and must be
+ * logically const.
+ *
+ * On top of containing a reference count, #bCopyOnWrite also knows how to destruct the referenced
+ * data. This is important because the code freeing the data in the end might not know how it was
+ * allocated (for example, it doesn't know whether an array was allocated using the system or
+ * guarded allocator).
+ *
+ * #bCopyOnWrite is used in two ways:
+ * - It can be allocated separately from the referenced data as is typically the case with raw
+ *   arrays (e.g. for mesh attributes).
+ * - It can be embedded into another struct. For that it's best to use #bCopyOnWriteMixin.
+ */
+struct bCopyOnWrite : blender::NonCopyable, blender::NonMovable {
+ private:
+  mutable std::atomic<int> users_;
+
+ public:
+  bCopyOnWrite(const int initial_users) : users_(initial_users)
+  {
+  }
+
+  virtual ~bCopyOnWrite()
+  {
+    BLI_assert(this->is_mutable());
+  }
+
+  bool is_shared() const
+  {
+    return users_.load(std::memory_order_relaxed) >= 2;
+  }
+
+  bool is_mutable() const
+  {
+    return !this->is_shared();
+  }
+
+  void add_user() const
+  {
+    users_.fetch_add(1, std::memory_order_relaxed);
+  }
+
+  void remove_user_and_delete_if_last() const
+  {
+    const int old_user_count = users_.fetch_sub(1, std::memory_order_relaxed);
+    BLI_assert(old_user_count >= 1);
+    const bool was_last_user = old_user_count == 1;
+    if (was_last_user) {
+      const_cast<bCopyOnWrite *>(this)->delete_self_with_data();
+    }
+  }
+
+ private:
+  /** Has to free the #bCopyOnWrite and the referenced data. */
+  virtual void delete_self_with_data() = 0;
+};
+
+/**
+ * Makes it easy to embed copy-on-write behavior into a struct.
+ */
+struct bCopyOnWriteMixin : public bCopyOnWrite {
+ public:
+  bCopyOnWriteMixin() : bCopyOnWrite(1)
+  {
+  }
+
+ private:
+  void delete_self_with_data() override
+  {
+    /* Can't use `delete this` here, because we don't know what allocator was used. */
+    this->delete_self();
+  }
+
+  virtual void delete_self() = 0;
+};
--- source/blender/blenlib/BLI_copy_on_write_user.hh
+++ source/blender/blenlib/BLI_copy_on_write_user.hh
@@ -6,59 +6,55 @@
  * \ingroup bli
  */
 
-#include <atomic>
+#include "BLI_copy_on_write.hh"
 
 namespace blender {
 
-/**
- * A simple automatic reference counter. It is similar to std::shared_ptr, but expects that the
- * reference count is inside the object.
- */
-template<typename T> class UserCounter {
+template<typename T> class COWUser {
  private:
   T *data_ = nullptr;
 
  public:
-  UserCounter() = default;
+  COWUser() = default;
 
-  UserCounter(T *data) : data_(data)
+  COWUser(T *data) : data_(data)
   {
   }
 
-  UserCounter(const UserCounter &other) : data_(other.data_)
+  COWUser(const COWUser &other) : data_(other.data_)
   {
-    this->user_add(data_);
+    this->add_user(data_);
   }
 
-  UserCounter(UserCounter &&other) : data_(other.data_)
+  COWUser(COWUser &&other) : data_(other.data_)
   {
     other.data_ = nullptr;
   }
 
-  ~UserCounter()
+  ~COWUser()
   {
-    this->user_remove(data_);
+    this->remove_user_and_delete_if_last(data_);
   }
 
-  UserCounter &operator=(const UserCounter &other)
+  COWUser &operator=(const COWUser &other)
   {
     if (this == &other) {
       return *this;
     }
 
-    this->user_remove(data_);
+    this->remove_user_and_delete_if_last(data_);
     data_ = other.data_;
-    this->user_add(data_);
+    this->add_user(data_);
     return *this;
   }
 
-  UserCounter &operator=(UserCounter &&other)
+  COWUser &operator=(COWUser &&other)
   {
     if (this == &other) {
       return *this;
     }
 
-    this->user_remove(data_);
+    this->remove_user_and_delete_if_last(data_);
     data_ = other.data_;
     other.data_ = nullptr;
     return *this;
@@ -112,7 +108,7 @@ template<typename T> class UserCounter {
 
   void reset()
   {
-    this->user_remove(data_);
+    this->remove_user_and_delete_if_last(data_);
     data_ = nullptr;
   }
 
@@ -126,29 +122,23 @@ template<typename T> class UserCounter {
     return get_default_hash(data_);
   }
 
-  friend bool operator==(const UserCounter &a, const UserCounter &b)
+  friend bool operator==(const COWUser &a, const COWUser &b)
   {
     return a.data_ == b.data_;
   }
 
-  friend std::ostream &operator<<(std::ostream &stream, const UserCounter &value)
-  {
-    stream << value.data_;
-    return stream;
-  }
-
  private:
-  static void user_add(T *data)
+  static void add_user(T *data)
   {
     if (data != nullptr) {
-      data->user_add();
+      data->add_user();
     }
   }
 
-  static void user_remove(T *data)
+  static void remove_user_and_delete_if_last(T *data)
   {
     if (data != nullptr) {
-      data->user_remove();
+      data->remove_user_and_delete_if_last();
     }
   }
 };
