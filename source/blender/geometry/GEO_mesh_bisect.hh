874346fc4144f02bee8b86411ed30e7887001325
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2023-04-10 19:49:59 +0200

    Decl.

---
+++ source/blender/geometry/GEO_mesh_bisect.hh
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#include "BLI_math_vector_types.hh"
+#include "BLI_span.hh"
+#include "DNA_node_types.h"
+
+struct Mesh;
+
+#include "BKE_anonymous_attribute_id.hh"
+
+namespace blender::geometry {
+
+struct BisectArgs {
+
+  float plane[4];
+  float3 plane_co;
+  float3 plane_no;
+  bool clear_inner;
+  bool clear_outer;
+};
+
+Mesh *bisect_mesh(const Mesh &mesh,
+                  const BisectArgs &args,
+                  const bke::AnonymousAttributePropagationInfo &propagation_info);
+
+}  // namespace blender::geometry
---
+++ source/blender/geometry/intern/mesh_bisect.cc
@@ -0,0 +1,64 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup bke
+ */
+
+#include "BLI_array_utils.hh"
+#include "BLI_bit_vector.hh"
+#include "BLI_math_geom.h"
+
+#include "DNA_mesh_types.h"
+#include "DNA_meshdata_types.h"
+
+#include "BKE_customdata.h"
+#include "BKE_mesh.hh"
+
+#include "GEO_mesh_bisect.hh"
+
+namespace blender::geometry {
+
+Mesh *bisect_mesh(const Mesh &mesh,
+                  const BisectArgs &args,
+                  const bke::AnonymousAttributePropagationInfo &propagation_info)
+{
+
+  /* Compute per-vert distance. */
+  const int src_num_vert = mesh.totvert;
+  const Span<float3> src_positions = mesh.vert_positions();
+  BLI_assert(src_num_vert == src_positions.size());
+
+  Array<float, 12> dist_buffer(src_num_vert);
+  threading::parallel_for(src_positions.index_range(), 512, [&](const IndexRange range) {
+    for (const int64_t i : range) {
+      dist_buffer[i] = dist_signed_to_plane_v3(src_positions[0], args.plane);
+    }
+  });
+
+  /* Compute edge intersections. */
+  const int src_num_edges = mesh.totedge;
+  const Span<MEdge> src_edges = mesh.edges();
+  BLI_assert(src_num_vert == src_edges.size());
+
+  Array<float, 12> edge_insertion_factor(src_num_vert);
+  BitVector<> intersects(src_num_vert, false);
+  threading::parallel_for(src_edges.index_range(), 512, [&](const IndexRange range) {
+    for (const int64_t i : range) {
+      const bool v1_outer = dist_buffer[src_edges[i].v1] > 0.0f;
+      const bool v2_outer = dist_buffer[src_edges[i].v2] > 0.0f;
+      if (v1_outer != v2_outer) {
+        /* Intersects plane */
+        const float abs_d1 = abs(dist_buffer[src_edges[i].v1]);
+        const float tot_dist = abs_d1 + abs(dist_buffer[src_edges[i].v2]);
+        edge_insertion_factor[i] = abs_d1 / tot_dist;
+        intersects[i].set();
+      }
+    }
+  });
+
+  const OffsetIndices src_polys = mesh.polys();
+  const Span<int> src_corner_verts = mesh.corner_verts();
+  const Span<int> src_corner_edges = mesh.corner_edges();
+}
+
+}  // namespace blender::geometry
