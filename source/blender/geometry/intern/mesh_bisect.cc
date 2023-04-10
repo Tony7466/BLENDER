07c1f865f6cdf7951177407db260ce05b7c3d2b0
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2023-04-10 20:09:29 +0200

    no intersect

--- source/blender/geometry/intern/mesh_bisect.cc
+++ source/blender/geometry/intern/mesh_bisect.cc
@@ -42,7 +42,9 @@ Mesh *bisect_mesh(const Mesh &mesh,
 
   Array<float, 12> edge_insertion_factor(src_num_vert);
   BitVector<> intersects(src_num_vert, false);
+  bool no_intersection = true; /* Not atomic: Only set to false. */
   threading::parallel_for(src_edges.index_range(), 512, [&](const IndexRange range) {
+    int counter = 0;
     for (const int64_t i : range) {
       const bool v1_outer = dist_buffer[src_edges[i].v1] > 0.0f;
       const bool v2_outer = dist_buffer[src_edges[i].v2] > 0.0f;
@@ -52,9 +54,14 @@ Mesh *bisect_mesh(const Mesh &mesh,
         const float tot_dist = abs_d1 + abs(dist_buffer[src_edges[i].v2]);
         edge_insertion_factor[i] = abs_d1 / tot_dist;
         intersects[i].set();
+        no_intersection = false;
       }
     }
   });
+
+  /* Handle keep/discard. */
+  if (no_intersection) {
+  }
 
   const OffsetIndices src_polys = mesh.polys();
   const Span<int> src_corner_verts = mesh.corner_verts();
