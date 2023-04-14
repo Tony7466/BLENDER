13eb45e131240d0ad9254a1dd6ad718134181a55
Hans Goudey <h.goudey@me.com>
2023-04-13 22:59:41 -0400

    Fix get_closest_mesh_polys

--- source/blender/nodes/geometry/nodes/node_geo_sample_nearest.cc
+++ source/blender/nodes/geometry/nodes/node_geo_sample_nearest.cc
@@ -149,7 +149,11 @@ static void get_closest_mesh_polys(const Mesh &mesh,
   Array<int> looptri_indices(positions.size());
   get_closest_mesh_looptris(mesh, positions, mask, looptri_indices, r_distances_sq, r_positions);
 
-  array_utils::gather(mesh.looptri_polys(), looptri_indices.as_span(), r_poly_indices);
+  const Span<int> looptri_polys = mesh.looptri_polys();
+
+  for (const int i : mask) {
+    r_poly_indices[i] = looptri_polys[looptri_indices[i]];
+  }
 }
 
 /* The closest corner is defined to be the closest corner on the closest face. */
