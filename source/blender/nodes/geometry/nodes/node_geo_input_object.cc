17a99bb46a0e516b2b1f6cec3425c57a9d3efcde
DB3D <dodododorian@gmail.com>
2023-04-16 01:15:18 +0200

    Fix old convention

--- source/blender/nodes/geometry/nodes/node_geo_input_object.cc
+++ source/blender/nodes/geometry/nodes/node_geo_input_object.cc
@@ -12,15 +12,14 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_output<decl::Object>(N_("Object"));
 }
 
-static void node_layout(uiLayout *layout, bContext *UNUSED(C), PointerRNA *ptr)
+static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
 {
   uiItemR(layout, ptr, "object", 0, "", ICON_NONE);
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
-  Object *object = (Object *)params.node().id;
-  params.set_output("Object", object);
+  params.set_output("Object", reinterpret_cast<Object *>(params.node().id));
 }
 
 } // namespace blender::nodes::node_geo_input_object_cc
@@ -31,7 +30,7 @@ void register_node_type_geo_input_object()
 
   static bNodeType ntype;
 
-  geo_node_type_base(&ntype, GEO_NODE_INPUT_OBJECT, "Object", NODE_CLASS_INPUT, 0);
+  geo_node_type_base(&ntype, GEO_NODE_INPUT_OBJECT, "Object", NODE_CLASS_INPUT);
   ntype.geometry_node_execute = file_ns::node_geo_exec;
   ntype.draw_buttons = file_ns::node_layout;
   ntype.declare = file_ns::node_declare;
