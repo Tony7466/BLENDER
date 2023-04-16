0bda83e97affdd8100eec4613e861c3300ba66b8
Michael Parkin-White <mparkinwhite@apple.com>
2023-04-16 17:37:46 +0100

    Prevent compilation warnings for concurrent shader compilation if building on older versions of macOS.

--- source/blender/gpu/metal/mtl_context.mm
+++ source/blender/gpu/metal/mtl_context.mm
@@ -189,10 +189,12 @@ MTLContext::MTLContext(void *ghost_window, void *ghost_context)
   [this->queue retain];
   [this->device retain];
 
+#if defined(MAC_OS_VERSION_13_3)
   /* Enable increased concurrent shader compiler limit. */
   if (@available(macOS 13.3, *)) {
     [this->device setShouldMaximizeConcurrentCompilation:YES];
   }
+#endif
 
   /* Register present callback. */
   this->ghost_context_->metalRegisterPresentCallback(&present);
