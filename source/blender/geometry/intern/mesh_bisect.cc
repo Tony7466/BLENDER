b81e970ca9b2b5d84b33bdcff0502a284bb6c07b
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2023-04-17 00:49:07 +0200

    Keep / discard

--- source/blender/geometry/intern/mesh_bisect.cc
+++ source/blender/geometry/intern/mesh_bisect.cc
@@ -22,50 +22,127 @@ Mesh *bisect_mesh(const Mesh &mesh,
                   const BisectArgs &args,
                   const bke::AnonymousAttributePropagationInfo &propagation_info)
 {
+  const int src_num_vert = mesh.totvert;
+  const int src_num_edges = mesh.totedge;
+  const int src_num_polys = mesh.totpoly;
 
   /* Compute per-vert distance. */
-  const int src_num_vert = mesh.totvert;
   const Span<float3> src_positions = mesh.vert_positions();
+  IndexRange src_vert_range = src_positions.index_range();
   BLI_assert(src_num_vert == src_positions.size());
 
+  BitVector<> outer_vertex(src_num_vert, false);
   Array<float, 12> dist_buffer(src_num_vert);
-  threading::parallel_for(src_positions.index_range(), 512, [&](const IndexRange range) {
+  threading::parallel_for(src_vert_range, 512, [&](const IndexRange range) {
     for (const int64_t i : range) {
       dist_buffer[i] = dist_signed_to_plane_v3(src_positions[0], args.plane);
+      const bool is_outer = dist_buffer[i] >= 0.0f;
+      outer_vertex[i].set(is_outer);
     }
   });
 
   /* Compute edge intersections. */
-  const int src_num_edges = mesh.totedge;
   const Span<MEdge> src_edges = mesh.edges();
   BLI_assert(src_num_vert == src_edges.size());
 
-  Array<float, 12> edge_insertion_factor(src_num_vert);
-  BitVector<> intersects(src_num_vert, false);
-  bool no_intersection = true; /* Not atomic: Only set to false. */
-  threading::parallel_for(src_edges.index_range(), 512, [&](const IndexRange range) {
-    int counter = 0;
-    for (const int64_t i : range) {
-      const bool v1_outer = dist_buffer[src_edges[i].v1] > 0.0f;
-      const bool v2_outer = dist_buffer[src_edges[i].v2] > 0.0f;
-      if (v1_outer != v2_outer) {
-        /* Intersects plane */
-        const float abs_d1 = abs(dist_buffer[src_edges[i].v1]);
-        const float tot_dist = abs_d1 + abs(dist_buffer[src_edges[i].v2]);
-        edge_insertion_factor[i] = abs_d1 / tot_dist;
-        intersects[i].set();
-        no_intersection = false;
-      }
-    }
-  });
+  Array<float, 12> edge_insertion_factor(src_num_edges);
+  BitVector<> intersects(src_num_edges, false);
+  const int num_edge_intersect = threading::parallel_reduce(
+      src_edges.index_range(),
+      512,
+      0,
+      [&](const IndexRange range) {
+        int intersect_counter = 0;
+        for (const int64_t i : range) {
+          const bool v1_outer = outer_vertex[src_edges[i].v1];
+          const bool v2_outer = outer_vertex[src_edges[i].v2];
+          if (v1_outer != v2_outer) {
+            /* Intersects plane */
+            const float abs_d1 = abs(dist_buffer[src_edges[i].v1]);
+            const float tot_dist = abs_d1 + abs(dist_buffer[src_edges[i].v2]);
+            edge_insertion_factor[i] = abs_d1 / tot_dist;
+            intersects[i].set();
+            intersect_counter++;
+          }
+        }
+        return intersect_counter;
+      },
+      [](int a, int b) { return a + b; });
 
-  /* Handle keep/discard. */
-  if (no_intersection) {
+  /* Handle keep/discard all. */
+  if (num_edge_intersect == 0) {
   }
 
   const OffsetIndices src_polys = mesh.polys();
   const Span<int> src_corner_verts = mesh.corner_verts();
   const Span<int> src_corner_edges = mesh.corner_edges();
+
+  auto keep = [&](int vert_index) {
+    return (outer_vertex[vert_index] && !args.clear_outer) ||
+           (!outer_vertex[vert_index] && !args.clear_inner);
+  };
+
+  /* Create copy masks */
+  /* Vertices */
+  Vector<int> old_to_new_vertex_map(src_num_vert);
+  Vector<int> new_to_old_vertex_map;
+  new_to_old_vertex_map.reserve(src_num_vert);
+  for (const int64_t i : src_vert_range) {
+    if (keep(i)) {
+      old_to_new_vertex_map[i] = new_to_old_vertex_map.size();
+      new_to_old_vertex_map.append(i);
+    }
+  }
+
+  /* Edges */
+  Vector<int> new_to_old_edge_map;
+  new_to_old_edge_map.reserve(src_num_edges);
+  for (const int64_t i : src_edges.index_range()) {
+    if (keep(src_edges[i].v1) && keep(src_edges[i].v2)) {
+      new_to_old_edge_map.append(i);
+    }
+  }
+
+  /* Polygons */
+  Vector<int> new_to_old_poly_map, new_poly_offsets, intersected_poly;
+  new_to_old_poly_map.reserve(src_num_polys);
+  new_poly_offsets.reserve(src_num_polys);
+
+  new_poly_offsets.append(0);
+  for (const int64_t index_poly : src_polys.index_range()) {
+
+    int keep_flag = 0;
+    for (const int64_t index_vert : src_corner_verts.slice(src_polys[index_poly])) {
+      keep_flag |= 1 << keep(index_vert);
+    }
+
+    if (keep_flag == 3) {
+      intersected_poly.append(index_poly);
+    }
+    else if (keep_flag & 2) {
+      new_to_old_poly_map.append(index_poly);
+      new_poly_offsets.append(new_poly_offsets.last() + src_polys[index_poly].size());
+    }
+  }
+
+  /* Create new mesh */
+  // TODO: Does not account for copied counts
+  const int result_nverts = new_to_old_vertex_map.size();
+  const int result_nedges = new_to_old_edge_map.size();
+  const int result_nloops = new_poly_offsets.last();
+  const int result_npolys = new_to_old_poly_map.size();
+
+  Mesh *result = BKE_mesh_new_nomain_from_template(
+      &mesh, result_nverts, result_nedges, result_nloops, result_npolys);
+
+  /* Copy kept data */
+
+  // TODO
+
+  /* Handle intersected data */
+
+  // TODO
+
 }
 
 }  // namespace blender::geometry
