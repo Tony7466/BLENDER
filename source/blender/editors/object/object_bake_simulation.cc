0a0c42752638a23346463cc2c2eb5a989f8e4e75
Jacques Lucke <jacques@blender.org>
2023-04-19 14:40:41 +0200

    support progress reporting

--- source/blender/editors/object/object_bake_simulation.cc
+++ source/blender/editors/object/object_bake_simulation.cc
@@ -11,6 +11,8 @@
 #include "BLI_serialize.hh"
 #include "BLI_vector.hh"
 
+#include "PIL_time.h"
+
 #include "WM_api.h"
 #include "WM_types.h"
 
@@ -26,6 +28,7 @@
 
 #include "BKE_context.h"
 #include "BKE_curves.hh"
+#include "BKE_global.h"
 #include "BKE_instances.hh"
 #include "BKE_lib_id.h"
 #include "BKE_main.h"
@@ -73,30 +76,29 @@ struct ObjectBakeData {
   Vector<ModifierBakeData> modifiers;
 };
 
-static int bake_simulation_exec(bContext *C, wmOperator *op)
+struct BakeSimulationJob {
+  wmWindowManager *wm;
+  Main *bmain;
+  Depsgraph *depsgraph;
+  Scene *scene;
+  Vector<Object *> objects;
+};
+
+static void bake_simulation_job_startjob(void *customdata,
+                                         bool *stop,
+                                         bool *do_update,
+                                         float *progress)
 {
   using namespace bke::sim;
 
-  Scene *scene = CTX_data_scene(C);
-  Depsgraph *depsgraph = CTX_data_depsgraph_pointer(C);
-  Main *bmain = CTX_data_main(C);
-
-  Vector<Object *> objects;
-  if (RNA_boolean_get(op->ptr, "selected")) {
-    CTX_DATA_BEGIN (C, Object *, object, selected_objects) {
-      objects.append(object);
-    }
-    CTX_DATA_END;
-  }
-  else {
-    if (Object *object = CTX_data_active_object(C)) {
-      objects.append(object);
-    }
-  }
+  BakeSimulationJob &job = *static_cast<BakeSimulationJob *>(customdata);
+  G.is_rendering = true;
+  G.is_break = false;
+  WM_set_locked_interface(job.wm, true);
 
   Vector<ObjectBakeData> objects_to_bake;
-  for (Object *object : objects) {
-    if (!BKE_id_is_editable(bmain, &object->id)) {
+  for (Object *object : job.objects) {
+    if (!BKE_id_is_editable(job.bmain, &object->id)) {
       continue;
     }
     ObjectBakeData bake_data;
@@ -108,27 +110,38 @@ static int bake_simulation_exec(bContext *C, wmOperator *op)
           nmd->simulation_cache->reset();
         }
         bake_data.modifiers.append({nmd,
-                                    bke::sim::get_meta_directory(*bmain, *object, *md),
-                                    bke::sim::get_bdata_directory(*bmain, *object, *md)});
+                                    bke::sim::get_meta_directory(*job.bmain, *object, *md),
+                                    bke::sim::get_bdata_directory(*job.bmain, *object, *md)});
       }
     }
     objects_to_bake.append(std::move(bake_data));
   }
 
-  const int old_frame = scene->r.cfra;
+  *progress = 0.0f;
+  *do_update = true;
 
-  for (float frame_f = scene->r.sfra; frame_f <= scene->r.efra; frame_f += 1.0f) {
+  const float frame_step_size = 1.0f;
+  const float progress_per_frame = 1.0f / (float(job.scene->r.efra - job.scene->r.sfra + 1) /
+                                           frame_step_size);
+  const int old_frame = job.scene->r.cfra;
+
+  for (float frame_f = job.scene->r.sfra; frame_f <= job.scene->r.efra;
+       frame_f += frame_step_size) {
     const SubFrame frame{frame_f};
 
-    scene->r.cfra = frame.frame();
-    scene->r.subframe = frame.subframe();
+    if (G.is_break || (stop != nullptr && *stop)) {
+      break;
+    }
+
+    job.scene->r.cfra = frame.frame();
+    job.scene->r.subframe = frame.subframe();
 
     char frame_file_c_str[64];
     BLI_snprintf(frame_file_c_str, sizeof(frame_file_c_str), "%011.5f", double(frame));
     BLI_str_replace_char(frame_file_c_str, '.', '_');
     const StringRefNull frame_file_str = frame_file_c_str;
 
-    BKE_scene_graph_update_for_newframe(depsgraph);
+    BKE_scene_graph_update_for_newframe(job.depsgraph);
 
     for (ObjectBakeData &object_bake_data : objects_to_bake) {
       for (ModifierBakeData &modifier_bake_data : object_bake_data.modifiers) {
@@ -167,6 +180,9 @@ static int bake_simulation_exec(bContext *C, wmOperator *op)
         io::serialize::write_json_file(meta_path, io_root);
       }
     }
+
+    *progress += progress_per_frame;
+    *do_update = true;
   }
 
   for (ObjectBakeData &object_bake_data : objects_to_bake) {
@@ -179,11 +195,70 @@ static int bake_simulation_exec(bContext *C, wmOperator *op)
     DEG_id_tag_update(&object_bake_data.object->id, ID_RECALC_GEOMETRY);
   }
 
-  scene->r.cfra = old_frame;
-  DEG_time_tag_update(bmain);
-  WM_event_add_notifier(C, NC_OBJECT | ND_MODIFIER, nullptr);
+  job.scene->r.cfra = old_frame;
+  DEG_time_tag_update(job.bmain);
 
-  return OPERATOR_FINISHED;
+  *progress = 1.0f;
+  *do_update = true;
+}
+
+static void bake_simulation_job_endjob(void *customdata)
+{
+  BakeSimulationJob &job = *static_cast<BakeSimulationJob *>(customdata);
+  WM_set_locked_interface(job.wm, false);
+  G.is_rendering = false;
+  WM_main_add_notifier(NC_OBJECT | ND_MODIFIER, nullptr);
+}
+
+static int bake_simulation_invoke(bContext *C, wmOperator *op, const wmEvent * /*event*/)
+{
+  wmWindowManager *wm = CTX_wm_manager(C);
+  Scene *scene = CTX_data_scene(C);
+  Depsgraph *depsgraph = CTX_data_depsgraph_pointer(C);
+  Main *bmain = CTX_data_main(C);
+
+  BakeSimulationJob *job = MEM_new<BakeSimulationJob>(__func__);
+  job->wm = wm;
+  job->bmain = bmain;
+  job->depsgraph = depsgraph;
+  job->scene = scene;
+
+  if (RNA_boolean_get(op->ptr, "selected")) {
+    CTX_DATA_BEGIN (C, Object *, object, selected_objects) {
+      job->objects.append(object);
+    }
+    CTX_DATA_END;
+  }
+  else {
+    if (Object *object = CTX_data_active_object(C)) {
+      job->objects.append(object);
+    }
+  }
+
+  wmJob *wm_job = WM_jobs_get(wm,
+                              CTX_wm_window(C),
+                              CTX_data_scene(C),
+                              "Bake Simulation Nodes",
+                              WM_JOB_PROGRESS,
+                              WM_JOB_TYPE_BAKE_SIMULATION_NODES);
+
+  WM_jobs_customdata_set(
+      wm_job, job, [](void *job) { MEM_delete(static_cast<BakeSimulationJob *>(job)); });
+  WM_jobs_timer(wm_job, 0.1, NC_OBJECT | ND_MODIFIER, NC_OBJECT | ND_MODIFIER);
+  WM_jobs_callbacks(
+      wm_job, bake_simulation_job_startjob, nullptr, nullptr, bake_simulation_job_endjob);
+
+  WM_jobs_start(CTX_wm_manager(C), wm_job);
+  WM_event_add_modal_handler(C, op);
+  return OPERATOR_RUNNING_MODAL;
+}
+
+static int bake_simulation_modal(bContext *C, wmOperator * /*op*/, const wmEvent * /*event*/)
+{
+  if (!WM_jobs_test(CTX_wm_manager(C), CTX_data_scene(C), WM_JOB_TYPE_BAKE_SIMULATION_NODES)) {
+    return OPERATOR_FINISHED | OPERATOR_PASS_THROUGH;
+  }
+  return OPERATOR_PASS_THROUGH;
 }
 
 static int delete_baked_simulation_exec(bContext *C, wmOperator *op)
@@ -233,7 +308,8 @@ void OBJECT_OT_simulation_nodes_cache_bake(wmOperatorType *ot)
   ot->description = "Bake simulations in geometry nodes modifiers";
   ot->idname = __func__;
 
-  ot->exec = bake_simulation_exec;
+  ot->invoke = bake_simulation_invoke;
+  ot->modal = bake_simulation_modal;
   ot->poll = bake_simulation_poll;
 
   RNA_def_boolean(ot->srna, "selected", false, "Selected", "Bake cache on all selected objects");
