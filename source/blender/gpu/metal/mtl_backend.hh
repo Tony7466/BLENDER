3b9da304743a32860eb2b46bc6abe01737664e97
Michael Parkin-White <mparkinwhite@apple.com>
2023-04-20 14:53:43 +0100

    Metal: Storage buffer and explicit bind location support
    Adds support for Storage buffers, including changes to the resource
    binding model to ensure explicit resource bind locations are supported
    for all resource types.
    
    Storage buffer support also includes required changes for shader
    source generation and SSBO wrapper support for other resource
    types such as GPUVertBuf, GPUIndexBuf and GPUUniformBuf.
    
    Authored by Apple: Michael Parkin-White

--- source/blender/gpu/metal/mtl_backend.hh
+++ source/blender/gpu/metal/mtl_backend.hh
@@ -52,12 +52,8 @@ class MTLBackend : public GPUBackend {
 
   void samplers_update() override;
   void compute_dispatch(int groups_x_len, int groups_y_len, int groups_z_len) override;
-
-  void compute_dispatch_indirect(StorageBuf *indirect_buf) override
-  {
-    /* Placeholder */
-  }
-
+  void compute_dispatch_indirect(StorageBuf *indirect_buf) override;
+  
   /* MTL Allocators need to be implemented in separate .mm files, due to allocation of Objective-C
    * objects. */
   Context *context_alloc(void *ghost_window, void *ghost_context) override;
