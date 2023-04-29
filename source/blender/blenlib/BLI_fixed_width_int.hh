506b624bfd06179f724ae636125dbab4bb9c9b34
Jacques Lucke <jacques@blender.org>
2023-04-29 09:58:57 +0200

    add signed addition/substraction

--- source/blender/blenlib/BLI_fixed_width_int.hh
+++ source/blender/blenlib/BLI_fixed_width_int.hh
@@ -16,9 +16,7 @@ template<typename T, int S> struct UIntF {
   static_assert(std::is_unsigned_v<T>);
   static_assert(S >= 1);
 
-  static constexpr int Size = S;
-
-  T v[S];
+  std::array<T, S> v;
 
   UIntF() = default;
 
@@ -50,6 +48,49 @@ template<typename T, int S> struct UIntF {
   }
 };
 
+template<typename T, int S> struct IntF {
+  static_assert(std::is_unsigned_v<T>);
+  static_assert(S >= 1);
+
+  std::array<T, S> v;
+
+  IntF() = default;
+
+  IntF(const char *str, const int base = 10)
+  {
+    if (str[0] == '-') {
+      const UIntF<T, S> unsigned_value(str + 1, base);
+      const UIntF<T, S> zero_value("0");
+      const UIntF<T, S> result = zero_value - unsigned_value;
+      this->v = result.v;
+    }
+    else {
+      const UIntF<T, S> unsigned_value(str, base);
+      this->v = unsigned_value.v;
+    }
+  }
+
+  void print(const int base = 10) const
+  {
+    const bool is_negative = (this->v[S - 1] & (T(1) << (8 * sizeof(T) - 1))) != 0;
+    if (is_negative) {
+      std::cout << "-";
+      UIntF<T, S> negated;
+      negated.v = this->v;
+      for (int i = 0; i < S; i++) {
+        negated.v[i] = ~negated.v[i];
+      }
+      negated = negated + UIntF<T, S>("1");
+      negated.print(base);
+    }
+    else {
+      UIntF<T, S> value;
+      value.v = this->v;
+      value.print(base);
+    }
+  }
+};
+
 template<typename T>
 using double_uint_type = std::conditional_t<
     std::is_same_v<T, uint8_t>,
@@ -62,7 +103,7 @@ using double_uint_type = std::conditional_t<
                            std::conditional_t<std::is_same_v<T, uint64_t>, __uint128_t, void>>>>;
 
 using UInt128_8 = UIntF<uint8_t, 16>;
-using Uint128_16 = UIntF<uint16_t, 8>;
+using UInt128_16 = UIntF<uint16_t, 8>;
 using UInt128_32 = UIntF<uint32_t, 4>;
 using UInt128_64 = UIntF<uint64_t, 2>;
 
@@ -73,6 +114,13 @@ using UInt256_64 = UIntF<uint64_t, 4>;
 
 using UInt128 = UInt128_64;
 using UInt256 = UInt256_64;
+
+using Int128_8 = IntF<uint8_t, 16>;
+using Int128_16 = IntF<uint16_t, 8>;
+using Int128_32 = IntF<uint32_t, 4>;
+using Int128_64 = IntF<uint64_t, 2>;
+
+using Int128 = Int128_8;
 
 template<typename T, typename T2, int S> inline void generic_add(T *dst, const T *a, const T *b)
 {
@@ -95,7 +143,8 @@ template<typename T, int S> inline void generic_sub(T *dst, const T *a, const T 
   }
 }
 
-template<typename T, typename T2, int S> inline void generic_mul(T *dst, const T *a, const T *b)
+template<typename T, typename T2, int S>
+inline void generic_unsigned_mul(T *dst, const T *a, const T *b)
 {
   constexpr int shift = 8 * sizeof(T);
 
@@ -123,7 +172,15 @@ template<typename T, int Size, BLI_ENABLE_IF((!std::is_void_v<double_uint_type<T
 inline UIntF<T, Size> operator+(const UIntF<T, Size> &a, const UIntF<T, Size> &b)
 {
   UIntF<T, Size> result;
-  generic_add<T, double_uint_type<T>, Size>(result.v, a.v, b.v);
+  generic_add<T, double_uint_type<T>, Size>(result.v.data(), a.v.data(), b.v.data());
+  return result;
+}
+
+template<typename T, int Size, BLI_ENABLE_IF((!std::is_void_v<double_uint_type<T>>))>
+inline IntF<T, Size> operator+(const IntF<T, Size> &a, const IntF<T, Size> &b)
+{
+  IntF<T, Size> result;
+  generic_add<T, double_uint_type<T>, Size>(result.v.data(), a.v.data(), b.v.data());
   return result;
 }
 
@@ -131,7 +188,15 @@ template<typename T, int Size>
 inline UIntF<T, Size> operator-(const UIntF<T, Size> &a, const UIntF<T, Size> &b)
 {
   UIntF<T, Size> result;
-  generic_sub<T, Size>(result.v, a.v, b.v);
+  generic_sub<T, Size>(result.v.data(), a.v.data(), b.v.data());
+  return result;
+}
+
+template<typename T, int Size>
+inline IntF<T, Size> operator-(const IntF<T, Size> &a, const IntF<T, Size> &b)
+{
+  IntF<T, Size> result;
+  generic_sub<T, Size>(result.v.data(), a.v.data(), b.v.data());
   return result;
 }
 
@@ -139,7 +204,7 @@ template<typename T, int Size, BLI_ENABLE_IF((!std::is_void_v<double_uint_type<T
 inline UIntF<T, Size> operator*(const UIntF<T, Size> &a, const UIntF<T, Size> &b)
 {
   UIntF<T, Size> result;
-  generic_mul<T, double_uint_type<T>, Size>(result.v, a.v, b.v);
+  generic_unsigned_mul<T, double_uint_type<T>, Size>(result.v.data(), a.v.data(), b.v.data());
   return result;
 }
 
