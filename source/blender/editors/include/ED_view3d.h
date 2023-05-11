18ee967658b7a5fac670851e2cd9fe789ffc6432
Jeroen Bakker <jeroen@blender.org>
2023-05-11 07:52:42 +0200

    macOS Metal viewport: Blocking artefacts on selected faces
    Previous fix 22e4e973104 worked on low density screens, but failed
    when using hi-density or hi resolution. This PR increases the
    offset to check for better results.
    
    NOTE: when the numbers have been found we should check if we
    need to make the offset scale based on the viewport resolution.
    
    Ref: #105830

--- source/blender/editors/include/ED_view3d.h
+++ source/blender/editors/include/ED_view3d.h
@@ -1332,8 +1332,8 @@ void ED_view3d_shade_update(struct Main *bmain, struct View3D *v3d, struct ScrAr
   (((overlay).edit_flag & V3D_OVERLAY_EDIT_RETOPOLOGY) != 0)
 #ifdef __APPLE__
 /* Apple silicon tile depth test requires a higher value to reduce drawing artifacts.*/
-#  define OVERLAY_RETOPOLOGY_MIN_OFFSET_ENABLED 0.0015f
-#  define OVERLAY_RETOPOLOGY_MIN_OFFSET_DISABLED 0.0015f
+#  define OVERLAY_RETOPOLOGY_MIN_OFFSET_ENABLED 0.0045f
+#  define OVERLAY_RETOPOLOGY_MIN_OFFSET_DISABLED 0.0045f
 #else
 #  define OVERLAY_RETOPOLOGY_MIN_OFFSET_ENABLED FLT_EPSILON
 #  define OVERLAY_RETOPOLOGY_MIN_OFFSET_DISABLED 0.0f
