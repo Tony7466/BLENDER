01135a85164099dd31ac22206f03bd4384b3f915
Jeroen Bakker <j.bakker@atmind.nl>
2023-05-15 21:13:21 +0200

    Use correct parameters

--- source/blender/gpu/vulkan/vk_data_conversion_test.cc
+++ source/blender/gpu/vulkan/vk_data_conversion_test.cc
@@ -12,12 +12,12 @@ TEST(VulkanDataConversion, ConvertF32F16)
 
   uint32_t f32_3 = 0b01000000010000000000000000000000;
   uint32_t f16_3_expected = 0b0100001000000000;
-  uint32_t f16_3 = convert_float_formats<Format16F, Format32F>(f32_2);
+  uint32_t f16_3 = convert_float_formats<Format16F, Format32F>(f32_3);
   EXPECT_EQ(f16_3, f16_3_expected);
 
   uint32_t f32_4 = 0b01000000100000000000000000000000;
   uint32_t f16_4_expected = 0b0100010000000000;
-  uint32_t f16_4 = convert_float_formats<Format16F, Format32F>(f32_2);
+  uint32_t f16_4 = convert_float_formats<Format16F, Format32F>(f32_4);
   EXPECT_EQ(f16_4, f16_4_expected);
 }
 }  // namespace blender::gpu::tests
