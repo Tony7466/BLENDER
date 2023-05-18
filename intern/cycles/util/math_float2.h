eefbe0ae676d24c2d060b316a9346b4b507fa988
Hoshinova <nucleohoshinova@gmail.com>
2023-05-18 21:20:33 +0200

    -- Remove duplicate definitions in "intern/cycles/util/math_float2.h"

--- intern/cycles/util/math_float2.h
+++ intern/cycles/util/math_float2.h
@@ -222,26 +222,6 @@ ccl_device_inline float2 floor(const float2 a)
   return make_float2(floorf(a.x), floorf(a.y));
 }
 
-ccl_device_inline float reduce_add(const float2 a)
-{
-  return a.x + a.y;
-}
-
-ccl_device_inline float reduce_min(const float2 a)
-{
-  return min(a.x, a.y);
-}
-
-ccl_device_inline float reduce_max(const float2 a)
-{
-  return max(a.x, a.y);
-}
-
-ccl_device_inline float2 pow(float2 v, float e)
-{
-  return make_float2(powf(v.x, e), powf(v.y, e));
-}
-
 #endif /* !__KERNEL_METAL__ */
 
 ccl_device_inline float2 safe_divide_float2_float(const float2 a, const float b)
