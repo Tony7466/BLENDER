28cf3a25d9adc4921c7e5d37b77711a309a209e7
Miguel Pozo <pragma37@gmail.com>
2023-05-19 18:34:29 +0200

    Lights

--- source/blender/draw/engines/overlay/overlay_next_extra.hh
+++ source/blender/draw/engines/overlay/overlay_next_extra.hh
@@ -220,6 +220,9 @@ class Extras {
       case OB_EMPTY:
         empty_sync(bufs, ob_ref, data, select_id);
         break;
+      case OB_LAMP:
+        light_sync(state, bufs, ob_ref, data, select_id);
+        break;
       case OB_LIGHTPROBE:
         probe_sync(bufs, ob_ref, data, select_id);
         break;
@@ -366,6 +369,95 @@ class Extras {
         /* This only show the frame. See OVERLAY_image_empty_cache_populate() for the image. */
         bufs.image.append(data, select_id);
         break;
+    }
+  }
+
+  void light_sync(const State &state,
+                  InstanceBuffers &bufs,
+                  const ObjectRef &ob_ref,
+                  const ExtraInstanceData data,
+                  const select::ID select_id)
+  {
+    Object *ob = ob_ref.object;
+    Light *la = static_cast<Light *>(ob->data);
+
+    ExtraInstanceData _data = data;
+
+    /* Pack render data into object matrix. */
+    float4x4 &matrix = _data.object_to_world_;
+    float &area_size_x = matrix[0].w;
+    float &area_size_y = matrix[1].w;
+    float &spot_cosine = matrix[0].w;
+    float &spot_blend = matrix[1].w;
+    float &clip_sta = matrix[2].w;
+    float &clip_end = matrix[3].w;
+
+    /* FIXME / TODO: clip_end has no meaning nowadays.
+     * In EEVEE, Only clip_sta is used shadow-mapping.
+     * Clip end is computed automatically based on light power.
+     * For now, always use the custom distance as clip_end. */
+    clip_end = la->att_dist;
+    clip_sta = la->clipsta;
+
+    /* Remove the alpha. */
+    float4 theme_color = float4(data.color_.xyz(), 1.0f);
+
+    float4 light_color = theme_color;
+    if (state.overlay.flag & V3D_OVERLAY_SHOW_LIGHT_COLORS) {
+      light_color = float4(la->r, la->g, la->b, 1.0f);
+    }
+
+    _data.color_ = theme_color;
+
+    /* TODO
+    DRW_buffer_add_entry(cb->groundline, matrix.pos);
+    */
+
+    bufs.light_icon_inner.append(_data.with_color(light_color), select_id);
+    bufs.light_icon_outer.append(_data, select_id);
+
+    if (la->type == LA_LOCAL) {
+      area_size_x = area_size_y = la->radius;
+      bufs.light_point.append(_data, select_id);
+    }
+    else if (la->type == LA_SUN) {
+      bufs.light_sun.append(_data, select_id);
+      bufs.light_icon_sun_rays.append(_data.with_color(light_color), select_id);
+    }
+    else if (la->type == LA_SPOT) {
+      /* Previous implementation was using the clip-end distance as cone size.
+       * We cannot do this anymore so we use a fixed size of 10. (see #72871) */
+      matrix = math::scale(matrix, float3(10.0f));
+      /* For cycles and EEVEE the spot attenuation is:
+       * `y = (1/sqrt(1 + x^2) - a)/((1 - a) b)`
+       * x being the tangent of the angle between the light direction and the
+       * generatrix of the cone. We solve the case where spot attenuation y = 1
+       * and y = 0 root for y = 1 is sqrt(1/c^2 - 1) root for y = 0 is
+       * sqrt(1/a^2 - 1) and use that to position the blend circle. */
+      float a = cosf(la->spotsize * 0.5f);
+      float b = la->spotblend;
+      float c = a * b - a - b;
+      float a2 = a * a;
+      float c2 = c * c;
+      /* Optimized version or root1 / root0 */
+      spot_blend = sqrtf((a2 - a2 * c2) / (c2 - a2 * c2));
+      spot_cosine = a;
+      /* HACK: We pack the area size in alpha color. This is decoded by the shader. */
+      _data.color_.w = -max_ff(la->radius, FLT_MIN);
+      bufs.light_spot.append(_data, select_id);
+      if ((la->mode & LA_SHOW_CONE) && !DRW_state_is_select()) {
+        bufs.light_spot_cone_front.append(_data.with_color({0.0f, 0.0f, 0.0f, 0.5f}), select_id);
+        bufs.light_spot_cone_back.append(_data.with_color({1.0f, 1.0f, 1.0f, 0.3f}), select_id);
+      }
+    }
+    else if (la->type == LA_AREA) {
+      InstanceBuf &buf = ELEM(la->area_shape, LA_AREA_SQUARE, LA_AREA_RECT) ?
+                             bufs.light_area_square :
+                             bufs.light_area_disk;
+      bool uniform_scale = !ELEM(la->area_shape, LA_AREA_RECT, LA_AREA_ELLIPSE);
+      area_size_x = la->area_size;
+      area_size_y = uniform_scale ? la->area_size : la->area_sizey;
+      buf.append(_data, select_id);
     }
   }
 
