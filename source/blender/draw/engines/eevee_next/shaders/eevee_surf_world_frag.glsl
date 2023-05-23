af07c86950e237738913ffbd3f007fbc3394411e
Miguel Pozo <pragma37@gmail.com>
2023-05-23 19:19:33 +0200

    Clear AOVs too

--- source/blender/draw/engines/eevee_next/shaders/eevee_surf_world_frag.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_surf_world_frag.glsl
@@ -13,6 +13,16 @@
 
 void main()
 {
+  ivec2 texel = ivec2(gl_FragCoord.xy);
+
+  /* Clear AOVs first. In case the material renders to them. */
+  for (int i = 0; i < AOV_MAX && i < rp_buf.aovs.color_len; i++) {
+    imageStore(rp_color_img, ivec3(texel, rp_buf.color_len + i), vec4(0));
+  }
+  for (int i = 0; i < AOV_MAX && i < rp_buf.aovs.value_len; i++) {
+    imageStore(rp_value_img, ivec3(texel, rp_buf.value_len + i), vec4(0));
+  }
+
   init_globals();
   /* View position is passed to keep accuracy. */
   g_data.N = normal_view_to_world(viewCameraVec(interp.P));
@@ -24,23 +34,23 @@ void main()
 
   g_holdout = saturate(g_holdout);
 
-  ivec2 out_texel = ivec2(gl_FragCoord.xy);
-  RP_OUTPUT_COLOR(normal_id, out_texel, vec4(0.0, 0.0, 0.0, 1.0));
-  RP_OUTPUT_COLOR(diffuse_light_id, out_texel, vec4(0.0, 0.0, 0.0, 1.0));
-  RP_OUTPUT_COLOR(specular_light_id, out_texel, vec4(0.0, 0.0, 0.0, 1.0));
-  RP_OUTPUT_COLOR(diffuse_color_id, out_texel, vec4(0.0, 0.0, 0.0, 1.0));
-  RP_OUTPUT_COLOR(specular_color_id, out_texel, vec4(0.0, 0.0, 0.0, 1.0));
-  RP_OUTPUT_COLOR(emission_id, out_texel, vec4(0.0, 0.0, 0.0, 1.0));
-  imageStore(rp_cryptomatte_img, out_texel, vec4(0.0));
-
   out_background.rgb = safe_color(g_emission) * (1.0 - g_holdout);
   out_background.a = saturate(avg(g_transmittance)) * g_holdout;
 
   /* World opacity. */
   out_background = mix(vec4(0.0, 0.0, 0.0, 1.0), out_background, world_opacity_fade);
 
+  /* Clear Render Buffers. */
   vec4 environment = out_background;
   environment.a = 1.0 - environment.a;
   environment.rgb *= environment.a;
-  RP_OUTPUT_COLOR(environment_id, out_texel, environment);
+  RP_OUTPUT_COLOR(environment_id, texel, environment);
+  vec4 clear_color = vec4(0.0, 0.0, 0.0, 1.0);
+  RP_OUTPUT_COLOR(normal_id, texel, clear_color);
+  RP_OUTPUT_COLOR(diffuse_light_id, texel, clear_color);
+  RP_OUTPUT_COLOR(specular_light_id, texel, clear_color);
+  RP_OUTPUT_COLOR(diffuse_color_id, texel, clear_color);
+  RP_OUTPUT_COLOR(specular_color_id, texel, clear_color);
+  RP_OUTPUT_COLOR(emission_id, texel, clear_color);
+  imageStore(rp_cryptomatte_img, texel, vec4(0.0));
 }
