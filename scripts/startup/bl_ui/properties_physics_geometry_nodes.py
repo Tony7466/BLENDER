96cc7c804c95cc510e94cff0ab57b66ac30b2371
Jacques Lucke <jacques@blender.org>
2023-05-24 14:00:05 +0200

    move bake path to modifier panel

--- scripts/startup/bl_ui/properties_physics_geometry_nodes.py
+++ scripts/startup/bl_ui/properties_physics_geometry_nodes.py
@@ -45,34 +45,8 @@ class PHYSICS_PT_geometry_nodes(Panel):
         layout.prop(ob, "use_simulation_cache", text="Cache")
 
 
-class PHYSICS_PT_geometry_nodes_bake_paths(Panel):
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = "physics"
-    bl_label = "Paths"
-    bl_parent_id = "PHYSICS_PT_geometry_nodes"
-    bl_options = {'DEFAULT_CLOSED'}
-
-    def draw(self, context):
-        layout = self.layout
-
-        objects = [ob for ob in context.selected_editable_objects
-                   if any(m.type == 'NODES' for m in ob.modifiers)]
-        multiple_objects = len(objects) > 1
-
-        for ob in objects:
-            col = layout.column()
-            if multiple_objects:
-                col.label(text=ob.name, icon='OBJECT_DATA')
-            for modifier in ob.modifiers:
-                if modifier.type != 'NODES':
-                    continue
-                col.prop(modifier, "simulation_bake_directory", text=modifier.name)
-
-
 classes = (
     PHYSICS_PT_geometry_nodes,
-    PHYSICS_PT_geometry_nodes_bake_paths,
 )
 
 
