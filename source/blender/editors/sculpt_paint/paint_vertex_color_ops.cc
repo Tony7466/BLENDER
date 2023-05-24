97fc2c1fe472d83a8133fce10e5ae0684b0302c8
Hans Goudey <h.goudey@me.com>
2023-05-23 21:59:38 -0400

    Fix: Attempt to fix build error on Windows

--- source/blender/editors/sculpt_paint/paint_vertex_color_ops.cc
+++ source/blender/editors/sculpt_paint/paint_vertex_color_ops.cc
@@ -272,20 +272,22 @@ static void transform_active_color_data(
     color_attribute.varray.type().to_static_type_tag<ColorGeometry4f, ColorGeometry4b>(
         [&](auto type_tag) {
           using T = typename decltype(type_tag)::type;
-          for ([[maybe_unused]] const int i : selection.slice(range)) {
-            if constexpr (std::is_void_v<T>) {
-              BLI_assert_unreachable();
-            }
-            else if constexpr (std::is_same_v<T, ColorGeometry4f>) {
-              ColorGeometry4f color = color_attribute.varray.get<ColorGeometry4f>(i);
-              transform_fn(color);
-              color_attribute.varray.set_by_copy(i, &color);
-            }
-            else if constexpr (std::is_same_v<T, ColorGeometry4b>) {
-              ColorGeometry4f color = color_attribute.varray.get<ColorGeometry4b>(i).decode();
-              transform_fn(color);
-              ColorGeometry4b color_encoded = color.encode();
-              color_attribute.varray.set_by_copy(i, &color_encoded);
+          if constexpr (std::is_void_v<T>) {
+            BLI_assert_unreachable();
+          }
+          else {
+            for ([[maybe_unused]] const int i : selection.slice(range)) {
+              if constexpr (std::is_same_v<T, ColorGeometry4f>) {
+                ColorGeometry4f color = color_attribute.varray.get<ColorGeometry4f>(i);
+                transform_fn(color);
+                color_attribute.varray.set_by_copy(i, &color);
+              }
+              else if constexpr (std::is_same_v<T, ColorGeometry4b>) {
+                ColorGeometry4f color = color_attribute.varray.get<ColorGeometry4b>(i).decode();
+                transform_fn(color);
+                ColorGeometry4b color_encoded = color.encode();
+                color_attribute.varray.set_by_copy(i, &color_encoded);
+              }
             }
           }
         });
