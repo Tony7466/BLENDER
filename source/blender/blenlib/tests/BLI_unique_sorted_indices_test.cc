b89816639ced0fe37dd701d9cf2e49faa02645e6
Iliya Katueshenock <modormoder@gmail.com>
2023-05-25 19:28:19 +0300

    progress

--- source/blender/blenlib/tests/BLI_unique_sorted_indices_test.cc
+++ source/blender/blenlib/tests/BLI_unique_sorted_indices_test.cc
@@ -49,16 +49,25 @@ TEST(unique_sorted_indices, FindStartOfNextRange)
 
 TEST(unique_sorted_indices, SplitToRangesAndSpans)
 {
-  Array<int> data = {1, 2, 3, 4, 7, 9, 10, 13, 14, 15, 20, 21, 22, 23, 24};
-  Vector<std::variant<IndexRange, Span<int>>> parts;
-  const int64_t parts_num = split_to_ranges_and_spans<int>(data, 3, parts);
+  Array<int> data_1 = {1, 2, 3, 4, 7, 9, 10, 13, 14, 15, 20, 21, 22, 23, 24};
+  Vector<std::variant<IndexRange, Span<int>>> parts_1;
+  const int64_t parts_num = split_to_ranges_and_spans<int>(data_1, 3, parts_1);
 
   EXPECT_EQ(parts_num, 4);
-  EXPECT_EQ(parts.size(), 4);
-  EXPECT_EQ(std::get<IndexRange>(parts[0]), IndexRange(1, 4));
-  EXPECT_EQ(std::get<Span<int>>(parts[1]), Span<int>({7, 9, 10}));
-  EXPECT_EQ(std::get<IndexRange>(parts[2]), IndexRange(13, 3));
-  EXPECT_EQ(std::get<IndexRange>(parts[3]), IndexRange(20, 5));
+  EXPECT_EQ(parts_1.size(), 4);
+  EXPECT_EQ(std::get<IndexRange>(parts_1[0]), IndexRange(1, 4));
+  EXPECT_EQ(std::get<Span<int>>(parts_1[1]), Span<int>({7, 9, 10}));
+  EXPECT_EQ(std::get<IndexRange>(parts_1[2]), IndexRange(13, 3));
+  EXPECT_EQ(std::get<IndexRange>(parts_1[3]), IndexRange(20, 5));
+
+  Array<int> data_2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 55};
+  Vector<std::variant<IndexRange, Span<int>>> parts_2;
+  const int64_t parts_num_2 = split_to_ranges_and_spans<int>(data_2, 3, parts_2);
+
+  EXPECT_EQ(parts_num_2, 2);
+  EXPECT_EQ(parts_2.size(), 2);
+  EXPECT_EQ(std::get<IndexRange>(parts_2[0]), IndexRange(1, 11));
+  EXPECT_EQ(std::get<Span<int>>(parts_2[1]), Span<int>({55}));
 }
 
 }  // namespace blender::unique_sorted_indices::tests
