f6a94de0eb6cd5abcb2c65e5dcc7bb708d1686f6
himisa <himisa@noreply.localhost>
2023-05-29 17:18:16 +0200

    revert a3aea691bfd9b129a8e7a896dabb09d6c8377dd6
    revert try to fix #101877

--- intern/rigidbody/rb_bullet_api.cpp
+++ intern/rigidbody/rb_bullet_api.cpp
@@ -352,13 +352,7 @@ void RB_body_delete(rbRigidBody *object)
    * but since we delete everything when the world is rebult, we need to do it manually here */
   for (int i = body->getNumConstraintRefs() - 1; i >= 0; i--) {
     btTypedConstraint *con = body->getConstraintRef(i);
-
-    if (con->isEnabled()) {
-      con->getRigidBodyA().removeConstraintRef(con);
-      con->getRigidBodyB().removeConstraintRef(con);
-      con->setEnabled(false);
-    }
-
+    body->removeConstraintRef(con);
   }
 
   delete body;
@@ -1125,12 +1119,6 @@ rbConstraint *RB_constraint_new_motor(float pivot[3],
 void RB_constraint_delete(rbConstraint *con)
 {
   btTypedConstraint *constraint = reinterpret_cast<btTypedConstraint *>(con);
-  
-  if (constraint->isEnabled()) {
-    constraint->getRigidBodyA().removeConstraintRef(constraint);
-    constraint->getRigidBodyB().removeConstraintRef(constraint);
-  }
-
   delete constraint;
 }
 
