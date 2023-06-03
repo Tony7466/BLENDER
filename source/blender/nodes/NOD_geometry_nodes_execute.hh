def5647072a0783bca692b2c87d1c72a11f59b97
Hans Goudey <h.goudey@me.com>
2023-06-03 09:39:22 -0400

    Remove extra stuff from header

--- source/blender/nodes/NOD_geometry_nodes_execute.hh
+++ source/blender/nodes/NOD_geometry_nodes_execute.hh
@@ -1,29 +1,12 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* SPDX-FileCopyrightText: 2023 Blender Foundation
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #pragma once
-
-/**
- * For evaluation, geometry node groups are converted to a lazy-function graph. The generated graph
- * is cached per node group, so it only has to be generated once after a change.
- *
- * Node groups are *not* inlined into the lazy-function graph. This could be added in the future as
- * it might improve performance in some cases, but generally does not seem necessary. Inlining node
- * groups also has disadvantages like making per-node-group caches less useful, resulting in more
- * overhead.
- *
- * Instead, group nodes are just like all other nodes in the lazy-function graph. What makes them
- * special is that they reference the lazy-function graph of the group they reference.
- *
- * During lazy-function graph generation, a mapping between the #bNodeTree and
- * #lazy_function::Graph is build that can be used when evaluating the graph (e.g. for logging).
- */
 
 #include "BLI_compute_context.hh"
 #include "BLI_function_ref.hh"
 #include "BLI_multi_value_map.hh"
-
-#include "FN_lazy_function_graph.hh"
-#include "FN_lazy_function_graph_executor.hh"
 
 #include "BKE_idprop.hh"
 
