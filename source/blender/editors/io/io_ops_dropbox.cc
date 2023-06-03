4bdc8f9fda473ef4d04c8f1e42de7663bd8b6119
guishe <guillermovcra@gmail.com>
2023-06-02 20:32:35 -0600

    cleanup

--- source/blender/editors/io/io_ops_dropbox.cc
+++ source/blender/editors/io/io_ops_dropbox.cc
@@ -16,6 +16,7 @@
 void files_drop_copy(bContext * /* C */, wmDrag *drag, wmDropBox *drop)
 {
   RNA_string_set(drop->ptr, "filepath", WM_drag_get_path(drag));
+
   if (!RNA_struct_find_property(drop->ptr, "directory")) {
     return;
   }
@@ -23,11 +24,11 @@ void files_drop_copy(bContext * /* C */, wmDrag *drag, wmDropBox *drop)
   // TODO(@guishe): Add support for multiple drag&drop files import
   char dir[FILE_MAX], file[FILE_MAX];
   BLI_path_split_dir_file(WM_drag_get_path(drag), dir, sizeof(dir), file, sizeof(file));
-
+  
   RNA_string_set(drop->ptr, "directory", dir);
 
-  RNA_collection_clear(drop->ptr, "files");
   PointerRNA itemptr{};
+  RNA_collection_clear(drop->ptr, "files");
   RNA_collection_add(drop->ptr, "files", &itemptr);
   RNA_string_set(&itemptr, "name", file);
 }
@@ -45,7 +46,7 @@ void add_drag_path_dropbox(ListBase *lb, const char *operator_name)
     return false;
   };
 
-  WM_dropbox_add(lb, operator_name, poll, files_drop_copy, NULL, NULL);
+  WM_dropbox_add(lb, operator_name, poll, files_drop_copy, nullptr,nullptr);
 }
 
 void ED_dropboxes_io(void)
