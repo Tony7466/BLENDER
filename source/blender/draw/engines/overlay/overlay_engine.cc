8e8109ad6ae610af1aef5be4b6526dd598269b83
Iliya Katueshenock <modormoder@gmail.com>
2023-06-04 16:42:10 +0300

    progress

--- source/blender/draw/engines/overlay/overlay_engine.cc
+++ source/blender/draw/engines/overlay/overlay_engine.cc
@@ -18,7 +18,6 @@
 #include "UI_interface.h"
 
 #include "BKE_duplilist.h"
-#include "BKE_geometry_set.hh"
 #include "BKE_object.h"
 #include "BKE_paint.h"
 
@@ -377,8 +376,6 @@ static void OVERLAY_cache_populate(void *vedata, Object *ob)
 
   const bool draw_motion_paths = (pd->overlay.flag & V3D_OVERLAY_HIDE_MOTION_PATHS) == 0;
 
-  const bool draw_geometry_node_gizmos = ob->runtime.geometry_set_eval->has_gizmos();
-
   bool do_init;
   OVERLAY_DupliData *dupli = OVERLAY_duplidata_get(ob, vedata, &do_init);
 
@@ -499,11 +496,6 @@ static void OVERLAY_cache_populate(void *vedata, Object *ob)
         break;
     }
   }
-
-  if (draw_geometry_node_gizmos) {
-    // OVERLAY_geometry_node_gizmo_cache_populate(data, ob); ?
-  }
-
   /* Non-Meshes */
   if (draw_extras) {
     switch (ob->type) {
