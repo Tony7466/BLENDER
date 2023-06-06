f35266c9d44c90c1fc3e34760bc8ff32c5fc126c
jon denning <gfxcoder@gmail.com>
2023-06-06 10:58:23 -0400

    Fixed warning with `GPU_scissor_test_get`
    Function declaration was missing `void` in arglist to make it a proper
    function template.

--- source/blender/gpu/GPU_state.h
+++ source/blender/gpu/GPU_state.h
@@ -173,7 +173,7 @@ void GPU_polygon_smooth(bool enable);
 void GPU_program_point_size(bool enable);
 void GPU_scissor(int x, int y, int width, int height);
 void GPU_scissor_get(int coords[4]);
-bool GPU_scissor_test_get();
+bool GPU_scissor_test_get(void);
 void GPU_viewport(int x, int y, int width, int height);
 void GPU_viewport_size_get_f(float coords[4]);
 void GPU_viewport_size_get_i(int coords[4]);
