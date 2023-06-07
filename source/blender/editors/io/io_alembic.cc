6a2603ed19a34ac4168977fcf571d08e66afaa62
guishe <guillermovcra@gmail.com>
2023-06-06 18:03:39 -0600

    Cleanup: replace the use of listbase in io_allembic.cc with vector
    No visible changes expected.

--- source/blender/editors/io/io_alembic.cc
+++ source/blender/editors/io/io_alembic.cc
@@ -29,7 +29,6 @@
 #  include "BKE_main.h"
 #  include "BKE_report.h"
 
-#  include "BLI_listbase.h"
 #  include "BLI_path_util.h"
 #  include "BLI_string.h"
 #  include "BLI_utildefines.h"
@@ -473,19 +472,15 @@ void WM_OT_alembic_export(wmOperatorType *ot)
 /* TODO(kevin): check on de-duplicating all this with code in image_ops.c */
 
 struct CacheFrame {
-  struct CacheFrame *next, *prev;
   int framenr;
 };
 
-static int cmp_frame(const void *a, const void *b)
+static int cmp_frame(const CacheFrame &frame_a, const CacheFrame &frame_b)
 {
-  const CacheFrame *frame_a = static_cast<const CacheFrame *>(a);
-  const CacheFrame *frame_b = static_cast<const CacheFrame *>(b);
-
-  if (frame_a->framenr < frame_b->framenr) {
+  if (frame_a.framenr < frame_b.framenr) {
     return -1;
   }
-  if (frame_a->framenr > frame_b->framenr) {
+  if (frame_a.framenr > frame_b.framenr) {
     return 1;
   }
   return 0;
@@ -524,8 +519,7 @@ static int get_sequence_len(const char *filepath, int *ofs)
   const char *basename = BLI_path_basename(filepath);
   const int len = strlen(basename) - (numdigit + strlen(ext));
 
-  ListBase frames{};
-  BLI_listbase_clear(&frames);
+  std::vector<CacheFrame> frames{};
 
   dirent *fname;
   while ((fname = readdir(dir)) != nullptr) {
@@ -538,29 +532,27 @@ static int get_sequence_len(const char *filepath, int *ofs)
       continue;
     }
 
-    CacheFrame *cache_frame = MEM_cnew<CacheFrame>("abc_frame");
+    CacheFrame cache_frame{};
 
-    BLI_path_frame_get(fname->d_name, &cache_frame->framenr, &numdigit);
+    BLI_path_frame_get(fname->d_name, &cache_frame.framenr, &numdigit);
 
-    BLI_addtail(&frames, cache_frame);
+    frames.push_back(cache_frame);
   }
 
   closedir(dir);
 
-  BLI_listbase_sort(&frames, cmp_frame);
+  std::sort(frames.begin(), frames.end(), cmp_frame);
 
-  CacheFrame *cache_frame = static_cast<CacheFrame *>(frames.first);
-
-  if (cache_frame != nullptr) {
-    int frame_curr = cache_frame->framenr;
+  if (frames.size() > 0) {
+    int frame_curr = frames[0].framenr;
     (*ofs) = frame_curr;
 
-    while (cache_frame && (cache_frame->framenr == frame_curr)) {
+    for (CacheFrame & cache_frame : frames) {
+      if (cache_frame.framenr != frame_curr) {
+        break;
+      }
       frame_curr++;
-      cache_frame = cache_frame->next;
     }
-
-    BLI_freelistN(&frames);
 
     return frame_curr - (*ofs);
   }
