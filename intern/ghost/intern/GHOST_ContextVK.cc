8c15503c70194fa98b2ac746e05b38c821dee4c5
Jeroen Bakker <jeroen@blender.org>
2023-06-08 12:58:28 +0200

    silence a vulkan error when starting blender.

--- intern/ghost/intern/GHOST_ContextVK.cc
+++ intern/ghost/intern/GHOST_ContextVK.cc
@@ -709,7 +709,7 @@ static GHOST_TSuccess create_render_pass(VkDevice device,
   colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
   colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
   colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-  colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+  colorAttachment.initialLayout = VK_IMAGE_LAYOUT_GENERAL;
   colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
   VkAttachmentReference colorAttachmentRef = {};
