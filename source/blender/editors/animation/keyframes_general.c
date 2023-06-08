6b0ceb2646567bc681da661926683e6cf9d51b9d
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-06-08 09:54:36 +0200

    blend to first key value

--- source/blender/editors/animation/keyframes_general.c
+++ source/blender/editors/animation/keyframes_general.c
@@ -482,24 +482,31 @@ void butterworth_smooth_fcurve_segment(FCurve *fcu,
     const double filtered_value = butterworth_filter_value(x, w0, w1, w2, bw_coeff);
     filtered_values[i] = (float)(filtered_value) + bwd_offset;
   }
+
   const int segment_end_index = segment->start_index + segment->length;
   BezTriple left_bezt = fcu->bezt[segment->start_index];
+  const float blend_in_y = samples[filter_order];
+  const float blend_out_y = evaluate_fcurve(fcu, fcu->bezt[segment_end_index].vec[1][0]);
+
   for (int i = segment->start_index; i < segment_end_index; i++) {
     const float x_delta = fcu->bezt[i].vec[1][0] - left_bezt.vec[1][0] + filter_order;
     const int filter_index = (int)(x_delta * sample_rate);
     float key_y_value = interpf(filtered_values[filter_index], samples[filter_index], factor);
 
     float blend_in_out_factor;
+    float blend_value = 0;
     if (blend_in_out == 0) {
       blend_in_out_factor = 1;
     }
     else if (i < segment_end_index / 2) {
       blend_in_out_factor = min_ff((float)(i - segment->start_index) / blend_in_out, 1.0f);
+      blend_value = blend_in_y;
     }
     else {
       blend_in_out_factor = min_ff((float)(segment_end_index - i) / blend_in_out, 1.0f);
+      blend_value = blend_out_y;
     }
-    key_y_value = interpf(key_y_value, samples[filter_index], blend_in_out_factor);
+    key_y_value = interpf(key_y_value, blend_value, blend_in_out_factor);
     BKE_fcurve_keyframe_move_value_with_handles(&fcu->bezt[i], key_y_value);
   }
 
