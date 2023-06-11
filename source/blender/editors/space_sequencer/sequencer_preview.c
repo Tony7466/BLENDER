435111f788f2ed63314693c7ca516cde02dbc5b7
Lucas Tadeu Teixeira <lucas@lucastadeu.com>
2023-06-11 17:10:31 +0100

    VSE: Process audio strips waveforms in parallel
    A TaskPool is used to execute tasks in the backghround.
    
    Whenever a new audio strip needs their waveform to be computed, it's
    added as a task to the TaskPool.
    
    Once the PreviewJob has submitted all available tasks to the task pool,
    it waits for all of them to finish before exiting.
    
    The TaskPool is configured to start tasks as soon as they are pushed
    onto the task queue.

--- source/blender/editors/space_sequencer/sequencer_preview.c
+++ source/blender/editors/space_sequencer/sequencer_preview.c
@@ -10,6 +10,7 @@
 #include "DNA_sound_types.h"
 
 #include "BLI_listbase.h"
+#include "BLI_task.h"
 #include "BLI_threads.h"
 
 #include "BKE_context.h"
@@ -42,6 +43,15 @@ typedef struct PreviewJobAudio {
   bool waveform; /* Reload sound or waveform. */
 } PreviewJobAudio;
 
+typedef struct ReadSoundWaveformTask {
+  PreviewJob *wm_job;
+  bool *wm_do_update;
+  float *wm_progress;
+
+  PreviewJobAudio *preview_job_audio;
+  bool *stop;
+} ReadSoundWaveformTask;
+
 static void free_preview_job(void *data)
 {
   PreviewJob *pj = (PreviewJob *)data;
@@ -51,9 +61,49 @@ static void free_preview_job(void *data)
   MEM_freeN(pj);
 }
 
+static void clear_sound_waveform_loading_tag(bSound *sound) {
+    BLI_spin_lock(sound->spinlock);
+    sound->tags &= ~SOUND_TAGS_WAVEFORM_LOADING;
+    BLI_spin_unlock(sound->spinlock);
+}
+
+static void free_read_sound_waveform_task(struct TaskPool *__restrict task_pool, void *data)
+{
+  UNUSED_VARS(task_pool);
+
+  ReadSoundWaveformTask *task = (ReadSoundWaveformTask *)data;
+
+  /* The job audio has already been removed from the list, now we just need to free it. */
+  MEM_freeN(task->preview_job_audio);
+
+  MEM_freeN(task);
+}
+
+static void execute_read_sound_waveform_task(struct TaskPool *__restrict task_pool,
+                                             void *task_data)
+{
+  ReadSoundWaveformTask *task = (ReadSoundWaveformTask *)task_data;
+
+  if (BLI_task_pool_current_canceled(task_pool)) {
+    clear_sound_waveform_loading_tag(task->preview_job_audio->sound);
+    return;
+  }
+
+  PreviewJobAudio *audio_job = task->preview_job_audio;
+  BKE_sound_read_waveform(audio_job->bmain, audio_job->sound, task->stop);
+
+  BLI_mutex_lock(task->wm_job->mutex);
+  task->wm_job->processed++;
+  *task->wm_progress = (task->wm_job->total > 0) ? (float)task->wm_job->processed / (float)task->wm_job->total : 1.0f;
+  *task->wm_do_update = true;
+  BLI_mutex_unlock(task->wm_job->mutex);
+}
+
 /* Only this runs inside thread. */
 static void preview_startjob(void *data, bool *stop, bool *do_update, float *progress)
 {
+  TaskPool *task_pool = BLI_task_pool_create(NULL, TASK_PRIORITY_LOW);
+
   PreviewJob *pj = data;
   PreviewJobAudio *previewjb;
 
@@ -63,21 +113,24 @@ static void preview_startjob(void *data, bool *stop, bool *do_update, float *pro
 
   while (previewjb) {
     PreviewJobAudio *preview_next;
-    bSound *sound = previewjb->sound;
 
-    BKE_sound_read_waveform(previewjb->bmain, sound, stop);
+    ReadSoundWaveformTask *task = (ReadSoundWaveformTask *)MEM_mallocN(sizeof(ReadSoundWaveformTask), "read sound waveform task");
+    task->wm_job = pj;
+    task->wm_progress = progress;
+    task->wm_do_update = do_update;
+    task->preview_job_audio = previewjb;
+    task->stop = stop;
+
+    BLI_task_pool_push(task_pool, execute_read_sound_waveform_task, task, false, NULL);
 
     if (*stop || G.is_break) {
+      BLI_task_pool_cancel(task_pool);
+
       BLI_mutex_lock(pj->mutex);
       previewjb = previewjb->next;
       BLI_mutex_unlock(pj->mutex);
       while (previewjb) {
-        sound = previewjb->sound;
-
-        /* Make sure we cleanup the loading flag! */
-        BLI_spin_lock(sound->spinlock);
-        sound->tags &= ~SOUND_TAGS_WAVEFORM_LOADING;
-        BLI_spin_unlock(sound->spinlock);
+        clear_sound_waveform_loading_tag(previewjb->sound);
 
         BLI_mutex_lock(pj->mutex);
         previewjb = previewjb->next;
@@ -86,6 +139,11 @@ static void preview_startjob(void *data, bool *stop, bool *do_update, float *pro
 
       BLI_mutex_lock(pj->mutex);
       BLI_freelistN(&pj->previews);
+
+      /* This is set to zero without an atomic operation because at this point there will be no other threads competing for access to this variable.
+       * BLI_task_pool_cancel ensures that all tasks submitted to the pool are stopped before returning.
+       */
+
       pj->total = 0;
       pj->processed = 0;
       BLI_mutex_unlock(pj->mutex);
@@ -94,13 +152,14 @@ static void preview_startjob(void *data, bool *stop, bool *do_update, float *pro
 
     BLI_mutex_lock(pj->mutex);
     preview_next = previewjb->next;
-    BLI_freelinkN(&pj->previews, previewjb);
+    BLI_remlink(&pj->previews, previewjb);
     previewjb = preview_next;
-    pj->processed++;
-    *progress = (pj->total > 0) ? (float)pj->processed / (float)pj->total : 1.0f;
-    *do_update = true;
+
     BLI_mutex_unlock(pj->mutex);
   }
+
+  BLI_task_pool_work_and_wait(task_pool);
+  BLI_task_pool_free(task_pool);
 }
 
 static void preview_endjob(void *data)
