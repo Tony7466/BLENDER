8192abe3b5d7bd8ec39d52269802fa7ebfc44ab1
Richard Antalik <richardantalik@gmail.com>
2023-06-11 23:51:47 +0200

    Revert "VSE: Add sound strip retiming support"
    This reverts commit b21695a507cd2d50d9855fc64ee92632d4e97c2b.

--- source/blender/sequencer/intern/strip_time.c
+++ source/blender/sequencer/intern/strip_time.c
@@ -53,16 +53,7 @@ float seq_time_media_playback_rate_factor_get(const Scene *scene, const Sequence
   return seq->media_playback_rate / scene_playback_rate;
 }
 
-int seq_time_strip_original_content_length_get(const Scene *scene, const Sequence *seq)
-{
-  if (seq->type == SEQ_TYPE_SOUND_RAM) {
-    return seq->len;
-  }
-
-  return seq->len / seq_time_media_playback_rate_factor_get(scene, seq);
-}
-
-float SEQ_give_frame_index(const Scene *scene, Sequence *seq, float timeline_frame)
+float seq_give_frame_index(const Scene *scene, Sequence *seq, float timeline_frame)
 {
   float frame_index;
   float sta = SEQ_time_start_frame_get(seq);
@@ -503,6 +494,10 @@ bool SEQ_time_has_still_frames(const Scene *scene, const Sequence *seq)
 
 int SEQ_time_strip_length_get(const Scene *scene, const Sequence *seq)
 {
+  if (seq->type == SEQ_TYPE_SOUND_RAM) {
+    return seq->len;
+  }
+
   if (SEQ_retiming_is_active(seq)) {
     SeqRetimingHandle *handle_start = seq->retiming_handles;
     SeqRetimingHandle *handle_end = seq->retiming_handles + (SEQ_retiming_handles_count(seq) - 1);
