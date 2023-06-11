5cc9f1552ee9f3bffcac8a75941f2bcb9d3334b1
Chao Li <chaolinyc@gmail.com>
2023-06-11 10:40:03 -0400

    Remove action in select linked exec

--- source/blender/editors/grease_pencil/intern/grease_pencil_ops.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_ops.cc
@@ -62,15 +62,14 @@ static int select_all_exec(bContext *C, wmOperator *op)
   return OPERATOR_FINISHED;
 }
 
-static int select_linked_exec(bContext *C, wmOperator *op)
+static int select_linked_exec(bContext *C, wmOperator * /*op*/)
 {
-  int action = RNA_enum_get(op->ptr, "action");
   Scene *scene = CTX_data_scene(C);
   Object *object = CTX_data_active_object(C);
   GreasePencil &grease_pencil = *static_cast<GreasePencil *>(object->data);
 
   grease_pencil.foreach_editable_drawing(
-      scene->r.cfra, [action](int /*drawing_index*/, GreasePencilDrawing &drawing) {
+      scene->r.cfra, [](int /*drawing_index*/, GreasePencilDrawing &drawing) {
         // TODO: Support different selection domains.
         blender::ed::curves::select_linked(drawing.geometry.wrap());
       });
