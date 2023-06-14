ba40c9c6a8473d595f3d1fc0ac1122d1b158ba69
Kévin Dietrich <kevin.dietrich@mailoo.org>
2023-06-14 13:04:32 +0200

    Fix topology check with new Mesh API.

--- source/blender/io/alembic/intern/abc_reader_mesh.cc
+++ source/blender/io/alembic/intern/abc_reader_mesh.cc
@@ -721,23 +721,23 @@ bool AbcMeshReader::topology_changed(const Mesh *existing_mesh, const ISampleSel
 
   /* Otherwise, we need to check the connectivity as files from e.g. videogrammetry may have the
    * same face count, but different connections between faces. */
+  uint abc_index = 0;
 
-  uint loop_index = 0;
-
-  const OffsetIndices<int> &polys = existing_mesh->polys();
+  const int *mesh_corners = existing_mesh->corner_verts().data();
+  const int *mesh_poly_offsets = existing_mesh->poly_offsets().data();
 
   for (int i = 0; i < face_counts->size(); i++) {
-    const int face_size = (*face_counts)[i];
-    const IndexRange &poly = polys[i];
-
-    if (poly[i] != loop_index || poly.size() != face_size) {
+    if (mesh_poly_offsets[i] != abc_index) {
       return true;
     }
 
+    const int abc_face_size = (*face_counts)[i];
     /* NOTE: Alembic data is stored in the reverse order. */
-    uint rev_loop_index = loop_index + (face_size - 1);
-    for (int f = 0; f < face_size; f++, loop_index++, rev_loop_index--) {
-      if (poly[i] != ((*face_indices)[loop_index])) {
+    uint rev_loop_index = abc_index + (abc_face_size - 1);
+    for (int f = 0; f < abc_face_size; f++, abc_index++, rev_loop_index--) {
+      const int mesh_vert = mesh_corners[rev_loop_index];
+      const int abc_vert = (*face_indices)[abc_index];
+      if (mesh_vert != abc_vert) {
         return true;
       }
     }
