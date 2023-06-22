16d290435c957dde1711b5154b6549b1d687214f
Jacques Lucke <jacques@blender.org>
2023-06-22 11:28:56 +0200

    fix deleting and grouping serial loops

--- source/blender/editors/space_node/node_group.cc
+++ source/blender/editors/space_node/node_group.cc
@@ -790,6 +790,33 @@ static bool node_group_make_test_selected(bNodeTree &ntree,
       }
     }
   }
+  for (bNode *input_node : ntree.nodes_by_type("GeometryNodeSerialLoopInput")) {
+    const NodeGeometrySerialLoopInput &input_data =
+        *static_cast<const NodeGeometrySerialLoopInput *>(input_node->storage);
+
+    if (bNode *output_node = ntree.node_by_id(input_data.output_node_id)) {
+      const bool input_selected = nodes_to_group.contains(input_node);
+      const bool output_selected = nodes_to_group.contains(output_node);
+      if (input_selected && !output_selected) {
+        BKE_reportf(
+            &reports,
+            RPT_WARNING,
+            "Can not add serial loop input node '%s' to a group without its paired output '%s'",
+            input_node->name,
+            output_node->name);
+        return false;
+      }
+      if (output_selected && !input_selected) {
+        BKE_reportf(
+            &reports,
+            RPT_WARNING,
+            "Can not add serial loop output node '%s' to a group without its paired input '%s'",
+            output_node->name,
+            input_node->name);
+        return false;
+      }
+    }
+  }
 
   return true;
 }
