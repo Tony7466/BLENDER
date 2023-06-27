1e331a624ea449b9bb8d38ce9467b34d94271f44
Miguel Pozo <pragma37@gmail.com>
2023-06-27 16:35:46 +0200

    Fix sampler overlap

--- source/blender/draw/engines/eevee_next/eevee_shader.cc
+++ source/blender/draw/engines/eevee_next/eevee_shader.cc
@@ -342,10 +342,8 @@ void ShaderModule::material_create_info_ammend(GPUMaterial *gpumat, GPUCodegenOu
     case MAT_GEOM_VOLUME_OBJECT:
     case MAT_GEOM_VOLUME_WORLD:
       /** Volume grid attributes come from 3D textures. Transfer attributes to samplers. */
-      /** WORKAROUND: Starts binding from the last unit to avoid collisions with node samplers. */
-      int bind_unit = 31;
       for (auto &input : info.vertex_inputs_) {
-        info.sampler(bind_unit--, ImageType::FLOAT_3D, input.name, Frequency::BATCH);
+        info.sampler(sampler_slot--, ImageType::FLOAT_3D, input.name, Frequency::BATCH);
       }
       info.vertex_inputs_.clear();
       break;
