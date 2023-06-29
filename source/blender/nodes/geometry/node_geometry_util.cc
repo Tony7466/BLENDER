e36436f32ba844420e5a1949ec9d3c01fbc7c973
Hans Goudey <h.goudey@me.com>
2023-06-29 12:03:18 -0400

    Fix build error

--- source/blender/nodes/geometry/node_geometry_util.cc
+++ source/blender/nodes/geometry/node_geometry_util.cc
@@ -51,7 +51,9 @@ bool check_operator_context_and_error(GeoNodeExecParams &params)
   if (!params.user_data()->operator_data) {
     params.error_message_add(NodeWarningType::Error, "Node must be run as operator");
     params.set_default_remaining_outputs();
+    return false;
   }
+  return true;
 }
 
 void search_link_ops_for_for_operator_node(GatherAddNodeSearchParams &params)
