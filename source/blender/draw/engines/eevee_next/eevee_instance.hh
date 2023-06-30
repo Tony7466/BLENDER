9203256df16d2ca2e23e5c3873bc8ec54c93a720
Jeroen Bakker <jeroen@blender.org>
2023-06-30 09:06:43 +0200

    Eevee-next: Lookdev HDRI Switcher
    This PR adds support to override the world with the HDRI of the
    viewport.
    
    Blurring of the background and camera based rotation isn't supported.
    Note the workflow when using irradiance caches isn't clear. As
    irradiance caches are created without the viewport known it would
    still bake the scene world in.
    
    Taken that into account the HDRI switcher would only change the
    reflections. We should find out how we could solve that.

--- source/blender/draw/engines/eevee_next/eevee_instance.hh
+++ source/blender/draw/engines/eevee_next/eevee_instance.hh
@@ -24,6 +24,7 @@
 #include "eevee_irradiance_cache.hh"
 #include "eevee_light.hh"
 #include "eevee_lightprobe.hh"
+#include "eevee_lookdev.hh"
 #include "eevee_material.hh"
 #include "eevee_motion_blur.hh"
 #include "eevee_pipeline.hh"
@@ -69,6 +70,7 @@ class Instance {
   MainView main_view;
   CaptureView capture_view;
   World world;
+  LookdevModule lookdev;
   LightProbeModule light_probes;
   IrradianceCache irradiance_cache;
 
@@ -121,6 +123,7 @@ class Instance {
         main_view(*this),
         capture_view(*this),
         world(*this),
+        lookdev(*this),
         light_probes(*this),
         irradiance_cache(*this){};
   ~Instance(){};
