781916a0e07b4f5900aee42e88a6fe63a46e0072
Harley Acheson <harley.acheson@gmail.com>
2023-06-30 17:20:10 -0700

    Cleanup

--- source/blender/imbuf/intern/format_svg.cc
+++ source/blender/imbuf/intern/format_svg.cc
@@ -22,7 +22,7 @@ bool imb_is_a_svg(const uchar *mem, size_t size)
   return strstr(str, "<svg") != nullptr;
 }
 
-ImBuf *imb_load_svg(const uchar *mem, size_t size, int flags, char colorspace[IM_MAX_SPACE])
+ImBuf *imb_load_svg(const uchar *mem, size_t size, int /* flags */, char colorspace[IM_MAX_SPACE])
 {
   NSVGimage *image = nullptr;
   char *data = (char *)malloc(size + 1);
@@ -38,30 +38,29 @@ ImBuf *imb_load_svg(const uchar *mem, size_t size, int flags, char colorspace[IM
     return nullptr;
   }
 
-	int w = (int)image->width;
-  int h = (int)image->height;
+	const int w = int(image->width);
+  const int h = int(image->height);
 
   NSVGrasterizer *rast = nsvgCreateRasterizer();
   if (rast == nullptr) {
+    nsvgDelete(image);
     return nullptr;
   }
 
   colorspace_set_default_role(colorspace, IM_MAX_SPACE, COLOR_ROLE_DEFAULT_BYTE);
+
   ImBuf *ibuf = IMB_allocImBuf(w, h, 32, IB_rect);
-  if (ibuf == nullptr) {
-    return nullptr;
+  if (ibuf != nullptr) {
+    nsvgRasterize(rast, image, 0, 0, 1.0f, ibuf->byte_buffer.data, w, h, w * 4);
+    nsvgDeleteRasterizer(rast);
+    nsvgDelete(image);
+    IMB_flipy(ibuf);
   }
-  nsvgRasterize(rast, image, 0, 0, 1.0f, ibuf->byte_buffer.data, w, h, w * 4);
-
-  nsvgDeleteRasterizer(rast);
-  nsvgDelete(image);
-
-  IMB_flipy(ibuf);
   return ibuf;
 }
 
 ImBuf *imb_load_filepath_thumbnail_svg(const char *filepath,
-                                       const int flags,
+                                       const int /* flags */,
                                        const size_t max_thumb_size,
                                        char colorspace[],
                                        size_t *r_width,
@@ -72,34 +71,33 @@ ImBuf *imb_load_filepath_thumbnail_svg(const char *filepath,
     return nullptr;
   }
 
-	int w = (int)image->width;
-  int h = (int)image->height;
-
-	NSVGrasterizer *rast = nsvgCreateRasterizer();
-  if (rast == nullptr) {
-    return nullptr;
-  }
+	int w = int(image->width);
+  int h = int(image->height);
 
   /* Return full size of the image. */
   *r_width = size_t(w);
   *r_height = size_t(h);
 
+	NSVGrasterizer *rast = nsvgCreateRasterizer();
+  if (rast == nullptr) {
+    nsvgDelete(image);
+    return nullptr;
+  }
+
+  colorspace_set_default_role(colorspace, IM_MAX_SPACE, COLOR_ROLE_DEFAULT_BYTE);
+
   const float scale = float(max_thumb_size) / MAX2(w, h);
   const int dest_w = MAX2(int(w * scale), 1);
   const int dest_h = MAX2(int(h * scale), 1);
 
-  colorspace_set_default_role(colorspace, IM_MAX_SPACE, COLOR_ROLE_DEFAULT_BYTE);
   ImBuf *ibuf = IMB_allocImBuf(dest_w, dest_h, 32, IB_rect);
-  if (ibuf == nullptr) {
-    return nullptr;
+  if (ibuf != nullptr) {
+    nsvgRasterize(rast, image, 0, 0, scale, ibuf->byte_buffer.data, dest_w, dest_h, dest_w * 4);
+    nsvgDeleteRasterizer(rast);
+    nsvgDelete(image);
+    IMB_flipy(ibuf);
   }
 
-  nsvgRasterize(rast, image, 0, 0, scale, ibuf->byte_buffer.data, dest_w, dest_h, dest_w * 4);
-
-	nsvgDeleteRasterizer(rast);
-  nsvgDelete(image);
-
-  IMB_flipy(ibuf);
   return ibuf;
 }
 }
