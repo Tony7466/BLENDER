b1bd508ff395cc24ffd6582f29636810956aea9f
Miguel Pozo <pragma37@gmail.com>
2023-07-04 16:26:54 +0200

    Random cosine normal distribution on the hair surface
    Match EEVEE implementation

--- source/blender/draw/engines/eevee_next/shaders/eevee_surf_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_surf_lib.glsl
@@ -2,6 +2,7 @@
 #pragma BLENDER_REQUIRE(common_math_lib.glsl)
 #pragma BLENDER_REQUIRE(gpu_shader_codegen_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_nodetree_lib.glsl)
+#pragma BLENDER_REQUIRE(eevee_sampling_lib.glsl)
 
 #if defined(USE_BARYCENTRICS) && defined(GPU_FRAGMENT_SHADER) && defined(MAT_GEOM_MESH)
 vec3 barycentric_distances_get()
@@ -37,13 +38,26 @@ void init_globals_mesh()
 
 void init_globals_curves()
 {
+  vec3 V = cameraVec(g_data.P);
   /* Shade as a cylinder. */
   float cos_theta = interp.curves_time_width / interp.curves_thickness;
+#if defined(GPU_FRAGMENT_SHADER) && defined(MAT_GEOM_CURVES)
+  if (hairThicknessRes == 1) {
+    /* Random cosine normal distribution on the hair surface. */
+    float noise = utility_tx_fetch(utility_tx, gl_FragCoord.xy, UTIL_BLUE_NOISE_LAYER).x;
+#  ifdef EEVEE_SAMPLING_DATA
+    /* Needs to check for SAMPLING_DATA,
+     * otherwise Surfel and World (?!?!) shader validation fails. */
+    /* TODO (Miguel Pozo): Use its own sampling sequence.  */
+    noise = fract(noise + sampling_rng_1D_get(SAMPLING_AO_U));
+#  endif
+    cos_theta = noise * 2.0 - 1.0;
+  }
+#endif
   float sin_theta = sqrt(max(0.0, 1.0 - cos_theta * cos_theta));
   g_data.N = g_data.Ni = normalize(interp.N * sin_theta + interp.curves_binormal * cos_theta);
 
   /* Costly, but follows cycles per pixel tangent space (not following curve shape). */
-  vec3 V = cameraVec(g_data.P);
   g_data.curve_T = -interp.curves_tangent;
   g_data.curve_B = cross(V, g_data.curve_T);
   g_data.curve_N = safe_normalize(cross(g_data.curve_T, g_data.curve_B));
