9dd104cfa9fcc3bc562f05dc4e29132782341834
Amelie <amelie.fondevilla@les-fees-speciales.coop>
2023-07-05 14:10:08 +0200

    Definition of the erase operator

---
+++ source/blender/editors/sculpt_paint/grease_pencil_erase.cc
@@ -0,0 +1,36 @@
+/* SPDX-FileCopyrightText: 2023 Blender Foundation
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "BKE_context.h"
+#include "BKE_curves.hh"
+#include "BKE_grease_pencil.h"
+#include "BKE_grease_pencil.hh"
+#include "BKE_scene.h"
+
+#include "DEG_depsgraph_query.h"
+
+#include "ED_view3d.h"
+
+#include "WM_api.h"
+#include "WM_types.h"
+
+#include "grease_pencil_intern.hh"
+
+namespace blender::ed::sculpt_paint::greasepencil {
+
+class PaintOperation : public GreasePencilStrokeOperation {
+
+ public:
+  ~PaintOperation() override {}
+
+  void on_stroke_extended(const bContext &C, const StrokeExtension &stroke_extension) override;
+  void on_stroke_done(const bContext &C) override;
+};
+
+std::unique_ptr<GreasePencilStrokeOperation> new_paint_operation()
+{
+  return std::make_unique<PaintOperation>();
+}
+
+}  // namespace blender::ed::sculpt_paint::greasepencil
