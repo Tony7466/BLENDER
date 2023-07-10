d1ea0fac720923f0b449be7604f0ef0a68635043
Jacques Lucke <jacques@blender.org>
2023-07-10 14:38:49 +0200

    move file_panels.c

--- source/blender/editors/space_file/file_panels.cc
+++ source/blender/editors/space_file/file_panels.cc
@@ -39,13 +39,13 @@
 
 #include <string.h>
 
-static bool file_panel_operator_poll(const bContext *C, PanelType *UNUSED(pt))
+static bool file_panel_operator_poll(const bContext *C, PanelType * /*pt*/)
 {
   SpaceFile *sfile = CTX_wm_space_file(C);
   return (sfile && sfile->op);
 }
 
-static bool file_panel_asset_browsing_poll(const bContext *C, PanelType *UNUSED(pt))
+static bool file_panel_asset_browsing_poll(const bContext *C, PanelType * /*pt*/)
 {
   SpaceFile *sfile = CTX_wm_space_file(C);
   return sfile && sfile->files && ED_fileselect_is_asset_browser(sfile);
@@ -64,7 +64,7 @@ static void file_panel_operator(const bContext *C, Panel *panel)
   SpaceFile *sfile = CTX_wm_space_file(C);
   wmOperator *op = sfile->op;
 
-  UI_block_func_set(uiLayoutGetBlock(panel->layout), file_draw_check_cb, NULL, NULL);
+  UI_block_func_set(uiLayoutGetBlock(panel->layout), file_draw_check_cb, nullptr, nullptr);
 
   /* Hack: temporary hide. */
   const char *hide[] = {"filepath", "files", "directory", "filename"};
@@ -86,14 +86,15 @@ static void file_panel_operator(const bContext *C, Panel *panel)
     }
   }
 
-  UI_block_func_set(uiLayoutGetBlock(panel->layout), NULL, NULL, NULL);
+  UI_block_func_set(uiLayoutGetBlock(panel->layout), nullptr, nullptr, nullptr);
 }
 
 void file_tool_props_region_panels_register(ARegionType *art)
 {
   PanelType *pt;
 
-  pt = MEM_callocN(sizeof(PanelType), "spacetype file operator properties");
+  pt = static_cast<PanelType *>(
+      MEM_callocN(sizeof(PanelType), "spacetype file operator properties"));
   STRNCPY(pt->idname, "FILE_PT_operator");
   STRNCPY(pt->label, N_("Operator"));
   STRNCPY(pt->translation_context, BLT_I18NCONTEXT_DEFAULT_BPYRNA);
@@ -146,7 +147,7 @@ static void file_panel_execution_buttons_draw(const bContext *C, Panel *panel)
   uiLayoutSetScaleY(row, 1.3f);
 
   /* callbacks for operator check functions */
-  UI_block_func_set(block, file_draw_check_cb, NULL, NULL);
+  UI_block_func_set(block, file_draw_check_cb, nullptr, nullptr);
 
   but = uiDefButR(block,
                   UI_BTYPE_TEXT,
@@ -168,10 +169,10 @@ static void file_panel_execution_buttons_draw(const bContext *C, Panel *panel)
   BLI_assert(!UI_but_flag_is_set(but, UI_BUT_UNDO));
   BLI_assert(!UI_but_is_utf8(but));
 
-  UI_but_func_complete_set(but, autocomplete_file, NULL);
+  UI_but_func_complete_set(but, autocomplete_file, nullptr);
   /* silly workaround calling NFunc to ensure this does not get called
    * immediate ui_apply_but_func but only after button deactivates */
-  UI_but_funcN_set(but, file_filename_enter_handle, NULL, but);
+  UI_but_funcN_set(but, file_filename_enter_handle, nullptr, but);
 
   if (params->flag & FILE_CHECK_EXISTING) {
     but_extra_rna_ptr = UI_but_extra_operator_icon_add(
@@ -186,7 +187,7 @@ static void file_panel_execution_buttons_draw(const bContext *C, Panel *panel)
   if (overwrite_alert) {
     UI_but_flag_enable(but, UI_BUT_REDALERT);
   }
-  UI_block_func_set(block, NULL, NULL, NULL);
+  UI_block_func_set(block, nullptr, nullptr, nullptr);
 
   {
     uiLayout *sub = uiLayoutRow(row, false);
@@ -207,7 +208,8 @@ void file_execute_region_panels_register(ARegionType *art)
 {
   PanelType *pt;
 
-  pt = MEM_callocN(sizeof(PanelType), "spacetype file execution buttons");
+  pt = static_cast<PanelType *>(
+      MEM_callocN(sizeof(PanelType), "spacetype file execution buttons"));
   STRNCPY(pt->idname, "FILE_PT_execution_buttons");
   STRNCPY(pt->label, N_("Execute Buttons"));
   STRNCPY(pt->translation_context, BLT_I18NCONTEXT_DEFAULT_BPYRNA);
@@ -224,7 +226,7 @@ static void file_panel_asset_catalog_buttons_draw(const bContext *C, Panel *pane
   /* May be null if the library wasn't loaded yet. */
   struct AssetLibrary *asset_library = filelist_asset_library(sfile->files);
   FileAssetSelectParams *params = ED_fileselect_get_asset_params(sfile);
-  BLI_assert(params != NULL);
+  BLI_assert(params != nullptr);
 
   uiLayout *col = uiLayoutColumn(panel->layout, false);
   uiLayout *row = uiLayoutRow(col, true);
@@ -259,7 +261,8 @@ void file_tools_region_panels_register(ARegionType *art)
 {
   PanelType *pt;
 
-  pt = MEM_callocN(sizeof(PanelType), "spacetype file asset catalog buttons");
+  pt = static_cast<PanelType *>(
+      MEM_callocN(sizeof(PanelType), "spacetype file asset catalog buttons"));
   STRNCPY(pt->idname, "FILE_PT_asset_catalog_buttons");
   STRNCPY(pt->label, N_("Asset Catalogs"));
   STRNCPY(pt->translation_context, BLT_I18NCONTEXT_DEFAULT_BPYRNA);
