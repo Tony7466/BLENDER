202091205956b2cea6d5e00f998ebb408b7dff74
Amelie <amelie.fondevilla@les-fees-speciales.coop>
2023-07-11 17:22:03 +0200

    Splitting curves

--- source/blender/editors/sculpt_paint/grease_pencil_erase.cc
+++ source/blender/editors/sculpt_paint/grease_pencil_erase.cc
@@ -99,13 +99,24 @@ struct EraseOperationExecutor {
                                      V3D_PROJ_TEST_NOP);
     }
     if (debug) {
-      std::cout << "Source positions = [ ";
+      std::cout << "Source positions (2D) = [ ";
       for (const float2 &pos : screen_space_positions) {
         print_float2(pos);
         std::cout << " ";
       }
       std::cout << "]" << std::endl;
+      std::cout << "Source positions (3D)  = [ ";
+      for (const float3 &pos : src.positions()) {
+        std::cout << "(" << pos[0] << ", " << pos[1] << ", " << pos[2] << ") ";
+      }
+      std::cout << "]" << std::endl;
     }
+
+    auto compute_intersection_parameter = [](float2 p0, float2 p1, float2 inter) {
+      float mu = (len_v2(inter - p0) / len_v2(p1 - p0));
+      float sign_mu = (dot_v2v2(inter - p0, p1 - p0) < 0) ? -1.0 : 1.0;
+      return sign_mu * mu;
+    };
 
     /* Check segments that have an intersection */
     Array<bool> has_intersection(src_points_num, false);
@@ -127,10 +138,10 @@ struct EraseOperationExecutor {
         /* The function above returns the intersections with the (infinite) line,
          * so we have to make sure they lie within the segment.  */
         float mu0 = (nb_inter > 0) ?
-                        (len_v2(inter0 - pos_view) / len_v2(pos_after_view - pos_view)) :
+                        compute_intersection_parameter(pos_view, pos_after_view, inter0) :
                         -1.0;
         float mu1 = (nb_inter > 1) ?
-                        (len_v2(inter1 - pos_view) / len_v2(pos_after_view - pos_view)) :
+                        compute_intersection_parameter(pos_view, pos_after_view, inter1) :
                         -1.0;
 
         has_intersection[src_point_index] = IN_RANGE(mu0, 0, 1) || IN_RANGE(mu1, 0, 1);
@@ -185,11 +196,15 @@ struct EraseOperationExecutor {
         /* The function above returns the intersections with the (infinite) line,
          * so we have to make sure they lie within the segment.  */
         float mu0 = (nb_inter > 0) ?
-                        (len_v2(inter0 - pos_view) / len_v2(pos_after_view - pos_view)) :
+                        compute_intersection_parameter(pos_view, pos_after_view, inter0) :
                         -1.0;
         float mu1 = (nb_inter > 1) ?
-                        (len_v2(inter1 - pos_view) / len_v2(pos_after_view - pos_view)) :
+                        compute_intersection_parameter(pos_view, pos_after_view, inter1) :
                         -1.0;
+
+        if (mu0 > mu1) {
+          std::swap(mu0, mu1);
+        }
 
         if (IN_RANGE(mu0, 0, 1)) {
           dst_points_parameters[++dst_point_index] = src_point_index + mu0;
@@ -214,30 +229,82 @@ struct EraseOperationExecutor {
       std::cout << "]" << std::endl;
     }
 
-    /* Create the new curves geometry*/
-    CurvesGeometry dst = bke::curves::copy_only_curve_domain(src);
-
-    /* Compute destination curves offsets
-       TODO : For now, we do not account for cuts.  */
-    MutableSpan<int> dst_curve_offsets = dst.offsets_for_write();
-    int offset = 0;
-    for (int curve_index = 0; curve_index < src_curves_num; curve_index++) {
-      dst_curve_offsets[curve_index] = offset;
-
-      IndexRange src_point_range = src_points_by_curves[curve_index];
+    Vector<int> dst_curves_offset;
+    Vector<int> dst_to_src_curve_index;
+    int dst_offset = 0;
+    dst_curves_offset.append(0);
+    for (int src_curve_index = 0; src_curve_index < src_curves_num; src_curve_index++) {
+      IndexRange src_point_range = src_points_by_curves[src_curve_index];
+      int length_of_current = 0;
       for (int src_point_index : src_point_range) {
-        offset += nb_intersections[src_point_index];
-        offset += 1 - int(is_point_inside[src_point_index]);
+        if (!is_point_inside[src_point_index]) {
+          ++length_of_current;
+        }
+        if (has_intersection[src_point_index]) {
+          if (nb_intersections[src_point_index] == 2) {
+            ++length_of_current;
+          }
+          if ((length_of_current != 0) &&
+              (is_point_inside[src_point_index] || (nb_intersections[src_point_index] == 2)))
+          {
+            /* The Intersection is the new first point of a curve */
+            dst_offset += length_of_current;
+            dst_curves_offset.append(dst_offset);
+            dst_to_src_curve_index.append(src_curve_index);
+            length_of_current = 0;
+          }
+          ++length_of_current;
+        }
+      }
+      if (length_of_current != 0) {
+        /* End of a source curve : add a new curve */
+        dst_offset += length_of_current;
+        dst_curves_offset.append(dst_offset);
+        dst_to_src_curve_index.append(src_curve_index);
       }
     }
-    dst_curve_offsets.last() = offset;
+    if (debug) {
+      std::cout << "Dest curves range = [ ";
+      for (const int &offset : dst_curves_offset) {
+        std::cout << offset << " ";
+      }
+      std::cout << "]" << std::endl;
+      std::cout << "Dest to source curve index = [ ";
+      int dst_curve = -1;
+      for (const int &src_curve : dst_to_src_curve_index) {
+        std::cout << ++dst_curve << ":" << src_curve << ", ";
+      }
+      std::cout << "]" << std::endl;
+    }
+    const int dst_curves_num = dst_curves_offset.size() - 1;
 
-    /* Resize point data to account for new points */
-    dst.resize(dst_points_num, dst.curves_num());
+    /* Create the new curves geometry*/
+    CurvesGeometry dst(dst_points_num, dst_curves_num);
+    MutableSpan<int> dst_offsets_for_write = dst.offsets_for_write();
+    for (int dst_curve_index : IndexRange(dst_curves_num)) {
+      dst_offsets_for_write[dst_curve_index] = dst_curves_offset[dst_curve_index];
+    }
 
     const bke::AttributeAccessor src_attributes = src.attributes();
     bke::MutableAttributeAccessor dst_attributes = dst.attributes_for_write();
     AnonymousAttributePropagationInfo propagation_info{};
+
+    /* Copy curves data */
+    for (bke::AttributeTransferData &attribute : bke::retrieve_attributes_for_transfer(
+             src_attributes, dst_attributes, ATTR_DOMAIN_MASK_CURVE, propagation_info))
+    {
+      bke::attribute_math::convert_to_static_type(attribute.dst.span.type(), [&](auto dummy) {
+        using T = decltype(dummy);
+        auto src_attr = attribute.src.typed<T>();
+        auto dst_attr = attribute.dst.span.typed<T>();
+
+        for (int dst_curve_index : IndexRange(dst_curves_num)) {
+          const int src_curve_index = dst_to_src_curve_index[dst_curve_index];
+          dst_attr[dst_curve_index] = src_attr[src_curve_index];
+        }
+        attribute.dst.finish();
+      });
+    }
 
     /* Copy/Interpolate point data */
     for (bke::AttributeTransferData &attribute : bke::retrieve_attributes_for_transfer(
@@ -245,12 +312,12 @@ struct EraseOperationExecutor {
     {
       bke::attribute_math::convert_to_static_type(attribute.dst.span.type(), [&](auto dummy) {
         using T = decltype(dummy);
+        auto src_attr = attribute.src.typed<T>();
+        auto dst_attr = attribute.dst.span.typed<T>();
 
         for (int dst_point_index = 0; dst_point_index < dst_points_num; ++dst_point_index) {
           const float dst_param = dst_points_parameters[dst_point_index];
           const int src_point_index = std::floor(dst_param);
-          auto src_attr = attribute.src.typed<T>();
-          auto dst_attr = attribute.dst.span.typed<T>();
 
           if (is_cut[dst_point_index]) {
             const float src_pt_factor = dst_param - src_point_index;
@@ -263,6 +330,25 @@ struct EraseOperationExecutor {
         }
         attribute.dst.finish();
       });
+    }
+
+    if (debug) {
+      std::cout << "New geometry" << std::endl;
+      std::cout << dst.points_num() << " points, " << dst.curves_num() << " curves." << std::endl;
+
+      offset_indices::OffsetIndices<int> dst_curves_offsets = dst.points_by_curve();
+      std::cout << "Curve offsets = [";
+      for (int curve_index : dst.curves_range()) {
+        IndexRange dst_pt_range = dst_curves_offsets[curve_index];
+        std::cout << dst_pt_range.first() << ":" << dst_pt_range.last() << " ";
+      }
+      std::cout << "]" << std::endl;
+
+      std::cout << "Destin positions (3D)  = [ ";
+      for (const float3 &pos : dst.positions()) {
+        std::cout << "(" << pos[0] << ", " << pos[1] << ", " << pos[2] << ") ";
+      }
+      std::cout << "]" << std::endl;
     }
 
     drawing.geometry.wrap() = dst;
