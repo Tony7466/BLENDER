ab926acd4526d612611099f9403083e54be02d65
Amelie <amelie.fondevilla@les-fees-speciales.coop>
2023-07-11 11:26:52 +0200

    The eraser finally does its job

--- source/blender/editors/sculpt_paint/grease_pencil_erase.cc
+++ source/blender/editors/sculpt_paint/grease_pencil_erase.cc
@@ -42,64 +42,6 @@ struct EraseOperationExecutor {
 
   EraseOperationExecutor(const bContext & /*C*/) {}
 
-  void interpolate_point(blender::bke::CurvesGeometry &dst,
-                         const int dst_index,
-                         const blender::bke::CurvesGeometry &src,
-                         const int src_index_0,
-                         const int src_index_1,
-                         const float factor,
-                         const blender::bke::AnonymousAttributePropagationInfo &propagation_info)
-  {
-    using namespace blender;
-    const bke::AttributeAccessor src_attributes = src.attributes();
-    bke::MutableAttributeAccessor dst_attributes = dst.attributes_for_write();
-
-    for (bke::AttributeTransferData &attribute : bke::retrieve_attributes_for_transfer(
-             src_attributes, dst_attributes, ATTR_DOMAIN_MASK_POINT, propagation_info))
-    {
-      attribute.dst.span.slice(IndexRange(dst_index, 1))
-          .copy_from(attribute.src.slice(IndexRange(src_index_0, 1)));
-      attribute.dst.finish();
-    }
-  }
-
-  void copy_point(blender::bke::CurvesGeometry &dst,
-                  const int dst_index,
-                  const blender::bke::CurvesGeometry &src,
-                  const int src_index,
-                  const blender::bke::AnonymousAttributePropagationInfo &propagation_info)
-  {
-    using namespace blender;
-    const bke::AttributeAccessor src_attributes = src.attributes();
-    bke::MutableAttributeAccessor dst_attributes = dst.attributes_for_write();
-
-    for (bke::AttributeTransferData &attribute : bke::retrieve_attributes_for_transfer(
-             src_attributes, dst_attributes, ATTR_DOMAIN_MASK_POINT, propagation_info))
-    {
-      attribute.dst.span.slice(IndexRange(dst_index, 1))
-          .copy_from(attribute.src.slice(IndexRange(src_index, 1)));
-      attribute.dst.finish();
-    }
-  }
-
-  void copy_point(blender::bke::CurvesGeometry &dst,
-                  const IndexRange dst_range,
-                  const blender::bke::CurvesGeometry &src,
-                  const IndexRange src_range,
-                  const blender::bke::AnonymousAttributePropagationInfo &propagation_info)
-  {
-    using namespace blender;
-    const bke::AttributeAccessor src_attributes = src.attributes();
-    bke::MutableAttributeAccessor dst_attributes = dst.attributes_for_write();
-
-    for (bke::AttributeTransferData &attribute : bke::retrieve_attributes_for_transfer(
-             src_attributes, dst_attributes, ATTR_DOMAIN_MASK_POINT, propagation_info))
-    {
-      attribute.dst.span.slice(dst_range).copy_from(attribute.src.slice(src_range));
-      attribute.dst.finish();
-    }
-  }
-
   template<typename T>
   static inline void interpolation(const T &a, const T &b, Span<float> factors, MutableSpan<T> dst)
   {
@@ -107,8 +49,6 @@ struct EraseOperationExecutor {
       dst[i] = bke::attribute_math::mix2<T>(factors[i], a, b);
     }
   }
-
-  void set_curves_range(CurvesGeometry &dst, const int curve_index, const int first_pt_index) {}
 
   void execute(EraseOperation & /*self*/,
                const bContext &C,
@@ -120,12 +60,19 @@ struct EraseOperationExecutor {
     ARegion *region = CTX_wm_region(&C);
     Object *obact = CTX_data_active_object(&C);
     Object *ob_eval = DEG_get_evaluated_object(depsgraph, obact);
-
-    const int MAX_NB_CUTS = 3;
+    bool debug = true;
+    auto print_float2 = [](const float2 &vec) {
+      std::cout << "(" << vec[0] << ", " << vec[1] << ")";
+    };
 
     /* Get the tool's data */
     float2 mouse_position = stroke_extension.mouse_position;
     float eraser_radius = stroke_extension.pressure * 100;  // TODO : Fix the computation of radius
+    if (debug) {
+      std::cout << "Mouse position ";
+      print_float2(mouse_position);
+      std::cout << std::endl;
+    }
 
     /* Get the grease pencil drawing */
     GreasePencil &grease_pencil = *static_cast<GreasePencil *>(obact->data);
@@ -139,55 +86,95 @@ struct EraseOperationExecutor {
             grease_pencil.drawings_for_write()[drawing_index]);
 
     CurvesGeometry &src = drawing.strokes_for_write();
-    const offset_indices::OffsetIndices<int> src_points_by_curve = src.points_by_curve();
     const VArray<bool> cyclic = src.cyclic();
+    const int src_points_num = src.points_num();
+    const int src_curves_num = src.curves_num();
 
-    int points_to_add = 0;
-    int points_to_remove = 0;
-    int curve_count = 0;
-    Array<int> points_in_dst(src.points_num() + src.curves_num(), 0);
-    Array<float> dst_factors(MAX_NB_CUTS * src.points_range().size(), -1.0f);
-    Array<bool> keep_point(src.points_num(), true);
+    /* Compute screen space positions */
+    Array<float2> screen_space_positions(src_points_num);
+    for (int point_index = 0; point_index < src_points_num; point_index++) {
+      ED_view3d_project_float_global(region,
+                                     deformation.positions[point_index],
+                                     screen_space_positions[point_index],
+                                     V3D_PROJ_TEST_NOP);
+    }
+    if (debug) {
+      std::cout << "Source positions = [ ";
+      for (const float2 &pos : screen_space_positions) {
+        print_float2(pos);
+        std::cout << " ";
+      }
+      std::cout << "]" << std::endl;
+    }
 
-    /* Compute intersection points with the eraser */
-    // threading::parallel_for(curves.curves_range(), 256, [&](const IndexRange &range) {
-    //   for (const int curve_i : range) {
+    /* Check segments that have an intersection */
+    Array<bool> has_intersection(src_points_num, false);
+    Array<int> nb_intersections(src_points_num, 0);
+    int intersection_count = 0;
+    offset_indices::OffsetIndices<int> src_points_by_curves = src.points_by_curve();
+    for (int curve_index = 0; curve_index < src_curves_num; curve_index++) {
+      IndexRange src_point_range = src_points_by_curves[curve_index];
+      for (int src_point_index : src_point_range.drop_back(1)) {
+        const float2 pos_view = screen_space_positions[src_point_index];
+        const float2 pos_after_view = screen_space_positions[src_point_index + 1];
 
-    for (const int curve_i : src.curves_range()) {
-      IndexRange points = src_points_by_curve[curve_i];
-      const bool is_cyclic = cyclic[curve_i];
-      const int nb_pts = points.size();
-      const int first_pt = points.first();
-      bool first_point_trimmed = false;
+        /* Compute intersections between the current segment and the eraser's area */
+        float2 inter0{};
+        float2 inter1{};
+        const int nb_inter = isect_line_sphere_v2(
+            pos_view, pos_after_view, mouse_position, eraser_radius, inter0, inter1);
 
-      // threading::parallel_for(points.drop_back(1), 256, [&](const IndexRange range) {
-      //   for (const int segment_i : range) {
+        /* The function above returns the intersections with the (infinite) line,
+         * so we have to make sure they lie within the segment.  */
+        float mu0 = (nb_inter > 0) ?
+                        (len_v2(inter0 - pos_view) / len_v2(pos_after_view - pos_view)) :
+                        -1.0;
+        float mu1 = (nb_inter > 1) ?
+                        (len_v2(inter1 - pos_view) / len_v2(pos_after_view - pos_view)) :
+                        -1.0;
 
-      for (const int segment_i : points.drop_back(1)) {
-        const float3 pos = deformation.positions[segment_i];
-        int intersection_index = MAX_NB_CUTS * segment_i - 1;
+        has_intersection[src_point_index] = IN_RANGE(mu0, 0, 1) || IN_RANGE(mu1, 0, 1);
+        nb_intersections[src_point_index] = int(IN_RANGE(mu0, 0, 1)) + int(IN_RANGE(mu1, 0, 1));
+        intersection_count += nb_intersections[src_point_index];
+      }
+    }
+    if (debug) {
+      std::cout << "NB Intersections = [ ";
+      for (const int &nb : nb_intersections) {
+        std::cout << nb << " ";
+      }
+      std::cout << "]" << std::endl;
+    }
 
-        /* Compute screen space point position */
-        float2 pos_view{};
-        ED_view3d_project_float_global(region, pos, pos_view, V3D_PROJ_TEST_NOP);
+    /* Check if points are inside the eraser */
+    Array<bool> is_point_inside(src_points_num, false);
+    int point_inside_count = 0;
+    for (int point_index = 0; point_index < src_points_num - 1; point_index++) {
+      const float2 pos_view = screen_space_positions[point_index];
+      is_point_inside[point_index] = (len_squared_v2v2(pos_view, mouse_position) <=
+                                      eraser_radius * eraser_radius);
+      point_inside_count += int(is_point_inside[point_index]);
+    }
+    if (debug) {
+      std::cout << "Point inside = [ ";
+      for (const bool &inside : is_point_inside) {
+        std::cout << inside << " ";
+      }
+      std::cout << "]" << std::endl;
+    }
 
-        float3 pos_after = deformation.positions[segment_i + 1];
-        float2 pos_after_view{};
-        ED_view3d_project_float_global(region, pos, pos_view, V3D_PROJ_TEST_NOP);
-
-        /* Check if the point is inside the eraser */
-        if (len_squared_v2v2(pos_view, mouse_position) <= eraser_radius * eraser_radius) {
-          points_to_remove++;
-          keep_point[segment_i] = false;
-
-          if (segment_i == points.first()) {
-            first_point_trimmed = true;
-          }
-        }
-        else {
-          points_in_dst[segment_i + curve_i]++;
-          dst_factors[++intersection_index] = 0.0;
-        }
+    /* Compute the factors of the destination points */
+    const int dst_points_num = src_points_num + intersection_count - point_inside_count;
+    Array<float> dst_points_parameters(dst_points_num);
+    Array<bool> is_cut(dst_points_num, false);
+    int dst_point_index = -1;
+    for (int src_point_index = 0; src_point_index < src_points_num; src_point_index++) {
+      if (!is_point_inside[src_point_index]) {
+        dst_points_parameters[++dst_point_index] = float(src_point_index);
+      }
+      if (has_intersection[src_point_index]) {
+        const float2 pos_view = screen_space_positions[src_point_index];
+        const float2 pos_after_view = screen_space_positions[src_point_index + 1];
 
         /* Compute intersections between the current segment and the eraser's area */
         float2 inter0{};
@@ -205,67 +192,51 @@ struct EraseOperationExecutor {
                         -1.0;
 
         if (IN_RANGE(mu0, 0, 1)) {
-          points_in_dst[segment_i + curve_i]++;
-          dst_factors[++intersection_index] = mu0;
-          points_to_add++;
+          dst_points_parameters[++dst_point_index] = src_point_index + mu0;
+          is_cut[dst_point_index] = true;
         }
         if (IN_RANGE(mu1, 0, 1)) {
-          points_in_dst[segment_i + curve_i]++;
-          dst_factors[++intersection_index] = mu1;
-          points_to_add++;
-        }
-
-        if (segment_i + 1 == points.last()) {
-          /* Last point of the curve */
-          if (len_squared_v2v2(pos_after_view, mouse_position) <= eraser_radius * eraser_radius) {
-            points_to_remove++;
-            keep_point[segment_i + 1] = false;
-          }
-          else {
-            points_in_dst[segment_i + curve_i + 1]++;
-            dst_factors[MAX_NB_CUTS * (segment_i + 1)] = 0.0;
-          }
+          dst_points_parameters[++dst_point_index] = src_point_index + mu1;
+          is_cut[dst_point_index] = true;
         }
       }
-      // });
-
-      if (is_cyclic) {
-        /* TODO */
+    }
+    if (debug) {
+      std::cout << "Dest factors = [ ";
+      for (const float &param : dst_points_parameters) {
+        std::cout << param << " ";
       }
+      std::cout << "]" << std::endl;
+      std::cout << "Is Cut = [ ";
+      for (const bool &cut : is_cut) {
+        std::cout << cut << " ";
+      }
+      std::cout << "]" << std::endl;
     }
-    // });
-
-    /* Compute the accumulated offset */
-    OffsetIndices<int> acc_offsets = blender::offset_indices::accumulate_counts_to_offsets(
-        points_in_dst);
-
-    std::cout << "acc_offsets: " << std::endl;
-    for (const int i : acc_offsets.index_range()) {
-      std::cout << acc_offsets[i] << ", ";
-    }
-    std::cout << std::endl;
 
     /* Create the new curves geometry*/
-    int point_count = src.points_num() - points_to_remove + points_to_add;
     CurvesGeometry dst = bke::curves::copy_only_curve_domain(src);
-    MutableSpan<int> dst_curve_offsets = dst.offsets_for_write();
 
-    /* Fill the curves offsets
-       TODO : For now, we do not account for cuts. */
-    IndexMask src_curves_range(src.curves_range());
-    src_curves_range.foreach_index([&](const int curve_index) {
-      const int curve_first_point = src_points_by_curve[curve_index].first();
-      dst_curve_offsets[curve_index] = acc_offsets[curve_first_point + curve_index].first();
-    });
-    dst_curve_offsets.last() = acc_offsets.total_size();
+    /* Compute destination curves offsets
+       TODO : For now, we do not account for cuts.  */
+    MutableSpan<int> dst_curve_offsets = dst.offsets_for_write();
+    int offset = 0;
+    for (int curve_index = 0; curve_index < src_curves_num; curve_index++) {
+      dst_curve_offsets[curve_index] = offset;
+
+      IndexRange src_point_range = src_points_by_curves[curve_index];
+      for (int src_point_index : src_point_range) {
+        offset += nb_intersections[src_point_index];
+        offset += 1 - int(is_point_inside[src_point_index]);
+      }
+    }
+    dst_curve_offsets.last() = offset;
 
     /* Resize point data to account for new points */
-    dst.resize(point_count, dst.curves_num());
+    dst.resize(dst_points_num, dst.curves_num());
 
     const bke::AttributeAccessor src_attributes = src.attributes();
     bke::MutableAttributeAccessor dst_attributes = dst.attributes_for_write();
-
-    OffsetIndices<int> dst_points_by_curve = dst.points_by_curve();
     AnonymousAttributePropagationInfo propagation_info{};
 
     /* Copy/Interpolate point data */
@@ -275,64 +246,30 @@ struct EraseOperationExecutor {
       bke::attribute_math::convert_to_static_type(attribute.dst.span.type(), [&](auto dummy) {
         using T = decltype(dummy);
 
-        src_curves_range.foreach_index(GrainSize(512), [&](const int curve_i) {
-          /* Copy/Interpolate point data of a single curve */
-          const IndexRange src_points_range = src_points_by_curve[curve_i];
-          const IndexRange src_offset_range = bke::curves::per_curve_point_offsets_range(
-              src_points_range, curve_i);
+        for (int dst_point_index = 0; dst_point_index < dst_points_num; ++dst_point_index) {
+          const float dst_param = dst_points_parameters[dst_point_index];
+          const int src_point_index = std::floor(dst_param);
+          auto src_attr = attribute.src.typed<T>();
+          auto dst_attr = attribute.dst.span.typed<T>();
 
-          OffsetIndices<int> dst_offset = acc_offsets.slice(src_offset_range);
-          const IndexRange dst_points_range = dst_points_by_curve[curve_i];
-
-          const Span<T> src_attr = attribute.src.typed<T>().slice(src_points_range);
-          MutableSpan<T> dst_attr = attribute.dst.span.typed<T>().slice(dst_points_range);
-
-          threading::parallel_for(
-              src_attr.index_range().drop_back(1), 1024, [&](IndexRange range) {
-                for (const int src_point_index : range) {
-                  const IndexRange segment_points = dst_offset[src_point_index];
-                  if (segment_points.size() == 0) {
-                    continue;
-                  }
-
-                  Span<float> factors = dst_factors.as_span().slice(
-                      IndexRange(MAX_NB_CUTS * src_point_index, segment_points.size()));
-
-                  // dst_factors.slice(segment_points)
-
-                  interpolation<T>(src_attr[src_point_index],
-                                   src_attr[src_point_index + 1],
-                                   factors,
-                                   dst_attr.slice(segment_points));
-                }
-              });
-          attribute.dst.finish();
-        });
+          if (is_cut[dst_point_index]) {
+            const float src_pt_factor = dst_param - src_point_index;
+            dst_attr[dst_point_index] = bke::attribute_math::mix2<T>(
+                src_pt_factor, src_attr[src_point_index], src_attr[src_point_index + 1]);
+          }
+          else {
+            dst_attr[dst_point_index] = src_attr[src_point_index];
+          }
+        }
+        attribute.dst.finish();
       });
     }
 
-    // /* Copy point domain. */
-    // AnonymousAttributePropagationInfo propagation_info{};
-
-    // const bke::AttributeAccessor src_attributes = src.attributes();
-    // bke::MutableAttributeAccessor dst_attributes = dst.attributes_for_write();
-
-    // const OffsetIndices<int> src_points_by_curve = src.points_by_curve();
-    // const OffsetIndices<int> dst_points_by_curve = dst.points_by_curve();
-
-    // for (auto &attribute : bke::retrieve_attributes_for_transfer(
-    //          src_attributes, dst_attributes, ATTR_DOMAIN_MASK_POINT, propagation_info))
-    // {
-    //   bke::curves::copy_point_data(src_points_by_curve,
-    //                                dst_points_by_curve,
-    //                                dst.curves_range(),
-    //                                attribute.src,
-    //                                attribute.dst.span);
-    //   attribute.dst.finish();
-    // }
-
     drawing.geometry.wrap() = dst;
     drawing.tag_positions_changed();
+
+    DEG_id_tag_update(&grease_pencil.id, ID_RECALC_GEOMETRY);
+    WM_event_add_notifier(&C, NC_GEOM | ND_DATA, &grease_pencil);
   }
 };
 
