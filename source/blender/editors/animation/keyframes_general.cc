21b3e48b8bfee9fedb026a3ce126ec20d75e2c53
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-07-13 15:03:41 +0200

    remove changes for better backporting to 3.6

--- source/blender/editors/animation/keyframes_general.cc
+++ source/blender/editors/animation/keyframes_general.cc
@@ -557,7 +557,7 @@ void butterworth_smooth_fcurve_segment(FCurve *fcu,
     }
 
     const float x_delta = fcu->bezt[i].vec[1][0] - left_bezt.vec[1][0] + filter_order;
-    const int filter_index = round(x_delta * sample_rate);
+    const int filter_index = (int)(x_delta * sample_rate);
     const float blend_value = butterworth_calculate_blend_value(samples,
                                                                 filtered_values,
                                                                 samples_start_index,
@@ -613,7 +613,7 @@ void smooth_fcurve_segment(FCurve *fcu,
                            const int kernel_size,
                            double *kernel)
 {
-  const float segment_end_index = segment->start_index + segment->length;
+  const int segment_end_index = segment->start_index + segment->length;
   const float segment_start_x = fcu->bezt[segment->start_index].vec[1][0];
   for (int i = segment->start_index; i < segment_end_index; i++) {
     const int sample_index = round(fcu->bezt[i].vec[1][0] - segment_start_x) + kernel_size;
