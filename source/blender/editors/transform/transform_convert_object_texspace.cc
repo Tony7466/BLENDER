71ce033ea33fcf8162d20b80e922d43628b68c11
Jacques Lucke <jacques@blender.org>
2023-07-13 09:45:43 +0200

    move transform_convert_object_texspace.c

--- source/blender/editors/transform/transform_convert_object_texspace.cc
+++ source/blender/editors/transform/transform_convert_object_texspace.cc
@@ -31,7 +31,7 @@
  *
  * \{ */
 
-static void createTransTexspace(bContext *UNUSED(C), TransInfo *t)
+static void createTransTexspace(bContext * /*C*/, TransInfo *t)
 {
   ViewLayer *view_layer = t->view_layer;
   TransData *td;
@@ -42,12 +42,12 @@ static void createTransTexspace(bContext *UNUSED(C), TransInfo *t)
   BKE_view_layer_synced_ensure(t->scene, t->view_layer);
   ob = BKE_view_layer_active_object_get(view_layer);
 
-  if (ob == NULL) { /* Shouldn't logically happen, but still. */
+  if (ob == nullptr) { /* Shouldn't logically happen, but still. */
     return;
   }
 
-  id = ob->data;
-  if (id == NULL || !ELEM(GS(id->name), ID_ME, ID_CU_LEGACY, ID_MB)) {
+  id = static_cast<ID *>(ob->data);
+  if (id == nullptr || !ELEM(GS(id->name), ID_ME, ID_CU_LEGACY, ID_MB)) {
     BKE_report(t->reports, RPT_ERROR, "Unsupported object type for text-space transform");
     return;
   }
@@ -61,8 +61,9 @@ static void createTransTexspace(bContext *UNUSED(C), TransInfo *t)
     BLI_assert(t->data_container_len == 1);
     TransDataContainer *tc = t->data_container;
     tc->data_len = 1;
-    td = tc->data = MEM_callocN(sizeof(TransData), "TransTexspace");
-    td->ext = tc->data_ext = MEM_callocN(sizeof(TransDataExtension), "TransTexspace");
+    td = tc->data = static_cast<TransData *>(MEM_callocN(sizeof(TransData), "TransTexspace"));
+    td->ext = tc->data_ext = static_cast<TransDataExtension *>(
+        MEM_callocN(sizeof(TransDataExtension), "TransTexspace"));
   }
 
   td->flag = TD_SELECTED;
@@ -114,5 +115,5 @@ TransConvertTypeInfo TransConvertType_ObjectTexSpace = {
     /*flags*/ 0,
     /*createTransData*/ createTransTexspace,
     /*recalcData*/ recalcData_texspace,
-    /*special_aftertrans_update*/ NULL,
+    /*special_aftertrans_update*/ nullptr,
 };
