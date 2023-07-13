23b91e9060dd51af788beda6f4a498f3e9a66dac
Jacques Lucke <jacques@blender.org>
2023-07-13 11:14:27 +0200

    add more static casts

--- source/blender/editors/transform/transform_convert.cc
+++ source/blender/editors/transform/transform_convert.cc
@@ -421,7 +421,7 @@ void calc_distanceCurveVerts(TransData *head, TransData *tail, bool cyclic)
     }
   }
 
-  while ((td = BLI_LINKSTACK_POP(queue))) {
+  while ((td = static_cast<TransData *>(BLI_LINKSTACK_POP(queue)))) {
     float dist;
     float vec[3];
 
