1f11e4378a56a956c8ca83e7df4937bd25fc24bf
Jacques Lucke <jacques@blender.org>
2023-07-13 10:04:37 +0200

    move transform_mode_customdata.c

--- source/blender/editors/transform/transform_mode_customdata.cc
+++ source/blender/editors/transform/transform_mode_customdata.cc
@@ -40,12 +40,12 @@ struct TransDataArgs_Value {
   float value;
 };
 
-static void transdata_elem_value(const TransInfo *UNUSED(t),
-                                 const TransDataContainer *UNUSED(tc),
+static void transdata_elem_value(const TransInfo * /*t*/,
+                                 const TransDataContainer * /*tc*/,
                                  TransData *td,
                                  const float value)
 {
-  if (td->val == NULL) {
+  if (td->val == nullptr) {
     return;
   }
 
@@ -55,9 +55,9 @@ static void transdata_elem_value(const TransInfo *UNUSED(t),
 
 static void transdata_elem_value_fn(void *__restrict iter_data_v,
                                     const int iter,
-                                    const TaskParallelTLS *__restrict UNUSED(tls))
+                                    const TaskParallelTLS *__restrict /*tls*/)
 {
-  struct TransDataArgs_Value *data = iter_data_v;
+  TransDataArgs_Value *data = static_cast<TransDataArgs_Value *>(iter_data_v);
   TransData *td = &data->tc->data[iter];
   if (td->flag & TD_SKIP) {
     return;
@@ -121,11 +121,10 @@ static void apply_value_impl(TransInfo *t, const char *value_name)
       }
     }
     else {
-      struct TransDataArgs_Value data = {
-          .t = t,
-          .tc = tc,
-          .value = value,
-      };
+      TransDataArgs_Value data{};
+      data.t = t;
+      data.tc = tc;
+      data.value = value;
       TaskParallelSettings settings;
       BLI_parallel_range_settings_defaults(&settings);
       BLI_task_parallel_range(0, tc->data_len, &data, transdata_elem_value_fn, &settings);
@@ -137,12 +136,12 @@ static void apply_value_impl(TransInfo *t, const char *value_name)
   ED_area_status_text(t->area, str);
 }
 
-static void applyCrease(TransInfo *t, const int UNUSED(mval[2]))
+static void applyCrease(TransInfo *t, const int[2] /*mval*/)
 {
   apply_value_impl(t, TIP_("Crease"));
 }
 
-static void applyBevelWeight(TransInfo *t, const int UNUSED(mval[2]))
+static void applyBevelWeight(TransInfo *t, const int[2] /*mval*/)
 {
   apply_value_impl(t, TIP_("Bevel Weight"));
 }
@@ -161,19 +160,19 @@ static void init_mode_impl(TransInfo *t)
   t->num.unit_type[0] = B_UNIT_NONE;
 }
 
-static void initEgdeCrease(TransInfo *t, struct wmOperator *UNUSED(op))
+static void initEgdeCrease(TransInfo *t, struct wmOperator * /*op*/)
 {
   init_mode_impl(t);
   t->mode = TFM_EDGE_CREASE;
 }
 
-static void initVertCrease(TransInfo *t, struct wmOperator *UNUSED(op))
+static void initVertCrease(TransInfo *t, struct wmOperator * /*op*/)
 {
   init_mode_impl(t);
   t->mode = TFM_VERT_CREASE;
 }
 
-static void initBevelWeight(TransInfo *t, struct wmOperator *UNUSED(op))
+static void initBevelWeight(TransInfo *t, struct wmOperator * /*op*/)
 {
   init_mode_impl(t);
   t->mode = TFM_BWEIGHT;
@@ -185,31 +184,31 @@ TransModeInfo TransMode_edgecrease = {
     /*flags*/ T_NO_CONSTRAINT | T_NO_PROJECT,
     /*init_fn*/ initEgdeCrease,
     /*transform_fn*/ applyCrease,
-    /*transform_matrix_fn*/ NULL,
-    /*handle_event_fn*/ NULL,
-    /*snap_distance_fn*/ NULL,
-    /*snap_apply_fn*/ NULL,
-    /*draw_fn*/ NULL,
+    /*transform_matrix_fn*/ nullptr,
+    /*handle_event_fn*/ nullptr,
+    /*snap_distance_fn*/ nullptr,
+    /*snap_apply_fn*/ nullptr,
+    /*draw_fn*/ nullptr,
 };
 
 TransModeInfo TransMode_vertcrease = {
     /*flags*/ T_NO_CONSTRAINT | T_NO_PROJECT,
     /*init_fn*/ initVertCrease,
     /*transform_fn*/ applyCrease,
-    /*transform_matrix_fn*/ NULL,
-    /*handle_event_fn*/ NULL,
-    /*snap_distance_fn*/ NULL,
-    /*snap_apply_fn*/ NULL,
-    /*draw_fn*/ NULL,
+    /*transform_matrix_fn*/ nullptr,
+    /*handle_event_fn*/ nullptr,
+    /*snap_distance_fn*/ nullptr,
+    /*snap_apply_fn*/ nullptr,
+    /*draw_fn*/ nullptr,
 };
 
 TransModeInfo TransMode_bevelweight = {
     /*flags*/ T_NO_CONSTRAINT | T_NO_PROJECT,
     /*init_fn*/ initBevelWeight,
     /*transform_fn*/ applyBevelWeight,
-    /*transform_matrix_fn*/ NULL,
-    /*handle_event_fn*/ NULL,
-    /*snap_distance_fn*/ NULL,
-    /*snap_apply_fn*/ NULL,
-    /*draw_fn*/ NULL,
+    /*transform_matrix_fn*/ nullptr,
+    /*handle_event_fn*/ nullptr,
+    /*snap_distance_fn*/ nullptr,
+    /*snap_apply_fn*/ nullptr,
+    /*draw_fn*/ nullptr,
 };
