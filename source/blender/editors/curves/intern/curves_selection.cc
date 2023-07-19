971d23e257de1c357e8aa9f3c3b7961ef4b6bad7
Iliya Katueshenock <modormoder@gmail.com>
2023-07-19 23:43:44 +0300

    init

--- source/blender/editors/curves/intern/curves_selection.cc
+++ source/blender/editors/curves/intern/curves_selection.cc
@@ -276,34 +276,22 @@ void select_alternate(bke::CurvesGeometry &curves, const bool deselect_ends)
   const VArray<bool> cyclic = curves.cyclic();
 
   MutableSpan<bool> selection_typed = selection.span.typed<bool>();
-  threading::parallel_for(curves.curves_range(), 256, [&](const IndexRange range) {
+  threading::parallel_for(points_by_curve.index_range(), 256, [&](const IndexRange range) {
     for (const int curve_i : range) {
       const IndexRange points = points_by_curve[curve_i];
 
-      if (!has_anything_selected(selection.span.slice(points))) {
-        continue;
+      const int half_of_size = points.size() / 2;
+      const bool end_should_be_selected = !bool(points.size() % 2);
+      const IndexRange selected = points.shift(deselect_ends ? 1 : 0);
+      const IndexRange deselected = points.shift(deselect_ends ? 0 : 1);
+      for (const int index : IndexRange(half_of_size)) {
+        const int selected_index = selected[index * 2];
+        const int deselected_index = deselected[index * 2];
+        selection_typed[selected_index] = true;
+        selection_typed[deselected_index] = false;
       }
-
-      for (const int index : points.index_range()) {
-        selection_typed[points[index]] = deselect_ends ? index % 2 : !(index % 2);
-      }
-
-      if (cyclic[curve_i]) {
-        if (deselect_ends) {
-          selection_typed[points.last()] = false;
-        }
-        else {
-          selection_typed[points.last()] = true;
-          if (points.size() > 2) {
-            selection_typed[points.last() - 1] = false;
-          }
-        }
-      }
-      else {
-        if (deselect_ends) {
-          selection_typed[points.last()] = false;
-        }
-      }
+      selection_typed[points.first()] = !deselect_ends;
+      selection_typed[points.last()] = !deselect_ends && !end_should_be_selected;
     }
   });
 
