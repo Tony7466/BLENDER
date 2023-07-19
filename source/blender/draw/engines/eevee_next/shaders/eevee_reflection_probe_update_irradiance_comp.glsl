837027cd40c838cc04428bfaf3bde4b4db5e80b4
Jeroen Bakker <jeroen@blender.org>
2023-07-19 13:45:03 +0200

    Add support for metal

--- source/blender/draw/engines/eevee_next/shaders/eevee_reflection_probe_update_irradiance_comp.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_reflection_probe_update_irradiance_comp.glsl
@@ -19,22 +19,15 @@ void atlas_store(vec4 sh_coefficient, ivec2 atlas_coord, int layer)
   }
 }
 
-shared SphericalHarmonicL1 cooefs[gl_WorkGroupSize.x];
+shared vec4 cooefs[gl_WorkGroupSize.x][4];
 
 void main()
 {
-
-  /* Initialize workgroup result. */
-  if (gl_LocalInvocationID.x == 0) {
-    for (int i = 0; i < gl_WorkGroupSize.x; i++) {
-      cooefs[i].L0.M0 = vec4(0.0);
-      cooefs[i].L1.Mn1 = vec4(0.0);
-      cooefs[i].L1.M0 = vec4(0.0);
-      cooefs[i].L1.Mp1 = vec4(0.0);
-    }
-  }
-
-  barrier();
+  SphericalHarmonicL1 cooef;
+  cooef.L0.M0 = vec4(0.0);
+  cooef.L1.Mn1 = vec4(0.0);
+  cooef.L1.M0 = vec4(0.0);
+  cooef.L1.Mp1 = vec4(0.0);
 
   ReflectionProbeData probe_data = reflection_probe_buf[reflection_probe_index];
   const int subdivision_64 = 5;
@@ -51,29 +44,33 @@ void main()
     vec2 rand = fract(hammersley_2d(sample_index + sample_offset, total_samples));
     vec3 direction = sample_sphere(rand);
     vec4 light = reflection_probes_sample(direction, layer_mipmap, probe_data);
-    spherical_harmonics_encode_signal_sample(
-        direction, light * sample_weight, cooefs[store_index]);
+    spherical_harmonics_encode_signal_sample(direction, light * sample_weight, cooef);
   }
+  cooefs[store_index][0] = cooef.L0.M0;
+  cooefs[store_index][1] = cooef.L1.Mn1;
+  cooefs[store_index][2] = cooef.L1.M0;
+  cooefs[store_index][3] = cooef.L1.Mp1;
 
   barrier();
   if (gl_LocalInvocationID.x == 0) {
     /* Join results */
-    SphericalHarmonicL1 result;
-    result.L0.M0 = vec4(0.0);
-    result.L1.Mn1 = vec4(0.0);
-    result.L1.M0 = vec4(0.0);
-    result.L1.Mp1 = vec4(0.0);
-    for (int i = 0; i < gl_WorkGroupSize.x; i++) {
-      result.L0.M0 += cooefs[i].L0.M0;
-      result.L1.Mn1 += cooefs[i].L1.Mn1;
-      result.L1.M0 += cooefs[i].L1.M0;
-      result.L1.Mp1 += cooefs[i].L1.Mp1;
+    vec4 result[4];
+    result[0] = vec4(0.0);
+    result[1] = vec4(0.0);
+    result[2] = vec4(0.0);
+    result[3] = vec4(0.0);
+
+    for (uint i = 0; i < gl_WorkGroupSize.x; i++) {
+      result[0] += cooefs[i][0];
+      result[1] += cooefs[i][1];
+      result[2] += cooefs[i][2];
+      result[3] += cooefs[i][3];
     }
 
     ivec2 atlas_coord = ivec2(0, 0);
-    atlas_store(result.L0.M0, atlas_coord, 0);
-    atlas_store(result.L1.Mn1, atlas_coord, 1);
-    atlas_store(result.L1.M0, atlas_coord, 2);
-    atlas_store(result.L1.Mp1, atlas_coord, 3);
+    atlas_store(result[0], atlas_coord, 0);
+    atlas_store(result[1], atlas_coord, 1);
+    atlas_store(result[2], atlas_coord, 2);
+    atlas_store(result[3], atlas_coord, 3);
   }
 }
