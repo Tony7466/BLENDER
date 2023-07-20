c61ff2491285e9be54fa595d0169b1f6e3d09231
Jacques Lucke <jacques@blender.org>
2023-07-20 12:50:12 +0200

    move bpy_app_icons.c

--- source/blender/python/intern/bpy_app_icons.cc
+++ source/blender/python/intern/bpy_app_icons.cc
@@ -35,13 +35,13 @@ PyDoc_STRVAR(
     "   :type colors: byte sequence.\n"
     "   :return: Unique icon value (pass to interface ``icon_value`` argument).\n"
     "   :rtype: int\n");
-static PyObject *bpy_app_icons_new_triangles(PyObject *UNUSED(self), PyObject *args, PyObject *kw)
+static PyObject *bpy_app_icons_new_triangles(PyObject * /*self*/, PyObject *args, PyObject *kw)
 {
   /* bytes */
   uchar coords_range[2];
   PyObject *py_coords, *py_colors;
 
-  static const char *_keywords[] = {"range", "coords", "colors", NULL};
+  static const char *_keywords[] = {"range", "coords", "colors", nullptr};
   static _PyArg_Parser _parser = {
       "(BB)" /* `range` */
       "S"    /* `coords` */
@@ -53,29 +53,29 @@ static PyObject *bpy_app_icons_new_triangles(PyObject *UNUSED(self), PyObject *a
   if (!_PyArg_ParseTupleAndKeywordsFast(
           args, kw, &_parser, &coords_range[0], &coords_range[1], &py_coords, &py_colors))
   {
-    return NULL;
+    return nullptr;
   }
 
   const int coords_len = PyBytes_GET_SIZE(py_coords);
   const int tris_len = coords_len / 6;
   if (tris_len * 6 != coords_len) {
     PyErr_SetString(PyExc_ValueError, "coords must be multiple of 6");
-    return NULL;
+    return nullptr;
   }
   if (PyBytes_GET_SIZE(py_colors) != 2 * coords_len) {
     PyErr_SetString(PyExc_ValueError, "colors must be twice size of coords");
-    return NULL;
+    return nullptr;
   }
 
   const int coords_size = sizeof(uchar[2]) * tris_len * 3;
   const int colors_size = sizeof(uchar[4]) * tris_len * 3;
-  uchar(*coords)[2] = MEM_mallocN(coords_size, __func__);
-  uchar(*colors)[4] = MEM_mallocN(colors_size, __func__);
+  uchar(*coords)[2] = static_cast<uchar(*)[2]>(MEM_mallocN(coords_size, __func__));
+  uchar(*colors)[4] = static_cast<uchar(*)[4]>(MEM_mallocN(colors_size, __func__));
 
   memcpy(coords, PyBytes_AS_STRING(py_coords), coords_size);
   memcpy(colors, PyBytes_AS_STRING(py_colors), colors_size);
 
-  struct Icon_Geom *geom = MEM_mallocN(sizeof(*geom), __func__);
+  struct Icon_Geom *geom = static_cast<Icon_Geom *>(MEM_mallocN(sizeof(*geom), __func__));
   geom->coords_len = tris_len;
   geom->coords_range[0] = coords_range[0];
   geom->coords_range[1] = coords_range[1];
@@ -95,14 +95,14 @@ PyDoc_STRVAR(bpy_app_icons_new_triangles_from_file_doc,
              "   :type filepath: string.\n"
              "   :return: Unique icon value (pass to interface ``icon_value`` argument).\n"
              "   :rtype: int\n");
-static PyObject *bpy_app_icons_new_triangles_from_file(PyObject *UNUSED(self),
+static PyObject *bpy_app_icons_new_triangles_from_file(PyObject * /*self*/,
                                                        PyObject *args,
                                                        PyObject *kw)
 {
   /* bytes */
   char *filepath;
 
-  static const char *_keywords[] = {"filepath", NULL};
+  static const char *_keywords[] = {"filepath", nullptr};
   static _PyArg_Parser _parser = {
       "s" /* `filepath` */
       ":new_triangles_from_file",
@@ -110,13 +110,13 @@ static PyObject *bpy_app_icons_new_triangles_from_file(PyObject *UNUSED(self),
       0,
   };
   if (!_PyArg_ParseTupleAndKeywordsFast(args, kw, &_parser, &filepath)) {
-    return NULL;
+    return nullptr;
   }
 
   struct Icon_Geom *geom = BKE_icon_geom_from_file(filepath);
-  if (geom == NULL) {
+  if (geom == nullptr) {
     PyErr_SetString(PyExc_ValueError, "Unable to load from file");
-    return NULL;
+    return nullptr;
   }
   const int icon_id = BKE_icon_geom_ensure(geom);
   return PyLong_FromLong(icon_id);
@@ -126,10 +126,10 @@ PyDoc_STRVAR(bpy_app_icons_release_doc,
              ".. function:: release(icon_id)\n"
              "\n"
              "   Release the icon.\n");
-static PyObject *bpy_app_icons_release(PyObject *UNUSED(self), PyObject *args, PyObject *kw)
+static PyObject *bpy_app_icons_release(PyObject * /*self*/, PyObject *args, PyObject *kw)
 {
   int icon_id;
-  static const char *_keywords[] = {"icon_id", NULL};
+  static const char *_keywords[] = {"icon_id", nullptr};
   static _PyArg_Parser _parser = {
       "i" /* `icon_id` */
       ":release",
@@ -137,12 +137,12 @@ static PyObject *bpy_app_icons_release(PyObject *UNUSED(self), PyObject *args, P
       0,
   };
   if (!_PyArg_ParseTupleAndKeywordsFast(args, kw, &_parser, &icon_id)) {
-    return NULL;
+    return nullptr;
   }
 
   if (!BKE_icon_delete_unmanaged(icon_id)) {
     PyErr_SetString(PyExc_ValueError, "invalid icon_id");
-    return NULL;
+    return nullptr;
   }
   Py_RETURN_NONE;
 }
@@ -160,19 +160,19 @@ static PyMethodDef M_AppIcons_methods[] = {
      (PyCFunction)bpy_app_icons_release,
      METH_VARARGS | METH_KEYWORDS,
      bpy_app_icons_release_doc},
-    {NULL, NULL, 0, NULL},
+    {nullptr, nullptr, 0, nullptr},
 };
 
 static PyModuleDef M_AppIcons_module_def = {
     /*m_base*/ PyModuleDef_HEAD_INIT,
     /*m_name*/ "bpy.app.icons",
-    /*m_doc*/ NULL,
+    /*m_doc*/ nullptr,
     /*m_size*/ 0,
     /*m_methods*/ M_AppIcons_methods,
-    /*m_slots*/ NULL,
-    /*m_traverse*/ NULL,
-    /*m_clear*/ NULL,
-    /*m_free*/ NULL,
+    /*m_slots*/ nullptr,
+    /*m_traverse*/ nullptr,
+    /*m_clear*/ nullptr,
+    /*m_free*/ nullptr,
 };
 
 PyObject *BPY_app_icons_module(void)
