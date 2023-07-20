ae371078f576c7aee8b62a24ab260300640577f3
Jacques Lucke <jacques@blender.org>
2023-07-20 13:20:18 +0200

    move bpy_operator.c

--- source/blender/python/intern/bpy_operator.cc
+++ source/blender/python/intern/bpy_operator.cc
@@ -49,24 +49,24 @@
 static wmOperatorType *ot_lookup_from_py_string(PyObject *value, const char *py_fn_id)
 {
   const char *opname = PyUnicode_AsUTF8(value);
-  if (opname == NULL) {
+  if (opname == nullptr) {
     PyErr_Format(PyExc_TypeError, "%s() expects a string argument", py_fn_id);
-    return NULL;
+    return nullptr;
   }
 
   wmOperatorType *ot = WM_operatortype_find(opname, true);
-  if (ot == NULL) {
+  if (ot == nullptr) {
     PyErr_Format(PyExc_KeyError, "%s(\"%s\") not found", py_fn_id, opname);
-    return NULL;
+    return nullptr;
   }
   return ot;
 }
 
-static PyObject *pyop_poll(PyObject *UNUSED(self), PyObject *args)
+static PyObject *pyop_poll(PyObject * /*self*/, PyObject *args)
 {
   wmOperatorType *ot;
   const char *opname;
-  const char *context_str = NULL;
+  const char *context_str = nullptr;
   PyObject *ret;
 
   wmOperatorCallContext context = WM_OP_EXEC_DEFAULT;
@@ -75,13 +75,13 @@ static PyObject *pyop_poll(PyObject *UNUSED(self), PyObject *args)
    * could make a tuple from self and pack the name and Context into it. */
   bContext *C = BPY_context_get();
 
-  if (C == NULL) {
+  if (C == nullptr) {
     PyErr_SetString(PyExc_RuntimeError, "Context is None, can't poll any operators");
-    return NULL;
+    return nullptr;
   }
 
   /* All arguments are positional. */
-  static const char *_keywords[] = {"", "", NULL};
+  static const char *_keywords[] = {"", "", nullptr};
   static _PyArg_Parser _parser = {
       "s" /* `opname` */
       "|" /* Optional arguments. */
@@ -90,18 +90,18 @@ static PyObject *pyop_poll(PyObject *UNUSED(self), PyObject *args)
       _keywords,
       0,
   };
-  if (!_PyArg_ParseTupleAndKeywordsFast(args, NULL, &_parser, &opname, &context_str)) {
-    return NULL;
+  if (!_PyArg_ParseTupleAndKeywordsFast(args, nullptr, &_parser, &opname, &context_str)) {
+    return nullptr;
   }
 
   ot = WM_operatortype_find(opname, true);
 
-  if (ot == NULL) {
+  if (ot == nullptr) {
     PyErr_Format(PyExc_AttributeError,
                  "Polling operator \"bpy.ops.%s\" error, "
                  "could not be found",
                  opname);
-    return NULL;
+    return nullptr;
   }
 
   if (context_str) {
@@ -115,10 +115,10 @@ static PyObject *pyop_poll(PyObject *UNUSED(self), PyObject *args)
                    opname,
                    enum_str);
       MEM_freeN(enum_str);
-      return NULL;
+      return nullptr;
     }
     /* Copy back to the properly typed enum. */
-    context = context_int;
+    context = wmOperatorCallContext(context_int);
   }
 
   /* main purpose of this function */
@@ -127,7 +127,7 @@ static PyObject *pyop_poll(PyObject *UNUSED(self), PyObject *args)
   return Py_INCREF_RET(ret);
 }
 
-static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
+static PyObject *pyop_call(PyObject * /*self*/, PyObject *args)
 {
   wmOperatorType *ot;
   int error_val = 0;
@@ -135,8 +135,8 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
   int operator_ret = OPERATOR_CANCELLED;
 
   const char *opname;
-  const char *context_str = NULL;
-  PyObject *kw = NULL; /* optional args */
+  const char *context_str = nullptr;
+  PyObject *kw = nullptr; /* optional args */
 
   wmOperatorCallContext context = WM_OP_EXEC_DEFAULT;
   int is_undo = false;
@@ -145,13 +145,13 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
    * could make a tuple from self and pack the name and Context into it. */
   bContext *C = BPY_context_get();
 
-  if (C == NULL) {
+  if (C == nullptr) {
     PyErr_SetString(PyExc_RuntimeError, "Context is None, can't poll any operators");
-    return NULL;
+    return nullptr;
   }
 
   /* All arguments are positional. */
-  static const char *_keywords[] = {"", "", "", "", NULL};
+  static const char *_keywords[] = {"", "", "", "", nullptr};
   static _PyArg_Parser _parser = {
       "s"  /* `opname` */
       "|"  /* Optional arguments. */
@@ -163,19 +163,19 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
       0,
   };
   if (!_PyArg_ParseTupleAndKeywordsFast(
-          args, NULL, &_parser, &opname, &PyDict_Type, &kw, &context_str, &is_undo))
+          args, nullptr, &_parser, &opname, &PyDict_Type, &kw, &context_str, &is_undo))
   {
-    return NULL;
+    return nullptr;
   }
 
   ot = WM_operatortype_find(opname, true);
 
-  if (ot == NULL) {
+  if (ot == nullptr) {
     PyErr_Format(PyExc_AttributeError,
                  "Calling operator \"bpy.ops.%s\" error, "
                  "could not be found",
                  opname);
-    return NULL;
+    return nullptr;
   }
 
   if (!pyrna_write_check()) {
@@ -183,7 +183,7 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
                  "Calling operator \"bpy.ops.%s\" error, "
                  "can't modify blend data in this state (drawing/rendering)",
                  opname);
-    return NULL;
+    return nullptr;
   }
 
   if (context_str) {
@@ -197,10 +197,10 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
                    opname,
                    enum_str);
       MEM_freeN(enum_str);
-      return NULL;
+      return nullptr;
     }
     /* Copy back to the properly typed enum. */
-    context = context_int;
+    context = wmOperatorCallContext(context_int);
   }
 
   if (WM_operator_poll_context((bContext *)C, ot, context) == false) {
@@ -228,7 +228,7 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
     if (error_val == 0) {
       ReportList *reports;
 
-      reports = MEM_mallocN(sizeof(ReportList), "wmOperatorReportList");
+      reports = static_cast<ReportList *>(MEM_mallocN(sizeof(ReportList), "wmOperatorReportList"));
 
       /* Own so these don't move into global reports. */
       BKE_reports_init(reports, RPT_STORE | RPT_OP_HOLD | RPT_PRINT_HANDLED_BY_OWNER);
@@ -254,7 +254,7 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
 
       /* operator output is nice to have in the terminal/console too */
       if (!BLI_listbase_is_empty(&reports->list)) {
-        BPy_reports_write_stdout(reports, NULL);
+        BPy_reports_write_stdout(reports, nullptr);
       }
 
       BKE_reports_clear(reports);
@@ -274,18 +274,18 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
     /* if there is some way to know an operator takes args we should use this */
     {
       /* no props */
-      if (kw != NULL) {
+      if (kw != nullptr) {
         PyErr_Format(PyExc_AttributeError, "Operator \"%s\" does not take any args", opname);
-        return NULL;
+        return nullptr;
       }
 
-      WM_operator_name_call(C, opname, WM_OP_EXEC_DEFAULT, NULL, NULL);
+      WM_operator_name_call(C, opname, WM_OP_EXEC_DEFAULT, nullptr, nullptr);
     }
 #endif
   }
 
   if (error_val == -1) {
-    return NULL;
+    return nullptr;
   }
 
   /* When calling `bpy.ops.wm.read_factory_settings()` `bpy.data's` main pointer
@@ -298,30 +298,30 @@ static PyObject *pyop_call(PyObject *UNUSED(self), PyObject *args)
   return pyrna_enum_bitfield_as_set(rna_enum_operator_return_items, operator_ret);
 }
 
-static PyObject *pyop_as_string(PyObject *UNUSED(self), PyObject *args)
+static PyObject *pyop_as_string(PyObject * /*self*/, PyObject *args)
 {
   wmOperatorType *ot;
   PointerRNA ptr;
 
   const char *opname;
-  PyObject *kw = NULL; /* optional args */
+  PyObject *kw = nullptr; /* optional args */
   bool all_args = true;
   bool macro_args = true;
   int error_val = 0;
 
-  char *buf = NULL;
+  char *buf = nullptr;
   PyObject *pybuf;
 
   bContext *C = BPY_context_get();
 
-  if (C == NULL) {
+  if (C == nullptr) {
     PyErr_SetString(PyExc_RuntimeError,
                     "Context is None, can't get the string representation of this object.");
-    return NULL;
+    return nullptr;
   }
 
   /* All arguments are positional. */
-  static const char *_keywords[] = {"", "", "", "", NULL};
+  static const char *_keywords[] = {"", "", "", "", nullptr};
   static _PyArg_Parser _parser = {
       "s"  /* `opname` */
       "|"  /* Optional arguments. */
@@ -333,7 +333,7 @@ static PyObject *pyop_as_string(PyObject *UNUSED(self), PyObject *args)
       0,
   };
   if (!_PyArg_ParseTupleAndKeywordsFast(args,
-                                        NULL,
+                                        nullptr,
                                         &_parser,
                                         &opname,
                                         &PyDict_Type,
@@ -343,22 +343,22 @@ static PyObject *pyop_as_string(PyObject *UNUSED(self), PyObject *args)
                                         PyC_ParseBool,
                                         &macro_args))
   {
-    return NULL;
+    return nullptr;
   }
 
   ot = WM_operatortype_find(opname, true);
 
-  if (ot == NULL) {
+  if (ot == nullptr) {
     PyErr_Format(PyExc_AttributeError,
                  "_bpy.ops.as_string: operator \"%.200s\" "
                  "could not be found",
                  opname);
-    return NULL;
+    return nullptr;
   }
 
   // WM_operator_properties_create(&ptr, opname);
   /* Save another lookup */
-  RNA_pointer_create(NULL, ot->srna, NULL, &ptr);
+  RNA_pointer_create(nullptr, ot->srna, nullptr, &ptr);
 
   if (kw && PyDict_Size(kw)) {
     error_val = pyrna_pydict_to_props(
@@ -366,13 +366,13 @@ static PyObject *pyop_as_string(PyObject *UNUSED(self), PyObject *args)
   }
 
   if (error_val == 0) {
-    buf = WM_operator_pystring_ex(C, NULL, all_args, macro_args, ot, &ptr);
+    buf = WM_operator_pystring_ex(C, nullptr, all_args, macro_args, ot, &ptr);
   }
 
   WM_operator_properties_free(&ptr);
 
   if (error_val == -1) {
-    return NULL;
+    return nullptr;
   }
 
   if (buf) {
@@ -386,7 +386,7 @@ static PyObject *pyop_as_string(PyObject *UNUSED(self), PyObject *args)
   return pybuf;
 }
 
-static PyObject *pyop_dir(PyObject *UNUSED(self))
+static PyObject *pyop_dir(PyObject * /*self*/)
 {
   GHashIterator iter;
   PyObject *list;
@@ -396,56 +396,56 @@ static PyObject *pyop_dir(PyObject *UNUSED(self))
   list = PyList_New(BLI_ghash_len(iter.gh));
 
   for (i = 0; !BLI_ghashIterator_done(&iter); BLI_ghashIterator_step(&iter), i++) {
-    wmOperatorType *ot = BLI_ghashIterator_getValue(&iter);
+    wmOperatorType *ot = static_cast<wmOperatorType *>(BLI_ghashIterator_getValue(&iter));
     PyList_SET_ITEM(list, i, PyUnicode_FromString(ot->idname));
   }
 
   return list;
 }
 
-static PyObject *pyop_getrna_type(PyObject *UNUSED(self), PyObject *value)
+static PyObject *pyop_getrna_type(PyObject * /*self*/, PyObject *value)
 {
   wmOperatorType *ot;
-  if ((ot = ot_lookup_from_py_string(value, "get_rna_type")) == NULL) {
-    return NULL;
+  if ((ot = ot_lookup_from_py_string(value, "get_rna_type")) == nullptr) {
+    return nullptr;
   }
 
   PointerRNA ptr;
-  RNA_pointer_create(NULL, &RNA_Struct, ot->srna, &ptr);
+  RNA_pointer_create(nullptr, &RNA_Struct, ot->srna, &ptr);
   BPy_StructRNA *pyrna = (BPy_StructRNA *)pyrna_struct_CreatePyObject(&ptr);
   return (PyObject *)pyrna;
 }
 
-static PyObject *pyop_get_bl_options(PyObject *UNUSED(self), PyObject *value)
+static PyObject *pyop_get_bl_options(PyObject * /*self*/, PyObject *value)
 {
   wmOperatorType *ot;
-  if ((ot = ot_lookup_from_py_string(value, "get_bl_options")) == NULL) {
-    return NULL;
+  if ((ot = ot_lookup_from_py_string(value, "get_bl_options")) == nullptr) {
+    return nullptr;
   }
   return pyrna_enum_bitfield_as_set(rna_enum_operator_type_flag_items, ot->flag);
 }
 
 static PyMethodDef bpy_ops_methods[] = {
-    {"poll", (PyCFunction)pyop_poll, METH_VARARGS, NULL},
-    {"call", (PyCFunction)pyop_call, METH_VARARGS, NULL},
-    {"as_string", (PyCFunction)pyop_as_string, METH_VARARGS, NULL},
-    {"dir", (PyCFunction)pyop_dir, METH_NOARGS, NULL},
-    {"get_rna_type", (PyCFunction)pyop_getrna_type, METH_O, NULL},
-    {"get_bl_options", (PyCFunction)pyop_get_bl_options, METH_O, NULL},
-    {"macro_define", (PyCFunction)PYOP_wrap_macro_define, METH_VARARGS, NULL},
-    {NULL, NULL, 0, NULL},
+    {"poll", (PyCFunction)pyop_poll, METH_VARARGS, nullptr},
+    {"call", (PyCFunction)pyop_call, METH_VARARGS, nullptr},
+    {"as_string", (PyCFunction)pyop_as_string, METH_VARARGS, nullptr},
+    {"dir", (PyCFunction)pyop_dir, METH_NOARGS, nullptr},
+    {"get_rna_type", (PyCFunction)pyop_getrna_type, METH_O, nullptr},
+    {"get_bl_options", (PyCFunction)pyop_get_bl_options, METH_O, nullptr},
+    {"macro_define", (PyCFunction)PYOP_wrap_macro_define, METH_VARARGS, nullptr},
+    {nullptr, nullptr, 0, nullptr},
 };
 
 static PyModuleDef bpy_ops_module = {
     /*m_base*/ PyModuleDef_HEAD_INIT,
     /*m_name*/ "_bpy.ops",
-    /*m_doc*/ NULL,
+    /*m_doc*/ nullptr,
     /*m_size*/ -1, /* multiple "initialization" just copies the module dict. */
     /*m_methods*/ bpy_ops_methods,
-    /*m_slots*/ NULL,
-    /*m_traverse*/ NULL,
-    /*m_clear*/ NULL,
-    /*m_free*/ NULL,
+    /*m_slots*/ nullptr,
+    /*m_traverse*/ nullptr,
+    /*m_clear*/ nullptr,
+    /*m_free*/ nullptr,
 };
 
 PyObject *BPY_operator_module(void)
