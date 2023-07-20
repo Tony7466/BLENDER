027dd5eb56ad09ecbbdaf17777bad05f85d36c2f
Amelie <amelie.fondevilla@les-fees-speciales.coop>
2023-07-20 14:23:52 +0200

    Refactor opacity copy (early return)

--- source/blender/editors/sculpt_paint/grease_pencil_erase.cc
+++ source/blender/editors/sculpt_paint/grease_pencil_erase.cc
@@ -743,12 +743,7 @@ struct EraseOperationExecutor {
       /* Write the opacity attribute*/
       SpanAttributeWriter<float> dst_opacity = dst_attributes.lookup_or_add_for_write_span<float>(
           opacity_attr, ATTR_DOMAIN_POINT);
-
-      threading::parallel_for(dst.points_range(), 256, [&](const IndexRange dst_points) {
-        for (const int dst_point : dst_points) {
-          dst_opacity.span[dst_point] = src_new_opacity[dst_point];
-        }
-      });
+      array_utils::gather(src_new_opacity.as_span(), dst_to_src_point.as_span(), dst_opacity.span);
       dst_opacity.finish();
 
       /* Note : the opacities were changed, so we still need to tag for changes. */
