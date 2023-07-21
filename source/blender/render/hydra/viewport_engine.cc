a3fcd24dd02149cae0a554bbd948141acd7535f7
Brecht Van Lommel <brecht@blender.org>
2023-07-21 18:53:27 +0200

    Cleanup: use SPDX for copyright

--- source/blender/render/hydra/viewport_engine.cc
+++ source/blender/render/hydra/viewport_engine.cc
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later
- * Copyright 2011-2022 Blender Foundation */
+ * SPDX-FileCopyrightText: 2011-2022 Blender Foundation */
 
 #include "viewport_engine.h"
 
