7622c47df6402f02e94bb717998a36848ed7b2d4
Falk David <falk@blender.org>
2023-07-21 12:31:24 +0200

    Update header

--- source/blender/editors/include/ED_grease_pencil.h
+++ source/blender/editors/include/ED_grease_pencil.h
@@ -72,7 +72,11 @@ void gaussian_blur_1D(const GSpan src,
                       bool is_cyclic,
                       GMutableSpan dst);
 
-void ramer_douglas_peucker_simplify(const Span<float3> src, float epsilon, MutableSpan<bool> dst);
+int64_t ramer_douglas_peucker_simplify(
+    const IndexRange range,
+    float epsilon,
+    const blender::FunctionRef<float(IndexRange, int64_t)> dist_function,
+    MutableSpan<bool> dst);
 
 }  // namespace blender::ed::greasepencil
 #endif
