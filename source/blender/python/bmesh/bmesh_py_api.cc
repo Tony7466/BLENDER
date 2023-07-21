b27ea530c07cf20563dc3b543300262e206d64cf
Jacques Lucke <jacques@blender.org>
2023-07-21 18:22:23 +0200

    move bmesh_py_api

--- source/blender/python/bmesh/bmesh_py_api.cc
+++ source/blender/python/bmesh/bmesh_py_api.cc
@@ -40,9 +40,9 @@ PyDoc_STRVAR(bpy_bm_new_doc,
              "   :return: Return a new, empty BMesh.\n"
              "   :rtype: :class:`bmesh.types.BMesh`\n");
 
-static PyObject *bpy_bm_new(PyObject *UNUSED(self), PyObject *args, PyObject *kw)
+static PyObject *bpy_bm_new(PyObject * /*self*/, PyObject *args, PyObject *kw)
 {
-  static const char *kwlist[] = {"use_operators", NULL};
+  static const char *kwlist[] = {"use_operators", nullptr};
   BMesh *bm;
 
   bool use_operators = true;
@@ -50,13 +50,12 @@ static PyObject *bpy_bm_new(PyObject *UNUSED(self), PyObject *args, PyObject *kw
   if (!PyArg_ParseTupleAndKeywords(
           args, kw, "|$O&:new", (char **)kwlist, PyC_ParseBool, &use_operators))
   {
-    return NULL;
+    return nullptr;
   }
 
-  bm = BM_mesh_create(&bm_mesh_allocsize_default,
-                      &((struct BMeshCreateParams){
-                          .use_toolflags = use_operators,
-                      }));
+  BMeshCreateParams params{};
+  params.use_toolflags = use_operators;
+  bm = BM_mesh_create(&bm_mesh_allocsize_default, &params);
 
   return BPy_BMesh_CreatePyObject(bm, BPY_BMFLAG_NOP);
 }
@@ -70,24 +69,26 @@ PyDoc_STRVAR(bpy_bm_from_edit_mesh_doc,
              "   :type mesh: :class:`bpy.types.Mesh`\n"
              "   :return: the BMesh associated with this mesh.\n"
              "   :rtype: :class:`bmesh.types.BMesh`\n");
-static PyObject *bpy_bm_from_edit_mesh(PyObject *UNUSED(self), PyObject *value)
+static PyObject *bpy_bm_from_edit_mesh(PyObject * /*self*/, PyObject *value)
 {
   BMesh *bm;
-  Mesh *me = PyC_RNA_AsPointer(value, "Mesh");
+  Mesh *me = static_cast<Mesh *>(PyC_RNA_AsPointer(value, "Mesh"));
 
-  if (me == NULL) {
-    return NULL;
+  if (me == nullptr) {
+    return nullptr;
   }
 
-  if (me->edit_mesh == NULL) {
+  if (me->edit_mesh == nullptr) {
     PyErr_SetString(PyExc_ValueError, "The mesh must be in editmode");
-    return NULL;
+    return nullptr;
   }
 
   bm = me->edit_mesh->bm;
 
   return BPy_BMesh_CreatePyObject(bm, BPY_BMFLAG_IS_WRAPPED);
 }
+
+extern "C" void EDBM_update_extern(Mesh *me, const bool do_tessface, const bool is_destructive);
 
 PyDoc_STRVAR(bpy_bm_update_edit_mesh_doc,
              ".. method:: update_edit_mesh(mesh, loop_triangles=True, destructive=True)\n"
@@ -101,9 +102,9 @@ PyDoc_STRVAR(bpy_bm_update_edit_mesh_doc,
              "   :type loop_triangles: boolean\n"
              "   :arg destructive: Use when geometry has been added or removed.\n"
              "   :type destructive: boolean\n");
-static PyObject *bpy_bm_update_edit_mesh(PyObject *UNUSED(self), PyObject *args, PyObject *kw)
+static PyObject *bpy_bm_update_edit_mesh(PyObject * /*self*/, PyObject *args, PyObject *kw)
 {
-  static const char *kwlist[] = {"mesh", "loop_triangles", "destructive", NULL};
+  static const char *kwlist[] = {"mesh", "loop_triangles", "destructive", nullptr};
   PyObject *py_me;
   Mesh *me;
   bool do_loop_triangles = true;
@@ -119,23 +120,21 @@ static PyObject *bpy_bm_update_edit_mesh(PyObject *UNUSED(self), PyObject *args,
                                    PyC_ParseBool,
                                    &is_destructive))
   {
-    return NULL;
+    return nullptr;
   }
 
-  me = PyC_RNA_AsPointer(py_me, "Mesh");
+  me = static_cast<Mesh *>(PyC_RNA_AsPointer(py_me, "Mesh"));
 
-  if (me == NULL) {
-    return NULL;
+  if (me == nullptr) {
+    return nullptr;
   }
 
-  if (me->edit_mesh == NULL) {
+  if (me->edit_mesh == nullptr) {
     PyErr_SetString(PyExc_ValueError, "The mesh must be in editmode");
-    return NULL;
+    return nullptr;
   }
 
   {
-    extern void EDBM_update_extern(Mesh * me, const bool do_tessface, const bool is_destructive);
-
     EDBM_update_extern(me, do_loop_triangles, is_destructive);
   }
 
@@ -149,7 +148,7 @@ static PyMethodDef BPy_BM_methods[] = {
      (PyCFunction)bpy_bm_update_edit_mesh,
      METH_VARARGS | METH_KEYWORDS,
      bpy_bm_update_edit_mesh_doc},
-    {NULL, NULL, 0, NULL},
+    {nullptr, nullptr, 0, nullptr},
 };
 
 PyDoc_STRVAR(BPy_BM_doc,
@@ -162,10 +161,10 @@ static PyModuleDef BPy_BM_module_def = {
     /*m_doc*/ BPy_BM_doc,
     /*m_size*/ 0,
     /*m_methods*/ BPy_BM_methods,
-    /*m_slots*/ NULL,
-    /*m_traverse*/ NULL,
-    /*m_clear*/ NULL,
-    /*m_free*/ NULL,
+    /*m_slots*/ nullptr,
+    /*m_traverse*/ nullptr,
+    /*m_clear*/ nullptr,
+    /*m_free*/ nullptr,
 };
 
 PyObject *BPyInit_bmesh(void)
