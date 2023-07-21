3e70b0d24c0f301f7230a3dc839e0681d3c2a6c1
Jacques Lucke <jacques@blender.org>
2023-07-21 18:06:31 +0200

    extract methods for converting between socket values and bake items

--- source/blender/blenkernel/BKE_bake_items_socket.hh
+++ source/blender/blenkernel/BKE_bake_items_socket.hh
@@ -4,25 +4,36 @@
 
 #pragma once
 
+#include "BLI_function_ref.hh"
+
 #include "DNA_node_types.h"
 
 #include "BKE_bake_items.hh"
+#include "BKE_geometry_fields.hh"
 
 namespace blender::bke {
 
-Vector<std::unique_ptr<BakeItem>> move_socket_values_to_bake_items(
-    Span<void *> socket_values,
-    Span<eNodeSocketDatatype> socket_types,
-    Span<Vector<int>> geometries_by_attribute);
+struct BakeSocketConfig {
+  Vector<eNodeSocketDatatype> types;
+  Vector<eAttrDomain> domains;
+  Vector<Vector<int, 1>> geometries_by_attribute;
+};
 
-void move_bake_items_to_socket_values(Span<BakeItem *> bake_items,
-                                      Span<eNodeSocketDatatype> socket_types,
-                                      Span<Vector<int>> geometries_by_attribute,
-                                      Span<void *> r_socket_values);
+Vector<std::unique_ptr<BakeItem>> move_socket_values_to_bake_items(Span<void *> socket_values,
+                                                                   const BakeSocketConfig &config);
 
-void copy_bake_items_to_socket_values(Span<const BakeItem *> bake_items,
-                                      Span<eNodeSocketDatatype> socket_types,
-                                      Span<Vector<int>> geometries_by_attribute,
-                                      Span<void *> r_socket_values);
+void move_bake_items_to_socket_values(
+    Span<BakeItem *> bake_items,
+    const BakeSocketConfig &config,
+    FunctionRef<std::shared_ptr<AnonymousAttributeFieldInput>(int, const CPPType &)>
+        make_attribute_field,
+    Span<void *> r_socket_values);
+
+void copy_bake_items_to_socket_values(
+    Span<const BakeItem *> bake_items,
+    const BakeSocketConfig &config,
+    FunctionRef<std::shared_ptr<AnonymousAttributeFieldInput>(int, const CPPType &)>
+        make_attribute_field,
+    Span<void *> r_socket_values);
 
 }  // namespace blender::bke
