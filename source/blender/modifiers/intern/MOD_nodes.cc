d2191d5d00cdbeff809c13637bf06813c04d33f7
André Dal Bosco <andredbsc@gmail.com>
2023-07-25 00:36:21 -0300

    fix crash when there's no active camera

--- source/blender/modifiers/intern/MOD_nodes.cc
+++ source/blender/modifiers/intern/MOD_nodes.cc
@@ -312,7 +312,7 @@ static void update_depsgraph(ModifierData *md, const ModifierUpdateDepsgraphCont
   if (needs_own_transform_relation) {
     DEG_add_depends_on_transform_relation(ctx->node, "Nodes Modifier");
   }
-  if (needs_camera_transform_relation) {
+  if (needs_camera_transform_relation && ctx->scene->camera != nullptr) {
   add_object_relation(ctx, *ctx->scene->camera);
   }
 }
