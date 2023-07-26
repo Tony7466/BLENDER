9e6921335d7cf2e2b228f29d63b059626f70c8a0
Jacques Lucke <jacques@blender.org>
2023-07-26 12:46:27 +0200

    move MOD_gpencil_legacy_length.c

--- source/blender/gpencil_modifiers_legacy/intern/MOD_gpencil_legacy_length.cc
+++ source/blender/gpencil_modifiers_legacy/intern/MOD_gpencil_legacy_length.cc
@@ -62,7 +62,7 @@ static void copyData(const GpencilModifierData *md, GpencilModifierData *target)
 
 static float *noise_table(int len, int offset, int seed)
 {
-  float *table = MEM_callocN(sizeof(float) * len, __func__);
+  float *table = static_cast<float *>(MEM_callocN(sizeof(float) * len, __func__));
   for (int i = 0; i < len; i++) {
     table[i] = BLI_hash_int_01(BLI_hash_int_2d(seed, i + offset + 1));
   }
@@ -213,7 +213,7 @@ static void deformStroke(GpencilModifierData *md,
                          bGPDframe *gpf,
                          bGPDstroke *gps)
 {
-  bGPdata *gpd = ob->data;
+  bGPdata *gpd = static_cast<bGPdata *>(ob->data);
   LengthGpencilModifierData *lmd = (LengthGpencilModifierData *)md;
   if (!is_stroke_affected_by_modifier(ob,
                                       lmd->layername,
@@ -237,7 +237,7 @@ static void deformStroke(GpencilModifierData *md,
   applyLength(md, depsgraph, gpd, gpf, gps, ob);
 }
 
-static void bakeModifier(Main *UNUSED(bmain),
+static void bakeModifier(Main * /*bmain*/,
                          Depsgraph *depsgraph,
                          GpencilModifierData *md,
                          Object *ob)
@@ -252,47 +252,47 @@ static void foreachIDLink(GpencilModifierData *md, Object *ob, IDWalkFunc walk, 
   walk(userData, ob, (ID **)&mmd->material, IDWALK_CB_USER);
 }
 
-static void random_header_draw(const bContext *UNUSED(C), Panel *panel)
+static void random_header_draw(const bContext * /*C*/, Panel *panel)
 {
   uiLayout *layout = panel->layout;
 
-  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, NULL);
+  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, nullptr);
 
   uiItemR(layout, ptr, "use_random", 0, IFACE_("Randomize"), ICON_NONE);
 }
 
-static void random_panel_draw(const bContext *UNUSED(C), Panel *panel)
+static void random_panel_draw(const bContext * /*C*/, Panel *panel)
 {
   uiLayout *layout = panel->layout;
 
-  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, NULL);
+  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, nullptr);
 
   uiLayoutSetPropSep(layout, true);
 
   uiLayoutSetActive(layout, RNA_boolean_get(ptr, "use_random"));
 
-  uiItemR(layout, ptr, "step", 0, NULL, ICON_NONE);
+  uiItemR(layout, ptr, "step", 0, nullptr, ICON_NONE);
 }
 
-static void offset_panel_draw(const bContext *UNUSED(C), Panel *panel)
+static void offset_panel_draw(const bContext * /*C*/, Panel *panel)
 {
   uiLayout *layout = panel->layout;
-  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, NULL);
+  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, nullptr);
   uiLayoutSetPropSep(layout, true);
   uiItemR(layout, ptr, "random_start_factor", 0, IFACE_("Random Offset Start"), ICON_NONE);
   uiItemR(layout, ptr, "random_end_factor", 0, IFACE_("Random Offset End"), ICON_NONE);
-  uiItemR(layout, ptr, "random_offset", 0, NULL, ICON_NONE);
-  uiItemR(layout, ptr, "seed", 0, NULL, ICON_NONE);
+  uiItemR(layout, ptr, "random_offset", 0, nullptr, ICON_NONE);
+  uiItemR(layout, ptr, "seed", 0, nullptr, ICON_NONE);
 }
 
-static void panel_draw(const bContext *UNUSED(C), Panel *panel)
+static void panel_draw(const bContext * /*C*/, Panel *panel)
 {
   uiLayout *layout = panel->layout;
 
-  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, NULL);
+  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, nullptr);
 
   uiLayoutSetPropSep(layout, true);
-  uiItemR(layout, ptr, "mode", 0, NULL, ICON_NONE);
+  uiItemR(layout, ptr, "mode", 0, nullptr, ICON_NONE);
 
   uiLayout *col = uiLayoutColumn(layout, true);
 
@@ -310,25 +310,25 @@ static void panel_draw(const bContext *UNUSED(C), Panel *panel)
   gpencil_modifier_panel_end(layout, ptr);
 }
 
-static void mask_panel_draw(const bContext *UNUSED(C), Panel *panel)
+static void mask_panel_draw(const bContext * /*C*/, Panel *panel)
 {
   gpencil_modifier_masking_panel_draw(panel, true, false);
 }
 
-static void curvature_header_draw(const bContext *UNUSED(C), Panel *panel)
+static void curvature_header_draw(const bContext * /*C*/, Panel *panel)
 {
   uiLayout *layout = panel->layout;
 
-  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, NULL);
+  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, nullptr);
 
   uiItemR(layout, ptr, "use_curvature", 0, IFACE_("Curvature"), ICON_NONE);
 }
 
-static void curvature_panel_draw(const bContext *UNUSED(C), Panel *panel)
+static void curvature_panel_draw(const bContext * /*C*/, Panel *panel)
 {
   uiLayout *layout = panel->layout;
 
-  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, NULL);
+  PointerRNA *ptr = gpencil_modifier_panel_get_property_pointers(panel, nullptr);
 
   uiLayoutSetPropSep(layout, true);
 
@@ -336,9 +336,9 @@ static void curvature_panel_draw(const bContext *UNUSED(C), Panel *panel)
 
   uiLayoutSetActive(col, RNA_boolean_get(ptr, "use_curvature"));
 
-  uiItemR(col, ptr, "point_density", 0, NULL, ICON_NONE);
-  uiItemR(col, ptr, "segment_influence", 0, NULL, ICON_NONE);
-  uiItemR(col, ptr, "max_angle", 0, NULL, ICON_NONE);
+  uiItemR(col, ptr, "point_density", 0, nullptr, ICON_NONE);
+  uiItemR(col, ptr, "segment_influence", 0, nullptr, ICON_NONE);
+  uiItemR(col, ptr, "max_angle", 0, nullptr, ICON_NONE);
   uiItemR(col, ptr, "invert_curvature", 0, IFACE_("Invert"), ICON_NONE);
 }
 
@@ -349,11 +349,11 @@ static void panelRegister(ARegionType *region_type)
   gpencil_modifier_subpanel_register(
       region_type, "curvature", "", curvature_header_draw, curvature_panel_draw, panel_type);
   PanelType *offset_panel = gpencil_modifier_subpanel_register(
-      region_type, "offset", "Random Offsets", NULL, offset_panel_draw, panel_type);
+      region_type, "offset", "Random Offsets", nullptr, offset_panel_draw, panel_type);
   gpencil_modifier_subpanel_register(
       region_type, "randomize", "", random_header_draw, random_panel_draw, offset_panel);
   gpencil_modifier_subpanel_register(
-      region_type, "mask", "Influence", NULL, mask_panel_draw, panel_type);
+      region_type, "mask", "Influence", nullptr, mask_panel_draw, panel_type);
 }
 
 GpencilModifierTypeInfo modifierType_Gpencil_Length = {
@@ -366,16 +366,16 @@ GpencilModifierTypeInfo modifierType_Gpencil_Length = {
     /*copyData*/ copyData,
 
     /*deformStroke*/ deformStroke,
-    /*generateStrokes*/ NULL,
+    /*generateStrokes*/ nullptr,
     /*bakeModifier*/ bakeModifier,
-    /*remapTime*/ NULL,
+    /*remapTime*/ nullptr,
 
     /*initData*/ initData,
-    /*freeData*/ NULL,
-    /*isDisabled*/ NULL,
-    /*updateDepsgraph*/ NULL,
-    /*dependsOnTime*/ NULL,
+    /*freeData*/ nullptr,
+    /*isDisabled*/ nullptr,
+    /*updateDepsgraph*/ nullptr,
+    /*dependsOnTime*/ nullptr,
     /*foreachIDLink*/ foreachIDLink,
-    /*foreachTexLink*/ NULL,
+    /*foreachTexLink*/ nullptr,
     /*panelRegister*/ panelRegister,
 };
