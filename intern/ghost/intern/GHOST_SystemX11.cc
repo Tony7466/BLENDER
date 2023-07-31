a4edce178b4209e72985b04aff17618d3df8c6a9
Harley Acheson <harley.acheson@gmail.com>
2023-07-30 18:09:14 -0700

    Updated to the current state of main

--- intern/ghost/intern/GHOST_SystemX11.cc
+++ intern/ghost/intern/GHOST_SystemX11.cc
@@ -1690,6 +1690,8 @@ GHOST_TCapabilityFlag GHOST_SystemX11::getCapabilities() const
 {
   return GHOST_TCapabilityFlag(GHOST_CAPABILITY_FLAG_ALL &
                                ~(
+                                   /* No support yet for desktop sampling. */
+                                   GHOST_kCapabilityDesktopSample |
                                    /* No support yet for image copy/paste. */
                                    GHOST_kCapabilityClipboardImages));
 }
