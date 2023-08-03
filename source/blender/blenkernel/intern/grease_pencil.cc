Tony7466/BLENDER

commit 331969159ef5806e2dd730d0254eace31d0cacfc

Fix: Unreported memory leak
Whenever `grow_array` in
`blender/source/blender/blenkernel/intern/grease_pencil.cc` was called,
it would not free the original array after relocating the data to the
newly allocated memory. This fixes the memory leak.

source/blender/blenkernel/intern/grease_pencil.cc
@@ -1341,6 +1341,7 @@ template<typename T> static void grow_array(T **array, int *num, const int add_n
   T *new_array = reinterpret_cast<T *>(MEM_cnew_array<T *>(new_array_num, __func__));
 
   blender::uninitialized_relocate_n(*array, *num, new_array);
+  MEM_freeN(*array);
 
   *array = new_array;
   *num = new_array_num;

commit 16cc74d1f044feed0329245638efb794f93c98d7

GPv3: assert due to freeing nullptr
Caused by 331969159ef5806e2dd730d0254eace31d0cacfc

`grease_pencil->drawing_array` is initially nullptr. Freeing it results
to assert hit in debug build

source/blender/blenkernel/intern/grease_pencil.cc
@@ -1342,7 +1342,10 @@ template<typename T> static void grow_array(T **array, int *num, const int add_n
   T *new_array = reinterpret_cast<T *>(MEM_cnew_array<T *>(new_array_num, __func__));
 
   blender::uninitialized_relocate_n(*array, *num, new_array);
-  MEM_freeN(*array);
+
+  if (*array) {
+    MEM_freeN(*array);
+  }
 
   *array = new_array;
   *num = new_array_num;