50cde7eceb4202db42a5e2b99b263d85adc7b115
Jacques Lucke <jacques@blender.org>
2023-08-04 13:28:08 +0200

    remove WITH_PYTHON from intern/audaspace

--- intern/audaspace/intern/AUD_PyInit.h
+++ intern/audaspace/intern/AUD_PyInit.h
@@ -9,22 +9,19 @@
 #ifndef __AUD_PYINIT_H__
 #define __AUD_PYINIT_H__
 
-#ifdef WITH_PYTHON
-#  include "Python.h"
+#include "Python.h"
 
-#  ifdef __cplusplus
+#ifdef __cplusplus
 extern "C" {
-#  endif
+#endif
 
 /**
  * Initializes the Python module.
  */
 extern PyObject *AUD_initPython(void);
 
-#  ifdef __cplusplus
+#ifdef __cplusplus
 }
-#  endif
-
 #endif
 
 #endif  //__AUD_PYINIT_H__
