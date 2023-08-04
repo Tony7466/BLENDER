4c77c86c8268a50d5912c4fb6b7aa2fd9a97f234
Lukas Tönne <lukas@blender.org>
2023-08-04 17:29:44 +0200

    Nodes: Remove the experimental "node panels" feature.
    This feature is going to be replaced with a more thorough refactoring
    of the node group interface UI, which has actual node drawing support
    and a new API for integration of panels into nodes.
    
    Design task: #109135
    Implementation: #110272

--- source/blender/makesdna/DNA_node_types.h
+++ source/blender/makesdna/DNA_node_types.h
@@ -180,9 +180,6 @@ typedef struct bNodeSocket {
   /** Custom data for inputs, only UI writes in this. */
   bNodeStack ns DNA_DEPRECATED;
 
-  /* UI panel of the socket. */
-  struct bNodePanel *panel;
-
   bNodeSocketRuntimeHandle *runtime;
 
 #ifdef __cplusplus
@@ -565,12 +562,6 @@ enum {
   NTREE_CHUNKSIZE_1024 = 1024,
 };
 
-/** Panel in node tree for grouping sockets. */
-typedef struct bNodePanel {
-  /** UI name of the panel (not unique). */
-  char *name;
-} bNodePanel;
-
 typedef struct bNestedNodePath {
   /** ID of the node that is or contains the nested node. */
   int32_t node_id;
@@ -666,11 +657,6 @@ typedef struct bNodeTree {
   /** Image representing what the node group does. */
   struct PreviewImage *preview;
 
-  /* UI panels */
-  struct bNodePanel **panels_array;
-  int panels_num;
-  int active_panel;
-
   bNodeTreeRuntimeHandle *runtime;
 
 #ifdef __cplusplus
@@ -747,8 +733,6 @@ typedef struct bNodeTree {
   blender::Span<const bNodeSocket *> interface_inputs() const;
   blender::Span<const bNodeSocket *> interface_outputs() const;
 
-  blender::Span<const bNodePanel *> panels() const;
-  blender::MutableSpan<bNodePanel *> panels_for_write();
   /** Zones in the node tree. Currently there are only simulation zones in geometry nodes. */
   const blender::bke::bNodeTreeZones *zones() const;
 #endif
