036851a5cd2417a43beb3d389f9bbbf0b04cc2db
Almaz Shinbay <shinbay.almaz@gmail.com>
2023-08-08 15:16:08 +0500

    Cleanup: add asserts for pose base tree elements

--- source/blender/editors/space_outliner/tree/tree_element_pose.cc
+++ source/blender/editors/space_outliner/tree/tree_element_pose.cc
@@ -24,6 +24,7 @@ namespace blender::ed::outliner {
 TreeElementPoseBase::TreeElementPoseBase(TreeElement &legacy_te, Object &object)
     : AbstractTreeElement(legacy_te), object_(object)
 {
+  BLI_assert(legacy_te.store_elem->type == TSE_POSE_BASE);
   legacy_te.name = IFACE_("Pose");
 }
 
