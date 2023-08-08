948e8e03b0421c5180a35bd8eb7c8fb5e5269e77
Almaz Shinbay <shinbay.almaz@gmail.com>
2023-08-08 15:13:04 +0500

    Cleanup: add asserts for pose group tree elements

--- source/blender/editors/space_outliner/tree/tree_element_pose_group.cc
+++ source/blender/editors/space_outliner/tree/tree_element_pose_group.cc
@@ -23,6 +23,7 @@ namespace blender::ed::outliner {
 TreeElementPoseGroupBase::TreeElementPoseGroupBase(TreeElement &legacy_te, Object &object)
     : AbstractTreeElement(legacy_te), object_(object)
 {
+  BLI_assert(legacy_te.store_elem->type == TSE_POSEGRP_BASE);
   legacy_te.name = IFACE_("Bone Groups");
 }
 
@@ -42,6 +43,7 @@ TreeElementPoseGroup::TreeElementPoseGroup(TreeElement &legacy_te,
                                            bActionGroup &agrp)
     : AbstractTreeElement(legacy_te), /* object_(object), */ agrp_(agrp)
 {
+  BLI_assert(legacy_te.store_elem->type == TSE_POSEGRP);
   legacy_te.name = agrp_.name;
   legacy_te.directdata = &agrp_;
 }
