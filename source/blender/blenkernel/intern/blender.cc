bbf1fbb886ae3effe8d1b41c9ad6be255db3c460
Sergey Sharybin <sergey@blender.org>
2023-08-09 10:18:31 +0200

    Less noisy approach: rename macro to LISTBASE_SWAP

--- source/blender/blenkernel/intern/blender.cc
+++ source/blender/blenkernel/intern/blender.cc
@@ -356,12 +356,7 @@ void BKE_blender_userdef_app_template_data_swap(UserDef *userdef_a, UserDef *use
   } \
   ((void)0)
 
-/* Indirect includes cab pull 'sys/queue.h', which has defines for the list operations,
- * including LIST_SWAP. */
-#ifdef LIST_SWAP
-#  undef LIST_SWAP
-#endif
-#define LIST_SWAP(id) \
+#define LISTBASE_SWAP(id) \
   { \
     SWAP(ListBase, userdef_a->id, userdef_b->id); \
   } \
@@ -378,12 +373,12 @@ void BKE_blender_userdef_app_template_data_swap(UserDef *userdef_a, UserDef *use
   } \
   ((void)0)
 
-  LIST_SWAP(uistyles);
-  LIST_SWAP(uifonts);
-  LIST_SWAP(themes);
-  LIST_SWAP(addons);
-  LIST_SWAP(user_keymaps);
-  LIST_SWAP(user_keyconfig_prefs);
+  LISTBASE_SWAP(uistyles);
+  LISTBASE_SWAP(uifonts);
+  LISTBASE_SWAP(themes);
+  LISTBASE_SWAP(addons);
+  LISTBASE_SWAP(user_keymaps);
+  LISTBASE_SWAP(user_keyconfig_prefs);
 
   DATA_SWAP(font_path_ui);
   DATA_SWAP(font_path_ui_mono);
@@ -399,7 +394,7 @@ void BKE_blender_userdef_app_template_data_swap(UserDef *userdef_a, UserDef *use
 
 #undef SWAP_TYPELESS
 #undef DATA_SWAP
-#undef LIST_SWAP
+#undef LISTBASE_SWAP
 #undef FLAG_SWAP
 }
 
