98792a19073a3b0dd8f5418d071483a179c3ccbf
Pratik Borhade <pratikborhade302@gmail.com>
2023-08-16 15:02:22 +0530

    Fix: GPv3 layer renaming when name is unchanged
    When old and new names of layers/groups are same, garbage string is
    displaed. This is because `oldname` char pointer is referenced to
    `node.name`. After function execution, `oldname` argument is freed in
    `interface_handler` (see `after.rename_orig`), thus displays garbage
    string.

--- source/blender/editors/space_outliner/outliner_draw.cc
+++ source/blender/editors/space_outliner/outliner_draw.cc
@@ -864,6 +864,9 @@ static void namebutton_fn(bContext *C, void *tsep, char *oldname)
           /* The node already has the new name set. To properly rename the node, we need to first
            * store the new name, restore the old name in the node, and then call the rename
            * function. */
+          if (strcmp(node.name, oldname) == 0) {
+            break;
+          }
           std::string new_name(node.name);
           node.name = oldname;
           if (node.is_group()) {
