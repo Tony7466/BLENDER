9db996ea5e150ac69ef1883f7f32b1d4beefa270
Richard Antalik <richardantalik@gmail.com>
2023-08-17 14:57:40 +0200

    "fix" FFTW3_FOUND not being defined despite `FindFftw3.cmake` file docs

--- extern/audaspace/blender_config.cmake
+++ extern/audaspace/blender_config.cmake
@@ -8,7 +8,7 @@ set(WITH_C TRUE)  # "Build C Module"
 set(WITH_DOCS FALSE)  # "Build C++ HTML Documentation with Doxygen"
 set(WITH_FFMPEG ${WITH_CODEC_FFMPEG})  # "Build With FFMPEG"
 if(DEFINED WITH_FFTW3) # "Build With FFTW"
-  set(FFTW_FOUND ${FFTW3_FOUND})
+  set(FFTW_FOUND TRUE)
   set(WITH_FFTW ${WITH_FFTW3})
   set(FFTW_INCLUDE_DIR ${FFTW3_INCLUDE_DIRS})
   set(FFTW_LIBRARY ${FFTW3_LIBRARIES})
