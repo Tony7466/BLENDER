bd35eb86b97dc990c49aea055d2c50a7d7cae3bd
Richard Antalik <richardantalik@gmail.com>
2023-08-17 14:10:13 +0200

    Update cmake variables, so it should build on Linux/Mac

--- extern/audaspace/blender_config.cmake
+++ extern/audaspace/blender_config.cmake
@@ -8,9 +8,10 @@ set(WITH_C TRUE)  # "Build C Module"
 set(WITH_DOCS FALSE)  # "Build C++ HTML Documentation with Doxygen"
 set(WITH_FFMPEG ${WITH_CODEC_FFMPEG})  # "Build With FFMPEG"
 if(DEFINED WITH_FFTW3) # "Build With FFTW"
-  set(FFTW_FOUND TRUE)
-  set(WITH_FFTW TRUE)
+  set(FFTW_FOUND ${FFTW3_FOUND})
+  set(WITH_FFTW ${WITH_FFTW3})
   set(FFTW_INCLUDE_DIR ${FFTW3_INCLUDE_DIRS})
+  set(FFTW_LIBRARY ${FFTW3_LIBRARIES})
 endif()
 set(WITH_LIBSNDFILE ${WITH_CODEC_SNDFILE})  # "Build With LibSndFile"
 set(SEPARATE_C FALSE)  # "Build C Binding as separate library"
