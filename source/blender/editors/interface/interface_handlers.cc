f9344704764522e94e43961b0cc71e447ffcb105
ChengduLittleA <xp8110@outlook.com>
2023-08-19 16:41:53 +0800

    Remove interface_handlers.cc

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -6286,7 +6286,7 @@ static int ui_do_but_COLOR(bContext *C, uiBut *but, uiHandleButtonData *data, co
         hsv[2] = clamp_f(hsv[2] + 0.05f, 0.0f, 1.0f);
       }
       else {
-        const float fac = 0.005 * WM_event_absolute_delta_y(event) / UI_SCALE_FAC;
+        const float fac = 0.005 * (event->xy[1] - event->prev_xy[1]);
         hsv[2] = clamp_f(hsv[2] + fac, 0.0f, 1.0f);
       }
 
