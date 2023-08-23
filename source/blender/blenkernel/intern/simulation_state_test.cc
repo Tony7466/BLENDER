0667fe78a69ba3084d8ebdf8661493353a9d8d4e
Vitor Boschi <vitorboschi@gmail.com>
2023-08-23 11:45:08 -0300

    clang-format

--- source/blender/blenkernel/intern/simulation_state_test.cc
+++ source/blender/blenkernel/intern/simulation_state_test.cc
@@ -3,17 +3,19 @@
 #include "testing/testing.h"
 
 namespace blender::bke::tests {
-TEST(simulation, sim_state_cache_starts_empty) {
+TEST(simulation, sim_state_cache_starts_empty)
+{
   sim::ModifierSimulationCache sim_cache;
   EXPECT_FALSE(sim_cache.has_state_at_frame(SubFrame(0)));
   EXPECT_FALSE(sim_cache.has_state_at_frame(SubFrame(20)));
   EXPECT_FALSE(sim_cache.has_states());
 }
 
-TEST(simulation, sim_state_cache_caches_state) {
-    sim::ModifierSimulationCache sim_cache;
+TEST(simulation, sim_state_cache_caches_state)
+{
+  sim::ModifierSimulationCache sim_cache;
 
-  auto& state20_old = sim_cache.get_state_at_frame_for_write(SubFrame(20));
+  auto &state20_old = sim_cache.get_state_at_frame_for_write(SubFrame(20));
 
   /* Set some data to make sure we get the same state back later on. */
   state20_old.meta_path_ = "test";
@@ -26,7 +28,8 @@ TEST(simulation, sim_state_cache_caches_state) {
   EXPECT_EQ(state20_new->meta_path_, "test");
 }
 
-TEST(simulation, sim_state_cache_get_state_around_frame_empty) {
+TEST(simulation, sim_state_cache_get_state_around_frame_empty)
+{
   sim::ModifierSimulationCache sim_cache;
 
   auto stateAroundFrames = sim_cache.get_states_around_frame(SubFrame(0));
@@ -35,7 +38,8 @@ TEST(simulation, sim_state_cache_get_state_around_frame_empty) {
   EXPECT_EQ(stateAroundFrames.next, nullptr);
 }
 
-TEST(simulation, sim_state_cache_get_state_around_frame_current) {
+TEST(simulation, sim_state_cache_get_state_around_frame_current)
+{
   sim::ModifierSimulationCache sim_cache;
 
   sim_cache.get_state_at_frame_for_write(SubFrame(0));
@@ -46,7 +50,8 @@ TEST(simulation, sim_state_cache_get_state_around_frame_current) {
   EXPECT_EQ(stateAroundFrames.next, nullptr);
 }
 
-TEST(simulation, sim_state_cache_get_state_around_frame_prev) {
+TEST(simulation, sim_state_cache_get_state_around_frame_prev)
+{
   sim::ModifierSimulationCache sim_cache;
 
   sim_cache.get_state_at_frame_for_write(SubFrame(0));
@@ -57,7 +62,8 @@ TEST(simulation, sim_state_cache_get_state_around_frame_prev) {
   EXPECT_EQ(stateAroundFrames.next, nullptr);
 }
 
-TEST(simulation, sim_state_cache_get_state_around_frame_next) {
+TEST(simulation, sim_state_cache_get_state_around_frame_next)
+{
   sim::ModifierSimulationCache sim_cache;
 
   sim_cache.get_state_at_frame_for_write(SubFrame(2));
@@ -68,7 +74,8 @@ TEST(simulation, sim_state_cache_get_state_around_frame_next) {
   EXPECT_NE(stateAroundFrames.next, nullptr);
 }
 
-TEST(simulation, sim_state_cache_get_state_around_frame_all) {
+TEST(simulation, sim_state_cache_get_state_around_frame_all)
+{
   sim::ModifierSimulationCache sim_cache;
 
   sim_cache.get_state_at_frame_for_write(SubFrame(0));
