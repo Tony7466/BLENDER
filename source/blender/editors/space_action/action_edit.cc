2f740ffa033fdc7606c2a0a5003e4d25843c5c98
Amelie Fondevilla <amelie.fondevilla@les-fees-speciales.coop>
2023-08-24 16:43:40 +0200

    Update `get_keyframe_extents` function to take into account grease pencil frames

--- source/blender/editors/space_action/action_edit.cc
+++ source/blender/editors/space_action/action_edit.cc
@@ -198,6 +198,19 @@ static bool get_keyframe_extents(bAnimContext *ac, float *min, float *max, const
           found = true;
         }
       }
+      else if (ale->datatype == ALE_GREASE_PENCIL_CEL) {
+        using namespace blender::bke::greasepencil;
+        const Layer &layer = static_cast<GreasePencilLayer *>(ale->data)->wrap();
+
+        for (const FramesMapKey key : layer.sorted_keys()) {
+          if (onlySel && !layer.frames().lookup(key).is_selected()) {
+            continue;
+          }
+          *min = min_ff(*min, float(key));
+          *max = max_ff(*max, float(key));
+          found = true;
+        }
+      }
       else {
         FCurve *fcu = (FCurve *)ale->key_data;
         float tmin, tmax;
