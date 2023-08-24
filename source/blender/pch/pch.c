ec27abd5d5d59f3f500992154396fbe1bfd03452
Aras Pranckevicius <aras@nesnausk.org>
2023-08-24 14:15:32 +0300

    Build: revert "share precompiled headers from base bf_pch library"
    REUSE_FROM causes some issues with transitive library dependencies
    on MSVC generator (see https://gitlab.kitware.com/cmake/cmake/-/issues/22630
    where a similar issue was solved, but the developer comments indicate
    that in more intricate cases the issue remains).
    
    With REUSE_FROM, some targets do not link, but only in Debug config.
    e.g. bf_blenlib wrongly tries to link with
    build_windows_x64_vc17_Release\source\blender\pch\bf_pch.dir\Debug\cmake_pch.obj
    which simply does not exist since nothing produces that.

--- source/blender/pch/pch.c
+++
@@ -1,9 +0,0 @@
-/* SPDX-FileCopyrightText: 2023 Blender Authors
- *
- * SPDX-License-Identifier: GPL-2.0-or-later */
-
-#include "pch.h"
-
-/* Dummy symbol so that the linker for PCH static library
- * does not emit a warning about an empty library. */
-int pch_dummy_symbol;
