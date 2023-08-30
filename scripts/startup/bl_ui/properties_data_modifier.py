12ffe3a50e689d6c153b78118b704793983f13e0
Hans Goudey <h.goudey@me.com>
2023-08-30 14:54:10 -0400

    Duplicate existing modifier add menu

--- scripts/startup/bl_ui/properties_data_modifier.py
+++ scripts/startup/bl_ui/properties_data_modifier.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-from bpy.types import Panel
+from bpy.types import Panel, Menu
 
 
 class ModifierButtonsPanel:
@@ -22,8 +22,107 @@ class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
 
     def draw(self, _context):
         layout = self.layout
-        layout.operator_menu_enum("object.modifier_add", "type")
+        layout.operator("wm.call_menu", text="Add Modifier", icon='ADD').name="OBJECT_MT_modifier_add"
         layout.template_modifiers()
+
+
+class OBJECT_MT_modifier_add(Menu):
+    bl_label = "Add Modifier"
+
+    def draw(self, context):
+        layout = self.layout
+
+        # layout.operator("node.new_geometry_nodes_modifier", text="New", icon='ADD')
+        layout.menu("OBJECT_MT_modifier_add_edit")
+        layout.menu("OBJECT_MT_modifier_add_generate")
+        layout.menu("OBJECT_MT_modifier_add_deform")
+        layout.menu("OBJECT_MT_modifier_add_physics")
+
+
+class OBJECT_MT_modifier_add_edit(Menu):
+    bl_label = "Generate"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("object.modifier_add", text="Data Transfer", icon='MOD_DATA_TRANSFER').type='DATA_TRANSFER'
+        layout.operator("object.modifier_add", text="Mesh Cache", icon='MOD_MESHDEFORM').type='MESH_CACHE'
+        layout.operator("object.modifier_add", text="Mesh Sequence Cache", icon='MOD_MESHDEFORM').type='MESH_SEQUENCE_CACHE'
+        layout.operator("object.modifier_add", text="Normal Edit", icon='MOD_NORMALEDIT').type='NORMAL_EDIT'
+        layout.operator("object.modifier_add", text="Weighted Normal", icon='MOD_NORMALEDIT').type='WEIGHTED_NORMAL'
+        layout.operator("object.modifier_add", text="UV Project", icon='MOD_UVPROJECT').type='UV_PROJECT'
+        layout.operator("object.modifier_add", text="UV Warp", icon='MOD_UVPROJECT').type='UV_WARP'
+        layout.operator("object.modifier_add", text="Vertex Weight Edit", icon='MOD_VERTEX_WEIGHT').type='VERTEX_WEIGHT_EDIT'
+        layout.operator("object.modifier_add", text="Vertex Weight Mix", icon='MOD_VERTEX_WEIGHT').type='VERTEX_WEIGHT_MIX'
+        layout.operator("object.modifier_add", text="Vertex Weight Proximity", icon='MOD_VERTEX_WEIGHT').type='VERTEX_WEIGHT_PROXIMITY'
+
+
+class OBJECT_MT_modifier_add_generate(Menu):
+    bl_label = "Generate"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("object.modifier_add", text="Array", icon='MOD_ARRAY').type='ARRAY'
+        layout.operator("object.modifier_add", text="Bevel", icon='MOD_BEVEL').type='BEVEL'
+        layout.operator("object.modifier_add", text="Boolean", icon='MOD_BOOLEAN').type='BOOLEAN'
+        layout.operator("object.modifier_add", text="Build", icon='MOD_BUILD').type='BUILD'
+        layout.operator("object.modifier_add", text="Decimate", icon='MOD_DECIM').type='DECIMATE'
+        layout.operator("object.modifier_add", text="Edge Split", icon='MOD_EDGESPLIT').type='EDGE_SPLIT'
+        layout.operator("object.modifier_add", text="Mask", icon='MOD_MASK').type='MASK'
+        layout.operator("object.modifier_add", text="Mirror", icon='MOD_MIRROR').type='MIRROR'
+        if context.object.type == 'VOLUME':
+            layout.operator("object.modifier_add", text="Mesh to Volume", icon='VOLUME_DATA').type='MESH_TO_VOLUME'
+        layout.operator("object.modifier_add", text="Multiresolution", icon='MOD_MULTIRES').type='MULTIRES'
+        layout.operator("object.modifier_add", text="Remesh", icon='MOD_REMESH').type='REMESH'
+        layout.operator("object.modifier_add", text="Screw", icon='MOD_SCREW').type='SCREW'
+        layout.operator("object.modifier_add", text="Skin", icon='MOD_SKIN').type='SKIN'
+        layout.operator("object.modifier_add", text="Solidify", icon='MOD_SOLIDIFY').type='SOLIDIFY'
+        layout.operator("object.modifier_add", text="Subdivision Surface", icon='MOD_SUBSURF').type='SUBSURF'
+        layout.operator("object.modifier_add", text="Triangulate", icon='MOD_TRIANGULATE').type='TRIANGULATE'
+        if context.object.type == 'VOLUME':
+            layout.operator("object.modifier_add", text="Volume to Mesh", icon='VOLUME_DATA').type='VOLUME_TO_MESH'
+        layout.operator("object.modifier_add", text="Weld", icon='AUTOMERGE_OFF').type='WELD'
+        layout.operator("object.modifier_add", text="Wireframe", icon='MOD_WIREFRAME').type='WIREFRAME'
+
+
+class OBJECT_MT_modifier_add_deform(Menu):
+    bl_label = "Deform"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("object.modifier_add", text="Armature", icon='MOD_ARMATURE').type='ARMATURE'
+        layout.operator("object.modifier_add", text="Cast", icon='MOD_CAST').type='CAST'
+        layout.operator("object.modifier_add", text="Curve", icon='MOD_CURVE').type='CURVE'
+        layout.operator("object.modifier_add", text="Displace", icon='MOD_DISPLACE').type='DISPLACE'
+        layout.operator("object.modifier_add", text="Hook", icon='HOOK').type='HOOK'
+        layout.operator("object.modifier_add", text="Laplacian Deform", icon='MOD_MESHDEFORM').type='LAPLACIANDEFORM'
+        layout.operator("object.modifier_add", text="Lattice", icon='MOD_LATTICE').type='LATTICE'
+        layout.operator("object.modifier_add", text="Mesh Deform", icon='MOD_MESHDEFORM').type='MESH_DEFORM'
+        layout.operator("object.modifier_add", text="Shrinkwrap", icon='MOD_SHRINKWRAP').type='SHRINKWRAP'
+        layout.operator("object.modifier_add", text="Simple Deform", icon='MOD_SIMPLEDEFORM').type='SIMPLE_DEFORM'
+        layout.operator("object.modifier_add", text="Smooth", icon='MOD_SMOOTH').type='SMOOTH'
+        layout.operator("object.modifier_add", text="Smooth Corrective", icon='MOD_SMOOTH').type='CORRECTIVE_SMOOTH'
+        layout.operator("object.modifier_add", text="Smooth Laplacian", icon='MOD_SMOOTH').type='LAPLACIANSMOOTH'
+        layout.operator("object.modifier_add", text="Surface Deform", icon='MOD_MESHDEFORM').type='SURFACE_DEFORM'
+        layout.operator("object.modifier_add", text="Warp", icon='MOD_WARP').type='WARP'
+        layout.operator("object.modifier_add", text="Wave", icon='MOD_WAVE').type='WAVE'
+        if context.object.type == 'VOLUME':
+            layout.operator("object.modifier_add", text="Volume Displace", icon='VOLUME_DATA').type='VOLUME_DISPLACE'
+
+
+class OBJECT_MT_modifier_add_physics(Menu):
+    bl_label = "Physics"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("object.modifier_add", text="Cloth", icon='MOD_CLOTH').type='CLOTH'
+        layout.operator("object.modifier_add", text="Collision", icon='MOD_PHYSICS').type='COLLISION'
+        layout.operator("object.modifier_add", text="Dynamic Paint", icon='MOD_DYNAMICPAINT').type='DYNAMIC_PAINT'
+        layout.operator("object.modifier_add", text="Explode", icon='MOD_EXPLODE').type='EXPLODE'
+        layout.operator("object.modifier_add", text="Fluid", icon='MOD_FLUIDSIM').type='FLUID'
+        layout.operator("object.modifier_add", text="Ocean", icon='MOD_OCEAN').type='OCEAN'
+        layout.operator("object.modifier_add", text="Particle Instance", icon='MOD_PARTICLE_INSTANCE').type='PARTICLE_INSTANCE'
+        layout.operator("object.modifier_add", text="Particle System", icon='MOD_PARTICLES').type='PARTICLE_SYSTEM'
+        layout.operator("object.modifier_add", text="Soft Body", icon='MOD_SOFT').type='SOFT_BODY'
 
 
 class DATA_PT_gpencil_modifiers(ModifierButtonsPanel, Panel):
@@ -42,6 +141,11 @@ class DATA_PT_gpencil_modifiers(ModifierButtonsPanel, Panel):
 
 classes = (
     DATA_PT_modifiers,
+    OBJECT_MT_modifier_add,
+    OBJECT_MT_modifier_add_edit,
+    OBJECT_MT_modifier_add_generate,
+    OBJECT_MT_modifier_add_deform,
+    OBJECT_MT_modifier_add_physics,
     DATA_PT_gpencil_modifiers,
 )
 
