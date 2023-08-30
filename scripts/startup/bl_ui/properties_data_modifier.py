292b8df1fa8de398ab3da5d56c8d04307ced221f
Hans Goudey <h.goudey@me.com>
2023-08-30 17:13:41 -0400

    Fix text object type

--- scripts/startup/bl_ui/properties_data_modifier.py
+++ scripts/startup/bl_ui/properties_data_modifier.py
@@ -49,9 +49,9 @@ class OBJECT_MT_modifier_add_edit(Menu):
         ob_type = context.object.type
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Data Transfer", icon='MOD_DATA_TRANSFER').type='DATA_TRANSFER'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE', 'LATTICE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
             layout.operator("object.modifier_add", text="Mesh Cache", icon='MOD_MESHDEFORM').type='MESH_CACHE'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
             layout.operator("object.modifier_add", text="Mesh Sequence Cache", icon='MOD_MESHDEFORM').type='MESH_SEQUENCE_CACHE'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Normal Edit", icon='MOD_NORMALEDIT').type='NORMAL_EDIT'
@@ -70,36 +70,36 @@ class OBJECT_MT_modifier_add_generate(Menu):
     def draw(self, context):
         layout = self.layout
         ob_type = context.object.type
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
             layout.operator("object.modifier_add", text="Array", icon='MOD_ARRAY').type='ARRAY'
             layout.operator("object.modifier_add", text="Bevel", icon='MOD_BEVEL').type='BEVEL'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Boolean", icon='MOD_BOOLEAN').type='BOOLEAN'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
             layout.operator("object.modifier_add", text="Build", icon='MOD_BUILD').type='BUILD'
             layout.operator("object.modifier_add", text="Decimate", icon='MOD_DECIM').type='DECIMATE'
             layout.operator("object.modifier_add", text="Edge Split", icon='MOD_EDGESPLIT').type='EDGE_SPLIT'
             layout.operator("object.modifier_add", text="Geometry Nodes", icon='GEOMETRY_NODES').type='NODES'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Mask", icon='MOD_MASK').type='MASK'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
             layout.operator("object.modifier_add", text="Mirror", icon='MOD_MIRROR').type='MIRROR'
         if ob_type == 'VOLUME':
             layout.operator("object.modifier_add", text="Mesh to Volume", icon='VOLUME_DATA').type='MESH_TO_VOLUME'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Multiresolution", icon='MOD_MULTIRES').type='MULTIRES'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
             layout.operator("object.modifier_add", text="Remesh", icon='MOD_REMESH').type='REMESH'
             layout.operator("object.modifier_add", text="Screw", icon='MOD_SCREW').type='SCREW'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Skin", icon='MOD_SKIN').type='SKIN'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
             layout.operator("object.modifier_add", text="Solidify", icon='MOD_SOLIDIFY').type='SOLIDIFY'
             layout.operator("object.modifier_add", text="Subdivision Surface", icon='MOD_SUBSURF').type='SUBSURF'
             layout.operator("object.modifier_add", text="Triangulate", icon='MOD_TRIANGULATE').type='TRIANGULATE'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Volume to Mesh", icon='VOLUME_DATA').type='VOLUME_TO_MESH'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
             layout.operator("object.modifier_add", text="Weld", icon='AUTOMERGE_OFF').type='WELD'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Wireframe", icon='MOD_WIREFRAME').type='WIREFRAME'
@@ -112,28 +112,28 @@ class OBJECT_MT_modifier_add_deform(Menu):
     def draw(self, context):
         layout = self.layout
         ob_type = context.object.type
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE', 'LATTICE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
             layout.operator("object.modifier_add", text="Armature", icon='MOD_ARMATURE').type='ARMATURE'
             layout.operator("object.modifier_add", text="Cast", icon='MOD_CAST').type='CAST'
             layout.operator("object.modifier_add", text="Curve", icon='MOD_CURVE').type='CURVE'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Displace", icon='MOD_DISPLACE').type='DISPLACE'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE', 'LATTICE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
             layout.operator("object.modifier_add", text="Hook", icon='HOOK').type='HOOK'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Laplacian Deform", icon='MOD_MESHDEFORM').type='LAPLACIANDEFORM'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE', 'LATTICE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
             layout.operator("object.modifier_add", text="Lattice", icon='MOD_LATTICE').type='LATTICE'
             layout.operator("object.modifier_add", text="Mesh Deform", icon='MOD_MESHDEFORM').type='MESH_DEFORM'
             layout.operator("object.modifier_add", text="Shrinkwrap", icon='MOD_SHRINKWRAP').type='SHRINKWRAP'
             layout.operator("object.modifier_add", text="Simple Deform", icon='MOD_SIMPLEDEFORM').type='SIMPLE_DEFORM'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE'}:
             layout.operator("object.modifier_add", text="Smooth", icon='MOD_SMOOTH').type='SMOOTH'
         if ob_type == 'MESH':
             layout.operator("object.modifier_add", text="Smooth Corrective", icon='MOD_SMOOTH').type='CORRECTIVE_SMOOTH'
             layout.operator("object.modifier_add", text="Smooth Laplacian", icon='MOD_SMOOTH').type='LAPLACIANSMOOTH'
             layout.operator("object.modifier_add", text="Surface Deform", icon='MOD_MESHDEFORM').type='SURFACE_DEFORM'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE', 'LATTICE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
             layout.operator("object.modifier_add", text="Warp", icon='MOD_WARP').type='WARP'
             layout.operator("object.modifier_add", text="Wave", icon='MOD_WAVE').type='WAVE'
         if ob_type == 'VOLUME':
@@ -156,7 +156,7 @@ class OBJECT_MT_modifier_add_physics(Menu):
             layout.operator("object.modifier_add", text="Ocean", icon='MOD_OCEAN').type='OCEAN'
             layout.operator("object.modifier_add", text="Particle Instance", icon='MOD_PARTICLE_INSTANCE').type='PARTICLE_INSTANCE'
             layout.operator("object.modifier_add", text="Particle System", icon='MOD_PARTICLES').type='PARTICLE_SYSTEM'
-        if ob_type in {'MESH', 'CURVE', 'TEXT', 'SURFACE', 'LATTICE'}:
+        if ob_type in {'MESH', 'CURVE', 'FONT', 'SURFACE', 'LATTICE'}:
             layout.operator("object.modifier_add", text="Soft Body", icon='MOD_SOFT').type='SOFT_BODY'
         layout.template_modifier_asset_menu_items(catalog_path=self.bl_label)
 
