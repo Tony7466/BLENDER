27dd26b31d02f3023fac659d690349652ca8038b
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-08-31 15:37:07 +0200

    resolve merge issues

--- scripts/startup/bl_ui/space_graph.py
+++ scripts/startup/bl_ui/space_graph.py
@@ -302,6 +302,7 @@ class GRAPH_MT_key_blending(Menu):
         layout.operator("graph.blend_offset", text="Blend Offset")
         layout.operator("graph.blend_to_ease", text="Blend to Ease")
         layout.operator("graph.match_slope", text="Match Slope")
+        layout.operator("graph.scale_average", text="Scale Average")
 
 
 class GRAPH_MT_key_smoothing(Menu):
--- source/blender/editors/space_graph/graph_slider_ops.cc
+++ source/blender/editors/space_graph/graph_slider_ops.cc
@@ -1298,7 +1298,8 @@ static void scale_average_graph_keys(bAnimContext *ac, const float factor)
 {
   ListBase anim_data = {NULL, NULL};
 
-  ANIM_animdata_filter(ac, &anim_data, OPERATOR_DATA_FILTER, ac->data, ac->datatype);
+  ANIM_animdata_filter(
+      ac, &anim_data, OPERATOR_DATA_FILTER, ac->data, eAnimCont_Types(ac->datatype));
   LISTBASE_FOREACH (bAnimListElem *, ale, &anim_data) {
     FCurve *fcu = (FCurve *)ale->key_data;
     ListBase segments = find_fcurve_segments(fcu);
@@ -1341,7 +1342,7 @@ static void scale_average_draw_status_header(bContext *C, tGraphSliderOp *gso)
 
 static void scale_average_modal_update(bContext *C, wmOperator *op)
 {
-  tGraphSliderOp *gso = op->customdata;
+  tGraphSliderOp *gso = static_cast<tGraphSliderOp *>(op->customdata);
 
   scale_average_draw_status_header(C, gso);
 
@@ -1360,11 +1361,11 @@ static int scale_average_invoke(bContext *C, wmOperator *op, const wmEvent *even
     return invoke_result;
   }
 
-  tGraphSliderOp *gso = op->customdata;
+  tGraphSliderOp *gso = static_cast<tGraphSliderOp *>(op->customdata);
   gso->modal_update = scale_average_modal_update;
   gso->factor_prop = RNA_struct_find_property(op->ptr, "factor");
   scale_average_draw_status_header(C, gso);
-  ED_slider_is_bidirectional_set(gso->slider, true);
+  ED_slider_factor_bounds_set(gso->slider, -1, 1);
   ED_slider_factor_set(gso->slider, 0.0f);
 
   return invoke_result;
