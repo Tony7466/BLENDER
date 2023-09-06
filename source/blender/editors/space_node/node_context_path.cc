Tony7466/BLENDER

commit fa34992def0a162bddbec5d903ef043d547fc4f2

Cleanup: Remove unnecessary includes from C++ data structure headers
The hash tables and vector blenlib headers were pulling many more
headers than they actually need, including the C base math header,
our C string API header, and the StringRef header. All of this
potentially slows down compilation and polutes autocomplete
with unrelated information.

Also remove the `ListBase` constructor for `Vector`. It wasn't used
much, and making it easy to use `ListBase` isn't worth it for the
same reasons mentioned above.

It turns out a lot of files depended on indirect includes of
`BLI_string.h` and `BLI_listbase.h`, so those are fixed here.

Pull Request: https://projects.blender.org/blender/blender/pulls/111801

source/blender/editors/space_node/node_context_path.cc
@@ -55,9 +55,10 @@ static void context_path_add_node_tree_and_node_groups(const SpaceNode &snode,
                                                        Vector<ui::ContextPathItem> &path,
                                                        const bool skip_base = false)
 {
-  Vector<const bNodeTreePath *> tree_path = snode.treepath;
-  for (const bNodeTreePath *path_item : tree_path.as_span().drop_front(int(skip_base))) {
-    ui::context_path_add_generic(path, RNA_NodeTree, path_item->nodetree, ICON_NODETREE);
+  LISTBASE_FOREACH (const bNodeTreePath *, path_item, &snode.treepath) {
+    if (skip_base && path_item == snode.treepath.first) {
+      ui::context_path_add_generic(path, RNA_NodeTree, path_item->nodetree, ICON_NODETREE);
+    }
   }
 }
 

commit 181a64ef486044f8e935fa931e5343dd2ab3c30f

Fix #111983 : Context Path in the node editors is broken
Mistake in fa34992def0a16 (reversed conditional).

source/blender/editors/space_node/node_context_path.cc
@@ -56,7 +56,7 @@ static void context_path_add_node_tree_and_node_groups(const SpaceNode &snode,
                                                        const bool skip_base = false)
 {
   LISTBASE_FOREACH (const bNodeTreePath *, path_item, &snode.treepath) {
-    if (skip_base && path_item == snode.treepath.first) {
+    if (!(skip_base && path_item == snode.treepath.first)) {
       ui::context_path_add_generic(path, RNA_NodeTree, path_item->nodetree, ICON_NODETREE);
     }
   }