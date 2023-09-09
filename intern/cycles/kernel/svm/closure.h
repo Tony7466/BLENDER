91a50f0aa005c58f30fc62480fb2b9876c0f12aa
Weizhen Huang <weizhen@blender.org>
2023-09-09 23:49:02 +0200

    Use square root of the base color for transmission color
    because the color is applied both on entrance and exit. Using square
    root makes the resulted color closer to the base color.

--- intern/cycles/kernel/svm/closure.h
+++ intern/cycles/kernel/svm/closure.h
@@ -257,7 +257,7 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
           fresnel->exponent = -eta;
 
           fresnel->reflection_tint = one_spectrum();
-          fresnel->transmission_tint = rgb_to_spectrum(base_color);
+          fresnel->transmission_tint = rgb_to_spectrum(sqrt(base_color));
 
           /* setup bsdf */
           sd->flag |= bsdf_microfacet_ggx_glass_setup(bsdf);
