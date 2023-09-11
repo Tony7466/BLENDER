ba699ca234d592b47e8731979de1bce1f4474a33
Jeroen Bakker <jeroen@blender.org>
2023-09-11 15:03:45 +0200

    Removed comment.

--- source/blender/gpu/vulkan/vk_shader.cc
+++ source/blender/gpu/vulkan/vk_shader.cc
@@ -507,7 +507,6 @@ static char *glsl_patch_get()
   STR_CONCAT(patch, slen, "#define gl_VertexID gl_VertexIndex\n");
   STR_CONCAT(patch, slen, "#define gpu_InstanceIndex (gl_InstanceIndex)\n");
   STR_CONCAT(patch, slen, "#define GPU_ARB_texture_cube_map_array\n");
-  /* `gl_InstanceID` should always start at 0. */
   STR_CONCAT(patch, slen, "#define gl_InstanceID (gpu_InstanceIndex - gpu_BaseInstance)\n");
 
   /* TODO(fclem): This creates a validation error and should be already part of Vulkan 1.2. */
