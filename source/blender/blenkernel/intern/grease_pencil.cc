2c6b9c2b0d6e76127ba9d5a6b3b773fc3d20ffe4
Falk David <falk@blender.org>
2023-09-12 16:01:26 +0200

    Remove namespace prefix

--- source/blender/blenkernel/intern/grease_pencil.cc
+++ source/blender/blenkernel/intern/grease_pencil.cc
@@ -2055,7 +2055,7 @@ static void write_drawing_array(GreasePencil &grease_pencil, BlendWriter *writer
     switch (drawing_base->type) {
       case GP_DRAWING: {
         GreasePencilDrawing *drawing = reinterpret_cast<GreasePencilDrawing *>(drawing_base);
-        blender::bke::CurvesGeometry::BlendWriteData write_data =
+        bke::CurvesGeometry::BlendWriteData write_data =
             drawing->wrap().strokes_for_write().blend_write_prepare();
         BLO_write_struct(writer, GreasePencilDrawing, drawing);
         drawing->wrap().strokes_for_write().blend_write(*writer, grease_pencil.id, write_data);
