3eb7a4f2effa4f04383f8224b78af80e7db4d894
Lukas Stockner <lukas.stockner@freenet.de>
2023-09-12 23:43:04 +0200

    Fix USD compilation

--- source/blender/io/usd/intern/usd_reader_material.cc
+++ source/blender/io/usd/intern/usd_reader_material.cc
@@ -481,11 +481,11 @@ void USDMaterialReader::set_principled_node_inputs(bNode *principled,
     set_node_input(roughness_input, principled, "Roughness", ntree, column, &context);
   }
 
-  if (pxr::UsdShadeInput coat_input = usd_shader.GetInput(usdtokens::coat)) {
+  if (pxr::UsdShadeInput coat_input = usd_shader.GetInput(usdtokens::clearcoat)) {
     set_node_input(coat_input, principled, "Coat", ntree, column, &context);
   }
 
-  if (pxr::UsdShadeInput coat_roughness_input = usd_shader.GetInput(usdtokens::coatRoughness)) {
+  if (pxr::UsdShadeInput coat_roughness_input = usd_shader.GetInput(usdtokens::clearcoatRoughness)) {
     set_node_input(coat_roughness_input, principled, "Coat Roughness", ntree, column, &context);
   }
 
