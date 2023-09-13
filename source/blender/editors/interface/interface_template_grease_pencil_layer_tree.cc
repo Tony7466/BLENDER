bff8bc64140325ea26eb02352d3456f3c9278ed2
Pratik Borhade <pratikborhade302@gmail.com>
2023-09-13 21:13:44 +0530

    Revert "Always send updates after execution"
    This reverts commit b450b2dc053a0cdd7d8418ce175e81d990291e6c.

--- source/blender/editors/interface/interface_template_grease_pencil_layer_tree.cc
+++ source/blender/editors/interface/interface_template_grease_pencil_layer_tree.cc
@@ -84,6 +84,7 @@ class LayerNodeDropTarget : public TreeViewItemDropTarget {
 
     LayerGroup &drag_parent = drag_layer.parent_group();
     LayerGroup *drop_parent_group = drop_tree_node_.parent_group();
+    bool is_dropped = false;
     if (!drop_parent_group) {
       /* Root node is not added to the tree view, so there should never be a drop target for this.
        */
@@ -104,22 +105,27 @@ class LayerNodeDropTarget : public TreeViewItemDropTarget {
         LayerGroup &drop_group = drop_tree_node_.as_group();
         drag_parent.unlink_node(&drag_layer.as_node());
         drop_group.add_layer(&drag_layer);
-        return true;
+        is_dropped = true;
+        break;
       }
-      case DropLocation::Before:
+      case DropLocation::Before: {
         drag_parent.unlink_node(&drag_layer.as_node());
         /* Draw order is inverted, so inserting before means inserting below. */
         drop_parent_group->add_layer_after(&drag_layer, &drop_tree_node_);
-        return true;
-      case DropLocation::After:
+        is_dropped = true;
+        break;
+      }
+      case DropLocation::After: {
         drag_parent.unlink_node(&drag_layer.as_node());
         /* Draw order is inverted, so inserting after means inserting above. */
         drop_parent_group->add_layer_before(&drag_layer, &drop_tree_node_);
-        return true;
+        is_dropped = true;
+        break;
+      }
     }
-
     WM_event_add_notifier(C, NC_GPENCIL | NA_EDITED, nullptr);
-    return false;
+
+    return is_dropped;
   }
 };
 
