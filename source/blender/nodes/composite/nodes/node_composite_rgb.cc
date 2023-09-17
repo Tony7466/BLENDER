998b0649e4d8d675b7be6c26aa97d104c4599034
Gangneron <clementgangneron@gmail.com>
2023-09-17 11:15:12 +0200

    Rename RGB to Color
    This patch replaces RGB and RGBA with Color as indicated in Pr D14202 and as requested in #96219.

--- source/blender/nodes/composite/nodes/node_composite_rgb.cc
+++ source/blender/nodes/composite/nodes/node_composite_rgb.cc
@@ -1,4 +1,4 @@
-/* SPDX-FileCopyrightText: 2006 Blender Foundation
+/* SPDX-FileCopyrightText: 2006 Blender Authors
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
@@ -20,7 +20,7 @@ namespace blender::nodes::node_composite_rgb_cc {
 
 static void cmp_node_rgb_declare(NodeDeclarationBuilder &b)
 {
-  b.add_output<decl::Color>("RGBA").default_value({0.5f, 0.5f, 0.5f, 1.0f});
+  b.add_output<decl::Color>("Color").default_value({0.5f, 0.5f, 0.5f, 1.0f});
 }
 
 using namespace blender::realtime_compositor;
@@ -31,7 +31,7 @@ class RGBOperation : public NodeOperation {
 
   void execute() override
   {
-    Result &result = get_result("RGBA");
+    Result &result = get_result("Color");
     result.allocate_single_value();
 
     const bNodeSocket *socket = static_cast<const bNodeSocket *>(bnode().outputs.first);
@@ -54,7 +54,7 @@ void register_node_type_cmp_rgb()
 
   static bNodeType ntype;
 
-  cmp_node_type_base(&ntype, CMP_NODE_RGB, "RGB", NODE_CLASS_INPUT);
+  cmp_node_type_base(&ntype, CMP_NODE_RGB, "Color", NODE_CLASS_INPUT);
   ntype.declare = file_ns::cmp_node_rgb_declare;
   blender::bke::node_type_size_preset(&ntype, blender::bke::eNodeSizePreset::DEFAULT);
   ntype.get_compositor_operation = file_ns::get_compositor_operation;
