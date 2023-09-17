634a426e90d5118cff920c2bfbff5e9b713e9aa7
Gangneron <clementgangneron@gmail.com>
2023-09-17 11:22:12 +0200

    Rename RGB to Color for shader node
    the input part of the node shader has been renamed Color instead of RGB.

--- source/blender/nodes/shader/nodes/node_shader_rgb.cc
+++ source/blender/nodes/shader/nodes/node_shader_rgb.cc
@@ -1,4 +1,4 @@
-/* SPDX-FileCopyrightText: 2005 Blender Foundation
+/* SPDX-FileCopyrightText: 2005 Blender Authors
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
@@ -34,7 +34,7 @@ void register_node_type_sh_rgb()
 
   static bNodeType ntype;
 
-  sh_node_type_base(&ntype, SH_NODE_RGB, "RGB", NODE_CLASS_INPUT);
+  sh_node_type_base(&ntype, SH_NODE_RGB, "Color", NODE_CLASS_INPUT);
   ntype.declare = file_ns::node_declare;
   ntype.gpu_fn = file_ns::gpu_shader_rgb;
 
