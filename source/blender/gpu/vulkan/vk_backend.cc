35a515e974a6bd184e0e2c32444f685c05ba67db
Jeroen Bakker <jeroen@blender.org>
2023-09-18 08:43:45 +0200

    Submit after each compute dispatch.

--- source/blender/gpu/vulkan/vk_backend.cc
+++ source/blender/gpu/vulkan/vk_backend.cc
@@ -107,6 +107,7 @@ void VKBackend::compute_dispatch(int groups_x_len, int groups_y_len, int groups_
   context.bind_compute_pipeline();
   VKCommandBuffer &command_buffer = context.command_buffer_get();
   command_buffer.dispatch(groups_x_len, groups_y_len, groups_z_len);
+  command_buffer.submit();
 }
 
 void VKBackend::compute_dispatch_indirect(StorageBuf *indirect_buf)
@@ -118,6 +119,7 @@ void VKBackend::compute_dispatch_indirect(StorageBuf *indirect_buf)
   VKStorageBuffer &indirect_buffer = *unwrap(indirect_buf);
   VKCommandBuffer &command_buffer = context.command_buffer_get();
   command_buffer.dispatch(indirect_buffer);
+  command_buffer.submit();
 }
 
 Context *VKBackend::context_alloc(void *ghost_window, void *ghost_context)
@@ -231,9 +233,7 @@ void VKBackend::capabilities_init(VKDevice &device)
   GCaps.max_textures = limits.maxDescriptorSetSampledImages;
   GCaps.max_textures_vert = limits.maxPerStageDescriptorSampledImages;
   GCaps.max_textures_geom = limits.maxPerStageDescriptorSampledImages;
-  /* Eevee next reorder textures based on this number, but vulkan uses this also to offset images.
-   * TODO: change the state manager to use none-indexed bindings. */
-  GCaps.max_textures_frag = min_ii(limits.maxPerStageDescriptorSampledImages, 12);
+  GCaps.max_textures_frag = limits.maxPerStageDescriptorSampledImages;
   GCaps.max_samplers = limits.maxSamplerAllocationCount;
   for (int i = 0; i < 3; i++) {
     GCaps.max_work_group_count[i] = limits.maxComputeWorkGroupCount[i];
