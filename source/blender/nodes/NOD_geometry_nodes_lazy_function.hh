21db031ed5dc07576d614e1afdcc4723c02ba7f1
Jacques Lucke <jacques@blender.org>
2023-09-20 10:18:13 +0200

    improve comment

--- source/blender/nodes/NOD_geometry_nodes_lazy_function.hh
+++ source/blender/nodes/NOD_geometry_nodes_lazy_function.hh
@@ -131,8 +131,8 @@ class GeoNodesSimulationParams {
 struct GeoNodesSideEffectNodes {
   MultiValueMap<ComputeContextHash, const lf::FunctionNode *> nodes_by_context;
   /**
-   * The repeat zone is identified by the compute context and the identifier of the repeat output
-   * node.
+   * The repeat zone is identified by the compute context of the parent and the identifier of the
+   * repeat output node.
    */
   MultiValueMap<std::pair<ComputeContextHash, int32_t>, int> iterations_by_repeat_zone;
 };
