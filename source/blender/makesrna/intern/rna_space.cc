d2f4a398b9e6c755ba9b15ff83ae2c55d2a0ac05
ChengduLittleA <xp8110@outlook.com>
2023-09-21 10:11:24 +0800

    Use "or" instead of "and" phrase

--- source/blender/makesrna/intern/rna_space.cc
+++ source/blender/makesrna/intern/rna_space.cc
@@ -6861,7 +6861,7 @@ static void rna_def_fileselect_params(BlenderRNA *brna)
   prop = RNA_def_property(srna, "filter_search", PROP_STRING, PROP_NONE);
   RNA_def_property_string_sdna(prop, nullptr, "filter_search");
   RNA_def_property_ui_text(
-      prop, "Name/Tag Filter", "Filter by name and tag, supports '*' wildcard");
+      prop, "Name or Tag Filter", "Filter by name or tag, supports '*' wildcard");
   RNA_def_property_flag(prop, PROP_TEXTEDIT_UPDATE);
   RNA_def_property_update(prop, NC_SPACE | ND_SPACE_FILE_LIST, nullptr);
 
