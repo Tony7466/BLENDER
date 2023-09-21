70c1d1e9bb550bca9e9cc645f5e82b626ce8b43d
Nathan Vegdahl <cessen@cessen.com>
2023-09-21 14:19:41 +0200

    Clang format

--- source/blender/editors/space_graph/graph_select.cc
+++ source/blender/editors/space_graph/graph_select.cc
@@ -2197,8 +2197,7 @@ void GRAPH_OT_select_key_handles(wmOperatorType *ot)
   /* identifiers */
   ot->name = "Select Key / Handles";
   ot->idname = "GRAPH_OT_select_key_handles";
-  ot->description =
-      "Select/deselect any combination of the key and handles of selected keyframes";
+  ot->description = "Select/deselect any combination of the key and handles of selected keyframes";
 
   /* callbacks */
   ot->poll = graphop_visible_keyframes_poll;
