0200e2d9ff43ff152a6e0bdabfccb331f2c91267
Jacques Lucke <jacques@blender.org>
2023-09-21 12:33:51 +0200

    dynamic number of gizmos

--- source/blender/editors/space_view3d/view3d_gizmo_geometry_nodes.cc
+++ source/blender/editors/space_view3d/view3d_gizmo_geometry_nodes.cc
@@ -11,8 +11,10 @@
 #include "WM_types.hh"
 
 #include "DNA_modifier_types.h"
+#include "DNA_node_types.h"
 
 #include "BKE_context.h"
+#include "BKE_node_runtime.hh"
 
 #include "BLI_math_matrix.h"
 #include "BLI_math_vector.h"
@@ -39,55 +41,78 @@ static bool WIDGETGROUP_geometry_nodes_poll(const bContext *C, wmGizmoGroupType 
 
 static void WIDGETGROUP_geometry_nodes_setup(const bContext * /*C*/, wmGizmoGroup *gzgroup)
 {
-  std::cout << __func__ << "\n";
   GeometryNodesGizmoGroup *gzgroup_data = MEM_new<GeometryNodesGizmoGroup>(__func__);
   gzgroup->customdata = gzgroup_data;
   gzgroup->customdata_free = [](void *data) {
     auto *gzgroup_data = static_cast<GeometryNodesGizmoGroup *>(data);
     MEM_delete(gzgroup_data);
   };
+}
 
-  for ([[maybe_unused]] const int i : IndexRange(5)) {
-    wmGizmo *gz = WM_gizmo_new("GIZMO_GT_arrow_3d", gzgroup, nullptr);
-    copy_v4_fl4(gz->color, 1.0f, 0.0f, 0.0f, 1.0f);
-    copy_v4_fl4(gz->color_hi, 0.0f, 1.0f, 0.0f, 1.0f);
-    gzgroup_data->gizmos.append(gz);
+static void ensure_gizmo_amount(const bContext *C, wmGizmoGroup *gzgroup, const int new_gizmo_num)
+{
+  GeometryNodesGizmoGroup *gzgroup_data = static_cast<GeometryNodesGizmoGroup *>(
+      gzgroup->customdata);
+  if (new_gizmo_num < gzgroup_data->gizmos.size()) {
+    for (const int i : gzgroup_data->gizmos.index_range().drop_front(new_gizmo_num)) {
+      WM_gizmo_unlink(&gzgroup->gizmos,
+                      gzgroup->parent_gzmap,
+                      gzgroup_data->gizmos[i],
+                      const_cast<bContext *>(C));
+    }
+    gzgroup_data->gizmos.resize(new_gizmo_num);
+    gzgroup_data->gizmo_values.resize(new_gizmo_num);
   }
-  gzgroup_data->gizmo_values.resize(gzgroup_data->gizmos.size(), 0.0f);
+  else if (new_gizmo_num > gzgroup_data->gizmos.size()) {
+    for (const int i : IndexRange(new_gizmo_num).drop_front(gzgroup_data->gizmos.size())) {
+      wmGizmo *gz = WM_gizmo_new("GIZMO_GT_arrow_3d", gzgroup, nullptr);
+      gzgroup_data->gizmo_values.append(0.0f);
+      gzgroup_data->gizmos.append(gz);
+      copy_v4_fl4(gz->color, 1.0f, 0.0f, 0.0f, 1.0f);
+      copy_v4_fl4(gz->color_hi, 0.0f, 1.0f, 0.0f, 1.0f);
 
-  for (const int i : gzgroup_data->gizmos.index_range()) {
-    wmGizmo *gz = gzgroup_data->gizmos[i];
-    float *value = &gzgroup_data->gizmo_values[i];
-    // wmGizmoProperty *gz_prop = WM_gizmo_target_property_find(gz, "offset");
+      struct UserData {
+        GeometryNodesGizmoGroup *gzgroup_data;
+        int index;
+      } *user_data = MEM_new<UserData>(__func__);
+      user_data->gzgroup_data = gzgroup_data;
+      user_data->index = i;
 
-    struct UserData {
-      GeometryNodesGizmoGroup *gzgroup_data;
-      int index;
-    } *user_data = MEM_new<UserData>(__func__);
-    user_data->gzgroup_data = gzgroup_data;
-    user_data->index = i;
+      wmGizmoPropertyFnParams params{};
+      params.user_data = user_data;
+      params.free_fn = [](const wmGizmo * /*gz*/, wmGizmoProperty *gz_prop) {
+        MEM_delete(static_cast<UserData *>(gz_prop->custom_func.user_data));
+      };
+      params.value_set_fn =
+          [](const wmGizmo *gz, wmGizmoProperty *gz_prop, const void *value_ptr) {
+            UserData *user_data = static_cast<UserData *>(gz_prop->custom_func.user_data);
+            user_data->gzgroup_data->gizmo_values[user_data->index] = *static_cast<const float *>(
+                value_ptr);
+          };
+      params.value_get_fn = [](const wmGizmo *gz, wmGizmoProperty *gz_prop, void *value_ptr) {
+        UserData *user_data = static_cast<UserData *>(gz_prop->custom_func.user_data);
+        *static_cast<float *>(value_ptr) = user_data->gzgroup_data->gizmo_values[user_data->index];
+      };
+      WM_gizmo_target_property_def_func(gz, "offset", &params);
+    }
+  }
+}
 
-    wmGizmoPropertyFnParams params{};
-    params.user_data = user_data;
-    params.free_fn = [](const wmGizmo * /*gz*/, wmGizmoProperty *gz_prop) {
-      MEM_delete(static_cast<UserData *>(gz_prop->custom_func.user_data));
-    };
-    params.value_set_fn = [](const wmGizmo *gz, wmGizmoProperty *gz_prop, const void *value_ptr) {
-      UserData *user_data = static_cast<UserData *>(gz_prop->custom_func.user_data);
-      user_data->gzgroup_data->gizmo_values[user_data->index] = *static_cast<const float *>(
-          value_ptr);
-    };
-    params.value_get_fn = [](const wmGizmo *gz, wmGizmoProperty *gz_prop, void *value_ptr) {
-      UserData *user_data = static_cast<UserData *>(gz_prop->custom_func.user_data);
-      *static_cast<float *>(value_ptr) = user_data->gzgroup_data->gizmo_values[user_data->index];
-    };
-    WM_gizmo_target_property_def_func(gz, "offset", &params);
+static void WIDGETGROUP_geometry_nodes_refresh(const bContext *C, wmGizmoGroup *gzgroup)
+{
+  Object *ob = CTX_data_active_object(C);
+  LISTBASE_FOREACH (ModifierData *, md, &ob->modifiers) {
+    if (md->type != eModifierType_Nodes) {
+      continue;
+    }
+    NodesModifierData &nmd = *reinterpret_cast<NodesModifierData *>(md);
+    const int num = nmd.node_group ? nmd.node_group->runtime->nodes_by_id.size() : 0;
+    ensure_gizmo_amount(C, gzgroup, num);
   }
 }
 
 static void WIDGETGROUP_geometry_nodes_draw_prepare(const bContext *C, wmGizmoGroup *gzgroup)
 {
-  std::cout << __func__ << "\n";
   GeometryNodesGizmoGroup *gzgroup_data = static_cast<GeometryNodesGizmoGroup *>(
       gzgroup->customdata);
   Object *ob = CTX_data_active_object(C);
@@ -112,6 +137,6 @@ void VIEW3D_GGT_geometry_nodes(wmGizmoGroupType *gzgt)
   gzgt->poll = WIDGETGROUP_geometry_nodes_poll;
   gzgt->setup = WIDGETGROUP_geometry_nodes_setup;
   gzgt->setup_keymap = WM_gizmogroup_setup_keymap_generic_maybe_drag;
-  gzgt->refresh = nullptr;
+  gzgt->refresh = WIDGETGROUP_geometry_nodes_refresh;
   gzgt->draw_prepare = WIDGETGROUP_geometry_nodes_draw_prepare;
 }
