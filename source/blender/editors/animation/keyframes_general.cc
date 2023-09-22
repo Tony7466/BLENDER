5e4af9861cdc8b42b7f844a66aa7267c42d24449
Christoph Lendenfeld <chris.lenden@gmail.com>
2023-09-22 13:21:16 +0200

    use insertkey_fast

--- source/blender/editors/animation/keyframes_general.cc
+++ source/blender/editors/animation/keyframes_general.cc
@@ -1289,7 +1289,7 @@ void bake_fcurve(FCurve *fcu,
 
   for (int i = 0; i < sample_count; i++) {
     insert_vert_fcurve(
-        fcu, range[0] + i * step, samples[i], BEZT_KEYTYPE_KEYFRAME, INSERTKEY_NOFLAGS);
+        fcu, range[0] + i * step, samples[i], BEZT_KEYTYPE_KEYFRAME, INSERTKEY_FAST);
   }
   MEM_freeN(samples);
   BKE_fcurve_handles_recalc(fcu);
