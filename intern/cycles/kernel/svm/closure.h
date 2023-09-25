Tony7466/BLENDER

commit 158dbc1b101b5c94596d21845dc0ad31bfdbc5b1

Cycles: Rework Principled BSDF Clearcoat
- Adds tint control, which simulates volumetric absorption inside the coating.
  This results in angle-dependent saturation and affects all underlying layers
  (diffuse, subsurface, metallic, transmission). It provides a physically-based
  alternative to ad-hoc effects such as tinted specular highlights.
- Renames the component from "Clearcoat" to "Coat", since it's no longer
  necessarily clear now. This matches naming in e.g. other renderers or OpenPBR.
- Adds an explicit Coat IOR input, in preparation for future smarter IOR logic
  around the interaction between Coat and main IOR. This used to be hardcoded
  to 1.5.
- Removes hardcoded 0.25 weight multiplier, and adds versioning code to update
  existing files accordingly. OBJ import/export still applies the factor.
- Replaces the GTR1 microfacet component with regular GGX. This removes a corner
  case in the Microfacet code, solves #53038, and makes us more consistent with
  other standard surface shaders. The original Disney BSDF used GTR1, but it
  doesn't appear that it caught on in the industry.

Co-authored-by: Weizhen Huang <weizhen@blender.org>
Pull Request: https://projects.blender.org/blender/blender/pulls/110993

intern/cycles/kernel/svm/closure.h
@@ -69,8 +69,9 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
   switch (type) {
     case CLOSURE_BSDF_PRINCIPLED_ID: {
       uint specular_offset, roughness_offset, specular_tint_offset, anisotropic_offset,
-          sheen_offset, sheen_tint_offset, clearcoat_offset, clearcoat_roughness_offset,
-          eta_offset, transmission_offset, anisotropic_rotation_offset, pad1;
+          sheen_offset, sheen_tint_offset, sheen_roughness_offset, coat_offset,
+          coat_roughness_offset, coat_ior_offset, eta_offset, transmission_offset,
+          anisotropic_rotation_offset, coat_tint_offset, dummy;
       uint4 data_node2 = read_node(kg, &offset);
 
       float3 T = stack_load_float3(stack, data_node.y);
@@ -79,13 +80,12 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
                              &roughness_offset,
                              &specular_tint_offset,
                              &anisotropic_offset);
-      svm_unpack_node_uchar4(data_node.w,
-                             &sheen_offset,
-                             &sheen_tint_offset,
-                             &clearcoat_offset,
-                             &clearcoat_roughness_offset);
       svm_unpack_node_uchar4(
-          data_node2.x, &eta_offset, &transmission_offset, &anisotropic_rotation_offset, &pad1);
+          data_node.w, &sheen_offset, &sheen_tint_offset, &sheen_roughness_offset, &dummy);
+      svm_unpack_node_uchar4(
+          data_node2.x, &eta_offset, &transmission_offset, &anisotropic_rotation_offset, &dummy);
+      svm_unpack_node_uchar4(
+          data_node2.w, &coat_offset, &coat_roughness_offset, &coat_ior_offset, &coat_tint_offset);
 
       // get Disney principled parameters
       float metallic = saturatef(param1);
@@ -96,9 +96,11 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
       float anisotropic = stack_load_float(stack, anisotropic_offset);
       float sheen = stack_load_float(stack, sheen_offset);
       float3 sheen_tint = stack_load_float3(stack, sheen_tint_offset);
-      float sheen_roughness = stack_load_float(stack, data_node2.w);
-      float clearcoat = stack_load_float(stack, clearcoat_offset);
-      float clearcoat_roughness = stack_load_float(stack, clearcoat_roughness_offset);
+      float sheen_roughness = stack_load_float(stack, sheen_roughness_offset);
+      float coat = stack_load_float(stack, coat_offset);
+      float coat_roughness = stack_load_float(stack, coat_roughness_offset);
+      float coat_ior = fmaxf(stack_load_float(stack, coat_ior_offset), 1.0f);
+      float3 coat_tint = stack_load_float3(stack, coat_tint_offset);
       float transmission = saturatef(stack_load_float(stack, transmission_offset));
       float anisotropic_rotation = stack_load_float(stack, anisotropic_rotation_offset);
       float eta = fmaxf(stack_load_float(stack, eta_offset), 1e-5f);
@@ -116,12 +118,11 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
                                           __uint_as_float(data_base_color.z),
                                           __uint_as_float(data_base_color.w));
 
-      // get the additional clearcoat normal and subsurface scattering radius
+      // get the additional coat normal and subsurface scattering radius
       uint4 data_cn_ssr = read_node(kg, &offset);
-      float3 clearcoat_normal = stack_valid(data_cn_ssr.x) ?
-                                    stack_load_float3(stack, data_cn_ssr.x) :
-                                    sd->N;
-      clearcoat_normal = maybe_ensure_valid_specular_reflection(sd, clearcoat_normal);
+      float3 coat_normal = stack_valid(data_cn_ssr.x) ? stack_load_float3(stack, data_cn_ssr.x) :
+                                                        sd->N;
+      coat_normal = maybe_ensure_valid_specular_reflection(sd, coat_normal);
       float3 subsurface_radius = stack_valid(data_cn_ssr.y) ?
                                      stack_load_float3(stack, data_cn_ssr.y) :
                                      one_float3();
@@ -175,29 +176,56 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
 
           /* Attenuate lower layers */
           Spectrum albedo = bsdf_albedo(kg, sd, (ccl_private ShaderClosure *)bsdf, true, false);
-          weight *= 1.0f - reduce_max(albedo / weight);
+          weight *= 1.0f - reduce_max(safe_divide_color(albedo, weight));
         }
       }
 
-      /* Second layer: Clearcoat */
-      if (reflective_caustics && clearcoat > CLOSURE_WEIGHT_CUTOFF) {
+      /* Second layer: Coat */
+      if (reflective_caustics && coat > CLOSURE_WEIGHT_CUTOFF) {
         ccl_private MicrofacetBsdf *bsdf = (ccl_private MicrofacetBsdf *)bsdf_alloc(
-            sd, sizeof(MicrofacetBsdf), 0.25f * clearcoat * weight);
+            sd, sizeof(MicrofacetBsdf), coat * weight);
 
         if (bsdf) {
-          bsdf->N = clearcoat_normal;
+          bsdf->N = coat_normal;
           bsdf->T = zero_float3();
-          bsdf->ior = 1.5f;
+          bsdf->ior = coat_ior;
 
-          bsdf->alpha_x = bsdf->alpha_y = sqr(clearcoat_roughness);
+          bsdf->alpha_x = bsdf->alpha_y = sqr(coat_roughness);
 
           /* setup bsdf */
-          sd->flag |= bsdf_microfacet_ggx_clearcoat_setup(kg, bsdf, sd);
+          sd->flag |= bsdf_microfacet_ggx_setup(bsdf);
+          bsdf_microfacet_setup_fresnel_dielectric(kg, bsdf, sd);
 
           /* Attenuate lower layers */
           Spectrum albedo = bsdf_albedo(kg, sd, (ccl_private ShaderClosure *)bsdf, true, false);
-          weight *= 1.0f - reduce_max(albedo / weight);
+          weight *= 1.0f - reduce_max(safe_divide_color(albedo, weight));
         }
+      }
+
+      if (coat > CLOSURE_WEIGHT_CUTOFF && !isequal(coat_tint, one_float3())) {
+        /* Tint is normalized to perpendicular incidence.
+         * Therefore, if we define the coat thickness as length 1, the length along the ray is
+         * t = sqrt(1+tan^2(angle(N, I))) = sqrt(1+tan^2(acos(dotNI))) = 1 / dotNI.
+         * From Beer's law, we have T = exp(-sigma_e * t).
+         * Therefore, tint = exp(-sigma_e * 1) (per def.), so -sigma_e = log(tint).
+         * From this, T = exp(log(tint) * t) = exp(log(tint)) ^ t = tint ^ t;
+         *
+         * Note that this is only an approximation - it assumes that the outgoing ray
+         * follows the same angle, and that there aren't multiple internal bounces.
+         * In particular, things that could be improved:
+         * - For transmissive materials, there should not be an outgoing path at all if the path
+         *   is transmitted.
+         * - For rough materials, we could blend towards a view-independent average path length
+         *   (e.g. 2 for diffuse reflection) for the outgoing direction.
+         * However, there's also an argument to be made for keeping parameters independent of each
+         * other for more intuitive control, in particular main roughness not affecting the coat.
+         */
+        float cosNI = dot(sd->wi, coat_normal);
+        /* Refract incoming direction into coat material.
+         * TIR is no concern here since we're always coming from the outside. */
+        float cosNT = sqrtf(1.0f - sqr(1.0f / coat_ior) * (1 - sqr(cosNI)));
+        float optical_depth = 1.0f / cosNT;
+        weight *= power(rgb_to_spectrum(coat_tint), coat * optical_depth);
       }
 
       /* Metallic component */
@@ -295,7 +323,7 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
 
           /* Attenuate lower layers */
           Spectrum albedo = bsdf_albedo(kg, sd, (ccl_private ShaderClosure *)bsdf, true, false);
-          weight *= 1.0f - reduce_max(albedo / weight);
+          weight *= 1.0f - reduce_max(safe_divide_color(albedo, weight));
         }
       }
 

intern/cycles/hydra/material.cpp
@@ -139,7 +139,7 @@ class UsdToCycles {
           {TfToken("diffuseColor"), ustring("base_color")},
           {TfToken("emissiveColor"), ustring("emission")},
           {TfToken("specularColor"), ustring("specular")},
-          {TfToken("clearcoatRoughness"), ustring("clearcoat_roughness")},
+          {TfToken("clearcoatRoughness"), ustring("coat_roughness")},
           {TfToken("opacity"), ustring("alpha")},
           // opacityThreshold
           // occlusion

intern/cycles/kernel/closure/bsdf.h
@@ -153,7 +153,6 @@ ccl_device_inline int bsdf_sample(KernelGlobals kg,
       *eta = 1.0f;
       break;
     case CLOSURE_BSDF_MICROFACET_GGX_ID:
-    case CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_GLASS_ID:
       label = bsdf_microfacet_ggx_sample(
@@ -284,7 +283,6 @@ ccl_device_inline void bsdf_roughness_eta(const KernelGlobals kg,
       *eta = 1.0f;
       break;
     case CLOSURE_BSDF_MICROFACET_GGX_ID:
-    case CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_GLASS_ID:
     case CLOSURE_BSDF_MICROFACET_BECKMANN_ID:
@@ -385,7 +383,6 @@ ccl_device_inline int bsdf_label(const KernelGlobals kg,
       label = LABEL_TRANSMIT | LABEL_TRANSPARENT;
       break;
     case CLOSURE_BSDF_MICROFACET_GGX_ID:
-    case CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID:
     case CLOSURE_BSDF_MICROFACET_BECKMANN_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID:
     case CLOSURE_BSDF_MICROFACET_BECKMANN_REFRACTION_ID:
@@ -484,7 +481,6 @@ ccl_device_inline
       eval = bsdf_transparent_eval(sc, sd->wi, wo, pdf);
       break;
     case CLOSURE_BSDF_MICROFACET_GGX_ID:
-    case CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_GLASS_ID:
       eval = bsdf_microfacet_ggx_eval(sc, Ng, sd->wi, wo, pdf);
@@ -555,7 +551,6 @@ ccl_device void bsdf_blur(KernelGlobals kg, ccl_private ShaderClosure *sc, float
 #if defined(__SVM__) || defined(__OSL__)
   switch (sc->type) {
     case CLOSURE_BSDF_MICROFACET_GGX_ID:
-    case CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_REFRACTION_ID:
     case CLOSURE_BSDF_MICROFACET_GGX_GLASS_ID:
     case CLOSURE_BSDF_MICROFACET_BECKMANN_ID:

intern/cycles/kernel/closure/bsdf_microfacet.h
@@ -377,17 +377,6 @@ ccl_device Spectrum bsdf_microfacet_estimate_albedo(KernelGlobals kg,
   return albedo;
 }
 
-/* Generalized Trowbridge-Reitz for clearcoat. */
-ccl_device_forceinline float bsdf_clearcoat_D(float alpha2, float cos_NH)
-{
-  if (alpha2 >= 1.0f) {
-    return M_1_PI_F;
-  }
-
-  const float t = 1.0f + (alpha2 - 1.0f) * cos_NH * cos_NH;
-  return (alpha2 - 1.0f) / (M_PI_F * logf(alpha2) * t);
-}
-
 /* Smith shadowing-masking term, here in the non-separable form.
  * For details, see:
  * Understanding the Masking-Shadowing Function in Microfacet-Based BRDFs.
@@ -529,18 +518,7 @@ ccl_device Spectrum bsdf_microfacet_eval(ccl_private const ShaderClosure *sc,
    * harder to compute. */
   if (alpha_x == alpha_y || is_transmission) { /* Isotropic. */
     float alpha2 = alpha_x * alpha_y;
-
-    if (bsdf->type == CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID) {
-      D = bsdf_clearcoat_D(alpha2, cos_NH);
-
-      /* The masking-shadowing term for clearcoat has a fixed alpha of 0.25
-       * => alpha2 = 0.25 * 0.25 */
-      alpha2 = 0.0625f;
-    }
-    else {
-      D = bsdf_D<m_type>(alpha2, cos_NH);
-    }
-
+    D = bsdf_D<m_type>(alpha2, cos_NH);
     lambdaI = bsdf_lambda<m_type>(alpha2, cos_NI);
     lambdaO = bsdf_lambda<m_type>(alpha2, cos_NO);
   }
@@ -687,17 +665,7 @@ ccl_device int bsdf_microfacet_sample(ccl_private const ShaderClosure *sc,
       const float cos_NH = dot(N, H);
       const float cos_NO = dot(N, *wo);
 
-      if (bsdf->type == CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID) {
-        D = bsdf_clearcoat_D(alpha2, cos_NH);
-
-        /* The masking-shadowing term for clearcoat has a fixed alpha of 0.25
-         * => alpha2 = 0.25 * 0.25 */
-        alpha2 = 0.0625f;
-      }
-      else {
-        D = bsdf_D<m_type>(alpha2, cos_NH);
-      }
-
+      D = bsdf_D<m_type>(alpha2, cos_NH);
       lambdaO = bsdf_lambda<m_type>(alpha2, cos_NO);
       lambdaI = bsdf_lambda<m_type>(alpha2, cos_NI);
     }
@@ -831,6 +799,14 @@ ccl_device void bsdf_microfacet_setup_fresnel_constant(KernelGlobals kg,
   microfacet_ggx_preserve_energy(kg, bsdf, sd, color);
 }
 
+ccl_device void bsdf_microfacet_setup_fresnel_dielectric(KernelGlobals kg,
+                                                         ccl_private MicrofacetBsdf *bsdf,
+                                                         ccl_private const ShaderData *sd)
+{
+  bsdf->fresnel_type = MicrofacetFresnel::DIELECTRIC;
+  bsdf->sample_weight *= average(bsdf_microfacet_estimate_albedo(kg, sd, bsdf, true, true));
+}
+
 /* GGX microfacet with Smith shadow-masking from:
  *
  * Microfacet Models for Refraction through Rough Surfaces
@@ -852,21 +828,6 @@ ccl_device int bsdf_microfacet_ggx_setup(ccl_private MicrofacetBsdf *bsdf)
   bsdf->fresnel_type = MicrofacetFresnel::NONE;
   bsdf->energy_scale = 1.0f;
   bsdf->type = CLOSURE_BSDF_MICROFACET_GGX_ID;
-
-  return SD_BSDF | bsdf_microfacet_eval_flag(bsdf);
-}
-
-ccl_device int bsdf_microfacet_ggx_clearcoat_setup(KernelGlobals kg,
-                                                   ccl_private MicrofacetBsdf *bsdf,
-                                                   ccl_private const ShaderData *sd)
-{
-  bsdf->alpha_x = saturatef(bsdf->alpha_x);
-  bsdf->alpha_y = bsdf->alpha_x;
-
-  bsdf->fresnel_type = MicrofacetFresnel::DIELECTRIC;
-  bsdf->energy_scale = 1.0f;
-  bsdf->type = CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID;
-  bsdf->sample_weight *= average(bsdf_microfacet_estimate_albedo(kg, sd, bsdf, true, true));
 
   return SD_BSDF | bsdf_microfacet_eval_flag(bsdf);
 }

intern/cycles/kernel/osl/closures_setup.h
@@ -420,10 +420,6 @@ ccl_device void osl_closure_microfacet_setup(KernelGlobals kg,
   else if (closure->distribution == make_string("ashikhmin_shirley", 11318482998918370922ull)) {
     sd->flag |= bsdf_ashikhmin_shirley_setup(bsdf);
   }
-  /* Clearcoat */
-  else if (closure->distribution == make_string("clearcoat", 3490136178980547276ull)) {
-    sd->flag |= bsdf_microfacet_ggx_clearcoat_setup(kg, bsdf, sd);
-  }
   /* GGX (either single- or multi-scattering) */
   else {
     if (closure->refract == 1) {

intern/cycles/kernel/osl/osl.h
@@ -140,7 +140,7 @@ ccl_device void flatten_closure_tree(KernelGlobals kg,
       if (stack_size == layer_stack_level) {
         /* We just finished processing the top layers of a Layer closure, so adjust the weight to
          * account for the layering. */
-        weight *= saturatef(1.0f - reduce_max(layer_albedo / weight));
+        weight *= saturatef(1.0f - reduce_max(safe_divide_color(layer_albedo, weight)));
         layer_stack_level = -1;
         if (is_zero(weight)) {
           /* If it's fully occluded, skip the base layer we just popped from the stack and grab

intern/cycles/kernel/osl/shaders/node_principled_bsdf.osl
@@ -22,12 +22,14 @@ shader node_principled_bsdf(string distribution = "multi_ggx",
                             float Sheen = 0.0,
                             float SheenRoughness = 0.5,
                             color SheenTint = 0.5,
-                            float Clearcoat = 0.0,
-                            float ClearcoatRoughness = 0.03,
+                            float Coat = 0.0,
+                            float CoatRoughness = 0.03,
+                            float CoatIOR = 1.5,
+                            color CoatTint = color(1.0, 1.0, 1.0),
                             float IOR = 1.45,
                             float Transmission = 0.0,
                             normal Normal = N,
-                            normal ClearcoatNormal = N,
+                            normal CoatNormal = N,
                             normal Tangent = normalize(dPdu),
                             output closure color BSDF = 0)
 {
@@ -97,10 +99,18 @@ shader node_principled_bsdf(string distribution = "multi_ggx",
     BSDF = mix(BSDF, MetallicBSDF, clamp(Metallic, 0.0, 1.0));
   }
 
-  if (Clearcoat > 1e-5) {
-    float clearcoat_r2 = ClearcoatRoughness * ClearcoatRoughness;
-    closure color ClearcoatBSDF = microfacet("clearcoat", ClearcoatNormal, clearcoat_r2, 1.5, 0);
-    BSDF = layer(0.25 * Clearcoat * ClearcoatBSDF, BSDF);
+  if (Coat > 1e-5) {
+    float coat_ior = max(CoatIOR, 1.0);
+    if (CoatTint != color(1.0)) {
+      float coat_neta = 1.0 / coat_ior;
+      float cosNI = dot(I, CoatNormal);
+      float cosNT = sqrt(1.0 - coat_neta * coat_neta * (1 - cosNI * cosNI));
+      BSDF *= pow(CoatTint, Coat / cosNT);
+    }
+    float coat_r2 = CoatRoughness * CoatRoughness;
+    closure color CoatBSDF = dielectric_bsdf(
+        CoatNormal, vector(0.0), color(1.0), color(0.0), coat_r2, coat_r2, coat_ior, "ggx");
+    BSDF = layer(Coat * CoatBSDF, BSDF);
   }
 
   if (Sheen > 1e-5) {

intern/cycles/kernel/svm/types.h
@@ -427,7 +427,6 @@ typedef enum ClosureType {
 
   /* Glossy */
   CLOSURE_BSDF_MICROFACET_GGX_ID,
-  CLOSURE_BSDF_MICROFACET_GGX_CLEARCOAT_ID,
   CLOSURE_BSDF_MICROFACET_BECKMANN_ID,
   CLOSURE_BSDF_MICROFACET_MULTI_GGX_ID, /* virtual closure */
   CLOSURE_BSDF_ASHIKHMIN_SHIRLEY_ID,

commit 29658cf7a2e6e9035131036c22e094f95dcfa64a

Cycles: oneAPI: fix perf regression by inlining svm_node_closure_bsdf
158dbc1b101b5c94596d21845dc0ad31bfdbc5b1 introduced a ~9% performance
regression on Intel GPUs, which we recover through this targeted change.

intern/cycles/kernel/svm/closure.h
@@ -29,13 +29,19 @@ ccl_device_inline int svm_node_closure_bsdf_skip(KernelGlobals kg, int offset, u
 }
 
 template<uint node_feature_mask, ShaderType shader_type>
-ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
-                                              ccl_private ShaderData *sd,
-                                              ccl_private float *stack,
-                                              Spectrum closure_weight,
-                                              uint4 node,
-                                              uint32_t path_flag,
-                                              int offset)
+#ifndef __KERNEL_ONEAPI__
+ccl_device_noinline
+#else
+ccl_device
+#endif
+    int
+    svm_node_closure_bsdf(KernelGlobals kg,
+                          ccl_private ShaderData *sd,
+                          ccl_private float *stack,
+                          Spectrum closure_weight,
+                          uint4 node,
+                          uint32_t path_flag,
+                          int offset)
 {
   uint type, param1_offset, param2_offset;
 