30c724d6158292f08dc7450781e96d83ec3f7df9
Michael Parkin-White <mparkinwhite@apple.com>
2023-09-28 16:00:08 +0100

    Remove ifdef in shadow_page_clear_comp

--- source/blender/draw/engines/eevee_next/shaders/eevee_shadow_page_clear_comp.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_shadow_page_clear_comp.glsl
@@ -16,10 +16,6 @@ void main()
   uvec3 page_co = shadow_page_unpack(page_packed);
   page_co.xy = page_co.xy * SHADOW_PAGE_RES + gl_GlobalInvocationID.xy;
 
-/* Clear to FLT_MAX instead of 1 so the far plane doesn't cast shadows onto farther objects. */
-#ifdef GPU_METAL
+  /* Clear to FLT_MAX instead of 1 so the far plane doesn't cast shadows onto farther objects. */
   imageStoreFast(shadow_atlas_img, ivec3(page_co), uvec4(floatBitsToUint(FLT_MAX)));
-#else
-  imageStore(shadow_atlas_img, ivec3(page_co), uvec4(floatBitsToUint(FLT_MAX)));
-#endif
 }
