117972c09d7c46fea987296c0b7eaf23f480523e
Jacques Lucke <jacques@blender.org>
2023-09-29 14:36:49 +0200

    support capture attribute node

--- source/blender/nodes/geometry/nodes/node_geo_attribute_capture.cc
+++ source/blender/nodes/geometry/nodes/node_geo_attribute_capture.cc
@@ -202,7 +202,8 @@ static void node_geo_exec(GeoNodeExecParams params)
   else {
     static const Array<GeometryComponent::Type> types = {GeometryComponent::Type::Mesh,
                                                          GeometryComponent::Type::PointCloud,
-                                                         GeometryComponent::Type::Curve};
+                                                         GeometryComponent::Type::Curve,
+                                                         GeometryComponent::Type::GreasePencil};
 
     geometry_set.modify_geometry_sets([&](GeometrySet &geometry_set) {
       for (const GeometryComponent::Type type : types) {
