22da46de8f5b0f15c541cac69e2e632b67f320e7
Jacques Lucke <jacques@blender.org>
2023-09-30 12:39:05 +0200

    initial working mesh separation

--- source/blender/nodes/geometry/nodes/node_geo_split_geometry_groups.cc
+++ source/blender/nodes/geometry/nodes/node_geo_split_geometry_groups.cc
@@ -4,18 +4,78 @@
 
 #include "node_geometry_util.hh"
 
+#include "GEO_mesh_copy_selection.hh"
+
+#include "BKE_instances.hh"
+
 namespace blender::nodes::node_geo_split_geometry_groups_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_input<decl::Geometry>("Geometry");
+  b.add_input<decl::Bool>("Selection").supports_field().hide_value().default_value(true);
   b.add_input<decl::Int>("Group ID").supports_field().hide_value();
-  b.add_output<decl::Geometry>("Geometry");
+  b.add_output<decl::Geometry>("Geometry").propagate_all();
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
-  params.set_default_remaining_outputs();
+  GeometrySet src_geometry = params.extract_input<GeometrySet>("Geometry");
+  const Field<bool> selection_field = params.extract_input<Field<bool>>("Selection");
+  const Field<int> group_id_field = params.extract_input<Field<int>>("Group ID");
+
+  const AnonymousAttributePropagationInfo &propagation_info = params.get_output_propagation_info(
+      "Geometry");
+
+  bke::Instances *dst_instances = new bke::Instances();
+  GeometrySet dst_geometry = GeometrySet::from_instances(dst_instances);
+
+  if (src_geometry.has_mesh()) {
+    const MeshComponent &component = *src_geometry.get_component<MeshComponent>();
+    const Mesh &src_mesh = *component.get();
+    const eAttrDomain domain = ATTR_DOMAIN_FACE;
+    const int domain_size = component.attribute_domain_size(domain);
+    const bke::MeshFieldContext field_context{src_mesh, domain};
+    FieldEvaluator field_evaluator{field_context, domain_size};
+    field_evaluator.set_selection(selection_field);
+    field_evaluator.add(group_id_field);
+    field_evaluator.evaluate();
+    const IndexMask selection = field_evaluator.get_evaluated_selection_as_mask();
+    const VArraySpan<int> group_ids = field_evaluator.get_evaluated<int>(0);
+
+    MultiValueMap<int, int> indices_by_group;
+    selection.foreach_index([&](const int i) { indices_by_group.add(group_ids[i], i); });
+
+    Array<bool> group_selection(domain_size, false);
+    const VArray<bool> group_selection_varray = VArray<bool>::ForSpan(group_selection);
+    for (auto item : indices_by_group.items()) {
+      const int group_id = item.key;
+      const Span<int> elements = item.value;
+      for (int i : elements) {
+        group_selection[i] = true;
+      }
+
+      std::optional<Mesh *> group_mesh_opt = geometry::mesh_copy_selection(
+          src_mesh, group_selection_varray, domain, propagation_info);
+      GeometrySet group_geometry;
+      if (group_mesh_opt.has_value()) {
+        if (Mesh *group_mesh = *group_mesh_opt) {
+          group_geometry = GeometrySet::from_mesh(group_mesh);
+        }
+      }
+      else {
+        group_geometry.add(component);
+      }
+      const int handle = dst_instances->add_reference(std::move(group_geometry));
+      dst_instances->add_instance(handle, float4x4::identity());
+
+      for (int i : elements) {
+        group_selection[i] = false;
+      }
+    }
+  }
+
+  params.set_output("Geometry", std::move(dst_geometry));
 }
 
 static void node_register()
