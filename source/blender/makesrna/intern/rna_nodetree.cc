e5b445f409c733e7e6cb190742a370f271724c6f
Jacques Lucke <jacques@blender.org>
2023-10-01 12:57:20 +0200

    progress

--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -3122,6 +3122,202 @@ static void rna_NodeCryptomatte_update_remove(Main *bmain, Scene *scene, Pointer
   rna_Node_update(bmain, scene, ptr);
 }
 
+template<typename T> struct ItemsArrayRef {
+  T **items_p;
+  int *items_num_p;
+  int *active_index_p = nullptr;
+};
+
+struct SimulationItemsAccessors {
+  using value_type = NodeSimulationItem;
+  static constexpr StructRNA *srna = &RNA_SimulationStateItem;
+
+  static ItemsArrayRef<NodeSimulationItem> get_items_from_node(bNode &node)
+  {
+    auto *storage = static_cast<NodeGeometrySimulationOutput *>(node.storage);
+    return {&storage->items, &storage->items_num, &storage->active_index};
+  }
+  static void destruct_item(NodeSimulationItem *item)
+  {
+    MEM_SAFE_FREE(item->name);
+  }
+  static short *get_socket_type(NodeSimulationItem *item)
+  {
+    return &item->socket_type;
+  }
+};
+
+struct RepeatItemsAccessors {
+  using value_type = NodeRepeatItem;
+  static constexpr StructRNA *srna = &RNA_RepeatItem;
+
+  static ItemsArrayRef<NodeRepeatItem> get_items_from_node(bNode &node)
+  {
+    auto *storage = static_cast<NodeGeometryRepeatOutput *>(node.storage);
+    return {&storage->items, &storage->items_num, &storage->active_index};
+  }
+  static void destruct_item(NodeRepeatItem *item)
+  {
+    MEM_SAFE_FREE(item->name);
+  }
+  static short *get_socket_type(NodeRepeatItem *item)
+  {
+    return &item->socket_type;
+  }
+};
+
+template<typename T>
+static void remove_item(T **items,
+                        int *items_num,
+                        int *active_index,
+                        const int remove_index,
+                        void (*destruct_item)(T *))
+{
+  static_assert(std::is_trivial_v<T>);
+  BLI_assert(remove_index >= 0);
+  BLI_assert(remove_index < *items_num);
+
+  const int old_items_num = *items_num;
+  const int new_items_num = old_items_num - 1;
+  const int old_active_index = *active_index;
+
+  T *old_items = *items;
+  T *new_items = MEM_cnew_array<T>(new_items_num, __func__);
+
+  std::copy_n(old_items, remove_index, new_items);
+  std::copy_n(
+      old_items + remove_index + 1, old_items_num - remove_index - 1, new_items + remove_index);
+
+  destruct_item(&old_items[remove_index]);
+  MEM_SAFE_FREE(old_items);
+
+  const int new_active_index = std::max(
+      0, old_active_index == new_items_num ? new_items_num - 1 : old_active_index);
+
+  *items = new_items;
+  *items_num = new_items_num;
+  *active_index = new_active_index;
+}
+
+template<typename T>
+static void clear_items(T **items, int *items_num, int *active_index, void (*destruct_item)(T *))
+{
+  static_assert(std::is_trivial_v<T>);
+  for (const int i : blender::IndexRange(*items_num)) {
+    destruct_item(&(*items)[i]);
+  }
+  MEM_SAFE_FREE(*items);
+  *items_num = 0;
+  *active_index = 0;
+}
+
+template<typename T>
+static void move_item(T *items, const int items_num, const int from_index, const int to_index)
+{
+  static_assert(std::is_trivial_v<T>);
+  BLI_assert(from_index >= 0);
+  BLI_assert(from_index < items_num);
+  BLI_assert(to_index >= 0);
+  BLI_assert(to_index < items_num);
+  UNUSED_VARS_NDEBUG(items_num);
+
+  if (from_index == to_index) {
+    return;
+  }
+
+  if (from_index < to_index) {
+    const T tmp = items[from_index];
+    for (int i = from_index; i < to_index; i++) {
+      items[i] = items[i + 1];
+    }
+    items[to_index] = tmp;
+  }
+  else if (from_index > to_index) {
+    const T tmp = items[from_index];
+    for (int i = from_index; i > to_index; i--) {
+      items[i] = items[i - 1];
+    }
+    items[to_index] = tmp;
+  }
+}
+
+template<typename Accessor>
+static void rna_Node_item_remove(ID *id,
+                                 bNode *node,
+                                 Main *bmain,
+                                 ReportList *reports,
+                                 typename Accessor::value_type *item_to_remove)
+{
+  ItemsArrayRef array = Accessor::get_items_from_node(*node);
+  if (item_to_remove < *array.items_p || item_to_remove >= *array.items_p + *array.items_num_p) {
+    BKE_reportf(reports, RPT_ERROR, "Unable to locate item '%s' in node", item_to_remove->name);
+    return;
+  }
+  const int remove_index = item_to_remove - *array.items_p;
+  remove_item(array.items_p,
+              array.items_num_p,
+              array.active_index_p,
+              remove_index,
+              Accessor::destruct_item);
+
+  bNodeTree *ntree = reinterpret_cast<bNodeTree *>(id);
+  BKE_ntree_update_tag_node_property(ntree, node);
+  ED_node_tree_propagate_change(nullptr, bmain, ntree);
+  WM_main_add_notifier(NC_NODE | NA_EDITED, ntree);
+}
+
+template<typename Accessor> static void rna_Node_items_clear(ID *id, bNode *node, Main *bmain)
+{
+  ItemsArrayRef array = Accessor::get_items_from_node(*node);
+  clear_items(array.items_p, array.items_num_p, array.active_index_p, Accessor::destruct_item);
+
+  bNodeTree *ntree = reinterpret_cast<bNodeTree *>(id);
+  BKE_ntree_update_tag_node_property(ntree, node);
+  ED_node_tree_propagate_change(nullptr, bmain, ntree);
+  WM_main_add_notifier(NC_NODE | NA_EDITED, ntree);
+}
+
+template<typename Accessor>
+static void rna_Node_item_move(
+    ID *id, bNode *node, Main *bmain, const int from_index, const int to_index)
+{
+  ItemsArrayRef array = Accessor::get_items_from_node(*node);
+  const int items_num = *array.items_num_p;
+  if (from_index < 0 || to_index < 0 || from_index >= items_num || to_index >= items_num) {
+    return;
+  }
+  move_item(*array.items_p, items_num, from_index, to_index);
+
+  bNodeTree *ntree = reinterpret_cast<bNodeTree *>(id);
+  BKE_ntree_update_tag_node_property(ntree, node);
+  ED_node_tree_propagate_change(nullptr, bmain, ntree);
+  WM_main_add_notifier(NC_NODE | NA_EDITED, ntree);
+}
+
+template<typename Accessor> static PointerRNA rna_Node_item_active_get(PointerRNA *ptr)
+{
+  bNode *node = static_cast<bNode *>(ptr->data);
+  ItemsArrayRef array = Accessor::get_items_from_node(*node);
+  typename Accessor::value_type *active_item = nullptr;
+  const int active_index = *array.active_index_p;
+  const int items_num = *array.items_num_p;
+  if (active_index >= 0 && active_index < items_num) {
+    active_item = &(*array.items_p)[active_index];
+  }
+  return RNA_pointer_create(ptr->owner_id, Accessor::srna, active_item);
+}
+template<typename Accessor> static void rna_Node_item_active_set(PointerRNA *ptr, PointerRNA value)
+{
+  using ItemT = typename Accessor::value_type;
+  bNode *node = static_cast<bNode *>(ptr->data);
+  ItemT *item = static_cast<ItemT *>(value.data);
+
+  ItemsArrayRef array = Accessor::get_items_from_node(*node);
+  if (item >= *array.items_p && item < *array.items_p + *array.items_num_p) {
+    *array.active_index_p = item - *array.items_p;
+  }
+}
+
 static void rna_SimulationStateItem_update(Main *bmain, Scene * /*scene*/, PointerRNA *ptr)
 {
   bNodeTree *ntree = reinterpret_cast<bNodeTree *>(ptr->owner_id);
@@ -3302,194 +3498,6 @@ static NodeRepeatItem *rna_NodeGeometryRepeatOutput_items_new(
   }
 
   return item;
-}
-
-template<typename T> struct ItemsArrayRef {
-  T **items_p;
-  int *items_num_p;
-  int *active_index_p = nullptr;
-};
-
-struct SimulationItemsAccessors {
-  using value_type = NodeSimulationItem;
-  static constexpr StructRNA *srna = &RNA_SimulationStateItem;
-
-  static ItemsArrayRef<NodeSimulationItem> get_items_from_node(bNode &node)
-  {
-    auto *storage = static_cast<NodeGeometrySimulationOutput *>(node.storage);
-    return {&storage->items, &storage->items_num, &storage->active_index};
-  }
-  static void destruct_item(NodeSimulationItem *item)
-  {
-    MEM_SAFE_FREE(item->name);
-  }
-};
-
-struct RepeatItemsAccessors {
-  using value_type = NodeRepeatItem;
-  static constexpr StructRNA *srna = &RNA_RepeatItem;
-
-  static ItemsArrayRef<NodeRepeatItem> get_items_from_node(bNode &node)
-  {
-    auto *storage = static_cast<NodeGeometryRepeatOutput *>(node.storage);
-    return {&storage->items, &storage->items_num, &storage->active_index};
-  }
-  static void destruct_item(NodeRepeatItem *item)
-  {
-    MEM_SAFE_FREE(item->name);
-  }
-};
-
-template<typename T>
-static void remove_item(T **items,
-                        int *items_num,
-                        int *active_index,
-                        const int remove_index,
-                        void (*destruct_item)(T *))
-{
-  static_assert(std::is_trivial_v<T>);
-  BLI_assert(remove_index >= 0);
-  BLI_assert(remove_index < *items_num);
-
-  const int old_items_num = *items_num;
-  const int new_items_num = old_items_num - 1;
-  const int old_active_index = *active_index;
-
-  T *old_items = *items;
-  T *new_items = MEM_cnew_array<T>(new_items_num, __func__);
-
-  std::copy_n(old_items, remove_index, new_items);
-  std::copy_n(
-      old_items + remove_index + 1, old_items_num - remove_index - 1, new_items + remove_index);
-
-  destruct_item(&old_items[remove_index]);
-  MEM_SAFE_FREE(old_items);
-
-  const int new_active_index = std::max(
-      0, old_active_index == new_items_num ? new_items_num - 1 : old_active_index);
-
-  *items = new_items;
-  *items_num = new_items_num;
-  *active_index = new_active_index;
-}
-
-template<typename T>
-static void clear_items(T **items, int *items_num, int *active_index, void (*destruct_item)(T *))
-{
-  static_assert(std::is_trivial_v<T>);
-  for (const int i : blender::IndexRange(*items_num)) {
-    destruct_item(&(*items)[i]);
-  }
-  MEM_SAFE_FREE(*items);
-  *items_num = 0;
-  *active_index = 0;
-}
-
-template<typename T>
-static void move_item(T *items, const int items_num, const int from_index, const int to_index)
-{
-  static_assert(std::is_trivial_v<T>);
-  BLI_assert(from_index >= 0);
-  BLI_assert(from_index < items_num);
-  BLI_assert(to_index >= 0);
-  BLI_assert(to_index < items_num);
-  UNUSED_VARS_NDEBUG(items_num);
-
-  if (from_index == to_index) {
-    return;
-  }
-
-  if (from_index < to_index) {
-    const T tmp = items[from_index];
-    for (int i = from_index; i < to_index; i++) {
-      items[i] = items[i + 1];
-    }
-    items[to_index] = tmp;
-  }
-  else if (from_index > to_index) {
-    const T tmp = items[from_index];
-    for (int i = from_index; i > to_index; i--) {
-      items[i] = items[i - 1];
-    }
-    items[to_index] = tmp;
-  }
-}
-
-template<typename Accessor>
-static void rna_Node_item_remove(ID *id,
-                                 bNode *node,
-                                 Main *bmain,
-                                 ReportList *reports,
-                                 typename Accessor::value_type *item_to_remove)
-{
-  ItemsArrayRef array = Accessor::get_items_from_node(*node);
-  if (item_to_remove < *array.items_p || item_to_remove >= *array.items_p + *array.items_num_p) {
-    BKE_reportf(reports, RPT_ERROR, "Unable to locate item '%s' in node", item_to_remove->name);
-    return;
-  }
-  const int remove_index = item_to_remove - *array.items_p;
-  remove_item(array.items_p,
-              array.items_num_p,
-              array.active_index_p,
-              remove_index,
-              Accessor::destruct_item);
-
-  bNodeTree *ntree = reinterpret_cast<bNodeTree *>(id);
-  BKE_ntree_update_tag_node_property(ntree, node);
-  ED_node_tree_propagate_change(nullptr, bmain, ntree);
-  WM_main_add_notifier(NC_NODE | NA_EDITED, ntree);
-}
-
-template<typename Accessor> static void rna_Node_items_clear(ID *id, bNode *node, Main *bmain)
-{
-  ItemsArrayRef array = Accessor::get_items_from_node(*node);
-  clear_items(array.items_p, array.items_num_p, array.active_index_p, Accessor::destruct_item);
-
-  bNodeTree *ntree = reinterpret_cast<bNodeTree *>(id);
-  BKE_ntree_update_tag_node_property(ntree, node);
-  ED_node_tree_propagate_change(nullptr, bmain, ntree);
-  WM_main_add_notifier(NC_NODE | NA_EDITED, ntree);
-}
-
-template<typename Accessor>
-static void rna_Node_item_move(
-    ID *id, bNode *node, Main *bmain, const int from_index, const int to_index)
-{
-  ItemsArrayRef array = Accessor::get_items_from_node(*node);
-  const int items_num = *array.items_num_p;
-  if (from_index < 0 || to_index < 0 || from_index >= items_num || to_index >= items_num) {
-    return;
-  }
-  move_item(*array.items_p, items_num, from_index, to_index);
-
-  bNodeTree *ntree = reinterpret_cast<bNodeTree *>(id);
-  BKE_ntree_update_tag_node_property(ntree, node);
-  ED_node_tree_propagate_change(nullptr, bmain, ntree);
-  WM_main_add_notifier(NC_NODE | NA_EDITED, ntree);
-}
-
-template<typename Accessor> static PointerRNA rna_Node_item_active_get(PointerRNA *ptr)
-{
-  bNode *node = static_cast<bNode *>(ptr->data);
-  ItemsArrayRef array = Accessor::get_items_from_node(*node);
-  typename Accessor::value_type *active_item = nullptr;
-  const int active_index = *array.active_index_p;
-  const int items_num = *array.items_num_p;
-  if (active_index >= 0 && active_index < items_num) {
-    active_item = &(*array.items_p)[active_index];
-  }
-  return RNA_pointer_create(ptr->owner_id, Accessor::srna, active_item);
-}
-template<typename Accessor> static void rna_Node_item_active_set(PointerRNA *ptr, PointerRNA value)
-{
-  using ItemT = typename Accessor::value_type;
-  bNode *node = static_cast<bNode *>(ptr->data);
-  ItemT *item = static_cast<ItemT *>(value.data);
-
-  ItemsArrayRef array = Accessor::get_items_from_node(*node);
-  if (item >= *array.items_p && item < *array.items_p + *array.items_num_p) {
-    *array.active_index_p = item - *array.items_p;
-  }
 }
 
 static void rna_NodeGeometrySimulationOutput_items_remove(
