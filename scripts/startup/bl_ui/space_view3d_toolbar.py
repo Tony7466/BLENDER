Tony7466/BLENDER

commit ef18cdf8dcaa49d1ed72e1b3462a67b6cb5b4d80

UI: Remove Masking dropdown from tool settings
In recent versions, masking is exposed as a popover in the header
so having this as well is confusing and takes up space.

scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -712,7 +712,6 @@ class VIEW3D_PT_slots_vertex_groups(Panel):
 
 class VIEW3D_PT_mask(View3DPanel, Panel):
     bl_category = "Tool"
-    bl_context = ".imagepaint"  # dot on purpose (access from topbar)
     bl_label = "Masking"
     bl_options = {'DEFAULT_CLOSED'}
 

commit 83226b0a311d06590c56a7531785fd460a082964

Fix #113078: Texture paint masking options are always visible
Caused by ef18cdf8dc. Commit did not properly remove the mask panels
from tool settings

scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -711,7 +711,8 @@ class VIEW3D_PT_slots_vertex_groups(Panel):
 
 
 class VIEW3D_PT_mask(View3DPanel, Panel):
-    bl_category = "Tool"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'HEADER'
     bl_label = "Masking"
     bl_options = {'DEFAULT_CLOSED'}
 
@@ -721,8 +722,8 @@ class VIEW3D_PT_mask(View3DPanel, Panel):
 
 # TODO, move to space_view3d.py
 class VIEW3D_PT_stencil_projectpaint(View3DPanel, Panel):
-    bl_category = "Tool"
-    bl_context = ".imagepaint"  # dot on purpose (access from topbar)
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'HEADER'
     bl_label = "Stencil Mask"
     bl_options = {'DEFAULT_CLOSED'}
     bl_parent_id = "VIEW3D_PT_mask"
@@ -1388,8 +1389,9 @@ class VIEW3D_PT_tools_imagepaint_options(View3DPaintPanel, Panel):
         col.prop(ipaint, "use_backface_culling", text="Backface Culling")
 
 
-class VIEW3D_PT_tools_imagepaint_options_cavity(View3DPaintPanel, Panel):
-    bl_context = ".imagepaint"  # dot on purpose (access from topbar)
+class VIEW3D_PT_tools_imagepaint_options_cavity(Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'HEADER'
     bl_label = "Cavity Mask"
     bl_parent_id = "VIEW3D_PT_mask"
     bl_options = {'DEFAULT_CLOSED'}