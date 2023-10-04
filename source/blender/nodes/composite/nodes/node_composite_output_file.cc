86d1bfc6001245457aae39c47791698238a1a957
Omar Emara <mail@OmarEmara.dev>
2023-10-04 20:10:40 +0300

    Implement single layer file writers

--- source/blender/nodes/composite/nodes/node_composite_output_file.cc
+++ source/blender/nodes/composite/nodes/node_composite_output_file.cc
@@ -33,6 +33,9 @@
 
 #include "WM_api.hh"
 
+#include "IMB_colormanagement.h"
+#include "IMB_imbuf.h"
+#include "IMB_imbuf_types.h"
 #include "IMB_openexr.h"
 
 #include "GPU_state.h"
@@ -463,6 +466,25 @@ class OutputFileOperation : public NodeOperation {
 
   void execute() override
   {
+    if (is_multi_layer_exr()) {
+      execute_multi_layer_exr();
+    }
+    else {
+      execute_single_layer();
+    }
+  }
+
+  /* -----------------------
+   * Multi-Layer EXR Images.
+   * ----------------------- */
+
+  bool is_multi_layer_exr()
+  {
+    return node_storage(bnode()).format.imtype == R_IMF_IMTYPE_MULTILAYER;
+  }
+
+  void execute_multi_layer_exr()
+  {
     const Domain domain = compute_domain();
     const int width = domain.size.x;
     const int height = domain.size.y;
@@ -473,7 +495,7 @@ class OutputFileOperation : public NodeOperation {
     /* If we are saving all views in a single multi-layer file, we supply an empty view to make
      * sure the file name does not contain a view suffix. */
     char image_path[FILE_MAX];
-    get_image_path(image_path, store_views_in_single_file ? "" : view);
+    get_multi_layer_exr_image_path(store_views_in_single_file ? "" : view, image_path);
 
     /* This function creates a global EXR image whose lifetime extends outside this operation and
      * even this compositor evaluation. This is useful for multi-view images, since the compositor
@@ -493,8 +515,8 @@ class OutputFileOperation : public NodeOperation {
       }
 
       GPU_memory_barrier(GPU_BARRIER_TEXTURE_UPDATE);
-      const eGPUDataFormat format = GPU_DATA_FLOAT;
-      float *buffer = static_cast<float *>(GPU_texture_read(input_result.texture(), format, 0));
+      float *buffer = static_cast<float *>(
+          GPU_texture_read(input_result.texture(), GPU_DATA_FLOAT, 0));
 
       const char *layer = (static_cast<NodeImageMultiFileSocket *>(input->storage))->layer;
 
@@ -573,11 +595,11 @@ class OutputFileOperation : public NodeOperation {
 
   /* Get the path of the image to be saved and ensure its directory tree exists. If the given view
    * is not empty, its corresponding file suffix will be appended to the name. */
-  void get_image_path(char *image_path, const char *view)
+  void get_multi_layer_exr_image_path(const char *view, char *image_path)
   {
     const char *suffix = BKE_scene_multiview_view_suffix_get(&context().get_render_data(), view);
     BKE_image_path_from_imtype(image_path,
-                               node_storage(bnode()).base_path,
+                               get_base_path(),
                                BKE_main_blendfile_path_from_global(),
                                context().get_frame_number(),
                                R_IMF_IMTYPE_MULTILAYER,
@@ -587,9 +609,225 @@ class OutputFileOperation : public NodeOperation {
     BLI_file_ensure_parent_dir_exists(image_path);
   }
 
+  /* --------------------
+   * Single Layer Images.
+   * -------------------- */
+
+  void execute_single_layer()
+  {
+    for (const bNodeSocket *input : this->node()->input_sockets()) {
+      const Result &input_result = get_input(input->identifier);
+      if (input_result.is_single_value()) {
+        continue;
+      }
+
+      NodeImageMultiFileSocket *socket = static_cast<NodeImageMultiFileSocket *>(input->storage);
+      const ImageFormatData &format = socket->use_node_format ? node_storage(bnode()).format :
+                                                                socket->format;
+
+      char base_path[FILE_MAX];
+      if (socket->path[0]) {
+        BLI_path_join(base_path, FILE_MAX, get_base_path(), socket->path);
+      }
+      else {
+        STRNCPY(base_path, get_base_path());
+        BLI_path_slash_ensure(base_path, FILE_MAX);
+      }
+
+      switch (format.views_format) {
+        case R_IMF_VIEWS_INDIVIDUAL:
+          execute_single_layer_single_view(input_result, *socket, format, base_path);
+          break;
+        case R_IMF_VIEWS_STEREO_3D:
+          execute_single_layer_stereo(input_result, *socket, format, base_path);
+          break;
+        case R_IMF_VIEWS_MULTIVIEW:
+          if (is_multi_view_scene()) {
+            execute_single_layer_multi_view(input_result, *socket, format, base_path);
+          }
+          else {
+            execute_single_layer_single_view(input_result, *socket, format, base_path);
+          }
+          break;
+      }
+    }
+  }
+
+  /* -------------------------
+   * Single Layer Single View.
+   * ------------------------- */
+
+  void execute_single_layer_single_view(const Result &input_result,
+                                        NodeImageMultiFileSocket &socket,
+                                        const ImageFormatData &format,
+                                        const char *base_path)
+  {
+    ImageFormatData write_format;
+    BKE_image_format_init_for_write(&write_format, &context().get_scene(), &format);
+
+    /* Avoid applying color conversion during writing if not saving as a render. */
+    if (!socket.save_as_render) {
+      write_format.linear_colorspace_settings.name[0] = '\0';
+    }
+
+    GPU_memory_barrier(GPU_BARRIER_TEXTURE_UPDATE);
+    float *pixel_buffer = static_cast<float *>(
+        GPU_texture_read(input_result.texture(), GPU_DATA_FLOAT, 0));
+
+    const Domain domain = input_result.domain();
+    const int width = domain.size.x;
+    const int height = domain.size.y;
+
+    ImBuf *image_buffer = IMB_allocImBuf(width, height, write_format.planes, 0);
+
+    image_buffer->channels = get_result_channels_count(input_result);
+    image_buffer->dither = context().get_render_data().dither_intensity;
+
+    IMB_assign_float_buffer(image_buffer, pixel_buffer, IB_TAKE_OWNERSHIP);
+
+    IMB_colormanagement_imbuf_for_write(image_buffer, socket.save_as_render, false, &write_format);
+
+    char image_path[FILE_MAX];
+    const char *suffix = BKE_scene_multiview_view_suffix_get(&context().get_render_data(),
+                                                             context().get_view_name().data());
+    BKE_image_path_from_imformat(image_path,
+                                 base_path,
+                                 BKE_main_blendfile_path_from_global(),
+                                 context().get_frame_number(),
+                                 &write_format,
+                                 use_file_extension(),
+                                 true,
+                                 suffix);
+
+    BKE_imbuf_write(image_buffer, image_path, &write_format);
+    IMB_freeImBuf(image_buffer);
+  }
+
+  /* --------------------
+   * Single Layer Stereo.
+   * -------------------- */
+
+  void execute_single_layer_stereo(const Result &input_result,
+                                   NodeImageMultiFileSocket &socket,
+                                   const ImageFormatData &format,
+                                   const char *base_path)
+  {
+    ImageFormatData write_format;
+    BKE_image_format_init_for_write(&write_format, &context().get_scene(), &format);
+
+    /* Avoid applying color conversion during writing if not saving as a render. */
+    if (!socket.save_as_render) {
+      write_format.linear_colorspace_settings.name[0] = '\0';
+    }
+
+    const Domain domain = compute_domain();
+    const int width = domain.size.x;
+    const int height = domain.size.y;
+
+    /* We are not necessarily saving an EXR image, however, we use the global EXR image structure
+     * to store both the left and right views at the same time, which we then extract from the EXR
+     * image and save using the standard IMB API. See the comments in execute_multi_layer_exr for
+     * more information. */
+    void *exr_handle = IMB_exr_get_handle_name(base_path);
+
+    const char *view = context().get_view_name().data();
+    IMB_exr_add_view(exr_handle, view);
+
+    GPU_memory_barrier(GPU_BARRIER_TEXTURE_UPDATE);
+    float *buffer = static_cast<float *>(
+        GPU_texture_read(input_result.texture(), GPU_DATA_FLOAT, 0));
+
+    /* The entire pixel buffer is encoded in a float channel, but is it is just a dummy channel for
+     * storage as explained above. */
+    IMB_exr_add_channel(exr_handle,
+                        nullptr,
+                        socket.layer,
+                        view,
+                        1,
+                        get_result_channels_count(input_result) * width * height,
+                        buffer,
+                        write_format.depth == R_IMF_CHAN_DEPTH_16);
+
+    /* This is the last view, so both views are now added and we can write our image. */
+    if (BKE_scene_multiview_is_render_view_last(&context().get_render_data(), view)) {
+      ImBuf *stereo_image_buffers[2];
+      const char *stereo_view_names[2] = {STEREO_LEFT_NAME, STEREO_RIGHT_NAME};
+
+      /* Extract and steal the pixel buffers of both views from the EXR and construct image buffers
+       * from them. */
+      for (int i = 0; i < 2; i++) {
+        float *rectf = IMB_exr_channel_rect(
+            exr_handle, nullptr, socket.layer, stereo_view_names[i]);
+
+        stereo_image_buffers[i] = IMB_allocImBuf(width, height, write_format.planes, 0);
+        stereo_image_buffers[i]->channels = get_result_channels_count(input_result);
+        stereo_image_buffers[i]->dither = context().get_render_data().dither_intensity;
+
+        IMB_assign_float_buffer(stereo_image_buffers[i], rectf, IB_TAKE_OWNERSHIP);
+
+        IMB_colormanagement_imbuf_for_write(stereo_image_buffers[i], true, false, &write_format);
+      }
+
+      ImBuf *stereo_image_buffer = IMB_stereo3d_ImBuf(
+          &write_format, stereo_image_buffers[0], stereo_image_buffers[1]);
+
+      char image_path[FILE_MAX];
+      BKE_image_path_from_imformat(image_path,
+                                   base_path,
+                                   BKE_main_blendfile_path_from_global(),
+                                   context().get_frame_number(),
+                                   &write_format,
+                                   use_file_extension(),
+                                   true,
+                                   nullptr);
+
+      BKE_imbuf_write(stereo_image_buffer, image_path, &write_format);
+
+      IMB_freeImBuf(stereo_image_buffers[0]);
+      IMB_freeImBuf(stereo_image_buffers[1]);
+      IMB_freeImBuf(stereo_image_buffer);
+
+      IMB_exr_close(exr_handle);
+    }
+  }
+
+  /* ------------------------
+   * Single Layer Multi-View.
+   * ------------------------ */
+
+  void execute_single_layer_multi_view(const Result &input_result,
+                                       NodeImageMultiFileSocket &socket,
+                                       const ImageFormatData &format,
+                                       const char *base_path)
+  {
+  }
+
+  /* Get the number of components in the type of the given result. Note that vectors are 4D. */
+  int get_result_channels_count(const Result &result)
+  {
+    switch (result.type()) {
+      case ResultType::Color:
+      case ResultType::Vector:
+        return 4;
+      case ResultType::Float:
+        return 1;
+      default:
+        /* Other types are internal and needn't be handled by operations. */
+        break;
+    }
+
+    BLI_assert_unreachable();
+    return 0;
+  }
+
   bool use_half_float()
   {
     return node_storage(bnode()).format.depth == R_IMF_CHAN_DEPTH_16;
+  }
+
+  const char *get_base_path()
+  {
+    return node_storage(bnode()).base_path;
   }
 
   /* The EXR compression method. */
