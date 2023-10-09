1ce1db21b9a41c41c85e68990d09718b1f5bdb2a
Harley Acheson <harley.acheson@gmail.com>
2023-10-09 12:02:49 -0700

    Minimum changes required to fix issue.

--- intern/ghost/intern/GHOST_Window.cc
+++ intern/ghost/intern/GHOST_Window.cc
@@ -173,9 +173,6 @@ GHOST_TSuccess GHOST_Window::setCursorGrab(GHOST_TGrabCursorMode mode,
 
 GHOST_TSuccess GHOST_Window::getCursorGrabBounds(GHOST_Rect &bounds) const
 {
-  if (m_cursorGrab != GHOST_kGrabWrap) {
-    return GHOST_kFailure;
-  }
   bounds = m_cursorGrabBounds;
   return (bounds.m_l == -1 && bounds.m_r == -1) ? GHOST_kFailure : GHOST_kSuccess;
 }
