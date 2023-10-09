39ae15f709cbc915c77fdcb5cbaeaa9bd0480676
Falk David <falk@blender.org>
2023-10-09 12:19:45 +0200

    GPv3: Add support for some geometry nodes
    This adds support for the following nodes:
    * Attribute Domain Size
    * Blur Attribute
    * Curve Endpoint Selection
    * Curve Length
    * Curve Trim
    * Transform Geometry

--- source/blender/nodes/geometry/nodes/node_geo_curve_length.cc
+++ source/blender/nodes/geometry/nodes/node_geo_curve_length.cc
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include "BKE_curves.hh"
+#include "BKE_grease_pencil.hh"
 
 #include "node_geometry_util.hh"
 
@@ -10,27 +11,48 @@ namespace blender::nodes::node_geo_curve_length_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_input<decl::Geometry>("Curve").supported_type(GeometryComponent::Type::Curve);
+  b.add_input<decl::Geometry>("Curve").supported_type(
+      {GeometryComponent::Type::Curve, GeometryComponent::Type::GreasePencil});
   b.add_output<decl::Float>("Length");
+}
+
+static float curves_total_length(const bke::CurvesGeometry &curves)
+{
+  const VArray<bool> cyclic = curves.cyclic();
+  curves.ensure_evaluated_lengths();
+  
+  float total_length = 0.0f;
+  for (const int i : curves.curves_range()) {
+    total_length += curves.evaluated_length_total_for_curve(i, cyclic[i]);
+  }
+  return total_length;
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
-  GeometrySet curve_set = params.extract_input<GeometrySet>("Curve");
-  if (!curve_set.has_curves()) {
+  GeometrySet geometry_set = params.extract_input<GeometrySet>("Curve");
+  float length = 0.0f;
+  if (geometry_set.has_curves()) {
+    const Curves &curves_id = *geometry_set.get_curves();
+    const bke::CurvesGeometry &curves = curves_id.geometry.wrap();
+    length += curves_total_length(curves);
+  }
+  else if (geometry_set.has_grease_pencil()) {
+    using namespace bke::greasepencil;
+    GreasePencil &grease_pencil = *geometry_set.get_grease_pencil_for_write();
+    for (const Layer *layer : grease_pencil.layers()) {
+      Drawing *drawing = grease_pencil.get_editable_drawing_at(layer,
+                                                               grease_pencil.runtime->eval_frame);
+      if (drawing == nullptr) {
+        continue;
+      }
+      const bke::CurvesGeometry &curves = drawing->strokes();
+      length += curves_total_length(curves);
+    }
+  }
+  else {
     params.set_default_remaining_outputs();
     return;
-  }
-
-  const Curves &curves_id = *curve_set.get_curves();
-  const bke::CurvesGeometry &curves = curves_id.geometry.wrap();
-  const VArray<bool> cyclic = curves.cyclic();
-
-  curves.ensure_evaluated_lengths();
-
-  float length = 0.0f;
-  for (const int i : curves.curves_range()) {
-    length += curves.evaluated_length_total_for_curve(i, cyclic[i]);
   }
 
   params.set_output("Length", length);
