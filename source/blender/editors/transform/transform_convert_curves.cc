30d012cc0b6ce7847a2d93687948512bc4a1d245
casey bianco-davis <caseycasey739@gmail.com>
2023-10-13 17:26:20 -0700

    Cleanup: remove const

--- source/blender/editors/transform/transform_convert_curves.cc
+++ source/blender/editors/transform/transform_convert_curves.cc
@@ -124,9 +124,9 @@ void curve_populate_trans_data_structs(TransDataContainer &tc,
                                        blender::bke::CurvesGeometry &curves,
                                        blender::MutableSpan<float> *value_attribute,
                                        const blender::IndexMask &selected_indices,
-                                       const bool use_proportional_edit,
-                                       const bool use_connected_only,
-                                       const int data_offset)
+                                       bool use_proportional_edit,
+                                       bool use_connected_only,
+                                       int data_offset)
 {
   using namespace blender;
 
