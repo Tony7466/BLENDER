181aeaee9fb61d1d5cd1f6f2ca159cd4706cb9ab
Clément Foucault <foucault.clem@gmail.com>
2023-10-23 02:19:50 +0200

    Make visibility bitmask work

--- source/blender/draw/engines/eevee_next/shaders/eevee_horizon_scan_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_horizon_scan_lib.glsl
@@ -4,8 +4,24 @@
 
 #pragma BLENDER_REQUIRE(draw_view_lib.glsl)
 #pragma BLENDER_REQUIRE(gpu_shader_utildefines_lib.glsl)
+#pragma BLENDER_REQUIRE(gpu_shader_math_vector_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_sampling_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_ray_types_lib.glsl)
+
+uint horizon_scan_angles_bitmask(float theta_min, float theta_max)
+{
+#if 0
+  uint a = uint(floor(32.0 * saturate((theta_min + M_PI_2) / M_PI)));
+  uint b = uint(ceil(32.0 * saturate((theta_max - theta_min) / M_PI)));
+  return ((1u << b) - 1u) << a;
+#else
+  theta_min = saturate(theta_min * M_1_PI + 0.5);
+  theta_max = saturate(theta_max * M_1_PI + 0.5);
+  uint a = uint(floor(32.0 * theta_min));
+  uint b = uint(ceil(32.0 * (theta_max - theta_min)));
+  return ((1u << b) - 1u) << a;
+#endif
+}
 
 float bsdf_eval(vec3 N, vec3 L)
 {
@@ -32,52 +48,60 @@ struct HorizonScanResult {
   vec3 bent_normal;
 };
 
-void horizon_scan_bitmask(vec3 vV,
-                          vec3 vP,
-                          vec3 vN,
-                          float angle_vN,
-                          float noise,
-                          ScreenSpaceRay ssray,
-                          sampler2D depth_tx,
-                          float search_distance,
-                          float thickness,
-                          inout uint slice_visbits,
-                          inout vec3 accum_light,
-                          const bool inverted,
-                          const int sample_count)
+float horizon_scan_direction_scan(vec3 vV,
+                                  vec3 vP,
+                                  vec3 vN,
+                                  float angle_vN,
+                                  float noise,
+                                  ScreenSpaceRay ssray,
+                                  sampler2D depth_tx,
+                                  float search_distance,
+                                  float thickness,
+                                  inout vec3 accum_light,
+                                  const bool inverted,
+                                  const int sample_count)
 {
   ssray.max_time -= 1.0;
 
   if (ssray.max_time <= 2.0) {
-    return;
+    return 1.0;
   }
 
   const float bitmask_len = 32.0;
+  uint slice_visbits = 0u;
   for (int j = 0; j < sample_count; j++) {
     /* Gives us good precision at center and ensure we cross at least one pixel per iteration. */
     float time = 1.0 + square((float(j) + noise) / float(sample_count)) * ssray.max_time;
     float lod = float(j >> 2) / (1.0 + uniform_buf.ao.quality);
 
-    vec2 sample_uv = ssray.origin.xy + ssray.direction.xy * time;
-    float sample_depth = textureLod(depth_tx, sample_uv * uniform_buf.hiz.uv_scale, floor(lod)).r;
+    for (int i = 0; i < 2; i++) {
+      vec2 sample_uv = ssray.origin.xy + ssray.direction.xy * ((i == 0) ? time : -time);
+      float sample_depth =
+          textureLod(depth_tx, sample_uv * uniform_buf.hiz.uv_scale, floor(lod)).r;
 
-    if (sample_depth == 1.0 && inverted) {
-      /* Skip background. Avoids making shadow on the geometry near the far plane. */
-      continue;
-    }
+      if (sample_depth == 1.0) {
+        /* Skip background. Avoids making shadow on the geometry near the far plane. */
+        continue;
+      }
 
-    vec3 front_vP = drw_point_screen_to_view(vec3(sample_uv, sample_depth));
-    vec3 back_vP = front_vP - thickness * vV;
-    float theta_front = acos_fast(dot(normalize(front_vP - vP), vV)) - angle_vN;
-    float theta_back = acos_fast(dot(normalize(back_vP - vP), vV)) - angle_vN;
-    float theta_min = min(theta_front, theta_back);
-    float theta_max = max(theta_front, theta_back);
-    int a = int(floor(bitmask_len * ((theta_min + M_PI_2) / M_PI)));
-    int b = int(ceil(bitmask_len * ((theta_max - theta_min + M_PI_2) / M_PI)));
-    /* Create bitmask from b-1 bit to a. */
-    uint sample_visbits = (1u << b) - (1u << a);
-    slice_visbits |= sample_visbits;
-#if 0
+      /* Bias depth a bit to avoid self shadowing issues. */
+      const float bias = 2.0 * 2.4e-7;
+      sample_depth += bias;
+
+      vec3 front_vP = drw_point_screen_to_view(vec3(sample_uv, sample_depth));
+      vec3 back_vP = front_vP - thickness * vV;
+
+      if (min(length(front_vP - vP), length(back_vP - vP)) > search_distance) {
+        // continue;
+      }
+
+      vec2 theta;
+      theta.x = acos_fast(dot(normalize(front_vP - vP), vV));
+      theta.y = acos_fast(dot(normalize(back_vP - vP), vV));
+      theta = (i == 0) ? theta : -theta;
+      theta -= angle_vN;
+      slice_visbits |= horizon_scan_angles_bitmask(reduce_min(theta), reduce_max(theta));
+#if 0 /* SSGI */
     vec3 sample_radiance = horizon_scan_sample_radiance(sample_uv);
     vec3 sample_normal = horizon_scan_sample_normal(sample_uv);
     vec3 vL = normalize(front_vP - vP);
@@ -85,7 +109,9 @@ void horizon_scan_bitmask(vec3 vV,
     sample_visibility *= dot(sample_normal, -vL);
     accum_light += sample_radiance * (bsdf_eval(vN, vL) * sample_visibility);
 #endif
+    }
   }
+  return 1.0 - float(bitCount(slice_visbits)) / bitmask_len;
 }
 
 vec2 horizon_scan_search_horizon(vec3 vV,
@@ -146,12 +172,8 @@ vec2 horizon_scan_search_horizon(vec3 vV,
       theta.y = acos_fast(dot(normalize(back_vP - vP), vV));
       theta = (i == 0) ? theta : -theta;
       theta -= angle_vN;
-      float theta_min = reduce_min(theta);
-      float theta_max = reduce_max(theta);
-      int a = int(floor(bitmask_len * ((theta_min + M_PI_2) / M_PI)));
-      int b = int(ceil(bitmask_len * ((theta_max - theta_min + M_PI_2) / M_PI)));
+      slice_visbits |= horizon_scan_angles_bitmask(reduce_min(theta), reduce_max(theta));
 
-      /* Sample's horizon angle cosine. */
       if (i == 0) {
         h[i] = min(h[i], theta.x);
       }
@@ -160,6 +182,8 @@ vec2 horizon_scan_search_horizon(vec3 vV,
       }
     }
   }
+  // h = vec2(float(clamp(findLSB(slice_visbits >> 16), 0, 16)) * M_PI_2 / 16.0,
+  //          float(clamp(findMSB(slice_visbits << 16), 16, 31) - 31) * M_PI_2 / 16.0);
   return h;
 }
 
@@ -202,29 +226,22 @@ HorizonScanResult horizon_scan(vec3 vP,
     ray.direction = vec3(v_dir, 0.0);
     ray.max_time = search_distance;
 
-#if 0
-    for (int j = 0; j < 2; j++) {
-      const float bitmask_len = 32.0;
-      ScreenSpaceRay ssray = raytrace_screenspace_ray_create(ray, pixel_size);
-      uint slice_visbits = 0u;
-      horizon_scan_bitmask(vV,
-                           vP,
-                           vN,
-                           angle_N,
-                           noise,
-                           ssray,
-                           depth_tx,
-                           search_distance,
-                           thickness,
-                           slice_visbits,
-                           accum_light,
-                           inverted,
-                           sample_count);
-      accum_visibility += 1.0 - float(bitCount(slice_visbits)) / bitmask_len;
-      ray.direction = -ray.direction;
-    }
-#else
     ScreenSpaceRay ssray = raytrace_screenspace_ray_create(ray, pixel_size);
+#if 0
+    float a = horizon_scan_direction_scan(vV,
+                                          vP,
+                                          vN,
+                                          angle_N,
+                                          noise,
+                                          ssray,
+                                          depth_tx,
+                                          search_distance,
+                                          thickness,
+                                          accum_light,
+                                          inverted,
+                                          sample_count);
+
+#else
     vec2 h = horizon_scan_search_horizon(vV,
                                          vP,
                                          vN,
@@ -245,8 +262,9 @@ HorizonScanResult horizon_scan(vec3 vP,
     /* Inner integral (Eq. 7). */
     float a = dot(-cos(2.0 * h) + N_cos + 2.0 * (h + angle_N) * N_sin, vec2(0.25));
     /* Correct normal not on plane (Eq. 8). */
-    accum_visibility += proj_vN_len * a;
+    // a *= proj_vN_len;
 #endif
+    accum_visibility += a;
 
     /* Rotate 90 degrees. */
     v_dir = orthogonal(v_dir);
