91353e41d11d23f3b4646ac8e060d81ea775ef11
Hans Goudey <hans@blender.org>
2023-11-02 16:46:40 +0100

    Fix

--- source/blender/blenlib/BLI_array_utils.hh
+++ source/blender/blenlib/BLI_array_utils.hh
@@ -86,23 +86,6 @@ inline void scatter(const Span<T> src,
 /**
  * Fill the destination span by gathering indexed values from the `src` array.
  */
-template<typename T, typename IndexT>
-inline void scatter(const Span<T> src,
-                    const Span<IndexT> indices,
-                    MutableSpan<T> dst,
-                    const int64_t grain_size = 4096)
-{
-  BLI_assert(indices.size() == src.size());
-  threading::parallel_for(indices.index_range(), grain_size, [&](const IndexRange range) {
-    for (const int64_t i : range) {
-      dst[indices[i]] = src[i];
-    }
-  });
-}
-
-/**
- * Fill the destination span by gathering indexed values from the `src` array.
- */
 void gather(const GVArray &src,
             const IndexMask &indices,
             GMutableSpan dst,
