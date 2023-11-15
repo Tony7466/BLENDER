5f919adfa5706f05a21d5ea1c1486b1a413d9e4b
Jacques Lucke <jacques@blender.org>
2023-11-15 14:59:11 +0100

    update object after changing gizmo

--- source/blender/editors/space_view3d/view3d_gizmo_geometry_nodes.cc
+++ source/blender/editors/space_view3d/view3d_gizmo_geometry_nodes.cc
@@ -136,6 +136,11 @@ struct FloatValuePath {
       }
     }
     RNA_property_update(C, &this->owner, this->property);
+    if (Object *object = CTX_data_active_object(C)) {
+      /* The recalc is necessary so that the gizmo positions are updated even if they don't affect
+       * the final output currently. */
+      DEG_id_tag_update(&object->id, ID_RECALC_GEOMETRY);
+    }
   }
 };
 
