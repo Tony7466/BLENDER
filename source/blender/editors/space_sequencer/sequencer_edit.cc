Tony7466/BLENDER

commit bfe6128748d7676db68c61a7a6926a52f579939a

Fix #114891: bpy.ops.sequencer.split ignores passed frame
Caused by 0e01667e25f6 .

Above commit changed it so that operator properties (set from python)
were ignored for `split_frame` & `split_channel` in case
`use_cursor_position` is False.

Should be the other way around.

Pull Request: https://projects.blender.org/blender/blender/pulls/114905

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -1429,8 +1429,8 @@ static int sequencer_split_exec(bContext *C, wmOperator *op)
 
   const bool use_cursor_position = RNA_boolean_get(op->ptr, "use_cursor_position");
 
-  const int split_frame = use_cursor_position ? RNA_int_get(op->ptr, "frame") : scene->r.cfra;
-  const int split_channel = use_cursor_position ? RNA_int_get(op->ptr, "channel") : 0;
+  const int split_frame = use_cursor_position ? scene->r.cfra : RNA_int_get(op->ptr, "frame");
+  const int split_channel = use_cursor_position ? 0 : RNA_int_get(op->ptr, "channel");
 
   const eSeqSplitMethod method = eSeqSplitMethod(RNA_enum_get(op->ptr, "type"));
   const int split_side = sequence_split_side_for_exec_get(op);

commit f79f1d9ed9fbe04a2d4aa697df4f01016a19e6a5

Fix Strip->Split menu entry not working after recent change
Caused by bfe6128748d.

The split frame logic actually became inverted in the update:
when the cursor position is true, then the frame is supposed
to be initialized in the invoke().

The menu does not use cursor position, so the code path where
the frame is retrieved from the RNA properties was used, and
the frame was never initialized in that case.

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -1429,8 +1429,10 @@ static int sequencer_split_exec(bContext *C, wmOperator *op)
 
   const bool use_cursor_position = RNA_boolean_get(op->ptr, "use_cursor_position");
 
-  const int split_frame = use_cursor_position ? scene->r.cfra : RNA_int_get(op->ptr, "frame");
-  const int split_channel = use_cursor_position ? 0 : RNA_int_get(op->ptr, "channel");
+  const int split_frame = RNA_struct_property_is_set(op->ptr, "frame") ?
+                              RNA_int_get(op->ptr, "frame") :
+                              scene->r.cfra;
+  const int split_channel = RNA_int_get(op->ptr, "channel");
 
   const eSeqSplitMethod method = eSeqSplitMethod(RNA_enum_get(op->ptr, "type"));
   const int split_side = sequence_split_side_for_exec_get(op);