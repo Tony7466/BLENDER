7b5c19c6fd41fbaa805f22903c6ea0d6ae0f796d
Iliya Katueshenock <modormoder@gmail.com>
2023-11-21 18:11:29 +0300

    progress
    cleanup, renaming, use std::atomic_int

--- source/blender/nodes/geometry/nodes/node_geo_mesh_face_group_boundaries.cc
+++ source/blender/nodes/geometry/nodes/node_geo_mesh_face_group_boundaries.cc
@@ -3,11 +3,14 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include "atomic_ops.h"
+#include <atomic>
 
 #include "DNA_mesh_types.h"
 #include "DNA_meshdata_types.h"
 
 #include "BKE_mesh.hh"
+
+#include "BLI_timeit.hh"
 
 #include "node_geometry_util.hh"
 
@@ -42,45 +45,63 @@ class BoundaryFieldInput final : public bke::MeshFieldInput {
                                  const eAttrDomain domain,
                                  const IndexMask & /*mask*/) const final
   {
+    SCOPED_TIMER_AVERAGED(__func__);
     const bke::MeshFieldContext face_context{mesh, ATTR_DOMAIN_FACE};
     FieldEvaluator face_evaluator{face_context, mesh.faces_num};
     face_evaluator.add(face_set_);
     face_evaluator.evaluate();
-    const VArray<int> face_set = face_evaluator.get_evaluated<int>(0);
-    if (face_set.is_single()) {
+    const VArray<int> faces_group_id = face_evaluator.get_evaluated<int>(0);
+    if (faces_group_id.is_single()) {
       return {};
     }
 
+    Array<bool> boundary(mesh.totedge, false);
+
+    Array<std::atomic_int> edge_states(mesh.totedge);
+    /* State is index of face or one of invalid values: */
     const static constexpr int no_face_yet = -1;
     const static constexpr int no_more_face = -2;
 
-    Array<bool> boundary(mesh.totedge, false);
-    Array<int> previos_face(mesh.totedge, no_face_yet);
+    threading::parallel_for(edge_states.index_range(), 4096, [&](const IndexRange range) {
+      for (std::atomic_int &v : edge_states.as_mutable_span().slice(range)) {
+        v.store(no_face_yet, std::memory_order_relaxed);
+      }
+    });
+
     const GroupedSpan<int> face_edges(mesh.face_offsets(), mesh.corner_edges());
     threading::parallel_for(face_edges.index_range(), 2048, [&](const IndexRange range) {
       for (const int face_i : range) {
-        const int group_id = face_set[face_i];
+        const int group_id = faces_group_id[face_i];
         for (const int edge_i : face_edges[face_i]) {
+          std::atomic_int &edge_state = edge_states[edge_i];
           while (true) {
-            const int last_face_i = atomic_load_int32(&previos_face[edge_i]);
-            switch (last_face_i) {
+            int edge_state_v = edge_state.load(std::memory_order_relaxed);
+            switch (edge_state_v) {
               case no_more_face:
                 break;
               case no_face_yet: {
-                if (atomic_cas_int32(&previos_face[edge_i], last_face_i, face_i) != last_face_i) {
-                  continue;
-                }
-                break;
-              }
-              default: {
-                if (face_set[last_face_i] == group_id) {
+                if (edge_state.compare_exchange_weak(edge_state_v,
+                                                     face_i,
+                                                     std::memory_order_relaxed,
+                                                     std::memory_order_relaxed))
+                {
                   break;
                 }
-                if (atomic_cas_int32(&previos_face[edge_i], last_face_i, no_more_face) !=
-                    last_face_i) {
-                  continue;
+                continue;
+              }
+              default: {
+                if (faces_group_id[edge_state_v] == group_id) {
+                  break;
                 }
-                boundary[edge_i] = true;
+                if (edge_state.compare_exchange_weak(edge_state_v,
+                                                     no_more_face,
+                                                     std::memory_order_release,
+                                                     std::memory_order_release))
+                {
+                  boundary[edge_i] = true;
+                  break;
+                }
+                continue;
               }
             }
             break;
