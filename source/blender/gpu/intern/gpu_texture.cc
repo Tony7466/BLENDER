dead1ac0aa67441ea7b1cb9272f5b8c14155f97f
Clément Foucault <foucault.clem@gmail.com>
2023-11-24 15:16:05 +0100

    Add stencil assert

--- source/blender/gpu/intern/gpu_texture.cc
+++ source/blender/gpu/intern/gpu_texture.cc
@@ -469,6 +469,9 @@ GPUTexture *GPU_texture_create_view(const char *name,
 {
   BLI_assert(mip_len > 0);
   BLI_assert(layer_len > 0);
+  BLI_assert_msg(use_stencil == false || (GPU_texture_usage(src) & GPU_TEXTURE_USAGE_FORMAT_VIEW),
+                 "Source texture of TextureView must have GPU_TEXTURE_USAGE_FORMAT_VIEW usage "
+                 "flag if view texture uses stencil texturing.");
   BLI_assert_msg((format == GPU_texture_format(src)) ||
                      (GPU_texture_usage(src) & GPU_TEXTURE_USAGE_FORMAT_VIEW),
                  "Source texture of TextureView must have GPU_TEXTURE_USAGE_FORMAT_VIEW usage "
