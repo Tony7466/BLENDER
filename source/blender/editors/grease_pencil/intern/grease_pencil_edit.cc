4f0247e83ff8e4f22916dfeb0791abb4e08e7911
Antonio Vazquez <blendergit@gmail.com>
2023-11-24 12:50:14 +0100

    GPv3: Update include files

--- source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -16,7 +16,7 @@
 
 #include "DNA_material_types.h"
 
-#include "BKE_context.h"
+#include "BKE_context.hh"
 #include "BKE_curves_utils.hh"
 #include "BKE_grease_pencil.hh"
 #include "BKE_material.h"
