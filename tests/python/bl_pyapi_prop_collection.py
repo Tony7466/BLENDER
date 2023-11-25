16802c4bf319000fa2225dbd71e79516df80fac1
Thomas Barlow <github@mysterymayhem.co.uk>
2023-11-25 18:57:37 +0000

    Remove hardcoded buffer/sequence lengths from prop_collection tests

--- tests/python/bl_pyapi_prop_collection.py
+++ tests/python/bl_pyapi_prop_collection.py
@@ -44,15 +44,16 @@ class SequenceCheckBuffer(np.ndarray):
 
 
 class TestPropertyGroup(bpy.types.PropertyGroup):
+    VECTOR_SIZE = 3
     _ENUM_ITEMS = (("0", "Item 0", ""), ("1", "Item 1", ""))
     _ENUM_IDENTIFIER_TO_INDEX = {item[0]: i for i, item in enumerate(_ENUM_ITEMS)}
 
     test_bool: BoolProperty(default=False)
-    test_bool_vector: BoolVectorProperty(size=3, default=[False, False, False])
+    test_bool_vector: BoolVectorProperty(size=VECTOR_SIZE, default=[False, False, False])
     test_float: FloatProperty(default=-1.0)
-    test_float_vector: FloatVectorProperty(size=3, default=[-1.0, -2.0, -3.0])
+    test_float_vector: FloatVectorProperty(size=VECTOR_SIZE, default=[-1.0, -2.0, -3.0])
     test_int: IntProperty(default=-1)
-    test_int_vector: IntVectorProperty(size=3, default=[-1, -2, -3])
+    test_int_vector: IntVectorProperty(size=VECTOR_SIZE, default=[-1, -2, -3])
     test_unsigned_int: IntProperty(subtype='UNSIGNED', default=2 ** 31 - 1)
     test_enum: EnumProperty(items=_ENUM_ITEMS, default=_ENUM_ITEMS[0][0])
 
@@ -89,7 +90,9 @@ class TestPropCollectionForeachGetSet(unittest.TestCase):
         bpy.utils.register_class(TestPropertyGroup)
         id_type.test_collection = CollectionProperty(type=TestPropertyGroup)
         self.collection = id_inst.test_collection
-        for _ in range(5):
+        self.num_items = 5
+        self.num_vector_items = self.num_items * TestPropertyGroup.VECTOR_SIZE
+        for _ in range(self.num_items):
             id_inst.test_collection.add()
 
     def tearDown(self):
@@ -148,106 +151,106 @@ class TestPropCollectionForeachGetSet(unittest.TestCase):
         self.do_getset_test(*args, **kwargs, is_get=False)
 
     def test_buffer_get_bool(self):
-        self.do_get_test(np.full(5, True, dtype=bool), "test_bool")
+        self.do_get_test(np.full(self.num_items, True, dtype=bool), "test_bool")
 
     def test_buffer_set_bool(self):
-        self.do_set_test(np.full(5, True, dtype=bool), "test_bool")
+        self.do_set_test(np.full(self.num_items, True, dtype=bool), "test_bool")
 
     def test_sequence_get_bool(self):
-        self.do_get_test([None] * 5, "test_bool")
+        self.do_get_test([None] * self.num_items, "test_bool")
 
     def test_sequence_set_bool(self):
-        self.do_set_test([True] * 5, "test_bool")
+        self.do_set_test([True] * self.num_items, "test_bool")
 
     def test_buffer_get_bool_vector(self):
-        self.do_get_test(np.full(5 * 3, True, dtype=bool), "test_bool_vector")
+        self.do_get_test(np.full(self.num_vector_items, True, dtype=bool), "test_bool_vector")
 
     def test_buffer_set_bool_vector(self):
-        self.do_set_test(np.full(5 * 3, True, dtype=bool), "test_bool_vector")
+        self.do_set_test(np.full(self.num_vector_items, True, dtype=bool), "test_bool_vector")
 
     def test_sequence_get_bool_vector(self):
-        self.do_get_test([None] * (5 * 3), "test_bool_vector")
+        self.do_get_test([None] * self.num_vector_items, "test_bool_vector")
 
     def test_sequence_set_bool_vector(self):
-        self.do_set_test([True] * (5 * 3), "test_bool_vector")
+        self.do_set_test([True] * self.num_vector_items, "test_bool_vector")
 
     def test_buffer_get_float(self):
-        self.do_get_test(np.zeros(5, dtype=np.float32), "test_float")
+        self.do_get_test(np.zeros(self.num_items, dtype=np.float32), "test_float")
 
     def test_buffer_set_float(self):
-        self.do_set_test(np.arange(5, dtype=np.float32), "test_float")
+        self.do_set_test(np.arange(self.num_items, dtype=np.float32), "test_float")
 
     def test_sequence_get_float(self):
-        self.do_get_test([None] * 5, "test_float")
+        self.do_get_test([None] * self.num_items, "test_float")
 
     def test_sequence_set_float(self):
-        self.do_set_test(range(5), "test_float")
+        self.do_set_test(range(self.num_items), "test_float")
 
     def test_buffer_get_float_vector(self):
-        self.do_get_test(np.zeros(5 * 3, dtype=np.float32), "test_float_vector")
+        self.do_get_test(np.zeros(self.num_vector_items, dtype=np.float32), "test_float_vector")
 
     def test_buffer_set_float_vector(self):
-        self.do_set_test(np.arange(5 * 3, dtype=np.float32), "test_float_vector")
+        self.do_set_test(np.arange(self.num_vector_items, dtype=np.float32), "test_float_vector")
 
     def test_sequence_get_float_vector(self):
-        self.do_get_test([None] * (5 * 3), "test_float_vector")
+        self.do_get_test([None] * self.num_vector_items, "test_float_vector")
 
     def test_sequence_set_float_vector(self):
-        self.do_set_test(range(5 * 3), "test_float_vector")
+        self.do_set_test(range(self.num_vector_items), "test_float_vector")
 
     def test_buffer_get_int(self):
-        self.do_get_test(np.zeros(5, dtype=np.int32), "test_int")
+        self.do_get_test(np.zeros(self.num_items, dtype=np.int32), "test_int")
 
     def test_buffer_set_int(self):
-        self.do_set_test(np.arange(5, dtype=np.int32), "test_int")
+        self.do_set_test(np.arange(self.num_items, dtype=np.int32), "test_int")
 
     def test_sequence_get_int(self):
-        self.do_get_test([None] * 5, "test_int")
+        self.do_get_test([None] * self.num_items, "test_int")
 
     def test_sequence_set_int(self):
-        self.do_set_test(range(5), "test_int")
+        self.do_set_test(range(self.num_items), "test_int")
 
     def test_buffer_get_int_vector(self):
-        self.do_get_test(np.zeros(5 * 3, dtype=np.int32), "test_int_vector")
+        self.do_get_test(np.zeros(self.num_vector_items, dtype=np.int32), "test_int_vector")
 
     def test_buffer_set_int_vector(self):
-        self.do_set_test(np.arange(5 * 3, dtype=np.int32), "test_int_vector")
+        self.do_set_test(np.arange(self.num_vector_items, dtype=np.int32), "test_int_vector")
 
     def test_sequence_get_int_vector(self):
-        self.do_get_test([None] * (5 * 3), "test_int_vector")
+        self.do_get_test([None] * self.num_vector_items, "test_int_vector")
 
     def test_sequence_set_int_vector(self):
-        self.do_set_test(range(5 * 3), "test_int_vector")
+        self.do_set_test(range(self.num_vector_items), "test_int_vector")
 
     def test_buffer_get_unsigned_int(self):
-        self.do_get_test(np.zeros(5, dtype=np.uint32), "test_unsigned_int")
+        self.do_get_test(np.zeros(self.num_items, dtype=np.uint32), "test_unsigned_int")
 
     def test_buffer_set_unsigned_int(self):
-        self.do_set_test(np.arange(5, dtype=np.uint32), "test_unsigned_int")
+        self.do_set_test(np.arange(self.num_items, dtype=np.uint32), "test_unsigned_int")
 
     def test_sequence_get_unsigned_int(self):
-        self.do_get_test([None] * 5, "test_unsigned_int")
+        self.do_get_test([None] * self.num_items, "test_unsigned_int")
 
     def test_sequence_set_unsigned_int(self):
-        self.do_set_test(range(5), "test_unsigned_int")
+        self.do_set_test(range(self.num_items), "test_unsigned_int")
 
     @unittest.expectedFailure  # See #92621
     def test_buffer_get_enum(self):
         # Note: RNA enum properties commonly vary in itemsize, int32 may not be correct for other properties.
-        self.do_get_test(np.ones(5, dtype=np.int32), "test_enum")
+        self.do_get_test(np.ones(self.num_items, dtype=np.int32), "test_enum")
 
     @unittest.expectedFailure  # See #92621
     def test_buffer_set_enum(self):
         # Note: RNA enum properties commonly vary in itemsize, int32 may not be correct for other properties.
-        self.do_set_test(np.ones(5, dtype=np.int32), "test_enum")
+        self.do_set_test(np.ones(self.num_items, dtype=np.int32), "test_enum")
 
     @unittest.expectedFailure  # See #92621
     def test_sequence_get_enum(self):
-        self.do_get_test([None] * 5, "test_enum")
+        self.do_get_test([None] * self.num_items, "test_enum")
 
     @unittest.expectedFailure  # See #92621
     def test_sequence_set_enum(self):
-        self.do_get_test([1] * 5, "test_enum")
+        self.do_get_test([1] * self.num_items, "test_enum")
 
 
 if __name__ == '__main__':
