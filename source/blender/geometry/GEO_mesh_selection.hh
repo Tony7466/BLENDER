734278056a97d200f3554a0ef42b8c110912cecd
Iliya Katueshenock <modormoder@gmail.com>
2023-11-25 15:33:08 +0300

    progres

--- source/blender/geometry/GEO_mesh_selection.hh
+++ source/blender/geometry/GEO_mesh_selection.hh
@@ -12,40 +12,40 @@
 namespace blender::geometry {
 
 /** A vertex is selected if it's used by a selected edge. */
-IndexMask vert_selection_from_edge(const Span<int2> edges,
-                                  const IndexMask &edge_mask,
-                                  int verts_num,
-                                  IndexMaskMemory &memory);
+IndexMask vert_selection_from_edge(Span<int2> edges,
+                                   const IndexMask &edge_mask,
+                                   int verts_num,
+                                   IndexMaskMemory &memory);
 
 /** A vertex is selected if it is used by a selected face. */
-IndexMask vert_selection_from_face(const OffsetIndices<int> faces,
-                                  const IndexMask &face_mask,
-                                  const Span<int> corner_verts,
-                                  int verts_num,
-                                  IndexMaskMemory &memory);
+IndexMask vert_selection_from_face(OffsetIndices<int> faces,
+                                   const IndexMask &face_mask,
+                                   Span<int> corner_verts,
+                                   int verts_num,
+                                   IndexMaskMemory &memory);
 
 /** An edge is selected if it is used by a selected face. */
-IndexMask edge_selection_from_face(const OffsetIndices<int> faces,
-                                  const IndexMask &face_mask,
-                                  const Span<int> corner_edges,
-                                  int edges_num,
-                                  IndexMaskMemory &memory);
+IndexMask edge_selection_from_face(OffsetIndices<int> faces,
+                                   const IndexMask &face_mask,
+                                   Span<int> corner_edges,
+                                   int edges_num,
+                                   IndexMaskMemory &memory);
 
 /** An edge is selected if both of its vertices are selected. */
-IndexMask edge_selection_from_vert(const Span<int2> edges,
-                                  const Span<bool> vert_selection,
-                                  IndexMaskMemory &memory);
+IndexMask edge_selection_from_vert(Span<int2> edges,
+                                   Span<bool> vert_selection,
+                                   IndexMaskMemory &memory);
 
 /** A face is selected if all of its vertices are selected. */
-IndexMask face_selection_from_vert(const OffsetIndices<int> faces,
-                                  const Span<int> corner_verts,
-                                  const Span<bool> vert_selection,
-                                  IndexMaskMemory &memory);
+IndexMask face_selection_from_vert(OffsetIndices<int> faces,
+                                   Span<int> corner_verts,
+                                   Span<bool> vert_selection,
+                                   IndexMaskMemory &memory);
 
 /** A face is selected if all of its edges are selected. */
-IndexMask face_selection_from_edge(const OffsetIndices<int> faces,
-                                  const Span<int> corner_edges,
-                                  const Span<bool> edge_mask,
-                                  IndexMaskMemory &memory);
+IndexMask face_selection_from_edge(OffsetIndices<int> faces,
+                                   Span<int> corner_edges,
+                                   Span<bool> edge_mask,
+                                   IndexMaskMemory &memory);
 
 }  // namespace blender::geometry
