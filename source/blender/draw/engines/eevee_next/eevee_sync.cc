81ca6fe8dc400560a68637be9bb1af65fdb9dbe4
Miguel Pozo <pragma37@gmail.com>
2023-11-30 18:10:15 +0100

    Ensure updates are consumed

--- source/blender/draw/engines/eevee_next/eevee_sync.cc
+++ source/blender/draw/engines/eevee_next/eevee_sync.cc
@@ -36,8 +36,12 @@ namespace blender::eevee {
 
 void SyncModule::view_update()
 {
-  world_updated_ = DEG_id_type_updated(inst_.depsgraph, ID_WO);
-  scene_updated_ = DEG_id_type_updated(inst_.depsgraph, ID_SCE);
+  if (DEG_id_type_updated(inst_.depsgraph, ID_WO)) {
+    world_updated_ = true;
+  }
+  if (DEG_id_type_updated(inst_.depsgraph, ID_SCE)) {
+    scene_updated_ = true;
+  }
 }
 
 ObjectHandle &SyncModule::sync_object(const ObjectRef &ob_ref)
@@ -59,6 +63,7 @@ WorldHandle SyncModule::sync_world()
 {
   WorldHandle handle;
   handle.recalc = world_updated_ ? ID_RECALC_SHADING : 0;
+  world_updated_ = false;
   return handle;
 }
 
@@ -66,6 +71,7 @@ SceneHandle SyncModule::sync_scene()
 {
   SceneHandle handle;
   handle.recalc = scene_updated_ ? ID_RECALC_SHADING : 0;
+  scene_updated_ = false;
   return handle;
 }
 
