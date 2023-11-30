733d310051a630f138e28913c163b3b53662f889
Philipp Oeser <philipp@blender.org>
2023-11-30 15:00:02 +0100

    C++ style casting

--- source/blender/editors/space_node/space_node.cc
+++ source/blender/editors/space_node/space_node.cc
@@ -1156,8 +1156,8 @@ static void node_id_remap_cb(ID *old_id, ID *new_id, void *user_data)
   }
   else if (GS(old_id->name) == ID_NT) {
 
-    if ((ID *)snode->geometry_nodes_tool_tree == old_id) {
-      snode->geometry_nodes_tool_tree = (bNodeTree *)new_id;
+    if (&snode->geometry_nodes_tool_tree->id == old_id) {
+      snode->geometry_nodes_tool_tree = reinterpret_cast<bNodeTree *>(new_id);
     }
 
     bNodeTreePath *path, *path_next;
