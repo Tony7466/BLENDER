e03cb546f52c01bdb18a21d82aeae326c8d06bc6
Jeroen Bakker <jeroen@blender.org>
2023-11-30 11:21:34 +0100

    Smooth edges of spheres

--- source/blender/draw/engines/eevee_next/shaders/eevee_lookdev_display_frag.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_lookdev_display_frag.glsl
@@ -1,7 +1,14 @@
 void main()
 {
-  vec4 color = texture(color_tx, uv_coord, 0);
+  vec2 texture_size = textureSize(color_tx, 0).xy;
+  vec2 texel_size = vec2(1.0) / texture_size;
 
-  color.a = 1.0 - color.a;
+  float distance_from_center = distance(uv_coord.xy, vec2(0.5));
+  float smooth_size = texel_size.x * 1.5;
+  float smooth_offset = texel_size.x * 0.5;
+  float factor = (distance_from_center - 0.5 + smooth_offset + smooth_size) * (1.0 / smooth_size);
+  float alpha = clamp(1.0 - factor, 0.0, 1.0);
+
+  vec4 color = vec4(texture(color_tx, uv_coord, 0).rgb, alpha);
   out_color = color;
 }
