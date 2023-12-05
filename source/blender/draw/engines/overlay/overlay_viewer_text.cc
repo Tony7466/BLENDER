883907596196d498fa00ee64f5a64af02687e587
Julian Plak <julian.plak@live.nl>
2023-12-05 16:04:16 +0100

    Sample positions from domain

--- source/blender/draw/engines/overlay/overlay_viewer_text.cc
+++ source/blender/draw/engines/overlay/overlay_viewer_text.cc
@@ -23,8 +23,12 @@ void OVERLAY_viewer_attribute_text(const Object &object)
     case OB_MESH: {
       Mesh *mesh = static_cast<Mesh *>(object.data);
       if (mesh->attributes().contains(".viewer")) {
-        VArray<float> viewer_attributes = *mesh->attributes().lookup<float>(".viewer");
-        Span<float3> positions = mesh->vert_positions();
+        const AttributeReader viewer_attribute_reader = mesh->attributes().lookup<float>(
+            ".viewer");
+        VArray<float> viewer_attributes = *viewer_attribute_reader;
+        printf("viewer_attributes.size() = %lld\n", viewer_attributes.size());
+        VArraySpan<float3> positions = *mesh->attributes().lookup<float3>(
+            "position", viewer_attribute_reader.domain);
         DRW_text_viewer_attribute(viewer_attributes, positions, modelMatrix);
       }
       break;
@@ -32,8 +36,11 @@ void OVERLAY_viewer_attribute_text(const Object &object)
     case OB_POINTCLOUD: {
       PointCloud *pointcloud = static_cast<PointCloud *>(object.data);
       if (pointcloud->attributes().contains(".viewer")) {
-        VArray<float> viewer_attributes = *pointcloud->attributes().lookup<float>(".viewer");
-        Span<float3> positions = pointcloud->positions();
+        const AttributeReader viewer_attribute_reader = pointcloud->attributes().lookup<float>(
+            ".viewer");
+        VArray<float> viewer_attributes = *viewer_attribute_reader;
+        VArraySpan<float3> positions = *pointcloud->attributes().lookup<float3>(
+            "position", viewer_attribute_reader.domain);
         DRW_text_viewer_attribute(viewer_attributes, positions, modelMatrix);
       }
       break;
@@ -43,8 +50,11 @@ void OVERLAY_viewer_attribute_text(const Object &object)
       if (curve->curve_eval) {
         const bke::CurvesGeometry &curves = curve->curve_eval->geometry.wrap();
         if (curves.attributes().contains(".viewer")) {
-          VArray<float> viewer_attributes = *curves.attributes().lookup<float>(".viewer");
-          Span<float3> positions = curves.positions();
+          const AttributeReader viewer_attribute_reader = curves.attributes().lookup<float>(
+              ".viewer");
+          VArray<float> viewer_attributes = *viewer_attribute_reader;
+          VArraySpan<float3> positions = *curves.attributes().lookup<float3>(
+              "position", viewer_attribute_reader.domain);
           DRW_text_viewer_attribute(viewer_attributes, positions, modelMatrix);
         }
       }
@@ -54,8 +64,11 @@ void OVERLAY_viewer_attribute_text(const Object &object)
       Curves *curves_id = static_cast<Curves *>(object.data);
       const bke::CurvesGeometry &curves = curves_id->geometry.wrap();
       if (curves.attributes().contains(".viewer")) {
-        VArray<float> viewer_attributes = *curves.attributes().lookup<float>(".viewer");
-        Span<float3> positions = curves.positions();
+        const AttributeReader viewer_attribute_reader = curves.attributes().lookup<float>(
+            ".viewer");
+        VArray<float> viewer_attributes = *viewer_attribute_reader;
+        VArraySpan<float3> positions = *curves.attributes().lookup<float3>(
+            "position", viewer_attribute_reader.domain);
         DRW_text_viewer_attribute(viewer_attributes, positions, modelMatrix);
       }
       break;
