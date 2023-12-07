2d89853101cbc660bb1ff35418add0eeed7b0070
Jacques Lucke <jacques@blender.org>
2023-12-07 18:43:54 +0100

    add support for tagging for side effects in depsgraph

--- source/blender/editors/object/object_bake_simulation.cc
+++ source/blender/editors/object/object_bake_simulation.cc
@@ -242,7 +242,8 @@ static void request_bakes_in_modifier_cache(BakeGeometryNodesJob &job)
 {
   for (NodeBakeRequest &request : job.bake_requests) {
     request.nmd->runtime->cache->requested_bakes.add(request.bake_id);
-    DEG_id_tag_update(&request.object->id, ID_RECALC_GEOMETRY);
+    DEG_id_tag_update_for_side_effect_request(
+        job.depsgraph, &request.object->id, ID_RECALC_GEOMETRY);
   }
 }
 
