f1fc7ccfa832e22b3526339f25970e2b4bf031b3
Guillermo Venegas <guillermovcra@gmail.com>
2023-12-11 10:23:49 -0600

    init

--- source/blender/blenkernel/intern/file_handler.cc
+++ source/blender/blenkernel/intern/file_handler.cc
@@ -2,9 +2,26 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "BKE_file_handler.hh"
-
+#include "BLI_path_util.h"
 #include "BLI_string.h"
+
+#include "BLT_translation.h"
+
+#include "BKE_file_handler.hh"
+#include "BKE_report.h"
+
+#include "CLG_log.h"
+
+#include "WM_api.hh"
+#include "WM_types.hh"
+
+#include "RNA_access.hh"
+#include "RNA_prototypes.h"
+
+#include "fmt/format.h"
+
+static CLG_LogRef FH_LOG = {"bke.file_handler"};
+#define LOG (&FH_LOG)
 
 static blender::RawVector<std::unique_ptr<FileHandlerType>> &file_handlers()
 {
@@ -57,3 +74,237 @@ void BKE_file_handler_remove(FileHandlerType *file_handler)
         return test_file_handler.get() == file_handler;
       });
 }
+
+blender::Vector<FileHandlerType *> BKE_file_handlers_poll_file_drop(
+    const bContext *C, const blender::Span<std::string> paths, const bool quiet)
+{
+  blender::Vector<std::string> path_extensions;
+  for (const std::string &path : paths) {
+    const char *extension = BLI_path_extension(path.c_str());
+    if (!extension) {
+      continue;
+    }
+    path_extensions.append_non_duplicates(extension);
+  }
+
+  blender::Vector<FileHandlerType *> result;
+  for (const std::unique_ptr<FileHandlerType> &file_handler_ptr : file_handlers()) {
+    FileHandlerType &file_handler = *file_handler_ptr;
+    const auto &file_extensions = file_handler.file_extensions;
+
+    const bool support_any_extension = std::any_of(
+        file_extensions.begin(),
+        file_extensions.end(),
+        [&path_extensions](const std::string &test_extension) {
+          return path_extensions.contains(test_extension);
+        });
+
+    if (!support_any_extension) {
+      continue;
+    }
+
+    if (!(file_handler.poll_drop && file_handler.poll_drop(C, &file_handler))) {
+      continue;
+    }
+
+    /* File handles can be registered without a import operator, or the import operator can be
+     * removed and the file handler can still be registered, filter too file handles with valid
+     * import operators. */
+    if (!file_handler.get_import_operator(quiet)) {
+      continue;
+    }
+
+    result.append(&file_handler);
+  }
+  return result;
+}
+
+blender::Vector<int64_t> FileHandlerType::filter_supported_paths(
+    const blender::Span<std::string> paths) const
+{
+  blender::Vector<int64_t> indices;
+
+  for (const int idx : paths.index_range()) {
+    const char *extension = BLI_path_extension(paths[idx].c_str());
+    if (!extension) {
+      continue;
+    }
+    if (file_extensions.contains(extension)) {
+      indices.append(idx);
+    }
+  }
+  return indices;
+}
+
+wmOperatorType *FileHandlerType::get_import_operator(const bool quiet) const
+{
+  return WM_operatortype_find(import_operator, quiet);
+}
+
+static const char *property_type_str(PropertyType prop_type)
+{
+  switch (prop_type) {
+    case PROP_BOOLEAN: {
+      return "BooleanProperty";
+    }
+    case PROP_INT: {
+      return "IntProperty";
+    }
+    case PROP_FLOAT: {
+      return "FloatProperty";
+    }
+    case PROP_STRING: {
+      return "StringProperty";
+    }
+    case PROP_ENUM: {
+      return "EnumProperty";
+    }
+    case PROP_POINTER: {
+      return "PointerProperty";
+    }
+    case PROP_COLLECTION: {
+      return "CollectionProperty";
+    }
+    default:
+      BLI_assert_unreachable();
+      return nullptr;
+  }
+}
+
+/** Finds a property in the operator properties with a specific #name and #property_type */
+static PropertyRNA *find_property_type(PointerRNA &props,
+                                       const char *name,
+                                       const PropertyType property_type)
+{
+  PropertyRNA *prop = RNA_struct_find_property(&props, name);
+  if (!prop) {
+    return nullptr;
+  }
+
+  const PropertyType prop_type = RNA_property_type(prop);
+  if (prop_type == property_type) {
+    return prop;
+  }
+
+  if (prop_type != property_type) {
+    std::string message = fmt::format(TIP_("'{} : {}()' expected, got '{} : {}()'."),
+                                      name,
+                                      property_type_str(property_type),
+                                      name,
+                                      property_type_str(prop_type));
+    CLOG_WARN(&FH_LOG, message.c_str());
+  }
+  return nullptr;
+}
+
+/**
+ * Finds a collection property in the operator properties with a specific #name and #struct_type.
+ */
+static PropertyRNA *find_collection_property(PointerRNA &props,
+                                             const char *name,
+                                             const StructRNA *struct_type)
+{
+  PropertyRNA *prop = RNA_struct_find_property(&props, name);
+  if (!prop) {
+    return nullptr;
+  }
+
+  const PropertyType prop_type = RNA_property_type(prop);
+  const StructRNA *prop_struct_type = RNA_property_pointer_type(&props, prop);
+  if (prop_type == PROP_COLLECTION && prop_struct_type == struct_type) {
+    return prop;
+  }
+
+  if (prop_type != PROP_COLLECTION) {
+    std::string message = fmt::format(TIP_("'{} : {}(type = {})' expected, got '{} : {}()'."),
+                                      name,
+                                      property_type_str(PROP_COLLECTION),
+                                      RNA_struct_identifier(struct_type),
+                                      name,
+                                      property_type_str(prop_type));
+    CLOG_WARN(&FH_LOG, message.c_str());
+  }
+
+  if (prop_struct_type != struct_type) {
+    std::string message = fmt::format(
+        TIP_("'{} : {}(type = {})' expected, got '{} : {}(type = {})'."),
+        name,
+        property_type_str(PROP_COLLECTION),
+        RNA_struct_identifier(struct_type),
+        name,
+        property_type_str(PROP_COLLECTION),
+        RNA_struct_identifier(prop_struct_type));
+    CLOG_WARN(&FH_LOG, message.c_str());
+  }
+
+  return nullptr;
+}
+
+PointerRNA FileHandlerType::import_operator_create_ptr(
+    const blender::Span<std::string> paths) const
+{
+  BLI_assert(get_import_operator());
+  PointerRNA props;
+  wmOperatorType *op = get_import_operator();
+  WM_operator_properties_create_ptr(&props, op);
+
+  const auto suported_paths = filter_supported_paths(paths);
+
+  PropertyRNA *filepath_prop = find_property_type(props, "filepath", PROP_STRING);
+  if (filepath_prop) {
+    RNA_property_string_set(&props, filepath_prop, paths[suported_paths[0]].c_str());
+  }
+
+  PropertyRNA *directory_prop = find_property_type(props, "directory", PROP_STRING);
+  if (directory_prop) {
+    char dir[FILE_MAX];
+    BLI_path_split_dir_part(paths[0].c_str(), dir, sizeof(dir));
+    RNA_property_string_set(&props, directory_prop, dir);
+  }
+
+  PropertyRNA *files_prop = find_collection_property(props, "files", &RNA_OperatorFileListElement);
+  if (files_prop) {
+    RNA_property_collection_clear(&props, files_prop);
+    for (const auto &index : suported_paths) {
+      char file[FILE_MAX];
+      BLI_path_split_file_part(paths[index].c_str(), file, sizeof(file));
+
+      PointerRNA item_ptr{};
+      RNA_property_collection_add(&props, files_prop, &item_ptr);
+      RNA_string_set(&item_ptr, "name", file);
+    }
+  }
+
+  if (!filepath_prop && !directory_prop && !files_prop) {
+    std::string message = fmt::format(
+        TIP_("{}: import operator '{}' requires file path properties:\n"
+             "- For single files: 'filepath : StringProperty()'\n"
+             "- For multiple files: 'directory : StringProperty()' and"
+             "'files : CollectionProperty(type = OperatorFileListElement)'"),
+        idname,
+        op->idname);
+    CLOG_WARN(&FH_LOG, message.c_str());
+  }
+
+  if (directory_prop && !files_prop) {
+    std::string message = fmt::format(
+        TIP_("{}: import operator '{}' have 'directory : StringProperty()' but misses 'files : "
+             "CollectionProperty(type = OperatorFileListElement)'."),
+        idname,
+        op->idname);
+    CLOG_WARN(&FH_LOG, message.c_str());
+  }
+
+  if (!directory_prop && files_prop) {
+    std::string message = fmt::format(
+        TIP_("{}: Import operator '{}' have 'files : CollectionProperty(type = "
+             "OperatorFileListElement)' but misses 'directory : StringProperty()'."),
+        idname,
+        op->idname);
+    CLOG_WARN(&FH_LOG, message.c_str());
+  }
+
+  return props;
+}
+
+#undef LOG
---
+++ source/blender/editors/io/io_drop_import_file.cc
@@ -0,0 +1,189 @@
+/* SPDX-FileCopyrightText: 2023 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "BLI_path_util.h"
+#include "BLI_string.h"
+
+#include "BLT_translation.h"
+
+#include "BKE_file_handler.hh"
+
+#include "RNA_access.hh"
+#include "RNA_define.hh"
+#include "RNA_prototypes.h"
+
+#include "WM_api.hh"
+#include "WM_types.hh"
+
+#include "UI_interface.hh"
+
+#include "io_drop_import_file.hh"
+
+/* Retuns the list of file paths stored in #WM_OT_drop_import_file operator properties. */
+static blender::Vector<std::string> drop_import_file_paths(const wmOperator *op)
+{
+  blender::Vector<std::string> result;
+  char dir[FILE_MAX], file[FILE_MAX];
+
+  RNA_string_get(op->ptr, "directory", dir);
+
+  PropertyRNA *prop = RNA_struct_find_property(op->ptr, "files");
+  int files_len = RNA_property_collection_length(op->ptr, prop);
+
+  for (int i = 0; i < files_len; i++) {
+    PointerRNA fileptr;
+    RNA_property_collection_lookup_int(op->ptr, prop, i, &fileptr);
+    RNA_string_get(&fileptr, "name", file);
+    char file_path[FILE_MAX];
+    BLI_path_join(file_path, sizeof(file_path), dir, file);
+    result.append(file_path);
+  }
+  return result;
+}
+
+static int wm_drop_import_file_exec(bContext *C, wmOperator *op)
+{
+  auto paths = drop_import_file_paths(op);
+  if (paths.size() < 1) {
+    return OPERATOR_CANCELLED;
+  }
+
+  auto file_handlers = BKE_file_handlers_poll_file_drop(C, paths);
+  if (file_handlers.size() == 0) {
+    return OPERATOR_CANCELLED;
+  }
+
+  PointerRNA file_props = file_handlers[0]->import_operator_create_ptr(paths);
+
+  WM_operator_name_call_ptr(
+      C, file_handlers[0]->get_import_operator(), WM_OP_INVOKE_DEFAULT, &file_props, nullptr);
+  WM_operator_properties_free(&file_props);
+  return OPERATOR_FINISHED;
+}
+
+static int wm_drop_import_file_invoke(bContext *C, wmOperator *op, const wmEvent * /*event*/)
+{
+  const auto paths = drop_import_file_paths(op);
+  if (paths.size() < 1) {
+    return OPERATOR_CANCELLED;
+  }
+
+  auto file_handlers = BKE_file_handlers_poll_file_drop(C, paths);
+  if (file_handlers.size() == 1) {
+    return wm_drop_import_file_exec(C, op);
+  }
+
+  /**
+   * Create a menu with all file handler import operators that can support any files in paths and
+   * let user decide which to use.
+   */
+  uiPopupMenu *pup = UI_popup_menu_begin(C, "", ICON_NONE);
+  uiLayout *layout = UI_popup_menu_layout(pup);
+  uiLayoutSetOperatorContext(layout, WM_OP_INVOKE_DEFAULT);
+
+  for (auto *file_handler : file_handlers) {
+    const PointerRNA file_props = file_handler->import_operator_create_ptr(paths);
+    uiItemFullO_ptr(layout,
+                    file_handler->get_import_operator(),
+                    TIP_(file_handlers[0]->get_import_operator()->name),
+                    ICON_NONE,
+                    static_cast<IDProperty *>(file_props.data),
+                    WM_OP_INVOKE_DEFAULT,
+                    UI_ITEM_NONE,
+                    nullptr);
+  }
+
+  UI_popup_menu_end(C, pup);
+  return OPERATOR_INTERFACE;
+}
+
+void WM_OT_drop_import_file(wmOperatorType *ot)
+{
+  ot->name = "Drop to Import File";
+  ot->description = "Operator that allows file handlers to receive file drops";
+  ot->idname = "WM_OT_drop_import_file";
+  ot->flag = OPTYPE_INTERNAL;
+  ot->exec = wm_drop_import_file_exec;
+  ot->invoke = wm_drop_import_file_invoke;
+
+  PropertyRNA *prop;
+
+  prop = RNA_def_string_dir_path(
+      ot->srna, "directory", nullptr, FILE_MAX, "Directory", "Directory of the file");
+  RNA_def_property_flag(prop, PROP_HIDDEN | PROP_SKIP_SAVE);
+
+  prop = RNA_def_collection_runtime(ot->srna, "files", &RNA_OperatorFileListElement, "Files", "");
+  RNA_def_property_flag(prop, PROP_HIDDEN | PROP_SKIP_SAVE);
+}
+
+void drop_import_file_copy(bContext * /*C*/, wmDrag *drag, wmDropBox *drop)
+{
+#if 0
+  const auto paths = WM_drag_get_paths(drag);
+#else
+  std::string file_path(WM_drag_get_path(drag));
+  const auto paths = blender::Span<std::string>(&file_path, 1);
+#endif
+
+  char dir[FILE_MAX];
+  BLI_path_split_dir_part(paths[0].c_str(), dir, sizeof(dir));
+  RNA_string_set(drop->ptr, "directory", dir);
+
+  RNA_collection_clear(drop->ptr, "files");
+  for (const auto &path : paths) {
+    char file[FILE_MAX];
+    BLI_path_split_file_part(path.c_str(), file, sizeof(file));
+
+    PointerRNA itemptr{};
+    RNA_collection_add(drop->ptr, "files", &itemptr);
+    RNA_string_set(&itemptr, "name", file);
+  }
+}
+
+static bool drop_import_file_poll(bContext *C, wmDrag *drag, const wmEvent * /*event*/)
+{
+  if (drag->type != WM_DRAG_PATH) {
+    return false;
+  }
+#if 0
+  const auto paths = WM_drag_get_paths(drag);
+#else
+  std::string file_path(WM_drag_get_path(drag));
+  const auto paths = blender::Span<std::string>(&file_path, 1);
+#endif
+
+  return BKE_file_handlers_poll_file_drop(C, paths, true).size() > 0;
+}
+
+static char *drop_import_file_tooltip(bContext *C,
+                                      wmDrag *drag,
+                                      const int /*xy*/[2],
+                                      wmDropBox * /*drop*/)
+{
+#if 0
+const auto paths = WM_drag_get_paths(drag);
+#else
+  std::string file_path(WM_drag_get_path(drag));
+  const auto paths = blender::Span<std::string>(&file_path, 1);
+#endif
+
+  const auto file_handlers = BKE_file_handlers_poll_file_drop(C, paths, true);
+
+  if (file_handlers.size() == 1) {
+    return BLI_strdup(TIP_(file_handlers[0]->get_import_operator()->name));
+  }
+
+  return BLI_strdup(TIP_("Multiple file handlers can be used, drop to pick which to use"));
+}
+
+void ED_dropbox_drop_import_file()
+{
+  ListBase *lb = WM_dropboxmap_find("Window", 0, 0);
+  WM_dropbox_add(lb,
+                 "WM_OT_drop_import_file",
+                 drop_import_file_poll,
+                 drop_import_file_copy,
+                 nullptr,
+                 drop_import_file_tooltip);
+}
