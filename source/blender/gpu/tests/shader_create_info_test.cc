7000ad460d8172679dfd3ae5cbf377927e39652c
Jeroen Bakker <jeroen@blender.org>
2023-12-12 09:20:13 +0100

    formatting

--- source/blender/gpu/tests/shader_create_info_test.cc
+++ source/blender/gpu/tests/shader_create_info_test.cc
@@ -13,11 +13,12 @@ namespace blender::gpu::tests {
  * Test if all static shaders can be compiled.
  *
  * \NOTE: OpenGL is not tested as this test is enabled on the buildbot that isn't capable to run
- *        OpenGL tests.
- * \NOTE: Vulkan is not tested as it requires GHOST system creation without Wayland or X11 on Linux.
- *        This should be possible as we support headless rendering. On Windows it requires a software
- *        vulkan driver to be installed but that requires some software packages to be installed on
- *        the Windows build-bot machines.
+ * OpenGL tests.
+ *
+ * \NOTE: Vulkan is not tested as it requires GHOST system creation without Wayland or X11 on
+ * Linux. This should be possible as we support headless rendering. On Windows it requires a
+ * software vulkan driver to be installed but that requires some software packages to be installed
+ * on the Windows build-bot machines.
  */
 static void test_static_shaders()
 {
