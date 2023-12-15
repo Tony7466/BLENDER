96b226b360dfc7602771092ea7b983c4ddc4ad76
Casey Bianco-Davis <caseycasey739@gmail.com>
2023-12-15 11:30:41 -0800

    GPv3: Sample color

--- scripts/startup/bl_ui/space_view3d.py
+++ scripts/startup/bl_ui/space_view3d.py
@@ -2179,6 +2179,10 @@ class VIEW3D_MT_paint_grease_pencil(Menu):
 
         layout.menu("VIEW3D_MT_edit_greasepencil_showhide")
 
+        layout.separator()
+
+        layout.operator("paint.sample_color")
+
 
 class VIEW3D_MT_paint_gpencil(Menu):
     bl_label = "Paint"
