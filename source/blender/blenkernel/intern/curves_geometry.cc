c67630b1dedf9cd0c692941bde5383c07e41743b
Hans Goudey <hans@blender.org>
2023-12-18 10:22:38 -0500

    Cleanup

--- source/blender/blenkernel/intern/curves_geometry.cc
+++ source/blender/blenkernel/intern/curves_geometry.cc
@@ -940,9 +940,8 @@ void CurvesGeometry::interpolate_to_evaluated(const int curve_index,
                                               GMutableSpan dst) const
 {
   const bke::CurvesGeometryRuntime &runtime = *this->runtime;
-  const OffsetIndices points_by_curve = this->points_by_curve();
   const EvalData eval_data{
-      points_by_curve,
+      this->points_by_curve(),
       this->curve_types(),
       this->cyclic(),
       this->resolution(),
@@ -951,8 +950,7 @@ void CurvesGeometry::interpolate_to_evaluated(const int curve_index,
       this->nurbs_orders(),
       this->nurbs_weights(),
   };
-  const IndexRange points = points_by_curve[curve_index];
-  BLI_assert(src.size() == points.size());
+  BLI_assert(src.size() == this->points_by_curve().size());
   BLI_assert(dst.size() == this->evaluated_points_by_curve()[curve_index].size());
   evaluate_generic_data_for_curve(eval_data, curve_index, src, dst);
 }
