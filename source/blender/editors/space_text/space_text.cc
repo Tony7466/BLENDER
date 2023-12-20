e72480558cf588e3fc8351c86e710421f9c54d08
Guillermo Venegas <guillermovcra@gmail.com>
2023-12-20 12:15:14 -0600

    fix

--- source/blender/editors/space_text/space_text.cc
+++ source/blender/editors/space_text/space_text.cc
@@ -377,6 +377,16 @@ static void text_properties_region_init(wmWindowManager *wm, ARegion *region)
 static void text_properties_region_draw(const bContext *C, ARegion *region)
 {
   ED_region_panels(C, region);
+
+  SpaceText *st = CTX_wm_space_text(C);
+  /** This flag trick is make sure buttons have been added already. */
+  if (st->flags & ST_FIND_ACTIVATE) {
+    if (UI_textbutton_activate_rna(C, region, st, "find_text")) {
+      /** Draw again so `find_text` text button is highlighted as active. */
+      ED_region_panels(C, region);
+    }
+    st->flags &= ~ST_FIND_ACTIVATE;
+  }
 }
 
 static void text_id_remap(ScrArea * /*area*/, SpaceLink *slink, const IDRemapper *mappings)
