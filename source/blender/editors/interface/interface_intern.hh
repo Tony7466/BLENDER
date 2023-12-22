a078d4d588e8b118e62528cb49e87e47ee65c671
Jacques Lucke <jacques@blender.org>
2023-12-22 15:25:57 +0100

    fix/cleanup after merge from remote

--- source/blender/editors/interface/interface_intern.hh
+++ source/blender/editors/interface/interface_intern.hh
@@ -61,10 +61,6 @@ struct wmTimer;
 #define UI_MENU_SCROLL_MOUSE (UI_MENU_SCROLL_ARROW + 2 * UI_SCALE_FAC)
 #define UI_MENU_SCROLL_PAD (4 * UI_SCALE_FAC)
 
-/* Panels. */
-#define UI_LAYOUT_PANEL_Y_OFFSET (4.0f * UI_SCALE_FAC)
-#define UI_LAYOUT_PANEL_Y_PADDING (8.0f * UI_SCALE_FAC)
-
 /** Popover width (multiplied by #U.widget_unit) */
 #define UI_POPOVER_WIDTH_UNITS 10
 
