ce4a0d0b5310a338a53931d75d34fa545c5b0771
AryeComb <129656622+AryeComb@users.noreply.github.com>
2023-12-25 15:08:50 +0200

    Add a new function to the join geometry header

--- source/blender/geometry/GEO_join_geometries.hh
+++ source/blender/geometry/GEO_join_geometries.hh
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #pragma once
-
+#include "BLI_math_matrix_types.hh"
 #include "BKE_anonymous_attribute_id.hh"
 #include "BKE_geometry_set.hh"
 
@@ -12,4 +12,7 @@ namespace blender::geometry {
 bke::GeometrySet join_geometries(Span<bke::GeometrySet> geometries,
                                  const bke::AnonymousAttributePropagationInfo &propagation_info);
 
+void join_transform_instance_components(Span<const bke::InstancesComponent *> src_components,
+                                        Span<blender::float4x4> src_base_transforms,
+                                        bke::GeometrySet &result);
 }
