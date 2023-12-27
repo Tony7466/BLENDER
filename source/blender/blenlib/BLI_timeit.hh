310f79e465453d83caa61e9fef306c6cdcc8e9f6
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2023-12-27 13:34:53 -0800

    Extending SCOPED_TIMER_AVERAGED

--- source/blender/blenlib/BLI_timeit.hh
+++ source/blender/blenlib/BLI_timeit.hh
@@ -6,7 +6,9 @@
 
 #include <chrono>
 #include <string>
+#include <mutex>
 
+#include "BLI_map.hh"
 #include "BLI_sys_types.h"
 
 namespace blender::timeit {
@@ -16,6 +18,29 @@ using TimePoint = Clock::time_point;
 using Nanoseconds = std::chrono::nanoseconds;
 
 void print_duration(Nanoseconds duration);
+
+template <typename U, typename V>
+class LockedMap: public blender::Map<U, V> {
+ private:
+  std::mutex mutex_;
+
+ public:
+  LockedMap() = default;
+
+  V lookup_or_add(const U &key, const V &value) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return blender::Map<U, V>::lookup_or_add(key, value);
+  }
+  V lookup(const U &key) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    V value = blender::Map<U, V>::lookup(key);
+    return value;
+  }
+  bool add_overwrite(const U &key, const V &value) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return blender::Map<U, V>::add_overwrite(key, value);
+  }
+};
 
 class ScopedTimer {
  private:
@@ -31,24 +56,24 @@ class ScopedTimer {
   ~ScopedTimer();
 };
 
+struct TimerMapEntry {
+  int64_t total_count;
+  blender::timeit::Nanoseconds total_time;
+  blender::timeit::Nanoseconds min_time;
+  TimePoint last_print;
+};
+
+using TimerMap = LockedMap<std::string, TimerMapEntry>;
+
 class ScopedTimerAveraged {
  private:
   std::string name_;
   TimePoint start_;
-
-  int64_t &total_count_;
-  Nanoseconds &total_time_;
-  Nanoseconds &min_time_;
+  TimerMap &map_;
 
  public:
-  ScopedTimerAveraged(std::string name,
-                      int64_t &total_count,
-                      Nanoseconds &total_time,
-                      Nanoseconds &min_time)
-      : name_(std::move(name)),
-        total_count_(total_count),
-        total_time_(total_time),
-        min_time_(min_time)
+  ScopedTimerAveraged(std::string name,TimerMap& map)
+      : name_(std::move(name)), map_(map)
   {
     start_ = Clock::now();
   }
@@ -61,11 +86,18 @@ class ScopedTimerAveraged {
 #define SCOPED_TIMER(name) blender::timeit::ScopedTimer scoped_timer(name)
 
 /**
+ * ## is normally evaluated before __LINE__,
+ * and we have to go through these contortions to use __LINE__ in variable names.
+ */
+#define TIMER_CONCAT2(X, Y) X##Y
+#define TIMER_CONCAT(X, Y) TIMER_CONCAT2(X, Y)
+
+/**
  * Print the average and minimum runtime of the timer's scope.
  * \warning This uses static variables, so it is not thread-safe.
  */
+
 #define SCOPED_TIMER_AVERAGED(name) \
-  static int64_t total_count_; \
-  static blender::timeit::Nanoseconds total_time_; \
-  static blender::timeit::Nanoseconds min_time_ = blender::timeit::Nanoseconds::max(); \
-  blender::timeit::ScopedTimerAveraged scoped_timer(name, total_count_, total_time_, min_time_)
+  static blender::timeit::TimerMap TIMER_CONCAT(timer_map_, __LINE__); \
+  blender::timeit::ScopedTimerAveraged TIMER_CONCAT(scoped_timer, __LINE__)(name, \
+      TIMER_CONCAT(timer_map_, __LINE__));
--- source/blender/blenlib/intern/timeit.cc
+++ source/blender/blenlib/intern/timeit.cc
@@ -61,20 +61,37 @@ ScopedTimer::~ScopedTimer()
 ScopedTimerAveraged::~ScopedTimerAveraged()
 {
   const TimePoint end = Clock::now();
-  const Nanoseconds duration = end - start_;
 
-  total_count_++;
-  total_time_ += duration;
-  min_time_ = std::min(duration, min_time_);
+  TimerMapEntry state{-1, blender::timeit::Nanoseconds::zero(), blender::timeit::Nanoseconds::max(), start_ - std::chrono::seconds(1)};
+  state = map_.lookup_or_add(name_, state);
+  if (state.total_count < 0) {
+    state.total_count = 0;
+    map_.add_overwrite(name_, state);
+    // First hit; discard the measurement and return.
+    return;
+  }
+
+  const Nanoseconds duration = end - start_;
+  state.total_count++;
+  state.total_time += duration;
+  state.min_time = std::min(duration, state.min_time);
+
+  // Each timer instance only prints at most once per second.
+  if ((end - state.last_print) < std::chrono::seconds(1)) {
+    map_.add_overwrite(name_, state);
+    return;
+  }
+
+  map_.add_overwrite(name_, state);
 
   fmt::memory_buffer buf;
   fmt::format_to(fmt::appender(buf), FMT_STRING("Timer '{}': (Average: "), name_);
-  format_duration(total_time_ / total_count_, buf);
+  format_duration(state.total_time / state.total_count, buf);
   buf.append(StringRef(", Min: "));
-  format_duration(min_time_, buf);
+  format_duration(state.min_time, buf);
   buf.append(StringRef(", Last: "));
   format_duration(duration, buf);
-  fmt::format_to(fmt::appender(buf), ", Samples: {})\n", total_count_);
+  fmt::format_to(fmt::appender(buf), ", Samples: {})\n", state.total_count);
   std::cout << StringRef(buf.data(), buf.size());
 }
 
