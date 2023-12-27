6a0ea942d4315112121aa7e5ea2e9fbd95c1c8bf
Clément Foucault <foucault.clem@gmail.com>
2023-12-27 18:16:31 +1300

    Fix previous commit

--- source/blender/draw/engines/eevee_next/eevee_hizbuffer.cc
+++ source/blender/draw/engines/eevee_next/eevee_hizbuffer.cc
@@ -40,7 +40,7 @@ void HiZBuffer::sync()
     PassSimple &pass = hiz_update_ps_;
     GPUShader *sh = inst_.shaders.static_shader_get(HIZ_UPDATE);
     pass.init();
-    pass.specialize_constant(sh, "SC_update_mip_0", update_mip_0);
+    pass.specialize_constant(sh, "update_mip_0", update_mip_0);
     pass.shader_set(sh);
     pass.bind_ssbo("finished_tile_counter", atomic_tile_counter_);
     /* TODO(fclem): Should be a parameter to avoid confusion. */
@@ -52,9 +52,6 @@ void HiZBuffer::sync()
     pass.bind_image("out_mip_4", &hiz_mip_ref_[4]);
     pass.bind_image("out_mip_5", &hiz_mip_ref_[5]);
     pass.bind_image("out_mip_6", &hiz_mip_ref_[6]);
-    /* TODO(@fclem): There might be occasions where we might not want to
-     * copy mip 0 for performance reasons if there is no need for it. */
-    pass.push_constant("update_mip_0", true);
     pass.dispatch(int3(dispatch_size, 1));
     pass.barrier(GPU_BARRIER_TEXTURE_FETCH);
   }
@@ -62,7 +59,7 @@ void HiZBuffer::sync()
     PassSimple &pass = hiz_update_layer_ps_;
     GPUShader *sh = inst_.shaders.static_shader_get(HIZ_UPDATE_LAYER);
     pass.init();
-    pass.specialize_constant(sh, "SC_update_mip_0", update_mip_0);
+    pass.specialize_constant(sh, "update_mip_0", update_mip_0);
     pass.shader_set(sh);
     pass.bind_ssbo("finished_tile_counter", atomic_tile_counter_);
     /* TODO(fclem): Should be a parameter to avoid confusion. */
@@ -74,9 +71,6 @@ void HiZBuffer::sync()
     pass.bind_image("out_mip_4", &hiz_mip_ref_[4]);
     pass.bind_image("out_mip_5", &hiz_mip_ref_[5]);
     pass.bind_image("out_mip_6", &hiz_mip_ref_[6]);
-    /* TODO(@fclem): There might be occasions where we might not want to
-     * copy mip 0 for performance reasons if there is no need for it. */
-    pass.push_constant("update_mip_0", true);
     pass.push_constant("layer_id", &layer_id_);
     pass.dispatch(int3(dispatch_size, 1));
     pass.barrier(GPU_BARRIER_TEXTURE_FETCH);
