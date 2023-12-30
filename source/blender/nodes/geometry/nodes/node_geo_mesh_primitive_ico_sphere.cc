e756b9cfc35d5b1f98e0162ab80c17478daa47b7
Iliya Katueshenock <modormoder@gmail.com>
2023-12-31 02:15:29 +0300

    init

--- source/blender/nodes/geometry/nodes/node_geo_mesh_primitive_ico_sphere.cc
+++ source/blender/nodes/geometry/nodes/node_geo_mesh_primitive_ico_sphere.cc
@@ -2,11 +2,22 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <array>
+
 #include "DNA_mesh_types.h"
 
 #include "BKE_lib_id.h"
 #include "BKE_material.h"
 #include "BKE_mesh.hh"
+
+#include "BLI_array_utils.hh"
+#include "BLI_math_base.h"
+#include "BLI_math_base.hh"
+#include "BLI_math_euler.hh"
+#include "BLI_math_quaternion.hh"
+#include "BLI_math_rotation.hh"
+#include "BLI_math_rotation_types.hh"
+#include "BLI_span.hh"
 
 #include "GEO_randomize.hh"
 
@@ -28,6 +39,9 @@ static void node_declare(NodeDeclarationBuilder &b)
       .min(1)
       .max(7)
       .description("Number of subdivisions on top of the basic icosahedron");
+
+  b.add_input<decl::Bool>("New");
+
   b.add_output<decl::Geometry>("Mesh");
   b.add_output<decl::Vector>("UV Map").field_on_all();
 }
@@ -59,6 +73,12 @@ static Bounds<float3> calculate_bounds_ico_sphere(const float radius, const int 
 
   return {bounds_min, bounds_max};
 }
+
+static constexpr int base_verts_num = 12;
+static constexpr int latitude_num = (base_verts_num - 2) / 2;
+static constexpr int base_edges_num = 30;
+static constexpr int base_faces_num = 20;
+static constexpr int base_face_quads = base_faces_num / 2;
 
 static Mesh *create_ico_sphere_mesh(const int subdivisions,
                                     const float radius,
@@ -118,10 +138,63 @@ static Mesh *create_ico_sphere_mesh(const int subdivisions,
   return mesh;
 }
 
+static void base_ico_sphere_positions(const float radius, MutableSpan<float3> positions)
+{
+  positions.first() = float3(0.0f, 0.0f, 1.0f);
+
+  const float latitude = math::atan(0.5f);
+
+  MutableSpan<float3> top_latitude = positions.drop_front(1).take_front(latitude_num);
+  MutableSpan<float3> bottom_latitude = positions.drop_back(1).take_back(latitude_num);
+
+  const constexpr float step = M_PI / latitude_num * 2.0f;
+  const float3 base_vector(1.0f, 0.0f, 0.0f);
+  for (const int i : IndexRange(latitude_num)) {
+    const math::EulerXYZ rotation(0.0f, latitude, (i + 0.5f) * step);
+    top_latitude[i] = math::transform_point(math::to_quaternion(rotation), base_vector);
+  }
+  for (const int i : IndexRange(latitude_num)) {
+    const math::EulerXYZ rotation(0.0f, -latitude, i * step);
+    bottom_latitude[i] = math::transform_point(math::to_quaternion(rotation), base_vector);
+  }
+
+  positions.last() = float3(0.0f, 0.0f, -1.0f);
+
+  for (float3 &position : positions) {
+    position *= radius;
+  }
+}
+
+static Mesh *ico_sphere(const int subdivisions, const float radius)
+{
+  const int subdiv_verts_num = math::max<int>(0, math::pow<int>(2, subdivisions - 1) + 1 - 2);
+  const int verts_num = base_verts_num + (base_edges_num - base_face_quads) * subdiv_verts_num +
+                        base_face_quads * subdiv_verts_num * subdiv_verts_num;
+
+  Mesh *mesh = BKE_mesh_new_nomain(verts_num, 0, 0, 0);
+
+  MutableSpan<float3> positions = mesh->vert_positions_for_write();
+
+  std::array<float3, base_verts_num> base_positions;
+  base_ico_sphere_positions(radius, base_positions);
+
+  array_utils::copy(Span<float3>(base_positions), positions.take_front(base_verts_num));
+
+  return mesh;
+}
+
 static void node_geo_exec(GeoNodeExecParams params)
 {
   const int subdivisions = std::min(params.extract_input<int>("Subdivisions"), 10);
   const float radius = params.extract_input<float>("Radius");
+
+  const bool new_type = params.extract_input<bool>("New");
+
+  if (new_type) {
+    Mesh *mesh = ico_sphere(subdivisions, radius);
+    params.set_output("Mesh", GeometrySet::from_mesh(mesh));
+    return;
+  }
 
   AnonymousAttributeIDPtr uv_map_id = params.get_output_anonymous_attribute_id_if_needed("UV Map");
 
