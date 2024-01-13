ef3ba6dfa2cb812ab2d0bde00287fbf9b2ec64f4
laurynas <laduem@gmail.com>
2024-01-13 18:53:54 +0200

    Fix: Curves extrude with all points selected
    CurvesGeometry has no ".selected" attribute when all control points are selected.
    The earlier code assumed that ".selected" always exists.
    Also Python test is added for "extrude" operator.

--- source/blender/editors/curves/intern/curves_extrude.cc
+++ source/blender/editors/curves/intern/curves_extrude.cc
@@ -17,7 +17,7 @@ namespace blender::ed::curves {
 
 /**
  * Merges copy intervals at curve endings to minimize number of copy operations.
- * For example above intervals [0, 3, 4, 4, 4] became [0, 4, 4].
+ * For example given in function 'extrude_curves' intervals [0, 3, 4, 4, 4] became [0, 4, 4].
  * Leading to only two copy operations.
  */
 static Span<int> compress_intervals(const Span<IndexRange> curve_interval_ranges,
@@ -263,7 +263,8 @@ static void extrude_curves(Curves &curves_id)
   Array<IndexRange> curve_interval_ranges(curves_num);
 
   /* Per curve boolean indicating if first interval in a curve is selected.
-   * Other can be calculated as in a curve two adjacent intervals can have same selection state. */
+   * Other can be calculated as in a curve two adjacent intervals can not have same selection
+   * state. */
   Array<bool> is_first_selected(curves_num);
 
   calc_curves_extrusion(extruded_points,
@@ -276,7 +277,9 @@ static void extrude_curves(Curves &curves_id)
   new_curves.resize(new_offsets.last(), new_curves.curves_num());
 
   const bke::AttributeAccessor src_attributes = curves.attributes();
-  const GVArraySpan src_selection = *src_attributes.lookup(".selection", bke::AttrDomain::Point);
+  const bool true_ = true;
+  const GVArraySpan src_selection = *src_attributes.lookup_or_default(
+      ".selection", bke::AttrDomain::Point, CD_PROP_BOOL, &true_);
   const CPPType &src_selection_type = src_selection.type();
   bke::GSpanAttributeWriter dst_selection = ensure_selection_attribute(
       new_curves,
