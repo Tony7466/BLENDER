274eff5a2d4473af74bc6fd80fabcd043e2d72e9
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-01-16 11:52:23 +0100

    remove comment

--- source/blender/python/intern/bpy_rna_anim.cc
+++ source/blender/python/intern/bpy_rna_anim.cc
@@ -339,11 +339,7 @@ PyObject *pyrna_struct_keyframe_insert(BPy_StructRNA *self, PyObject *args, PyOb
 
   /* This assumes that keyframes are only added on original data & using the active depsgraph. If
    * it turns out to be necessary for some reason to insert keyframes on evaluated objects, we can
-   * revisit this and add an explicit `depsgraph` keyword argument to the function call.
-   *
-   * It is unlikely that driver code (which is the reason this depsgraph pointer is obtained) will
-   * be executed from this function call, as this only happens when `options` has
-   * `INSERTKEY_DRIVER`, which is not exposed to Python. */
+   * revisit this and add an explicit `depsgraph` keyword argument to the function call. */
   bContext *C = BPY_context_get();
   Depsgraph *depsgraph = CTX_data_depsgraph_pointer(C);
   const AnimationEvalContext anim_eval_context = BKE_animsys_eval_context_construct(depsgraph,
