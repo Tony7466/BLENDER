a73ba9308ea234e25fedd0f5d627a4379676b06e
Brecht Van Lommel <brecht@blender.org>
2024-01-24 17:31:04 +0100

    Address review comments

--- build_files/build_environment/cmake/check_compilers.cmake
+++ build_files/build_environment/cmake/check_compilers.cmake
@@ -2,12 +2,6 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-# Compiler version used for precompiled library builds used for official releases.
-
-SET(RELEASE_GCC_VERSION 11.2.*)
-SET(RELEASE_CUDA_VERSION 12.3.*)
-SET(RELEASE_HIP_VERSION 5.7.*)
-
 # Check against installed versions.
 
 message(STATUS "Found C Compiler: ${CMAKE_C_COMPILER_ID} ${CMAKE_C_COMPILER_VERSION}")
@@ -37,13 +31,13 @@ if(NOT APPLE)
     endif()
   endif()
 
-  check_language(HIP)
-  if (NOT CMAKE_HIP_COMPILER)
+
+  find_package(HIP QUIET)
+  if (NOT HIP_FOUND)
     message(STATUS "Missing HIP compiler")
   else()
-    enable_language(HIP)
-    message(STATUS "Found HIP Compiler: ${CMAKE_HIP_COMPILER_ID} ${CMAKE_HIP_COMPILER_VERSION}")
-    if(NOT CMAKE_HIP_COMPILER_VERSION MATCHES ${RELEASE_HIP_VERSION})
+    message(STATUS "Found HIP Compiler: ${HIP_HIPCC_EXECUTABLE} ${HIP_VERSION}")
+    if(NOT HIP_VERSION MATCHES ${RELEASE_HIP_VERSION})
       message(STATUS "  NOTE: Official releases uses HIP ${RELEASE_HIP_VERSION}")
     endif()
   endif()
