Tony7466/BLENDER

commit 7ca4dcac5a113db514faf7362b95bfe584e252be

Cleanup: Use std::string for UI drop box tooltip return type
And use fmt for formatting strings.

source/blender/editors/space_outliner/outliner_dragdrop.cc
@@ -892,34 +892,28 @@ static bool datastack_drop_poll(bContext *C, wmDrag *drag, const wmEvent *event)
   return true;
 }
 
-static char *datastack_drop_tooltip(bContext * /*C*/,
-                                    wmDrag *drag,
-                                    const int /*xy*/[2],
-                                    wmDropBox * /*drop*/)
+static std::string datastack_drop_tooltip(bContext * /*C*/,
+                                          wmDrag *drag,
+                                          const int /*xy*/[2],
+                                          wmDropBox * /*drop*/)
 {
   StackDropData *drop_data = static_cast<StackDropData *>(drag->poin);
   switch (drop_data->drop_action) {
     case DATA_STACK_DROP_REORDER:
-      return BLI_strdup(TIP_("Reorder"));
-      break;
+      return TIP_("Reorder");
     case DATA_STACK_DROP_COPY:
       if (drop_data->pchan_parent) {
-        return BLI_strdup(TIP_("Copy to bone"));
+        return TIP_("Copy to bone");
       }
-      else {
-        return BLI_strdup(TIP_("Copy to object"));
-      }
-      break;
+      return TIP_("Copy to object");
+
     case DATA_STACK_DROP_LINK:
       if (drop_data->pchan_parent) {
-        return BLI_strdup(TIP_("Link all to bone"));
+        return TIP_("Link all to bone");
       }
-      else {
-        return BLI_strdup(TIP_("Link all to object"));
-      }
-      break;
+      return TIP_("Link all to object");
   }
-  return nullptr;
+  return {};
 }
 
 static void datastack_drop_link(bContext *C, StackDropData *drop_data)
@@ -1234,10 +1228,10 @@ static bool collection_drop_poll(bContext *C, wmDrag *drag, const wmEvent *event
   return false;
 }
 
-static char *collection_drop_tooltip(bContext *C,
-                                     wmDrag *drag,
-                                     const int xy[2],
-                                     wmDropBox * /*drop*/)
+static std::string collection_drop_tooltip(bContext *C,
+                                           wmDrag *drag,
+                                           const int xy[2],
+                                           wmDropBox * /*drop*/)
 {
   wmWindow *win = CTX_wm_window(C);
   const wmEvent *event = win ? win->eventstate : nullptr;
@@ -1268,23 +1262,17 @@ static char *collection_drop_tooltip(bContext *C,
     switch (data.insert_type) {
       case TE_INSERT_BEFORE:
         if (te->prev && outliner_is_collection_tree_element(te->prev)) {
-          return BLI_strdup(tooltip_between);
+          return tooltip_between;
         }
-        else {
-          return BLI_strdup(tooltip_before);
-        }
-        break;
+        return tooltip_before;
       case TE_INSERT_AFTER:
         if (te->next && outliner_is_collection_tree_element(te->next)) {
-          return BLI_strdup(tooltip_between);
+          return tooltip_between;
         }
-        else {
-          return BLI_strdup(tooltip_after);
-        }
-        break;
+        return tooltip_after;
       case TE_INSERT_INTO: {
         if (is_link) {
-          return BLI_strdup(TIP_("Link inside collection"));
+          return TIP_("Link inside collection");
         }
 
         /* Check the type of the drag IDs to avoid the incorrect "Shift to parent"
@@ -1293,10 +1281,9 @@ static char *collection_drop_tooltip(bContext *C,
         wmDragID *drag_id = (wmDragID *)drag->ids.first;
         const bool is_object = (GS(drag_id->id->name) == ID_OB);
         if (is_object) {
-          return BLI_strdup(TIP_("Move inside collection (Ctrl to link, Shift to parent)"));
+          return TIP_("Move inside collection (Ctrl to link, Shift to parent)");
         }
-        return BLI_strdup(TIP_("Move inside collection (Ctrl to link)"));
-        break;
+        return TIP_("Move inside collection (Ctrl to link)");
       }
     }
   }

source/blender/editors/include/ED_object.hh
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <string>
+
 #include "BLI_compiler_attrs.h"
 #include "BLI_string_ref.hh"
 
@@ -248,10 +250,12 @@ Base *ED_object_add_duplicate(
     Main *bmain, Scene *scene, ViewLayer *view_layer, Base *base, eDupli_ID_Flags dupflag);
 
 void ED_object_parent(Object *ob, Object *parent, int type, const char *substr);
-char *ED_object_ot_drop_named_material_tooltip(bContext *C, const char *name, const int mval[2]);
-char *ED_object_ot_drop_geometry_nodes_tooltip(bContext *C,
-                                               PointerRNA *properties,
-                                               const int mval[2]);
+std::string ED_object_ot_drop_named_material_tooltip(bContext *C,
+                                                     const char *name,
+                                                     const int mval[2]);
+std::string ED_object_ot_drop_geometry_nodes_tooltip(bContext *C,
+                                                     PointerRNA *properties,
+                                                     const int mval[2]);
 
 /* bitflags for enter/exit editmode */
 enum {

source/blender/editors/include/UI_interface.hh
@@ -179,10 +179,10 @@ bool drop_target_apply_drop(bContext &C,
  * Call #DropTargetInterface::drop_tooltip() and return the result as newly allocated C string
  * (unless the result is empty, returns null then). Needs freeing with MEM_freeN().
  */
-char *drop_target_tooltip(const ARegion &region,
-                          const DropTargetInterface &drop_target,
-                          const wmDrag &drag,
-                          const wmEvent &event);
+std::string drop_target_tooltip(const ARegion &region,
+                                const DropTargetInterface &drop_target,
+                                const wmDrag &drag,
+                                const wmEvent &event);
 
 std::unique_ptr<DropTargetInterface> view_drop_target(uiViewHandle *view_handle);
 std::unique_ptr<DropTargetInterface> view_item_drop_target(uiViewItemHandle *item_handle);

source/blender/editors/include/UI_interface_c.hh
@@ -3171,7 +3171,7 @@ void UI_fontstyle_draw_simple(
 void UI_fontstyle_draw_simple_backdrop(const uiFontStyle *fs,
                                        float x,
                                        float y,
-                                       const char *str,
+                                       blender::StringRef str,
                                        const float col_fg[4],
                                        const float col_bg[4]);
 

source/blender/editors/interface/interface_drop.cc
@@ -48,10 +48,10 @@ bool drop_target_apply_drop(bContext &C,
   return false;
 }
 
-char *drop_target_tooltip(const ARegion &region,
-                          const DropTargetInterface &drop_target,
-                          const wmDrag &drag,
-                          const wmEvent &event)
+std::string drop_target_tooltip(const ARegion &region,
+                                const DropTargetInterface &drop_target,
+                                const wmDrag &drag,
+                                const wmEvent &event)
 {
   const char *disabled_hint_dummy = nullptr;
   if (!drop_target.can_drop(drag, &disabled_hint_dummy)) {
@@ -65,8 +65,7 @@ char *drop_target_tooltip(const ARegion &region,
   }
 
   const DragInfo drag_info{drag, event, *drop_location};
-  const std::string tooltip = drop_target.drop_tooltip(drag_info);
-  return tooltip.empty() ? nullptr : BLI_strdup(tooltip.c_str());
+  return drop_target.drop_tooltip(drag_info);
 }
 
 }  // namespace blender::ui

source/blender/editors/interface/interface_dropboxes.cc
@@ -6,6 +6,8 @@
  * \ingroup edinterface
  */
 
+#include <fmt/format.h>
+
 #include "BKE_context.hh"
 
 #include "BLI_string.h"
@@ -47,7 +49,10 @@ static bool ui_view_drop_poll(bContext *C, wmDrag *drag, const wmEvent *event)
   return drop_target->can_drop(*drag, &drag->drop_state.disabled_info);
 }
 
-static char *ui_view_drop_tooltip(bContext *C, wmDrag *drag, const int xy[2], wmDropBox * /*drop*/)
+static std::string ui_view_drop_tooltip(bContext *C,
+                                        wmDrag *drag,
+                                        const int xy[2],
+                                        wmDropBox * /*drop*/)
 {
   const wmWindow *win = CTX_wm_window(C);
   const ARegion *region = CTX_wm_region(C);
@@ -91,10 +96,10 @@ static void ui_drop_material_copy(bContext *C, wmDrag *drag, wmDropBox *drop)
   RNA_int_set(drop->ptr, "session_uuid", int(id->session_uuid));
 }
 
-static char *ui_drop_material_tooltip(bContext *C,
-                                      wmDrag *drag,
-                                      const int /*xy*/[2],
-                                      wmDropBox * /*drop*/)
+static std::string ui_drop_material_tooltip(bContext *C,
+                                            wmDrag *drag,
+                                            const int /*xy*/[2],
+                                            wmDropBox * /*drop*/)
 {
   PointerRNA rna_ptr = CTX_data_pointer_get_type(C, "object", &RNA_Object);
   Object *ob = (Object *)rna_ptr.data;
@@ -109,25 +114,21 @@ static char *ui_drop_material_tooltip(bContext *C,
   Material *prev_mat_in_slot = (Material *)rna_prev_material.data;
   const char *dragged_material_name = WM_drag_get_item_name(drag);
 
-  char *result;
   if (prev_mat_in_slot) {
-    const char *tooltip = TIP_("Drop %s on slot %d (replacing %s) of %s");
-    result = BLI_sprintfN(tooltip,
-                          dragged_material_name,
-                          target_slot,
-                          prev_mat_in_slot->id.name + 2,
-                          ob->id.name + 2);
+    return fmt::format(TIP_("Drop {} on slot {} (replacing {}) of {}"),
+                       dragged_material_name,
+                       target_slot,
+                       prev_mat_in_slot->id.name + 2,
+                       ob->id.name + 2);
   }
-  else if (target_slot == ob->actcol) {
-    const char *tooltip = TIP_("Drop %s on slot %d (active slot) of %s");
-    result = BLI_sprintfN(tooltip, dragged_material_name, target_slot, ob->id.name + 2);
+  if (target_slot == ob->actcol) {
+    return fmt::format(TIP_("Drop {} on slot {} (active slot) of {}"),
+                       dragged_material_name,
+                       target_slot,
+                       ob->id.name + 2);
   }
-  else {
-    const char *tooltip = TIP_("Drop %s on slot %d of %s");
-    result = BLI_sprintfN(tooltip, dragged_material_name, target_slot, ob->id.name + 2);
-  }
-
-  return result;
+  return fmt::format(
+      TIP_("Drop {} on slot {} of {}"), dragged_material_name, target_slot, ob->id.name + 2);
 }
 
 /** \} */

source/blender/editors/interface/interface_style.cc
@@ -279,14 +279,14 @@ void UI_fontstyle_draw_simple(
 void UI_fontstyle_draw_simple_backdrop(const uiFontStyle *fs,
                                        float x,
                                        float y,
-                                       const char *str,
+                                       const blender::StringRef str,
                                        const float col_fg[4],
                                        const float col_bg[4])
 {
   UI_fontstyle_set(fs);
 
   {
-    const int width = BLF_width(fs->uifont_id, str, BLF_DRAW_STR_DUMMY_MAX);
+    const int width = BLF_width(fs->uifont_id, str.data(), str.size());
     const int height = BLF_height_max(fs->uifont_id);
     const int decent = BLF_descender(fs->uifont_id);
     const float margin = height / 4.0f;
@@ -302,7 +302,7 @@ void UI_fontstyle_draw_simple_backdrop(const uiFontStyle *fs,
 
   BLF_position(fs->uifont_id, x, y, 0.0f);
   BLF_color4fv(fs->uifont_id, col_fg);
-  BLF_draw(fs->uifont_id, str, BLF_DRAW_STR_DUMMY_MAX);
+  BLF_draw(fs->uifont_id, str.data(), str.size());
 }
 
 /* ************** helpers ************************ */

source/blender/editors/io/io_drop_import_file.cc
@@ -223,19 +223,19 @@ static bool drop_import_file_poll(bContext *C, wmDrag *drag, const wmEvent * /*e
   return !drop_import_file_poll_file_handlers(C, paths, true).is_empty();
 }
 
-static char *drop_import_file_tooltip(bContext *C,
-                                      wmDrag *drag,
-                                      const int /*xy*/[2],
-                                      wmDropBox * /*drop*/)
+static std::string drop_import_file_tooltip(bContext *C,
+                                            wmDrag *drag,
+                                            const int /*xy*/[2],
+                                            wmDropBox * /*drop*/)
 {
   const auto paths = WM_drag_get_paths(drag);
   const auto file_handlers = drop_import_file_poll_file_handlers(C, paths, true);
   if (file_handlers.size() == 1) {
     wmOperatorType *ot = WM_operatortype_find(file_handlers[0]->import_operator, false);
-    return BLI_strdup(TIP_(ot->name));
+    return TIP_(ot->name);
   }
 
-  return BLI_strdup(TIP_("Multiple file handlers can be used, drop to pick which to use"));
+  return TIP_("Multiple file handlers can be used, drop to pick which to use");
 }
 
 void ED_dropbox_drop_import_file()

commit aee4099c61006a36d6896216ff40888fffa897ab

Fix #117494: Crash when dropping collection
Caused by 7ca4dcac5a113db514faf7362b95bfe584e252be
Crash is due to wrong return value.

source/blender/editors/space_outliner/outliner_dragdrop.cc
@@ -1287,7 +1287,7 @@ static std::string collection_drop_tooltip(bContext *C,
       }
     }
   }
-  return nullptr;
+  return {};
 }
 
 static int collection_drop_invoke(bContext *C, wmOperator * /*op*/, const wmEvent *event)