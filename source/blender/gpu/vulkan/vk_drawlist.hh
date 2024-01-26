646541ec6028dabb648f322d7c34de19c8955409
Jeroen Bakker <jeroen@blender.org>
2024-01-26 15:32:40 +0100

    Vulkan: Draw-List
    A draw list bundles multiple draw commands for the same geometry
    and sends the draw commands in a single command. This reduces
    the overhead of pipeline checking, resource validation and can
    keep the load higher on the gpu as more work needs to be done.
    
    Previously the draw list didn't bundle any commands and would still
    send each call separately to the GPU. This PR implements the bundling
    of the commands.

--- source/blender/gpu/vulkan/vk_drawlist.hh
+++ source/blender/gpu/vulkan/vk_drawlist.hh
@@ -8,14 +8,38 @@
 
 #pragma once
 
+#include "BLI_vector.hh"
+
 #include "gpu_drawlist_private.hh"
 
+#include "vk_storage_buffer.hh"
+
 namespace blender::gpu {
+class VKBatch;
+
+struct GLDrawCommandIndexed {};
 
 class VKDrawList : public DrawList {
+ private:
+  VKBatch *batch_ = nullptr;
+  VKStorageBuffer command_buffer_;
+  int length_;
+  int command_index_ = 0;
+
  public:
+  VKDrawList(int list_length);
   void append(GPUBatch *batch, int instance_first, int instance_count) override;
   void submit() override;
+
+ private:
+  bool is_indexed() const;
+
+  template<typename CommandType> CommandType &get_command() const
+  {
+    return MutableSpan<CommandType>(
+        static_cast<CommandType *>(command_buffer_.buffer_get().mapped_memory_get()),
+        length_)[command_index_];
+  }
 };
 
 }  // namespace blender::gpu
