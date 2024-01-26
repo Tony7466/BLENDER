463d805e3de755c46c59563ffa76b4634063770d
Lukas Tönne <lukas@blender.org>
2024-01-26 11:47:41 +0100

    Use "Item_" prefix for menu switch sockets.

--- source/blender/nodes/geometry/nodes/node_geo_menu_switch.cc
+++ source/blender/nodes/geometry/nodes/node_geo_menu_switch.cc
@@ -62,7 +62,7 @@ static void node_declare(blender::nodes::NodeDeclarationBuilder &b)
   }
 
   for (const NodeEnumItem &enum_item : storage.enum_definition.items()) {
-    const std::string identifier = "Item" + std::to_string(enum_item.identifier);
+    const std::string identifier = "Item_" + std::to_string(enum_item.identifier);
     auto &input = b.add_input(data_type, enum_item.name, std::move(identifier));
     if (supports_fields) {
       input.supports_field();
