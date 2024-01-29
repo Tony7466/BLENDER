0a93669ea0386785366c97202a6b8553abdcf9ad
Lukas Tönne <lukas@blender.org>
2024-01-29 16:40:25 +0100

    Revert accidental change.

--- source/blender/blenkernel/intern/node_tree_update.cc
+++ source/blender/blenkernel/intern/node_tree_update.cc
@@ -822,7 +822,6 @@ class NodeTreeMainUpdater {
           bNodeSocket &input = *node->input_sockets()[0];
           BLI_assert(input.is_available() && input.type == SOCK_MENU);
           this->set_enum_ptr(*input.default_value_typed<bNodeSocketValueMenu>(), enum_items);
-          enum_items->remove_user_and_delete_if_last();
         }
         continue;
       }
