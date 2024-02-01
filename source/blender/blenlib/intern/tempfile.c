d61b434c63b491d7746f5d3638b95dfc29f87023
Marty E. Plummer <hanetzer@startmail.com>
2024-02-01 16:01:49 -0600

    blenlib: use XDG_STATE_HOME for temp dirs on linux.
    Also add 'blender' as the final element of the temp directory structure.
    
    Signed-off-by: Marty E. Plummer <hanetzer@startmail.com>

--- source/blender/blenlib/intern/tempfile.c
+++ source/blender/blenlib/intern/tempfile.c
@@ -16,10 +16,9 @@ void BLI_temp_directory_path_get(char *temp_directory, const size_t buffer_size)
 #ifdef WIN32
       "TEMP",
 #else
-      /* Non standard (could be removed). */
-      "TMP",
-      /* Posix standard. */
-      "TMPDIR",
+      /* XDG Base Directory Specification
+       * Suggested default of HOME/.local/state/ */
+      "XDG_STATE_HOME",
 #endif
   };
 
@@ -27,12 +26,14 @@ void BLI_temp_directory_path_get(char *temp_directory, const size_t buffer_size)
     const char *tmp = BLI_getenv(env_vars[i]);
     if (tmp && (tmp[0] != '\0') && BLI_is_dir(tmp)) {
       BLI_strncpy(temp_directory, tmp, buffer_size);
+      BLI_strncat(temp_directory, "/blender/", FILE_MAX);
       break;
     }
   }
 
   if (temp_directory[0] == '\0') {
-    BLI_strncpy(temp_directory, "/tmp/", buffer_size);
+    BLI_strncpy(temp_directory, BLI_getenv("HOME"), buffer_size);
+    BLI_strncat(temp_directory, "/.local/state/blender/", FILE_MAX);
   }
   else {
     /* Add a trailing slash if needed. */
