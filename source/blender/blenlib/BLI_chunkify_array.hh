e54152d8c82a37d75c083519fea17a48fd30f946
Jacques Lucke <jacques@blender.org>
2024-02-02 11:53:54 +0100

    add comment

--- source/blender/blenlib/BLI_chunkify_array.hh
+++ source/blender/blenlib/BLI_chunkify_array.hh
@@ -15,6 +15,12 @@ struct ChunkifyArrayParams {
   int64_t element_size = 8;
 };
 
+/**
+ * Split the data into chunks. The chunk boundaries are based on the content. That means that even
+ * if the data is modified or elements are added and removed, the chunks remain relatively stable
+ * (i.e. most chunks stay the same). This stability makes the chunks suitable for data
+ * deduplication/compression.
+ */
 OffsetIndices<int64_t> chunkify_array(const void *data,
                                       int64_t bytes_num,
                                       const ChunkifyArrayParams &params,
