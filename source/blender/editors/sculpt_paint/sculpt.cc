cb22d0f92beb32884be506684cdf86e6dcfb47fa
Sean Kim <SeanCTKim@protonmail.com>
2024-02-02 16:19:26 -0800

    Replace custom visibility code, prevent sculpt cursor from reacting to invisible geometry

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -5627,41 +5627,6 @@ static void sculpt_stroke_done(const bContext *C, PaintStroke * /*stroke*/)
   sculpt_brush_exit_tex(sd);
 }
 
-static bool sculpt_can_see_object(bContext *C, Object *ob)
-{
-  /* Test that ob is visible; otherwise we won't be able to get evaluated data
-   * from the depsgraph. We do this here instead of SCULPT_mode_poll
-   * to avoid falling through to the translate operator in the
-   * global view3d keymap.
-   *
-   * NOTE: #BKE_object_is_visible_in_viewport is not working here (it returns false
-   * if the object is in local view); instead, test for OB_HIDE_VIEWPORT directly.
-   */
-  if (ob->visibility_flag & OB_HIDE_VIEWPORT) {
-    return false;
-  }
-
-  const Scene *scene = CTX_data_scene(C);
-  ViewLayer *view_layer = CTX_data_view_layer(C);
-
-  BKE_view_layer_synced_ensure(scene, view_layer);
-  const Base *base = BKE_view_layer_base_find(view_layer, ob);
-
-  if (base->flag & BASE_HIDDEN) {
-    return false;
-  }
-
-  const Vector<LayerCollection *> collections = BKE_enclosing_layer_collections_get(view_layer,
-                                                                                    ob);
-  for (const int idx : collections.index_range()) {
-    if (collections[idx]->flag & LAYER_COLLECTION_HIDE) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
 static int sculpt_brush_stroke_invoke(bContext *C, wmOperator *op, const wmEvent *event)
 {
   PaintStroke *stroke;
@@ -5669,7 +5634,18 @@ static int sculpt_brush_stroke_invoke(bContext *C, wmOperator *op, const wmEvent
   int retval;
   Object *ob = CTX_data_active_object(C);
 
-  if (!sculpt_can_see_object(C, ob)) {
+  const View3D *v3d = CTX_wm_view3d(C);
+  const Scene *scene = CTX_data_scene(C);
+  ViewLayer *view_layer = CTX_data_view_layer(C);
+
+  BKE_view_layer_synced_ensure(scene, view_layer);
+  const Base *base = BKE_view_layer_base_find(view_layer, ob);
+
+  /* Test that ob is visible; otherwise we won't be able to get evaluated data
+   * from the depsgraph. We do this here instead of SCULPT_mode_poll
+   * to avoid falling through to the translate operator in the
+   * global view3d keymap. */
+  if (!BKE_base_is_visible(v3d, base)) {
     return OPERATOR_CANCELLED;
   }
 
