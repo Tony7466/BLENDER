13d38bd7de167bc9bb2d489ad4e0ffad9795029a
Hans Goudey <hans@blender.org>
2024-02-07 11:43:02 -0500

    Geometry Nodes: Move instance reference indices to a builtin attribute
    This means the array can be shared between geometries when unchanged,
    reducing memory usage and increasing performance. It also means that
    handling the data can be done more generically using the attribute
    system. In the future the transforms can become attributes too.

--- source/blender/blenkernel/intern/bake_items_serialize.cc
+++ source/blender/blenkernel/intern/bake_items_serialize.cc
@@ -724,14 +724,6 @@ static std::unique_ptr<Instances> try_load_instances(const DictionaryValue &io_g
     return {};
   }
 
-  const auto *io_handles = io_instances->lookup_dict("handles");
-  if (!io_handles) {
-    return {};
-  }
-  if (!read_blob_simple_gspan(blob_reader, *io_handles, instances->reference_handles())) {
-    return {};
-  }
-
   MutableAttributeAccessor attributes = instances->attributes_for_write();
   if (!load_attributes(*io_attributes, attributes, blob_reader, blob_sharing)) {
     return {};
@@ -969,9 +961,6 @@ static std::shared_ptr<DictionaryValue> serialize_geometry_set(const GeometrySet
 
     io_instances->append(
         "transforms", write_blob_simple_gspan(blob_writer, blob_sharing, instances.transforms()));
-    io_instances->append(
-        "handles",
-        write_blob_simple_gspan(blob_writer, blob_sharing, instances.reference_handles()));
 
     auto io_attributes = serialize_attributes(
         instances.attributes(), blob_writer, blob_sharing, {"position"});
