bb3072320b11a0b17b245358fea32410801a54af
Jacques Lucke <jacques@blender.org>
2024-02-07 14:15:02 +0100

    keep persistent uid of mod_dst instead of creating a new one

--- source/blender/makesrna/intern/rna_object.cc
+++ source/blender/makesrna/intern/rna_object.cc
@@ -1865,12 +1865,9 @@ bool rna_Object_modifiers_override_apply(Main *bmain,
   ParticleSystem *psys_dst = (mod_dst->type == eModifierType_ParticleSystem) ?
                                  (reinterpret_cast<ParticleSystemModifierData *>(mod_dst))->psys :
                                  nullptr;
-  const bool uid_is_used_already = BKE_modifiers_findby_persistent_uid(
-                                       ob_dst, mod_src->persistent_uid) != nullptr;
+  const int persistent_uid = mod_dst->persistent_uid;
   BKE_modifier_copydata(mod_src, mod_dst);
-  if (uid_is_used_already) {
-    BKE_modifiers_persistent_uid_init(*ob_dst, *mod_dst);
-  }
+  mod_dst->persistent_uid = persistent_uid;
   if (mod_dst->type == eModifierType_ParticleSystem) {
     psys_dst->flag &= ~PSYS_DELETE;
     (reinterpret_cast<ParticleSystemModifierData *>(mod_dst))->psys = psys_dst;
