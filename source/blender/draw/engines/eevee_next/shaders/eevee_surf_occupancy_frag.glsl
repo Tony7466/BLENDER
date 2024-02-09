ca77bf503e3114bcc1911992361a1d7c6c630153
Michael Parkin-White <mparkinwhite@apple.com>
2024-02-09 17:13:47 +0000

    Further reduction to change set size.

--- source/blender/draw/engines/eevee_next/shaders/eevee_surf_occupancy_frag.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_surf_occupancy_frag.glsl
@@ -68,7 +68,7 @@ void main()
     uint hit_id = imageAtomicAdd(hit_count_img, texel, 1u);
     if (hit_id < VOLUME_HIT_DEPTH_MAX) {
       float value = gl_FrontFacing ? volume_z : -volume_z;
-      imageStoreFast(hit_depth_img, ivec3(texel, hit_id), vec4(value));
+      imageStore(hit_depth_img, ivec3(texel, hit_id), vec4(value));
     }
   }
 }
