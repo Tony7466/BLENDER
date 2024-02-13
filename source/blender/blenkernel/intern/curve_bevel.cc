Tony7466/BLENDER

commit 9e4129feebddd6f82ca7fe867ec35c9e0f78cebd

Cleanup: unused includes in source/blender/blenkernel
Remove 397 includes.

source/blender/blenkernel/intern/curve_bevel.cc
@@ -11,9 +11,7 @@
 
 #include <cstring>
 
-#include "BLI_alloca.h"
 #include "BLI_listbase.h"
-#include "BLI_math_base.h"
 
 #include "MEM_guardedalloc.h"
 
@@ -22,7 +20,6 @@
 #include "DNA_object_types.h"
 
 #include "BKE_curve.hh"
-#include "BKE_curveprofile.h"
 #include "BKE_displist.h"
 #include "BKE_object_types.hh"
 

commit 26672063aae01a3966eaf45ed379ff4d9caaadd7

Windows: Compile error due to missing header
Caused by 9e4129feeb

source/blender/blenkernel/intern/curve_bevel.cc
@@ -12,6 +12,7 @@
 #include <cstring>
 
 #include "BLI_listbase.h"
+#include "BLI_math_base.h"
 
 #include "MEM_guardedalloc.h"
 