b4b0af03bb77f3ba4f3b1a771a2c7959a79c9df9
Hans Goudey <hans@blender.org>
2024-02-14 10:06:25 -0500

    Add defaults suggested by Iliya

--- source/blender/nodes/function/nodes/node_fn_axes_to_rotation.cc
+++ source/blender/nodes/function/nodes/node_fn_axes_to_rotation.cc
@@ -15,8 +15,8 @@ namespace blender::nodes::node_fn_axis_to_euler_cc {
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.is_function_node();
-  b.add_input<decl::Vector>(N_("Primary Axis")).hide_value();
-  b.add_input<decl::Vector>(N_("Secondary Axis")).hide_value();
+  b.add_input<decl::Vector>(N_("Primary Axis")).default_value(float3(1, 0, 0)).hide_value();
+  b.add_input<decl::Vector>(N_("Secondary Axis")).default_value(float3(0, 1, 0)).hide_value();
   b.add_output<decl::Rotation>(N_("Rotation"));
 }
 
