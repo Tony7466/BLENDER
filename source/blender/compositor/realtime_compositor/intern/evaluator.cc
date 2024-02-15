bc7bc8e945101dc6a9ba05f1f5ef734029ab37f7
Omar Emara <mail@OmarEmara.dev>
2024-02-15 11:28:29 +0200

    Revert evaluator canceling

--- source/blender/compositor/realtime_compositor/intern/evaluator.cc
+++ source/blender/compositor/realtime_compositor/intern/evaluator.cc
@@ -4,8 +4,6 @@
 
 #include <string>
 
-#include "BLI_timeit.hh"
-
 #include "DNA_node_types.h"
 
 #include "NOD_derived_node_tree.hh"
@@ -34,13 +32,11 @@ void Evaluator::evaluate()
 
   if (!is_compiled_) {
     compile_and_evaluate();
+    is_compiled_ = true;
     return;
   }
 
   for (const std::unique_ptr<Operation> &operation : operations_stream_) {
-    if (context_.is_canceled()) {
-      return;
-    }
     operation->evaluate();
   }
 }
@@ -72,7 +68,7 @@ void Evaluator::compile_and_evaluate()
 {
   derived_node_tree_ = std::make_unique<DerivedNodeTree>(context_.get_node_tree());
 
-  if (!validate_node_tree() || context_.is_canceled()) {
+  if (!validate_node_tree()) {
     return;
   }
 
@@ -81,11 +77,6 @@ void Evaluator::compile_and_evaluate()
   CompileState compile_state(schedule);
 
   for (const DNode &node : schedule) {
-    if (context_.is_canceled()) {
-      reset();
-      return;
-    }
-
     if (compile_state.should_compile_shader_compile_unit(node)) {
       compile_and_evaluate_shader_compile_unit(compile_state);
     }
@@ -97,8 +88,6 @@ void Evaluator::compile_and_evaluate()
       compile_and_evaluate_node(node, compile_state);
     }
   }
-
-  is_compiled_ = true;
 }
 
 void Evaluator::compile_and_evaluate_node(DNode node, CompileState &compile_state)
