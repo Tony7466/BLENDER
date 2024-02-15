2eb7c332586acde30267c9debd619487cd0c3339
Gangneron <clementgangneron@gmail.com>
2024-02-15 19:35:10 +0100

    Make format

--- source/blender/editors/space_text/text_format.hh
+++ source/blender/editors/space_text/text_format.hh
@@ -114,8 +114,8 @@ TextFormatType *ED_text_format_get(Text *text);
 void ED_text_format_register(TextFormatType *tft);
 
 /* formatters */
-void ED_text_format_register_py();
 void ED_text_format_register_glsl();
+void ED_text_format_register_py();
 void ED_text_format_register_osl();
 void ED_text_format_register_pov();
 void ED_text_format_register_pov_ini();
