595dfac42b8b47c1f620d6c86e84705d0e8c356f
Clément Foucault <foucault.clem@gmail.com>
2024-02-18 03:12:57 +0100

    Reorganize the convolve shader and use variable cone aperture

--- source/blender/draw/engines/eevee_next/shaders/eevee_reflection_probe_convolve_comp.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_reflection_probe_convolve_comp.glsl
@@ -10,49 +10,41 @@
 #pragma BLENDER_REQUIRE(eevee_reflection_probe_mapping_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_sampling_lib.glsl)
 
-float sample_weight(vec3 out_direction, vec3 in_direction)
+float cone_cosine_from_roughness(float linear_roughness)
+{
+  float m = linear_roughness;
+  /* Jimenez 2016 in Practical Realtime Strategies for Accurate Indirect Occlusion. (eq 26) */
+  // return exp2(-3.32193 * square(m));
+  /* Using the same derivation as above but with u = 0.244 (lobe fitting cone). */
+  return exp2(-1.017523474 * square(m));
+}
+
+float sample_weight(vec3 out_direction, vec3 in_direction, float linear_roughness)
 {
   out_direction = normalize(out_direction);
   in_direction = normalize(in_direction);
 
-  float cos_theta = clamp(dot(out_direction, in_direction), 0.0, 1.0);
+  float cos_theta = saturate(dot(out_direction, in_direction));
 
-  /* From mip to linear roughness (same as UI). */
-  float mip_roughness = sphere_probe_lod_to_roughness(float(read_lod + 1));
-  /* Clamp to avoid numerical imprecision. */
-  mip_roughness = max(mip_roughness, BSDF_ROUGHNESS_THRESHOLD);
   /* From linear roughness to GGX roughness input. */
-  float m = square(mip_roughness);
+  float m = square(linear_roughness);
   /* Map GGX roughness to spherical gaussian sharpness.
    * From "SG Series Part 4: Specular Lighting From an SG Light Source" by MJP
    * https://therealmjp.github.io/posts/sg-series-part-4-specular-lighting-from-an-sg-light-source/
    */
   vec3 N = out_direction;
   vec3 H = normalize(out_direction + in_direction);
-  float NH = clamp(dot(N, H), 0.0, 1.0);
+  float NH = saturate(dot(N, H));
   /* GGX. */
   // return exp(-square(acos(NH) / m));
   /* Spherical Gaussian. */
   return exp(2.0 * (NH - 1.0) / square(m));
 }
 
-void convolve_sample(vec3 in_direction,
-                     vec3 out_direction,
-                     SphereProbeUvArea sample_coord,
-                     inout vec4 radiance_accum,
-                     inout float weight_accum)
+mat3x3 tangent_basis(vec3 N)
 {
-  float weight = sample_weight(out_direction, in_direction);
-
-#if 1
-  vec2 in_uv = sphere_probe_direction_to_uv(in_direction, float(read_lod), sample_coord);
-  vec4 radiance = texture(in_atlas_mip_tx, vec3(in_uv, sample_coord.layer));
-#else /* For reference and debugging.  */
-  vec4 radiance = texture(cubemap_tx, in_direction);
-#endif
-
-  radiance_accum += radiance * weight;
-  weight_accum += weight;
+  /* TODO(fclem): This create a discontinuity at Z=0. */
+  return from_up_axis(N);
 }
 
 void main()
@@ -69,28 +61,45 @@ void main()
     return;
   }
 
+  /* From mip to linear roughness (same as UI). */
+  float mip_roughness = sphere_probe_lod_to_roughness(float(read_lod + 1));
+  /* Clamp to avoid numerical imprecision. */
+  mip_roughness = max(mip_roughness, BSDF_ROUGHNESS_THRESHOLD);
+  float cone_cos = cone_cosine_from_roughness(mip_roughness);
+
   vec3 out_direction = sphere_probe_texel_to_direction(
       out_local_texel, out_texel_area, sample_coord);
   out_direction = normalize(out_direction);
 
-  mat3x3 basis = from_up_axis(out_direction);
+  mat3x3 basis = tangent_basis(out_direction);
 
   ivec2 out_texel = out_texel_area.offset + out_local_texel;
 
-  float weight = 0.0;
-  vec4 radiance = vec4(0.0);
+  float weight_accum = 0.0;
+  vec4 radiance_accum = vec4(0.0);
 
   int sample_count = 1024;
   for (int i = 0; i < sample_count; i++) {
     vec2 rand = hammersley_2d(i, sample_count);
-    vec3 in_direction = basis * normalize(sample_uniform_cone(rand, 0.9));
-    convolve_sample(in_direction, out_direction, sample_coord, radiance, weight);
+    vec3 in_direction = basis * sample_uniform_cone(rand, cone_cos);
+
+#if 0
+    vec2 in_uv = sphere_probe_direction_to_uv(in_direction, float(read_lod), sample_coord);
+    vec4 radiance = texture(in_atlas_mip_tx, vec3(in_uv, sample_coord.layer));
+#else /* For reference and debugging.  */
+    vec4 radiance = texture(cubemap_tx, in_direction);
+#endif
+
+    float weight = sample_weight(out_direction, in_direction, mip_roughness);
+    radiance_accum += radiance * weight;
+    weight_accum += weight;
   }
-  radiance *= safe_rcp(weight);
+  vec4 out_radiance = radiance_accum * safe_rcp(weight_accum);
 
 #if 0 /* Debugging texel alignment. */
   ivec2 a = out_texel % 2;
-  radiance = vec4(a.x == a.y);
+  out_radiance = vec4(a.x == a.y);
 #endif
-  imageStore(out_atlas_mip_img, ivec3(out_texel, out_texel_area.layer), radiance);
+
+  imageStore(out_atlas_mip_img, ivec3(out_texel, out_texel_area.layer), out_radiance);
 }
