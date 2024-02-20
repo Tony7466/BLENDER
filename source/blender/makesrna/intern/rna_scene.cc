a2566b7b0754421021367d7aacbee5e126cdd479
Miguel Pozo <pragma37@gmail.com>
2024-02-20 20:58:18 +0100

    Fix typo

--- source/blender/makesrna/intern/rna_scene.cc
+++ source/blender/makesrna/intern/rna_scene.cc
@@ -8319,7 +8319,7 @@ static void rna_def_scene_eevee(BlenderRNA *brna)
   RNA_def_property_range(prop, 0.0f, FLT_MAX);
   RNA_def_property_ui_range(prop, 0.0f, 5.0f, 1.0f, 2);
   RNA_def_property_ui_text(
-      prop, "Shadow PCF Radius", "Blur shadow aliasing using Percentage Close Filtering");
+      prop, "Shadow PCF Radius", "Blur shadow aliasing using Percentage Closer Filtering");
   RNA_def_property_override_flag(prop, PROPOVERRIDE_OVERRIDABLE_LIBRARY);
   RNA_def_property_update(prop, NC_SCENE | ND_RENDER_OPTIONS, nullptr);
 
