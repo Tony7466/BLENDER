2647baef55c409228014eeea025eeadd8bff3426
YimingWu <xp8110@outlook.com>
2024-02-20 19:23:00 +0800

    Cleanups.

--- source/blender/modifiers/intern/MOD_grease_pencil_hook.cc
+++ source/blender/modifiers/intern/MOD_grease_pencil_hook.cc
@@ -174,7 +174,8 @@ static void deform_drawing(const ModifierData &md,
   const bool use_falloff = falloff_sq != 0.0f;
   const bool use_uniform = (mmd.flag & MOD_GRAESE_PENCIL_HOOK_UNIFORM_SPACE) != 0;
 
-  const float3x3 mat_uniform = use_uniform ? float3x3(float4x4(mmd.parentinv)) : float3x3::identity();
+  const float3x3 mat_uniform = use_uniform ? float3x3(float4x4(mmd.parentinv)) :
+                                             float3x3::identity();
   const float3 cent = use_uniform ? math::transform_point(mat_uniform, float3(mmd.cent)) :
                                     float3(mmd.cent);
 
@@ -191,7 +192,6 @@ static void deform_drawing(const ModifierData &md,
     /* Just object target. */
     dmat = mmd.object->object_to_world();
   }
-  ob.runtime->world_to_object = math::invert(ob.object_to_world());
   float4x4 use_mat = ob.world_to_object() * dmat * float4x4(mmd.parentinv);
 
   auto get_weight = [&](const int point) {
