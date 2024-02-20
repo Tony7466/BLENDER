6ee25ea0642ce75ee3cfef70e5e9e0232223aaa2
YimingWu <xp8110@outlook.com>
2024-02-20 08:26:04 +0800

    Simpler code path

--- source/blender/modifiers/intern/MOD_grease_pencil_hook.cc
+++ source/blender/modifiers/intern/MOD_grease_pencil_hook.cc
@@ -174,17 +174,9 @@ static void deform_drawing(const ModifierData &md,
   const bool use_falloff = falloff_sq != 0.0f;
   const bool use_uniform = (mmd.flag & MOD_GRAESE_PENCIL_HOOK_UNIFORM_SPACE) != 0;
 
-  float3x3 mat_uniform;
-  float3 cent;
-
-  if (use_uniform) {
-    mat_uniform = float3x3(float4x4(mmd.parentinv));
-    cent = math::transform_point(mat_uniform, float3(mmd.cent));
-  }
-  else {
-    mat_uniform = float3x3::identity();
-    cent = float3(mmd.cent);
-  }
+  const float3x3 mat_uniform = use_uniform ? float3x3(float4x4(mmd.parentinv)); : float3x3::identity();
+  const float3 cent = use_uniform ? math::transform_point(mat_uniform, float3(mmd.cent)) :
+                                    float3(mmd.cent);
 
   float4x4 dmat;
   /* Get world-space matrix of target, corrected for the space the verts are in. */
