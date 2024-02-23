4854752287bb474cb0a53651820556d75de24784
Jesse Yurkovich <jesse.y@gmail.com>
2024-02-22 17:34:49 -0800

    Header name change

--- source/blender/io/usd/intern/usd_reader_pointinstancer.cc
+++ source/blender/io/usd/intern/usd_reader_pointinstancer.cc
@@ -6,7 +6,7 @@
 
 #include "BKE_attribute.hh"
 #include "BKE_modifier.hh"
-#include "BKE_node.h"
+#include "BKE_node.hh"
 #include "BKE_node_runtime.hh"
 #include "BKE_node_tree_update.hh"
 #include "BKE_object.hh"
