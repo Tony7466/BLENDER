73b8ea0967fd4405964d5e83be18641cb527d1d7
Philipp Oeser <philipp@blender.org>
2024-02-26 13:22:03 +0100

    additional polish by @dr.sybren thx!

--- source/blender/blenkernel/intern/action.cc
+++ source/blender/blenkernel/intern/action.cc
@@ -374,8 +374,9 @@ void action_group_colors_sync(bActionGroup *grp, const bActionGroup *ref_grp)
 
 void action_group_colors_set_from_posebone(bActionGroup *grp, const bPoseChannel *pchan)
 {
-  /* pchan->bone is only set after leaving editmode. */
-  if (pchan == nullptr || pchan->bone == nullptr) {
+  BLI_assert_msg(pchan, "cannot 'set action group colors from posebone' without a posebone");
+  if (!pchan->bone) {
+    /* pchan->bone is only set after leaving editmode. */
     return;
   }
 
