c6dcac6250d2ee3e63310ff2fec5f22f10d1eb5e
Clément Foucault <foucault.clem@gmail.com>
2024-02-29 12:51:26 +0100

    Remove usage of gbuffer in tracing

--- source/blender/draw/engines/eevee_next/shaders/eevee_horizon_scan_comp.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_horizon_scan_comp.glsl
@@ -6,7 +6,6 @@
 #pragma BLENDER_REQUIRE(eevee_sampling_lib.glsl)
 #pragma BLENDER_REQUIRE(gpu_shader_utildefines_lib.glsl)
 #pragma BLENDER_REQUIRE(gpu_shader_math_vector_lib.glsl)
-#pragma BLENDER_REQUIRE(eevee_gbuffer_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_horizon_scan_eval_lib.glsl)
 
 void main()
@@ -23,27 +22,18 @@ void main()
     return;
   }
 
+  /* Do not trace where nothing was rendered. */
+  if (texelFetch(gbuf_header_tx, texel_fullres, 0).r == 0u) {
+    imageStore(horizon_radiance_img, ivec3(texel, 0), vec4(FLT_11_11_10_MAX, 0.0));
+    return;
+  }
+
   vec2 uv = (vec2(texel_fullres) + 0.5) * uniform_buf.raytrace.full_resolution_inv;
   float depth = texelFetch(hiz_tx, texel_fullres, 0).r;
-
-  if (depth == 1.0) {
-    /* Do not trace for background */
-    imageStore(horizon_radiance_img, ivec3(texel, 0), vec4(FLT_11_11_10_MAX, 0.0));
-    return;
-  }
-
-  GBufferReader gbuf = gbuffer_read(
-      gbuf_header_tx, gbuf_closure_tx, gbuf_normal_tx, texel_fullres);
+  vec3 vP = drw_point_screen_to_view(vec3(uv, depth));
 
   HorizonScanContext ctx;
-  ctx.closure.N = drw_normal_world_to_view(gbuf.surface_N);
-
-  if (gbuf.header == 0u) {
-    imageStore(horizon_radiance_img, ivec3(texel, 0), vec4(FLT_11_11_10_MAX, 0.0));
-    return;
-  }
-
-  vec3 vP = drw_point_screen_to_view(vec3(uv, depth));
+  ctx.closure.N = horizon_scan_sample_normal(uv);
 
   vec2 noise = utility_tx_fetch(utility_tx, vec2(texel), UTIL_BLUE_NOISE_LAYER).rg;
   noise = fract(noise + sampling_rng_2D_get(SAMPLING_AO_U));
