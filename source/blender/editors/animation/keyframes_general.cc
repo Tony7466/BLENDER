28e965a2bc69344bac17b02c51c6149a7e3b45b2
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-02-29 15:36:09 +0100

    rename helper function

--- source/blender/editors/animation/keyframes_general.cc
+++ source/blender/editors/animation/keyframes_general.cc
@@ -662,7 +662,7 @@ void smooth_fcurve_segment(FCurve *fcu,
 }
 /* ---------------- */
 
-static float ease_to_ease_function(const float x, const float width, const float shift)
+static float ease_sigmoid_function(const float x, const float width, const float shift)
 {
   const float x_shift = (x - shift) * width;
   const float y = x_shift / sqrt(1 + pow2f(x_shift));
@@ -689,13 +689,13 @@ void ease_fcurve_segment(FCurve *fcu,
 
   /* Using the factor on the xshift we are basicaly moving the curve horizontaly. */
   const float shift = -factor;
-  const float y_min = ease_to_ease_function(-1, width, shift);
-  const float y_max = ease_to_ease_function(1, width, shift);
+  const float y_min = ease_sigmoid_function(-1, width, shift);
+  const float y_max = ease_sigmoid_function(1, width, shift);
 
   for (int i = segment->start_index; i < segment->start_index + segment->length; i++) {
     /* Mapping the x-location of the key within the segment to a -1/1 range. */
     const float x = ((fcu->bezt[i].vec[1][0] - left_key->vec[1][0]) / key_x_range) * 2 - 1;
-    const float y = ease_to_ease_function(x, width, shift);
+    const float y = ease_sigmoid_function(x, width, shift);
     /* Normalizing the y value to the min and max to ensure that the keys at the end are not
      * detached from the rest of the animation. */
     const float blend = (y - y_min) * (1 / (y_max - y_min));
