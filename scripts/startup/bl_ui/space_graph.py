daac91adc2c106dd56af8aaefdb0354c7dca30dc
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-02-29 15:27:14 +0100

    remove python call to non existent operator

--- scripts/startup/bl_ui/space_graph.py
+++ scripts/startup/bl_ui/space_graph.py
@@ -339,7 +339,6 @@ class GRAPH_MT_key_blending(Menu):
         layout.operator("graph.blend_to_neighbor", text="Blend to Neighbor")
         layout.operator("graph.blend_to_default", text="Blend to Default Value")
         layout.operator("graph.ease", text="Ease")
-        layout.operator("graph.ease_to_ease", text="Ease to Ease")
         layout.operator("graph.blend_offset", text="Blend Offset")
         layout.operator("graph.blend_to_ease", text="Blend to Ease")
         layout.operator("graph.match_slope", text="Match Slope")
