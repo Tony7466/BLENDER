02f3fad1c0358f206f77fb2c37e7e0be1dfe3549
Sybren A. Stüvel <sybren@blender.org>
2024-03-04 18:02:49 +0100

    Anim: add Animation data-block management functions
    Add code (including RNA wrappers) for:
    
    - Creating, removing, and accessing `Animation` data-blocks.
    - Creating and removing layers, strips, and bindings on those `Animation`
      data-blocks.
    - Accessing those via RNA.
    
    Note that this does not include assignment to any animated data-block,
    so it is of limited practical use.
    
    For more info, see #113594.

--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -7189,6 +7189,12 @@ static void rna_def_userdef_experimental(BlenderRNA *brna)
                            "section of the preferences");
   RNA_def_property_boolean_funcs(
       prop, nullptr, "rna_PreferencesExperimental_use_extension_repos_set");
+
+  prop = RNA_def_property(srna, "use_animation_baklava", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_sdna(prop, nullptr, "use_animation_baklava", 1);
+  RNA_def_property_ui_text(
+      prop, "Animation: Project Baklava", "Enable the new multi-id, layered animation system");
+  RNA_def_property_update(prop, 0, "rna_userdef_update");
 }
 
 static void rna_def_userdef_addon_collection(BlenderRNA *brna, PropertyRNA *cprop)
