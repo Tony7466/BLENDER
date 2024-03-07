528cbe3dabdee4a98258f9a205e1c3280ee34754
Alaska <alaskayou01@gmail.com>
2024-03-07 21:47:48 +1300

    Fix Cycles Metal compilaiton error after recent changes

--- intern/cycles/kernel/types.h
+++ intern/cycles/kernel/types.h
@@ -1217,7 +1217,7 @@ typedef enum KernelBVHLayout {
 } KernelBVHLayout;
 
 /* Specialized struct that can become constants in dynamic compilation. */
-#define KERNEL_STRUCT_BEGIN(name, parent) ccl_align(16) struct name {
+#define KERNEL_STRUCT_BEGIN(name, parent) struct ccl_align(16) name {
 #define KERNEL_STRUCT_END(name) \
   } \
   ; \
@@ -1259,7 +1259,7 @@ typedef struct KernelLightLinkSet {
   uint light_tree_root;
 } KernelLightLinkSet;
 
-typedef ccl_align(16) struct KernelData {
+typedef struct ccl_align(16) KernelData {
   /* Features and limits. */
   uint kernel_features;
   uint max_closures;
