c335802c533bca7386b7cbdaee872cabd8c927d8
Raul Fernandez <farsthary84@gmail.com>
2024-03-13 16:22:25 -0400

    nit

--- source/blender/editors/mesh/editmesh_mask_extract.cc
+++ source/blender/editors/mesh/editmesh_mask_extract.cc
@@ -471,8 +471,8 @@ static int paint_mask_slice_exec(bContext *C, wmOperator *op)
   Mesh *mesh = static_cast<Mesh *>(ob->data);
   Mesh *new_mesh = (Mesh *)BKE_id_copy(bmain, &mesh->id);
 
-  /* Fix for https://projects.blender.org/blender/blender/issues/87243   */
-  /*   Undo crashes when new object is created in the middle of a sculpt */
+  /* Fix for #87243 */
+  /* Undo crashes when new object is created in the middle of a sculpt */
   if (ob->mode == OB_MODE_SCULPT && !create_new_object) {
     sculpt_paint::undo::geometry_begin(ob, op);
   }
