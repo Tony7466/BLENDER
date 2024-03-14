Tony7466/BLENDER

commit 3c3f1e6d62e2dadb9cc3aa230033647d441b0a7b

Fix #117603: Auto smooth replacement versioning skipped for link/append
Unfortunately the only versioning code that can "properly" add data-
blocks is not run when linking or appending (`do_versions_after_setup`
has details). The versioning has to be done manually for this case.
Fortunately that is simple since the versioning function already just
operators on a main database.

source/blender/blenkernel/intern/blendfile_link_append.cc
@@ -47,6 +47,7 @@
 #include "BKE_main.hh"
 #include "BKE_main_namemap.hh"
 #include "BKE_material.h"
+#include "BKE_mesh_legacy_convert.hh"
 #include "BKE_object.hh"
 #include "BKE_report.h"
 #include "BKE_rigidbody.h"
@@ -1377,6 +1378,7 @@ void BKE_blendfile_append(BlendfileLinkAppendContext *lapp_context, ReportList *
   BKE_main_id_newptr_and_tag_clear(bmain);
 
   blendfile_link_append_proxies_convert(bmain, reports);
+  BKE_main_mesh_legacy_convert_auto_smooth(*bmain);
 }
 
 void BKE_blendfile_link(BlendfileLinkAppendContext *lapp_context, ReportList *reports)

commit adaa44d810837817c5a101e6cc5f2a4a2579c05a

Fix #119455, #119447: Incorrect auto smooth versioning on append
Caused by a misunderstanding in 3c3f1e6d62e2dadb9cc3 which caused the
versioning to be applied to all objects in the main data-base, not just new
objects. Now only run the versioning on new objects when appending or
linking.

source/blender/blenkernel/BKE_mesh_legacy_convert.hh
@@ -115,6 +115,7 @@ void BKE_mesh_calc_edges_legacy(Mesh *mesh);
 void BKE_mesh_do_versions_cd_flag_init(Mesh *mesh);
 
 void BKE_main_mesh_legacy_convert_auto_smooth(Main &bmain);
+void BKE_main_mesh_legacy_convert_auto_smooth(Main &bmain, blender::Span<Object *> objects);
 
 /**
  * Calculate/create edges from tessface data

source/blender/blenkernel/intern/blendfile_link_append.cc
@@ -1375,10 +1375,19 @@ void BKE_blendfile_append(BlendfileLinkAppendContext *lapp_context, ReportList *
   instantiate_context.active_collection = nullptr;
   loose_data_instantiate(&instantiate_context);
 
+  {
+    blender::Vector<Object *> new_objects;
+    LISTBASE_FOREACH (Object *, object, &bmain->objects) {
+      if (object->type == OB_MESH && object->id.tag & LIB_TAG_NEW) {
+        new_objects.append(object);
+      }
+    }
+    BKE_main_mesh_legacy_convert_auto_smooth(*bmain, new_objects);
+  }
+
   BKE_main_id_newptr_and_tag_clear(bmain);
 
   blendfile_link_append_proxies_convert(bmain, reports);
-  BKE_main_mesh_legacy_convert_auto_smooth(*bmain);
 }
 
 void BKE_blendfile_link(BlendfileLinkAppendContext *lapp_context, ReportList *reports)

source/blender/blenkernel/intern/mesh_legacy_convert.cc
@@ -2263,7 +2263,7 @@ static ModifierData *create_auto_smooth_modifier(Object &object,
 
 }  // namespace blender::bke
 
-void BKE_main_mesh_legacy_convert_auto_smooth(Main &bmain)
+void BKE_main_mesh_legacy_convert_auto_smooth(Main &bmain, const blender::Span<Object *> objects)
 {
   using namespace blender::bke;
 
@@ -2275,10 +2275,8 @@ void BKE_main_mesh_legacy_convert_auto_smooth(Main &bmain)
     return node_group;
   };
 
-  LISTBASE_FOREACH (Object *, object, &bmain.objects) {
-    if (object->type != OB_MESH) {
-      continue;
-    }
+  for (Object *object : objects) {
+    BLI_assert(object->type == OB_MESH);
     Mesh *mesh = static_cast<Mesh *>(object->data);
     const float angle = mesh->smoothresh_legacy;
     if (!(mesh->flag & ME_AUTOSMOOTH_LEGACY)) {
@@ -2327,6 +2325,17 @@ void BKE_main_mesh_legacy_convert_auto_smooth(Main &bmain)
       BLI_addtail(&object->modifiers, new_md);
     }
   }
+}
+
+void BKE_main_mesh_legacy_convert_auto_smooth(Main &bmain)
+{
+  blender::Vector<Object *> objects;
+  LISTBASE_FOREACH (Object *, object, &bmain.objects) {
+    if (object->type == OB_MESH) {
+      objects.append(object);
+    }
+  }
+  BKE_main_mesh_legacy_convert_auto_smooth(bmain, objects);
 }
 
 namespace blender::bke {