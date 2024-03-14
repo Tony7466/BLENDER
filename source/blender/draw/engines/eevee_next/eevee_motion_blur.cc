92f18686c454f7ef6897d4122773316d88ce45a1
Miguel Pozo <pragma37@gmail.com>
2024-03-14 19:01:42 +0100

    Disable viewport motion blur outside of playback

--- source/blender/draw/engines/eevee_next/eevee_motion_blur.cc
+++ source/blender/draw/engines/eevee_next/eevee_motion_blur.cc
@@ -28,7 +28,8 @@ void MotionBlurModule::init()
   const Scene *scene = inst_.scene;
   const ViewLayer *view_layer = inst_.view_layer;
 
-  enabled_ = (scene->r.mode & R_MBLUR) != 0;
+  enabled_ = (scene->r.mode & R_MBLUR) != 0 &&
+             (DRW_state_is_image_render() || DRW_state_is_playback());
   if (enabled_) {
     enabled_ = (view_layer->layflag & SCE_LAY_MOTION_BLUR) != 0;
   }
