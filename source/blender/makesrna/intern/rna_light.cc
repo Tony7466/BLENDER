6bbb7c7d3a683a2f17f822d82fd42ea50fbeffb3
Miguel Pozo <pragma37@gmail.com>
2024-03-15 16:29:36 +0100

    Update tooltip

--- source/blender/makesrna/intern/rna_light.cc
+++ source/blender/makesrna/intern/rna_light.cc
@@ -307,7 +307,11 @@ static void rna_def_light_shadow(StructRNA *srna, bool sun)
   prop = RNA_def_property(srna, "shadow_resolution_scale", PROP_FLOAT, PROP_FACTOR);
   RNA_def_property_range(prop, 0.0f, 2.0f);
   RNA_def_property_ui_range(prop, 0.0f, 2.0f, 0.25f, 2);
-  RNA_def_property_ui_text(prop, "Shadow Resolution Scale", "Bias the Shadow Map resolution");
+  RNA_def_property_ui_text(
+      prop,
+      "Shadow Resolution Scale",
+      "Scale the Shadow Map target resolution, where 1.0 tries to match shadow map and screen "
+      "pixel density. (The scale is applied on top of the scene Simplify Shadow Resolution)");
   RNA_def_property_override_flag(prop, PROPOVERRIDE_OVERRIDABLE_LIBRARY);
   RNA_def_property_update(prop, 0, "rna_Light_update");
 
