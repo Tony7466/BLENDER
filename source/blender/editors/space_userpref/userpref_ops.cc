acefcdced0b383a356f17fe2084617f5d620c3cd
Harley Acheson <harley.acheson@gmail.com>
2024-03-16 11:07:43 -0700

    UI: Update Polls on Single Repository Sync and Upgrade
    The buttons to sync and update individual repositories only work for
    remote repositories. This PR just updates their poll functions to
    disable them for local repos.

--- source/blender/editors/space_userpref/userpref_ops.cc
+++ source/blender/editors/space_userpref/userpref_ops.cc
@@ -500,6 +500,19 @@ static bool preferences_extension_repo_active_enabled_poll(bContext *C)
   return true;
 }
 
+static bool preferences_extension_repo_remote_active_enabled_poll(bContext *C)
+{
+  const bUserExtensionRepo *repo = BKE_preferences_extension_repo_find_index(
+      &U, U.active_extension_repo);
+  if (repo == nullptr || (repo->flag & USER_EXTENSION_REPO_FLAG_DISABLED) ||
+      !(repo->flag & USER_EXTENSION_REPO_FLAG_USE_REMOTE_PATH))
+  {
+    CTX_wm_operator_poll_msg_set(C, "An enabled remote repository must be selected");
+    return false;
+  }
+  return true;
+}
+
 /** \} */
 
 /* -------------------------------------------------------------------- */
@@ -649,7 +662,7 @@ static void PREFERENCES_OT_extension_repo_sync(wmOperatorType *ot)
   ot->description = "Synchronize the active extension repository with its remote URL";
 
   ot->exec = preferences_extension_repo_sync_exec;
-  ot->poll = preferences_extension_repo_active_enabled_poll;
+  ot->poll = preferences_extension_repo_remote_active_enabled_poll;
 
   ot->flag = OPTYPE_INTERNAL;
 }
@@ -675,7 +688,7 @@ static void PREFERENCES_OT_extension_repo_upgrade(wmOperatorType *ot)
   ot->description = "Update any outdated extensions for the active extension repository";
 
   ot->exec = preferences_extension_repo_upgrade_exec;
-  ot->poll = preferences_extension_repo_active_enabled_poll;
+  ot->poll = preferences_extension_repo_remote_active_enabled_poll;
 
   ot->flag = OPTYPE_INTERNAL;
 }
