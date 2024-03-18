a6fc311bffc95234b61f2463ad251606a91f4958
Miguel Pozo <pragma37@gmail.com>
2024-03-18 20:17:31 +0100

    Use pcg4d for transparency threshold

--- source/blender/draw/engines/eevee_next/shaders/eevee_surf_shadow_frag.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_surf_shadow_frag.glsl
@@ -17,6 +17,7 @@
 #pragma BLENDER_REQUIRE(eevee_transparency_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_sampling_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_shadow_tilemap_lib.glsl)
+#pragma BLENDER_REQUIRE(gpu_shader_common_hash.glsl)
 
 vec4 closure_to_rgba(Closure cl)
 {
@@ -31,13 +32,7 @@ void main()
   nodetree_surface(0.0);
 
   float noise_offset = sampling_rng_1D_get(SAMPLING_TRANSPARENCY);
-#  if 0
-  float random_threshold = transparency_hashed_alpha_threshold(
-      uniform_buf.pipeline.alpha_hash_scale, noise_offset, g_data.P);
-#  else
-  /* A fully random threshold yields cleaner results than the hashed alpha. */
-  float random_threshold = hash_vec3_to_float(g_data.P * (1.0 + noise_offset));
-#  endif
+  float random_threshold = pcg4d(vec4(g_data.P, noise_offset)).x;
 
   float transparency = average(g_transmittance);
   if (transparency > random_threshold) {
