19bbc01adbbfd0200072b349c00f45199cb0e1f8
Julian Eisel <julian@blender.org>
2024-03-20 13:19:05 +0100

    Update RNA function parameter description

--- source/blender/makesrna/intern/rna_ui.cc
+++ source/blender/makesrna/intern/rna_ui.cc
@@ -2079,11 +2079,11 @@ static void rna_def_uilist(BlenderRNA *brna)
   prop = RNA_def_property(func, "filter_flags", PROP_INT, PROP_UNSIGNED);
   RNA_def_property_flag(prop, PropertyFlag(PARM_REQUIRED | PROP_DYNAMIC));
   RNA_def_property_array(prop, 1); /* XXX Dummy value, default 0 does not work */
-  RNA_def_property_ui_text(
-      prop,
-      "",
-      "An array of filter flags, one for each item in the collection (NOTE: "
-      "FILTER_ITEM bit is reserved, it defines whether the item is shown or not)");
+  RNA_def_property_ui_text(prop,
+                           "",
+                           "An array of filter flags, one for each item in the collection (NOTE: "
+                           "The upper 16 bits, including FILTER_ITEM, are reserved, only use the "
+                           "lower 16 bits for custom usages).");
   RNA_def_function_output(func, prop);
   prop = RNA_def_property(func, "filter_neworder", PROP_INT, PROP_UNSIGNED);
   RNA_def_property_flag(prop, PropertyFlag(PARM_REQUIRED | PROP_DYNAMIC));
