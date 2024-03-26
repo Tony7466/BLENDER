cf1fe94a62461a691a7ed409ea77767f6dda9a2d
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-03-26 16:58:20 +0100

    avoid copying of string

--- source/blender/editors/animation/keyframing.cc
+++ source/blender/editors/animation/keyframing.cc
@@ -302,8 +302,7 @@ static blender::Vector<std::string> construct_rna_paths(PointerRNA *ptr)
       LISTBASE_FOREACH (IDProperty *, prop, &properties->data.group) {
         char name_escaped[MAX_IDPROP_NAME * 2];
         BLI_str_escape(name_escaped, prop->name, sizeof(name_escaped));
-        std::string rna_path = fmt::format("[\"{}\"]", name_escaped);
-        paths.append(rna_path);
+        paths.append(fmt::format("[\"{}\"]", name_escaped));
       }
     }
   }
