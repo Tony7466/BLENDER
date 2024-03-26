da5980064f869e076c94a02fe233effc90e43af2
Michael Kowalski <makowalski@nvidia.com>
2024-03-26 18:48:36 -0400

    USD: Updated license identifiers

--- source/blender/io/usd/intern/usd_reader_points.hh
+++ source/blender/io/usd/intern/usd_reader_points.hh
@@ -1,7 +1,6 @@
-/* SPDX-FileCopyrightText: 2023 ????. All rights reserved.
+/* SPDX-FileCopyrightText: 2024 Blender Authors
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
-
 #pragma once
 
 #include "usd.hh"
