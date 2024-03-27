Tony7466/BLENDER

commit 0cdd429b44ce3147bc8bd12bfdff33665b0d43ec

Cleanup: Use newer API for creating IDProperties in most places
There are still a few places that are more complicated where the replacement
to `IDP_New` isn't obvious, but this commit replaces most uses of the ugly
`IDPropertyTemplate` usage.

source/blender/python/generic/idprop_py_api.cc
@@ -409,26 +409,21 @@ static const char *idp_try_read_name(PyObject *name_obj)
 
 static IDProperty *idp_from_PyFloat(const char *name, PyObject *ob)
 {
-  IDPropertyTemplate val = {0};
-  val.d = PyFloat_AsDouble(ob);
-  return IDP_New(IDP_DOUBLE, &val, name);
+  return blender::bke::idprop::create(name, PyFloat_AsDouble(ob)).release();
 }
 
 static IDProperty *idp_from_PyBool(const char *name, PyObject *ob)
 {
-  IDPropertyTemplate val = {0};
-  val.i = PyC_Long_AsBool(ob);
-  return IDP_New(IDP_BOOLEAN, &val, name);
+  return blender::bke::idprop::create(name, int(PyC_Long_AsBool(ob))).release();
 }
 
 static IDProperty *idp_from_PyLong(const char *name, PyObject *ob)
 {
-  IDPropertyTemplate val = {0};
-  val.i = PyC_Long_AsI32(ob);
-  if (val.i == -1 && PyErr_Occurred()) {
+  const int value = PyC_Long_AsI32(ob);
+  if (value == -1 && PyErr_Occurred()) {
     return nullptr;
   }
-  return IDP_New(IDP_INT, &val, name);
+  return blender::bke::idprop::create(name, value).release();
 }
 
 static IDProperty *idp_from_PyUnicode(const char *name, PyObject *ob)
@@ -642,7 +637,6 @@ static IDProperty *idp_from_PySequence(const char *name, PyObject *ob)
 static IDProperty *idp_from_PyMapping(const char *name, PyObject *ob)
 {
   IDProperty *prop;
-  const IDPropertyTemplate val = {0};
 
   PyObject *keys, *vals, *key, *pval;
   int i, len;
@@ -652,7 +646,7 @@ static IDProperty *idp_from_PyMapping(const char *name, PyObject *ob)
 
   /* We allocate the group first; if we hit any invalid data,
    * we can delete it easily enough. */
-  prop = IDP_New(IDP_GROUP, &val, name);
+  prop = blender::bke::idprop::create_group(name).release();
   len = PyMapping_Length(ob);
   for (i = 0; i < len; i++) {
     key = PySequence_GetItem(keys, i);
@@ -676,9 +670,9 @@ static IDProperty *idp_from_PyMapping(const char *name, PyObject *ob)
 
 static IDProperty *idp_from_DatablockPointer(const char *name, PyObject *ob)
 {
-  IDPropertyTemplate val = {0};
-  pyrna_id_FromPyObject(ob, &val.id);
-  return IDP_New(IDP_ID, &val, name);
+  ID *id = nullptr;
+  pyrna_id_FromPyObject(ob, &id);
+  return blender::bke::idprop::create(name, id).release();
 }
 
 static IDProperty *idp_from_PyObject(PyObject *name_obj, PyObject *ob)

source/blender/blenkernel/intern/action.cc
@@ -247,13 +247,9 @@ static void action_blend_read_data(BlendDataReader *reader, ID *id)
 
 static IDProperty *action_asset_type_property(const bAction *action)
 {
+  using namespace blender;
   const bool is_single_frame = BKE_action_has_single_frame(action);
-
-  IDPropertyTemplate idprop = {0};
-  idprop.i = is_single_frame;
-
-  IDProperty *property = IDP_New(IDP_INT, &idprop, "is_single_frame");
-  return property;
+  return bke::idprop::create("is_single_frame", int(is_single_frame)).release();
 }
 
 static void action_asset_metadata_ensure(void *asset_ptr, AssetMetaData *asset_data)

source/blender/blenkernel/intern/asset.cc
@@ -155,9 +155,9 @@ void BKE_asset_metadata_catalog_id_set(AssetMetaData *asset_data,
 
 void BKE_asset_metadata_idprop_ensure(AssetMetaData *asset_data, IDProperty *prop)
 {
+  using namespace blender::bke;
   if (!asset_data->properties) {
-    IDPropertyTemplate val = {0};
-    asset_data->properties = IDP_New(IDP_GROUP, &val, "AssetMetaData.properties");
+    asset_data->properties = idprop::create_group("AssetMetaData.properties").release();
   }
   /* Important: The property may already exist. For now just allow always allow a newly allocated
    * property, and replace the existing one as a way of updating. */

source/blender/blenkernel/intern/camera.cc
@@ -124,33 +124,26 @@ static CameraCyclesCompatibilityData camera_write_cycles_compatibility_data_crea
     if (prop) {
       return prop;
     }
-    IDPropertyTemplate val = {0};
-    prop = IDP_New(IDP_GROUP, &val, "cycles");
+    prop = blender::bke::idprop::create_group("cycles").release();
     IDP_AddToGroup(group, prop);
     return prop;
   };
 
   auto cycles_property_int_set = [](IDProperty *idprop, const char *name, int value) {
-    IDProperty *prop = IDP_GetPropertyTypeFromGroup(idprop, name, IDP_INT);
-    if (prop) {
+    if (IDProperty *prop = IDP_GetPropertyTypeFromGroup(idprop, name, IDP_INT)) {
       IDP_Int(prop) = value;
     }
     else {
-      IDPropertyTemplate val = {0};
-      val.i = value;
-      IDP_AddToGroup(idprop, IDP_New(IDP_INT, &val, name));
+      IDP_AddToGroup(idprop, blender::bke::idprop::create(name, value).release());
     }
   };
 
   auto cycles_property_float_set = [](IDProperty *idprop, const char *name, float value) {
-    IDProperty *prop = IDP_GetPropertyTypeFromGroup(idprop, name, IDP_FLOAT);
-    if (prop) {
+    if (IDProperty *prop = IDP_GetPropertyTypeFromGroup(idprop, name, IDP_FLOAT)) {
       IDP_Float(prop) = value;
     }
     else {
-      IDPropertyTemplate val = {0};
-      val.f = value;
-      IDP_AddToGroup(idprop, IDP_New(IDP_FLOAT, &val, name));
+      IDP_AddToGroup(idprop, blender::bke::idprop::create(name, value).release());
     }
   };
 

source/blender/blenkernel/intern/idprop.cc
@@ -198,11 +198,8 @@ static void idp_resize_group_array(IDProperty *prop, int newlen, void *newarr)
   if (newlen >= prop->len) {
     /* bigger */
     IDProperty **array = static_cast<IDProperty **>(newarr);
-    IDPropertyTemplate val;
-
     for (int a = prop->len; a < newlen; a++) {
-      val.i = 0; /* silence MSVC warning about uninitialized var when debugging */
-      array[a] = IDP_New(IDP_GROUP, &val, "IDP_ResizeArray group");
+      array[a] = blender::bke::idprop::create_group("IDP_ResizeArray group").release();
     }
   }
   else {

source/blender/blenkernel/intern/keyconfig.cc
@@ -42,8 +42,8 @@ wmKeyConfigPref *BKE_keyconfig_pref_ensure(UserDef *userdef, const char *kc_idna
     BLI_addtail(&userdef->user_keyconfig_prefs, kpt);
   }
   if (kpt->prop == nullptr) {
-    IDPropertyTemplate val = {0};
-    kpt->prop = IDP_New(IDP_GROUP, &val, kc_idname); /* name is unimportant. */
+    /* name is unimportant. */
+    kpt->prop = blender::bke::idprop::create_group(kc_idname).release();
   }
   return kpt;
 }
@@ -115,9 +115,7 @@ void BKE_keyconfig_pref_set_select_mouse(UserDef *userdef, int value, bool overr
   wmKeyConfigPref *kpt = BKE_keyconfig_pref_ensure(userdef, WM_KEYCONFIG_STR_DEFAULT);
   IDProperty *idprop = IDP_GetPropertyFromGroup(kpt->prop, "select_mouse");
   if (!idprop) {
-    IDPropertyTemplate tmp{};
-    tmp.i = value;
-    IDP_AddToGroup(kpt->prop, IDP_New(IDP_INT, &tmp, "select_mouse"));
+    IDP_AddToGroup(kpt->prop, blender::bke::idprop::create("select_mouse", value).release());
   }
   else if (override) {
     IDP_Int(idprop) = value;

source/blender/blenkernel/intern/object.cc
@@ -1044,20 +1044,13 @@ static void object_lib_override_apply_post(ID *id_dst, ID *id_src)
 
 static IDProperty *object_asset_dimensions_property(Object *ob)
 {
+  using namespace blender::bke;
   float3 dimensions;
   BKE_object_dimensions_get(ob, dimensions);
   if (is_zero_v3(dimensions)) {
     return nullptr;
   }
-
-  IDPropertyTemplate idprop{};
-  idprop.array.len = 3;
-  idprop.array.type = IDP_FLOAT;
-
-  IDProperty *property = IDP_New(IDP_ARRAY, &idprop, "dimensions");
-  memcpy(IDP_Array(property), dimensions, sizeof(dimensions));
-
-  return property;
+  return idprop::create("dimensions", Span(&dimensions.x, 3)).release();
 }
 
 static void object_asset_metadata_ensure(void *asset_ptr, AssetMetaData *asset_data)
@@ -1066,8 +1059,7 @@ static void object_asset_metadata_ensure(void *asset_ptr, AssetMetaData *asset_d
   BLI_assert(GS(ob->id.name) == ID_OB);
 
   /* Update dimensions hint for the asset. */
-  IDProperty *dimensions_prop = object_asset_dimensions_property(ob);
-  if (dimensions_prop) {
+  if (IDProperty *dimensions_prop = object_asset_dimensions_property(ob)) {
     BKE_asset_metadata_idprop_ensure(asset_data, dimensions_prop);
   }
 }

source/blender/blenloader/intern/versioning_300.cc
@@ -1561,6 +1561,7 @@ static void do_version_subsurface_methods(bNode *node)
 
 static void version_geometry_nodes_add_attribute_input_settings(NodesModifierData *nmd)
 {
+  using namespace blender;
   if (nmd->settings.properties == nullptr) {
     return;
   }
@@ -1583,14 +1584,13 @@ static void version_geometry_nodes_add_attribute_input_settings(NodesModifierDat
     char use_attribute_prop_name[MAX_IDPROP_NAME];
     SNPRINTF(use_attribute_prop_name, "%s%s", property->name, "_use_attribute");
 
-    IDPropertyTemplate idprop = {0};
-    IDProperty *use_attribute_prop = IDP_New(IDP_INT, &idprop, use_attribute_prop_name);
+    IDProperty *use_attribute_prop = bke::idprop::create(use_attribute_prop_name, 0).release();
     IDP_AddToGroup(nmd->settings.properties, use_attribute_prop);
 
     char attribute_name_prop_name[MAX_IDPROP_NAME];
     SNPRINTF(attribute_name_prop_name, "%s%s", property->name, "_attribute_name");
 
-    IDProperty *attribute_prop = IDP_New(IDP_STRING, &idprop, attribute_name_prop_name);
+    IDProperty *attribute_prop = bke::idprop::create(attribute_name_prop_name, "").release();
     IDP_AddToGroup(nmd->settings.properties, attribute_prop);
   }
 }

commit cd8985ab32bd0c57c6c6feeea53602e8e523c868

Fix: Unable to change ID property type to Boolean
Caused by 0cdd429b44c.

Steps to reproduce:
- Add a custom property to the default Cube object
- Open property editor
- Change type to Boolean
- Apply the changes

Observe that the property is still Int.

On a code side the issue is caused by the change in the idp_from_PyBool()
which used to return property of type IDP_BOOLEAN before the change, but
IDP_INT after the change.

source/blender/python/generic/idprop_py_api.cc
@@ -414,7 +414,7 @@ static IDProperty *idp_from_PyFloat(const char *name, PyObject *ob)
 
 static IDProperty *idp_from_PyBool(const char *name, PyObject *ob)
 {
-  return blender::bke::idprop::create(name, int(PyC_Long_AsBool(ob))).release();
+  return blender::bke::idprop::create_bool(name, PyC_Long_AsBool(ob)).release();
 }
 
 static IDProperty *idp_from_PyLong(const char *name, PyObject *ob)