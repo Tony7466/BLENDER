Tony7466/BLENDER

commit 6a79a6a24ab249ef44ed9e42ed185611b2ef5381

Cleanup/Refactor: Move animdata read/write code into generic ID handling.
There is no reason at all for each ID read/write callbacks to have to
deal with this generic, common data explicitely. Generic ID read/write
code is the place to handle such data (just like asset, liboverride,
etc. data are handled already).

Note behavioral change expected here.

source/blender/blenkernel/intern/mask.cc
@@ -96,10 +96,6 @@ static void mask_blend_write(BlendWriter *writer, ID *id, const void *id_address
   BLO_write_id_struct(writer, Mask, id_address, &mask->id);
   BKE_id_blend_write(writer, &mask->id);
 
-  if (mask->adt) {
-    BKE_animdata_blend_write(writer, mask->adt);
-  }
-
   LISTBASE_FOREACH (MaskLayer *, masklay, &mask->masklayers) {
     BLO_write_struct(writer, MaskLayer, masklay);
 

source/blender/blenkernel/BKE_anim_data.h
@@ -130,8 +130,8 @@ void BKE_animdata_merge_copy(struct Main *bmain,
                              eAnimData_MergeCopy_Modes action_mode,
                              bool fix_drivers);
 
-void BKE_animdata_blend_write(struct BlendWriter *writer, struct AnimData *adt);
-void BKE_animdata_blend_read_data(struct BlendDataReader *reader, struct AnimData *adt);
+void BKE_animdata_blend_write(struct BlendWriter *writer, struct ID *id);
+void BKE_animdata_blend_read_data(struct BlendDataReader *reader, struct ID *id);
 void BKE_animdata_blend_read_lib(struct BlendLibReader *reader,
                                  struct ID *id,
                                  struct AnimData *adt);

source/blender/blenkernel/intern/anim_data.cc
@@ -1439,8 +1439,13 @@ void BKE_animdata_fix_paths_rename_all_ex(Main *bmain,
 
 /* .blend file API -------------------------------------------- */
 
-void BKE_animdata_blend_write(BlendWriter *writer, AnimData *adt)
+void BKE_animdata_blend_write(BlendWriter *writer, ID *id)
 {
+  AnimData *adt = BKE_animdata_from_id(id);
+  if (!adt) {
+    return;
+  }
+
   /* firstly, just write the AnimData block */
   BLO_write_struct(writer, AnimData, adt);
 
@@ -1461,9 +1466,14 @@ void BKE_animdata_blend_write(BlendWriter *writer, AnimData *adt)
   BKE_nla_blend_write(writer, &adt->nla_tracks);
 }
 
-void BKE_animdata_blend_read_data(BlendDataReader *reader, AnimData *adt)
+void BKE_animdata_blend_read_data(BlendDataReader *reader, ID *id)
 {
-  /* NOTE: must have called BLO_read_data_address already before doing this... */
+  IdAdtTemplate *iat = id_can_have_animdata(id) ? reinterpret_cast<IdAdtTemplate *>(id) : nullptr;
+  if (!iat || !iat->adt) {
+    return;
+  }
+
+  AnimData *adt = static_cast<AnimData *>(BLO_read_data_address(reader, &iat->adt));
   if (adt == nullptr) {
     return;
   }

source/blender/blenkernel/intern/armature.cc
@@ -219,10 +219,6 @@ static void armature_blend_write(BlendWriter *writer, ID *id, const void *id_add
   BLO_write_id_struct(writer, bArmature, id_address, &arm->id);
   BKE_id_blend_write(writer, &arm->id);
 
-  if (arm->adt) {
-    BKE_animdata_blend_write(writer, arm->adt);
-  }
-
   /* Direct data */
   LISTBASE_FOREACH (Bone *, bone, &arm->bonebase) {
     write_bone(writer, bone);
@@ -255,9 +251,6 @@ static void armature_blend_read_data(BlendDataReader *reader, ID *id)
   arm->edbo = nullptr;
   /* Must always be cleared (armatures don't have their own edit-data). */
   arm->needs_flush_to_id = 0;
-
-  BLO_read_data_address(reader, &arm->adt);
-  BKE_animdata_blend_read_data(reader, arm->adt);
 
   LISTBASE_FOREACH (Bone *, bone, &arm->bonebase) {
     direct_link_bones(reader, bone);

source/blender/blenkernel/intern/cachefile.cc
@@ -104,10 +104,6 @@ static void cache_file_blend_write(BlendWriter *writer, ID *id, const void *id_a
   BLO_write_id_struct(writer, CacheFile, id_address, &cache_file->id);
   BKE_id_blend_write(writer, &cache_file->id);
 
-  if (cache_file->adt) {
-    BKE_animdata_blend_write(writer, cache_file->adt);
-  }
-
   /* write layers */
   LISTBASE_FOREACH (CacheFileLayer *, layer, &cache_file->layers) {
     BLO_write_struct(writer, CacheFileLayer, layer);
@@ -121,10 +117,6 @@ static void cache_file_blend_read_data(BlendDataReader *reader, ID *id)
   cache_file->handle = nullptr;
   cache_file->handle_filepath[0] = '\0';
   cache_file->handle_readers = nullptr;
-
-  /* relink animdata */
-  BLO_read_data_address(reader, &cache_file->adt);
-  BKE_animdata_blend_read_data(reader, cache_file->adt);
 
   /* relink layers */
   BLO_read_list(reader, &cache_file->layers);

source/blender/blenkernel/intern/camera.cc
@@ -115,10 +115,6 @@ static void camera_blend_write(BlendWriter *writer, ID *id, const void *id_addre
   BLO_write_id_struct(writer, Camera, id_address, &cam->id);
   BKE_id_blend_write(writer, &cam->id);
 
-  if (cam->adt) {
-    BKE_animdata_blend_write(writer, cam->adt);
-  }
-
   LISTBASE_FOREACH (CameraBGImage *, bgpic, &cam->bg_images) {
     BLO_write_struct(writer, CameraBGImage, bgpic);
   }
@@ -127,8 +123,6 @@ static void camera_blend_write(BlendWriter *writer, ID *id, const void *id_addre
 static void camera_blend_read_data(BlendDataReader *reader, ID *id)
 {
   Camera *ca = (Camera *)id;
-  BLO_read_data_address(reader, &ca->adt);
-  BKE_animdata_blend_read_data(reader, ca->adt);
 
   BLO_read_list(reader, &ca->bg_images);
 

source/blender/blenkernel/intern/curve.cc
@@ -163,9 +163,6 @@ static void curve_blend_write(BlendWriter *writer, ID *id, const void *id_addres
 
   /* direct data */
   BLO_write_pointer_array(writer, cu->totcol, cu->mat);
-  if (cu->adt) {
-    BKE_animdata_blend_write(writer, cu->adt);
-  }
 
   if (cu->vfont) {
     BLO_write_raw(writer, cu->len + 1, cu->str);
@@ -211,8 +208,6 @@ static void switch_endian_knots(Nurb *nu)
 static void curve_blend_read_data(BlendDataReader *reader, ID *id)
 {
   Curve *cu = (Curve *)id;
-  BLO_read_data_address(reader, &cu->adt);
-  BKE_animdata_blend_read_data(reader, cu->adt);
 
   /* Protect against integer overflow vulnerability. */
   CLAMP(cu->len_char32, 0, INT_MAX - 4);

source/blender/blenkernel/intern/curves.cc
@@ -115,16 +115,11 @@ static void curves_blend_write(BlendWriter *writer, ID *id, const void *id_addre
   BLO_write_string(writer, curves->surface_uv_map);
 
   BLO_write_pointer_array(writer, curves->totcol, curves->mat);
-  if (curves->adt) {
-    BKE_animdata_blend_write(writer, curves->adt);
-  }
 }
 
 static void curves_blend_read_data(BlendDataReader *reader, ID *id)
 {
   Curves *curves = (Curves *)id;
-  BLO_read_data_address(reader, &curves->adt);
-  BKE_animdata_blend_read_data(reader, curves->adt);
 
   /* Geometry */
   curves->geometry.wrap().blend_read(*reader);

commit f0b826e88f2df9f69ab2c655e9eca30c313ade66

Fix #119925: Mask property keyframes missing when reload the file
Caused by 6a79a6a24ab2

`Mask` `AnimData` is read by `BKE_animdata_blend_read_data` since above
commit, so `BLO_read_data_address` on `AnimData` is already done there.
However, `mask_blend_read_data` does it again (and this seems to break
the reading process somehow -- or gets overwritten?).
Similar calls to `BLO_read_data_address` in `blend_read_data` callbacks
were usually removed in 6a79a6a24ab2, Masks being an exception.

NOTE: I am a bit on shaky ground wrt. `BLO_read_*` process, lacking
proper explanation as to why **exactly** double calls fail here.

source/blender/blenkernel/intern/mask.cc
@@ -136,7 +136,6 @@ static void mask_blend_write(BlendWriter *writer, ID *id, const void *id_address
 static void mask_blend_read_data(BlendDataReader *reader, ID *id)
 {
   Mask *mask = (Mask *)id;
-  BLO_read_data_address(reader, &mask->adt);
 
   BLO_read_list(reader, &mask->masklayers);
 