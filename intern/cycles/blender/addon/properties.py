41a6f8b17c0efc879ab5a3f066db0fe8b87972a1
Alaska <alaskayou01@gmail.com>
2024-03-28 05:40:24 +1300

    Integrate Brechts request

--- intern/cycles/blender/addon/properties.py
+++ intern/cycles/blender/addon/properties.py
@@ -235,9 +235,8 @@ def enum_preview_denoiser(self, context):
         items = [
             ('AUTO',
              "Automatic",
-             ("Use the best denoiser for viewport rendering (GPU OpenImageDenoise if supported "
-              "and the GPU is being used for rendering, otherwise OptiX or CPU OpenImageDenoise "
-              "depending on your hardware)"),
+             ("Use GPU accelerated denoising if supported, for the best performance.
+              "Prefer OpenImageDenoise over OptiX"),
              0)]
     else:
         items = [('AUTO', "None", "Blender was compiled without a viewport denoiser", 0)]
