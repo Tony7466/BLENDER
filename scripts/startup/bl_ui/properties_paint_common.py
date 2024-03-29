268a73f5cbec692c7cc264fcfe0bca93d910dc88
YimingWu <xp8110@outlook.com>
2024-03-29 19:18:46 +0800

    Remove extra U popovers

--- scripts/startup/bl_ui/properties_paint_common.py
+++ scripts/startup/bl_ui/properties_paint_common.py
@@ -1444,9 +1444,6 @@ def brush_basic_grease_pencil_paint_settings(layout, context, brush, *, compact=
     elif grease_pencil_tool == 'TINT':
         layout.prop(gp_settings, "vertex_mode", text="Mode")
         layout.popover("VIEW3D_PT_tools_brush_settings_advanced", text="Brush")
-        layout.popover("VIEW3D_PT_tools_brush_stroke")
-        layout.popover("VIEW3D_PT_tools_brush_falloff")
-        layout.popover("VIEW3D_PT_tools_brush_display")
 
 
 def brush_basic_gpencil_sculpt_settings(layout, _context, brush, *, compact=False):
