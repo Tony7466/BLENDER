Tony7466/BLENDER

commit 36e603c430ca90a4a19574a5071db54a7cebcb39

Cycles: Add option to control smoothing when using bump map
Cycles implements the "Taming the Shadow Terminator" paper by Matt Jen-Yuan
Chiang to solve shadow terminator issues when a bump map is applied, as well
as similar approach for the glossy reflection to ensure ray does not get
reflected to inside of the object.

This correction term is applied unconditionally, which makes it harder to have
full control over shading via normals for stylistic reasons.

This change exposes this corrective term as an option called "Bump Map
Correction" which is available in the shader settings next to the
"Transparent Shadows".

The reason to make it per-shader rather than per-object is to allow flexibility
of a control: it is possible that an object has multiple shaders attached to it,
and only some of them used for bump mapping. Another, and possibly stronger
reason to have it per-shader is ease of assets control: shader brings settings
which are needed for its proper behavior. So if material at some point
decides to take over normals, artists would not need to update settings on
every asset which uses that material.

The option is enabled by default, so there is no changes for existing setups.

Pull Request: https://projects.blender.org/blender/blender/pulls/113480

intern/cycles/kernel/closure/bsdf.h
@@ -233,7 +233,7 @@ ccl_device_inline int bsdf_sample(KernelGlobals kg,
       *eval *= shift_cos_in(cosNO, frequency_multiplier);
     }
     if (label & LABEL_DIFFUSE) {
-      if (!isequal(sc->N, sd->N)) {
+      if ((sd->flag & SD_USE_BUMP_MAP_CORRECTION) && !isequal(sc->N, sd->N)) {
         *eval *= bump_shadowing_term(sd->N, sc->N, *wo);
       }
     }
@@ -521,7 +521,7 @@ ccl_device_inline
   }
 
   if (CLOSURE_IS_BSDF_DIFFUSE(sc->type)) {
-    if (!isequal(sc->N, sd->N)) {
+    if ((sd->flag & SD_USE_BUMP_MAP_CORRECTION) && !isequal(sc->N, sd->N)) {
       eval *= bump_shadowing_term(sd->N, sc->N, wo);
     }
   }

intern/cycles/kernel/integrator/shade_surface.h
@@ -492,7 +492,7 @@ ccl_device_forceinline int integrate_surface_bsdf_bssrdf_bounce(
   /* Update path state */
   if (!(label & LABEL_TRANSPARENT)) {
     INTEGRATOR_STATE_WRITE(state, path, mis_ray_pdf) = mis_pdf;
-    INTEGRATOR_STATE_WRITE(state, path, mis_origin_n) = sd->N;
+    INTEGRATOR_STATE_WRITE(state, path, mis_origin_n) = sc->N;
     INTEGRATOR_STATE_WRITE(state, path, min_ray_pdf) = fminf(
         unguided_bsdf_pdf, INTEGRATOR_STATE(state, path, min_ray_pdf));
   }

intern/cycles/blender/addon/properties.py
@@ -1019,6 +1019,11 @@ class CyclesMaterialSettings(bpy.types.PropertyGroup):
         "disabling will render faster but not give accurate shadows",
         default=True,
     )
+    use_bump_map_correction: BoolProperty(
+        name="Bump Map Correction",
+        description="Apply corrections to solve shadow terminator artifacts caused by bump mapping",
+        default=True,
+    )
     homogeneous_volume: BoolProperty(
         name="Homogeneous Volume",
         description="When using volume rendering, assume volume has the same density everywhere "

intern/cycles/blender/addon/ui.py
@@ -1967,6 +1967,7 @@ class CYCLES_MATERIAL_PT_settings_surface(CyclesButtonsPanel, Panel):
         col.prop(cmat, "displacement_method", text="Displacement")
         col.prop(cmat, "emission_sampling")
         col.prop(cmat, "use_transparent_shadow")
+        col.prop(cmat, "use_bump_map_correction")
 
     def draw(self, context):
         self.draw_shared(self, context.material)

intern/cycles/blender/shader.cpp
@@ -1551,6 +1551,7 @@ void BlenderSync::sync_materials(BL::Depsgraph &b_depsgraph, bool update_all)
       PointerRNA cmat = RNA_pointer_get(&b_mat.ptr, "cycles");
       shader->set_emission_sampling_method(get_emission_sampling(cmat));
       shader->set_use_transparent_shadow(get_boolean(cmat, "use_transparent_shadow"));
+      shader->set_use_bump_map_correction(get_boolean(cmat, "use_bump_map_correction"));
       shader->set_heterogeneous_volume(!get_boolean(cmat, "homogeneous_volume"));
       shader->set_volume_sampling_method(get_volume_sampling(cmat));
       shader->set_volume_interpolation_method(get_volume_interpolation(cmat));

intern/cycles/kernel/closure/bsdf_util.h
@@ -191,6 +191,9 @@ ccl_device float3 ensure_valid_specular_reflection(float3 Ng, float3 I, float3 N
  * normal and the shading normal is the same. */
 ccl_device float3 maybe_ensure_valid_specular_reflection(ccl_private ShaderData *sd, float3 N)
 {
+  if ((sd->flag & SD_USE_BUMP_MAP_CORRECTION) == 0) {
+    return N;
+  }
   if ((sd->type & PRIMITIVE_CURVE) || isequal(sd->Ng, N)) {
     return N;
   }

intern/cycles/kernel/types.h
@@ -831,6 +831,8 @@ enum ShaderDataFlag {
 
   /* Shader flags. */
 
+  /* Apply a correction term to smooth illumination on grazing angles when using bump mapping.. */
+  SD_USE_BUMP_MAP_CORRECTION = (1 << 15),
   /* Use front side for direct light sampling. */
   SD_MIS_FRONT = (1 << 16),
   /* Has transparent shadow. */

intern/cycles/scene/shader.cpp
@@ -54,6 +54,7 @@ NODE_DEFINE(Shader)
               EMISSION_SAMPLING_AUTO);
 
   SOCKET_BOOLEAN(use_transparent_shadow, "Use Transparent Shadow", true);
+  SOCKET_BOOLEAN(use_bump_map_correction, "Bump Map Correction", true);
   SOCKET_BOOLEAN(heterogeneous_volume, "Heterogeneous Volume", true);
 
   static NodeEnum volume_sampling_method_enum;
@@ -580,6 +581,9 @@ void ShaderManager::device_update_common(Device * /*device*/,
     }
     if (shader->get_displacement_method() != DISPLACE_BUMP) {
       flag |= SD_HAS_DISPLACEMENT;
+    }
+    if (shader->get_use_bump_map_correction()) {
+      flag |= SD_USE_BUMP_MAP_CORRECTION;
     }
 
     /* constant emission check */

commit 95457a4570e38edd70d04ace1a977d4b4485562d

Fix #120119: Area lamp artifacts in Cycles with light trees
This is a regression in 4.1, caused by 36e603c430.

On a code side the issue was caused by a change in the mis_origin_n
which is a normal at a scatter point for light sampling.

Details about that specific change are a bit unclear. If not an
overseen mistake, it could have been part of idea that when no
bump map correction is used artists should have fuller control over
shading normal.

In any case, the change to this normal lead to some discrepancies
with shading/sampling/NEE code which uses sd->N and has no access
to closure.

In order to bring code back to consistent state this change reverts
the change to mis_origin_n. However it also does some extra things
to help with the original file used during development the bump map
correction option, as without doing anything else the file renders
with firefly-looking artifacts.

Those artifacts are due to normal going into the surface. While the
original thought was to let artists to have full control over the
normals and deal with such cases from shader, it seems that it
happens more often, could be hard to understand by artists what's
going on, and is also not compatible with features like light
trees.

Hence this change also does partial bump map correction when the
option is disabled: namely it forces evaluation to be 0 when normal
goes below the surface. This minimizes an impact on the files which
are currently relying on the dump map corrections being disabled.

intern/cycles/kernel/closure/bsdf.h
@@ -60,7 +60,10 @@ ccl_device_inline float bsdf_get_roughness_pass_squared(ccl_private const Shader
 /* An additional term to smooth illumination on grazing angles when using bump mapping.
  * Based on "Taming the Shadow Terminator" by Matt Jen-Yuan Chiang,
  * Yining Karl Li and Brent Burley. */
-ccl_device_inline float bump_shadowing_term(float3 Ng, float3 N, float3 I)
+ccl_device_inline float bump_shadowing_term(ccl_private const ShaderData &sd,
+                                            float3 Ng,
+                                            float3 N,
+                                            float3 I)
 {
   const float cosNI = dot(N, I);
   if (cosNI < 0.0f) {
@@ -76,6 +79,14 @@ ccl_device_inline float bump_shadowing_term(float3 Ng, float3 N, float3 I)
   /* If the incoming light points away from the surface, return black. */
   if (g < 0.0f) {
     return 0.0f;
+  }
+
+  /* When bump map correction is not used do not do smoothing, let the normal to be the one which
+   * artists desires it to be via shader configuration.
+   * Effectively, when bump map correction is not used only scale shading to 0 when the normal
+   * points inside of the mesh, which is not well supported by functionality like light trees. */
+  if ((sd.flag & SD_USE_BUMP_MAP_CORRECTION) == 0) {
+    return 1.0f;
   }
 
   /* Return smoothed value to avoid discontinuity at perpendicular angle. */
@@ -235,8 +246,8 @@ ccl_device_inline int bsdf_sample(KernelGlobals kg,
       *eval *= shift_cos_in(cosNO, frequency_multiplier);
     }
     if (label & LABEL_DIFFUSE) {
-      if ((sd->flag & SD_USE_BUMP_MAP_CORRECTION) && !isequal(sc->N, sd->N)) {
-        *eval *= bump_shadowing_term(sd->N, sc->N, *wo);
+      if (!isequal(sc->N, sd->N)) {
+        *eval *= bump_shadowing_term(*sd, sd->N, sc->N, *wo);
       }
     }
   }
@@ -531,8 +542,8 @@ ccl_device_inline
   }
 
   if (CLOSURE_IS_BSDF_DIFFUSE(sc->type)) {
-    if ((sd->flag & SD_USE_BUMP_MAP_CORRECTION) && !isequal(sc->N, sd->N)) {
-      eval *= bump_shadowing_term(sd->N, sc->N, wo);
+    if (!isequal(sc->N, sd->N)) {
+      eval *= bump_shadowing_term(*sd, sd->N, sc->N, wo);
     }
   }
 

intern/cycles/kernel/integrator/shade_surface.h
@@ -507,7 +507,7 @@ ccl_device_forceinline int integrate_surface_bsdf_bssrdf_bounce(
   /* Update path state */
   if (!(label & LABEL_TRANSPARENT)) {
     INTEGRATOR_STATE_WRITE(state, path, mis_ray_pdf) = mis_pdf;
-    INTEGRATOR_STATE_WRITE(state, path, mis_origin_n) = sc->N;
+    INTEGRATOR_STATE_WRITE(state, path, mis_origin_n) = sd->N;
     INTEGRATOR_STATE_WRITE(state, path, min_ray_pdf) = fminf(
         unguided_bsdf_pdf, INTEGRATOR_STATE(state, path, min_ray_pdf));
 