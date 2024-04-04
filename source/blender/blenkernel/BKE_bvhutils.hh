Tony7466/BLENDER

commit 1c77779160abe2f02481041790d06a46819887b9

Refactor: Store a 'Mesh' in the editmesh snap cache and use it for snapping
In this commit, a temporary mesh is created representing the edit mesh.
This mesh is then used in the Edit Mesh snapping system instead of the
BMesh.

By using a Mesh object for snapping, we remove a considerable amount of
code and use a more optimized version of snapping.

This simplifies the code and makes it easier to implement new features.

## Performance test: Face + Edge + Vert
|        | Cache     | Gen. Snap |
|--------|-----------|-----------|
| Before | 680.88 ms | 0.1250 ms |
| After  | 489.06 ms | 0.1064 ms |
| Improv | 28.65%    | 14.88%    |

## Performance test: Face
|        | Cache     | Gen. Snap |
|--------|-----------|-----------|
| Before | 293.90 ms | 0.0230 ms |
| After  | 411.92 ms | 0.0256 ms |
| Improv | -40.15%   | -11.30%   |

Pull Request: https://projects.blender.org/blender/blender/pulls/117047

source/blender/blenkernel/BKE_bvhutils.hh
@@ -16,26 +16,11 @@
 #include "BLI_math_vector_types.hh"
 #include "BLI_span.hh"
 
-struct BMEditMesh;
 struct BVHCache;
 struct BVHTree;
 struct MFace;
 struct Mesh;
 struct PointCloud;
-
-/**
- * Struct that stores basic information about a BVHTree built from a edit-mesh.
- */
-struct BVHTreeFromEditMesh {
-  BVHTree *tree;
-
-  /** Default callbacks to BVH nearest and ray-cast. */
-  BVHTree_NearestPointCallback nearest_callback;
-  BVHTree_RayCastCallback raycast_callback;
-
-  /* Private data */
-  bool cached;
-};
 
 /**
  * Struct that stores basic information about a #BVHTree built from a mesh.
@@ -69,38 +54,9 @@ enum BVHCacheType {
   BVHTREE_FROM_LOOSEVERTS,
   BVHTREE_FROM_LOOSEEDGES,
 
-  BVHTREE_FROM_EM_LOOSEVERTS,
-  BVHTREE_FROM_EM_EDGES,
-  BVHTREE_FROM_EM_LOOPTRIS,
-
   /* Keep `BVHTREE_MAX_ITEM` as last item. */
   BVHTREE_MAX_ITEM,
 };
-
-/**
- * Builds a BVH tree where nodes are the relevant elements of the given mesh.
- * Configures #BVHTreeFromMesh.
- *
- * The tree is build in mesh space coordinates, this means special care must be made on queries
- * so that the coordinates and rays are first translated on the mesh local coordinates.
- * Reason for this is that bvh_from_mesh_* can use a cache in some cases and so it
- * becomes possible to reuse a #BVHTree.
- *
- * #free_bvhtree_from_mesh should be called when the tree is no longer needed.
- */
-BVHTree *bvhtree_from_editmesh_verts(
-    BVHTreeFromEditMesh *data, BMEditMesh *em, float epsilon, int tree_type, int axis);
-
-/**
- * Builds a BVH-tree where nodes are the vertices of the given `em`.
- */
-BVHTree *bvhtree_from_editmesh_verts_ex(BVHTreeFromEditMesh *data,
-                                        BMEditMesh *em,
-                                        blender::BitSpan verts_mask,
-                                        int verts_num_active,
-                                        float epsilon,
-                                        int tree_type,
-                                        int axis);
 
 /**
  * Builds a BVH-tree where nodes are the given vertices (NOTE: does not copy given `vert`!).
@@ -116,20 +72,6 @@ BVHTree *bvhtree_from_mesh_verts_ex(BVHTreeFromMesh *data,
                                     float epsilon,
                                     int tree_type,
                                     int axis);
-
-BVHTree *bvhtree_from_editmesh_edges(
-    BVHTreeFromEditMesh *data, BMEditMesh *em, float epsilon, int tree_type, int axis);
-
-/**
- * Builds a BVH-tree where nodes are the edges of the given `em`.
- */
-BVHTree *bvhtree_from_editmesh_edges_ex(BVHTreeFromEditMesh *data,
-                                        BMEditMesh *em,
-                                        blender::BitSpan edges_mask,
-                                        int edges_num_active,
-                                        float epsilon,
-                                        int tree_type,
-                                        int axis);
 
 /**
  * Builds a BVH-tree where nodes are the given edges.
@@ -147,20 +89,6 @@ BVHTree *bvhtree_from_mesh_edges_ex(BVHTreeFromMesh *data,
                                     float epsilon,
                                     int tree_type,
                                     int axis);
-
-BVHTree *bvhtree_from_editmesh_corner_tris(
-    BVHTreeFromEditMesh *data, BMEditMesh *em, float epsilon, int tree_type, int axis);
-
-/**
- * Builds a BVH-tree where nodes are triangles faces (#MLoopTri) of the given `bm`.
- */
-BVHTree *bvhtree_from_editmesh_looptris_ex(BVHTreeFromEditMesh *data,
-                                           BMEditMesh *em,
-                                           blender::BitSpan corner_tris_mask,
-                                           int corner_tris_num_active,
-                                           float epsilon,
-                                           int tree_type,
-                                           int axis);
 
 /**
  * Builds a BVH-tree where nodes are the triangle faces (#MLoopTri) of the given mesh.
@@ -186,20 +114,6 @@ BVHTree *BKE_bvhtree_from_mesh_get(BVHTreeFromMesh *data,
                                    BVHCacheType bvh_cache_type,
                                    int tree_type);
 
-/**
- * Builds or queries a BVH-cache for the cache BVH-tree of the request type.
- */
-BVHTree *BKE_bvhtree_from_editmesh_get(BVHTreeFromEditMesh *data,
-                                       BMEditMesh *em,
-                                       int tree_type,
-                                       BVHCacheType bvh_cache_type,
-                                       BVHCache **bvh_cache_p,
-                                       std::mutex *mesh_eval_mutex);
-
-/**
- * Frees data allocated by a call to `bvhtree_from_editmesh_*`.
- */
-void free_bvhtree_from_editmesh(BVHTreeFromEditMesh *data);
 /**
  * Frees data allocated by a call to `bvhtree_from_mesh_*`.
  */

commit 7e8aa02c7247e981e9a39f1d4f80809d65c10349

Fix #120239: Snap in edit mode ignoring some elements
Caused by 1c77779160 when we create a mesh representing the edited
mesh.

Caused by 1c77779160 when we create a mesh representing the edited
mesh.

Mesh Snap to Vertex works in the following order:
- Snap to vertices of visible triangles
- Snap to vertices of Loose Edges
- Snap to Loose Vertices

The problem happens because, in editing mode, we ignore faces whose
vertices are being transformed. We mark this face as being hidden in
the snap. So we lost some vertices in triangles.

The solution then is to consider the edges and vertices of hidden faces
as loose elements, because, although connected to faces, they are still
visible to snap.

Therefore, two new types of BVHTree were created:
- BVHTREE_FROM_LOOSEVERTS_NO_HIDDEN
- BVHTREE_FROM_LOOSEEDGES_NO_HIDDEN

The disadvantage of this solution is that we can no longer rely on
cached `mesh->loose_verts()` and `mesh->loose_edges()` which can be bad
for performance.

source/blender/blenkernel/BKE_bvhutils.hh
@@ -55,6 +55,12 @@ enum BVHCacheType {
   BVHTREE_FROM_LOOSEVERTS,
   BVHTREE_FROM_LOOSEEDGES,
 
+  /* These types consider geometry visibility when getting loose elements.
+   * NOTE: If the element is linked to a face or edge that is hidden, but the element itself is not
+   * hidden, it is considered a loose element. */
+  BVHTREE_FROM_LOOSEVERTS_NO_HIDDEN,
+  BVHTREE_FROM_LOOSEEDGES_NO_HIDDEN,
+
   /* Keep `BVHTREE_MAX_ITEM` as last item. */
   BVHTREE_MAX_ITEM,
 };

source/blender/blenkernel/intern/bvhutils.cc
@@ -509,14 +509,15 @@ static void bvhtree_from_mesh_setup_data(BVHTree *tree,
   switch (bvh_cache_type) {
     case BVHTREE_FROM_VERTS:
     case BVHTREE_FROM_LOOSEVERTS:
+    case BVHTREE_FROM_LOOSEVERTS_NO_HIDDEN:
       /* a nullptr nearest callback works fine
        * remember the min distance to point is the same as the min distance to BV of point */
       r_data->nearest_callback = nullptr;
       r_data->raycast_callback = mesh_verts_spherecast;
       break;
-
     case BVHTREE_FROM_EDGES:
     case BVHTREE_FROM_LOOSEEDGES:
+    case BVHTREE_FROM_LOOSEEDGES_NO_HIDDEN:
       r_data->nearest_callback = mesh_edges_nearest_point;
       r_data->raycast_callback = mesh_edges_spherecast;
       break;
@@ -780,6 +781,86 @@ BVHTree *bvhtree_from_mesh_corner_tris_ex(BVHTreeFromMesh *data,
   return tree;
 }
 
+static BitVector<> loose_verts_no_hidden_map_get(const Mesh &mesh, int *r_corner_tris_active_len)
+{
+  using namespace blender;
+  using namespace blender::bke;
+
+  int count = mesh.verts_num;
+  BitVector<> verts_mask(count, true);
+
+  AttributeAccessor attributes = mesh.attributes();
+  const Span<int2> &edges = mesh.edges();
+  const VArray<bool> &hide_edge = *attributes.lookup_or_default(
+      ".hide_edge", AttrDomain::Edge, false);
+  const VArray<bool> &hide_vert = *attributes.lookup_or_default(
+      ".hide_vert", AttrDomain::Point, false);
+
+  for (const int i : edges.index_range()) {
+    if (hide_edge[i]) {
+      continue;
+    }
+    for (const int vert_index : {edges[i][0], edges[i][1]}) {
+      if (verts_mask[vert_index]) {
+        verts_mask[vert_index].reset();
+        count--;
+      }
+    }
+  }
+
+  for (const int vert_index : verts_mask.index_range()) {
+    if (verts_mask[vert_index] && hide_vert[vert_index]) {
+      verts_mask[vert_index].reset();
+      count--;
+    }
+  }
+
+  *r_corner_tris_active_len = count;
+
+  return verts_mask;
+}
+
+static BitVector<> loose_edges_no_hidden_map_get(const Mesh &mesh, int *r_corner_tris_active_len)
+{
+  using namespace blender;
+  using namespace blender::bke;
+
+  int count = mesh.edges_num;
+  BitVector<> edge_mask(count, true);
+
+  AttributeAccessor attributes = mesh.attributes();
+  const OffsetIndices<int> &faces = mesh.faces();
+  const Span<int> &corner_edges = mesh.corner_edges();
+  const VArray<bool> &hide_poly = *attributes.lookup_or_default(
+      ".hide_poly", AttrDomain::Face, false);
+  const VArray<bool> &hide_edge = *attributes.lookup_or_default(
+      ".hide_edge", AttrDomain::Edge, false);
+
+  for (const int i : faces.index_range()) {
+    if (hide_poly[i]) {
+      continue;
+    }
+    for (const int corner_index : faces[i]) {
+      const int edge_index = corner_edges[corner_index];
+      if (edge_mask[edge_index]) {
+        edge_mask[edge_index].reset();
+        count--;
+      }
+    }
+  }
+
+  for (const int edge_index : edge_mask.index_range()) {
+    if (edge_mask[edge_index] && hide_edge[edge_index]) {
+      edge_mask[edge_index].reset();
+      count--;
+    }
+  }
+
+  *r_corner_tris_active_len = count;
+
+  return edge_mask;
+}
+
 static BitVector<> corner_tris_no_hidden_map_get(const blender::OffsetIndices<int> faces,
                                                  const VArray<bool> &hide_poly,
                                                  const int corner_tris_len,
@@ -854,25 +935,38 @@ BVHTree *BKE_bvhtree_from_mesh_get(BVHTreeFromMesh *data,
   /* Create BVHTree. */
 
   switch (bvh_cache_type) {
-    case BVHTREE_FROM_LOOSEVERTS: {
-      const LooseVertCache &loose_verts = mesh->loose_verts();
+    case BVHTREE_FROM_VERTS:
+    case BVHTREE_FROM_LOOSEVERTS:
+    case BVHTREE_FROM_LOOSEVERTS_NO_HIDDEN: {
+      int mask_bits_act_len = -1;
+      BitVector<> mask = {};
+      if (bvh_cache_type == BVHTREE_FROM_LOOSEVERTS_NO_HIDDEN) {
+        mask = loose_verts_no_hidden_map_get(*mesh, &mask_bits_act_len);
+      }
+      else if (bvh_cache_type == BVHTREE_FROM_LOOSEVERTS) {
+        const LooseVertCache &loose_verts = mesh->loose_verts();
+        mask = loose_verts.is_loose_bits;
+        mask_bits_act_len = loose_verts.count;
+      }
       data->tree = bvhtree_from_mesh_verts_create_tree(
-          0.0f, tree_type, 6, positions, loose_verts.is_loose_bits, loose_verts.count);
+          0.0f, tree_type, 6, positions, mask, mask_bits_act_len);
       break;
     }
-    case BVHTREE_FROM_VERTS: {
-      data->tree = bvhtree_from_mesh_verts_create_tree(0.0f, tree_type, 6, positions, {}, -1);
-      break;
-    }
-    case BVHTREE_FROM_LOOSEEDGES: {
-      const LooseEdgeCache &loose_edges = mesh->loose_edges();
+    case BVHTREE_FROM_EDGES:
+    case BVHTREE_FROM_LOOSEEDGES:
+    case BVHTREE_FROM_LOOSEEDGES_NO_HIDDEN: {
+      int mask_bits_act_len = -1;
+      BitVector<> mask = {};
+      if (bvh_cache_type == BVHTREE_FROM_LOOSEEDGES_NO_HIDDEN) {
+        mask = loose_edges_no_hidden_map_get(*mesh, &mask_bits_act_len);
+      }
+      else if (bvh_cache_type == BVHTREE_FROM_LOOSEVERTS) {
+        const LooseEdgeCache &loose_edges = mesh->loose_edges();
+        mask = loose_edges.is_loose_bits;
+        mask_bits_act_len = loose_edges.count;
+      }
       data->tree = bvhtree_from_mesh_edges_create_tree(
-          positions, edges, loose_edges.is_loose_bits, loose_edges.count, 0.0f, tree_type, 6);
-      break;
-    }
-    case BVHTREE_FROM_EDGES: {
-      data->tree = bvhtree_from_mesh_edges_create_tree(
-          positions, edges, {}, -1, 0.0f, tree_type, 6);
+          positions, edges, mask, mask_bits_act_len, 0.0f, tree_type, 6);
       break;
     }
     case BVHTREE_FROM_FACES: {

source/blender/editors/transform/transform_snap_object_mesh.cc
@@ -424,9 +424,13 @@ eSnapMode snap_edge_points_mesh(SnapObjectContext *sctx,
   return elem;
 }
 
-static eSnapMode mesh_snap_mode_supported(const Mesh *mesh)
+static eSnapMode mesh_snap_mode_supported(const Mesh *mesh, const bool use_hide)
 {
-  eSnapMode snap_mode_supported = mesh->loose_verts().count ? SCE_SNAP_TO_POINT : SCE_SNAP_TO_NONE;
+  /* When considering hidden geometry, we still cannot obtain the number of loose verts until
+   * computing #BVHTREE_FROM_LOOSEVERTS_NO_HIDDEN. Therefore, consider #SCE_SNAP_TO_POINT supported
+   * even if the mesh has no loose vertices in this case. */
+  eSnapMode snap_mode_supported = (use_hide || mesh->loose_verts().count) ? SCE_SNAP_TO_POINT :
+                                                                            SCE_SNAP_TO_NONE;
   if (mesh->faces_num) {
     snap_mode_supported |= SCE_SNAP_TO_FACE | SCE_SNAP_INDIVIDUAL_NEAREST | SNAP_TO_EDGE_ELEMENTS;
   }
@@ -455,7 +459,8 @@ static eSnapMode snapMesh(SnapObjectContext *sctx,
     }
   }
 
-  snap_to &= mesh_snap_mode_supported(mesh_eval) & (SNAP_TO_EDGE_ELEMENTS | SCE_SNAP_TO_POINT);
+  snap_to &= mesh_snap_mode_supported(mesh_eval, use_hide) &
+             (SNAP_TO_EDGE_ELEMENTS | SCE_SNAP_TO_POINT);
   if (snap_to == SCE_SNAP_TO_NONE) {
     return SCE_SNAP_TO_NONE;
   }
@@ -464,10 +469,18 @@ static eSnapMode snapMesh(SnapObjectContext *sctx,
   snap_object_data_mesh_get(mesh_eval, use_hide, &treedata);
 
   BVHTree *bvhtree[2] = {nullptr};
-  bvhtree[0] = BKE_bvhtree_from_mesh_get(&treedata_dummy, mesh_eval, BVHTREE_FROM_LOOSEEDGES, 2);
+  bvhtree[0] = BKE_bvhtree_from_mesh_get(&treedata_dummy,
+                                         mesh_eval,
+                                         use_hide ? BVHTREE_FROM_LOOSEEDGES_NO_HIDDEN :
+                                                    BVHTREE_FROM_LOOSEEDGES,
+                                         2);
   BLI_assert(treedata_dummy.cached);
   if (snap_to & SCE_SNAP_TO_POINT) {
-    bvhtree[1] = BKE_bvhtree_from_mesh_get(&treedata_dummy, mesh_eval, BVHTREE_FROM_LOOSEVERTS, 2);
+    bvhtree[1] = BKE_bvhtree_from_mesh_get(&treedata_dummy,
+                                           mesh_eval,
+                                           use_hide ? BVHTREE_FROM_LOOSEVERTS_NO_HIDDEN :
+                                                      BVHTREE_FROM_LOOSEVERTS,
+                                           2);
     BLI_assert(treedata_dummy.cached);
   }
 