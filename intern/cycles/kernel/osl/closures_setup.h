5a10b83714b1a1ee607120c643c8d06aa54e577a
David Murmann <david@23x5.net>
2024-04-08 01:11:05 +0200

    add OSL support

--- intern/cycles/kernel/osl/closures_setup.h
+++ intern/cycles/kernel/osl/closures_setup.h
@@ -197,6 +197,16 @@ ccl_device void osl_closure_transparent_setup(KernelGlobals kg,
   bsdf_transparent_setup(sd, rgb_to_spectrum(weight), path_flag);
 }
 
+ccl_device void osl_closure_portal_bsdf_setup(KernelGlobals kg,
+                                              ccl_private ShaderData *sd,
+                                              uint32_t path_flag,
+                                              float3 weight,
+                                              ccl_private const PortalBSDFClosure *closure,
+                                              float3 *layer_albedo)
+{
+  bsdf_portal_setup(sd, rgb_to_spectrum(weight), path_flag, closure->position, closure->direction);
+}
+
 /* MaterialX closures */
 ccl_device void osl_closure_dielectric_bsdf_setup(KernelGlobals kg,
                                                   ccl_private ShaderData *sd,
--- intern/cycles/kernel/osl/shaders/stdcycles.h
+++ intern/cycles/kernel/osl/shaders/stdcycles.h
@@ -63,4 +63,7 @@ closure color hair_huang(normal N,
 closure color henyey_greenstein(float g) BUILTIN;
 closure color absorption() BUILTIN;
 
+// Portal
+closure color portal_bsdf(vector position, vector direction) BUILTIN;
+
 #endif /* CCL_STDOSL_H */
