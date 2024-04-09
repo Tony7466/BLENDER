67e731c1728ef03854b06afe00a60d15b47631e3
Falk David <falk@blender.org>
2024-04-09 17:41:06 +0200

    Formatting

--- source/blender/editors/gpencil_legacy/gpencil_paint.cc
+++ source/blender/editors/gpencil_legacy/gpencil_paint.cc
@@ -2044,7 +2044,8 @@ static void gpencil_init_colors(tGPsdata *p)
   /* Reduce slightly the opacity of fill to make easy fill areas while drawing. */
   gpd->runtime.fill_opacity_fac = 0.8f;
   if ((brush->gpencil_settings->flag & GP_BRUSH_DISSABLE_LASSO) != 0) {
-    /* Don't set it to 0 so that there is still some feedback if the material has no stroke color. */
+    /* Don't set it to 0 so that there is still some feedback if the material has no stroke color.
+     */
     gpd->runtime.fill_opacity_fac = 0.1f;
   }
 }
