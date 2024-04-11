dcca52b6047494740f1e80a066778ebacf85cb54
Raul Fernandez <farsthary84@gmail.com>
2024-04-11 19:12:59 -0400

    Revert "Cleanup: avoid redundant string formatting"
    This reverts commit 80a082a3e361665d2ab1bb4db45c8953242fd3e9.

--- scripts/startup/bl_ui/space_view3d_toolbar.py
+++ scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -640,9 +640,8 @@ class VIEW3D_PT_slots_color_attributes(Panel):
 
     def draw_header(self, context):
         me = context.object.data
-        active_color = me.color_attributes.active_color
         self.bl_label = (
-            iface_(active_color.name) if active_color else
+            iface_("%s") % (me.color_attributes.active_color.name) if me.color_attributes.active_color else
             iface_("Color Attributes")
         )
 
@@ -683,7 +682,7 @@ class VIEW3D_PT_slots_vertex_groups(Panel):
         ob = context.object
         groups = ob.vertex_groups
         self.bl_label = (
-            iface_(groups.active.name) if groups and groups.active else
+            iface_("%s") % (groups.active.name) if groups and groups.active else
             iface_("Vertex Groups")
         )
 
