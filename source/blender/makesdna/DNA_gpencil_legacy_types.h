859edb1776058334f5093fb212d179afe722590e
Raul Fernandez <farsthary84@gmail.com>
2024-04-11 19:13:25 -0400

    Revert "Fix #120436: "Show Fill Color" setting not working"
    This reverts commit 78abe7d651a4e5e03b1533c59e4d95c4eed192e7.

--- source/blender/makesdna/DNA_gpencil_legacy_types.h
+++ source/blender/makesdna/DNA_gpencil_legacy_types.h
@@ -652,9 +652,6 @@ typedef struct bGPdata_Runtime {
   /** Vertex Color applied to Fill (while drawing). */
   float vert_color_fill[4];
 
-  /** Opacity for fills while drawing. */
-  float fill_opacity_fac;
-
   /** Arrow points for stroke corners. */
   float arrow_start[8];
   float arrow_end[8];
@@ -664,6 +661,7 @@ typedef struct bGPdata_Runtime {
 
   /** Number of control-points for stroke. */
   int tot_cp_points;
+  char _pad2[4];
   /** Array of control-points for stroke. */
   bGPDcontrolpoint *cp_points;
   /** Brush pointer */
