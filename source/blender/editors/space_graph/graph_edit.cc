efc088aa9400cfe38f5be88136019e8842a4fca4
Raul Fernandez <farsthary84@gmail.com>
2024-04-11 19:14:23 -0400

    Revert "Cleanup: remove redundant checks & struct declaration"
    This reverts commit 45a11276f826d0c580bcb2aba030cb6d7e6b4f2f.

--- source/blender/editors/space_graph/graph_edit.cc
+++ source/blender/editors/space_graph/graph_edit.cc
@@ -115,6 +115,7 @@ static void insert_graph_keys(bAnimContext *ac, eGraphKeys_InsertKey_Types mode)
   SpaceGraph *sipo = (SpaceGraph *)ac->sl;
   Scene *scene = ac->scene;
   ToolSettings *ts = scene->toolsettings;
+  eInsertKeyFlags flag = eInsertKeyFlags(0);
 
   /* Filter data. */
   filter = (ANIMFILTER_DATA_VISIBLE | ANIMFILTER_CURVE_VISIBLE | ANIMFILTER_FCURVESONLY |
@@ -145,7 +146,7 @@ static void insert_graph_keys(bAnimContext *ac, eGraphKeys_InsertKey_Types mode)
   }
 
   /* Init key-framing flag. */
-  eInsertKeyFlags flag = ANIM_get_keyframing_flags(scene);
+  flag = ANIM_get_keyframing_flags(scene);
   KeyframeSettings settings = get_keyframe_settings(true);
   settings.keyframe_type = eBezTriple_KeyframeType(ts->keyframe_type);
 
