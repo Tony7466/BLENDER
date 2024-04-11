1c871d40462a0e73996b39692b869d0c70c22e23
Raul Fernandez <farsthary84@gmail.com>
2024-04-11 19:14:08 -0400

    Revert "Anim: show animated properties with the appropriate color in the GUI"
    This reverts commit 250a92b5d81712a99195efdd30e34856721dc9d9.

--- source/blender/editors/animation/anim_channels_edit.cc
+++ source/blender/editors/animation/anim_channels_edit.cc
@@ -39,7 +39,6 @@
 #include "BKE_lib_id.hh"
 #include "BKE_mask.h"
 #include "BKE_nla.h"
-#include "BKE_scene.hh"
 #include "BKE_screen.hh"
 #include "BKE_workspace.h"
 
@@ -4577,7 +4576,7 @@ static blender::Vector<FCurve *> get_fcurves_of_property(
     const int length = RNA_property_array_length(ptr, prop);
     for (int i = 0; i < length; i++) {
       FCurve *fcurve = BKE_animadata_fcurve_find_by_rna_path(
-          id, anim_data, path->c_str(), i, nullptr, nullptr);
+          anim_data, path->c_str(), i, nullptr, nullptr);
       if (fcurve != nullptr) {
         fcurves.append(fcurve);
       }
@@ -4585,7 +4584,7 @@ static blender::Vector<FCurve *> get_fcurves_of_property(
   }
   else {
     FCurve *fcurve = BKE_animadata_fcurve_find_by_rna_path(
-        id, anim_data, path->c_str(), index, nullptr, nullptr);
+        anim_data, path->c_str(), index, nullptr, nullptr);
     if (fcurve != nullptr) {
       fcurves.append(fcurve);
     }
