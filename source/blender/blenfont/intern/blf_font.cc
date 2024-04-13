101963a748fe6f2be159c500ea6b050132a7c512
Harley Acheson <harley.acheson@gmail.com>
2024-04-13 14:01:59 -0700

    Refactor: BLF Extract Function BLF_str_offset_to_cursor
    Move into own BLF function, code that returns a text cursor (caret)
    position given a string offset. This code is currently just in
    interface_widgets.cc but should be a separate BLF function.

--- source/blender/blenfont/intern/blf_font.cc
+++ source/blender/blenfont/intern/blf_font.cc
@@ -1075,6 +1075,51 @@ void blf_str_offset_to_glyph_bounds(FontBLF *font,
   *glyph_bounds = data.bounds;
 }
 
+int blf_str_offset_to_cursor(
+    FontBLF *font, const char *str, size_t str_len, size_t str_offset, float cursor_width)
+{
+  if (!str || !str[0]) {
+    return 0;
+  }
+
+  /* Find right edge of previous character if available. */
+  rcti prev = {0};
+  if (str_offset > 0) {
+    blf_str_offset_to_glyph_bounds(font, str, str_offset - 1, &prev);
+  }
+
+  /* Find left edge of next character if available. */
+  rcti next = {0};
+  if (str_offset < strlen(str)) {
+    blf_str_offset_to_glyph_bounds(font, str, str_offset, &next);
+  }
+
+  if ((prev.xmax == prev.xmin) && next.xmax) {
+    /* Left of the first character or a space. */
+    return next.xmin - int(cursor_width);
+  }
+  else if ((prev.xmax != prev.xmin) && !next.xmax) {
+    /* Right of the last character. */
+    return prev.xmax;
+  }
+  else if (prev.xmax && next.xmax) {
+    /* Middle of the string, so in between. */
+    if (next.xmin >= prev.xmax) {
+      return int((prev.xmax + next.xmin - cursor_width) / 2.0f);
+    }
+    /* Possible with RTL. */
+    return int((next.xmax + prev.xmin - cursor_width) / 2.0f);
+  }
+  else if (!str_offset) {
+    /* Before first space character. */
+    return 0 - int(cursor_width);
+  }
+  else {
+    /* After last character. */
+    return int(blf_font_width(font, str, str_len, nullptr));
+  }
+}
+
 /** \} */
 
 /* -------------------------------------------------------------------- */
