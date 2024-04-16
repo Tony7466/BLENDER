0e4f509040bc02881d85b217fa4a480857e93d4e
David-Haver <davidnhaver@gmail.com>
2024-04-16 19:37:48 +0300

    Forgot to make format

--- source/blender/geometry/GEO_join_geometries.hh
+++ source/blender/geometry/GEO_join_geometries.hh
@@ -12,7 +12,7 @@ namespace blender::geometry {
 bke::GeometrySet join_geometries(Span<bke::GeometrySet> geometries,
                                  const bke::AnonymousAttributePropagationInfo &propagation_info);
 
-void join_attributes(const Span<const GeometryComponent *> src_components,
-                            GeometryComponent &r_result,
-                            const Span<StringRef> ignored_attributes = {})
+void join_attributes(const Span<const bke::GeometryComponent *> src_components,
+                     bke::GeometryComponent &r_result,
+                     const Span<StringRef> ignored_attributes = {})
 }  // namespace blender::geometry
