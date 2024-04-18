9f24344099a5f68f14f350b453f0a9954c5824e6
Ray Molenkamp <github@lazydodo.com>
2024-04-18 13:16:33 -0600

    WIP: make.bat enable .py formatting with make format
    autopep8_format_paths.py was never wired up in the windows
    tooling, this PR wires it up, but currently doesn't run
    because the command generated goes over the maximum command
    line length on windows (8191 characters)

---
+++ build_files/windows/autopep8.cmd
@@ -0,0 +1,15 @@
+if NOT EXIST %PYTHON% (
+    echo python not found, required for this operation
+    exit /b 1
+)
+
+set FORMAT_PATHS=%BLENDER_DIR%\tools\utils_maintenance\autopep8_format_paths.py
+
+for %%a in (%PYTHON%) do (
+    set PEP8_LOCATION=%%~dpa\..\lib\site-packages\autopep8.py
+)
+
+REM Use -B to avoid writing __pycache__ in lib directory and causing update conflicts.
+%PYTHON% -B %FORMAT_PATHS%  --autopep8-command "%PEP8_LOCATION%" %FORMAT_ARGS%
+
+:EOF
