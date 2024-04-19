d46a76dc93df8a6754875301d740514c48de76f7
Clément Foucault <foucault.clem@gmail.com>
2024-04-19 11:57:53 +0200

    Fix make format

--- scripts/startup/bl_ui/properties_render.py
+++ scripts/startup/bl_ui/properties_render.py
@@ -840,7 +840,6 @@ class RENDER_PT_eevee_next_sampling_viewport(RenderButtonsPanel, Panel):
         col.prop(props, "use_taa_reprojection", text="Temporal Reprojection")
         col.prop(props, "use_shadow_jittered_viewport", text="Jittered Shadows")
 
-
         # Add SSS sample count here.
 
 
