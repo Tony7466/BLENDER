199e2d3365eb4ebaa8ab90343cde3382f847807c
Jacques Lucke <jacques@blender.org>
2024-04-22 20:38:20 +0200

    fix

--- tools/triage/gitea_utils.py
+++ tools/triage/gitea_utils.py
@@ -40,10 +40,10 @@ def url_json_get(url: str) -> Optional[Union[Dict[str, Any], List[Dict[str, Any]
 
 def url_json_get_all_pages(
         url: str,
-        limit: int = 30,
+        limit: int = 50,
         verbose: bool = False,
 ) -> List[Dict[str, Any]]:
-    assert limit <= 30, "30 is the maximum limit of items per page"
+    assert limit <= 50, "50 is the maximum limit of items per page"
     result: List[Dict[str, Any]] = []
     page = 1
     while True:
@@ -61,7 +61,7 @@ def url_json_get_all_pages(
         assert isinstance(result_page, list)
         result.extend(result_page)
 
-        if len(result_page) < limit:
+        if len(result_page) == 0:
             break
 
         page += 1
