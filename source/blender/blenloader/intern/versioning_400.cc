409fdcb2f5c7aab5b3134ea88e8b3a95b8c40ea0
Hans Goudey <hans@blender.org>
2024-04-24 11:26:29 -0400

    Don't hide header region

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -2082,7 +2082,6 @@ static void add_image_editor_asset_shelf(Main &bmain)
                 regionbase, RGN_TYPE_ASSET_SHELF_HEADER, __func__, RGN_TYPE_ASSET_SHELF))
         {
           new_shelf_header->alignment = RGN_ALIGN_BOTTOM | RGN_ALIGN_HIDE_WITH_PREV;
-          new_shelf_header->flag |= RGN_FLAG_HIDDEN;
         }
       }
     }
