f651e537f9323906b14fda43a6bb554b21e4089c
Hans Goudey <hans@blender.org>
2024-04-24 09:57:40 -0400

    Add back versioning

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -3226,35 +3226,36 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 402, 21)) {
     update_paint_modes_for_brush_assets(*bmain);
 
-    // LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
-    //   LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
-    //     LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
-    //       if (sl->spacetype != SPACE_IMAGE) {
-    //         continue;
-    //       }
+    /* Add image editor asset shelf. */
+    LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
+      LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+        LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
+          if (sl->spacetype != SPACE_IMAGE) {
+            continue;
+          }
 
-    //       ListBase *regionbase = (sl == area->spacedata.first) ? &area->regionbase :
-    //                                                              &sl->regionbase;
+          ListBase *regionbase = (sl == area->spacedata.first) ? &area->regionbase :
+                                                                 &sl->regionbase;
 
-    //       if (ARegion *new_shelf_region = do_versions_add_region_if_not_found(
-    //               regionbase,
-    //               RGN_TYPE_ASSET_SHELF,
-    //               "asset shelf for spaceimage (versioning)",
-    //               RGN_TYPE_TOOL_HEADER))
-    //       {
-    //         new_shelf_region->alignment = RGN_ALIGN_BOTTOM;
-    //       }
-    //       if (ARegion *new_shelf_header = do_versions_add_region_if_not_found(
-    //               regionbase,
-    //               RGN_TYPE_ASSET_SHELF_HEADER,
-    //               "asset shelf header for spaceimage (versioning)",
-    //               RGN_TYPE_ASSET_SHELF))
-    //       {
-    //         new_shelf_header->alignment = RGN_ALIGN_BOTTOM | RGN_SPLIT_PREV;
-    //       }
-    //     }
-    //   }
-    // }
+          if (ARegion *new_shelf_region = do_versions_add_region_if_not_found(
+                  regionbase,
+                  RGN_TYPE_ASSET_SHELF,
+                  "asset shelf for spaceimage (versioning)",
+                  RGN_TYPE_TOOL_HEADER))
+          {
+            new_shelf_region->alignment = RGN_ALIGN_BOTTOM;
+          }
+          if (ARegion *new_shelf_header = do_versions_add_region_if_not_found(
+                  regionbase,
+                  RGN_TYPE_ASSET_SHELF_HEADER,
+                  "asset shelf header for spaceimage (versioning)",
+                  RGN_TYPE_ASSET_SHELF))
+          {
+            new_shelf_header->alignment = RGN_ALIGN_BOTTOM | RGN_ALIGN_HIDE_WITH_PREV;
+          }
+        }
+      }
+    }
   }
 
   /**
