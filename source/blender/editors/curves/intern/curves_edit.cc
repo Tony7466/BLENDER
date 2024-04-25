df72cc08ab0f02201d0bc715216528b19c12e441
Amelie Fondevilla <amelie.fondevilla@les-fees-speciales.coop>
2024-04-25 11:06:02 +0200

    Extrude points, inner point behavior only

--- source/blender/editors/curves/intern/curves_edit.cc
+++ source/blender/editors/curves/intern/curves_edit.cc
@@ -225,6 +225,98 @@ void duplicate_curves(bke::CurvesGeometry &curves, const IndexMask &mask)
   }
 }
 
-void extrude_curve_points(bke::CurvesGeometry &curves, const IndexMask &points_mask) {}
+void extrude_curve_points(bke::CurvesGeometry &curves, const IndexMask &mask)
+{
+  const OffsetIndices<int> points_by_curve = curves.points_by_curve();
+
+  Array<bool> points_to_duplicate(curves.points_num());
+  mask.to_bools(points_to_duplicate.as_mutable_span());
+  const int num_points_to_add = 2 * mask.size();
+
+  int curr_dst_point_start = 0;
+  Array<int> dst_to_src_point(num_points_to_add);
+  Vector<int> dst_curve_counts;
+  Vector<int> dst_to_src_curve;
+
+  /* Add the duplicated curves and points. */
+  for (const int curve_i : curves.curves_range()) {
+    const IndexRange points = points_by_curve[curve_i];
+    const Span<bool> curve_points_to_duplicate = points_to_duplicate.as_span().slice(points);
+
+    for (const int point_index : points) {
+      if (!curve_points_to_duplicate[point_index - points.first()]) {
+        continue;
+      }
+      dst_to_src_point[curr_dst_point_start] = point_index;
+      dst_to_src_point[curr_dst_point_start + 1] = point_index;
+      dst_curve_counts.append(2);
+      dst_to_src_curve.append(curve_i);
+      curr_dst_point_start += 2;
+    }
+  }
+
+  const int old_curves_num = curves.curves_num();
+  const int old_points_num = curves.points_num();
+  const int num_curves_to_add = dst_to_src_curve.size();
+
+  bke::MutableAttributeAccessor attributes = curves.attributes_for_write();
+
+  /* Delete selection attribute so that it will not have to be resized. */
+  remove_selection_attributes(attributes);
+
+  curves.resize(old_points_num + num_points_to_add, old_curves_num + num_curves_to_add);
+
+  MutableSpan<int> new_curve_offsets = curves.offsets_for_write();
+  array_utils::copy(dst_curve_counts.as_span(),
+                    new_curve_offsets.drop_front(old_curves_num).drop_back(1));
+  offset_indices::accumulate_counts_to_offsets(new_curve_offsets.drop_front(old_curves_num),
+                                               old_points_num);
+
+  /* Transfer curve and point attributes. */
+  attributes.for_all([&](const bke::AttributeIDRef &id, const bke::AttributeMetaData meta_data) {
+    bke::GSpanAttributeWriter attribute = attributes.lookup_for_write_span(id);
+    if (!attribute) {
+      return true;
+    }
+
+    switch (meta_data.domain) {
+      case bke::AttrDomain::Curve: {
+        bke::attribute_math::gather(
+            attribute.span,
+            dst_to_src_curve,
+            attribute.span.slice(IndexRange(old_curves_num, num_curves_to_add)));
+        break;
+      }
+      case bke::AttrDomain::Point: {
+        bke::attribute_math::gather(
+            attribute.span,
+            dst_to_src_point,
+            attribute.span.slice(IndexRange(old_points_num, num_points_to_add)));
+        break;
+      }
+      default: {
+        attribute.finish();
+        BLI_assert_unreachable();
+        return true;
+      }
+    }
+
+    attribute.finish();
+
+    return true;
+  });
+
+  curves.update_curve_types();
+  curves.tag_topology_changed();
+
+  for (const StringRef selection_name : get_curves_selection_attribute_names(curves)) {
+    bke::SpanAttributeWriter<bool> selection = attributes.lookup_or_add_for_write_span<bool>(
+        selection_name, bke::AttrDomain::Point);
+    for (int point_index = 0; point_index < num_points_to_add; ++point_index) {
+      selection.span[old_points_num + point_index] = (point_index % 2 == 0);
+    }
+    selection.finish();
+  }
+}
 
 }  // namespace blender::ed::curves
