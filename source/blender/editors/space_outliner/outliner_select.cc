73e57b7bed1aa6fdbb2db21bf75b153478262b87
Philipp Oeser <philipp@blender.org>
2024-04-26 15:01:27 +0200

    Fix #121075: Outliner Ctrl-clicking to toggle selection has speed limit
    There are a couple of keymap entries for `OUTLINER_OT_item_activate` in
    our keymap. One of them has Ctrl+dblclick as a binding to trigger
    recursion, but that one exits early when not dblclicking on the icon.
    
    It does not exit in a way that the non-dblclick variant can run, for
    that we need to exit with OPERATOR_PASS_THROUGH, so that the "other"
    operator can run as well.

--- source/blender/editors/space_outliner/outliner_select.cc
+++ source/blender/editors/space_outliner/outliner_select.cc
@@ -1846,7 +1846,7 @@ static int outliner_item_do_activate_from_cursor(bContext *C,
         }
         else {
           /* Double-clicked, but it wasn't on the icon. */
-          return OPERATOR_CANCELLED;
+          return OPERATOR_CANCELLED | OPERATOR_PASS_THROUGH;
         }
       }
       else {
