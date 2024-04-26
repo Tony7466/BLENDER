3279447e0296a7c97ff0ce434dce29ac12c6815f
Michael Parkin-White <mparkinwhite@apple.com>
2024-04-26 12:40:43 +0100

    EEVEE Next: Optimize closure light stack array access
    This PR ensures the ClosureLightStack is only accessed
    with constant indices. This reduces compiler spill and allows
    furhter constant folding, improving the deferred lighting
    evaluation performance by 30%.
    
    Authored by Apple: Michael Parkin-White

--- source/blender/draw/engines/eevee_next/shaders/eevee_deferred_light_frag.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_deferred_light_frag.glsl
@@ -34,9 +34,20 @@ void main()
   float vPz = dot(drw_view_forward(), P) - dot(drw_view_forward(), drw_view_position());
 
   ClosureLightStack stack;
-  for (int i = 0; i < LIGHT_CLOSURE_EVAL_COUNT && i < gbuf.closure_count; i++) {
-    stack.cl[i] = closure_light_new(gbuffer_closure_get(gbuf, i), V);
+  /* Unroll light stack array assignments to avoid non-constant indexing. */
+  if (gbuf.closure_count > 0) {
+    stack.cl[0] = closure_light_new(gbuffer_closure_get(gbuf, 0), V);
   }
+#if LIGHT_CLOSURE_EVAL_COUNT > 1
+  if (gbuf.closure_count > 1) {
+    stack.cl[1] = closure_light_new(gbuffer_closure_get(gbuf, 1), V);
+  }
+#endif
+#if LIGHT_CLOSURE_EVAL_COUNT > 2
+  if (gbuf.closure_count > 2) {
+    stack.cl[2] = closure_light_new(gbuffer_closure_get(gbuf, 2), V);
+  }
+#endif
 
   /* TODO(fclem): If transmission (no SSS) is present, we could reduce LIGHT_CLOSURE_EVAL_COUNT
    * by 1 for this evaluation and skip evaluating the transmission closure twice. */
@@ -85,8 +96,8 @@ void main()
     vec3 radiance_shadowed = vec3(0);
     vec3 radiance_unshadowed = vec3(0);
     for (int i = 0; i < LIGHT_CLOSURE_EVAL_COUNT && i < gbuf.closure_count; i++) {
-      radiance_shadowed += stack.cl[i].light_shadowed;
-      radiance_unshadowed += stack.cl[i].light_unshadowed;
+      radiance_shadowed += closure_light_get(stack, i).light_shadowed;
+      radiance_unshadowed += closure_light_get(stack, i).light_unshadowed;
     }
     /* TODO(fclem): Change shadow pass to be colored. */
     vec3 shadows = radiance_shadowed * safe_rcp(radiance_unshadowed);
@@ -117,7 +128,20 @@ void main()
         }
       }
       else {
-        stack.cl[i].light_shadowed += indirect_light;
+        /* Unroll array acess to avoid non-constant indexing. */
+        if (i == 0) {
+          stack.cl[0].light_shadowed += indirect_light;
+        }
+#if LIGHT_CLOSURE_EVAL_COUNT > 1
+        else if (i == 1) {
+          stack.cl[1].light_shadowed += indirect_light;
+        }
+#endif
+#if LIGHT_CLOSURE_EVAL_COUNT > 2
+        else if (i == 2) {
+          stack.cl[2].light_shadowed += indirect_light;
+        }
+#endif
       }
     }
   }
@@ -126,13 +150,16 @@ void main()
     int layer_index = gbuffer_closure_get_bin_index(gbuf, i);
     /* TODO(fclem): Layered texture. */
     if (layer_index == 0) {
-      imageStore(direct_radiance_1_img, texel, vec4(stack.cl[i].light_shadowed, 1.0));
+      imageStore(
+          direct_radiance_1_img, texel, vec4(closure_light_get(stack, i).light_shadowed, 1.0));
     }
     else if (layer_index == 1) {
-      imageStore(direct_radiance_2_img, texel, vec4(stack.cl[i].light_shadowed, 1.0));
+      imageStore(
+          direct_radiance_2_img, texel, vec4(closure_light_get(stack, i).light_shadowed, 1.0));
     }
     else if (layer_index == 2) {
-      imageStore(direct_radiance_3_img, texel, vec4(stack.cl[i].light_shadowed, 1.0));
+      imageStore(
+          direct_radiance_3_img, texel, vec4(closure_light_get(stack, i).light_shadowed, 1.0));
     }
   }
 }
--- source/blender/draw/engines/eevee_next/shaders/eevee_light_eval_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_light_eval_lib.glsl
@@ -115,6 +115,28 @@ struct ClosureLightStack {
   ClosureLight cl[LIGHT_CLOSURE_EVAL_COUNT];
 };
 
+ClosureLight closure_light_get(ClosureLightStack stack, int index)
+{
+  switch (index) {
+    case 0:
+      return stack.cl[0];
+#if LIGHT_CLOSURE_EVAL_COUNT > 1
+    case 1:
+      return stack.cl[1];
+#endif
+#if LIGHT_CLOSURE_EVAL_COUNT > 2
+    case 2:
+      return stack.cl[2];
+#endif
+#if LIGHT_CLOSURE_EVAL_COUNT > 3
+#  error
+#endif
+  }
+
+  ClosureLight closure_null;
+  return closure_null;
+}
+
 ClosureLight closure_light_new_ex(ClosureUndetermined cl,
                                   vec3 V,
                                   float thickness,
