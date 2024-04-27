1570d88ca9454a98f5b74b917842befc630620ee
Jacques Lucke <jacques@blender.org>
2024-04-27 22:44:41 +0200

    extract add operator

--- source/blender/nodes/NOD_socket_items_ops.hh
+++ source/blender/nodes/NOD_socket_items_ops.hh
@@ -13,13 +13,13 @@
 
 #include "ED_node.hh"
 
-namespace blender::nodes::socket_items {
+namespace blender::nodes::socket_items::ops {
 
 template<typename Accessor>
-inline void remove_item_operator(wmOperatorType *ot,
-                                 const char *name,
-                                 const char *idname,
-                                 const char *description)
+inline void remove_item(wmOperatorType *ot,
+                        const char *name,
+                        const char *idname,
+                        const char *description)
 {
   ot->name = name;
   ot->idname = idname;
@@ -47,4 +47,53 @@ inline void remove_item_operator(wmOperatorType *ot,
   };
 }
 
-}  // namespace blender::nodes::socket_items
+template<typename Accessor>
+inline void add_item_with_name_and_type(wmOperatorType *ot,
+                                        const char *name,
+                                        const char *idname,
+                                        const char *description)
+{
+  ot->name = name;
+  ot->idname = idname;
+  ot->description = description;
+
+  ot->exec = [](bContext *C, wmOperator * /*op*/) -> int {
+    PointerRNA node_ptr = CTX_data_pointer_get(C, "active_node");
+    bNode *node = static_cast<bNode *>(node_ptr.data);
+    if (node == nullptr) {
+      return OPERATOR_CANCELLED;
+    }
+    if (node->type != Accessor::node_type) {
+      return OPERATOR_CANCELLED;
+    }
+    socket_items::SocketItemsRef ref = Accessor::get_items_from_node(*node);
+    const int old_active_index = *ref.active_index;
+
+    eNodeSocketDatatype socket_type;
+    std::string name;
+    int dst_index;
+    if (old_active_index >= 0 && old_active_index < *ref.items_num) {
+      dst_index = old_active_index + 1;
+      const typename Accessor::ItemT &active_item = (*ref.items)[old_active_index];
+      socket_type = eNodeSocketDatatype(active_item.socket_type);
+      name = active_item.name;
+    }
+    else {
+      dst_index = *ref.items_num;
+      socket_type = SOCK_GEOMETRY;
+      /* Empty name so it is based on the type. */
+      name = "";
+    }
+    socket_items::add_item_with_socket_and_name<Accessor>(*node, socket_type, name.c_str());
+    dna::array::move_index(*ref.items, *ref.items_num, *ref.items_num - 1, dst_index);
+    *ref.active_index = dst_index;
+
+    bNodeTree *ntree = reinterpret_cast<bNodeTree *>(node_ptr.owner_id);
+    BKE_ntree_update_tag_node_property(ntree, node);
+    ED_node_tree_propagate_change(nullptr, CTX_data_main(C), ntree);
+    WM_main_add_notifier(NC_NODE | NA_EDITED, ntree);
+    return OPERATOR_FINISHED;
+  };
+}
+
+}  // namespace blender::nodes::socket_items::ops
