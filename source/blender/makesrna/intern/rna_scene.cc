06ca6af8495a0311ce228d52b1e96b8d4f9dcd39
Clément Foucault <foucault.clem@gmail.com>
2024-04-27 14:05:17 +0200

    Fix make format

--- source/blender/makesrna/intern/rna_scene.cc
+++ source/blender/makesrna/intern/rna_scene.cc
@@ -8419,7 +8419,6 @@ static void rna_def_scene_eevee(BlenderRNA *brna)
   RNA_def_property_override_flag(prop, PROPOVERRIDE_OVERRIDABLE_LIBRARY);
   RNA_def_property_update(prop, NC_SCENE | ND_RENDER_OPTIONS, nullptr);
 
-
   prop = RNA_def_property(srna, "use_shadow_high_bitdepth", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "flag", SCE_EEVEE_SHADOW_HIGH_BITDEPTH);
   RNA_def_property_ui_text(prop, "High Bit Depth", "Use 32-bit shadows");
