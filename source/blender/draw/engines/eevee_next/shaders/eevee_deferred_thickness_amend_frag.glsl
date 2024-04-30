e9b8f8deca3c8bdb16e10ab531712c8619580127
Clément Foucault <foucault.clem@gmail.com>
2024-04-30 12:45:01 +0200

    WIP

--- source/blender/draw/engines/eevee_next/shaders/eevee_deferred_thickness_amend_frag.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_deferred_thickness_amend_frag.glsl
@@ -11,7 +11,7 @@
  */
 
 #pragma BLENDER_REQUIRE(draw_view_lib.glsl)
-#pragma BLENDER_REQUIRE(eevee_shadow_lib.glsl)
+#pragma BLENDER_REQUIRE(eevee_shadow_tracing_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_light_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_light_iter_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_thickness_lib.glsl)
@@ -37,26 +37,34 @@ void thickness_from_shadow_single(uint l_idx,
 
   LightVector lv = light_vector_get(light, is_directional, P);
   float attenuation = light_attenuation_surface(light, is_directional, true, false, Ng, lv);
-  if ((attenuation < LIGHT_ATTENUATION_THRESHOLD)) {
+  if (attenuation < LIGHT_ATTENUATION_THRESHOLD) {
     return;
   }
-  /* Weight result by facing ratio to avoid harsh transitions. */
-  float weight = saturate(dot(lv.L, -Ng));
+
+  float texel_radius = shadow_texel_radius_at_position(light, is_directional, P);
+
+  /* Invert all biases to get value inside the surface. */
+  P -= Ng * (texel_radius * shadow_normal_offset(Ng, lv.L));
+  P -= lv.L * texel_radius;
+  /* Inverting this bias means we will over estimate the distance. Which removes some artifacts. */
+  // P -= (2.0 * texel_radius) * shadow_pcf_offset(lv.L, Ng, pcg(gl_FragCoord.xy));
+
   ShadowEvalResult result = shadow_sample(
       is_directional, shadow_atlas_tx, shadow_tilemaps_tx, light, P);
 
   if (result.light_visibilty == 0.0) {
+    /* Weight result by facing ratio to avoid harsh transitions. */
+    float weight = saturate(dot(lv.L, -Ng));
     /* Flatten the accumulation to avoid weighting the outliers too much. */
     thickness_accum += safe_sqrt(result.occluder_distance) * weight;
     weight_accum += weight;
   }
 }
 
-#define THICKNESS_NO_VALUE -1.0
 /**
  * Return the apparent thickness of an object behind surface considering all shadow maps
  * available. If no shadow-map has a record of the other side of the surface, this function
- * returns THICKNESS_NO_VALUE.
+ * returns -1.
  */
 float thickness_from_shadow(vec3 P, vec3 Ng, float vPz)
 {
@@ -74,7 +82,7 @@ float thickness_from_shadow(vec3 P, vec3 Ng, float vPz)
   LIGHT_FOREACH_END
 
   if (weight_accum == 0.0) {
-    return THICKNESS_NO_VALUE;
+    return -1.0;
   }
 
   float thickness = thickness_accum / weight_accum;
@@ -102,22 +110,20 @@ void main()
   vec3 Ng = gbuffer_normal_unpack(imageLoad(gbuf_normal_img, ivec3(texel, 0)).rg);
 
   float shadow_thickness = thickness_from_shadow(P, Ng, vPz);
-
+  if (shadow_thickness <= 0.0) {
+    return;
+  }
   /* Use manual fetch because gbuffer_read_thickness expect a read only texture input. */
   uint header = texelFetch(gbuf_header_tx, texel, 0).r;
   int data_layer = gbuffer_normal_count(header);
   vec2 data_packed = imageLoad(gbuf_normal_img, ivec3(texel, data_layer)).rg;
-  float thickness = gbuffer_thickness_unpack(data_packed.x);
-
-  if (shadow_thickness == THICKNESS_NO_VALUE) {
-    return;
+  float data_thickness = gbuffer_thickness_unpack(data_packed.x);
+  float gbuffer_thickness = abs(data_thickness);
+  /* Only amend the thickness if new value is inside the valid range [10%..150%]. */
+  if ((shadow_thickness > gbuffer_thickness * 0.01) &&
+      (shadow_thickness < gbuffer_thickness * 1.5))
+  {
+    data_packed.x = sign(data_thickness) * shadow_thickness;
+    imageStore(gbuf_normal_img, ivec3(texel, data_layer), vec4(data_packed, 0.0, 0.0));
   }
-
-  float thickness_modified = sign(thickness) * min(shadow_thickness, abs(thickness));
-  if (thickness_modified == thickness) {
-    return;
-  }
-
-  data_packed.x = thickness_modified;
-  imageStore(gbuf_normal_img, ivec3(texel, data_layer), vec4(data_packed, 0.0, 0.0));
 }
--- source/blender/draw/engines/eevee_next/shaders/eevee_shadow_tracing_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_shadow_tracing_lib.glsl
@@ -405,17 +405,11 @@ SHADOW_MAP_TRACE_FN(ShadowRayPunctual)
 
 /* Compute the world space offset of the shading position required for
  * stochastic percentage closer filtering of shadow-maps. */
-vec3 shadow_pcf_offset(
-    LightData light, const bool is_directional, vec3 L, vec3 Ng, float texel_radius, vec2 random)
+vec3 shadow_pcf_offset(vec3 L, vec3 Ng, vec2 random)
 {
-  if (light.pcf_radius <= 0.001) {
-    /* Early return. */
-    return vec3(0.0);
-  }
-
   /* We choose a random disk distribution because it is rotationally invariant.
    * This sames us the trouble of getting the correct orientation for punctual. */
-  vec2 disk_sample = sample_disk(random) * (texel_radius * light.pcf_radius);
+  vec2 disk_sample = sample_disk(random);
   /* Compute the offset as a disk around the normal. */
   mat3x3 tangent_frame = from_up_axis(Ng);
   vec3 pcf_offset = tangent_frame[0] * disk_sample.x + tangent_frame[1] * disk_sample.y;
@@ -425,7 +419,6 @@ vec3 shadow_pcf_offset(
      * polygon behind the shading point. */
     pcf_offset = reflect(pcf_offset, L);
   }
-
   return pcf_offset;
 }
 
@@ -480,7 +473,7 @@ float shadow_texel_radius_at_position(LightData light, const bool is_directional
  * shadowing from the current polygon, which is not enough in cases with adjacent polygons with
  * very different slopes.
  */
-float shadow_normal_offset(float texel_radius, vec3 Ng, vec3 L)
+float shadow_normal_offset(vec3 Ng, vec3 L)
 {
   /* Attenuate depending on light angle. */
   /* TODO: Should we take the light shape into consideration? */
@@ -489,7 +482,7 @@ float shadow_normal_offset(float texel_radius, vec3 Ng, vec3 L)
   /* Note that we still bias by one pixel anyway to fight quantization artifacts.
    * This helps with self intersection of slopped surfaces and gives softer soft shadow (?! why).
    * FIXME: This is likely to hide some issue, and we need a user facing bias parameter anyway. */
-  return texel_radius * (sin_theta + 3.0);
+  return sin_theta + 3.0;
 }
 
 /**
@@ -523,7 +516,7 @@ ShadowEvalResult shadow_eval(LightData light,
   /* Shadow map texel radius at the receiver position. */
   float texel_radius = shadow_texel_radius_at_position(light, is_directional, P);
 
-  P += shadow_pcf_offset(light, is_directional, L, Ng, texel_radius, random_pcf_2d);
+  P += (light.pcf_radius * texel_radius) * shadow_pcf_offset(L, Ng, random_pcf_2d);
 
   /* We want to bias inside the object for transmission to go through the object itself.
    * But doing so splits the shadow in two different directions at the horizon. Also this
@@ -534,7 +527,7 @@ ShadowEvalResult shadow_eval(LightData light,
   /* Avoid self intersection with respect to numerical precision. */
   P = offset_ray(P, N_bias);
   /* The above offset isn't enough in most situation. Still add a bigger bias. */
-  P += N_bias * shadow_normal_offset(texel_radius, Ng, L);
+  P += N_bias * (texel_radius * shadow_normal_offset(Ng, L));
 
   vec3 lP = is_directional ? light_world_to_local(light, P) :
                              light_world_to_local(light, P - light_position_get(light));
