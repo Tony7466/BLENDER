d60f8a17459488cf022e94f8b646e3e4da4f5a04
Jesse Yurkovich <jesse.y@gmail.com>
2024-05-01 13:31:38 -0700

    First pass at factoring out attribute reading common code

--- source/blender/io/usd/intern/usd_attribute_utils.hh
+++ source/blender/io/usd/intern/usd_attribute_utils.hh
@@ -3,12 +3,16 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
+#include "usd_hash_types.hh"
+
 #include "BLI_color.hh"
 #include "BLI_generic_virtual_array.hh"
 #include "BLI_math_quaternion_types.hh"
 #include "BLI_math_vector_types.hh"
 #include "BLI_span.hh"
 #include "BLI_virtual_array.hh"
+
+#include "BKE_attribute.hh"
 
 #include "DNA_customdata_types.h"
 
@@ -40,7 +44,7 @@ template<> struct is_layout_compatible<float2, pxr::GfVec2f> : std::true_type {}
 template<> struct is_layout_compatible<float3, pxr::GfVec3f> : std::true_type {};
 
 /* Conversion utilities to convert a Blender type to an USD type. */
-template<typename BlenderT, typename USDT> inline USDT convert_value(const BlenderT value)
+template<typename From, typename To> inline To convert_value(const From value)
 {
   return value;
 }
@@ -60,6 +64,19 @@ template<> inline pxr::GfVec3f convert_value(const ColorGeometry4f value)
 template<> inline pxr::GfQuatf convert_value(const math::Quaternion value)
 {
   return pxr::GfQuatf(value.x, value.y, value.z, value.w);
+}
+
+template<> inline float2 convert_value(const pxr::GfVec2f value)
+{
+  return float2(value[0], value[1]);
+}
+template<> inline float3 convert_value(const pxr::GfVec3f value)
+{
+  return float3(value[0], value[1], value[2]);
+}
+template<> inline ColorGeometry4f convert_value(const pxr::GfVec3f value)
+{
+  return ColorGeometry4f(value[0], value[1], value[2], 1.0f);
 }
 
 }  // namespace detail
@@ -92,7 +109,7 @@ constexpr std::optional<pxr::SdfValueTypeName> convert_blender_type_to_usd(
 template<typename BlenderT, typename USDT>
 void copy_blender_buffer_to_primvar(const VArray<BlenderT> &buffer,
                                     const pxr::UsdTimeCode timecode,
-                                    pxr::UsdGeomPrimvar primvar,
+                                    const pxr::UsdGeomPrimvar &primvar,
                                     pxr::UsdUtilsSparseValueWriter &value_writer)
 {
   constexpr bool is_same = std::is_same_v<BlenderT, USDT>;
@@ -124,9 +141,9 @@ void copy_blender_buffer_to_primvar(const VArray<BlenderT> &buffer,
 }
 
 inline void copy_blender_attribute_to_primvar(const GVArray &attribute,
-                                              const int data_type,
+                                              const eCustomDataType data_type,
                                               const pxr::UsdTimeCode timecode,
-                                              pxr::UsdGeomPrimvar primvar,
+                                              const pxr::UsdGeomPrimvar &primvar,
                                               pxr::UsdUtilsSparseValueWriter &value_writer)
 {
   switch (data_type) {
@@ -161,6 +178,195 @@ inline void copy_blender_attribute_to_primvar(const GVArray &attribute,
     default:
       BLI_assert_unreachable();
   }
+}
+
+inline std::optional<eCustomDataType> convert_usd_type_to_blender(
+    const pxr::SdfValueTypeName usd_type)
+{
+  static const blender::Map<pxr::SdfValueTypeName, eCustomDataType> type_map = []() {
+    blender::Map<pxr::SdfValueTypeName, eCustomDataType> map;
+    map.add_new(pxr::SdfValueTypeNames->FloatArray, CD_PROP_FLOAT);
+    map.add_new(pxr::SdfValueTypeNames->Double, CD_PROP_FLOAT);
+    map.add_new(pxr::SdfValueTypeNames->IntArray, CD_PROP_INT32);
+    map.add_new(pxr::SdfValueTypeNames->Float2Array, CD_PROP_FLOAT2);
+    map.add_new(pxr::SdfValueTypeNames->TexCoord2dArray, CD_PROP_FLOAT2);
+    map.add_new(pxr::SdfValueTypeNames->TexCoord2fArray, CD_PROP_FLOAT2);
+    map.add_new(pxr::SdfValueTypeNames->TexCoord2hArray, CD_PROP_FLOAT2);
+    map.add_new(pxr::SdfValueTypeNames->TexCoord3dArray, CD_PROP_FLOAT2);
+    map.add_new(pxr::SdfValueTypeNames->TexCoord3fArray, CD_PROP_FLOAT2);
+    map.add_new(pxr::SdfValueTypeNames->TexCoord3hArray, CD_PROP_FLOAT2);
+    map.add_new(pxr::SdfValueTypeNames->Float3Array, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Point3fArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Point3dArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Point3hArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Normal3fArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Normal3dArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Normal3hArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Vector3fArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Vector3hArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Vector3dArray, CD_PROP_FLOAT3);
+    map.add_new(pxr::SdfValueTypeNames->Color3fArray, CD_PROP_COLOR);
+    map.add_new(pxr::SdfValueTypeNames->Color3hArray, CD_PROP_COLOR);
+    map.add_new(pxr::SdfValueTypeNames->Color3dArray, CD_PROP_COLOR);
+    map.add_new(pxr::SdfValueTypeNames->StringArray, CD_PROP_STRING);
+    map.add_new(pxr::SdfValueTypeNames->BoolArray, CD_PROP_BOOL);
+    map.add_new(pxr::SdfValueTypeNames->QuatfArray, CD_PROP_QUATERNION);
+    map.add_new(pxr::SdfValueTypeNames->QuatdArray, CD_PROP_QUATERNION);
+    map.add_new(pxr::SdfValueTypeNames->QuathArray, CD_PROP_QUATERNION);
+    return map;
+  }();
+
+  const eCustomDataType *value = type_map.lookup_ptr(usd_type);
+  if (value == nullptr) {
+    return std::nullopt;
+  }
+
+  return *value;
+}
+
+template<typename T>
+pxr::VtArray<T> get_prim_attribute_array(const pxr::UsdGeomPrimvar &primvar,
+                                         const double motionSampleTime)
+{
+  pxr::VtArray<T> array;
+
+  pxr::VtValue primvar_val;
+
+  if (!primvar.ComputeFlattened(&primvar_val, motionSampleTime)) {
+    return array;
+  }
+
+  if (!primvar_val.CanCast<pxr::VtArray<T>>()) {
+    return array;
+  }
+
+  array = primvar_val.Cast<pxr::VtArray<T>>().template UncheckedGet<pxr::VtArray<T>>();
+  return array;
+}
+
+template<typename T>
+pxr::VtArray<T> get_primvar_array(const pxr::UsdGeomPrimvar &primvar,
+                                  const pxr::UsdTimeCode timecode)
+{
+  pxr::VtArray<T> array;
+
+  pxr::VtValue primvar_val;
+
+  if (!primvar.ComputeFlattened(&primvar_val, timecode)) {
+    return array;
+  }
+
+  if (!primvar_val.CanCast<pxr::VtArray<T>>()) {
+    return array;
+  }
+
+  array = primvar_val.Cast<pxr::VtArray<T>>().template UncheckedGet<pxr::VtArray<T>>();
+  return array;
+}
+
+template<typename USDT, typename BlenderT>
+void copy_primvar_to_blender_buffer(const pxr::UsdGeomPrimvar &primvar,
+                                    const pxr::UsdTimeCode timecode,
+                                    const blender::OffsetIndices<int> face_indices,
+                                    MutableSpan<BlenderT> attribute)
+{
+  const pxr::TfToken interp = primvar.GetInterpolation();
+  pxr::VtArray<USDT> primvar_array = get_primvar_array<USDT>(primvar, timecode);
+  if (primvar_array.empty()) {
+    return;
+  }
+
+  if (interp == pxr::UsdGeomTokens->constant) {
+    /* For situations where there's only a single item, flood fill the object. */
+    attribute.fill(detail::convert_value<USDT, BlenderT>(primvar_array[0]));
+  }
+  else if (interp == pxr::UsdGeomTokens->faceVarying) {
+    if (!face_indices.is_empty()) {
+      /* Reverse the index order. */
+      const OffsetIndices faces = face_indices;
+      for (const int i : faces.index_range()) {
+        const IndexRange face = faces[i];
+        for (int j : face.index_range()) {
+          const int rev_index = face.last(j);
+          attribute[face.start() + j] = detail::convert_value<USDT, BlenderT>(
+              primvar_array[rev_index]);
+        }
+      }
+    }
+    else {
+      for (const int64_t i : attribute.index_range()) {
+        attribute[i] = detail::convert_value<USDT, BlenderT>(primvar_array[i]);
+      }
+    }
+  }
+
+  else {
+    /* Assume direct one-to-one mapping. */
+    if (primvar_array.size() == attribute.size()) {
+      if constexpr (std::is_same_v<USDT, BlenderT>) {
+        const Span<USDT> src(primvar_array.data(), primvar_array.size());
+        attribute.copy_from(src);
+      }
+      else {
+        for (const int64_t i : attribute.index_range()) {
+          attribute[i] = detail::convert_value<USDT, BlenderT>(primvar_array[i]);
+        }
+      }
+    }
+  }
+}
+
+inline void copy_primvar_to_blender_attribute(const pxr::UsdGeomPrimvar &primvar,
+                                              const pxr::UsdTimeCode timecode,
+                                              const eCustomDataType data_type,
+                                              const bke::AttrDomain domain,
+                                              const blender::OffsetIndices<int> face_indices,
+                                              bke::MutableAttributeAccessor attributes)
+{
+  const pxr::SdfValueTypeName sdf_type = primvar.GetTypeName();
+  const pxr::TfToken varying_type = primvar.GetInterpolation();
+  const pxr::TfToken name = pxr::UsdGeomPrimvar::StripPrimvarsName(primvar.GetPrimvarName());
+
+  const std::optional<eCustomDataType> type = convert_usd_type_to_blender(sdf_type);
+
+  if (!type.has_value()) {
+    return;
+  }
+
+  bke::GSpanAttributeWriter attribute = attributes.lookup_or_add_for_write_span(
+      name.GetText(), domain, data_type);
+
+  switch (data_type) {
+    case CD_PROP_FLOAT:
+      copy_primvar_to_blender_buffer<float>(
+          primvar, timecode, face_indices, attribute.span.typed<float>());
+      break;
+    case CD_PROP_INT32:
+      copy_primvar_to_blender_buffer<int32_t>(
+          primvar, timecode, face_indices, attribute.span.typed<int>());
+      break;
+    case CD_PROP_FLOAT2:
+      copy_primvar_to_blender_buffer<pxr::GfVec2f>(
+          primvar, timecode, face_indices, attribute.span.typed<float2>());
+      break;
+    case CD_PROP_FLOAT3:
+      copy_primvar_to_blender_buffer<pxr::GfVec3f>(
+          primvar, timecode, face_indices, attribute.span.typed<float3>());
+      break;
+    case CD_PROP_COLOR:
+      copy_primvar_to_blender_buffer<pxr::GfVec3f>(
+          primvar, timecode, face_indices, attribute.span.typed<ColorGeometry4f>());
+      break;
+    case CD_PROP_BOOL:
+      copy_primvar_to_blender_buffer<bool>(
+          primvar, timecode, face_indices, attribute.span.typed<bool>());
+      break;
+
+    default:
+      BLI_assert_unreachable();
+  }
+
+  attribute.finish();
 }
 
 }  // namespace blender::io::usd
