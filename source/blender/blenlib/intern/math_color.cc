6851f608c49bca98992e72b7cf575ff5faf69edb
Aras Pranckevicius <aras@nesnausk.org>
2024-05-02 22:41:28 +0300

    BLI: _bli_math_blend_sse can use fewer hoops on SSE4

--- source/blender/blenlib/intern/math_color.cc
+++ source/blender/blenlib/intern/math_color.cc
@@ -515,7 +515,11 @@ MALWAYS_INLINE __m128 _bli_math_fastpow512(const __m128 arg)
 
 MALWAYS_INLINE __m128 _bli_math_blend_sse(const __m128 mask, const __m128 a, const __m128 b)
 {
+#  if BLI_HAVE_SSE4
+  return _mm_blendv_ps(b, a, mask);
+#  else
   return _mm_or_ps(_mm_and_ps(mask, a), _mm_andnot_ps(mask, b));
+#  endif
 }
 
 MALWAYS_INLINE __m128 srgb_to_linearrgb_v4_simd(const __m128 c)
