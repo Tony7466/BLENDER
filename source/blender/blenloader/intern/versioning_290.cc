Tony7466/BLENDER

commit d9da86dd151d4c5b0ee4e3d45d6ea20a31a4b8e8

Refactor: move Cycles versioning into regular versioning files
It's confusing to have it separately, and could lead to potential bugs in the
future if there is some versioning depending on the order.

Pull Request: https://projects.blender.org/blender/blender/pulls/109166

source/blender/blenloader/intern/versioning_290.cc
@@ -926,6 +926,56 @@ void blo_do_versions_290(FileData *fd, Library * /*lib*/, Main *bmain)
     }
   }
 
+  if (!MAIN_VERSION_ATLEAST(bmain, 290, 5)) {
+    /* New denoiser settings. */
+    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
+      IDProperty *cscene = version_cycles_properties_from_ID(&scene->id);
+
+      /* Check if any view layers had (optix) denoising enabled. */
+      bool use_optix = false;
+      bool use_denoising = false;
+      LISTBASE_FOREACH (ViewLayer *, view_layer, &scene->view_layers) {
+        IDProperty *cview_layer = version_cycles_properties_from_view_layer(view_layer);
+        if (cview_layer) {
+          use_denoising = use_denoising ||
+                          version_cycles_property_boolean(cview_layer, "use_denoising", false);
+          use_optix = use_optix ||
+                      version_cycles_property_boolean(cview_layer, "use_optix_denoising", false);
+        }
+      }
+
+      if (cscene) {
+        enum {
+          DENOISER_AUTO = 0,
+          DENOISER_NLM = 1,
+          DENOISER_OPTIX = 2,
+        };
+
+        /* Enable denoiser if it was enabled for one view layer before. */
+        version_cycles_property_int_set(
+            cscene, "denoiser", (use_optix) ? DENOISER_OPTIX : DENOISER_NLM);
+        version_cycles_property_boolean_set(cscene, "use_denoising", use_denoising);
+
+        /* Migrate Optix denoiser to new settings. */
+        if (version_cycles_property_int(cscene, "preview_denoising", 0)) {
+          version_cycles_property_boolean_set(cscene, "use_preview_denoising", true);
+          version_cycles_property_int_set(cscene, "preview_denoiser", DENOISER_AUTO);
+        }
+      }
+
+      /* Enable denoising in all view layer if there was no denoising before,
+       * so that enabling the scene settings auto enables it for all view layers. */
+      if (!use_denoising) {
+        LISTBASE_FOREACH (ViewLayer *, view_layer, &scene->view_layers) {
+          IDProperty *cview_layer = version_cycles_properties_from_view_layer(view_layer);
+          if (cview_layer) {
+            version_cycles_property_boolean_set(cview_layer, "use_denoising", true);
+          }
+        }
+      }
+    }
+  }
+
   if (!MAIN_VERSION_ATLEAST(bmain, 290, 6)) {
     /* Transition to saving expansion for all of a modifier's sub-panels. */
     if (!DNA_struct_elem_find(fd->filesdna, "ModifierData", "short", "ui_expand_flag")) {

source/blender/blenloader/CMakeLists.txt
@@ -48,7 +48,6 @@ set(SRC
   intern/versioning_300.cc
   intern/versioning_400.cc
   intern/versioning_common.cc
-  intern/versioning_cycles.c
   intern/versioning_defaults.cc
   intern/versioning_dna.c
   intern/versioning_legacy.c

source/blender/blenloader/intern/readfile.cc
@@ -3138,9 +3138,6 @@ static void do_versions(FileData *fd, Library *lib, Main *main)
   if (!main->is_read_invalid) {
     blo_do_versions_400(fd, lib, main);
   }
-  if (!main->is_read_invalid) {
-    blo_do_versions_cycles(fd, lib, main);
-  }
 
   /* WATCH IT!!!: pointers from libdata have not been converted yet here! */
   /* WATCH IT 2!: Userdef struct init see do_versions_userdef() above! */
@@ -3183,9 +3180,6 @@ static void do_versions_after_linking(FileData *fd, Main *main)
   }
   if (!main->is_read_invalid) {
     do_versions_after_linking_400(fd, main);
-  }
-  if (!main->is_read_invalid) {
-    do_versions_after_linking_cycles(main);
   }
 
   main->is_locked_for_linking = false;

source/blender/blenloader/intern/versioning_300.cc
@@ -2708,6 +2708,44 @@ void blo_do_versions_300(FileData *fd, Library * /*lib*/, Main *bmain)
     }
   }
 
+  /* Move visibility from Cycles to Blender. */
+  if (!MAIN_VERSION_ATLEAST(bmain, 300, 17)) {
+    LISTBASE_FOREACH (Object *, object, &bmain->objects) {
+      IDProperty *cvisibility = version_cycles_visibility_properties_from_ID(&object->id);
+      int flag = 0;
+
+      if (cvisibility) {
+        flag |= version_cycles_property_boolean(cvisibility, "camera", true) ? 0 : OB_HIDE_CAMERA;
+        flag |= version_cycles_property_boolean(cvisibility, "diffuse", true) ? 0 :
+                                                                                OB_HIDE_DIFFUSE;
+        flag |= version_cycles_property_boolean(cvisibility, "glossy", true) ? 0 : OB_HIDE_GLOSSY;
+        flag |= version_cycles_property_boolean(cvisibility, "transmission", true) ?
+                    0 :
+                    OB_HIDE_TRANSMISSION;
+        flag |= version_cycles_property_boolean(cvisibility, "scatter", true) ?
+                    0 :
+                    OB_HIDE_VOLUME_SCATTER;
+        flag |= version_cycles_property_boolean(cvisibility, "shadow", true) ? 0 : OB_HIDE_SHADOW;
+      }
+
+      IDProperty *cobject = version_cycles_properties_from_ID(&object->id);
+      if (cobject) {
+        flag |= version_cycles_property_boolean(cobject, "is_holdout", false) ? OB_HOLDOUT : 0;
+        flag |= version_cycles_property_boolean(cobject, "is_shadow_catcher", false) ?
+                    OB_SHADOW_CATCHER :
+                    0;
+      }
+
+      if (object->type == OB_LAMP) {
+        flag |= OB_HIDE_CAMERA | OB_SHADOW_CATCHER;
+      }
+
+      /* Clear unused bits from old version, and add new flags. */
+      object->visibility_flag &= (OB_HIDE_VIEWPORT | OB_HIDE_SELECT | OB_HIDE_RENDER);
+      object->visibility_flag |= flag;
+    }
+  }
+
   if (!MAIN_VERSION_ATLEAST(bmain, 300, 18)) {
     if (!DNA_struct_elem_find(
             fd->filesdna, "WorkSpace", "AssetLibraryReference", "asset_library_ref")) {
@@ -2913,6 +2951,24 @@ void blo_do_versions_300(FileData *fd, Library * /*lib*/, Main *bmain)
     };
     LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
       scene->r.scemode &= ~(R_EXR_TILE_FILE | R_FULL_SAMPLE);
+    }
+  }
+
+  if (!MAIN_VERSION_ATLEAST(bmain, 300, 25)) {
+    enum {
+      DENOISER_NLM = 1,
+      DENOISER_OPENIMAGEDENOISE = 4,
+    };
+
+    /* Removal of NLM denoiser. */
+    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
+      IDProperty *cscene = version_cycles_properties_from_ID(&scene->id);
+
+      if (cscene) {
+        if (version_cycles_property_int(cscene, "denoiser", DENOISER_NLM) == DENOISER_NLM) {
+          version_cycles_property_int_set(cscene, "denoiser", DENOISER_OPENIMAGEDENOISE);
+        }
+      }
     }
   }
 

source/blender/blenloader/intern/versioning_common.cc
@@ -19,6 +19,7 @@
 #include "BLI_string_ref.hh"
 
 #include "BKE_animsys.h"
+#include "BKE_idprop.h"
 #include "BKE_lib_id.h"
 #include "BKE_main.h"
 #include "BKE_main_namemap.h"
@@ -111,6 +112,11 @@ static void change_node_socket_name(ListBase *sockets, const char *old_name, con
       STRNCPY(socket->identifier, new_name);
     }
   }
+}
+
+bool version_node_socket_is_used(bNodeSocket *sock)
+{
+  return sock->flag & SOCK_IS_LINKED;
 }
 
 void version_node_socket_id_delim(bNodeSocket *socket)
@@ -324,4 +330,76 @@ void add_realize_instances_before_socket(bNodeTree *ntree,
     link->fromnode = realize_node;
     link->fromsock = static_cast<bNodeSocket *>(realize_node->outputs.first);
   }
+}
+
+float *version_cycles_node_socket_float_value(bNodeSocket *socket)
+{
+  bNodeSocketValueFloat *socket_data = static_cast<bNodeSocketValueFloat *>(socket->default_value);
+  return &socket_data->value;
+}
+
+float *version_cycles_node_socket_rgba_value(bNodeSocket *socket)
+{
+  bNodeSocketValueRGBA *socket_data = static_cast<bNodeSocketValueRGBA *>(socket->default_value);
+  return socket_data->value;
+}
+
+float *version_cycles_node_socket_vector_value(bNodeSocket *socket)
+{
+  bNodeSocketValueVector *socket_data = static_cast<bNodeSocketValueVector *>(
+      socket->default_value);
+  return socket_data->value;
+}
+
+IDProperty *version_cycles_properties_from_ID(ID *id)
+{
+  IDProperty *idprop = IDP_GetProperties(id, false);
+  return (idprop) ? IDP_GetPropertyTypeFromGroup(idprop, "cycles", IDP_GROUP) : NULL;
+}
+
+IDProperty *version_cycles_properties_from_view_layer(ViewLayer *view_layer)
+{
+  IDProperty *idprop = view_layer->id_properties;
+  return (idprop) ? IDP_GetPropertyTypeFromGroup(idprop, "cycles", IDP_GROUP) : NULL;
+}
+
+float version_cycles_property_float(IDProperty *idprop, const char *name, float default_value)
+{
+  IDProperty *prop = IDP_GetPropertyTypeFromGroup(idprop, name, IDP_FLOAT);
+  return (prop) ? IDP_Float(prop) : default_value;
+}
+
+int version_cycles_property_int(IDProperty *idprop, const char *name, int default_value)
+{
+  IDProperty *prop = IDP_GetPropertyTypeFromGroup(idprop, name, IDP_INT);
+  return (prop) ? IDP_Int(prop) : default_value;
+}
+
+void version_cycles_property_int_set(IDProperty *idprop, const char *name, int value)
+{
+  IDProperty *prop = IDP_GetPropertyTypeFromGroup(idprop, name, IDP_INT);
+  if (prop) {
+    IDP_Int(prop) = value;
+  }
+  else {
+    IDPropertyTemplate val = {0};
+    val.i = value;
+    IDP_AddToGroup(idprop, IDP_New(IDP_INT, &val, name));
+  }
+}
+
+bool version_cycles_property_boolean(IDProperty *idprop, const char *name, bool default_value)
+{
+  return version_cycles_property_int(idprop, name, default_value);
+}
+
+void version_cycles_property_boolean_set(IDProperty *idprop, const char *name, bool value)
+{
+  version_cycles_property_int_set(idprop, name, value);
+}
+
+IDProperty *version_cycles_visibility_properties_from_ID(ID *id)
+{
+  IDProperty *idprop = IDP_GetProperties(id, false);
+  return (idprop) ? IDP_GetPropertyTypeFromGroup(idprop, "cycles_visibility", IDP_GROUP) : NULL;
 }

source/blender/blenloader/intern/versioning_common.h
@@ -13,9 +13,13 @@
 #endif
 
 struct ARegion;
+struct bNodeSocket;
+struct bNodeTree;
+struct ID;
+struct IDProperty;
 struct ListBase;
 struct Main;
-struct bNodeTree;
+struct ViewLayer;
 
 #ifdef __cplusplus
 extern "C" {
@@ -46,6 +50,8 @@ ARegion *do_versions_ensure_region(ListBase *regionbase,
  * \return the ID (if found).
  */
 ID *do_versions_rename_id(Main *bmain, short id_type, const char *name_src, const char *name_dst);
+
+bool version_node_socket_is_used(struct bNodeSocket *sock);
 
 void version_node_socket_name(struct bNodeTree *ntree,
                               int node_type,
@@ -115,6 +121,24 @@ void sequencer_init_preview_region(ARegion *region);
 void add_realize_instances_before_socket(bNodeTree *ntree,
                                          bNode *node,
                                          bNodeSocket *geometry_socket);
+
+float *version_cycles_node_socket_float_value(struct bNodeSocket *socket);
+float *version_cycles_node_socket_rgba_value(struct bNodeSocket *socket);
+float *version_cycles_node_socket_vector_value(struct bNodeSocket *socket);
+
+struct IDProperty *version_cycles_properties_from_ID(struct ID *id);
+struct IDProperty *version_cycles_properties_from_view_layer(struct ViewLayer *view_layer);
+struct IDProperty *version_cycles_visibility_properties_from_ID(ID *id);
+
+float version_cycles_property_float(struct IDProperty *idprop,
+                                    const char *name,
+                                    float default_value);
+int version_cycles_property_int(struct IDProperty *idprop, const char *name, int default_value);
+void version_cycles_property_int_set(struct IDProperty *idprop, const char *name, int value);
+bool version_cycles_property_boolean(struct IDProperty *idprop,
+                                     const char *name,
+                                     bool default_value);
+void version_cycles_property_boolean_set(struct IDProperty *idprop, const char *name, bool value);
 
 #ifdef __cplusplus
 }

commit f381124be2bb10ba7c5e1bf684658688035c45c2

Fix #121345: Viewlayer denoising versioning dont apply correctly
Caused by d9da86dd15

Above commit moved denoising versioning from the
`do_versions_after_linking` variant of versioning to `do_versions`, now
move back.

NOTE: I still have to gain full understanding as to where
`after_linking` is needed and where it is not, trying to read up on
this... do_versions_after_linking

source/blender/blenloader/intern/versioning_290.cc
@@ -575,6 +575,56 @@ void do_versions_after_linking_290(FileData * /*fd*/, Main *bmain)
     }
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 290, 5)) {
+    /* New denoiser settings. */
+    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
+      IDProperty *cscene = version_cycles_properties_from_ID(&scene->id);
+
+      /* Check if any view layers had (optix) denoising enabled. */
+      bool use_optix = false;
+      bool use_denoising = false;
+      LISTBASE_FOREACH (ViewLayer *, view_layer, &scene->view_layers) {
+        IDProperty *cview_layer = version_cycles_properties_from_view_layer(view_layer);
+        if (cview_layer) {
+          use_denoising = use_denoising ||
+                          version_cycles_property_boolean(cview_layer, "use_denoising", false);
+          use_optix = use_optix ||
+                      version_cycles_property_boolean(cview_layer, "use_optix_denoising", false);
+        }
+      }
+
+      if (cscene) {
+        enum {
+          DENOISER_AUTO = 0,
+          DENOISER_NLM = 1,
+          DENOISER_OPTIX = 2,
+        };
+
+        /* Enable denoiser if it was enabled for one view layer before. */
+        version_cycles_property_int_set(
+            cscene, "denoiser", (use_optix) ? DENOISER_OPTIX : DENOISER_NLM);
+        version_cycles_property_boolean_set(cscene, "use_denoising", use_denoising);
+
+        /* Migrate Optix denoiser to new settings. */
+        if (version_cycles_property_int(cscene, "preview_denoising", 0)) {
+          version_cycles_property_boolean_set(cscene, "use_preview_denoising", true);
+          version_cycles_property_int_set(cscene, "preview_denoiser", DENOISER_AUTO);
+        }
+      }
+
+      /* Enable denoising in all view layer if there was no denoising before,
+       * so that enabling the scene settings auto enables it for all view layers. */
+      if (!use_denoising) {
+        LISTBASE_FOREACH (ViewLayer *, view_layer, &scene->view_layers) {
+          IDProperty *cview_layer = version_cycles_properties_from_view_layer(view_layer);
+          if (cview_layer) {
+            version_cycles_property_boolean_set(cview_layer, "use_denoising", true);
+          }
+        }
+      }
+    }
+  }
+
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 291, 1)) {
     LISTBASE_FOREACH (Collection *, collection, &bmain->collections) {
       if (BKE_collection_cycles_fix(bmain, collection)) {
@@ -921,56 +971,6 @@ void blo_do_versions_290(FileData *fd, Library * /*lib*/, Main *bmain)
         }
       }
       FOREACH_NODETREE_END;
-    }
-  }
-
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 290, 5)) {
-    /* New denoiser settings. */
-    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
-      IDProperty *cscene = version_cycles_properties_from_ID(&scene->id);
-
-      /* Check if any view layers had (optix) denoising enabled. */
-      bool use_optix = false;
-      bool use_denoising = false;
-      LISTBASE_FOREACH (ViewLayer *, view_layer, &scene->view_layers) {
-        IDProperty *cview_layer = version_cycles_properties_from_view_layer(view_layer);
-        if (cview_layer) {
-          use_denoising = use_denoising ||
-                          version_cycles_property_boolean(cview_layer, "use_denoising", false);
-          use_optix = use_optix ||
-                      version_cycles_property_boolean(cview_layer, "use_optix_denoising", false);
-        }
-      }
-
-      if (cscene) {
-        enum {
-          DENOISER_AUTO = 0,
-          DENOISER_NLM = 1,
-          DENOISER_OPTIX = 2,
-        };
-
-        /* Enable denoiser if it was enabled for one view layer before. */
-        version_cycles_property_int_set(
-            cscene, "denoiser", (use_optix) ? DENOISER_OPTIX : DENOISER_NLM);
-        version_cycles_property_boolean_set(cscene, "use_denoising", use_denoising);
-
-        /* Migrate Optix denoiser to new settings. */
-        if (version_cycles_property_int(cscene, "preview_denoising", 0)) {
-          version_cycles_property_boolean_set(cscene, "use_preview_denoising", true);
-          version_cycles_property_int_set(cscene, "preview_denoiser", DENOISER_AUTO);
-        }
-      }
-
-      /* Enable denoising in all view layer if there was no denoising before,
-       * so that enabling the scene settings auto enables it for all view layers. */
-      if (!use_denoising) {
-        LISTBASE_FOREACH (ViewLayer *, view_layer, &scene->view_layers) {
-          IDProperty *cview_layer = version_cycles_properties_from_view_layer(view_layer);
-          if (cview_layer) {
-            version_cycles_property_boolean_set(cview_layer, "use_denoising", true);
-          }
-        }
-      }
     }
   }
 