5d770f7634eb221d4bbb2ca04eb846dc3d0a35e9
Jacques Lucke <jacques@blender.org>
2024-05-07 11:21:19 +0200

    add missing include

--- source/blender/editors/object/object_edit.cc
+++ source/blender/editors/object/object_edit.cc
@@ -60,6 +60,7 @@
 #include "BKE_mball.hh"
 #include "BKE_mesh.hh"
 #include "BKE_modifier.hh"
+#include "BKE_node_runtime.hh"
 #include "BKE_object.hh"
 #include "BKE_paint.hh"
 #include "BKE_particle.h"
