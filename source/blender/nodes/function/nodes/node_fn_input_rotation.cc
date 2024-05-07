95c458b66c86a4021c2dc2214cfac5d9eac43c81
Iliya Katueshenock <modormoder@gmail.com>
2024-05-07 16:04:18 +0300

    init

--- source/blender/nodes/function/nodes/node_fn_input_rotation.cc
+++ source/blender/nodes/function/nodes/node_fn_input_rotation.cc
@@ -4,6 +4,9 @@
 
 #include "BLI_math_euler.hh"
 #include "BLI_math_quaternion.hh"
+#include "BLI_set.hh"
+
+#include "BKE_type_conversions.hh"
 
 #include "NOD_socket_search_link.hh"
 
@@ -14,6 +17,8 @@
 
 namespace blender::nodes::node_fn_input_rotation_cc {
 
+NODE_STORAGE_FUNCS(NodeInputRotation)
+
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Rotation>("Rotation");
@@ -23,6 +28,47 @@ static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
 {
   uiLayout *col = uiLayoutColumn(layout, true);
   uiItemR(col, ptr, "rotation_euler", UI_ITEM_R_EXPAND, "", ICON_NONE);
+}
+
+static const bNodeSocket *find_single_targer(const bNodeSocket *output)
+{
+  return output;
+}
+
+static bool node_insert_link(bNodeTree *ntree, bNode *node, bNodeLink *link)
+{
+  BLI_assert(node == link->fromnode);
+  ntree->ensure_topology_cache();
+  NodeInputRotation &storage = node_storage(*node);
+  const CPPType &node_type = CPPType::get<math::Quaternion>();
+
+  const bNodeSocket *target = find_single_targer(link->tosock);
+  if (target == nullptr) {
+    return true;
+  }
+
+  const CPPType &target_value_type = *target->typeinfo->base_cpp_type;
+
+  const bke::DataTypeConversions &convert = bke::get_implicit_type_conversions();
+  if (&node_type != &target_value_type) {
+    if (!convert.is_convertible(node_type, target_value_type)) {
+      return true;
+    }
+  }
+
+  BUFFER_FOR_CPP_TYPE_VALUE(target_value_type, target_value);
+  target->typeinfo->get_base_cpp_value(target->default_value, target_value);
+  target_value_type.destruct(target_value);
+
+  math::Quaternion node_value;
+  convert.convert_to_uninitialized(target_value_type, node_type, target_value, &node_value);
+  const math::EulerXYZ storage_value = math::to_euler(node_value);
+
+  storage.rotation_euler[0] = storage_value.x().radian();
+  storage.rotation_euler[1] = storage_value.y().radian();
+  storage.rotation_euler[2] = storage_value.z().radian();
+
+  return true;
 }
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
@@ -53,6 +99,7 @@ static void node_register()
       &ntype, "NodeInputRotation", node_free_standard_storage, node_copy_standard_storage);
   ntype.build_multi_function = node_build_multi_function;
   ntype.draw_buttons = node_layout;
+  ntype.insert_link = node_insert_link;
   nodeRegisterType(&ntype);
 }
 NOD_REGISTER_NODE(node_register)
