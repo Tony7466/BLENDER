3a8e5f2757ac1444a57361d6069d549584e1a1e7
Jacques Lucke <jacques@blender.org>
2024-05-07 11:43:38 +0200

    rename socket to Instances

--- source/blender/nodes/geometry/nodes/node_geo_set_instance_transform.cc
+++ source/blender/nodes/geometry/nodes/node_geo_set_instance_transform.cc
@@ -8,15 +8,15 @@ namespace blender::nodes::node_geo_set_instance_transform_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_input<decl::Geometry>("Geometry").only_instances();
+  b.add_input<decl::Geometry>("Instances").only_instances();
   b.add_input<decl::Bool>("Selection").default_value(true).hide_value().field_on_all();
   b.add_input<decl::Matrix>("Transform").field_on_all();
-  b.add_output<decl::Geometry>("Geometry").propagate_all();
+  b.add_output<decl::Geometry>("Instances").propagate_all();
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
-  GeometrySet geometry_set = params.extract_input<GeometrySet>("Geometry");
+  GeometrySet geometry_set = params.extract_input<GeometrySet>("Instances");
   Field<bool> selection_field = params.extract_input<Field<bool>>("Selection");
   Field<float4x4> transform_field = params.extract_input<Field<float4x4>>("Transform");
 
@@ -26,7 +26,7 @@ static void node_geo_exec(GeoNodeExecParams params)
         instances, "instance_transform", AttrDomain::Instance, selection_field, transform_field);
   }
 
-  params.set_output("Geometry", std::move(geometry_set));
+  params.set_output("Instances", std::move(geometry_set));
 }
 
 static void node_register()
@@ -37,7 +37,7 @@ static void node_register()
       &ntype, GEO_NODE_SET_INSTANCE_TRANSFORM, "Set Instance Transform", NODE_CLASS_GEOMETRY);
   ntype.geometry_node_execute = node_geo_exec;
   ntype.declare = node_declare;
-
+  bke::node_type_size(&ntype, 140, 100, 700);
   nodeRegisterType(&ntype);
 }
 NOD_REGISTER_NODE(node_register)
