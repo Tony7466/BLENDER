0b4f20691e439dbb261d9412cbdcf48fcc6932b5
Iliya Katueshenock <modormoder@gmail.com>
2024-05-08 21:56:45 +0300

    init

--- source/blender/makesdna/DNA_node_types.h
+++ source/blender/makesdna/DNA_node_types.h
@@ -1338,7 +1338,7 @@ typedef struct NodeTexMagic {
 } NodeTexMagic;
 
 typedef struct NodeShaderAttribute {
-  char name[64];
+  char name[256];
   int type;
   char _pad[4];
 } NodeShaderAttribute;
