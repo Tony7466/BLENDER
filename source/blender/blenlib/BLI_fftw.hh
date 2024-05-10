cbcb87a78df286ea505855e34e523b852a693d44
Omar Emara <mail@OmarEmara.dev>
2024-05-10 13:48:59 +0300

    Compositor: Optimize Fog Glow Glare node

---
+++ source/blender/blenlib/BLI_fftw.hh
@@ -0,0 +1,29 @@
+/* SPDX-FileCopyrightText: 2024 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#if defined(WITH_FFTW3)
+#  include <fftw3.h>
+#endif
+
+#include "BLI_index_range.hh"
+#include "BLI_math_vector_types.hh"
+#include "BLI_task.hh"
+#include "BLI_threads.h"
+
+#pragma once
+
+namespace blender::fftw {
+
+/* FFTW's real to complex and complex to real transforms are more efficient when their input has a
+ * specific size. This function finds the most optimal size that is more than or equal the given
+ * size. The input data can then be zero padded to the optimal size for better performance. See
+ * Section 4.3.3 Real-data DFTs in the FFTW manual for more information. */
+int optimal_size_for_real_transform(int size);
+int2 optimal_size_for_real_transform(int2 size);
+
+/* Initialize the float variant of FFTW. This essentially setup the multithreading hooks to enable
+ * multithreading using TBB's parallel_for and makes the FFTW planner thread safe. */
+void initialize_float();
+
+}  // namespace blender::fftw
