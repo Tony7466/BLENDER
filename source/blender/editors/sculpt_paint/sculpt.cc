5bbc73a98dd32464ccd5fb51b54ebc67b6cf7db2
Hans Goudey <hans@blender.org>
2024-05-15 11:31:52 -0400

    Cleanup comments in shape key

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -6538,13 +6538,12 @@ void flush_positions_to_shape_keys(Object &object,
   /* Modifying of basis key should update mesh. */
   if (active_key == mesh.key->refkey) {
     /* XXX: There are too many positions arrays getting passed around. We should have a better
-     * naming system or not have to constantly update both the basis and original positions.
-     * OTOH, maybe that's just a consequence of the bad design of shape keys in general. */
+     * naming system or not have to constantly update both the base shape key and original
+     * positions. OTOH, maybe it's just a consequence of the bad design of shape keys. */
     apply_translations(positions, verts, positions_mesh);
   }
 
-  /* Apply new coords on active key block, no need to re-allocate kb->data here! */
-  // #array_utils::copy
+  /* Apply new positions to active shape key. */
   for (const int vert : verts) {
     active_key_data[vert] = positions[vert];
   }
