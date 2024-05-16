76a45376c1d5fa185dfeb6dffe24fc50d095f88c
Hans Goudey <hans@blender.org>
2024-05-16 09:08:37 -0400

    Add docstrings for brush functions

--- source/blender/editors/sculpt_paint/brushes/types.hh
+++ source/blender/editors/sculpt_paint/brushes/types.hh
@@ -12,7 +12,9 @@ struct PBVHNode;
 
 namespace blender::ed::sculpt_paint {
 
+/** A simple normal-direction displacement. */
 void do_draw_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
+/** A simple normal-direction displacement based on image texture RGB/XYZ values. */
 void do_draw_vector_displacement_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
 
 }  // namespace blender::ed::sculpt_paint
