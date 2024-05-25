adbc509187f69fe60873b56c79fcff578df63837
Lukas Tönne <lukas@blender.org>
2024-05-25 14:37:13 +0200

    Basic setup of the dynamics world.

--- source/blender/simulation/SIM_rigid_body.hh
+++ source/blender/simulation/SIM_rigid_body.hh
@@ -8,9 +8,24 @@
 
 #pragma once
 
+#include "BLI_math_vector_types.hh"
+#include "BLI_math_quaternion_types.hh"
+
+#include <functional>
+
 namespace blender::simulation {
 
+struct RigidBodyWorldImpl;
+
+using RigidBodyID = int64_t;
+
 class RigidBodyWorld {
+ public:
+  using OverlapFilterFn = std::function<bool(const RigidBodyID a, const RigidBodyID b)>;
+
+ private:
+  RigidBodyWorldImpl *impl_;
+
  public:
   RigidBodyWorld();
   ~RigidBodyWorld();
@@ -18,6 +33,14 @@ class RigidBodyWorld {
   int bodies_num() const;
   int constraints_num() const;
   int shapes_num() const;
+
+  void set_overlap_filter(OverlapFilterFn fn);
+  void clear_overlap_filter();
+
+  float3 gravity() const;
+  void set_gravity(const float3 &gravity);
+  void set_solver_iterations(int num_solver_iterations);
+  void set_split_impulse(bool split_impulse);
 };
 
 }  // namespace blender::simulation
--- source/blender/simulation/intern/rigid_body.cc
+++ source/blender/simulation/intern/rigid_body.cc
@@ -8,7 +8,152 @@
 
 #include "SIM_rigid_body.hh"
 
+#ifdef WITH_BULLET
+#include "BulletCollision/Gimpact/btGImpactCollisionAlgorithm.h"
+#include "BulletCollision/Gimpact/btGImpactShape.h"
+#include "BulletDynamics/Dynamics/btDiscreteDynamicsWorld.h"
+#include "btBulletDynamicsCommon.h"
+#endif
+
 namespace blender::simulation {
+
+#ifdef WITH_BULLET
+
+inline float3 to_blender(const btVector3 &v) {
+  return float3(v);
+}
+
+inline math::Quaternion to_blender(const btQuaternion &q)
+{
+  return math::Quaternion(q.w(), q.x(), q.y(), q.z());
+}
+
+inline btVector3 to_bullet(const float3 &v)
+{
+  return btVector3(v.x, v.y, v.z);
+}
+
+inline btQuaternion to_bullet(const math::Quaternion &q)
+{
+  return btQuaternion(q.x, q.y, q.z, q.w);
+}
+
+struct RigidBodyWorldImpl {
+  btCollisionConfiguration *config;
+  btCollisionDispatcher *dispatcher; 
+  btDynamicsWorld *world;
+  btBroadphaseInterface *broadphase;
+  btConstraintSolver *constraint_solver;
+  btOverlapFilterCallback *overlap_filter;
+};
+
+struct DefaultOverlapFilter : public btOverlapFilterCallback {
+  virtual bool needBroadphaseCollision(btBroadphaseProxy *proxy0, btBroadphaseProxy *proxy1) const
+  {
+    const int64_t body0 = int64_t(proxy0->m_clientObject);
+    const int64_t body1 = int64_t(proxy1->m_clientObject);
+    return (proxy0->m_collisionFilterGroup & proxy1->m_collisionFilterMask) &&
+           (proxy1->m_collisionFilterGroup & proxy0->m_collisionFilterMask);
+  }
+};
+
+struct OverlapFilterWrapper : public btOverlapFilterCallback {
+  using OverlapFilterFn = RigidBodyWorld::OverlapFilterFn;
+
+  OverlapFilterFn fn;
+
+  OverlapFilterWrapper(OverlapFilterFn fn) : fn(fn) {}
+
+  virtual bool needBroadphaseCollision(btBroadphaseProxy *proxy0, btBroadphaseProxy *proxy1) const
+  {
+    const int64_t body0 =  int64_t(proxy0->m_clientObject);
+    const int64_t body1 = int64_t(proxy1->m_clientObject);
+    return (proxy0->m_collisionFilterGroup & proxy1->m_collisionFilterMask) &&
+           (proxy1->m_collisionFilterGroup & proxy0->m_collisionFilterMask) && fn(body0, body1);
+  }
+};
+
+RigidBodyWorld::RigidBodyWorld() {
+  impl_ = new RigidBodyWorldImpl;
+  impl_->config = new btDefaultCollisionConfiguration();
+  impl_->dispatcher = new btCollisionDispatcher(impl_->config);
+  btGImpactCollisionAlgorithm::registerAlgorithm((btCollisionDispatcher *)impl_->dispatcher);
+
+  impl_->broadphase = new btDbvtBroadphase();
+  impl_->overlap_filter = nullptr;
+
+  impl_->constraint_solver = new btSequentialImpulseConstraintSolver();
+
+  impl_->world = new btDiscreteDynamicsWorld(
+      impl_->dispatcher, impl_->broadphase, impl_->constraint_solver, impl_->config);
+
+  //RB_dworld_set_gravity(world, gravity);
+}
+
+RigidBodyWorld::~RigidBodyWorld()
+{
+  delete impl_->world;
+  delete impl_->constraint_solver;
+  delete impl_->broadphase;
+  delete impl_->dispatcher;
+  delete impl_->config;
+  delete impl_->overlap_filter;
+  delete impl_;
+}
+
+int RigidBodyWorld::bodies_num() const
+{
+  return 0;
+}
+
+int RigidBodyWorld::constraints_num() const
+{
+  return 0;
+}
+
+int RigidBodyWorld::shapes_num() const
+{
+  return 0;
+}
+
+void RigidBodyWorld::set_overlap_filter(OverlapFilterFn fn)
+{
+  impl_->overlap_filter = new OverlapFilterWrapper(std::move(fn));
+  impl_->broadphase->getOverlappingPairCache()->setOverlapFilterCallback(impl_->overlap_filter);
+}
+
+void RigidBodyWorld::clear_overlap_filter() {
+  impl_->broadphase->getOverlappingPairCache()->setOverlapFilterCallback(nullptr);
+  if (impl_->overlap_filter) {
+    delete impl_->overlap_filter;
+    impl_->overlap_filter = nullptr;
+  }
+}
+
+float3 RigidBodyWorld::gravity() const
+{
+  return to_blender(impl_->world->getGravity());
+}
+
+void RigidBodyWorld::set_gravity(const float3 &gravity)
+{
+  impl_->world->setGravity(to_bullet(gravity));
+}
+
+void RigidBodyWorld::set_solver_iterations(const int num_solver_iterations)
+{
+  btContactSolverInfo &info = impl_->world->getSolverInfo();
+  info.m_numIterations = num_solver_iterations;
+}
+
+void RigidBodyWorld::set_split_impulse(const bool split_impulse)
+{
+  btContactSolverInfo &info = impl_->world->getSolverInfo();
+  /* Note: Bullet stores this as int, but it's used as a bool. */
+  info.m_splitImpulse = int(split_impulse);
+}
+
+#else
 
 RigidBodyWorld::RigidBodyWorld() {}
 
@@ -28,5 +173,11 @@ int RigidBodyWorld::shapes_num() const
 {
   return 0;
 }
+
+void RigidBodyWorld::set_overlap_filter(OverlapFilterFn /*fn*/) {}
+
+void RigidBodyWorld::clear_overlap_filter() {}
+
+#endif
 
 }  // namespace blender::simulation
