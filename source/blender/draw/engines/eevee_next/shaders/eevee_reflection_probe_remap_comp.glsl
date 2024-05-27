dc79dbc7d1b1c816bfff6a660ec698deb52ed68e
Jeroen Bakker <jeroen@blender.org>
2024-05-27 14:40:51 +0200

    Grammar in comments

--- source/blender/draw/engines/eevee_next/shaders/eevee_reflection_probe_remap_comp.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_reflection_probe_remap_comp.glsl
@@ -9,7 +9,7 @@
 #pragma BLENDER_REQUIRE(eevee_spherical_harmonics_lib.glsl)
 
 /* OpenGL/Intel drivers have known issues where it isn't able to compile barriers inside for loops.
- * Macros are needed for complex shaders as the driver doesn't unroll in these cases. */
+ * Macros are needed as driver can decide to not unroll in shaders with more complexity. */
 #define PARALLEL_SUM_INNER() \
   barrier(); \
   if (local_index < stride) { \
