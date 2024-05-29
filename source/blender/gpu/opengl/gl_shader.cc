e5f80b1c13b15698c91696b972dc844bdddd43cd
Miguel Pozo <pragma37@gmail.com>
2024-05-29 16:14:25 +0200

    Fix cmd path

--- source/blender/gpu/opengl/gl_shader.cc
+++ source/blender/gpu/opengl/gl_shader.cc
@@ -1609,7 +1609,8 @@ GLCompilerWorker::GLCompilerWorker()
   close_semaphore_ = std::make_unique<SharedSemaphore>(name + "_CLOSE");
 
   /* TODO: Pass max_size. */
-  std::string cmd = std::string(BKE_appdir_program_path()) + " --compilation-subprocess " + name;
+  std::string cmd = '"' + std::string(BKE_appdir_program_path()) + '"' +
+                    " --compilation-subprocess " + name;
 
   compiler_ = popen(cmd.c_str(), "w");
 }
