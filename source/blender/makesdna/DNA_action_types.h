50e9df037b96a0f125f02c02ea4921b5ba93ca66
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-05-30 17:39:11 +0200

    clean up merge issue

--- source/blender/makesdna/DNA_action_types.h
+++ source/blender/makesdna/DNA_action_types.h
@@ -268,12 +268,6 @@ typedef struct bPoseChannel_Runtime {
 
 /* PoseChannel ------------------------------------ */
 
-/* Used in the armature drawing code. Due to the encoding clamping the passed in floats, the wire
- * width needs to be scaled down. */
-#ifdef __cplusplus
-static constexpr float WIRE_WIDTH_COMPRESSION = 16.0f;
-#endif
-
 /**
  * PoseChannel
  *
