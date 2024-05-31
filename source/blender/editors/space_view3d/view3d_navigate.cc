Tony7466/BLENDER

commit 172221e7fb892f7cbf7a8c58a44351c5a48321e1

Fix #114714: Rotating the View can drastically change the view's position when Auto Depth or Orbit Arround Select is set
Caused by 6faa39edb7

In that commit it was assumed that the view offset does not need to be
updated if the operator is `V3D_OP_MODE_ROTATE` instead of simply
checking `this->use_dyn_ofs`.

Since `use_dyn_ofs` is always `True` when using Auto Depth or Orbit
Arround Select, the offset should always be updated in these cases.

source/blender/editors/space_view3d/view3d_navigate.cc
@@ -279,8 +279,9 @@ void ViewOpsData::init_navigation(bContext *C,
     negate_v3_v3(this->dyn_ofs, pivot_new);
     this->use_dyn_ofs = true;
 
-    if (!(nav_type->flag & VIEWOPS_FLAG_ORBIT_SELECT)) {
-      /* Calculate new #RegionView3D::ofs and #RegionView3D::dist. */
+    {
+      /* The pivot has changed so the offset needs to be updated as well.
+       * Calculate new #RegionView3D::ofs and #RegionView3D::dist. */
 
       if (rv3d->is_persp) {
         float my_origin[3]; /* Original #RegionView3D.ofs. */

commit 20c852a73a61f9a3eb54aae54a18420c64bab23d

Fix #122438: Orbit Around Selection drastically moves view
Would happen if an object that is out of view is selected and then
navigating.

Caused by 172221e7fb89.

This part of the code has gone though some refactors:
- 15a4609ecefc
- 6faa39edb7
- 384c2e1f36eb

What we can notice here is that part of the code that used to be
reserved for `VIEWOPS_FLAG_DEPTH_NAVIGATE` was now called for different
conditions:
- in 6faa39edb7, it was now depending on `V3D_OP_MODE_ROTATE` (without
further explanation), caused #114714
- in 172221e7fb89 (the fix for #114714), it was now done unconditionally
(the explanation is not clear to me either)

To resolve, make the specific part of the code depend on
`VIEWOPS_FLAG_DEPTH_NAVIGATE` again

source/blender/editors/space_view3d/view3d_navigate.cc
@@ -277,7 +277,7 @@ void ViewOpsData::init_navigation(bContext *C,
     negate_v3_v3(this->dyn_ofs, pivot_new);
     this->use_dyn_ofs = true;
 
-    {
+    if (viewops_flag & VIEWOPS_FLAG_DEPTH_NAVIGATE) {
       /* The pivot has changed so the offset needs to be updated as well.
        * Calculate new #RegionView3D::ofs and #RegionView3D::dist. */
 