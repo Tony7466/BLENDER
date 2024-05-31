73d3b63ffd95f74f59fc127b7796ce0d51306251
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-05-31 09:47:21 +0200

    add string comparison test

--- source/blender/animrig/intern/action_test.cc
+++ source/blender/animrig/intern/action_test.cc
@@ -484,6 +484,7 @@ TEST_F(ActionLayersTest, conversion_to_layered)
   add_fmodifier(&legacy_fcu_1->modifiers, FMODIFIER_TYPE_NOISE, legacy_fcu_1);
 
   Action *converted = convert_to_layered_action(*bmain, *anim);
+  EXPECT_STREQ(converted->id.name, "ACACÄnimåtië_layered");
   Strip *strip = converted->layers()[0]->strips()[0];
   KeyframeStrip key_strip = strip->as<KeyframeStrip>();
   ChannelBag *bag = key_strip.channelbags()[0];
