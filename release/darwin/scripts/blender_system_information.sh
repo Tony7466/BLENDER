fdccff0ba98c958c09e2e70106392f10a403514d
Alaska <alaskayou01@gmail.com>
2024-05-31 15:24:55 +1200

    Try and remove reliance on bash

--- release/darwin/scripts/blender_system_information.sh
+++ release/darwin/scripts/blender_system_information.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+script_dir=$(dirname "$0") # Directory to this sh file
 
 for folder in "$script_dir"/*; do
     if [[ -d "$folder" ]]; then
-        if [[ -d "$folder/python/bin" ]]; then
+        if [[ -d "$folder/python/bin" ]]; then # If Python bin folder exists
             for file in "$folder/python/bin"/*; do
                 filename=$(basename "$file")
-                if [[ $filename =~ ^python+ ]] ; then
-                    "$file" "$folder/scripts/system_info/system_info.py"
+                if [[ $filename =~ ^python+ ]] ; then # Search for Python executable
+                    "$file" "$folder/scripts/system_info/system_info.py" # Run Python script
                 fi
             done
         fi
--- release/freedesktop/scripts/blender_system_information.sh
+++ release/freedesktop/scripts/blender_system_information.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+script=$(readlink -f "$0") # Path to this sh file
+script_dir=$(dirname "$script")
 
 for folder in "$script_dir"/*; do
     if [[ -d "$folder" ]]; then
-        if [[ -d "$folder/python/bin" ]]; then
+        if [[ -d "$folder/python/bin" ]]; then # If Python bin folder exists
             for file in "$folder/python/bin"/*; do
                 filename=$(basename "$file")
-                if [[ $filename =~ ^python+ ]] ; then
-                    "$file" "$folder/scripts/system_info/system_info.py"
+                if [[ $filename =~ ^python+ ]] ; then # Search for Python executable
+                    "$file" "$folder/scripts/system_info/system_info.py" # Run Python script
                 fi
             done
         fi
