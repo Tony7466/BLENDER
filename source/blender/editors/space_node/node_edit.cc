2922cecf4d921d6dc4f71b1ce51e267c9e6f68f6
Omar Emara <mail@OmarEmara.dev>
2024-06-03 16:18:04 +0300

    Fix: Crash when toggling compositor device
    Blender crashes when toggling the compositor device from CPU to GPU.
    This is because when submitting the compositor job, the device might be
    set to CPU, so GPU related data are not initialized in the job, but
    between the time the job is submitted and the time it gets executed, the
    device might change to GPU, which then tries to accept the uninitialized
    GPU data from the job.
    
    To fix this, we always initialize GPU data for now. We previously made
    that conditional to avoid superfluous GPU context creation, but this
    should no longer make much of a difference due to a162149cfa.

--- source/blender/editors/space_node/node_edit.cc
+++ source/blender/editors/space_node/node_edit.cc
@@ -252,10 +252,8 @@ static void compo_initjob(void *cjv)
     compo_tag_output_nodes(cj->localtree, cj->recalc_flags);
   }
 
-  if (scene->r.compositor_device == SCE_COMPOSITOR_DEVICE_GPU) {
-    cj->re = RE_NewInteractiveCompositorRender(scene);
-    RE_system_gpu_context_ensure(cj->re);
-  }
+  cj->re = RE_NewInteractiveCompositorRender(scene);
+  RE_system_gpu_context_ensure(cj->re);
 }
 
 /* Called before redraw notifiers, it moves finished previews over. */
