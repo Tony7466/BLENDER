ad054818b970a98329e7bc0981ba9932f9e946d7
Jacques Lucke <jacques@blender.org>
2024-06-04 16:56:22 +0200

    cleanup

--- source/blender/nodes/geometry/nodes/node_geo_attribute_capture.cc
+++ source/blender/nodes/geometry/nodes/node_geo_attribute_capture.cc
@@ -315,7 +315,6 @@ static void node_gather_link_searches(GatherLinkSearchOpParams &params)
 
   params.add_item(IFACE_("Value"), [type](LinkSearchOpParams &params) {
     bNode &node = params.add_node("GeometryNodeCaptureAttribute");
-    NodeGeometryAttributeCapture &storage = node_storage(node);
     socket_items::add_item_with_socket_type_and_name<CaptureAttributeItemsAccessor>(
         node, type, params.socket.name);
     params.update_and_connect_available_socket(node, params.socket.name);
