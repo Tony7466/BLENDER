18bac212fec2792dff61bede77e6c4e8230fcad0
Iliya Katueshenock <modormoder@gmail.com>
2024-06-05 21:47:08 +0300

    fix other compilers error

--- source/blender/blenlib/BLI_math_quaternion.hh
+++ source/blender/blenlib/BLI_math_quaternion.hh
@@ -630,8 +630,8 @@ AxisAngleBase<T, AngleT> to_axis_angle(const QuaternionBase<T> &quat)
   T sin_half_angle = math::length(axis);
   /* Prevent division by zero for axis conversion. */
   if (sin_half_angle < T(0.0005)) {
-    const AxisAngleBase<T, AngleT>::vec3_type identity_axis =
-        AxisAngleBase<T, AngleT>::identity().axis();
+    using AngleAxisT = typename AxisAngleBase<T, AngleT>::vec3_type;
+    const AngleAxisT identity_axis = AxisAngleBase<T, AngleT>::identity().axis();
     BLI_assert(abs(cos_half_angle) > 0.0005);
     AxisAngleBase<T, AngleT> identity(identity_axis * sign(cos_half_angle), AngleT(0));
     return identity;
