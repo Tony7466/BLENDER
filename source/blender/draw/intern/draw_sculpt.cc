bea22260449db4386e69cf47f8cb2885d2d72841
Sean Kim <SeanCTKim@protonmail.com>
2024-06-06 11:44:59 -0700

    Fix #121008: Paint undo causes uncolored mesh

--- source/blender/draw/intern/draw_sculpt.cc
+++ source/blender/draw/intern/draw_sculpt.cc
@@ -96,7 +96,7 @@ static Vector<SculptBatch> sculpt_batches_get_ex(const Object *ob,
     update_only_visible = true;
   }
 
-  const Mesh *mesh = static_cast<const Mesh *>(ob->data);
+  const Mesh *mesh = BKE_object_get_original_mesh(ob);
   bke::pbvh::update_normals(*pbvh, mesh->runtime->subdiv_ccg.get());
 
   Vector<SculptBatch> result_batches;
