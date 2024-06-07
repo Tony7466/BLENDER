b513f2900674e8d7faa2f4f237ea7c4c62d89fec
Nathan Vegdahl <cessen@cessen.com>
2024-06-07 11:59:32 +0200

    Remove accidental meaningless code changes

--- source/blender/animrig/intern/action_test.cc
+++ source/blender/animrig/intern/action_test.cc
@@ -128,10 +128,9 @@ TEST_F(ActionLayersTest, add_strip)
   /* Add some keys to check that also the strip data is freed correctly. */
   const KeyframeSettings settings = get_keyframe_settings(false);
   Binding &binding = anim->binding_add();
-  strip.as<KeyframeStrip>().keyframe_insert(
-      binding, "location", 0, {1.0f, 47.0f}, settings, INSERTKEY_NOFLAGS);
+  strip.as<KeyframeStrip>().keyframe_insert(binding, "location", 0, {1.0f, 47.0f}, settings);
   another_strip.as<KeyframeStrip>().keyframe_insert(
-      binding, "location", 0, {1.0f, 47.0f}, settings, INSERTKEY_NOFLAGS);
+      binding, "location", 0, {1.0f, 47.0f}, settings);
 }
 
 TEST_F(ActionLayersTest, remove_strip)
@@ -144,12 +143,9 @@ TEST_F(ActionLayersTest, remove_strip)
   /* Add some keys to check that also the strip data is freed correctly. */
   const KeyframeSettings settings = get_keyframe_settings(false);
   Binding &binding = anim->binding_add();
-  strip0.as<KeyframeStrip>().keyframe_insert(
-      binding, "location", 0, {1.0f, 47.0f}, settings, INSERTKEY_NOFLAGS);
-  strip1.as<KeyframeStrip>().keyframe_insert(
-      binding, "location", 0, {1.0f, 47.0f}, settings, INSERTKEY_NOFLAGS);
-  strip2.as<KeyframeStrip>().keyframe_insert(
-      binding, "location", 0, {1.0f, 47.0f}, settings, INSERTKEY_NOFLAGS);
+  strip0.as<KeyframeStrip>().keyframe_insert(binding, "location", 0, {1.0f, 47.0f}, settings);
+  strip1.as<KeyframeStrip>().keyframe_insert(binding, "location", 0, {1.0f, 47.0f}, settings);
+  strip2.as<KeyframeStrip>().keyframe_insert(binding, "location", 0, {1.0f, 47.0f}, settings);
 
   EXPECT_TRUE(layer.strip_remove(strip1));
   EXPECT_EQ(2, layer.strips().size());
@@ -452,7 +448,7 @@ TEST_F(ActionLayersTest, KeyframeStrip__keyframe_insert)
 
   const KeyframeSettings settings = get_keyframe_settings(false);
   SingleKeyingResult result_loc_a = key_strip.keyframe_insert(
-      binding, "location", 0, {1.0f, 47.0f}, settings, INSERTKEY_NOFLAGS);
+      binding, "location", 0, {1.0f, 47.0f}, settings);
   ASSERT_EQ(SingleKeyingResult::SUCCESS, result_loc_a)
       << "Expected keyframe insertion to be successful";
 
@@ -463,7 +459,7 @@ TEST_F(ActionLayersTest, KeyframeStrip__keyframe_insert)
 
   /* Insert a second key, should insert into the same FCurve as before. */
   SingleKeyingResult result_loc_b = key_strip.keyframe_insert(
-      binding, "location", 0, {5.0f, 47.1f}, settings, INSERTKEY_NOFLAGS);
+      binding, "location", 0, {5.0f, 47.1f}, settings);
   EXPECT_EQ(SingleKeyingResult::SUCCESS, result_loc_b);
   ASSERT_EQ(1, channels->fcurves().size()) << "Expect insertion with the same (binding/rna "
                                               "path/array index) tuple to go into the same FCurve";
@@ -476,7 +472,7 @@ TEST_F(ActionLayersTest, KeyframeStrip__keyframe_insert)
 
   /* Insert another key for another property, should create another FCurve. */
   SingleKeyingResult result_rot = key_strip.keyframe_insert(
-      binding, "rotation_quaternion", 0, {1.0f, 0.25f}, settings, INSERTKEY_NOFLAGS);
+      binding, "rotation_quaternion", 0, {1.0f, 0.25f}, settings);
   EXPECT_EQ(SingleKeyingResult::SUCCESS, result_rot);
   ASSERT_EQ(2, channels->fcurves().size()) << "Expected a second FCurve to be created.";
   EXPECT_EQ(2, channels->fcurves()[0]->totvert);
