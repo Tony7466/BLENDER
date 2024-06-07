76ce29574926be41992ae66528081e8c725960be
Miguel Pozo <pragma37@gmail.com>
2024-06-07 16:18:44 +0200

    Revert "EEVEE: Parallel static shader compilation"
    This reverts commit 9b4b248f363d8f320a8e2410e48f1be70e69f3b5.

--- source/blender/draw/engines/eevee_next/eevee_shader.hh
+++ source/blender/draw/engines/eevee_next/eevee_shader.hh
@@ -158,7 +158,6 @@ enum eShaderType {
 class ShaderModule {
  private:
   std::array<GPUShader *, MAX_SHADER_TYPE> shaders_;
-  BatchHandle compilation_handle_ = 0;
 
   /** Shared shader module across all engine instances. */
   static ShaderModule *g_shader_module;
@@ -166,8 +165,6 @@ class ShaderModule {
  public:
   ShaderModule();
   ~ShaderModule();
-
-  bool is_ready(bool block = false);
 
   GPUShader *static_shader_get(eShaderType shader_type);
   GPUMaterial *material_default_shader_get(eMaterialPipeline pipeline_type,
