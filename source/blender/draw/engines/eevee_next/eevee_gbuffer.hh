Tony7466/BLENDER

commit e97e06ea2c298b4cf2fd906cf77d36d2da698cda

Metal: Re-enable loadstore config for EEVEE Next framebuffer
A previous PR resolved the loadstore config setup on bind, so
we can now re-enable load-store config with clearing of the
header texture via render pass.

Authored by Apple: Michael Parkin-White

Pull Request: https://projects.blender.org/blender/blender/pulls/121610

source/blender/draw/engines/eevee_next/eevee_gbuffer.hh
@@ -156,35 +156,27 @@ struct GBuffer {
   void bind(Framebuffer &gbuffer_fb)
   {
     if (/* FIXME(fclem): Vulkan doesn't implement load / store config yet. */
-        GPU_backend_get_type() == GPU_BACKEND_VULKAN ||
-        /* FIXME(fclem): Metal has bug in backend. */
-        GPU_backend_get_type() == GPU_BACKEND_METAL)
+        GPU_backend_get_type() == GPU_BACKEND_VULKAN)
     {
       header_tx.clear(uint4(0));
     }
 
-    if (GPU_backend_get_type() == GPU_BACKEND_METAL) {
-      /* TODO(fclem): Load/store action is broken on Metal. */
+    if (!GPU_stencil_export_support()) {
+      /* Clearing custom load-store frame-buffers is invalid,
+       * clear the stencil as a regular frame-buffer first. */
       GPU_framebuffer_bind(gbuffer_fb);
+      GPU_framebuffer_clear_stencil(gbuffer_fb, 0x0u);
     }
-    else {
-      if (!GPU_stencil_export_support()) {
-        /* Clearing custom load-store frame-buffers is invalid,
-         * clear the stencil as a regular frame-buffer first. */
-        GPU_framebuffer_bind(gbuffer_fb);
-        GPU_framebuffer_clear_stencil(gbuffer_fb, 0x0u);
-      }
-      GPU_framebuffer_bind_ex(
-          gbuffer_fb,
-          {
-              {GPU_LOADACTION_LOAD, GPU_STOREACTION_STORE},       /* Depth */
-              {GPU_LOADACTION_LOAD, GPU_STOREACTION_STORE},       /* Combined */
-              {GPU_LOADACTION_CLEAR, GPU_STOREACTION_STORE, {0}}, /* GBuf Header */
-              {GPU_LOADACTION_DONT_CARE, GPU_STOREACTION_STORE},  /* GBuf Normal */
-              {GPU_LOADACTION_DONT_CARE, GPU_STOREACTION_STORE},  /* GBuf Closure */
-              {GPU_LOADACTION_DONT_CARE, GPU_STOREACTION_STORE},  /* GBuf Closure 2*/
-          });
-    }
+    GPU_framebuffer_bind_ex(
+        gbuffer_fb,
+        {
+            {GPU_LOADACTION_LOAD, GPU_STOREACTION_STORE},       /* Depth */
+            {GPU_LOADACTION_LOAD, GPU_STOREACTION_STORE},       /* Combined */
+            {GPU_LOADACTION_CLEAR, GPU_STOREACTION_STORE, {0}}, /* GBuf Header */
+            {GPU_LOADACTION_DONT_CARE, GPU_STOREACTION_STORE},  /* GBuf Normal */
+            {GPU_LOADACTION_DONT_CARE, GPU_STOREACTION_STORE},  /* GBuf Closure */
+            {GPU_LOADACTION_DONT_CARE, GPU_STOREACTION_STORE},  /* GBuf Closure 2*/
+        });
   }
 
   void release()

commit 94a1ab5d3dd278f0d5c30cec382afdd480595cc3

Fix #122551: Metal displays black screen on ATI/Intel GPUs
When using Metal backend on ATI/Intel GPUs the stencil buffer doesn't
contain the correct information, skipping diffuse and glossy PBR components.

Failing behavior was introduced by e97e06ea2c298b4cf2fd906cf77d36d2da698cda
This PR works around this by reverting the code when using ATI/Intel GPUs.
The root cause is somewhere the combination of stencil export support, clear
operations and subpass.

**TODO**

- [ ] Validate that this fixes the ATI issue as well

source/blender/draw/engines/eevee_next/eevee_gbuffer.hh
@@ -161,6 +161,15 @@ struct GBuffer {
       header_tx.clear(uint4(0));
     }
 
+    /* Workaround a Metal bug that is only showing up on ATI/Intel GPUs. */
+    if (GPU_type_matches(
+            GPU_DEVICE_ATI | GPU_DEVICE_INTEL | GPU_DEVICE_INTEL_UHD, GPU_OS_MAC, GPU_DRIVER_ANY))
+    {
+      header_tx.clear(uint4(0));
+      GPU_framebuffer_bind(gbuffer_fb);
+      return;
+    }
+
     if (!GPU_stencil_export_support()) {
       /* Clearing custom load-store frame-buffers is invalid,
        * clear the stencil as a regular frame-buffer first. */