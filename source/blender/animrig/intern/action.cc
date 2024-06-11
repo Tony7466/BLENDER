ccbafd854a9846af4dd0b0b5b79d0b534b7e5318
Christoph Lendenfeld <chris.lenden@gmail.com>
2024-06-11 16:03:01 +0200

    fix compile issues

--- source/blender/animrig/intern/action.cc
+++ source/blender/animrig/intern/action.cc
@@ -514,64 +514,6 @@ void Action::unassign_id(ID &animated_id)
   adt->action = nullptr;
 }
 
-static void clear_selection_legacy_action(Action &action)
-{
-  BLI_assert(action.is_action_legacy());
-  LISTBASE_FOREACH (FCurve *, fcu, &action.curves) {
-    if (!fcu->bezt) {
-      continue;
-    }
-    BKE_fcurve_deselect_all_keys(fcu);
-  }
-}
-
-static void clear_selection_layered_action(Action &action)
-{
-  BLI_assert(action.is_action_layered());
-  for (Layer *layer : action.layers()) {
-    if (!layer) {
-      continue;
-    }
-    for (Strip *strip : layer->strips()) {
-      if (!strip || !strip->is<KeyframeStrip>()) {
-        continue;
-      }
-      KeyframeStrip &key_strip = strip->as<KeyframeStrip>();
-      for (ChannelBag *ch_bag : key_strip.channelbags()) {
-        if (!ch_bag) {
-          continue;
-        }
-        for (FCurve *fcu : ch_bag->fcurves()) {
-          BKE_fcurve_deselect_all_keys(fcu);
-        }
-      }
-    }
-  }
-}
-
-void Action::deselect_keys()
-{
-  if (this->is_action_layered()) {
-    clear_selection_layered_action(*this);
-  }
-  else {
-    BLI_assert(this->is_action_legacy());
-    clear_selection_legacy_action(*this);
-  }
-}
-
-void deselect_action_keys(blender::Span<bAction *> actions)
-{
-  Span<bAction *> visited_actions;
-  for (bAction *action : actions) {
-    if (visited_actions.contains(action)) {
-      continue;
-    }
-    action->wrap().deselect_keys();
-    visited_actions.add(action);
-  }
-}
-
 /* ----- ActionLayer implementation ----------- */
 
 Layer::Layer(const Layer &other)
@@ -1222,6 +1164,64 @@ FCurve *action_fcurve_ensure(Main *bmain,
   DEG_relations_tag_update(bmain);
 
   return fcu;
+}
+
+static void clear_selection_legacy_action(Action &action)
+{
+  BLI_assert(action.is_action_legacy());
+  LISTBASE_FOREACH (FCurve *, fcu, &action.curves) {
+    if (!fcu->bezt) {
+      continue;
+    }
+    BKE_fcurve_deselect_all_keys(fcu);
+  }
+}
+
+static void clear_selection_layered_action(Action &action)
+{
+  BLI_assert(action.is_action_layered());
+  for (Layer *layer : action.layers()) {
+    if (!layer) {
+      continue;
+    }
+    for (Strip *strip : layer->strips()) {
+      if (!strip || !strip->is<KeyframeStrip>()) {
+        continue;
+      }
+      KeyframeStrip &key_strip = strip->as<KeyframeStrip>();
+      for (ChannelBag *ch_bag : key_strip.channelbags()) {
+        if (!ch_bag) {
+          continue;
+        }
+        for (FCurve *fcu : ch_bag->fcurves()) {
+          BKE_fcurve_deselect_all_keys(fcu);
+        }
+      }
+    }
+  }
+}
+
+void Action::deselect_keys()
+{
+  if (this->is_action_layered()) {
+    clear_selection_layered_action(*this);
+  }
+  else {
+    BLI_assert(this->is_action_legacy());
+    clear_selection_legacy_action(*this);
+  }
+}
+
+void deselect_action_keys(blender::Span<bAction *> actions)
+{
+  Set<bAction *> visited_actions;
+  for (bAction *action : actions) {
+    if (visited_actions.contains(action)) {
+      continue;
+    }
+    action->wrap().deselect_keys();
+    visited_actions.add(action);
+  }
 }
 
 }  // namespace blender::animrig
