6ef2b47cdd179a91e6a7e635de8392b5a325ea5b
Hans Goudey <hans@blender.org>
2024-06-11 11:59:39 -0400

    Apply translations to deformed positions before applying inverse deformation

--- source/blender/editors/sculpt_paint/brushes/smooth.cc
+++ source/blender/editors/sculpt_paint/brushes/smooth.cc
@@ -158,13 +158,14 @@ BLI_NOINLINE static void apply_positions_faces(const Sculpt &sd,
 
   clip_and_lock_translations(sd, ss, positions_eval, verts, translations);
 
+  apply_translations_to_pbvh(*ss.pbvh, verts, positions_orig);
+
   if (!ss.deform_imats.is_empty()) {
     apply_crazyspace_to_translations(ss.deform_imats, verts, translations);
   }
 
   apply_translations(translations, verts, positions_orig);
   apply_translations_to_shape_keys(object, verts, translations, positions_orig);
-  apply_translations_to_pbvh(*ss.pbvh, verts, positions_orig);
 }
 
 BLI_NOINLINE static void do_smooth_brush_mesh(const Sculpt &sd,
