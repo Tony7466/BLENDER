Tony7466/BLENDER

commit 0089a90625dd425803e8921a6573e17719b4bf9f

Refactor: Attribute API: Remove ID owner dependency
The attribute API defined in `attribute.cc` was dependent on
the assumption that `ID`s are always the "direct" owners of attributes.

For Grease Pencil drawings, this is not the case. The Grease Pencil ID
stores the attributes for layers, and the attributes for drawings are stored
in `CurvesGeometry` on the drawings themselves.

In order to make use of  `rna_attribute.cc`, we need that API to handle
other types of attribute owners.

This adds an `AttributeOwner` which is basically just a type and a
pointer. We replace the `ID` pointers and pass `AttributeOwner`s instead.

For cases where we have to do a switch based on the type, all the
types are handled and the `default` statment is left out. This ensures
that we get a compiler warning when a new `AttributeOwnerType`
is added.

No functional changes expected.

Pull Request: https://projects.blender.org/blender/blender/pulls/122765

source/blender/editors/geometry/geometry_attributes.cc
@@ -194,7 +194,8 @@ void rna_property_for_attribute_type_set_value(PointerRNA &ptr,
 
 bool attribute_set_poll(bContext &C, const ID &object_data)
 {
-  const CustomDataLayer *layer = BKE_id_attributes_active_get(&const_cast<ID &>(object_data));
+  AttributeOwner owner = AttributeOwner::from_id(&const_cast<ID &>(object_data));
+  const CustomDataLayer *layer = BKE_attributes_active_get(owner);
   if (!layer) {
     CTX_wm_operator_poll_msg_set(&C, "No active attribute");
     return false;
@@ -212,9 +213,10 @@ static bool geometry_attributes_poll(bContext *C)
 {
   const Object *ob = object::context_object(C);
   const Main *bmain = CTX_data_main(C);
-  const ID *data = (ob) ? static_cast<ID *>(ob->data) : nullptr;
+  ID *data = (ob) ? static_cast<ID *>(ob->data) : nullptr;
+  AttributeOwner owner = AttributeOwner::from_id(data);
   return (ob && BKE_id_is_editable(bmain, &ob->id) && data && BKE_id_is_editable(bmain, data)) &&
-         BKE_id_attributes_supported(data);
+         BKE_attributes_supported(owner);
 }
 
 static bool geometry_attributes_remove_poll(bContext *C)
@@ -225,7 +227,8 @@ static bool geometry_attributes_remove_poll(bContext *C)
 
   Object *ob = object::context_object(C);
   ID *data = (ob) ? static_cast<ID *>(ob->data) : nullptr;
-  if (BKE_id_attributes_active_get(data) != nullptr) {
+  AttributeOwner owner = AttributeOwner::from_id(data);
+  if (BKE_attributes_active_get(owner) != nullptr) {
     return true;
   }
 
@@ -258,13 +261,14 @@ static int geometry_attribute_add_exec(bContext *C, wmOperator *op)
   RNA_string_get(op->ptr, "name", name);
   eCustomDataType type = (eCustomDataType)RNA_enum_get(op->ptr, "data_type");
   bke::AttrDomain domain = bke::AttrDomain(RNA_enum_get(op->ptr, "domain"));
-  CustomDataLayer *layer = BKE_id_attribute_new(id, name, type, domain, op->reports);
+  AttributeOwner owner = AttributeOwner::from_id(id);
+  CustomDataLayer *layer = BKE_attribute_new(owner, name, type, domain, op->reports);
 
   if (layer == nullptr) {
     return OPERATOR_CANCELLED;
   }
 
-  BKE_id_attributes_active_set(id, layer->name);
+  BKE_attributes_active_set(owner, layer->name);
 
   DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
   WM_main_add_notifier(NC_GEOM | ND_DATA, id);
@@ -328,13 +332,14 @@ static int geometry_attribute_remove_exec(bContext *C, wmOperator *op)
 {
   Object *ob = object::context_object(C);
   ID *id = static_cast<ID *>(ob->data);
-  CustomDataLayer *layer = BKE_id_attributes_active_get(id);
+  AttributeOwner owner = AttributeOwner::from_id(id);
+  CustomDataLayer *layer = BKE_attributes_active_get(owner);
 
-  if (!BKE_id_attribute_remove(id, layer->name, op->reports)) {
+  if (!BKE_attribute_remove(owner, layer->name, op->reports)) {
     return OPERATOR_CANCELLED;
   }
 
-  int *active_index = BKE_id_attributes_active_index_p(id);
+  int *active_index = BKE_attributes_active_index_p(owner);
   if (*active_index > 0) {
     *active_index -= 1;
   }
@@ -369,7 +374,8 @@ static int geometry_color_attribute_add_exec(bContext *C, wmOperator *op)
   RNA_string_get(op->ptr, "name", name);
   eCustomDataType type = (eCustomDataType)RNA_enum_get(op->ptr, "data_type");
   bke::AttrDomain domain = bke::AttrDomain(RNA_enum_get(op->ptr, "domain"));
-  CustomDataLayer *layer = BKE_id_attribute_new(id, name, type, domain, op->reports);
+  AttributeOwner owner = AttributeOwner::from_id(id);
+  CustomDataLayer *layer = BKE_attribute_new(owner, name, type, domain, op->reports);
 
   float color[4];
   RNA_float_get_array(op->ptr, "color", color);
@@ -416,6 +422,7 @@ static bool geometry_attribute_convert_poll(bContext *C)
 
   Object *ob = object::context_object(C);
   ID *data = static_cast<ID *>(ob->data);
+  AttributeOwner owner = AttributeOwner::from_id(data);
   if (GS(data->name) != ID_ME) {
     return false;
   }
@@ -423,7 +430,7 @@ static bool geometry_attribute_convert_poll(bContext *C)
     CTX_wm_operator_poll_msg_set(C, "Operation is not allowed in edit mode");
     return false;
   }
-  if (BKE_id_attributes_active_get(data) == nullptr) {
+  if (BKE_attributes_active_get(owner) == nullptr) {
     return false;
   }
   return true;
@@ -433,7 +440,8 @@ static int geometry_attribute_convert_exec(bContext *C, wmOperator *op)
 {
   Object *ob = object::context_object(C);
   ID *ob_data = static_cast<ID *>(ob->data);
-  CustomDataLayer *layer = BKE_id_attributes_active_get(ob_data);
+  AttributeOwner owner = AttributeOwner::from_id(ob_data);
+  CustomDataLayer *layer = BKE_attributes_active_get(owner);
   const ConvertAttributeMode mode = static_cast<ConvertAttributeMode>(
       RNA_enum_get(op->ptr, "mode"));
   Mesh *mesh = reinterpret_cast<Mesh *>(ob_data);
@@ -474,7 +482,8 @@ static int geometry_attribute_convert_exec(bContext *C, wmOperator *op)
           BKE_defvert_add_index_notest(dverts + i, defgroup_index, weight);
         }
       }
-      int *active_index = BKE_id_attributes_active_index_p(&mesh->id);
+      AttributeOwner owner = AttributeOwner::from_id(&mesh->id);
+      int *active_index = BKE_attributes_active_index_p(owner);
       if (*active_index > 0) {
         *active_index -= 1;
       }
@@ -596,8 +605,8 @@ static int geometry_color_attribute_remove_exec(bContext *C, wmOperator *op)
   if (active_name.empty()) {
     return OPERATOR_CANCELLED;
   }
-
-  if (!BKE_id_attribute_remove(id, active_name.c_str(), op->reports)) {
+  AttributeOwner owner = AttributeOwner::from_id(id);
+  if (!BKE_attribute_remove(owner, active_name.c_str(), op->reports)) {
     return OPERATOR_CANCELLED;
   }
 
@@ -647,7 +656,8 @@ static int geometry_color_attribute_duplicate_exec(bContext *C, wmOperator *op)
     return OPERATOR_CANCELLED;
   }
 
-  CustomDataLayer *new_layer = BKE_id_attribute_duplicate(id, active_name, op->reports);
+  AttributeOwner owner = AttributeOwner::from_id(id);
+  CustomDataLayer *new_layer = BKE_attribute_duplicate(owner, active_name, op->reports);
   if (new_layer == nullptr) {
     return OPERATOR_CANCELLED;
   }
@@ -701,9 +711,10 @@ static int geometry_attribute_convert_invoke(bContext *C,
 {
   Object *ob = object::context_object(C);
   Mesh *mesh = static_cast<Mesh *>(ob->data);
+  AttributeOwner owner = AttributeOwner::from_id(&mesh->id);
 
   const bke::AttributeMetaData meta_data = *mesh->attributes().lookup_meta_data(
-      BKE_id_attributes_active_get(&mesh->id)->name);
+      BKE_attributes_active_get(owner)->name);
 
   PropertyRNA *prop = RNA_struct_find_property(op->ptr, "domain");
   if (!RNA_property_is_set(op->ptr, prop)) {

source/blender/blenkernel/BKE_attribute.h
@@ -11,6 +11,7 @@
 
 #include <string>
 
+#include "BLI_string_ref.hh"
 #include "BLI_sys_types.h"
 
 #include "DNA_customdata_types.h"
@@ -22,6 +23,10 @@ struct CustomData;
 struct CustomDataLayer;
 struct ID;
 struct ReportList;
+struct Mesh;
+struct PointCloud;
+struct Curves;
+struct GreasePencil;
 
 typedef enum AttrDomainMask {
   ATTR_DOMAIN_MASK_POINT = (1 << 0),
@@ -34,6 +39,33 @@ typedef enum AttrDomainMask {
 } AttrDomainMask;
 ENUM_OPERATORS(AttrDomainMask, ATTR_DOMAIN_MASK_ALL);
 
+enum class AttributeOwnerType {
+  None = 0,
+  Mesh,
+  PointCloud,
+  Curves,
+  GreasePencil,
+};
+
+class AttributeOwner {
+  AttributeOwnerType type_ = AttributeOwnerType::None;
+  void *ptr_ = nullptr;
+
+ public:
+  AttributeOwner(){};
+  AttributeOwner(AttributeOwnerType type, void *ptr) : type_(type), ptr_(ptr){};
+
+  static AttributeOwner from_id(ID *id);
+
+  AttributeOwnerType type() const;
+  bool is_valid() const;
+
+  Mesh *get_mesh() const;
+  PointCloud *get_pointcloud() const;
+  Curves *get_curves() const;
+  GreasePencil *get_grease_pencil() const;
+};
+
 #define ATTR_DOMAIN_AS_MASK(domain) ((AttrDomainMask)((1 << (int)(domain))))
 
 /* All domains that support color attributes. */
@@ -42,69 +74,70 @@ ENUM_OPERATORS(AttrDomainMask, ATTR_DOMAIN_MASK_ALL);
 
 /* Attributes. */
 
-bool BKE_id_attributes_supported(const struct ID *id);
+bool BKE_attributes_supported(const AttributeOwner &owner);
 bool BKE_attribute_allow_procedural_access(const char *attribute_name);
 
 /**
  * Create a new attribute layer.
  */
-struct CustomDataLayer *BKE_id_attribute_new(struct ID *id,
-                                             const char *name,
-                                             eCustomDataType type,
-                                             blender::bke::AttrDomain domain,
-                                             struct ReportList *reports);
-bool BKE_id_attribute_remove(struct ID *id, const char *name, struct ReportList *reports);
+struct CustomDataLayer *BKE_attribute_new(AttributeOwner &owner,
+                                          const char *name,
+                                          eCustomDataType type,
+                                          blender::bke::AttrDomain domain,
+                                          struct ReportList *reports);
+bool BKE_attribute_remove(AttributeOwner &owner, const char *name, struct ReportList *reports);
 
 /**
  * Creates a duplicate attribute layer.
  */
-struct CustomDataLayer *BKE_id_attribute_duplicate(struct ID *id,
+struct CustomDataLayer *BKE_attribute_duplicate(AttributeOwner &owner,
+                                                const char *name,
+                                                struct ReportList *reports);
+
+struct CustomDataLayer *BKE_attribute_find(const AttributeOwner &owner,
+                                           const char *name,
+                                           eCustomDataType type,
+                                           blender::bke::AttrDomain domain);
+
+const struct CustomDataLayer *BKE_attribute_search(const AttributeOwner &owner,
                                                    const char *name,
-                                                   struct ReportList *reports);
+                                                   eCustomDataMask type,
+                                                   AttrDomainMask domain_mask);
 
-struct CustomDataLayer *BKE_id_attribute_find(const struct ID *id,
-                                              const char *name,
-                                              eCustomDataType type,
-                                              blender::bke::AttrDomain domain);
+struct CustomDataLayer *BKE_attribute_search_for_write(AttributeOwner &owner,
+                                                       const char *name,
+                                                       eCustomDataMask type,
+                                                       AttrDomainMask domain_mask);
 
-const struct CustomDataLayer *BKE_id_attribute_search(const struct ID *id,
-                                                      const char *name,
-                                                      eCustomDataMask type,
-                                                      AttrDomainMask domain_mask);
+blender::bke::AttrDomain BKE_attribute_domain(const AttributeOwner &owner,
+                                              const struct CustomDataLayer *layer);
+int BKE_attribute_data_length(AttributeOwner &owner, struct CustomDataLayer *layer);
+bool BKE_attribute_required(const AttributeOwner &owner, const char *name);
+bool BKE_attribute_rename(AttributeOwner &owner,
+                          const char *old_name,
+                          const char *new_name,
+                          struct ReportList *reports);
 
-struct CustomDataLayer *BKE_id_attribute_search_for_write(struct ID *id,
-                                                          const char *name,
-                                                          eCustomDataMask type,
-                                                          AttrDomainMask domain_mask);
+int BKE_attributes_length(const AttributeOwner &owner,
+                          AttrDomainMask domain_mask,
+                          eCustomDataMask mask);
 
-blender::bke::AttrDomain BKE_id_attribute_domain(const struct ID *id,
-                                                 const struct CustomDataLayer *layer);
-int BKE_id_attribute_data_length(struct ID *id, struct CustomDataLayer *layer);
-bool BKE_id_attribute_required(const struct ID *id, const char *name);
-bool BKE_id_attribute_rename(struct ID *id,
-                             const char *old_name,
-                             const char *new_name,
-                             struct ReportList *reports);
+struct CustomDataLayer *BKE_attributes_active_get(AttributeOwner &owner);
+void BKE_attributes_active_set(AttributeOwner &owner, const char *name);
+int *BKE_attributes_active_index_p(AttributeOwner &owner);
 
-int BKE_id_attributes_length(const struct ID *id,
-                             AttrDomainMask domain_mask,
-                             eCustomDataMask mask);
-
-struct CustomDataLayer *BKE_id_attributes_active_get(struct ID *id);
-void BKE_id_attributes_active_set(struct ID *id, const char *name);
-int *BKE_id_attributes_active_index_p(struct ID *id);
-
-CustomData *BKE_id_attributes_iterator_next_domain(struct ID *id, struct CustomDataLayer *layers);
-CustomDataLayer *BKE_id_attribute_from_index(struct ID *id,
-                                             int lookup_index,
-                                             AttrDomainMask domain_mask,
-                                             eCustomDataMask layer_mask);
+CustomData *BKE_attributes_iterator_next_domain(AttributeOwner &owner,
+                                                struct CustomDataLayer *layers);
+CustomDataLayer *BKE_attribute_from_index(AttributeOwner &owner,
+                                          int lookup_index,
+                                          AttrDomainMask domain_mask,
+                                          eCustomDataMask layer_mask);
 
 /** Layer is allowed to be nullptr; if so -1 (layer not found) will be returned. */
-int BKE_id_attribute_to_index(const struct ID *id,
-                              const CustomDataLayer *layer,
-                              AttrDomainMask domain_mask,
-                              eCustomDataMask layer_mask);
+int BKE_attribute_to_index(const AttributeOwner &owner,
+                           const CustomDataLayer *layer,
+                           AttrDomainMask domain_mask,
+                           eCustomDataMask layer_mask);
 
 const char *BKE_id_attributes_active_color_name(const struct ID *id);
 const char *BKE_id_attributes_default_color_name(const struct ID *id);
@@ -114,7 +147,8 @@ void BKE_id_attributes_default_color_set(struct ID *id, const char *name);
 const struct CustomDataLayer *BKE_id_attributes_color_find(const struct ID *id, const char *name);
 bool BKE_color_attribute_supported(const struct Mesh &mesh, const blender::StringRef name);
 
-std::string BKE_id_attribute_calc_unique_name(const struct ID &id, const blender::StringRef name);
+std::string BKE_attribute_calc_unique_name(const AttributeOwner &owner,
+                                           const blender::StringRef name);
 
 const char *BKE_uv_map_vert_select_name_get(const char *uv_map_name, char *buffer);
 const char *BKE_uv_map_edge_select_name_get(const char *uv_map_name, char *buffer);

source/blender/blenkernel/intern/data_transfer.cc
@@ -260,39 +260,39 @@ static void data_transfer_mesh_attributes_transfer_active_color_string(
     return;
   }
 
+  const AttributeOwner owner_src = AttributeOwner::from_id(const_cast<ID *>(&mesh_src->id));
+  AttributeOwner owner_dst = AttributeOwner::from_id(&mesh_dst->id);
+
   const char *active_color_src = BKE_id_attributes_active_color_name(&mesh_src->id);
 
-  if ((data_type == CD_PROP_COLOR) && !BKE_id_attribute_search(&const_cast<ID &>(mesh_src->id),
-                                                               active_color_src,
-                                                               CD_MASK_PROP_COLOR,
-                                                               ATTR_DOMAIN_MASK_COLOR))
+  if ((data_type == CD_PROP_COLOR) &&
+      !BKE_attribute_search(
+          owner_src, active_color_src, CD_MASK_PROP_COLOR, ATTR_DOMAIN_MASK_COLOR))
   {
     return;
   }
   if ((data_type == CD_PROP_BYTE_COLOR) &&
-      !BKE_id_attribute_search(&const_cast<ID &>(mesh_src->id),
-                               active_color_src,
-                               CD_MASK_PROP_BYTE_COLOR,
-                               ATTR_DOMAIN_MASK_COLOR))
+      !BKE_attribute_search(
+          owner_src, active_color_src, CD_MASK_PROP_BYTE_COLOR, ATTR_DOMAIN_MASK_COLOR))
   {
     return;
   }
 
   if ((data_type == CD_PROP_COLOR) &&
-      BKE_id_attribute_search(
-          &mesh_dst->id, active_color_src, CD_MASK_PROP_COLOR, ATTR_DOMAIN_MASK_COLOR))
+      BKE_attribute_search(
+          owner_dst, active_color_src, CD_MASK_PROP_COLOR, ATTR_DOMAIN_MASK_COLOR))
   {
     mesh_dst->active_color_attribute = BLI_strdup(active_color_src);
   }
   else if ((data_type == CD_PROP_BYTE_COLOR) &&
-           BKE_id_attribute_search(
-               &mesh_dst->id, active_color_src, CD_MASK_PROP_BYTE_COLOR, ATTR_DOMAIN_MASK_COLOR))
+           BKE_attribute_search(
+               owner_dst, active_color_src, CD_MASK_PROP_BYTE_COLOR, ATTR_DOMAIN_MASK_COLOR))
   {
     mesh_dst->active_color_attribute = BLI_strdup(active_color_src);
   }
   else {
-    CustomDataLayer *first_color_layer = BKE_id_attribute_from_index(
-        &mesh_dst->id, 0, mask_domain, CD_MASK_COLOR_ALL);
+    CustomDataLayer *first_color_layer = BKE_attribute_from_index(
+        owner_dst, 0, mask_domain, CD_MASK_COLOR_ALL);
     if (first_color_layer != nullptr) {
       mesh_dst->active_color_attribute = BLI_strdup(first_color_layer->name);
     }
@@ -311,39 +311,39 @@ static void data_transfer_mesh_attributes_transfer_default_color_string(
     return;
   }
 
+  const AttributeOwner owner_src = AttributeOwner::from_id(const_cast<ID *>(&mesh_src->id));
+  AttributeOwner owner_dst = AttributeOwner::from_id(&mesh_dst->id);
+
   const char *default_color_src = BKE_id_attributes_default_color_name(&mesh_src->id);
 
-  if ((data_type == CD_PROP_COLOR) && !BKE_id_attribute_search(&const_cast<ID &>(mesh_src->id),
-                                                               default_color_src,
-                                                               CD_MASK_PROP_COLOR,
-                                                               ATTR_DOMAIN_MASK_COLOR))
+  if ((data_type == CD_PROP_COLOR) &&
+      !BKE_attribute_search(
+          owner_src, default_color_src, CD_MASK_PROP_COLOR, ATTR_DOMAIN_MASK_COLOR))
   {
     return;
   }
   if ((data_type == CD_PROP_BYTE_COLOR) &&
-      !BKE_id_attribute_search(&const_cast<ID &>(mesh_src->id),
-                               default_color_src,
-                               CD_MASK_PROP_BYTE_COLOR,
-                               ATTR_DOMAIN_MASK_COLOR))
+      !BKE_attribute_search(
+          owner_src, default_color_src, CD_MASK_PROP_BYTE_COLOR, ATTR_DOMAIN_MASK_COLOR))
   {
     return;
   }
 
   if ((data_type == CD_PROP_COLOR) &&
-      BKE_id_attribute_search(
-          &mesh_dst->id, default_color_src, CD_MASK_PROP_COLOR, ATTR_DOMAIN_MASK_COLOR))
+      BKE_attribute_search(
+          owner_dst, default_color_src, CD_MASK_PROP_COLOR, ATTR_DOMAIN_MASK_COLOR))
   {
     mesh_dst->default_color_attribute = BLI_strdup(default_color_src);
   }
   else if ((data_type == CD_PROP_BYTE_COLOR) &&
-           BKE_id_attribute_search(
-               &mesh_dst->id, default_color_src, CD_MASK_PROP_BYTE_COLOR, ATTR_DOMAIN_MASK_COLOR))
+           BKE_attribute_search(
+               owner_dst, default_color_src, CD_MASK_PROP_BYTE_COLOR, ATTR_DOMAIN_MASK_COLOR))
   {
     mesh_dst->default_color_attribute = BLI_strdup(default_color_src);
   }
   else {
-    CustomDataLayer *first_color_layer = BKE_id_attribute_from_index(
-        &mesh_dst->id, 0, mask_domain, CD_MASK_COLOR_ALL);
+    CustomDataLayer *first_color_layer = BKE_attribute_from_index(
+        owner_dst, 0, mask_domain, CD_MASK_COLOR_ALL);
     if (first_color_layer != nullptr) {
       mesh_dst->default_color_attribute = BLI_strdup(first_color_layer->name);
     }

source/blender/blenkernel/intern/fluid.cc
@@ -3295,8 +3295,9 @@ static Mesh *create_liquid_geometry(FluidDomainSettings *fds,
   float time_mult = fds->dx / (DT_DEFAULT * (25.0f / FPS));
 
   if (use_speedvectors) {
-    CustomDataLayer *velocity_layer = BKE_id_attribute_new(
-        &mesh->id, "velocity", CD_PROP_FLOAT3, AttrDomain::Point, nullptr);
+    AttributeOwner owner = AttributeOwner::from_id(&mesh->id);
+    CustomDataLayer *velocity_layer = BKE_attribute_new(
+        owner, "velocity", CD_PROP_FLOAT3, AttrDomain::Point, nullptr);
     velarray = static_cast<float(*)[3]>(velocity_layer->data);
   }
 

source/blender/blenkernel/intern/mesh_legacy_convert.cc
@@ -1766,7 +1766,8 @@ void BKE_mesh_legacy_convert_uvs_to_generic(Mesh *mesh)
 
     CustomData_free_layer_named(&mesh->corner_data, uv_names[i], mesh->corners_num);
 
-    const std::string new_name = BKE_id_attribute_calc_unique_name(mesh->id, uv_names[i].c_str());
+    AttributeOwner owner = AttributeOwner::from_id(&mesh->id);
+    const std::string new_name = BKE_attribute_calc_unique_name(owner, uv_names[i].c_str());
     uv_names[i] = new_name;
 
     CustomData_add_layer_named_with_data(

source/blender/blenkernel/intern/paint.cc
@@ -1891,7 +1891,8 @@ void BKE_sculpt_color_layer_create_if_needed(Object *object)
     return;
   }
 
-  const std::string unique_name = BKE_id_attribute_calc_unique_name(orig_me->id, "Color");
+  AttributeOwner owner = AttributeOwner::from_id(&orig_me->id);
+  const std::string unique_name = BKE_attribute_calc_unique_name(owner, "Color");
   if (!orig_me->attributes_for_write().add(
           unique_name, AttrDomain::Point, CD_PROP_COLOR, AttributeInitDefaultValue()))
   {

source/blender/blenkernel/intern/pbvh.cc
@@ -1620,9 +1620,10 @@ IndexMask nodes_to_face_selection_grids(const SubdivCCG &subdiv_ccg,
 
 bool BKE_pbvh_get_color_layer(Mesh *mesh, CustomDataLayer **r_layer, AttrDomain *r_domain)
 {
-  *r_layer = BKE_id_attribute_search_for_write(
-      &mesh->id, mesh->active_color_attribute, CD_MASK_COLOR_ALL, ATTR_DOMAIN_MASK_COLOR);
-  *r_domain = *r_layer ? BKE_id_attribute_domain(&mesh->id, *r_layer) : AttrDomain::Point;
+  AttributeOwner owner = AttributeOwner::from_id(&mesh->id);
+  *r_layer = BKE_attribute_search_for_write(
+      owner, mesh->active_color_attribute, CD_MASK_COLOR_ALL, ATTR_DOMAIN_MASK_COLOR);
+  *r_domain = *r_layer ? BKE_attribute_domain(owner, *r_layer) : AttrDomain::Point;
   return *r_layer != nullptr;
 }
 

commit 535dfd0fd48a4a7ef72ec07687eb3a33cfb15210

Fix #123116: Regression: F3 search crash in pose mode
Caused by 0089a90625dd425803e8921a6573e17719b4bf9f
Return empty struct for ids that don't support attributes. Also add a check
for owner ptr.

source/blender/blenkernel/intern/attribute.cc
@@ -54,8 +54,6 @@ AttributeOwner AttributeOwner::from_id(ID *id)
       return AttributeOwner(AttributeOwnerType::Curves, id);
     case ID_GP:
       return AttributeOwner(AttributeOwnerType::GreasePencil, id);
-    default:
-      BLI_assert_unreachable();
   }
   return {};
 }

source/blender/editors/geometry/geometry_attributes.cc
@@ -215,6 +215,9 @@ static bool geometry_attributes_poll(bContext *C)
   const Main *bmain = CTX_data_main(C);
   ID *data = (ob) ? static_cast<ID *>(ob->data) : nullptr;
   AttributeOwner owner = AttributeOwner::from_id(data);
+  if (!owner.is_valid()) {
+    return false;
+  }
   return (ob && BKE_id_is_editable(bmain, &ob->id) && data && BKE_id_is_editable(bmain, data)) &&
          BKE_attributes_supported(owner);
 }