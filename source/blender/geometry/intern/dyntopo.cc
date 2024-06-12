2b8723cf682e7e6fad5275705adb0852706ad7e1
Iliya Katueshenock <modormoder@gmail.com>
2024-06-13 02:38:21 +0300

    progress

--- source/blender/geometry/intern/dyntopo.cc
+++ source/blender/geometry/intern/dyntopo.cc
@@ -58,54 +58,17 @@ static void parallel_transform(const Span<In> src,
   });
 }
 
-static void build_face_to_face_by_edge_map(const OffsetIndices<int> faces,
-                                           const Span<int> corner_edges,
-                                           const int edges_num,
-                                           Array<int> &r_offsets,
-                                           Array<int> &r_indices)
-{
-  Array<int> edge_to_face_offset_data;
-  Array<int> edge_to_face_indices;
-  const GroupedSpan<int> edge_to_face_map = bke::mesh::build_edge_to_face_map(
-      faces, corner_edges, edges_num, edge_to_face_offset_data, edge_to_face_indices);
-  const OffsetIndices<int> edge_to_face_offsets(edge_to_face_offset_data);
-
-  r_offsets = Array<int>(faces.size() + 1, 0);
-  threading::parallel_for(faces.index_range(), 4096, [&](const IndexRange range) {
-    for (const int face_i : range) {
-      for (const int edge : corner_edges.slice(faces[face_i])) {
-        /* Subtract face itself from the number of faces connected to the edge. */
-        r_offsets[face_i] += edge_to_face_offsets[edge].size() - 1;
-      }
-    }
-  });
-  const OffsetIndices<int> offsets = offset_indices::accumulate_counts_to_offsets(r_offsets);
-  r_indices.reinitialize(offsets.total_size());
-
-  threading::parallel_for(faces.index_range(), 1024, [&](IndexRange range) {
-    for (const int face_i : range) {
-      MutableSpan<int> neighbors = r_indices.as_mutable_span().slice(offsets[face_i]);
-      if (neighbors.is_empty()) {
-        continue;
-      }
-      int count = 0;
-      for (const int edge : corner_edges.slice(faces[face_i])) {
-        for (const int neighbor : edge_to_face_map[edge]) {
-          if (neighbor != face_i) {
-            neighbors[count] = neighbor;
-            count++;
-          }
-        }
-      }
-    }
-  });
-}
-
 static float len_squared_to_tris(const std::array<float3, 3> tri, const float3 pos)
 {
   float3 result;
   closest_on_tri_to_point_v3(result, pos, tri[0], tri[1], tri[2]);
   return math::distance_squared(result, pos);
+}
+
+static int3 gather_tri(const IndexRange range, const Span<int> indices)
+{
+  BLI_assert(range.size() == 3);
+  return int3(indices[range[0]], indices[range[1]], indices[range[2]]);
 }
 
 Mesh *subdivide(const Mesh &src_mesh,
@@ -123,16 +86,34 @@ Mesh *subdivide(const Mesh &src_mesh,
     }
     return true;
   }());
-  Array<int> face_to_face_map_offsets;
-  Array<int> face_to_face_map_indices;
-  build_face_to_face_by_edge_map(src_mesh.faces(),
-                                 src_mesh.corner_edges(),
-                                 src_mesh.edges_num,
-                                 face_to_face_map_offsets,
-                                 face_to_face_map_indices);
-  GroupedSpan<int>(OffsetIndices<int>(face_to_face_map_offsets), face_to_face_map_indices);
+
+  const OffsetIndices<int> faces = src_mesh.faces();
+  const Span<int> corner_edges = src_mesh.corner_edges();
+  const Span<int> corner_verts = src_mesh.corner_verts();
+
+  Array<int> edge_to_face_offset_data;
+  Array<int> edge_to_face_indices;
+  const GroupedSpan<int> edge_to_face_map = bke::mesh::build_edge_to_face_map(
+      faces, corner_edges, src_mesh.edges_num, edge_to_face_offset_data, edge_to_face_indices);
 
   Array<int> face_total_points(src_mesh.faces_num + 1);
+
+  for (const int face_i : faces.index_range()) {
+    const IndexRange face = faces[face_i];
+    const int3 edges = gather_tri(face, corner_edges);
+    const int3 verts = gather_tri(face, corner_verts);
+
+    const Span<int> a_faces = edge_to_face_map[edges[0]];
+    const Span<int> b_faces = edge_to_face_map[edges[1]];
+    const Span<int> c_faces = edge_to_face_map[edges[2]];
+
+    const int3 a_verts = gather_tri(faces[a_faces[0] == face_i ? a_faces[1] : a_faces[0]],
+                                    corner_verts);
+    const int3 b_verts = gather_tri(faces[b_faces[0] == face_i ? b_faces[1] : b_faces[0]],
+                                    corner_verts);
+    const int3 c_verts = gather_tri(faces[c_faces[0] == face_i ? c_faces[1] : c_faces[0]],
+                                    corner_verts);
+  }
 
   std::cout << face_total_points.as_span() << ";\n";
 
