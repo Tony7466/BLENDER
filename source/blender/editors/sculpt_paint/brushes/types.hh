b12ad830b9d3a6bb9a2d7dc55e9b2dee8ede8484
Raul Fernandez <farsthary84@gmail.com>
2024-06-13 01:42:36 -0400

    wip

--- source/blender/editors/sculpt_paint/brushes/types.hh
+++ source/blender/editors/sculpt_paint/brushes/types.hh
@@ -16,5 +16,6 @@ namespace blender::ed::sculpt_paint {
 void do_draw_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
 /** A simple normal-direction displacement based on image texture RGB/XYZ values. */
 void do_draw_vector_displacement_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
-
+/** A draw sharp brush */
+void do_draw_sharp_brush(const Sculpt &sd, Object &object, Span<PBVHNode *> nodes);
 }  // namespace blender::ed::sculpt_paint
