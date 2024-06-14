77d8c8b559323ee18d3900f47ad8aa75cd57e8ef
Devashish Lal <laldevashish@gmail.com>
2024-06-14 12:57:09 -0700

    rna changes

--- source/blender/makesrna/intern/rna_node_socket.cc
+++ source/blender/makesrna/intern/rna_node_socket.cc
@@ -1236,7 +1236,9 @@ static void rna_def_node_socket_string(BlenderRNA *brna, const char *identifier)
   RNA_def_struct_sdna_from(srna, "bNodeSocket", nullptr);
 }
 
-static void rna_def_node_socket_interface_string(BlenderRNA *brna, const char *identifier)
+static void rna_def_node_socket_interface_string(BlenderRNA *brna,
+                                                 const char *identifier,
+                                                 PropertySubType subtype)
 {
   StructRNA *srna;
   PropertyRNA *prop;
@@ -1247,9 +1249,19 @@ static void rna_def_node_socket_interface_string(BlenderRNA *brna, const char *i
 
   RNA_def_struct_sdna_from(srna, "bNodeSocketValueString", "socket_data");
 
-  prop = RNA_def_property(srna, "default_value", PROP_STRING, PROP_NONE);
+  prop = RNA_def_property(srna, "subtype", PROP_ENUM, PROP_NONE);
+  RNA_def_property_enum_items(prop, rna_enum_dummy_DEFAULT_items);
+  RNA_def_property_enum_sdna(prop, nullptr, "subtype");
+  RNA_def_property_enum_funcs(
+      prop, nullptr, nullptr, "rna_NodeTreeInterfaceSocketString_subtype_itemf");
+  RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
+  RNA_def_property_ui_text(prop, "Subtype", "Subtype of the default value");
+  RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_NodeTreeInterfaceSocket_value_update");
+
+  prop = RNA_def_property(srna, "default_value", PROP_STRING, subtype);
   RNA_def_property_string_sdna(prop, nullptr, "value");
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
+  // TODO: Do I need to call RNA_def_property_string_funcs() ?
   RNA_def_property_ui_text(prop, "Default Value", "Input value used for unconnected socket");
   RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_NodeTreeInterfaceSocket_value_update");
 
@@ -1739,7 +1751,7 @@ void rna_def_node_socket_interface_subtypes(BlenderRNA *brna)
         rna_def_node_socket_interface_color(brna, identifier);
         break;
       case SOCK_STRING:
-        rna_def_node_socket_interface_string(brna, identifier);
+        rna_def_node_socket_interface_string(brna, identifier, info.subtype);
         break;
       case SOCK_MENU:
         rna_def_node_socket_interface_menu(brna, identifier);
