e983b6d70efff76114506c9c58c16d5dd6a8d7e9
Raul Fernandez <farsthary84@gmail.com>
2024-06-16 18:07:27 -0400

    x265

--- build_files/cmake/platform/platform_apple.cmake
+++ build_files/cmake/platform/platform_apple.cmake
@@ -170,7 +170,7 @@ if(WITH_CODEC_FFMPEG)
     avcodec avdevice avformat avutil
     mp3lame ogg opus swresample swscale
     theora theoradec theoraenc vorbis vorbisenc
-    vorbisfile vpx x264)
+    vorbisfile vpx x264 x265)
   if(EXISTS ${LIBDIR}/ffmpeg/lib/libaom.a)
     list(APPEND FFMPEG_FIND_COMPONENTS aom)
   endif()
