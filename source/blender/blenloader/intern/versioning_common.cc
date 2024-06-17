d33c69cd159ff246d669d498dd2632965847755a
Falk David <falk@blender.org>
2024-06-17 11:22:41 +0200

    Fix subversion number

--- source/blender/blenloader/intern/versioning_common.cc
+++ source/blender/blenloader/intern/versioning_common.cc
@@ -661,7 +661,7 @@ void do_versions_after_setup(Main *new_bmain, BlendFileReadReport *reports)
     BKE_main_mesh_legacy_convert_auto_smooth(*new_bmain);
   }
 
-  if (!blendfile_or_libraries_versions_atleast(new_bmain, 403, 1)) {
+  if (!blendfile_or_libraries_versions_atleast(new_bmain, 403, 3)) {
     /* Convert all the legacy grease pencil objects. This does not touch annotations. */
     blender::bke::greasepencil::convert::legacy_main(*new_bmain, *reports);
   }
