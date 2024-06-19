a1165588d1a65d35779d1cfe8c812d9b1901d2d5
Aras Pranckevicius <aras@nesnausk.org>
2024-06-19 10:01:19 +0300

    VSE: further handle color tweaks
    Selected handles always use the same color as the outline. Previously
    they were almost always using it, except while actively transforming -
    the outline was brighter but the handle itself was not.
    
    With this, there's no need for handle colors state for the shader,
    it is enough to put in flags which handles are selected vs which
    ones are merely drawn.

--- source/blender/editors/space_sequencer/sequencer_timeline_draw.cc
+++ source/blender/editors/space_sequencer/sequencer_timeline_draw.cc
@@ -1435,37 +1435,24 @@ static void draw_strips_foreground(TimelineDrawContext *timeline_ctx,
 
     /* Handles on left/right side. */
     if (!locked && ED_sequencer_can_select_handle(scene, strip.seq, timeline_ctx->v2d)) {
-      const bool selected_l = ED_sequencer_handle_is_selected(strip.seq, SEQ_HANDLE_LEFT);
-      const bool selected_r = ED_sequencer_handle_is_selected(strip.seq, SEQ_HANDLE_RIGHT);
-      const bool show_l = show_handles || (selected && selected_l);
-      const bool show_r = show_handles || (selected && selected_r);
-      if (show_l || show_r) {
-        data.flags |= GPU_SEQ_FLAG_HANDLES;
+      const bool selected_l = selected &&
+                              ED_sequencer_handle_is_selected(strip.seq, SEQ_HANDLE_LEFT);
+      const bool selected_r = selected &&
+                              ED_sequencer_handle_is_selected(strip.seq, SEQ_HANDLE_RIGHT);
+      const bool show_l = show_handles || selected_l;
+      const bool show_r = show_handles || selected_r;
+      if (show_l) {
+        data.flags |= GPU_SEQ_FLAG_DRAW_LH;
       }
-
-      /* Left handle color. */
-      col[0] = col[1] = col[2] = 0;
-      col[3] = 50;
-      if (selected && selected_l) {
-        UI_GetThemeColor4ubv(active ? TH_SEQ_ACTIVE : TH_SEQ_SELECTED, col);
+      if (show_r) {
+        data.flags |= GPU_SEQ_FLAG_DRAW_RH;
+      }
+      if (selected_l) {
         data.flags |= GPU_SEQ_FLAG_SELECTED_LH;
       }
-      if (!show_l) {
-        col[3] = 0;
-      }
-      data.col_handle_left = color_pack(col);
-
-      /* Right handle color. */
-      col[0] = col[1] = col[2] = 0;
-      col[3] = 50;
-      if (selected && selected_r) {
-        UI_GetThemeColor4ubv(active ? TH_SEQ_ACTIVE : TH_SEQ_SELECTED, col);
+      if (selected_r) {
         data.flags |= GPU_SEQ_FLAG_SELECTED_RH;
       }
-      if (!show_r) {
-        col[3] = 0;
-      }
-      data.col_handle_right = color_pack(col);
     }
   }
   strips_batch.flush_batch();
--- source/blender/gpu/shaders/gpu_shader_sequencer_strips_frag.glsl
+++ source/blender/gpu/shaders/gpu_shader_sequencer_strips_frag.glsl
@@ -71,15 +71,15 @@ void main()
   /* Distance to whole strip shape. */
   float sdf = sdf_rounded_box(pos - center, size, radius);
 
-  /* Distance to inner part when optional handles are taken into account. */
+  /* Distance to inner part when handles are taken into account. */
   float sdf_inner = sdf;
-  if ((strip.flags & GPU_SEQ_FLAG_HANDLES) != 0) {
+  if ((strip.flags & GPU_SEQ_FLAG_ANY_HANDLE) != 0) {
     float handle_width = strip.handle_width * view_to_pixel.x;
     /* Take left/right handle from horizontal sides. */
-    if (strip.col_handle_left != 0) {
+    if ((strip.flags & GPU_SEQ_FLAG_DRAW_LH) != 0) {
       pos1.x += handle_width;
     }
-    if (strip.col_handle_right != 0) {
+    if ((strip.flags & GPU_SEQ_FLAG_DRAW_RH) != 0) {
       pos2.x -= handle_width;
     }
     /* Reduce vertical size by outline width. */
@@ -159,20 +159,20 @@ void main()
   }
 
   /* Handles. */
-  if ((strip.flags & GPU_SEQ_FLAG_HANDLES) != 0) {
+  vec4 col_outline = unpackUnorm4x8(strip.col_outline);
+  if ((strip.flags & GPU_SEQ_FLAG_ANY_HANDLE) != 0) {
     bool left_side = pos.x < center.x;
+    uint handle_flag = left_side ? GPU_SEQ_FLAG_SELECTED_LH : GPU_SEQ_FLAG_SELECTED_RH;
+    bool selected_handle = (strip.flags & handle_flag) != 0;
     /* Blend in handle color in between strip shape and inner handle shape. */
     if (sdf <= 0.0 && sdf_inner >= 0.0) {
-      vec4 hcol = unpackUnorm4x8(left_side ? strip.col_handle_left : strip.col_handle_right);
+      vec4 hcol = selected_handle ? col_outline : vec4(0, 0, 0, 0.2);
       hcol.a *= clamp(sdf_inner, 0.0, 1.0);
       col = blend_color(col, hcol);
     }
     /* For an unselected handle, no longer take it into account
      * for the "inner" distance. */
-    if (left_side && (strip.flags & GPU_SEQ_FLAG_SELECTED_LH) == 0) {
-      sdf_inner = sdf;
-    }
-    if (!left_side && (strip.flags & GPU_SEQ_FLAG_SELECTED_RH) == 0) {
+    if (!selected_handle) {
       sdf_inner = sdf;
     }
   }
@@ -191,7 +191,6 @@ void main()
 
   /* Outline / border. */
   if (border) {
-    vec4 col_outline = unpackUnorm4x8(strip.col_outline);
     col = add_outline(sdf, 0.0, outline_width, col, col_outline);
   }
 
