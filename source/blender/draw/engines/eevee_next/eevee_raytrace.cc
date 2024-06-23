213e815a1468aab48e334a7be870c4e6ad5c463c
Clément Foucault <foucault.clem@gmail.com>
2024-06-23 16:14:18 +0200

    Use bijection instead of random sampling for resolution bias

--- source/blender/draw/engines/eevee_next/eevee_raytrace.cc
+++ source/blender/draw/engines/eevee_next/eevee_raytrace.cc
@@ -399,16 +399,25 @@ RayTraceResult RayTraceModule::render(RayTraceBuffer &rt_buffer,
   data_.roughness_mask_scale = 1.0 / roughness_mask_fade;
   data_.roughness_mask_bias = data_.roughness_mask_scale * roughness_mask_start;
 
+  auto bias_get = [&](int scale_factor) -> int2 {
+    if (scale_factor == 1) {
+      return int2(0);
+    }
+    int pixel_count = square_i(scale_factor);
+    int prime = (pixel_count / 2) - 1;
+    int index_random = int((inst_.sampling.sample_index() * prime) % pixel_count);
+    return int2(index_random % scale_factor, index_random / scale_factor);
+  };
+
   /* Data for the radiance setup. */
   data_.resolution_scale = resolution_scale;
-  data_.resolution_bias = int2(inst_.sampling.rng_2d_get(SAMPLING_RAYTRACE_U) * resolution_scale);
+  data_.resolution_bias = bias_get(resolution_scale);
   data_.radiance_persmat = render_view.persmat();
   data_.full_resolution = extent;
   data_.full_resolution_inv = 1.0f / float2(extent);
 
   data_.horizon_resolution_scale = horizon_resolution_scale;
-  data_.horizon_resolution_bias = int2(inst_.sampling.rng_2d_get(SAMPLING_RAYTRACE_U) *
-                                       horizon_resolution_scale);
+  data_.horizon_resolution_bias = bias_get(horizon_resolution_scale);
   /* TODO(fclem): Eventually all uniform data is setup here. */
 
   inst_.uniform_data.push_update();
@@ -520,8 +529,6 @@ RayTraceResultTexture RayTraceModule::trace(
   data_.roughness_mask_scale = 1.0 / roughness_mask_fade;
   data_.roughness_mask_bias = data_.roughness_mask_scale * roughness_mask_start;
 
-  data_.resolution_scale = resolution_scale;
-  data_.resolution_bias = int2(inst_.sampling.rng_2d_get(SAMPLING_RAYTRACE_U) * resolution_scale);
   data_.history_persmat = denoise_buf->history_persmat;
   data_.radiance_persmat = render_view.persmat();
   data_.full_resolution = extent;
