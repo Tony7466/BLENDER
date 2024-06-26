bb382708da20f6802b4694651dbae9d77d34f787
Alexander Brock <alexander@rabbitai.de>
2024-06-26 15:32:09 +0200

    Use f suffix for float values and M_PI_F

--- intern/cycles/test/kernel_camera_projection_test.cpp
+++ intern/cycles/test/kernel_camera_projection_test.cpp
@@ -24,7 +24,7 @@ CCL_NAMESPACE_BEGIN
  */
 TEST(KernelCamera, FisheyeLensPolynomialRoundtrip)
 {
-  const float fov = 150.0 / 180 * M_PI;
+  const float fov = 150.0f / 180.0f * M_PI_F;
   const float width = 36;
   const float height = 41.142857142857144;
 
