e55b144e19f47872951c767897465933e232fafc
Anish Bharadwa <bharadwajanish@gmail.com>
2024-06-27 03:24:47 -0700

    Completed the implementation of bmesh_edgeloop_uv API
    This API returns an loops that are first grouped by connected line segment and
    then grouped by common UV coordinate given same underlying vertex. The
    index of all loops will be -1 except for endpoint loops who will have an
    index of 1

--- source/blender/bmesh/intern/bmesh_edgeloop_uv.cc
+++ source/blender/bmesh/intern/bmesh_edgeloop_uv.cc
@@ -1,5 +1,6 @@
 #include "bmesh_edgeloop_uv.hh"
 #include "bmesh.hh"
+#include <queue>
 using namespace blender;
 
 void UV_get_edgeloops(
@@ -7,17 +8,72 @@ void UV_get_edgeloops(
     BMesh *bm,
     std::vector<std::vector<std::vector<BMLoop *>>> *edgeloops,
     blender::FunctionRef<bool(const Scene *scene, BMLoop *l, const BMUVOffsets offsets)> callback)
+
 {
+  BMVert *v;
   BMLoop *l;
   BMIter viter, liter;
-  const BMUVOffsets offset = BM_uv_map_get_offsets(bm);
+  const BMUVOffsets offsets = BM_uv_map_get_offsets(bm);
 
-  int counter = 0;
+  std::queue<BMLoop *> queue;
+  BM_ITER_MESH (v, &viter, bm, BM_VERTS_OF_MESH) {
+    BM_ITER_ELEM (l, &liter, v, BM_LOOPS_OF_VERT) {
+      if (callback(scene, l, offsets) and (l->head.index != -1 and l->head.index != 1)) {
+        queue.push(l);
+      }
+      else {
+        continue;
+      }
+      std::vector<std::vector<BMLoop *>> edgeloop;
+      // while the queue is not empty
+      while (!queue.empty()) {
+        // pop the front of queue and store in queuel
+        BMLoop *queuel = queue.front();
+        queue.pop();
 
-  BM_ITER_MESH (l, &viter, bm, BM_VERTS_OF_MESH) {
-    BM_ITER_ELEM (l, &liter, l, BM_LOOPS_OF_VERT) {
-      if (callback(scene, l, offset)) {
-        counter++;
+        // get UV location of queuel
+        float *luv = BM_ELEM_CD_GET_FLOAT_P(queuel, offsets.uv);
+
+        // get the vertex of queuel
+        BMVert *queuev = queuel->v;
+
+        // vector of current UV coord
+        std::vector<BMLoop *> uvcoord;
+
+        // iterate over the loops of the vertex
+        BMLoop *currl;
+        BMIter liter2;
+        int selectedconnections = 0;
+        BM_ITER_ELEM (currl, &liter2, queuev, BM_LOOPS_OF_VERT) {
+          float *currluv = BM_ELEM_CD_GET_FLOAT_P(currl, offsets.uv);
+          if (currluv[0] != luv[0] || currluv[1] != luv[1]) {
+            continue;
+          }
+          BM_elem_index_set(currl, -1);
+          uvcoord.push_back(currl);
+
+          if (callback(scene, currl->next, offsets)) {
+            selectedconnections++;
+            if (currl->next->head.index != -1 and currl->next->head.index != 1) {
+              queue.push(currl->next);
+            }
+          }
+          if (callback(scene, currl->prev, offsets)) {
+            selectedconnections++;
+            if (currl->prev->head.index != -1 and currl->prev->head.index != 1) {
+              queue.push(currl->prev);
+            }
+          }
+        }
+        if (selectedconnections == 1) {
+          for (BMLoop *endpointloop : uvcoord) {
+            BM_elem_index_set(endpointloop, 1);
+          }
+        }
+        edgeloop.push_back(uvcoord);
+      }
+      if (!edgeloop.empty()) {
+        edgeloops->push_back(edgeloop);
       }
     }
   }
