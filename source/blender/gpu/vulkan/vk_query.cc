b9ec7a8a8e5b128b19f84b73f330c7b0232a5e2c
Jeroen Bakker <jeroen@blender.org>
2024-06-28 10:51:42 +0200

    Vulkan: Occlusion Queries

--- source/blender/gpu/vulkan/vk_query.cc
+++ source/blender/gpu/vulkan/vk_query.cc
@@ -33,16 +33,20 @@ uint32_t VKQueryPool::query_index_in_pool() const
 void VKQueryPool::init(GPUQueryType type)
 {
   BLI_assert(vk_query_pools_.is_empty());
-  queries_allocated_ = 0;
   queries_issued_ = 0;
   vk_query_type_ = to_vk_query_type(type);
 }
 
 void VKQueryPool::begin_query()
 {
-  if (queries_issued_ == queries_allocated_) {
-    VKBackend &backend = VKBackend::get();
-    const VKDevice &device = backend.device;
+  VKBackend &backend = VKBackend::get();
+  const VKDevice &device = backend.device;
+
+  uint32_t pool_index = queries_issued_ / query_chunk_len_;
+  bool is_new_pool = (queries_issued_ % query_chunk_len_) == 0;
+
+  if (pool_index == vk_query_pools_.size()) {
+    BLI_assert(is_new_pool);
     VK_ALLOCATION_CALLBACKS;
 
     VkQueryPoolCreateInfo create_info = {};
@@ -53,11 +57,15 @@ void VKQueryPool::begin_query()
     VkQueryPool vk_query_pool = VK_NULL_HANDLE;
     vkCreateQueryPool(device.vk_handle(), &create_info, vk_allocation_callbacks, &vk_query_pool);
     vk_query_pools_.append(vk_query_pool);
-    queries_allocated_ += query_chunk_len_;
+  }
+  BLI_assert(pool_index < vk_query_pools_.size());
+
+  /* When using a new query pool make sure to reset it before first usage. */
+  if (is_new_pool) {
+    // vkResetQueryPool(device.vk_handle(), vk_query_pools_.last(), 0, query_chunk_len_);
   }
 
   VKContext &context = *VKContext::get();
-
   render_graph::VKBeginQueryNode::Data begin_query = {};
   begin_query.vk_query_pool = vk_query_pools_.last();
   begin_query.query_index = query_index_in_pool();
@@ -76,38 +84,31 @@ void VKQueryPool::end_query()
 
 void VKQueryPool::get_occlusion_result(MutableSpan<uint32_t> r_values)
 {
-  if (vk_query_pools_.is_empty()) {
-    return;
-  }
-  // TODO: base this on queries_issued and not the vector len.
+  VKContext &context = *VKContext::get();
+  /* During selection the frame buffer is still rendering. It needs to finish the render scope to
+   * ensure the END_RENDERING node */
+  context.rendering_end();
+  context.render_graph.submit();
 
+  int queries_left = queries_issued_;
+  int pool_index = 0;
   VKBackend &backend = VKBackend::get();
   const VKDevice &device = backend.device;
-  for (int index : IndexRange(vk_query_pools_.size() - 1)) {
-    VkQueryPool vk_query_pool = vk_query_pools_[index];
-    uint32_t *r_values_chunk = &r_values[index * query_chunk_len_];
+  while (queries_left) {
+    VkQueryPool vk_query_pool = vk_query_pools_[pool_index];
+    uint32_t *r_values_chunk = &r_values[pool_index * query_chunk_len_];
+    uint32_t values_chunk_size = min_ii(queries_left, query_chunk_len_);
     vkGetQueryPoolResults(device.vk_handle(),
                           vk_query_pool,
                           0,
-                          query_chunk_len_,
-                          query_chunk_len_ * sizeof(uint32_t),
+                          values_chunk_size,
+                          values_chunk_size * sizeof(uint32_t),
                           r_values_chunk,
                           sizeof(uint32_t),
                           VK_QUERY_RESULT_WAIT_BIT);
-  }
 
-  {
-    VkQueryPool vk_query_pool = vk_query_pools_.last();
-    uint32_t *r_values_chunk = &r_values[(vk_query_pools_.size() - 1) * query_chunk_len_];
-    uint32_t values_left = queries_issued_ - (vk_query_pools_.size() - 1) * query_chunk_len_;
-    vkGetQueryPoolResults(device.vk_handle(),
-                          vk_query_pool,
-                          0,
-                          values_left,
-                          values_left * sizeof(uint32_t),
-                          r_values_chunk,
-                          sizeof(uint32_t),
-                          VK_QUERY_RESULT_WAIT_BIT);
+    queries_left = max_ii(queries_left - query_chunk_len_, 0);
+    pool_index += 1;
   }
 }
 
