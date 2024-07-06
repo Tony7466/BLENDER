294b4359d9b37017eaf72bcebf814783710275ba
Germain Le Chapelain <germain.lechapelain@lanvaux.fr>
2024-07-05 17:16:46 -0700

    Revert "= Fix (more Patch) test failures in BuildBot"
    This reverts commit e4d0b4ef02dad88c0168f4536e31c7c3e7832ec8.

--- source/blender/io/wavefront_obj/exporter/obj_export_io.hh
+++ source/blender/io/wavefront_obj/exporter/obj_export_io.hh
@@ -147,7 +147,7 @@ class FormatHandler : NonCopyable, NonMovable {
   {
     write_impl("deg {} {}\n", degs.first, degs.second);
   }
-  void write_obj_curve_begin(ObjectType type = OB_CURVES_LEGACY)
+  void write_obj_curve_begin(ObjectType type)
   {
     switch (type) {
       case OB_CURVES_LEGACY:
