d43a799c2509a5b29fafb916723e52a5caf2d179
Jacques Lucke <jacques@blender.org>
2024-07-08 16:35:35 +0200

    add selection in curves to grease pencil node

--- source/blender/nodes/geometry/nodes/node_geo_curves_to_grease_pencil.cc
+++ source/blender/nodes/geometry/nodes/node_geo_curves_to_grease_pencil.cc
@@ -13,6 +13,7 @@ namespace blender::nodes::node_geo_curves_to_grease_pencil_cc {
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_input<decl::Geometry>("Curve Instances").only_instances();
+  b.add_input<decl::Bool>("Selection").default_value(true).hide_value().field_on_all();
   b.add_output<decl::Geometry>("Grease Pencil");
 }
 
@@ -34,16 +35,29 @@ static void node_geo_exec(GeoNodeExecParams params)
     return;
   }
 
+  const bke::InstancesFieldContext field_context{*instances};
+  const Field<bool> selection_field = params.extract_input<Field<bool>>("Selection");
+  FieldEvaluator evaluator{field_context, instances_num};
+  evaluator.set_selection(selection_field);
+  evaluator.evaluate();
+  const IndexMask instance_selection = evaluator.get_evaluated_selection_as_mask();
+
+  const int layer_num = instance_selection.size();
+  if (layer_num == 0) {
+    params.set_default_remaining_outputs();
+    return;
+  }
+
   GreasePencil *grease_pencil = BKE_grease_pencil_new_nomain();
-  grease_pencil->add_layers_with_empty_drawings(instances_num);
+  grease_pencil->add_layers_with_empty_drawings(layer_num);
 
   VectorSet<Material *> all_materials;
 
-  for (const int instance_i : IndexRange(instances_num)) {
+  instance_selection.foreach_index([&](const int instance_i, const int layer_i) {
     const bke::InstanceReference &reference = references[reference_handles[instance_i]];
     const std::string name = reference.name();
 
-    bke::greasepencil::Layer *layer = grease_pencil->layer(instance_i);
+    bke::greasepencil::Layer *layer = grease_pencil->layer(layer_i);
     BLI_assert(layer);
     layer->set_name(name);
     layer->set_local_transform(transforms[instance_i]);
@@ -55,7 +69,7 @@ static void node_geo_exec(GeoNodeExecParams params)
     reference.to_geometry_set(instance_geometry);
     const Curves *instance_curves = instance_geometry.get_curves();
     if (!instance_curves) {
-      continue;
+      return;
     }
 
     bke::CurvesGeometry &strokes = drawing->strokes_for_write();
@@ -76,7 +90,7 @@ static void node_geo_exec(GeoNodeExecParams params)
       }
     }
     material_indices.finish();
-  }
+  });
 
   grease_pencil->material_array_num = all_materials.size();
   grease_pencil->material_array = MEM_cnew_array<Material *>(all_materials.size(), __func__);
@@ -84,32 +98,40 @@ static void node_geo_exec(GeoNodeExecParams params)
 
   const bke::AttributeAccessor instances_attributes = instances->attributes();
   bke::MutableAttributeAccessor grease_pencil_attributes = grease_pencil->attributes_for_write();
-  instances_attributes.for_all(
-      [&](const AttributeIDRef &attribute_id, const AttributeMetaData &meta_data) {
-        if (instances_attributes.is_builtin(attribute_id) &&
-            !grease_pencil_attributes.is_builtin(attribute_id))
-        {
-          return true;
-        }
-        const GAttributeReader src_attribute = instances_attributes.lookup(attribute_id);
-        if (!src_attribute) {
-          return true;
-        }
-        if (src_attribute.varray.is_span() && src_attribute.sharing_info) {
-          grease_pencil_attributes.add(
-              attribute_id,
-              AttrDomain::Layer,
-              meta_data.data_type,
-              bke::AttributeInitShared{src_attribute.varray.get_internal_span().data(),
-                                       *src_attribute.sharing_info});
-          return true;
-        }
-        grease_pencil_attributes.add(attribute_id,
-                                     AttrDomain::Layer,
-                                     meta_data.data_type,
-                                     bke::AttributeInitVArray(src_attribute.varray));
-        return false;
-      });
+  instances_attributes.for_all([&](const AttributeIDRef &attribute_id,
+                                   const AttributeMetaData &meta_data) {
+    if (instances_attributes.is_builtin(attribute_id) &&
+        !grease_pencil_attributes.is_builtin(attribute_id))
+    {
+      return true;
+    }
+    const GAttributeReader src_attribute = instances_attributes.lookup(attribute_id);
+    if (!src_attribute) {
+      return true;
+    }
+    if (instance_selection.size() == instances_num && src_attribute.varray.is_span() &&
+        src_attribute.sharing_info)
+    {
+      /* Try reusing existing attribute array. */
+      grease_pencil_attributes.add(
+          attribute_id,
+          AttrDomain::Layer,
+          meta_data.data_type,
+          bke::AttributeInitShared{src_attribute.varray.get_internal_span().data(),
+                                   *src_attribute.sharing_info});
+      return true;
+    }
+    if (!grease_pencil_attributes.add(
+            attribute_id, AttrDomain::Layer, meta_data.data_type, bke::AttributeInitConstruct()))
+    {
+      return true;
+    }
+    bke::GSpanAttributeWriter dst_attribute = grease_pencil_attributes.lookup_for_write_span(
+        attribute_id);
+    array_utils::gather(src_attribute.varray, instance_selection, dst_attribute.span);
+    dst_attribute.finish();
+    return true;
+  });
 
   params.set_output("Grease Pencil", GeometrySet::from_grease_pencil(grease_pencil));
 }
