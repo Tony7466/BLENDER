zyxcambridge/blender_llm

Fix #94125: Collada: not all edit mode changes are exported
This was the case when mulitple objects had changes in multi-object-
editmode.

Similar to f8b11528b2 & 3dd08beab308, this now ensures we have mesh data
in editmode.

Pull Request: https://projects.blender.org/blender/blender/pulls/123732

source/blender/io/collada/collada_utils.cpp
@@ -44,6 +44,7 @@
 #include "BKE_material.h"
 #include "BKE_mesh.hh"
 #include "BKE_mesh_legacy_convert.hh"
+#include "BKE_mesh_wrapper.hh"
 #include "BKE_mesh_runtime.hh"
 #include "BKE_node.hh"
 #include "BKE_object.hh"
@@ -288,6 +289,10 @@ Mesh *bc_get_mesh_copy(BlenderContext &blender_context,
     bc_triangulate_mesh(mesh);
   }
   BKE_mesh_tessface_ensure(mesh);
+
+  /* Ensure data exists if currently in edit mode. */
+  BKE_mesh_wrapper_ensure_mdata(mesh);
+
   return mesh;
 }
 

Tony7466/BLENDER

source/blender/io/collada/collada_utils.cpp
@@ -42,6 +42,7 @@
 #include "BKE_mesh.hh"
 #include "BKE_mesh_legacy_convert.h"
 #include "BKE_mesh_runtime.h"
+#include "BKE_mesh_wrapper.h"
 #include "BKE_node.hh"
 #include "BKE_object.h"
 #include "BKE_scene.h"
@@ -242,6 +243,10 @@ Mesh *bc_get_mesh_copy(BlenderContext &blender_context,
     bc_triangulate_mesh(tmpmesh);
   }
   BKE_mesh_tessface_ensure(tmpmesh);
+
+  /* Ensure data exists if currently in edit mode. */
+  BKE_mesh_wrapper_ensure_mdata(tmpmesh);
+
   return tmpmesh;
 }
 