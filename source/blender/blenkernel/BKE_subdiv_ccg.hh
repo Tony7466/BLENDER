d7a2ea26dccc1e7f660b8fdd8c2b2a0cdb018226
Sean Kim <SeanCTKim@protonmail.com>
2024-07-10 10:45:22 -0700

    Pass by value instead of reference for BitSpan and SubdivCCGCoord

--- source/blender/blenkernel/BKE_subdiv_ccg.hh
+++ source/blender/blenkernel/BKE_subdiv_ccg.hh
@@ -285,9 +285,9 @@ SubdivCCGAdjacencyType BKE_subdiv_ccg_coarse_mesh_adjacency_info_get(
 /* Determines if a given grid coordinate is on a coarse mesh boundary. */
 bool BKE_subdiv_ccg_coord_is_mesh_boundary(blender::OffsetIndices<int> faces,
                                            blender::Span<int> corner_verts,
-                                           const blender::BitSpan &boundary_verts,
+                                           blender::BitSpan boundary_verts,
                                            const SubdivCCG &subdiv_ccg,
-                                           const SubdivCCGCoord &coord);
+                                           SubdivCCGCoord coord);
 
 /* Get array which is indexed by face index and contains index of a first grid of the face.
  *
