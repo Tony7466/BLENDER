bbf7cb97b30ae4e21f1b9d6cfa3df946511cb62a
Hans Goudey <hans@blender.org>
2024-07-11 23:08:38 -0400

    Sculpt: Data oriented refactor for enhance details brush
    This brush first stores an array of translations for each vertex and
    then reuses those translations for the rest of the stroke. I added
    a few utilities to simplify storing the data for all vertices for the
    multires and BMesh implementations. Compared to the old code,
    computing the translations is skipped for completely hidden or
    masked nodes. Also we don't skip hiden neighbors since that gives
    better results. That wasn't as easy with the old API.

--- source/blender/editors/sculpt_paint/mesh_brush_common.hh
+++ source/blender/editors/sculpt_paint/mesh_brush_common.hh
@@ -96,6 +96,24 @@ void gather_grids_normals(const SubdivCCG &subdiv_ccg,
                           MutableSpan<float3> normals);
 void gather_bmesh_normals(const Set<BMVert *, 0> &verts, MutableSpan<float3> normals);
 
+/** Gather data from an array aligned with all geometry vertices. */
+void gather_grids_data_from_array(const SubdivCCG &subdiv_ccg,
+                                  Span<float3> array,
+                                  Span<int> grids,
+                                  MutableSpan<float3> node_data);
+void gather_bmesh_vert_data_from_array(Span<float3> array,
+                                       const Set<BMVert *, 0> &verts,
+                                       MutableSpan<float3> node_data);
+
+/** Scatter data from an array of the node's data to the referenced geometry vertices. */
+void scatter_grids_data_to_array(const SubdivCCG &subdiv_ccg,
+                                 Span<float3> node_data,
+                                 Span<int> grids,
+                                 MutableSpan<float3> array);
+void scatter_bmesh_vert_data_to_array(Span<float3> node_data,
+                                      const Set<BMVert *, 0> &verts,
+                                      MutableSpan<float3> array);
+
 /**
  * Calculate initial influence factors based on vertex visibility.
  */
