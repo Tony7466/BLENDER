c7a76ce5d73724902a88c60714123e26c298db76
Nathan Vegdahl <cessen@cessen.com>
2024-07-12 15:40:25 +0200

    Remove unnecessary const from function declarations

--- source/blender/animrig/ANIM_fcurve.hh
+++ source/blender/animrig/ANIM_fcurve.hh
@@ -50,8 +50,8 @@ KeyframeSettings get_keyframe_settings(bool from_userprefs);
  *
  * If no matching fcurve is found, returns nullptr.
  */
-const FCurve *fcurve_find(Span<const FCurve *> fcurves, const FCurveDescriptor fcurve_descriptor);
-FCurve *fcurve_find(Span<FCurve *> fcurves, const FCurveDescriptor fcurve_descriptor);
+const FCurve *fcurve_find(Span<const FCurve *> fcurves, FCurveDescriptor fcurve_descriptor);
+FCurve *fcurve_find(Span<FCurve *> fcurves, FCurveDescriptor fcurve_descriptor);
 
 /**
  * Create an fcurve for a specific channel, pre-set-up with default flags and
