904fe67717fdb979c5c1a92ffff3017a2d15e619
Laurynas Duburas <laduem@gmail.com>
2024-07-13 20:47:27 +0300

    collisions added

--- source/blender/draw/engines/overlay/overlay_next_bounds.hh
+++ source/blender/draw/engines/overlay/overlay_next_bounds.hh
@@ -13,6 +13,8 @@
 #include "BLI_bounds_types.hh"
 #include "BLI_utildefines.h"
 
+#include "DNA_rigidbody_types.h"
+
 #include "overlay_next_private.hh"
 
 namespace blender::draw::overlay {
@@ -55,76 +57,97 @@ class Bounds {
 
   void object_sync(const ObjectRef &ob_ref, Resources &res, const State &state)
   {
-    const bool around_origin = false;
     const Object *ob = ob_ref.object;
     const bool from_dupli = (ob->base_flag & (BASE_FROM_SET | BASE_FROM_DUPLI)) != 0;
     const bool has_bounds = !ELEM(
         ob->type, OB_LAMP, OB_CAMERA, OB_EMPTY, OB_SPEAKER, OB_LIGHTPROBE);
     const bool draw_bounds = has_bounds && ((ob->dt == OB_BOUNDBOX) ||
                                             ((ob->dtx & OB_DRAWBOUNDOX) && !from_dupli));
-    if (!draw_bounds || (ob_ref.object->type == OB_MBALL && !BKE_mball_is_basis(ob))) {
-      return;
-    }
-    const float4x4 object_mat{ob->object_to_world().ptr()};
-    const blender::Bounds<float3> bounds = BKE_object_boundbox_get(ob).value_or(
-        blender::Bounds(float3(-1.0f), float3(1.0f)));
-
     const float4 color = res.object_wire_color(ob_ref, state);
-    const float3 size = (bounds.max - bounds.min) * 0.5f;
-    const float3 center = around_origin ? float3(0) : math::midpoint(bounds.min, bounds.max);
-    const select::ID select_id = res.select_id(ob_ref);
-
     CallBuffers &call_bufs = call_buffers_[int((ob_ref.object->dtx & OB_DRAW_IN_FRONT) != 0)];
 
-    switch (ob->boundtype) {
-      case OB_BOUND_BOX: {
-        float4x4 scale = math::from_scale<float4x4>(size);
-        scale.location() = center;
-        ExtraInstanceData data(object_mat * scale, color, 1.0f);
-        call_bufs.box.append(data, select_id);
-        break;
+    auto add_bounds = [&](const bool around_origin, const char bound_type) {
+      if (ob->type == OB_MBALL && !BKE_mball_is_basis(ob)) {
+        return;
       }
-      case OB_BOUND_SPHERE: {
-        float4x4 scale = math::from_scale<float4x4>(float3{math::reduce_max(size)});
-        scale.location() = center;
-        ExtraInstanceData data(object_mat * scale, color, 1.0f);
-        call_bufs.sphere.append(data, select_id);
-        break;
+      const float4x4 object_mat{ob->object_to_world().ptr()};
+      const blender::Bounds<float3> bounds = BKE_object_boundbox_get(ob).value_or(
+          blender::Bounds(float3(-1.0f), float3(1.0f)));
+      const float3 size = (bounds.max - bounds.min) * 0.5f;
+      const float3 center = around_origin ? float3(0) : math::midpoint(bounds.min, bounds.max);
+      const select::ID select_id = res.select_id(ob_ref);
+
+      switch (bound_type) {
+        case OB_BOUND_BOX: {
+          float4x4 scale = math::from_scale<float4x4>(size);
+          scale.location() = center;
+          ExtraInstanceData data(object_mat * scale, color, 1.0f);
+          call_bufs.box.append(data, select_id);
+          break;
+        }
+        case OB_BOUND_SPHERE: {
+          float4x4 scale = math::from_scale<float4x4>(float3{math::reduce_max(size)});
+          scale.location() = center;
+          ExtraInstanceData data(object_mat * scale, color, 1.0f);
+          call_bufs.sphere.append(data, select_id);
+          break;
+        }
+        case OB_BOUND_CYLINDER: {
+          float4x4 scale = math::from_scale<float4x4>(
+              float3{float2{math::max(size.x, size.y)}, size.z});
+          scale.location() = center;
+          ExtraInstanceData data(object_mat * scale, color, 1.0f);
+          call_bufs.cylinder.append(data, select_id);
+          break;
+        }
+        case OB_BOUND_CONE: {
+          float4x4 mat = math::from_scale<float4x4>(
+              float3{float2{math::max(size.x, size.y)}, size.z});
+          mat.location() = center;
+          /* Cone batch has base at 0 and is pointing towards +Y. */
+          std::swap(mat[1], mat[2]);
+          mat.location().z -= size.z;
+          ExtraInstanceData data(object_mat * mat, color, 1.0f);
+          call_bufs.cone.append(data, select_id);
+          break;
+        }
+        case OB_BOUND_CAPSULE: {
+          float4x4 mat = math::from_scale<float4x4>(float3{math::max(size.x, size.y)});
+          mat.location() = center;
+          mat.location().z = center.z + std::max(0.0f, size.z - size.x);
+          ExtraInstanceData data(object_mat * mat, color, 1.0f);
+          call_bufs.capsule_cap.append(data, select_id);
+          mat.z_axis() *= -1;
+          mat.location().z = center.z - std::max(0.0f, size.z - size.x);
+          data.object_to_world_ = object_mat * mat;
+          call_bufs.capsule_cap.append(data, select_id);
+          mat.z_axis().z = std::max(0.0f, size.z * 2.0f - size.x * 2.0f);
+          data.object_to_world_ = object_mat * mat;
+          call_bufs.capsule_body.append(data, select_id);
+          break;
+        }
       }
-      case OB_BOUND_CYLINDER: {
-        float4x4 scale = math::from_scale<float4x4>(
-            float3{float2{math::max(size.x, size.y)}, size.z});
-        scale.location() = center;
-        ExtraInstanceData data(object_mat * scale, color, 1.0f);
-        call_bufs.cylinder.append(data, select_id);
-        break;
-      }
-      case OB_BOUND_CONE: {
-        float4x4 mat = math::from_scale<float4x4>(
-            float3{float2{math::max(size.x, size.y)}, size.z});
-        mat.location() = center;
-        /* Cone batch has base at 0 and is pointing towards +Y. */
-        std::swap(mat[1], mat[2]);
-        mat.location().z -= size.z;
-        ExtraInstanceData data(object_mat * mat, color, 1.0f);
-        call_bufs.cone.append(data, select_id);
-        break;
-      }
-      case OB_BOUND_CAPSULE: {
-        float4x4 mat = math::from_scale<float4x4>(float3{math::max(size.x, size.y)});
-        mat.location() = center;
-        mat.location().z = center.z + std::max(0.0f, size.z - size.x);
-        ExtraInstanceData data(object_mat * mat, color, 1.0f);
-        call_bufs.capsule_cap.append(data, select_id);
-        mat.z_axis() *= -1;
-        mat.location().z = center.z - std::max(0.0f, size.z - size.x);
-        data.object_to_world_ = object_mat * mat;
-        call_bufs.capsule_cap.append(data, select_id);
-        mat.z_axis().z = std::max(0.0f, size.z * 2.0f - size.x * 2.0f);
-        data.object_to_world_ = object_mat * mat;
-        call_bufs.capsule_body.append(data, select_id);
-        break;
-      }
+    };
+
+    if (draw_bounds) {
+      add_bounds(false, ob->boundtype);
+    }
+    if (!from_dupli && ob->rigidbody_object != nullptr) {
+      const char bound_type = [&]() {
+        switch (ob->rigidbody_object->shape) {
+          case RB_SHAPE_BOX:
+            return OB_BOUND_BOX;
+          case RB_SHAPE_SPHERE:
+            return OB_BOUND_SPHERE;
+          case RB_SHAPE_CONE:
+            return OB_BOUND_CONE;
+          case RB_SHAPE_CYLINDER:
+            return OB_BOUND_CYLINDER;
+          case RB_SHAPE_CAPSULE:
+            return OB_BOUND_CAPSULE;
+        }
+      }();
+      add_bounds(true, bound_type);
     }
   }
 
