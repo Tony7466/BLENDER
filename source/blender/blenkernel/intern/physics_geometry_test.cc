0641f21216b0dabcff6b49177ef753a7a3a51178
Lukas Tönne <lukas@blender.org>
2024-07-16 12:39:48 +0200

    Universal data test function for physics tests.

--- source/blender/blenkernel/intern/physics_geometry_test.cc
+++ source/blender/blenkernel/intern/physics_geometry_test.cc
@@ -8,64 +8,51 @@
 
 namespace blender::bke::tests {
 
-TEST(PhysicsGeometryTest, construct)
+class PhysicsGeometryTest : public testing::Test {
+ protected:
+  PhysicsGeometryTest() {}
+
+  void test_data(const bke::PhysicsGeometry &physics,
+                 const bool has_world,
+                 const int bodies_num,
+                 const int constraints_num,
+                 const int shapes_num)
+  {
+    EXPECT_EQ(physics.has_world(), has_world);
+    EXPECT_EQ(physics.bodies_num(), bodies_num);
+    EXPECT_EQ(physics.constraints_num(), constraints_num);
+    EXPECT_EQ(physics.shapes_num(), shapes_num);
+  }
+};
+
+TEST_F(PhysicsGeometryTest, construct)
 {
   bke::PhysicsGeometry geo1 = bke::PhysicsGeometry();
-  EXPECT_EQ(geo1.bodies_num(), 0);
-  EXPECT_EQ(geo1.constraints_num(), 0);
-  EXPECT_EQ(geo1.shapes_num(), 0);
-  EXPECT_EQ(geo1.has_world(), false);
+  test_data(geo1, false, 0, 0, 0);
 
   bke::PhysicsGeometry geo2 = bke::PhysicsGeometry(1, 0, 0);
-  EXPECT_EQ(geo2.bodies_num(), 1);
-  EXPECT_EQ(geo2.constraints_num(), 0);
-  EXPECT_EQ(geo2.shapes_num(), 0);
-  EXPECT_EQ(geo2.has_world(), false);
+  test_data(geo2, false, 1, 0, 0);
 
   bke::PhysicsGeometry geo3 = bke::PhysicsGeometry(0, 1, 0);
-  EXPECT_EQ(geo3.bodies_num(), 0);
-  EXPECT_EQ(geo3.constraints_num(), 1);
-  EXPECT_EQ(geo3.shapes_num(), 0);
-  EXPECT_EQ(geo3.has_world(), false);
+  test_data(geo3, false, 0, 1, 0);
 
   bke::PhysicsGeometry geo4 = bke::PhysicsGeometry(0, 0, 1);
-  EXPECT_EQ(geo4.bodies_num(), 0);
-  EXPECT_EQ(geo4.constraints_num(), 0);
-  EXPECT_EQ(geo4.shapes_num(), 1);
-  EXPECT_EQ(geo4.has_world(), false);
+  test_data(geo4, false, 0, 0, 1);
 
   bke::PhysicsGeometry geo5 = bke::PhysicsGeometry(1, 1, 1);
-  EXPECT_EQ(geo5.bodies_num(), 1);
-  EXPECT_EQ(geo5.constraints_num(), 1);
-  EXPECT_EQ(geo5.shapes_num(), 1);
-  EXPECT_EQ(geo5.has_world(), false);
-
-  bke::PhysicsGeometry geo6 = bke::PhysicsGeometry(geo5);
-  EXPECT_EQ(geo6.bodies_num(), 1);
-  EXPECT_EQ(geo6.constraints_num(), 1);
-  EXPECT_EQ(geo6.shapes_num(), 1);
-  EXPECT_EQ(geo6.has_world(), false);
+  test_data(geo5, false, 1, 1, 1);
 }
 
-TEST(PhysicsGeometryTest, create_and_remove_world)
+TEST_F(PhysicsGeometryTest, create_and_remove_world)
 {
   bke::PhysicsGeometry geo = bke::PhysicsGeometry(5, 2, 3);
-  EXPECT_EQ(geo.bodies_num(), 5);
-  EXPECT_EQ(geo.constraints_num(), 2);
-  EXPECT_EQ(geo.shapes_num(), 3);
-  EXPECT_EQ(geo.has_world(), false);
+  test_data(geo, false, 5, 2, 3);
 
   geo.create_world();
-  EXPECT_EQ(geo.bodies_num(), 5);
-  EXPECT_EQ(geo.constraints_num(), 2);
-  EXPECT_EQ(geo.shapes_num(), 3);
-  EXPECT_EQ(geo.has_world(), true);
+  test_data(geo, true, 5, 2, 3);
 
   geo.destroy_world();
-  EXPECT_EQ(geo.bodies_num(), 5);
-  EXPECT_EQ(geo.constraints_num(), 2);
-  EXPECT_EQ(geo.shapes_num(), 3);
-  EXPECT_EQ(geo.has_world(), false);
+  test_data(geo, false, 5, 2, 3);
 }
 
 }  // namespace blender::bke::tests
