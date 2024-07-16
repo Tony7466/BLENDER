Tony7466/BLENDER

commit 525293a95db0c807a64abf4682d6ba784eba2ba3

Fix: MEM_new/MEM_freeN mismatch in file browser code
Caused by cd84d3f3da197b12b56d766ee469c5fc1c368a21

source/blender/editors/space_file/folder_history.cc
@@ -113,10 +113,10 @@ bool folderlist_clear_next(SpaceFile *sfile)
 void folderlist_free(ListBase *folderlist)
 {
   if (folderlist) {
-    LISTBASE_FOREACH (FolderList *, folder, folderlist) {
+    LISTBASE_FOREACH_MUTABLE (FolderList *, folder, folderlist) {
       MEM_freeN(folder->foldername);
+      MEM_delete(folder);
     }
-    BLI_freelistN(folderlist);
   }
 }
 

commit 7df8eeb32fd3062a14fb17f0c824d44ca50fdf92

Fix #124770: File Browser: Crash when going in and out of folders
Caused by 525293a95d
Crash due to attempt of double freeing. Fix is to make first and
last nullptr of list after freeing the memory. This avoids execution of
`folderlist_free` (`if(folderlist_clear_next())` is false)

source/blender/editors/space_file/folder_history.cc
@@ -117,6 +117,7 @@ void folderlist_free(ListBase *folderlist)
       MEM_freeN(folder->foldername);
       MEM_delete(folder);
     }
+    BLI_listbase_clear(folderlist);
   }
 }
 