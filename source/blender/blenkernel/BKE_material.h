Tony7466/BLENDER

commit 296d05060d22714a5fe46af7206aa7066982e9c8

Fix #123560: Select by active material fails in multi-object-edit mode
This was the case when the slot index in the active object is greater
than the available slots of other selected objects.

Code was trying to avoid searching all materials by using
`BKE_object_material_get` to get an appropriate index (an addition to
D4441 added in 6b39dc7672eb). That function has the behavior of clamping
if a target index is greater than the available slots (for good
reasons), so we cant rely on the slot index being the same if
`BKE_object_material_get` finds a material. So in essence, this is not
what we want to use in this case.

 Now use the much simpler `BKE_object_material_index_get` to get the
index [this might be less performant in certain scenarios, dont think
this is critical though and correctness should beat performance here]

Pull Request: https://projects.blender.org/blender/blender/pulls/123718

source/blender/blenkernel/BKE_material.h
@@ -114,7 +114,7 @@ bool BKE_object_material_slot_add(struct Main *bmain, struct Object *ob);
 bool BKE_object_material_slot_remove(struct Main *bmain, struct Object *ob);
 bool BKE_object_material_slot_used(struct Object *object, short actcol);
 
-int BKE_object_material_index_get(Object *ob, Material *ma);
+int BKE_object_material_index_get(Object *ob, const Material *ma);
 int BKE_object_material_ensure(Main *bmain, Object *ob, Material *material);
 
 struct Material *BKE_gpencil_material(struct Object *ob, short act);

source/blender/blenkernel/intern/material.cc
@@ -831,7 +831,7 @@ void BKE_id_material_eval_ensure_default_slot(ID *id)
   }
 }
 
-int BKE_object_material_index_get(Object *ob, Material *ma)
+int BKE_object_material_index_get(Object *ob, const Material *ma)
 {
   short *totcol = BKE_object_material_len_p(ob);
   Material *read_ma = nullptr;

source/blender/editors/render/render_shading.cc
@@ -391,29 +391,14 @@ static int material_slot_de_select(bContext *C, bool select)
 
   Vector<Object *> objects = object_array_for_shading_edit_mode_enabled(C);
   for (Object *ob : objects) {
-    short mat_nr_active = -1;
-
     if (ob->totcol == 0) {
       continue;
     }
-    if (obact && (mat_active == BKE_object_material_get(ob, obact->actcol))) {
-      /* Avoid searching since there may be multiple slots with the same material.
-       * For the active object or duplicates: match the material slot index first. */
-      mat_nr_active = obact->actcol - 1;
-    }
-    else {
-      /* Find the first matching material.
-       * NOTE: there may be multiple but that's not a common use case. */
-      for (int i = 0; i < ob->totcol; i++) {
-        const Material *mat = BKE_object_material_get(ob, i + 1);
-        if (mat_active == mat) {
-          mat_nr_active = i;
-          break;
-        }
-      }
-      if (mat_nr_active == -1) {
-        continue;
-      }
+
+    short mat_nr_active = BKE_object_material_index_get(ob, mat_active);
+
+    if (mat_nr_active == -1) {
+      continue;
     }
 
     bool changed = false;

commit 2d30aa3d96998f1b90390660e331e941df138db8

Fix #125353: Select by active material fails to select the selected slot
Caused by 296d05060d22

This was the case if multiple slots had the same material, then the
first corresponding slot was taken (instead of the preferred active).

Now restore the special behavior from 6b39dc7672eb but without the issue
of clamping the slot index (which was the problem in #123560). This is
done by introducing a non-clamping version of `BKE_object_material_get`.

source/blender/blenkernel/BKE_material.h
@@ -84,8 +84,12 @@ enum {
   BKE_MAT_ASSIGN_OBJECT,
 };
 
-struct Material **BKE_object_material_get_p(struct Object *ob, short act);
+struct Material **BKE_object_material_get_p(struct Object *ob, short act, bool clamp_slot_index);
 struct Material *BKE_object_material_get(struct Object *ob, short act);
+/**
+ * \note Same as #BKE_object_material_get but not clamping slot index.
+ */
+struct Material *BKE_object_material_get_no_clamp(struct Object *ob, short act);
 void BKE_id_material_assign(struct Main *bmain, struct ID *id, struct Material *ma, short act);
 void BKE_object_material_assign(
     struct Main *bmain, struct Object *ob, struct Material *ma, short act, int assign_type);

source/blender/blenkernel/intern/material.cc
@@ -679,7 +679,7 @@ void BKE_id_material_clear(Main *bmain, ID *id)
   }
 }
 
-Material **BKE_object_material_get_p(Object *ob, short act)
+Material **BKE_object_material_get_p(Object *ob, short act, bool clamp_slot_index)
 {
   Material ***matarar, **ma_p;
   const short *totcolp;
@@ -695,7 +695,7 @@ Material **BKE_object_material_get_p(Object *ob, short act)
   }
 
   /* Clamp to number of slots if index is out of range, same convention as used for rendering. */
-  const int slot_index = clamp_i(act - 1, 0, *totcolp - 1);
+  const int slot_index = clamp_slot_index ? clamp_i(act - 1, 0, *totcolp - 1) : act - 1;
 
   /* Fix inconsistency which may happen when library linked data reduces the number of
    * slots but object was not updated. Ideally should be fixed elsewhere. */
@@ -724,7 +724,13 @@ Material **BKE_object_material_get_p(Object *ob, short act)
 
 Material *BKE_object_material_get(Object *ob, short act)
 {
-  Material **ma_p = BKE_object_material_get_p(ob, act);
+  Material **ma_p = BKE_object_material_get_p(ob, act, true);
+  return ma_p ? *ma_p : nullptr;
+}
+
+Material *BKE_object_material_get_no_clamp(Object *ob, short act)
+{
+  Material **ma_p = BKE_object_material_get_p(ob, act, false);
   return ma_p ? *ma_p : nullptr;
 }
 

source/blender/editors/render/render_shading.cc
@@ -396,7 +396,15 @@ static int material_slot_de_select(bContext *C, bool select)
       continue;
     }
 
-    short mat_nr_active = BKE_object_material_index_get(ob, mat_active);
+    short mat_nr_active = -1;
+    if (obact && (mat_active == BKE_object_material_get_no_clamp(ob, obact->actcol))) {
+      /* Avoid searching since there may be multiple slots with the same material.
+       * For the active object or duplicates: match the material slot index first. */
+      mat_nr_active = obact->actcol - 1;
+    }
+    else {
+      mat_nr_active = BKE_object_material_index_get(ob, mat_active);
+    }
 
     if (mat_nr_active == -1) {
       continue;
@@ -777,7 +785,7 @@ static int new_material_exec(bContext *C, wmOperator * /*op*/)
     if (ob != nullptr) {
       /* Add slot follows user-preferences for creating new slots,
        * RNA pointer assignment doesn't, see: #60014. */
-      if (BKE_object_material_get_p(ob, ob->actcol) == nullptr) {
+      if (BKE_object_material_get_p(ob, ob->actcol, true) == nullptr) {
         BKE_object_material_slot_add(bmain, ob);
       }
     }