d6eca9e060459e1acd39efd1c53cc1a445f94ed3
Jacques Lucke <jacques@blender.org>
2024-07-31 18:27:45 +0200

    fix

--- source/blender/makesdna/intern/dna_genfile.cc
+++ source/blender/makesdna/intern/dna_genfile.cc
@@ -1729,6 +1729,9 @@ int DNA_struct_alignment(const SDNA *sdna, const int struct_nr)
 
 const char *DNA_struct_identifier(struct SDNA *sdna, const int struct_index)
 {
+  if (struct_index == 0) {
+    return "Raw data or DrawDataList";
+  }
   DNA_sdna_alias_data_ensure(sdna);
   const SDNA_Struct *struct_info = sdna->structs[struct_index];
   return sdna->alias.types[struct_info->type];
