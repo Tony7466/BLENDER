9d05d1bf479ede0e9bcd3cbbc990a4d64dff7e22
Sean Kim <SeanCTKim@protonmail.com>
2024-07-31 16:05:16 -0700

    WIP

---
+++ source/blender/editors/sculpt_paint/brushes/boundary.cc
@@ -0,0 +1,463 @@
+/* SPDX-FileCopyrightText: 2024 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+#include "BKE_brush.hh"
+#include "editors/sculpt_paint/brushes/types.hh"
+
+#include "DNA_brush_types.h"
+#include "DNA_mesh_types.h"
+
+#include "BKE_mesh.hh"
+#include "BKE_pbvh.hh"
+#include "BKE_subdiv_ccg.hh"
+
+#include "BLI_enumerable_thread_specific.hh"
+#include "BLI_math_vector.hh"
+#include "BLI_task.hh"
+
+#include "editors/sculpt_paint/mesh_brush_common.hh"
+#include "editors/sculpt_paint/paint_intern.hh"
+#include "editors/sculpt_paint/sculpt_intern.hh"
+
+namespace blender::ed::sculpt_paint {
+
+inline namespace boundary_cc {
+
+};  // namespace boundary_cc
+
+static void init_falloff(const Brush &brush, const float radius, SculptBoundary &boundary)
+{
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num != -1) {
+      boundary.edit_info[i].strength_factor = BKE_brush_curve_strength(
+          &brush, boundary.edit_info[i].propagation_steps_num, boundary.max_propagation_steps);
+    }
+
+    if (boundary.edit_info[i].original_vertex_i == boundary.initial_vert_i) {
+      /* All vertices that are propagated from the original vertex won't be affected by the
+       * boundary falloff, so there is no need to calculate anything else. */
+      continue;
+    }
+
+    const bool use_boundary_distances = brush.boundary_falloff_type !=
+                                        BRUSH_BOUNDARY_FALLOFF_CONSTANT;
+
+    if (!use_boundary_distances) {
+      /* There are falloff modes that do not require to modify the previously calculated falloff
+       * based on boundary distances. */
+      continue;
+    }
+
+    const float boundary_distance = boundary.distance.lookup_default(
+        boundary.edit_info[i].original_vertex_i, 0.0f);
+    float falloff_distance = 0.0f;
+    float direction = 1.0f;
+
+    switch (brush.boundary_falloff_type) {
+      case BRUSH_BOUNDARY_FALLOFF_RADIUS:
+        falloff_distance = boundary_distance;
+        break;
+      case BRUSH_BOUNDARY_FALLOFF_LOOP: {
+        const int div = boundary_distance / radius;
+        const float mod = fmodf(boundary_distance, radius);
+        falloff_distance = div % 2 == 0 ? mod : radius - mod;
+        break;
+      }
+      case BRUSH_BOUNDARY_FALLOFF_LOOP_INVERT: {
+        const int div = boundary_distance / radius;
+        const float mod = fmodf(boundary_distance, radius);
+        falloff_distance = div % 2 == 0 ? mod : radius - mod;
+        /* Inverts the falloff in the intervals 1 2 5 6 9 10 ... etc. */
+        if (((div - 1) & 2) == 0) {
+          direction = -1.0f;
+        }
+        break;
+      }
+      case BRUSH_BOUNDARY_FALLOFF_CONSTANT:
+        /* For constant falloff distances are not allocated, so this should never happen. */
+        BLI_assert(false);
+        break;
+      default:
+        BLI_assert_unreachable();
+        break;
+    }
+
+    boundary.edit_info[i].strength_factor *= direction * BKE_brush_curve_strength(
+                                                             &brush, falloff_distance, radius);
+  }
+}
+
+static void bend_data_init_mesh(const Span<float3> vert_positions,
+                                const Span<float3> vert_normals,
+                                SculptBoundary &boundary)
+{
+  boundary.bend.pivot_rotation_axis = Array<float3>(boundary.edit_info.size(), float3(0));
+  boundary.bend.pivot_positions = Array<float3>(boundary.edit_info.size(), float3(0));
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num != boundary.max_propagation_steps) {
+      continue;
+    }
+
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+
+    const float3 normal = vert_normals[i];
+    const float3 dir = vert_positions[orig_vert_i] - vert_positions[i];
+    boundary.bend.pivot_rotation_axis[orig_vert_i] = math::normalize(math::cross(dir, normal));
+    boundary.bend.pivot_positions[orig_vert_i] = vert_positions[i];
+  }
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num == BOUNDARY_STEPS_NONE) {
+      continue;
+    }
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+
+    boundary.bend.pivot_positions[i] = boundary.bend.pivot_positions[orig_vert_i];
+    boundary.bend.pivot_rotation_axis[i] = boundary.bend.pivot_rotation_axis[orig_vert_i];
+  }
+}
+
+static void bend_data_init_grids(const SubdivCCG &subdiv_ccg, SculptBoundary &boundary)
+{
+  const CCGKey &key = BKE_subdiv_ccg_key_top_level(subdiv_ccg);
+  Span<CCGElem *> grids = subdiv_ccg.grids;
+
+  boundary.bend.pivot_rotation_axis = Array<float3>(boundary.edit_info.size(), float3(0));
+  boundary.bend.pivot_positions = Array<float3>(boundary.edit_info.size(), float3(0));
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num != boundary.max_propagation_steps) {
+      continue;
+    }
+
+    const SubdivCCGCoord vert = SubdivCCGCoord::from_index(key, i);
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+    const SubdivCCGCoord orig_vert = SubdivCCGCoord::from_index(key, orig_vert_i);
+
+    const float3 normal = CCG_grid_elem_no(key, grids[vert.grid_index], vert.x, vert.y);
+    const float3 dir = CCG_grid_elem_co(
+                           key, grids[orig_vert.grid_index], orig_vert.x, orig_vert.y) -
+                       CCG_grid_elem_co(key, grids[vert.grid_index], vert.x, vert.y);
+    boundary.bend.pivot_rotation_axis[orig_vert_i] = math::normalize(math::cross(dir, normal));
+    boundary.bend.pivot_positions[orig_vert_i] = CCG_grid_elem_co(
+        key, grids[vert.grid_index], vert.x, vert.y);
+  }
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num == BOUNDARY_STEPS_NONE) {
+      continue;
+    }
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+
+    boundary.bend.pivot_positions[i] = boundary.bend.pivot_positions[orig_vert_i];
+    boundary.bend.pivot_rotation_axis[i] = boundary.bend.pivot_rotation_axis[orig_vert_i];
+  }
+}
+
+static void bend_data_init_bmesh(BMesh *bm, SculptBoundary &boundary)
+{
+  boundary.bend.pivot_rotation_axis = Array<float3>(boundary.edit_info.size(), float3(0));
+  boundary.bend.pivot_positions = Array<float3>(boundary.edit_info.size(), float3(0));
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num != boundary.max_propagation_steps) {
+      continue;
+    }
+
+    BMVert *vert = BM_vert_at_index(bm, i);
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+    BMVert *orig_vert = BM_vert_at_index(bm, orig_vert_i);
+
+    const float3 normal = vert->no;
+    const float3 dir = float3(orig_vert->co) - float3(vert->co);
+    boundary.bend.pivot_rotation_axis[orig_vert_i] = math::normalize(math::cross(dir, normal));
+    boundary.bend.pivot_positions[boundary.edit_info[i].original_vertex_i] = vert->co;
+  }
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num == BOUNDARY_STEPS_NONE) {
+      continue;
+    }
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+    boundary.bend.pivot_positions[i] = boundary.bend.pivot_positions[orig_vert_i];
+    boundary.bend.pivot_rotation_axis[i] = boundary.bend.pivot_rotation_axis[orig_vert_i];
+  }
+}
+
+static void slide_data_init_mesh(const Span<float3> vert_positions, SculptBoundary &boundary)
+{
+  boundary.slide.directions = Array<float3>(boundary.edit_info.size(), float3(0));
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num != boundary.max_propagation_steps) {
+      continue;
+    }
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+    boundary.slide.directions[orig_vert_i] = math::normalize(vert_positions[orig_vert_i] -
+                                                             vert_positions[i]);
+  }
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num == BOUNDARY_STEPS_NONE) {
+      continue;
+    }
+    boundary.slide.directions[i] =
+        boundary.slide.directions[boundary.edit_info[i].original_vertex_i];
+  }
+}
+
+static void slide_data_init_grids(const SubdivCCG &subdiv_ccg, SculptBoundary &boundary)
+{
+  const CCGKey &key = BKE_subdiv_ccg_key_top_level(subdiv_ccg);
+  Span<CCGElem *> grids = subdiv_ccg.grids;
+
+  boundary.slide.directions = Array<float3>(boundary.edit_info.size(), float3(0));
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num != boundary.max_propagation_steps) {
+      continue;
+    }
+    const SubdivCCGCoord vert = SubdivCCGCoord::from_index(key, i);
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+    const SubdivCCGCoord orig_vert = SubdivCCGCoord::from_index(key, orig_vert_i);
+
+    boundary.slide.directions[orig_vert_i] = math::normalize(
+        CCG_grid_elem_co(key, grids[orig_vert.grid_index], orig_vert.x, orig_vert.y) -
+        CCG_grid_elem_co(key, grids[vert.grid_index], vert.x, vert.y));
+  }
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num == BOUNDARY_STEPS_NONE) {
+      continue;
+    }
+    boundary.slide.directions[i] =
+        boundary.slide.directions[boundary.edit_info[i].original_vertex_i];
+  }
+}
+
+static void slide_data_init_bmesh(BMesh *bm, SculptBoundary &boundary)
+{
+  boundary.slide.directions = Array<float3>(boundary.edit_info.size(), float3(0));
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num != boundary.max_propagation_steps) {
+      continue;
+    }
+    BMVert *vert = BM_vert_at_index(bm, i);
+    const int orig_vert_i = boundary.edit_info[i].original_vertex_i;
+    BMVert *orig_vert = BM_vert_at_index(bm, orig_vert_i);
+    boundary.slide.directions[orig_vert_i] = math::normalize(float3(orig_vert->co) -
+                                                             float3(vert->co));
+  }
+
+  for (const int i : boundary.edit_info.index_range()) {
+    if (boundary.edit_info[i].propagation_steps_num == BOUNDARY_STEPS_NONE) {
+      continue;
+    }
+    boundary.slide.directions[i] =
+        boundary.slide.directions[boundary.edit_info[i].original_vertex_i];
+  }
+}
+
+static void populate_twist_data(const Span<float3> positions, SculptBoundary &boundary)
+{
+  boundary.twist.pivot_position = float3(0);
+  for (const float3 &position : positions) {
+    boundary.twist.pivot_position += position;
+  }
+  boundary.twist.pivot_position *= 1.0f / boundary.verts.size();
+  boundary.twist.rotation_axis = math::normalize(boundary.pivot_position -
+                                                 boundary.initial_vert_position);
+}
+
+static void twist_data_init_mesh(const Span<float3> vert_positions, SculptBoundary &boundary)
+{
+  Array<float3> positions(boundary.verts.size());
+  array_utils::gather(vert_positions, boundary.verts.as_span(), positions.as_mutable_span());
+  populate_twist_data(positions, boundary);
+}
+
+static void twist_data_init_grids(const SubdivCCG &subdiv_ccg, SculptBoundary &boundary)
+{
+  const CCGKey &key = BKE_subdiv_ccg_key_top_level(subdiv_ccg);
+  const Span<CCGElem *> grids = subdiv_ccg.grids;
+
+  Array<float3> positions(boundary.verts.size());
+  for (const int i : positions.index_range()) {
+    const SubdivCCGCoord vert = SubdivCCGCoord::from_index(key, boundary.verts[i]);
+    positions[i] = CCG_grid_elem_co(key, grids[vert.grid_index], vert.x, vert.y);
+  }
+  populate_twist_data(positions, boundary);
+}
+
+static void twist_data_init_bmesh(BMesh *bm, SculptBoundary &boundary)
+{
+  Array<float3> positions(boundary.verts.size());
+  for (const int i : positions.index_range()) {
+    BMVert *vert = BM_vert_at_index(bm, i);
+    positions[i] = vert->co;
+  }
+  populate_twist_data(positions, boundary);
+}
+
+static void init_boundary_mesh(Object &object,
+                               const Brush &brush,
+                               const ePaintSymmetryFlags symm_area)
+{
+  const SculptSession &ss = *object.sculpt;
+  const bke::pbvh::Tree &pbvh = *ss.pbvh;
+
+  const Mesh &mesh = *static_cast<const Mesh *>(object.data);
+  const bke::AttributeAccessor attributes = mesh.attributes();
+  VArraySpan<bool> hide_vert = *attributes.lookup<bool>(".hide_vert", bke::AttrDomain::Point);
+
+  const Span<float3> positions_eval = BKE_pbvh_get_vert_positions(pbvh);
+
+  /* TODO: Remove PBVHVertRef here once we decide how we are storing the active_vertex value. */
+  PBVHVertRef initial_vert_ref = SCULPT_active_vertex_get(ss);
+  if (initial_vert_ref.i == PBVH_REF_NONE) {
+    return;
+  }
+
+  std::optional<int> initial_vert;
+  if (ss.cache->mirror_symmetry_pass == 0) {
+    initial_vert = initial_vert_ref.i;
+  }
+  else {
+    float3 location;
+    flip_v3_v3(location, positions_eval[initial_vert_ref.i], symm_area);
+    initial_vert = nearest_vert_calc_mesh(
+        *ss.pbvh, positions_eval, hide_vert, location, ss.cache->radius_squared, false);
+  }
+
+  if (!initial_vert) {
+    return;
+  }
+
+  ss.cache->boundaries[symm_area] = boundary::data_init_mesh(
+      object, &brush, *initial_vert, ss.cache->initial_radius);
+
+  if (ss.cache->boundaries[symm_area]) {
+    switch (brush.boundary_deform_type) {
+      case BRUSH_BOUNDARY_DEFORM_BEND:
+        bend_data_init_mesh(object, *ss.cache->boundaries[symm_area]);
+        break;
+      case BRUSH_BOUNDARY_DEFORM_EXPAND:
+        slide_data_init_mesh(object, *ss.cache->boundaries[symm_area]);
+        break;
+      case BRUSH_BOUNDARY_DEFORM_TWIST:
+        twist_data_init_mesh(object, *ss.cache->boundaries[symm_area]);
+        break;
+      case BRUSH_BOUNDARY_DEFORM_INFLATE:
+      case BRUSH_BOUNDARY_DEFORM_GRAB:
+        /* Do nothing. These deform modes don't need any extra data to be precomputed. */
+        break;
+      default:
+        BLI_assert_unreachable();
+        break;
+    }
+
+    init_falloff(brush, ss.cache->initial_radius, *ss.cache->boundaries[symm_area]);
+  }
+}
+
+static void init_boundary_grids() {}
+
+static void init_boundary_bmesh() {}
+
+void do_boundary_brush(const Sculpt &sd, Object &ob, Span<bke::pbvh::Node *> nodes)
+{
+  SculptSession &ss = *ob.sculpt;
+  const Brush &brush = *BKE_paint_brush_for_read(&sd.paint);
+
+  const ePaintSymmetryFlags symm_area = ss.cache->mirror_symmetry_pass;
+  if (SCULPT_stroke_is_first_brush_step_of_symmetry_pass(*ss.cache)) {
+
+    PBVHVertRef initial_vert;
+
+    if (ss.cache->mirror_symmetry_pass == 0) {
+      initial_vert = SCULPT_active_vertex_get(ss);
+    }
+    else {
+      float location[3];
+      flip_v3_v3(location, SCULPT_active_vertex_co_get(ss), symm_area);
+      initial_vert = nearest_vert_calc(ob, location, ss.cache->radius_squared, false);
+    }
+
+    ss.cache->boundaries[symm_area] = boundary::data_init(
+        ob, &brush, initial_vert, ss.cache->initial_radius);
+
+    if (ss.cache->boundaries[symm_area]) {
+      switch (brush.boundary_deform_type) {
+        case BRUSH_BOUNDARY_DEFORM_BEND:
+          bend_data_init(ob, *ss.cache->boundaries[symm_area]);
+          break;
+        case BRUSH_BOUNDARY_DEFORM_EXPAND:
+          slide_data_init(ob, *ss.cache->boundaries[symm_area]);
+          break;
+        case BRUSH_BOUNDARY_DEFORM_TWIST:
+          twist_data_init(ob, *ss.cache->boundaries[symm_area]);
+          break;
+        case BRUSH_BOUNDARY_DEFORM_INFLATE:
+        case BRUSH_BOUNDARY_DEFORM_GRAB:
+          /* Do nothing. These deform modes don't need any extra data to be precomputed. */
+          break;
+      }
+
+      init_falloff(brush, ss.cache->initial_radius, *ss.cache->boundaries[symm_area]);
+    }
+  }
+
+  /* No active boundary under the cursor. */
+  if (!ss.cache->boundaries[symm_area]) {
+    return;
+  }
+
+  switch (brush.boundary_deform_type) {
+    case BRUSH_BOUNDARY_DEFORM_BEND:
+      threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+        for (const int i : range) {
+          boundary_brush_bend_task(ob, brush, nodes[i]);
+        }
+      });
+      break;
+    case BRUSH_BOUNDARY_DEFORM_EXPAND:
+      threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+        for (const int i : range) {
+          brush_slide_task(ob, brush, nodes[i]);
+        }
+      });
+      break;
+    case BRUSH_BOUNDARY_DEFORM_INFLATE:
+      threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+        for (const int i : range) {
+          brush_inflate_task(ob, brush, nodes[i]);
+        }
+      });
+      break;
+    case BRUSH_BOUNDARY_DEFORM_GRAB:
+      threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+        for (const int i : range) {
+          brush_grab_task(ob, brush, nodes[i]);
+        }
+      });
+      break;
+    case BRUSH_BOUNDARY_DEFORM_TWIST:
+      threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+        for (const int i : range) {
+          brush_twist_task(ob, brush, nodes[i]);
+        }
+      });
+      break;
+    case BRUSH_BOUNDARY_DEFORM_SMOOTH:
+      threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+        for (const int i : range) {
+          brush_smooth_task(ob, brush, nodes[i]);
+        }
+      });
+      break;
+  }
+}
+
+};  // namespace blender::ed::sculpt_paint
