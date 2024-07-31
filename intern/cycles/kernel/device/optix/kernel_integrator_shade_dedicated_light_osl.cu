e1b7a42a18bd4d96f19350ec669fb368e43b1408
Patrick Mours <pmours@nvidia.com>
2024-07-31 17:17:35 +0200

    Fix missing OptiX source files in install target

--- intern/cycles/kernel/device/optix/kernel_integrator_shade_dedicated_light_osl.cu
+++ intern/cycles/kernel/device/optix/kernel_integrator_shade_dedicated_light_osl.cu
@@ -4,4 +4,23 @@
 
 #define __OSL__
 
-#include "kernel/device/optix/kernel_integrator_shade_dedicated_light.cu"
+#include "kernel/device/optix/compat.h"
+#include "kernel/device/optix/globals.h"
+
+#include "kernel/device/gpu/image.h"
+#include "kernel/tables.h"
+
+#include "kernel/integrator/state.h"
+#include "kernel/integrator/state_flow.h"
+#include "kernel/integrator/state_util.h"
+
+#include "kernel/integrator/shade_dedicated_light.h"
+
+extern "C" __global__ void __raygen__kernel_optix_integrator_shade_dedicated_light()
+{
+  const int global_index = optixGetLaunchIndex().x;
+  const int path_index = (kernel_params.path_index_array) ?
+                             kernel_params.path_index_array[global_index] :
+                             global_index;
+  integrator_shade_dedicated_light(nullptr, path_index, kernel_params.render_buffer);
+}
--- intern/cycles/kernel/device/optix/kernel_integrator_shade_volume_osl.cu
+++ intern/cycles/kernel/device/optix/kernel_integrator_shade_volume_osl.cu
@@ -4,4 +4,23 @@
 
 #define __OSL__
 
-#include "kernel/device/optix/kernel_integrator_shade_volume.cu"
+#include "kernel/device/optix/compat.h"
+#include "kernel/device/optix/globals.h"
+
+#include "kernel/device/gpu/image.h"
+#include "kernel/tables.h"
+
+#include "kernel/integrator/state.h"
+#include "kernel/integrator/state_flow.h"
+#include "kernel/integrator/state_util.h"
+
+#include "kernel/integrator/shade_volume.h"
+
+extern "C" __global__ void __raygen__kernel_optix_integrator_shade_volume()
+{
+  const int global_index = optixGetLaunchIndex().x;
+  const int path_index = (kernel_params.path_index_array) ?
+                             kernel_params.path_index_array[global_index] :
+                             global_index;
+  integrator_shade_volume(nullptr, path_index, kernel_params.render_buffer);
+}
