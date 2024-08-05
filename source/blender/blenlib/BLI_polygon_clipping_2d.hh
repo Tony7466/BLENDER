0bb0750a77ba38c233fcaf687515329499dad100
Casey Bianco-Davis <caseycasey739@gmail.com>
2024-08-04 19:34:55 -0700

    Add interpolate between start and end segment.

--- source/blender/blenlib/BLI_polygon_clipping_2d.hh
+++ source/blender/blenlib/BLI_polygon_clipping_2d.hh
@@ -144,11 +144,70 @@ static T interpolate_data_of_b_intersection_point(const VArray<T> data_b,
   return math::interpolate(b0, b1, alpha_b);
 }
 
+static Array<int2> calculate_segments(const BooleanResult &result)
+{
+  const OffsetIndices<int> points_by_polygon = OffsetIndices<int>(result.offsets);
+  Array<int2> segment_indices(result.verts.size(), int2(-1, -1));
+
+  for (const int polygon_id : points_by_polygon.index_range()) {
+    const IndexRange vert_ids = points_by_polygon[polygon_id];
+
+    int first_i = -1;
+    int last_i = -1;
+
+    for (const int i : vert_ids.index_range()) {
+      const Vertex &vert = result.verts[vert_ids[i]];
+      if (vert.type == VertexType::Intersection) {
+        first_i = vert.point_id;
+        break;
+      }
+    }
+
+    for (const int i : vert_ids.index_range()) {
+      const Vertex &vert = result.verts[vert_ids[vert_ids.size() - 1 - i]];
+      if (vert.type == VertexType::Intersection) {
+        last_i = vert.point_id;
+        break;
+      }
+    }
+
+    int inter_point = last_i;
+
+    for (const int i : vert_ids.index_range()) {
+      const int j = vert_ids[i];
+      const Vertex &vert = result.verts[j];
+
+      if (vert.type == VertexType::Intersection) {
+        inter_point = vert.point_id;
+      }
+
+      segment_indices[j][0] = inter_point;
+    }
+
+    inter_point = first_i;
+
+    for (const int i : vert_ids.index_range()) {
+      const int j = vert_ids[vert_ids.size() - 1 - i];
+      const Vertex &vert = result.verts[j];
+
+      if (vert.type == VertexType::Intersection) {
+        inter_point = vert.point_id;
+      }
+
+      segment_indices[j][1] = inter_point;
+    }
+  }
+
+  return segment_indices;
+}
+
 template<typename T>
 void interpolate_data_from_a_result(const VArray<T> data_a,
                                     const BooleanResult &result,
                                     MutableSpan<T> dst_attr)
 {
+  const Array<int2> segment_indices = calculate_segments(result);
+
   for (const int i : result.verts.index_range()) {
     const Vertex &vert = result.verts[i];
     const VertexType &type = vert.type;
@@ -157,8 +216,21 @@ void interpolate_data_from_a_result(const VArray<T> data_a,
       dst_attr[i] = data_a[vert.point_id];
     }
     else if (type == VertexType::PointB) {
-      /* TODO: Interpolate between start and end of the segment. */
-      dst_attr[i] = data_a.first();
+      const int2 &segment = segment_indices[i];
+
+      const IntersectionPoint &i0 = result.intersections_data[segment[0]];
+      const IntersectionPoint &i1 = result.intersections_data[segment[1]];
+
+      const T a0 = interpolate_data_of_a_intersection_point<T>(data_a, i0);
+      const T a1 = interpolate_data_of_a_intersection_point<T>(data_a, i1);
+
+      const float min_b = i0.point_b + i0.alpha_b;
+      const float max_b = i1.point_b + i1.alpha_b;
+      const float id_b = vert.point_id;
+
+      const float alpha = (id_b - min_b) / (max_b - min_b);
+
+      dst_attr[i] = math::interpolate(a0, a1, alpha);
     }
     else if (type == VertexType::Intersection) {
       const IntersectionPoint &inter_point = result.intersections_data[vert.point_id];
@@ -172,13 +244,28 @@ void interpolate_data_from_b_result(const VArray<T> data_b,
                                     const BooleanResult &result,
                                     MutableSpan<T> dst_attr)
 {
+  const Array<int2> segment_indices = calculate_segments(result);
+
   for (const int i : result.verts.index_range()) {
     const Vertex &vert = result.verts[i];
     const VertexType &type = vert.type;
 
     if (type == VertexType::PointA) {
-      /* TODO: Interpolate between start and end of the segment. */
-      dst_attr[i] = data_b.first();
+      const int2 &segment = segment_indices[i];
+
+      const IntersectionPoint &i0 = result.intersections_data[segment[0]];
+      const IntersectionPoint &i1 = result.intersections_data[segment[1]];
+
+      const T b0 = interpolate_data_of_b_intersection_point<T>(data_b, i0);
+      const T b1 = interpolate_data_of_b_intersection_point<T>(data_b, i1);
+
+      const float min_a = i0.point_a + i0.alpha_a;
+      const float max_a = i1.point_a + i1.alpha_a;
+      const float id_a = vert.point_id;
+
+      const float alpha = (id_a - min_a) / (max_a - min_a);
+
+      dst_attr[i] = math::interpolate(b0, b1, alpha);
     }
     else if (type == VertexType::PointB) {
       dst_attr[i] = data_b[vert.point_id];
