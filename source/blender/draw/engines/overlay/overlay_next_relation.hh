b4926533e3ffbf7e3f2d6140e9daf40fea4db406
Laurynas Duburas <laduem@gmail.com>
2024-08-06 12:33:15 +0300

    DRW_STATE_DEPTH_LESS_EQUAL added

--- source/blender/draw/engines/overlay/overlay_next_relation.hh
+++ source/blender/draw/engines/overlay/overlay_next_relation.hh
@@ -162,8 +162,8 @@ class Relations {
     }
     {
       PassSimple::Sub &sub_pass = ps_.sub("loose_points");
-      sub_pass.state_set(DRW_STATE_WRITE_COLOR | DRW_STATE_WRITE_DEPTH | DRW_STATE_WRITE_DEPTH |
-                         state.clipping_state);
+      sub_pass.state_set(DRW_STATE_WRITE_COLOR | DRW_STATE_WRITE_DEPTH |
+                         DRW_STATE_DEPTH_LESS_EQUAL | state.clipping_state);
       sub_pass.shader_set(res.shaders.extra_loose_points.get());
       sub_pass.bind_ubo("globalsBlock", &res.globals_buf);
       points_buf_.end_sync(sub_pass);
