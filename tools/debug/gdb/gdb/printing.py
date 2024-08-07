5986ae153145ea90198b301c546b7f98f688dc08
Jacques Lucke <jacques@blender.org>
2024-08-07 22:02:05 +0200

    initial commit

---
+++ tools/debug/gdb/gdb/printing.py
@@ -0,0 +1,10 @@
+from .gdb import Value
+import typing as t
+
+
+class PrettyPrinter:
+    def __init__(self, name: str, subprinters=None):
+        pass
+
+    def __call__(self, value: Value):
+        pass
