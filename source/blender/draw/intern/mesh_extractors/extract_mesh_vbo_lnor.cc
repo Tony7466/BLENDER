9ef0f61b989fb2dfadea47969f9153a687ab6cde
Hans Goudey <hans@blender.org>
2024-08-07 12:51:05 -0400

    Fix initialization

--- source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_lnor.cc
+++ source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_lnor.cc
@@ -267,13 +267,15 @@ void extract_normals_subdiv(const MeshRenderData &mr,
   const int vbo_size = subdiv_full_vbo_size(mr, subdiv_cache);
   const int loose_geom_start = subdiv_cache.num_subdiv_loops;
 
-  const float3 up(0.0f, 0.0f, 1.0f);
-  for (const int i : IndexRange::from_begin_end(loose_geom_start, vbo_size)) {
-    GPU_vertbuf_update_sub(&lnor, i * sizeof(float3), sizeof(float3), &up);
-  }
-
   GPU_vertbuf_init_build_on_device(lnor, get_subdiv_lnor_format(), vbo_size);
   draw_subdiv_build_lnor_buffer(subdiv_cache, &pos_nor, &lnor);
-}
 
+  /* Push VBO content to the GPU and bind the VBO so that #GPU_vertbuf_update_sub can work. */
+  GPU_vertbuf_use(&lnor);
+
+  const float4 up(0.0f, 0.0f, 1.0f, 0.0f);
+  for (const int i : IndexRange::from_begin_end(loose_geom_start, vbo_size)) {
+    GPU_vertbuf_update_sub(&lnor, i * sizeof(float4), sizeof(float4), &up);
+  }
+}
 }  // namespace blender::draw
