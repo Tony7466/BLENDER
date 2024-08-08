da93220e6e8188d9dcc614661253d65cfb6bee48
Hans Goudey <hans@blender.org>
2024-08-08 10:26:36 -0400

    Remove unnecessary change

--- source/blender/gpu/GPU_vertex_buffer.hh
+++ source/blender/gpu/GPU_vertex_buffer.hh
@@ -141,7 +141,7 @@ class VertBuf {
    */
   template<typename T> MutableSpan<T> data()
   {
-    return data_ ? MutableSpan<uchar>(data_, this->size_alloc_get()).cast<T>() : MutableSpan<T>();
+    return MutableSpan<uchar>(data_, this->size_alloc_get()).cast<T>();
   }
 
   virtual void update_sub(uint start, uint len, const void *data) = 0;
