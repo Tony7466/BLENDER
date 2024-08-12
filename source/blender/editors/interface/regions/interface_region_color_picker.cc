0e215dab8bd6486e86632c45e1421f1dde8d7e23
Jonas Holzman <jonas@holzman.fr>
2024-08-12 23:36:47 +0200

    Feedback: Improve Hex input field button check and add comment

--- source/blender/editors/interface/regions/interface_region_color_picker.cc
+++ source/blender/editors/interface/regions/interface_region_color_picker.cc
@@ -194,7 +194,8 @@ static void ui_update_color_picker_buts_rgb(uiBut *from_but,
        * push, so disable it on RNA buttons in the color picker block */
       UI_but_flag_disable(bt, UI_BUT_UNDO);
     }
-    else if (bt->type == UI_BTYPE_TEXT) {
+    else if (bt->type == UI_BTYPE_TEXT && bt->drawstr[0] == '#') {
+      /* Hex text input field. */
       float rgb_hex[3];
       uchar rgb_hex_uchar[3];
       char col[16];
