3e7cb791435e12c6e89747e6bb02bbcacbdb4bf9
Tolga Talha YILDIZ <tolgatalhayldz@gmail.com>
2024-08-14 17:38:58 -0500

    handling scope

--- source/blender/nodes/geometry/nodes/node_geo_principal_components.cc
+++ source/blender/nodes/geometry/nodes/node_geo_principal_components.cc
@@ -220,7 +220,7 @@ static void node_register()
   ntype.draw_buttons = node_layout;
   ntype.declare = node_declare;
   ntype.gather_link_search_ops = node_gather_link_searches;
-  nodeRegisterType(&ntype);
+  blender::bke::nodeRegisterType(&ntype);
 
   node_rna(ntype.rna_ext.srna);
 }
