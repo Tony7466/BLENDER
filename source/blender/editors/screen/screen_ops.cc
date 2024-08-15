0baa69a4fa7b706ad3a9e642c16937018c12dca2
Jonas Holzman <jonas@holzman.fr>
2024-08-15 18:00:47 +0200

    Fix: UI: Editor window(s) title update when using interactive docking
    When using the experimental Interactive Editor Docking feature single
    editor window titles were not properly updated due to the interactive
    editor using a different set of functions for applying area splitting,
    joining and, new to this system, docking.
    
    Using the previous area split/join system, single editor window title
    were set in two different `screen_ops` area functions,`area_join_apply`,
    which checks if the window has turned into a single editor window, and
    update its title accordingly, and `area_split_apply` which would always
    update the title.
    
    While `area_join_apply` is used by the the new interactive area
    mechanism, `area_split_apply` is not, and the actual docking split
    operation ends up being carried in `area_join_modal`. To cover all
    cases (docking, joining and splitting), this commit adds an additional
    window title update call the end of the `area_join_modal` function.
    Including logic to update both the source and target area windows when
    required.

--- source/blender/editors/screen/screen_ops.cc
+++ source/blender/editors/screen/screen_ops.cc
@@ -4120,6 +4120,22 @@ static int area_join_modal(bContext *C, wmOperator *op, const wmEvent *event)
           return OPERATOR_CANCELLED;
         }
 
+        /* Areas changed, update window(s) title. */
+        if (jd->close_win) {
+          if (jd->win2) {
+            WM_window_title(CTX_wm_manager(C), jd->win2);
+          }
+        }
+        else {
+          if (jd->win1) {
+            WM_window_title(CTX_wm_manager(C), jd->win1);
+          }
+          if (jd->win2 && jd->win1 != jd->win2) {
+            /* Drag and dropping an area from one window to another. */
+            WM_window_title(CTX_wm_manager(C), jd->win2);
+          }
+        }
+
         const bool do_close_win = jd->close_win;
         wmWindow *close_win = jd->win1;
         area_join_exit(C, op);
