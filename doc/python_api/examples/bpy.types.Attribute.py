f93ef13b028398ccd7b2db966a03baa8dd75f9c3
Falk David <falk@blender.org>
2024-08-15 15:19:25 +0200

    Fix typo

--- doc/python_api/examples/bpy.types.Attribute.py
+++ doc/python_api/examples/bpy.types.Attribute.py
@@ -10,7 +10,7 @@ Using Attributes
 ++++++++++++++++
 
 Attributes can be stored on geometries like :class:`Mesh`, :class:`Curves`, :class:`PointCloud`, etc.
-These geometries have attribute groups (usally called ``attributes``). Using the groups, attributes can then be accessed by their name:
+These geometries have attribute groups (usually called ``attributes``). Using the groups, attributes can then be accessed by their name:
  
 .. code-block:: python
 
