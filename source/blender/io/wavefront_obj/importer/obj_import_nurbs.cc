ebe6b90c75ba3023aaf611cbfc8041492bc15fac
Germain Le Chapelain <germain.lechapelain@lanvaux.fr>
2024-08-19 16:35:31 -0700

    -> My response to https://blender.chat/channel/blender-coders?msg=byc85ttSL6Dk5tPyZ
    Signed-off-by: Germain Le Chapelain <germain.lechapelain@lanvaux.fr>

--- source/blender/io/wavefront_obj/importer/obj_import_nurbs.cc
+++ source/blender/io/wavefront_obj/importer/obj_import_nurbs.cc
@@ -120,8 +120,15 @@ void CurveFromGeometry::create_nurbs(Curve *curve)
   const int64_t tot_vert{nurbs_geometry.curv_indices.size()};
 
   BKE_nurb_points_add(nurb, tot_vert);
-  nurb->pntsu = nurbs_geometry.u.parms.size() - nurb->orderu;
-  nurb->pntsv = std::max(int(nurbs_geometry.v.parms.size() - nurb->orderv), 1);
+  if(GEOM_SURF == curve_geometry_.geom_type_) {
+    nurb->pntsu = nurbs_geometry.u.parms.size() - nurb->orderu;
+    nurb->pntsv = 1;
+  } else
+  {
+    /* pntsu was set in BKE_nurb_points_add. */
+    nurb->pntsv = 1;
+  }
+  BLI_assert(tot_vert == nurb->pntsu * nurb->pntsv);
 
   for (int i = 0; i < tot_vert; i++) {
     BPoint &bpoint = nurb->bp[i];
