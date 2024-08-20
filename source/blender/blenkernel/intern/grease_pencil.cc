d69f182a765d52d73e99eb30257239ec135a721a
Lukas Tönne <lukas@blender.org>
2024-08-20 11:00:14 +0200

    Remove unnecessary change.

--- source/blender/blenkernel/intern/grease_pencil.cc
+++ source/blender/blenkernel/intern/grease_pencil.cc
@@ -2693,9 +2693,8 @@ std::optional<blender::Bounds<blender::float3>> GreasePencil::bounds_min_max(con
     if (!layer.is_visible()) {
       continue;
     }
-    if (layer.has_drawing_at(frame)) {
-      const bke::greasepencil::Drawing &drawing = *this->get_drawing_at(layer, frame);
-      const bke::CurvesGeometry &curves = drawing.strokes();
+    if (const bke::greasepencil::Drawing *drawing = this->get_drawing_at(layer, frame)) {
+      const bke::CurvesGeometry &curves = drawing->strokes();
       bounds = bounds::merge(bounds, curves.bounds_min_max());
     }
   }
