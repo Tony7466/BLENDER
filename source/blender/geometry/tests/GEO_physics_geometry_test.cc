ebf63444cf1813c069536d2e2936ca7deee58c26
Lukas Tönne <lukas@blender.org>
2024-08-21 13:29:41 +0200

    Use correct variables in test.

--- source/blender/geometry/tests/GEO_physics_geometry_test.cc
+++ source/blender/geometry/tests/GEO_physics_geometry_test.cc
@@ -422,7 +422,9 @@ TEST_F(PhysicsGeometryTest, realize_instances)
        math::from_scale<float4x4>(float3(1.5f, 1.0f, 2.0f)),
        math::from_location<float4x4>(float3(-3, 1, 1)),
        float4x4::identity(),
-       math::from_location<float4x4>(float3(2, 1, 0))});
+       math::from_location<float4x4>(float3(2, 1, 0)),
+       float4x4::identity(),
+       float4x4::identity()});
 
   bke::PhysicsGeometry *geo1 = new bke::PhysicsGeometry(5, 2, 3);
   test_data(*geo1, false, 5, 2, 3);
@@ -439,7 +441,7 @@ TEST_F(PhysicsGeometryTest, realize_instances)
     AttributeWriter<int> body_shapes = geo1->body_shapes_for_write();
     AttributeWriter<float4x4> center_of_mass = geo1->body_center_of_mass_for_write();
     body_shapes.varray.set_all({2, 0, 2, -1, 1});
-    center_of_mass.varray.set_all(CoM_values.as_span());
+    center_of_mass.varray.set_all(CoM_values.as_span().slice(0, 5));
     body_shapes.finish();
     center_of_mass.finish();
     geo1->compute_local_inertia(geo1->bodies_range());
@@ -461,8 +463,11 @@ TEST_F(PhysicsGeometryTest, realize_instances)
   /* Invalid shape index should be handled fine. */
   {
     AttributeWriter<int> body_shapes = geo3->body_shapes_for_write();
+    AttributeWriter<float4x4> center_of_mass = geo3->body_center_of_mass_for_write();
     body_shapes.varray.set_all({0, 100});
+    center_of_mass.varray.set_all(CoM_values.as_span().slice(5, 2));
     body_shapes.finish();
+    center_of_mass.finish();
     geo3->compute_local_inertia(geo3->bodies_range());
   }
   test_data(*geo3, true, 2, 1, 1);
