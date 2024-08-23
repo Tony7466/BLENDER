47dc13749c4fd3cbdd2fe07ca942b062305a77b5
Alaska <Alaskayou01@gmail.com>
2024-08-23 17:24:25 +1200

    Add a error message to the Windows sciprt

--- release/windows/batch/blender_system_information.cmd
+++ release/windows/batch/blender_system_information.cmd
@@ -3,5 +3,12 @@
 For /D %%d in (*) do (
   if exist "%%d\python\bin\python.exe" (
     "%%d\python\bin\python.exe" "%%d\scripts\system_info\system_info.py"
+    exit /b
   )
 )
+
+echo ERROR: Failed to find python.exe. Possible causes include:
+echo - Your Blender installation is corrupt or missing python.exe.
+echo - You're a developer using a debug build of Blender.
+echo - The location or name of python.exe is changed.
+pause
