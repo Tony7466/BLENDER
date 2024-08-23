38c453241914a89814754a7393ce125e26506bdd
Alaska <Alaskayou01@gmail.com>
2024-08-23 17:08:59 +1200

    Update Windows bat file
    Check for Python exe directly
    Put quotation marks around directories as it may contain spaces

--- release/windows/batch/blender_system_information.cmd
+++ release/windows/batch/blender_system_information.cmd
@@ -1,9 +1,7 @@
 @echo off
 
 For /D %%d in (*) do (
-  if exist "%%d\python\bin" (
-    %%d\python\bin\python.exe %%d\scripts\system_info\system_info.py
+  if exist "%%d\python\bin\python.exe" (
+    "%%d\python\bin\python.exe" "%%d\scripts\system_info\system_info.py"
   )
 )
-
-pause
