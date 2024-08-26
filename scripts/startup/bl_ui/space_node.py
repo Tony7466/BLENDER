e1ef1b8e2d861ee054a81a5009004db11bf2c7f2
Jacques Lucke <jacques@blender.org>
2024-08-26 12:55:32 +0200

    initial new setting

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -689,9 +689,13 @@ class NODE_PT_active_node_generic(Panel):
     def draw(self, context):
         layout = self.layout
         node = context.active_node
+        tree = node.id_data
 
         layout.prop(node, "name", icon='NODE')
         layout.prop(node, "label", icon='NODE')
+
+        if tree.type == "GEOMETRY":
+            layout.prop(node, "warning_propagation")
 
 
 class NODE_PT_active_node_color(Panel):
