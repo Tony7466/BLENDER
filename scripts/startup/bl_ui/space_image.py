863e71718c26f5bcb38aa166749a53a8eeaad490
Pratik Borhade <pratikborhade302@gmail.com>
2024-08-27 17:09:47 +0530

    Move pivot point and proportional edit to the right of mask template

--- scripts/startup/bl_ui/space_image.py
+++ scripts/startup/bl_ui/space_image.py
@@ -783,9 +783,8 @@ class IMAGE_HT_header(Header):
     def draw_xform_template(layout, context):
         sima = context.space_data
         show_uvedit = sima.show_uvedit
-        show_maskedit = sima.show_maskedit
 
-        if show_uvedit or show_maskedit:
+        if show_uvedit:
             layout.prop(sima, "pivot_point", icon_only=True)
 
         if show_uvedit:
@@ -876,6 +875,9 @@ class IMAGE_HT_header(Header):
         layout.template_ID(sima, "image", new="image.new", open="image.open")
 
         if show_maskedit:
+            layout.template_ID(sima, "mask", new="mask.new")
+            layout.prop(sima, "pivot_point", icon_only=True)
+
             row = layout.row(align=True)
             row.prop(tool_settings, "use_proportional_edit_mask", text="", icon_only=True)
             sub = row.row(align=True)
@@ -887,7 +889,6 @@ class IMAGE_HT_header(Header):
                 icon_only=True,
                 panel="IMAGE_PT_proportional_edit",
             )
-            row.template_ID(sima, "mask", new="mask.new")
 
         if not show_render:
             layout.prop(sima, "use_image_pin", text="", emboss=False)
