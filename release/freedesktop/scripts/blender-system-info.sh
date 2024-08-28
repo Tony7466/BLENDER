5dadb478807de1249a0b23dbfddff3175578ee5f
Alaska <alaskayou01@gmail.com>
2024-08-29 02:19:15 +1200

    Add error message in the case that Python can't be found

--- release/freedesktop/scripts/blender-system-info.sh
+++ release/freedesktop/scripts/blender-system-info.sh
@@ -14,3 +14,8 @@ for directory in "$script_dir"/*; do
         fi
     fi
 done
+
+echo "ERROR: Failed to find python executable. Possible causes include:"
+echo "- Your Blender installation is corrupt or missing python."
+echo "- You're a developer using a debug build of Blender."
+echo "- The location or name of python of the python executable has changed."
