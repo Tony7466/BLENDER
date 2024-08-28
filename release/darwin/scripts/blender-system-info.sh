16770a0137db663782fdf37f390a7891b3e462ab
Alaska <alaskayou01@gmail.com>
2024-08-29 01:49:53 +1200

    Use directory instead of folder in sh files

--- release/darwin/scripts/blender-system-info.sh
+++ release/darwin/scripts/blender-system-info.sh
@@ -2,13 +2,13 @@
 
 script_dir=$(dirname "$0") # Directory to this sh file
 
-for folder in "$script_dir"/*; do
-    if test -d "$folder"; then
-        if test -d "$folder/python/bin"; then # If Python bin folder exists
-            for file in "$folder/python/bin"/*; do
+for directory in "$script_dir"/*; do
+    if test -d "$directory"; then
+        if test -d "$directory/python/bin"; then # If Python bin directory exists
+            for file in "$directory/python/bin"/*; do
                 filename=$(basename "$file")
                 if grep -i "python" "$file" > /dev/null; then # Search for Python executable
-                    "$file" "$folder/scripts/system_info/system_info.py" # Run Python script
+                    "$file" "$directory/scripts/system_info/system_info.py" # Run Python script
                 fi
             done
         fi
