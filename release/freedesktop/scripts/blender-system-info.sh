c354c9baa2d9860654784765cca6880aef9f9d48
Alaska <alaskayou01@gmail.com>
2024-08-29 01:52:17 +1200

    Remove unused variable in sh sciprts

--- release/freedesktop/scripts/blender-system-info.sh
+++ release/freedesktop/scripts/blender-system-info.sh
@@ -6,7 +6,6 @@ for directory in "$script_dir"/*; do
     if test -d "$directory"; then
         if test -d "$directory/python/bin"; then # If Python bin directory exists
             for file in "$directory/python/bin"/*; do
-                filename=$(basename "$file")
                 if grep -i "python" "$file" > /dev/null; then # Search for Python executable
                     "$file" "$directory/scripts/system_info/system_info.py" # Run Python script
                 fi
