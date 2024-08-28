Tony7466/BLENDER

commit 03ae57dd8b8fcd847736b6dd2802dadee2924614

UI: Disable "Render Active Object" for previews of incompatible object types
Object types like empties, lamps and cameras don't have geometry that can be
rendered by regular render engines. Attempting to render them for an asset
preview doesn't work.

36c5606fc3 already fixed a crash in this situation, resulting in an empty
preview. But better to entirely disallow this action, with useful feedback in
the UI.

source/blender/editors/util/ed_util_ops.cc
@@ -60,6 +60,10 @@ static bool lib_id_preview_editing_poll(bContext *C)
     CTX_wm_operator_poll_msg_set(C, "Data-block does not support previews");
     return false;
   }
+  if (!ED_preview_id_is_supported(id)) {
+    CTX_wm_operator_poll_msg_set(C, "Object type does not support previews");
+    return false;
+  }
 
   return true;
 }

commit 315298f7dca3291bee7535364af79db7cda4712f

Fix #126872: Cannot assign custom preview to node asset
Caused by 03ae57dd8b8fcd847736b6dd2802dadee2924614
Custom preview operator uses `lib_id_preview_editing_poll`. To enable
this operator in UI, move the new condition to `lib_id_generate_preview_poll`
poll function so adding custom preview to unsupported type would
still be possible.

source/blender/editors/util/ed_util_ops.cc
@@ -60,10 +60,6 @@ static bool lib_id_preview_editing_poll(bContext *C)
     CTX_wm_operator_poll_msg_set(C, "Data-block does not support previews");
     return false;
   }
-  if (!ED_preview_id_is_supported(id)) {
-    CTX_wm_operator_poll_msg_set(C, "Object type does not support previews");
-    return false;
-  }
 
   return true;
 }
@@ -147,8 +143,8 @@ static bool lib_id_generate_preview_poll(bContext *C)
 
   const PointerRNA idptr = CTX_data_pointer_get(C, "id");
   const ID *id = (ID *)idptr.data;
-  if (GS(id->name) == ID_NT) {
-    CTX_wm_operator_poll_msg_set(C, "Can't generate automatic preview for node group");
+  if (!ED_preview_id_is_supported(id)) {
+    CTX_wm_operator_poll_msg_set(C, "Object type does not support previews");
     return false;
   }
 
@@ -192,7 +188,7 @@ static void ED_OT_lib_id_generate_preview(wmOperatorType *ot)
 
 static bool lib_id_generate_preview_from_object_poll(bContext *C)
 {
-  if (!lib_id_preview_editing_poll(C)) {
+  if (!lib_id_generate_preview_poll(C)) {
     return false;
   }
   if (CTX_data_active_object(C) == nullptr) {