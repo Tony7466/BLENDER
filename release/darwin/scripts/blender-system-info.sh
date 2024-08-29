0da023f062318f703591b91e761082a18a363fe6
Alaska <alaskayou01@gmail.com>
2024-08-29 16:38:53 +1200

    Print missing Python path

--- release/darwin/scripts/blender-system-info.sh
+++ release/darwin/scripts/blender-system-info.sh
@@ -10,7 +10,8 @@ if test -f "$PYTHON_BIN"; then
   exec "$PYTHON_BIN" -I "$SYSTEM_INFO_STARTUP_PY"
 fi
 
-echo "ERROR: Failed to find python executable. Possible causes include:"
+echo "ERROR: Failed to find Python executable at: $PYTHON_BIN"
+echo "Possible causes include:"
 echo "- Your Blender installation is corrupt or missing python."
 echo "- You're a developer using a debug build of Blender."
 echo "- The location or name of python of the python executable has changed."
