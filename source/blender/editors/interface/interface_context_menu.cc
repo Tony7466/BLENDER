c000833fbc24d9266f86c778e38a7f1b5965dbb4
Pratik Borhade <pratikborhade302@gmail.com>
2024-08-29 15:23:34 +0530

    Fix #126006: UI: Few id operators appears in nested context menus
    lib override and add asset operation appears in nested context menu.
    They are incorrectly exposed when right clicked on an operator in
    context menu. So don't expose then when opening nested context menu on
    operator.

--- source/blender/editors/interface/interface_context_menu.cc
+++ source/blender/editors/interface/interface_context_menu.cc
@@ -1097,51 +1097,55 @@ bool ui_popup_context_menu_for_button(bContext *C, uiBut *but, const wmEvent *ev
     }
   }
 
-  /* If the button represents an id, it can set the "id" context pointer. */
-  if (asset::can_mark_single_from_context(C)) {
-    const ID *id = static_cast<const ID *>(CTX_data_pointer_get_type(C, "id", &RNA_ID).data);
+  /* Expose id specific operators in context menu when button. Otherwise they would appear in
+   * nested context menus, see: #126006. */
+  if ((but->optype == nullptr) && (but->apply_func == nullptr)) {
+    /* If the button represents an id, it can set the "id" context pointer. */
+    if (asset::can_mark_single_from_context(C)) {
+      const ID *id = static_cast<const ID *>(CTX_data_pointer_get_type(C, "id", &RNA_ID).data);
 
-    /* Gray out items depending on if data-block is an asset. Preferably this could be done via
-     * operator poll, but that doesn't work since the operator also works with "selected_ids",
-     * which isn't cheap to check. */
-    uiLayout *sub = uiLayoutColumn(layout, true);
-    uiLayoutSetEnabled(sub, !id->asset_data);
-    uiItemO(sub,
-            CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Mark as Asset"),
-            ICON_ASSET_MANAGER,
-            "ASSET_OT_mark_single");
-    sub = uiLayoutColumn(layout, true);
-    uiLayoutSetEnabled(sub, id->asset_data);
-    uiItemO(sub,
-            CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Clear Asset"),
-            ICON_NONE,
-            "ASSET_OT_clear_single");
-    uiItemS(layout);
-  }
-
-  MenuType *mt_idtemplate_liboverride = WM_menutype_find("UI_MT_idtemplate_liboverride", true);
-  if (mt_idtemplate_liboverride && mt_idtemplate_liboverride->poll(C, mt_idtemplate_liboverride)) {
-    uiItemM_ptr(layout, mt_idtemplate_liboverride, IFACE_("Library Override"), ICON_NONE);
-    uiItemS(layout);
-  }
-
-  /* Pointer properties and string properties with
-   * prop_search support jumping to target object/bone. */
-  if (but->rnapoin.data && but->rnaprop) {
-    const PropertyType prop_type = RNA_property_type(but->rnaprop);
-    if (((prop_type == PROP_POINTER) ||
-         (prop_type == PROP_STRING && but->type == UI_BTYPE_SEARCH_MENU &&
-          ((uiButSearch *)but)->items_update_fn == ui_rna_collection_search_update_fn)) &&
-        ui_jump_to_target_button_poll(C))
-    {
-      uiItemO(layout,
-              CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Jump to Target"),
+      /* Gray out items depending on if data-block is an asset. Preferably this could be done via
+       * operator poll, but that doesn't work since the operator also works with "selected_ids",
+       * which isn't cheap to check. */
+      uiLayout *sub = uiLayoutColumn(layout, true);
+      uiLayoutSetEnabled(sub, !id->asset_data);
+      uiItemO(sub,
+              CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Mark as Asset"),
+              ICON_ASSET_MANAGER,
+              "ASSET_OT_mark_single");
+      sub = uiLayoutColumn(layout, true);
+      uiLayoutSetEnabled(sub, id->asset_data);
+      uiItemO(sub,
+              CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Clear Asset"),
               ICON_NONE,
-              "UI_OT_jump_to_target_button");
+              "ASSET_OT_clear_single");
       uiItemS(layout);
     }
-  }
 
+    MenuType *mt_idtemplate_liboverride = WM_menutype_find("UI_MT_idtemplate_liboverride", true);
+    if (mt_idtemplate_liboverride && mt_idtemplate_liboverride->poll(C, mt_idtemplate_liboverride))
+    {
+      uiItemM_ptr(layout, mt_idtemplate_liboverride, IFACE_("Library Override"), ICON_NONE);
+      uiItemS(layout);
+    }
+
+    /* Pointer properties and string properties with
+     * prop_search support jumping to target object/bone. */
+    if (but->rnapoin.data && but->rnaprop) {
+      const PropertyType prop_type = RNA_property_type(but->rnaprop);
+      if (((prop_type == PROP_POINTER) ||
+           (prop_type == PROP_STRING && but->type == UI_BTYPE_SEARCH_MENU &&
+            ((uiButSearch *)but)->items_update_fn == ui_rna_collection_search_update_fn)) &&
+          ui_jump_to_target_button_poll(C))
+      {
+        uiItemO(layout,
+                CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Jump to Target"),
+                ICON_NONE,
+                "UI_OT_jump_to_target_button");
+        uiItemS(layout);
+      }
+    }
+  }
   /* Favorites Menu */
   if (ui_but_is_user_menu_compatible(C, but)) {
     uiBlock *block = uiLayoutGetBlock(layout);
