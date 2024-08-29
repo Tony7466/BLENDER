32d22733a7cd196206f402ce9716e2098cd0d0df
Nathan Vegdahl <cessen@cessen.com>
2024-08-29 17:39:57 +0200

    Undo autoformatting that wasn't on main

--- intern/ghost/intern/GHOST_SystemPathsCocoa.mm
+++ intern/ghost/intern/GHOST_SystemPathsCocoa.mm
@@ -22,7 +22,8 @@ static const char *GetApplicationSupportDir(const char *versionstr,
                                             const std::size_t len_tempPath)
 {
   @autoreleasepool {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, mask, YES);
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(
+        NSApplicationSupportDirectory, mask, YES);
 
     if ([paths count] == 0) {
       return nullptr;
@@ -85,7 +86,8 @@ const char *GHOST_SystemPathsCocoa::getUserSpecialDir(GHOST_TUserSpecialDirTypes
         return nullptr;
     }
 
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(ns_directory, NSUserDomainMask, YES);
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(
+        ns_directory, NSUserDomainMask, YES);
     if ([paths count] == 0) {
       return nullptr;
     }
