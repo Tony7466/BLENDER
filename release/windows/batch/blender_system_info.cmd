461e53d0f1b455d422f397247d60b44c42458bfd
Alaska <Alaskayou01@gmail.com>
2024-08-30 16:54:00 +1200

    Update Windows batch script to use configure_file in some places

--- release/windows/batch/blender_system_info.cmd
+++ release/windows/batch/blender_system_info.cmd
@@ -1,13 +1,16 @@
 @echo off
 
-For /D %%d in (*) do (
-  if exist "%%d\python\bin\python.exe" (
-    "%%d\python\bin\python.exe" "-I" "%%d\scripts\modules\_bpy_internal\system_info\startup.py"
+set BLENDER_INSTALL_DIRECTORY=%~dp0
+set BLENDER_VERSION_FOLDER=%BLENDER_INSTALL_DIRECTORY%%SCRIPT_DIRECTORY%@BLENDER_VERSION@
+set PYTHON_BIN=%BLENDER_VERSION_FOLDER%\python\bin\python.exe
+
+if exist "%PYTHON_BIN%" (
+    "%PYTHON_BIN%" -I "%BLENDER_VERSION_FOLDER%\scripts\modules\_bpy_internal\system_info\startup.py"
     exit /b
-  )
 )
 
-echo ERROR: Failed to find python.exe. Possible causes include:
+echo ERROR: Failed to find python executable at: %PYTHON_BIN%
+echo Possible causes include:
 echo - Your Blender installation is corrupt or missing python.exe.
 echo - You're a developer using a debug build of Blender.
 echo - The location or name of python.exe has changed.
