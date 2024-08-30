436da28f8e3f4a57f96dbd054c9c891425bb7226
Nathan Vegdahl <cessen@cessen.com>
2024-08-30 12:30:16 +0200

    Remove assert

--- source/blender/animrig/intern/action.cc
+++ source/blender/animrig/intern/action.cc
@@ -1604,16 +1604,7 @@ void ChannelBag::restore_channel_group_invariants()
 
 bool ChannelGroup::is_legacy() const
 {
-  const bool group_is_legacy = this->channel_bag == nullptr;
-
-  /* This assert is just a sanity check: failing it is benign since
-   * layered-action groups never access the `channels` listbase anyway.
-   * Nevertheless, despite being benign on its own, it should still hold true,
-   * and failure may indicate issues elsewhere. */
-  BLI_assert_msg(group_is_legacy || this->channels.first == nullptr,
-                 "Layered-action channel group has legacy-action data.");
-
-  return group_is_legacy;
+  return this->channel_bag == nullptr;
 }
 
 Span<FCurve *> ChannelGroup::fcurves()
