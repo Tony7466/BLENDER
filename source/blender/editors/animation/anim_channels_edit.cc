cc1a4a37230be44df49fba6afea438730e79365e
Nathan Vegdahl <cessen@cessen.com>
2024-09-06 13:08:43 +0200

    Fix: crash when deleting layered-action channel groups via the UI

--- source/blender/editors/animation/anim_channels_edit.cc
+++ source/blender/editors/animation/anim_channels_edit.cc
@@ -2314,29 +2314,59 @@ static int animchannels_delete_exec(bContext *C, wmOperator * /*op*/)
           FCurve *fcu, *fcn;
 
           /* skip this group if no AnimData available, as we can't safely remove the F-Curves */
-          if (adt == nullptr) {
+          if (adt == nullptr || adt->action == nullptr) {
             continue;
           }
 
-          /* delete all of the Group's F-Curves, but no others */
-          for (fcu = static_cast<FCurve *>(agrp->channels.first); fcu && fcu->grp == agrp;
-               fcu = fcn)
-          {
-            fcn = fcu->next;
+          blender::animrig::Action &action = adt->action->wrap();
 
-            /* remove from group and action, then free */
-            action_groups_remove_channel(adt->action, fcu);
-            BKE_fcurve_free(fcu);
+          /* Legacy actions */
+          if (!action.is_action_layered()) {
+            /* delete all of the Group's F-Curves, but no others */
+            for (fcu = static_cast<FCurve *>(agrp->channels.first); fcu && fcu->grp == agrp;
+                 fcu = fcn)
+            {
+              fcn = fcu->next;
+
+              /* remove from group and action, then free */
+              action_groups_remove_channel(adt->action, fcu);
+              BKE_fcurve_free(fcu);
+            }
+
+            /* free the group itself */
+            if (adt->action) {
+              BLI_freelinkN(&adt->action->groups, agrp);
+              DEG_id_tag_update_ex(CTX_data_main(C), &adt->action->id, ID_RECALC_ANIMATION);
+            }
+            else {
+              MEM_freeN(agrp);
+            }
+            break;
           }
 
-          /* free the group itself */
-          if (adt->action) {
-            BLI_freelinkN(&adt->action->groups, agrp);
-            DEG_id_tag_update_ex(CTX_data_main(C), &adt->action->id, ID_RECALC_ANIMATION);
+          /* Layered actions.
+           *
+           * Note that the behavior here is different from deleting groups via
+           * the Python API: in the Python API the fcurves that belonged to the
+           * group remain, and just get ungrouped, whereas here they are deleted
+           * along with the group. This difference in behavior is replicated
+           * from legacy actions. */
+
+          blender::animrig::ChannelBag &channel_bag = agrp->channel_bag->wrap();
+
+          /* Remove all the fcurves in the group, which also automatically
+           * deletes the group when the last fcurve is deleted. Since the group
+           * is automatically deleted, we store the fcurve range ahead of time
+           * so we don't have to worry about the memory disappearing out from
+           * under us. */
+          const int fcurve_range_start = agrp->fcurve_range_start;
+          const int fcurve_range_length = agrp->fcurve_range_length;
+          for (int i = 0; i < fcurve_range_length; i++) {
+            channel_bag.fcurve_remove(*channel_bag.fcurve(fcurve_range_start));
           }
-          else {
-            MEM_freeN(agrp);
-          }
+
+          DEG_id_tag_update_ex(CTX_data_main(C), &adt->action->id, ID_RECALC_ANIMATION);
+
           break;
         }
 
