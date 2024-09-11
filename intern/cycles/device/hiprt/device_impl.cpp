9e756e679e1c74c4e56c06a32b7020b8b2868070
salipourto <sahar.alipourkashi@amd.com>
2024-09-11 14:40:55 -0700

    Fix for issue #126749, HIP-RT memory leak in viewport

--- intern/cycles/device/hiprt/device_impl.cpp
+++ intern/cycles/device/hiprt/device_impl.cpp
@@ -1073,6 +1073,9 @@ void HIPRTDevice::build_bvh(BVH *bvh, Progress &progress, bool refit)
   else {
 
     const vector<Object *> objects = bvh_rt->objects;
+    if (scene) {
+      hiprtDestroyScene(hiprt_context, scene);
+    }
     scene = build_tlas(bvh_rt, objects, options, refit);
   }
 }
