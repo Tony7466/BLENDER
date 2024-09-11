cbc382526275112e2f2e7e88c149740921731e40
Hans Goudey <hans@blender.org>
2024-09-11 15:40:01 -0400

    Sculpt: Replace automasking sculpt attribute API usage
    Current the automasking code stores temporary data as geometry
    attributes (except for multires where that doesn't work) in order to
    persist the cached factors for cavity and occlusion accross multiple
    strokes. However, the benefits of this aren't clear, compared to
    improving the performance of these calculations. It's also the last
    usage of the sculpt attribute API which we plan to remove. The system
    is also hard to understand, to make a more solid cache of automasking
    values it would be better to start from scratch.
    
    This commit removes the sculpt attribute API and the automasking stroke
    ID system, and moves the cached factors to be cached per stroke rather
    than more permanently.

--- source/blender/editors/sculpt_paint/sculpt_ops.cc
+++ source/blender/editors/sculpt_paint/sculpt_ops.cc
@@ -1155,8 +1155,6 @@ static int sculpt_bake_cavity_exec(bContext *C, wmOperator *op)
   brush2.automasking_boundary_edges_propagation_steps = 1;
   brush2.automasking_cavity_curve = sd2.automasking_cavity_curve;
 
-  SCULPT_stroke_id_next(ob);
-
   std::unique_ptr<auto_mask::Cache> automasking = auto_mask::cache_init(
       *depsgraph, sd2, &brush2, ob);
 
