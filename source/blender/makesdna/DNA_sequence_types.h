338b399a269ee34fb3fd4d12daf954105076bbf5
Aras Pranckevicius <aras@nesnausk.org>
2024-09-11 14:06:38 +0300

    Cleanup: remove unneeded padding members from Sequence DNA struct
    Struct size 464 -> 440 bytes

--- source/blender/makesdna/DNA_sequence_types.h
+++ source/blender/makesdna/DNA_sequence_types.h
@@ -194,16 +194,14 @@ typedef struct Sequence {
   float startstill, endstill;
   /** Machine: the strip channel */
   int machine;
-  int _pad;
   /** Starting and ending points of the effect strip. Undefined for other strip types. */
   int startdisp, enddisp;
   float sat;
   float mul;
-  float _pad1;
 
-  short anim_preseek; /* UNUSED. */
   /** Stream-index for movie or sound files with several streams. */
   short streamindex;
+  short _pad;
   /** For multi-camera source selection. */
   int multicam_source;
   /** MOVIECLIP render flags. */
@@ -232,7 +230,6 @@ typedef struct Sequence {
 
   /* pointers for effects: */
   struct Sequence *seq1, *seq2;
-  void *_pad7;
 
   /** List of strips for meta-strips. */
   ListBase seqbase;
@@ -292,7 +289,6 @@ typedef struct Sequence {
   float speed_factor;
 
   struct SeqRetimingKey *retiming_keys;
-  void *_pad5;
   int retiming_keys_num;
   char _pad6[4];
 
