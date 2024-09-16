c29e2a2478403b99d91e4a2c49e7cf6242feebf2
Miguel Pozo <pragma37@gmail.com>
2024-09-16 18:46:40 +0200

    Use world space points for velocity_background

--- source/blender/draw/engines/eevee_next/shaders/eevee_velocity_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_velocity_lib.glsl
@@ -24,7 +24,6 @@ vec4 velocity_unpack(vec4 data)
  */
 vec4 velocity_surface(vec3 P_prv, vec3 P, vec3 P_nxt)
 {
-  /* NOTE: We don't use the drw_view.persmat to avoid adding the TAA jitter to the velocity. */
   vec2 prev_uv = project_point(camera_prev.persmat, P_prv).xy;
   vec2 curr_uv = project_point(camera_curr.persmat, P).xy;
   vec2 next_uv = project_point(camera_next.persmat, P_nxt).xy;
@@ -45,18 +44,15 @@ vec4 velocity_surface(vec3 P_prv, vec3 P, vec3 P_nxt)
 }
 
 /**
- * Given a view space view vector \a vV, compute the previous and next motion vectors for
+ * Given a background world point \a P, compute the previous and next motion vectors for
  * background pixels.
  * Returns uv space motion vectors in pairs (motion_prev.xy, motion_next.xy).
  */
-vec4 velocity_background(vec3 vV)
+vec4 velocity_background(vec3 P)
 {
-  /* Only transform direction to avoid losing precision. */
-  vec3 V = transform_direction(camera_curr.viewinv, vV);
-  /* NOTE: We don't use the drw_view.winmat to avoid adding the TAA jitter to the velocity. */
-  vec2 prev_uv = project_point(camera_prev.winmat, V).xy;
-  vec2 curr_uv = project_point(camera_curr.winmat, V).xy;
-  vec2 next_uv = project_point(camera_next.winmat, V).xy;
+  vec2 prev_uv = project_point(camera_prev.persmat, P).xy;
+  vec2 curr_uv = project_point(camera_curr.persmat, P).xy;
+  vec2 next_uv = project_point(camera_next.persmat, P).xy;
   /* NOTE: We output both vectors in the same direction so we can reuse the same vector
    * with RGRG swizzle in viewport. */
   vec4 motion = vec4(prev_uv - curr_uv, curr_uv - next_uv);
@@ -71,9 +67,8 @@ vec4 velocity_resolve(vec4 vector, vec2 uv, float depth)
   if (vector.x == VELOCITY_INVALID) {
     bool is_background = (depth == 1.0);
     if (is_background) {
-      /* NOTE: Use viewCameraVec to avoid imprecision if camera is far from origin. */
-      vec3 vV = drw_view_incident_vector(drw_point_screen_to_view(vec3(uv, 1.0)));
-      return velocity_background(vV);
+      vec3 P = drw_point_screen_to_world(vec3(uv, 1.0));
+      return velocity_background(P);
     }
     else {
       /* Static geometry. No translation in world space. */
