228c5814bb3a85652056de33c2c2c6263077a45b
Jeroen Bakker <jeroen@blender.org>
2024-09-16 14:21:17 +0200

    Vulkan: Refactor cached compiler instance
    ShaderC compiler was cached on the Vulkan backend. The compiler itself
    is light-weight and doesn't require any caching. This PR removes the
    cached instance from the backend.

--- source/blender/gpu/vulkan/vk_backend.cc
+++ source/blender/gpu/vulkan/vk_backend.cc
@@ -430,10 +430,6 @@ void VKBackend::render_end()
 
 void VKBackend::render_step() {}
 
-shaderc::Compiler &VKBackend::get_shaderc_compiler()
-{
-  return shaderc_compiler_;
-}
 
 void VKBackend::capabilities_init(VKDevice &device)
 {
