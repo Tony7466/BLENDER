147a1a8cd2bf2ccc9492d3a1355d474df6e08b64
Miguel Pozo <pragma37@gmail.com>
2024-09-17 19:17:01 +0200

    Feedback

--- source/blender/draw/engines/eevee_next/shaders/eevee_velocity_lib.glsl
+++ source/blender/draw/engines/eevee_next/shaders/eevee_velocity_lib.glsl
@@ -24,6 +24,8 @@ vec4 velocity_unpack(vec4 data)
  */
 vec4 velocity_surface(vec3 P_prv, vec3 P, vec3 P_nxt)
 {
+  /* NOTE: We use CameraData matrices instead of drw_view.persmat to avoid adding the TAA jitter to
+   * the velocity. */
   vec2 prev_uv = project_point(camera_prev.persmat, P_prv).xy;
   vec2 curr_uv = project_point(camera_curr.persmat, P).xy;
   vec2 next_uv = project_point(camera_next.persmat, P_nxt).xy;
@@ -44,14 +46,15 @@ vec4 velocity_surface(vec3 P_prv, vec3 P, vec3 P_nxt)
 }
 
 /**
- * Given a background view space point \a vP, compute the previous and next motion vectors for
+ * Given a view space view vector \a vV, compute the previous and next motion vectors for
  * background pixels.
  * Returns uv space motion vectors in pairs (motion_prev.xy, motion_next.xy).
  */
-vec4 velocity_background(vec3 vP)
+vec4 velocity_background(vec3 vV)
 {
-  /** NOTE: Transform as a direction to avoid imprecision if camera is far from origin. */
-  vec3 V = transform_direction(camera_curr.viewinv, vP);
+  vec3 V = transform_direction(camera_curr.viewinv, vV);
+  /* NOTE: We use CameraData matrices instead of drw_view.winmat to avoid adding the TAA jitter to
+   * the velocity. */
   vec2 prev_uv = project_point(camera_prev.winmat, transform_direction(camera_prev.viewmat, V)).xy;
   vec2 curr_uv = project_point(camera_curr.winmat, transform_direction(camera_curr.viewmat, V)).xy;
   vec2 next_uv = project_point(camera_next.winmat, transform_direction(camera_next.viewmat, V)).xy;
@@ -69,8 +72,9 @@ vec4 velocity_resolve(vec4 vector, vec2 uv, float depth)
   if (vector.x == VELOCITY_INVALID) {
     bool is_background = (depth == 1.0);
     if (is_background) {
-      vec3 vP = drw_point_screen_to_view(vec3(uv, 1.0));
-      return velocity_background(vP);
+      /* NOTE: Use view vector to avoid imprecision if camera is far from origin. */
+      vec3 vV = -drw_view_incident_vector(drw_point_screen_to_view(vec3(uv, 1.0)));
+      return velocity_background(vV);
     }
     else {
       /* Static geometry. No translation in world space. */
