800f4a4f0c759e036fd59d432b66d93b6455dbb4
secext2022 <secext2022@outlook.com>
2024-09-19 19:15:16 +0800

    Remove unnecessary comment.
    No more enable twice, should call enable after `enter` event.
    see `text_input_handle_enter` function.

--- intern/ghost/intern/GHOST_SystemWayland.cc
+++ intern/ghost/intern/GHOST_SystemWayland.cc
@@ -8920,8 +8920,6 @@ void GHOST_SystemWayland::ime_begin(const GHOST_WindowWayland *win,
     seat->ime.has_preedit = false;
     seat->ime.is_enabled = true;
 
-    /* No more enable twice, should call enable after `enter` event.
-     * see `text_input_handle_enter` function. */
     zwp_text_input_v3_enable(seat->wp.text_input);
     zwp_text_input_v3_commit(seat->wp.text_input);
 
