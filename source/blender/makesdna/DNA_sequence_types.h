51bbe56e3e22dec854f23b7c3dd2f1f74a9e0bad
Richard Antalik <richardantalik@gmail.com>
2024-10-01 04:14:06 +0200

    Cleanup: VSE: Remove seq->tmp usage
    Tmp field was still used in strip duplication. This was a source of bugs
    previously. Now it uses `Map` of new to old strips to fix strip
    relations.

--- source/blender/makesdna/DNA_sequence_types.h
+++ source/blender/makesdna/DNA_sequence_types.h
@@ -167,7 +167,7 @@ typedef struct SequenceRuntime {
 typedef struct Sequence {
   struct Sequence *next, *prev;
   /** Temp var for duplication, pointing to the newly duplicated Sequence. */
-  void *tmp;
+  void *tmp DNA_DEPRECATED;
   /** Needed (to be like ipo), else it will raise libdata warnings, this should never be used. */
   void *lib;
   /** SEQ_NAME_MAXSTR - name, set by default and needs to be unique, for RNA paths. */
