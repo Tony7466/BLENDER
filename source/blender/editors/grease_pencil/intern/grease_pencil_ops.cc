5227d856a3823e4efaae1372dab60aef87c3e5ed
Falk David <falk@blender.org>
2024-10-04 17:16:44 +0200

    Rename internal mode from `*GPENCIL_LEGACY` to `*GREASE_PENCIL`

--- source/blender/editors/grease_pencil/intern/grease_pencil_ops.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_ops.cc
@@ -89,8 +89,8 @@ bool grease_pencil_selection_poll(bContext *C)
    * paint mode. */
   if (!ELEM(object->mode,
             OB_MODE_EDIT_GPENCIL_LEGACY,
-            OB_MODE_SCULPT_GPENCIL_LEGACY,
-            OB_MODE_VERTEX_GPENCIL_LEGACY))
+            OB_MODE_SCULPT_GREASE_PENCIL,
+            OB_MODE_VERTEX_GREASE_PENCIL))
   {
     return false;
   }
@@ -103,7 +103,7 @@ bool grease_pencil_painting_poll(bContext *C)
     return false;
   }
   Object *object = CTX_data_active_object(C);
-  if ((object->mode & OB_MODE_PAINT_GPENCIL_LEGACY) == 0) {
+  if ((object->mode & OB_MODE_PAINT_GREASE_PENCIL) == 0) {
     return false;
   }
   ToolSettings *ts = CTX_data_tool_settings(C);
@@ -131,7 +131,7 @@ bool grease_pencil_sculpting_poll(bContext *C)
     return false;
   }
   Object *object = CTX_data_active_object(C);
-  if ((object->mode & OB_MODE_SCULPT_GPENCIL_LEGACY) == 0) {
+  if ((object->mode & OB_MODE_SCULPT_GREASE_PENCIL) == 0) {
     return false;
   }
   ToolSettings *ts = CTX_data_tool_settings(C);
@@ -147,7 +147,7 @@ bool grease_pencil_weight_painting_poll(bContext *C)
     return false;
   }
   Object *object = CTX_data_active_object(C);
-  if ((object->mode & OB_MODE_WEIGHT_GPENCIL_LEGACY) == 0) {
+  if ((object->mode & OB_MODE_WEIGHT_GREASE_PENCIL) == 0) {
     return false;
   }
   ToolSettings *ts = CTX_data_tool_settings(C);
@@ -163,7 +163,7 @@ bool grease_pencil_vertex_painting_poll(bContext *C)
     return false;
   }
   Object *object = CTX_data_active_object(C);
-  if ((object->mode & OB_MODE_VERTEX_GPENCIL_LEGACY) == 0) {
+  if ((object->mode & OB_MODE_VERTEX_GREASE_PENCIL) == 0) {
     return false;
   }
   ToolSettings *ts = CTX_data_tool_settings(C);
