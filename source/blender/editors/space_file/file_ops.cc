73351d13ef8ca995f581899648efb2e005d02965
Campbell Barton <campbell@blender.org>
2024-10-07 12:16:57 +1100

    Fix crash using "~" in the file selector when the "home" is null

--- source/blender/editors/space_file/file_ops.cc
+++ source/blender/editors/space_file/file_ops.cc
@@ -2768,9 +2768,15 @@ static void file_expand_directory(const Main *bmain, FileSelectParams *params)
     /* While path handling expansion typically doesn't support home directory expansion
      * in Blender, this is a convenience to be able to type in a single character.
      * Even though this is a UNIX convention, it's harmless to expand on WIN32 as well. */
-    char tmpstr[sizeof(params->dir) - 1];
-    STRNCPY(tmpstr, params->dir + 1);
-    BLI_path_join(params->dir, sizeof(params->dir), BKE_appdir_folder_home(), tmpstr);
+    if (const char *home = BKE_appdir_folder_home()) {
+      char tmpstr[sizeof(params->dir) - 1];
+      STRNCPY(tmpstr, params->dir + 1);
+      BLI_path_join(params->dir, sizeof(params->dir), home, tmpstr);
+    }
+    else {
+      /* Fall back to the default root. */
+      params->dir[0] = '\0';
+    }
   }
 
   else if (params->dir[0] == '\0')
