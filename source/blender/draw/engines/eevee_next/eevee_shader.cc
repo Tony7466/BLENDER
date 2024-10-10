be9f4bade582a37e36519bd6ebf8cbdea4a7fadc
Jeroen Bakker <jeroen@blender.org>
2024-10-10 10:35:59 +0200

    Fix by using macros. Pre processing should ignore the missing functions

--- source/blender/draw/engines/eevee_next/eevee_shader.cc
+++ source/blender/draw/engines/eevee_next/eevee_shader.cc
@@ -724,10 +724,6 @@ void ShaderModule::material_create_info_amend(GPUMaterial *gpumat, GPUCodegenOut
     const bool use_vertex_displacement = !codegen.displacement.empty() &&
                                          (displacement_type != MAT_DISPLACEMENT_BUMP) &&
                                          !ELEM(geometry_type, MAT_GEOM_WORLD, MAT_GEOM_VOLUME);
-
-    vert_gen << (use_vertex_displacement && !codegen.material_functions.empty() ?
-                     codegen.material_functions :
-                     "\n");
     vert_gen << "vec3 nodetree_displacement()\n";
     vert_gen << "{\n";
     vert_gen << ((use_vertex_displacement) ? codegen.displacement : "return vec3(0);\n");
